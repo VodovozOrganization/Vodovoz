0661d54532f3fbf70a9b540975144617ae4646d2
Andrey Gankov <gav@qsolution.ru>
2014-11-18 10:58:50 +0300

    Исправление доп. соглашений, Stetic не умеет работать с наследованием.

--- Vodovoz/Dialogs/AdditionalAgreementWater.cs
+++ Vodovoz/Dialogs/AdditionalAgreementWater.cs
@@ -1,13 +1,96 @@
 ﻿using System;
+using System.Data.Bindings;
+using NHibernate;
+using NLog;
 using QSOrmProject;
+using QSTDI;
 
 namespace Vodovoz
 {
 	[System.ComponentModel.ToolboxItem (true)]
-	public partial class AdditionalAgreementWater : AdditionalAgreementBase
+	public partial class AdditionalAgreementWater : Gtk.Bin, ITdiDialog, IOrmSlaveDialog
 	{
+		protected static Logger logger = LogManager.GetCurrentClassLogger();
+		protected ISession session;
+		protected Adaptor adaptor = new Adaptor();
+		protected IAdditionalAgreementOwner AgreementOwner;
+
+		public bool HasChanges {
+			get {return Session.IsDirty();}
+		}
+
+		#region ITdiTab implementation
+		public event EventHandler<TdiTabNameChangedEventArgs> TabNameChanged;
+		public event EventHandler<TdiTabCloseEventArgs> CloseTab;
+		public QSTDI.ITdiTabParent TabParent { get ; set ; }
+
+		protected string _tabName = "Новое доп. соглашение";
+		public string TabName
+		{
+			get{return _tabName;}
+			set{
+				if (_tabName == value)
+					return;
+				_tabName = value;
+				if (TabNameChanged != null)
+					TabNameChanged(this, new TdiTabNameChangedEventArgs(value));
+			}
+		}
+		#endregion
+
+		#region IOrmDialog implementation
+		public NHibernate.ISession Session {
+			get {
+				if (session == null)
+					Session = OrmMain.Sessions.OpenSession ();
+				return session;
+			}
+			set {
+				session = value;
+			}
+		}
+		#endregion
+
+		OrmParentReference parentReference;
+		public OrmParentReference ParentReference {
+			set {
+				parentReference = value;
+				if (parentReference != null) {
+					Session = parentReference.Session;
+					if (!(parentReference.ParentObject is IAdditionalAgreementOwner)) {
+						throw new ArgumentException (String.Format ("Родительский объект в parentReference должен реализовывать интерфейс {0}", typeof(IAdditionalAgreementOwner)));
+					}
+					AgreementOwner = (IAdditionalAgreementOwner)parentReference.ParentObject;
+				}
+			}
+			get {
+				return parentReference;
+			}
+		}
+
+		protected void OnButtonSaveClicked (object sender, EventArgs e)
+		{
+			if (!this.HasChanges || Save())
+				OnCloseTab(false);
+		}
+
+		protected void OnCloseTab(bool askSave)
+		{
+			if (TabParent.CheckClosingSlaveTabs ((ITdiTab)this))
+				return;
+
+			if (CloseTab != null)
+				CloseTab(this, new TdiTabCloseEventArgs(askSave));
+		}
+
+		public override void Destroy()
+		{
+			adaptor.Disconnect();
+			base.Destroy();
+		}
+
 		private WaterSalesAgreement subject;
-		public override object Subject {
+		public object Subject {
 			get {
 				return subject;
 			}
@@ -43,7 +126,7 @@ namespace Vodovoz
 			entryAgreementNumber.IsEditable = true;
 		}
 
-		public override bool Save ()
+		public bool Save ()
 		{
 			if (entryAgreementNumber.Text == String.Empty) {
 				logger.Warn("В доп. соглашении не заполнен номер - не сохраняем.");
