51584f2877e797dea5dcc1325a22bec1ded09ffd
anri-vin <anri.vin@gmail.com>
2014-12-26 12:51:41 +0300

    Обработка горячих клавиш. Добавлена платформо-зависимая обработка основных  (CTRL+C, CTRL+V, CTRL+X) горячих клавиш для главного окна.

--- Vodovoz/MainWindow.cs
+++ Vodovoz/MainWindow.cs
@@ -12,13 +12,14 @@ using QSContacts;
 
 public partial class MainWindow: Gtk.Window
 {
+	private static Gtk.Clipboard clipboard = Gtk.Clipboard.Get(Gdk.Atom.Intern("CLIPBOARD", false));
 	private static Logger logger = LogManager.GetCurrentClassLogger();
 	uint uiIdOrders, uiIdServices;
 
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
 	{
 		Build();
-
+		this.KeyReleaseEvent += HandleKeyReleaseEvent;
 		//Передаем лебл
 		MainClass.StatusBarLabel = labelStatus;
 		this.Title = QSSupportLib.MainSupport.GetTitle();
@@ -71,6 +72,71 @@ public partial class MainWindow: Gtk.Window
 
 		//Настраиваем виджет вкладок
 		tdiMain.CreateDialogWidget += OnCreateDialogWidget;
+	}
+
+	void HandleKeyReleaseEvent (object o, KeyReleaseEventArgs args)
+	{
+		int platform = (int)Environment.OSVersion.Platform;
+		int version = (int)Environment.OSVersion.Version.Major;
+		Gdk.ModifierType modifier;
+
+		//Kind of MacOSX
+		if ((platform == 4 || platform == 6 || platform == 128) && version > 8)
+			modifier = Gdk.ModifierType.MetaMask | Gdk.ModifierType.Mod1Mask;
+		//Kind of Windows or Unix
+		else
+			modifier = Gdk.ModifierType.ControlMask;
+		//CTRL+C	
+		if ((args.Event.Key == Gdk.Key.Cyrillic_es || args.Event.Key == Gdk.Key.Cyrillic_ES) && args.Event.State == modifier) {
+			Widget w = (o as MainWindow).Focus;
+			CopyToClipboard (w);
+		}//CTRL+X
+		else if ((args.Event.Key == Gdk.Key.Cyrillic_che || args.Event.Key == Gdk.Key.Cyrillic_CHE) && args.Event.State == modifier) {
+			Widget w = (o as MainWindow).Focus;
+			CutToClipboard (w);
+		}//CTRL+V
+		else if ((args.Event.Key == Gdk.Key.Cyrillic_em || args.Event.Key == Gdk.Key.Cyrillic_EM) && args.Event.State == modifier) {
+			Widget w = (o as MainWindow).Focus;
+			PasteFromClipboard (w);
+		}
+	}
+
+	void CopyToClipboard(Widget w)
+	{
+		int start, end;
+
+		if (w is Editable)
+			(w as Editable).CopyClipboard ();
+		else if (w is TextView)
+			(w as TextView).Buffer.CopyClipboard (clipboard);
+		else if (w is Label) {
+			(w as Label).GetSelectionBounds (out start, out end);
+			if (start != end)
+				clipboard.Text = (w as Label).Text.Substring (start, end - start);
+		}
+	}
+
+	void CutToClipboard(Widget w)
+	{
+		int start, end;
+
+		if (w is Editable)
+			(w as Editable).CutClipboard ();
+		else if (w is TextView)
+			(w as TextView).Buffer.CutClipboard (clipboard, true);
+		else if (w is Label) {
+			(w as Label).GetSelectionBounds (out start, out end);
+			if (start != end)
+				clipboard.Text = (w as Label).Text.Substring (start, end - start);
+		}
+	}
+
+	void PasteFromClipboard(Widget w)
+	{
+		if (w is Editable) 
+			(w as Editable).PasteClipboard ();
+		else if (w is TextView) 
+			(w as TextView).Buffer.PasteClipboard (clipboard);
 	}
 
 	void OnCreateDialogWidget (object sender, QSTDI.TdiOpenObjDialogEventArgs e)
