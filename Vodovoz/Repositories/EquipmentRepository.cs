4d9d7143443426e86793fd7402d637c2f6c70c51
anri-vin <anri.vin@gmail.com>
2015-06-26 17:00:38 +0300

    Добавлены репозитории для пакетов аренды и оборудования.

---
+++ Vodovoz/Repositories/EquipmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate.Criterion;
+using Vodovoz.Domain;
+using System.Collections.Generic;
+
+namespace Vodovoz.Repository
+{
+	public static class EquipmentRepository
+	{
+		public static QueryOver<Equipment> GetEquipmentWithTypesQuery (List<EquipmentType> types)
+		{
+			Nomenclature nomenclatureAlias = null;
+			var Query = QueryOver.Of<Equipment> ()
+				.JoinAlias (e => e.Nomenclature, () => nomenclatureAlias)
+				.Where (() => nomenclatureAlias.Type.IsIn (types));
+			return Query;
+		}
+		
+	}
+}
+
---
+++ Vodovoz/Repositories/FreeRentPackageRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Vodovoz.Domain;
+using QSOrmProject;
+using System.Linq;
+
+namespace Vodovoz
+{
+	public static class FreeRentPackageRepository
+	{
+		public static List<EquipmentType> GetPresentEquipmentTypes (IUnitOfWork uow)
+		{
+			var availableTypes = uow.Session.CreateCriteria (typeof(FreeRentPackage))
+				.List<FreeRentPackage> ()
+				.Select (p => p.EquipmentType)
+				.Distinct ().ToList ();
+			return availableTypes;
+		}
+	}
+}
+
