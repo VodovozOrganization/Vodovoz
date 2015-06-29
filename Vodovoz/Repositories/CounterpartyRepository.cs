f67f6c3f3288be12ea7bb21936ef1504761d94bf
anri-vin <anri.vin@gmail.com>
2015-06-29 15:27:01 +0300

    Обновил базу и репозитории

---
+++ Vodovoz/Repositories/CounterpartyRepository.cs
@@ -0,0 +1,21 @@
+﻿using QSOrmProject;
+using Vodovoz.Domain;
+
+
+namespace Vodovoz.Repository
+{
+	public class CounterpartyRepository
+	{
+		public static CounterpartyContract GetCounterpartyContract (IUnitOfWork uow)
+		{
+			CounterpartyContract contract = null;
+			Order order = uow.RootObject as Order;
+			return null;
+			//return uow.Session.QueryOver<CounterpartyContract> ()
+			//	.Where (co => co.Counterparty == order.Client && !co.IsArchive && !co.OnCancellation);
+		}
+
+
+	}
+}
+
---
+++ Vodovoz/Repositories/OrganizationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Vodovoz.Domain;
+using QSOrmProject;
+
+namespace Vodovoz.Repository
+{
+	public static class OrganizationRepository
+	{
+		public static Organization GetCashOrganization (IUnitOfWork uow)
+		{
+			return uow.GetById<Organization> (12);
+		}
+
+		public static Organization GetCashlessOrganization (IUnitOfWork uow)
+		{
+			return uow.GetById<Organization> (11);
+		}
+	}
+}
+
