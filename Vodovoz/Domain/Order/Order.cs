abc04ad695d5e3fa7e2e119c264f22886d08f6e9
Andrey Gankov <gav@qsolution.ru>
2015-07-22 13:14:20 +0300

    Вынес из зкаказа в отдельный файл перечисления.

--- Vodovoz/Domain/Order/Order.cs
+++ Vodovoz/Domain/Order/Order.cs
@@ -476,41 +476,6 @@ namespace Vodovoz.Domain.Orders
 				}
 			}
 		}
-	}
-
-	public enum OrderStatus
-	{
-		[ItemTitleAttribute ("Новый")] NewOrder,
-		[ItemTitleAttribute ("Принят")] Accepted,
-		[ItemTitleAttribute ("В маршрутном листе")]InTravelList,
-		[ItemTitleAttribute ("На погрузке")]OnLoading,
-		[ItemTitleAttribute ("В пути")]OnTheWay,
-		[ItemTitleAttribute ("Доставлен")]Shipped,
-		[ItemTitleAttribute ("Выгрузка на складе")]UnloadingOnStock,
-		[ItemTitleAttribute ("Отчет не закрыт")]ReportNotClosed,
-		[ItemTitleAttribute ("Закрыт")]Closed,
-		[ItemTitleAttribute ("Отменен")]Canceled,
-		[ItemTitleAttribute ("Недовоз")]NotDelivered
-	}
-
-	public class OrderStatusStringType : NHibernate.Type.EnumStringType
-	{
-		public OrderStatusStringType () : base (typeof(OrderStatus))
-		{
-		}
-	}
-
-	public enum OrderSignatureType
-	{
-		[ItemTitleAttribute ("По печати")]BySeal,
-		[ItemTitleAttribute ("По доверенности")]ByProxy
-	}
-
-	public class OrderSignatureTypeStringType : NHibernate.Type.EnumStringType
-	{
-		public OrderSignatureTypeStringType () : base (typeof(OrderSignatureType))
-		{
-		}
-	}
+	}		
 }
 
