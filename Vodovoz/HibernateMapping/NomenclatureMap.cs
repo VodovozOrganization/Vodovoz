670703aa8cdd6d3dd05a790e2d74cc3b46f22e41
Andrey Gankov <gav@qsolution.ru>
2015-07-27 14:21:24 +0300

    Ссылочные поля в базе данных переименованы с добавление Id

--- Vodovoz/HibernateMapping/NomenclatureMap.cs
+++ Vodovoz/HibernateMapping/NomenclatureMap.cs
@@ -18,10 +18,11 @@ namespace Vodovoz.HMap
 			Map (x => x.DoNotReserve).Column ("reserve");
 			Map (x => x.Serial).Column ("serial");
 			Map (x => x.Category).Column ("category").CustomType<NomenclatureCategoryStringType> ();
-			References (x => x.Unit).Column ("unit");
-			References (x => x.Color).Column ("color");
-			References (x => x.Type).Column ("type");
-			References (x => x.Manufacturer).Column ("manufacturer");
+			References (x => x.Unit).Column ("unit_id");
+			References (x => x.Color).Column ("color_id");
+			References (x => x.Type).Column ("type_id");
+			References (x => x.Manufacturer).Column ("manufacturer_id");
+			References (x => x.RouteListColumn).Column ("route_column_id");
 			HasMany (x => x.NomenclaturePrice).Cascade.AllDeleteOrphan ().LazyLoad ().KeyColumn ("nomenclature_id");
 		}
 	}
