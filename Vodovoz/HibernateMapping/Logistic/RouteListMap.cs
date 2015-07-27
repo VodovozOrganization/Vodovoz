92f23c2857de3389c2edde79f872917d6e4e1168
Andrey Gankov <gav@qsolution.ru>
2015-07-27 23:08:32 +0300

    Переписал мапинг маршрутного листа на Fluent

---
+++ Vodovoz/HibernateMapping/Logistic/RouteListMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Vodovoz.Domain.Logistic;
+using FluentNHibernate.Mapping;
+
+namespace Vodovoz.HMap
+{
+	public class RouteListMap : ClassMap<RouteList>
+	{
+		public RouteListMap ()
+		{
+			Table("route_lists");
+
+			Id(x => x.Id).Column ("id").GeneratedBy.Native();
+			Map(x => x.PlannedDistance).Column ("planned_distance");
+			Map(x => x.ActualDistance).Column ("actual_distance");
+			Map(x => x.Date).Column ("date");
+			Map(x => x.Status).Column ("status").CustomType<RouteListStatusStringType> ();
+			References (x => x.Car).Column ("car_id");
+			References (x => x.Driver).Column ("driver_id");
+			HasMany (x => x.Addresses).Cascade.AllDeleteOrphan ()
+				.KeyColumn ("route_list_id")
+				.AsList (x => x.Column ("order_in_route"));
+		}
+	}
+}
+
