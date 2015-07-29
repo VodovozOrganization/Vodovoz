2c98104fc557db3646cf2e18faff91c3d2f132e9
Andrey Gankov <gav@qsolution.ru>
2015-07-29 16:25:53 +0300

    Добавлен репозиторий колонки маршрутного листа.

---
+++ Vodovoz/Repositories/Logistics/RouteColumnRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QSOrmProject;
+using NHibernate.Criterion;
+using System.Collections.Generic;
+using Vodovoz.Domain.Logistic;
+
+namespace Vodovoz.Repository.Logistics
+{
+	public static class RouteColumnRepository
+	{
+		public static IList<RouteColumn> ActiveColumns (IUnitOfWork uow)
+		{
+			return uow.Session.QueryOver<RouteColumn> ().List<RouteColumn> ();
+		}
+	}
+}
+
