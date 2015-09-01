300865b3ac314d8cca0e6641ef45fe8bbf78889b
Andrey Gankov <gav@qsolution.ru>
2015-09-01 16:54:50 +0300

    Перенос мапинга оборудования в заказе на флуент.

---
+++ Vodovoz/HibernateMapping/Order/OrderEquipmentMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Orders;
+
+namespace Vodovoz.HMap
+{
+	public class OrderEquipmentMap : ClassMap<OrderEquipment>
+	{
+		public OrderEquipmentMap ()
+		{
+			Table ("order_equipment");
+			Not.LazyLoad ();
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+
+			Map (x => x.Direction).Column ("direction").CustomType<DirectionStringType> ();
+			Map (x => x.Reason).Column ("reason").CustomType<ReasonStringType> ();
+
+			References (x => x.Equipment).Column ("equipment_id");
+			References (x => x.OrderItem).Column ("order_item_id");
+			References (x => x.NewEquipmentNomenclature).Column ("new_eq_nomenclature_id");
+		}
+	}
+}
