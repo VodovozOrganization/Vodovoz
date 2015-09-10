ee6c78cd41db19c17bdb3fc7915e91df0c96e608
Andrei Vinogradov <anri.vin@gmail.com>
2015-09-10 14:17:10 +0300

    Перенес остатки доменной модели заказа на Fluent.

---
+++ Vodovoz/HibernateMapping/Order/OrderDepositRefundItemMap.cs
@@ -0,0 +1,25 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Orders;
+using Vodovoz.Domain.Operations;
+
+namespace Vodovoz.HMap
+{
+	public class OrderDepositRefundItemMap : ClassMap<OrderDepositRefundItem>
+	{
+		public OrderDepositRefundItemMap ()
+		{
+			Table ("order_deposit_refund_items");
+			Not.LazyLoad ();
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+
+			Map (x => x.RefundDeposit).Column ("refund_deposit");
+			Map (x => x.DepositType).Column ("deposit_type").CustomType<DepositTypeStringType> ();
+
+			References (x => x.Order).Column ("order_id");
+			References (x => x.DepositOperation).Column ("deposit_operation_id");
+			References (x => x.PaidRentItem).Column ("paid_rent_equipment_id");
+			References (x => x.FreeRentItem).Column ("free_rent_equipment_id");
+		}
+	}
+}
---
+++ Vodovoz/HibernateMapping/Order/OrderDocumentMap.cs
@@ -0,0 +1,36 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Orders;
+
+namespace Vodovoz.HMap
+{
+	public class OrderDocumentMap : ClassMap<OrderDocument>
+	{
+		public OrderDocumentMap ()
+		{
+			Table ("order_documents");
+			Not.LazyLoad ();
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+			DiscriminateSubClassesOnColumn ("type");
+			References (x => x.Order).Column ("order_id");
+		}
+	}
+
+	public class OrderAgreementMap : SubclassMap<OrderAgreement>
+	{
+		public OrderAgreementMap ()
+		{
+			DiscriminatorValue ("order_agreement");
+			References (x => x.AdditionalAgreement).Column ("agreement_id");
+		}
+	}
+
+	public class OrderContractMap : SubclassMap<OrderContract>
+	{
+		public OrderContractMap ()
+		{
+			DiscriminatorValue ("order_contract");
+			References (x => x.Contract).Column ("contract_id");
+		}
+	}
+}
