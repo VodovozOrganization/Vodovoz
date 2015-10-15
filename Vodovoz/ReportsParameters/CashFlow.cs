e529cab825b86ec3495927c97db61523b617b51e
Andrey Gankov <gav@qsolution.ru>
2015-10-15 14:24:03 +0300

    Расширенные варианты отчетов.

--- Vodovoz/ReportsParameters/CashFlow.cs
+++ Vodovoz/ReportsParameters/CashFlow.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using QSReport;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Gamma.Widgets;
 
 namespace Vodovoz.Reports
 {
@@ -9,6 +11,7 @@ namespace Vodovoz.Reports
 		public CashFlow ()
 		{
 			this.Build ();
+			comboPart.ItemsEnum = typeof(ReportParts);
 		}
 
 		#region IParametersWidget implementation
@@ -36,8 +39,23 @@ namespace Vodovoz.Reports
 
 		private ReportInfo GetReportInfo()
 		{
+			string ReportName;
+			if (checkDetail.Active) {
+				if (comboPart.SelectedItem.Equals (SpecialComboState.All))
+					ReportName = "Cash.CashFlowDetail";
+				else if (comboPart.SelectedItem.Equals (ReportParts.Income))
+					ReportName = "Cash.CashFlowDetailIncome";
+				else if (comboPart.SelectedItem.Equals (ReportParts.Expense))
+					ReportName = "Cash.CashFlowDetailExpense";
+				else if (comboPart.SelectedItem.Equals (ReportParts.Advance))
+					ReportName = "Cash.CashFlowDetailAdvance";
+				else
+					throw new InvalidOperationException ("Неизвестный раздел.");
+			} else
+				ReportName = "Cash.CashFlow";
+
 			return new ReportInfo {
-				Identifier = checkDetail.Active ? "Cash.CashFlowDetail" : "Cash.CashFlow",
+				Identifier = ReportName,
 				Parameters = new Dictionary<string, object> {
 					{"StartDate", dateperiodpicker1.StartDateOrNull.Value},
 					{"EndDate", dateperiodpicker1.EndDateOrNull.Value}
@@ -48,6 +66,21 @@ namespace Vodovoz.Reports
 		protected void OnDateperiodpicker1PeriodChanged (object sender, EventArgs e)
 		{
 			buttonRun.Sensitive = dateperiodpicker1.EndDateOrNull != null && dateperiodpicker1.StartDateOrNull != null;
+		}
+
+		protected void OnCheckDetailToggled (object sender, EventArgs e)
+		{
+			comboPart.Sensitive = checkDetail.Active;
+		}
+
+		enum ReportParts
+		{
+			[Display(Name = "Приход")]
+			Income,
+			[Display(Name = "Расход")]
+			Expense,
+			[Display(Name = "Авансы")]
+			Advance
 		}
 	}
 }
