4f7a310da9d2d1e58ed6ad395bafd6bfb5998a47
Andrei Vinogradov <anri.vin@gmail.com>
2015-10-22 12:31:13 +0300

    Добавил репозитории движений по безналу.

---
+++ Vodovoz/Repositories/Accounting/AccountExpenseRepository.cs
@@ -0,0 +1,23 @@
+﻿using QSOrmProject;
+using Vodovoz.Domain.Accounting;
+using QSBanks;
+
+namespace Vodovoz.Repository
+{
+	public static class AccountExpenseRepository
+	{
+		public static bool AccountExpenseExists (IUnitOfWork uow, int year, int number, string accountNumber)
+		{
+			Account accountAlias;
+
+			var expense = uow.Session.QueryOver<AccountExpense> ()
+				.JoinAlias (ae => ae.OrganizationAccount, () => accountAlias)
+				.Where (ae => ae.Date.Year == year &&
+			              ae.Number == number &&
+			              accountAlias.Number == accountNumber)
+				.SingleOrDefault<AccountExpense> ();
+			return expense != null;
+		}
+	}
+}
+
---
+++ Vodovoz/Repositories/Accounting/AccountIncomeRepository.cs
@@ -0,0 +1,27 @@
+﻿using QSOrmProject;
+using QSBanks;
+using Vodovoz.Domain.Accounting;
+using Vodovoz.Domain;
+
+namespace Vodovoz.Repository
+{
+	public static class AccountIncomeRepository
+	{
+		public static bool AccountIncomeExists (IUnitOfWork uow, int year, int number, string counterpartyInn, string accountNumber)
+		{
+			Account accountAlias;
+			Counterparty counterpartyAlias;
+
+			var income = uow.Session.QueryOver<AccountIncome> ()
+				.JoinAlias (ai => ai.CounterpartyAccount, () => accountAlias)
+				.JoinAlias (ai => ai.Counterparty, () => counterpartyAlias)
+				.Where (ai => ai.Date.Year == year &&
+			             ai.Number == number &&
+			             accountAlias.Number == accountNumber &&
+			             counterpartyAlias.INN == counterpartyInn)
+				.SingleOrDefault<AccountIncome> ();
+			return income != null;
+		}
+	}
+}
+
