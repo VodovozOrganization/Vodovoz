9eca84bcaf9340e1d0a31546e0f91ed0c6aa24ac
Andrei Vinogradov <anri.vin@gmail.com>
2015-10-28 09:38:26 +0300

    Прикрутил фильтр и удаление.

--- Vodovoz/JournalViewers/AccountingView.cs
+++ Vodovoz/JournalViewers/AccountingView.cs
@@ -3,6 +3,7 @@ using QSTDI;
 using NLog;
 using Vodovoz.ViewModel;
 using QSOrmProject;
+using Vodovoz.Domain.Accounting;
 
 namespace Vodovoz
 {
@@ -18,6 +19,28 @@ namespace Vodovoz
 			accountingFilter.UoW = UnitOfWorkFactory.CreateWithoutRoot ();
 			tableAccountingOperations.RepresentationModel = new AccountingVM (accountingFilter);
 			tableAccountingOperations.RepresentationModel.UpdateNodes ();
+			tableAccountingOperations.Selection.Changed += OnSelectionChanged;
+			buttonDelete.Sensitive = false;
+			accountingFilter.Visible = false;
+		}
+
+		void OnSelectionChanged (object sender, EventArgs e)
+		{
+			buttonDelete.Sensitive = (tableAccountingOperations.Selection.CountSelectedRows () > 0);
+		}
+
+		protected void OnButtonDeleteClicked (object sender, EventArgs e)
+		{
+			var node = (ViewModel.AccountingVMNode)tableAccountingOperations.GetSelectedNode ();
+			Type operationType = node.Expense == 0 ? typeof(AccountIncome) : typeof(AccountExpense);
+			if (OrmMain.DeleteObject (operationType, tableAccountingOperations.GetSelectedId ())) {
+				tableAccountingOperations.RepresentationModel.UpdateNodes ();
+			}
+		}
+
+		protected void OnCheckFilterToggled (object sender, EventArgs e)
+		{
+			accountingFilter.Visible = checkFilter.Active;
 		}
 	}
 }
--- Vodovoz/gtk-gui/Vodovoz.AccountingView.cs
+++ Vodovoz/gtk-gui/Vodovoz.AccountingView.cs
@@ -6,6 +6,12 @@ namespace Vodovoz
 	{
 		private global::Gtk.VBox vbox1;
 		
+		private global::Gtk.HBox hbox3;
+		
+		private global::Gtk.Button buttonDelete;
+		
+		private global::Gtk.CheckButton checkFilter;
+		
 		private global::Vodovoz.AccountingFilter accountingFilter;
 		
 		private global::Gtk.ScrolledWindow GtkScrolledWindow;
@@ -22,15 +28,51 @@ namespace Vodovoz
 			this.vbox1 = new global::Gtk.VBox ();
 			this.vbox1.Name = "vbox1";
 			this.vbox1.Spacing = 6;
+			this.vbox1.BorderWidth = ((uint)(6));
+			// Container child vbox1.Gtk.Box+BoxChild
+			this.hbox3 = new global::Gtk.HBox ();
+			this.hbox3.Name = "hbox3";
+			this.hbox3.Spacing = 6;
+			// Container child hbox3.Gtk.Box+BoxChild
+			this.buttonDelete = new global::Gtk.Button ();
+			this.buttonDelete.CanFocus = true;
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.UseUnderline = true;
+			this.buttonDelete.Label = global::Mono.Unix.Catalog.GetString ("Удалить");
+			global::Gtk.Image w1 = new global::Gtk.Image ();
+			w1.Pixbuf = global::Stetic.IconLoader.LoadIcon (this, "gtk-delete", global::Gtk.IconSize.Menu);
+			this.buttonDelete.Image = w1;
+			this.hbox3.Add (this.buttonDelete);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.hbox3 [this.buttonDelete]));
+			w2.Position = 0;
+			w2.Expand = false;
+			w2.Fill = false;
+			// Container child hbox3.Gtk.Box+BoxChild
+			this.checkFilter = new global::Gtk.CheckButton ();
+			this.checkFilter.CanFocus = true;
+			this.checkFilter.Name = "checkFilter";
+			this.checkFilter.Label = global::Mono.Unix.Catalog.GetString ("Фильтр");
+			this.checkFilter.DrawIndicator = false;
+			this.checkFilter.UseUnderline = true;
+			this.hbox3.Add (this.checkFilter);
+			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox3 [this.checkFilter]));
+			w3.PackType = ((global::Gtk.PackType)(1));
+			w3.Position = 1;
+			w3.Expand = false;
+			this.vbox1.Add (this.hbox3);
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.hbox3]));
+			w4.Position = 0;
+			w4.Expand = false;
+			w4.Fill = false;
 			// Container child vbox1.Gtk.Box+BoxChild
 			this.accountingFilter = new global::Vodovoz.AccountingFilter ();
 			this.accountingFilter.Events = ((global::Gdk.EventMask)(256));
 			this.accountingFilter.Name = "accountingFilter";
 			this.vbox1.Add (this.accountingFilter);
-			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.accountingFilter]));
-			w1.Position = 0;
-			w1.Expand = false;
-			w1.Fill = false;
+			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.accountingFilter]));
+			w5.Position = 1;
+			w5.Expand = false;
+			w5.Fill = false;
 			// Container child vbox1.Gtk.Box+BoxChild
 			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
 			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
@@ -46,13 +88,15 @@ namespace Vodovoz
 			this.tableAccountingOperations.InheritedBoundaryDataSource = false;
 			this.GtkScrolledWindow.Add (this.tableAccountingOperations);
 			this.vbox1.Add (this.GtkScrolledWindow);
-			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.GtkScrolledWindow]));
-			w3.Position = 1;
+			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.GtkScrolledWindow]));
+			w7.Position = 2;
 			this.Add (this.vbox1);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
 			this.Hide ();
+			this.buttonDelete.Clicked += new global::System.EventHandler (this.OnButtonDeleteClicked);
+			this.checkFilter.Toggled += new global::System.EventHandler (this.OnCheckFilterToggled);
 		}
 	}
 }
