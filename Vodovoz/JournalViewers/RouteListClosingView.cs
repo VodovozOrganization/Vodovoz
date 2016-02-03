b9de565f7a2289848c73610dfa2c04b9ab71ab99
Dmitry Nikitin <baragon92@gmail.com>
2016-02-03 10:24:38 +0300

    Недостающие файлы

---
+++ Vodovoz/JournalViewers/RouteListClosingView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using QSOrmProject;
+using Vodovoz.Domain.Logistic;
+using QSTDI;
+
+namespace Vodovoz
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RouteListClosingView : TdiTabBase
+	{
+
+		private IUnitOfWork uow;
+
+		ViewModel.RouteListsVM viewModel;
+
+		public IUnitOfWork UoW {
+			get {
+				return uow;
+			}
+			set {
+				if (uow == value)
+					return;
+				uow = value;
+				viewModel = new ViewModel.RouteListsVM (value);
+				viewModel.Filter = new RouteListsFilter(uow);
+				viewModel.Filter.RestrictStatus = RouteListStatus.ReadyToReport;
+				treeRouteLists.RepresentationModel = viewModel;
+				treeRouteLists.RepresentationModel.UpdateNodes ();
+			}
+		}
+				
+		public RouteListClosingView()
+		{
+			this.Build();
+			this.TabName = "Готовые к закрытию";
+			UoW = UnitOfWorkFactory.CreateWithoutRoot ();
+			treeRouteLists.Selection.Changed += OnSelectionChanged;
+		}
+
+		void OnSelectionChanged(object sender, EventArgs args)
+		{
+			buttonCloseRouteList.Sensitive = treeRouteLists.Selection.CountSelectedRows() > 0;
+		}
+
+		protected void OnButtonCloseRouteListClicked (object sender, EventArgs e)
+		{
+			var node = treeRouteLists.GetSelectedNode () as ViewModel.RouteListsVMNode;
+			var dlg = new RouteListClosingDlg (node.Id);
+			TabParent.AddTab (dlg, this);
+		}
+
+		protected void OnRouteListActivated (object o, Gtk.RowActivatedArgs args)
+		{
+			OnButtonCloseRouteListClicked(o, args);
+		}
+	}
+}
+
