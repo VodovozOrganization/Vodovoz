f5ddb9e36b84b601f0e1402f97e50ac45477ec67
Andrey Gankov <gav@qsolution.ru>
2016-02-03 14:59:05 +0300

    Мерж

--- Vodovoz/Representations/OrdersVM.cs
+++ Vodovoz/Representations/OrdersVM.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Gamma.ColumnConfig;
 using Gamma.Utilities;
+using Gtk;
 using NHibernate;
 using NHibernate.Transform;
 using QSOrmProject;
 using QSOrmProject.RepresentationModel;
 using Vodovoz.Domain;
 using Vodovoz.Domain.Orders;
-using System.Linq;
 
 namespace Vodovoz.ViewModel
 {
@@ -85,7 +86,76 @@ namespace Vodovoz.ViewModel
 			get { return columnsConfig; }
 		}
 
+		public override bool PopupMenuExist
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public override Gtk.Menu GetPopupMenu(RepresentationSelectResult[] selected)
+		{
+			lastMenuSelected = selected;
+
+			Menu popupMenu = new Gtk.Menu();
+			Gtk.MenuItem menuItemYandex = new MenuItem("Открыть на Yandex картах(координаты)");
+			menuItemYandex.Activated += MenuItemYandex_Activated; 
+			popupMenu.Add(menuItemYandex);
+			Gtk.MenuItem menuItemYandexAddress = new MenuItem("Открыть на Yandex картах(адрес)");
+			menuItemYandexAddress.Activated += MenuItemYandexAddress_Activated;
+			popupMenu.Add(menuItemYandexAddress);
+			Gtk.MenuItem menuItemOSM = new MenuItem("Открыть на карте OSM");
+			menuItemOSM.Activated += MenuItemOSM_Activated;
+			popupMenu.Add(menuItemOSM);
+			return popupMenu;
+		}
+
 		#endregion
+
+		private RepresentationSelectResult[] lastMenuSelected;
+
+		void MenuItemOSM_Activated (object sender, EventArgs e)
+		{
+			foreach(var sel in lastMenuSelected)
+			{
+				var order = UoW.GetById<Order>(sel.EntityId);
+				if (order.DeliveryPoint == null || order.DeliveryPoint.Latitude == null || order.DeliveryPoint.Longitude == null)
+					continue;
+
+				System.Diagnostics.Process.Start(String.Format(CultureInfo.InvariantCulture, "http://www.openstreetmap.org/#map=17/{1}/{0}", order.DeliveryPoint.Longitude, order.DeliveryPoint.Latitude));
+			}
+		}
+
+		void MenuItemYandexAddress_Activated (object sender, EventArgs e)
+		{
+			foreach(var sel in lastMenuSelected)
+			{
+				var order = UoW.GetById<Order>(sel.EntityId);
+				if (order.DeliveryPoint == null)
+					continue;
+
+				System.Diagnostics.Process.Start(
+					String.Format(CultureInfo.InvariantCulture, 
+						"https://maps.yandex.ru/?text={0} {1} {2}", 
+						order.DeliveryPoint.City,
+						order.DeliveryPoint.Street,
+						order.DeliveryPoint.Building
+					));
+			}
+		}
+
+		void MenuItemYandex_Activated (object sender, EventArgs e)
+		{
+			foreach(var sel in lastMenuSelected)
+			{
+				var order = UoW.GetById<Order>(sel.EntityId);
+				if (order.DeliveryPoint == null || order.DeliveryPoint.Latitude == null || order.DeliveryPoint.Longitude == null)
+					continue;
+
+				System.Diagnostics.Process.Start(String.Format(CultureInfo.InvariantCulture, "https://maps.yandex.ru/?ll={0},{1}&z=17", order.DeliveryPoint.Longitude, order.DeliveryPoint.Latitude));
+			}
+		}
 
 		#region implemented abstract members of RepresentationModelBase
 
