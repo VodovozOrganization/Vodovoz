d58ae5b4bab58d816cbbedaa9783f816fd3bf313
Dmitry Nikitin <baragon92@gmail.com>
2016-02-16 16:39:49 +0300

    Зарплаты перенесены в отдельный файл

--- Vodovoz/Domain/Logistic/RouteListItem.cs
+++ Vodovoz/Domain/Logistic/RouteListItem.cs
@@ -236,7 +236,7 @@ namespace Vodovoz.Domain.Logistic
 			if (WithoutForwarder)
 				return 0;
 
-			if (!IsDelivered())
+			if (Status != RouteListItemStatus.Completed)
 				return 0;
 			
 			bool largeOrder = fullBottleCount >= rates.LargeOrderMinimumBottles;
@@ -348,7 +348,7 @@ namespace Vodovoz.Domain.Logistic
 			var routeListUnloaded = (RouteList.Status == RouteListStatus.ReadyToReport) || 
 				(RouteList.Status == RouteListStatus.MileageCheck) ||
 				(RouteList.Status==RouteListStatus.Closed);
-			return Status == RouteListItemStatus.Completed || Status == RouteListItemStatus.EnRoute && routeListUnloaded;
+			return Status == RouteListItemStatus.Completed || Status == RouteListItemStatus.EnRoute && routeListUnloaded; 
 		}
 
 		public int GetFullBottlesDeliveredCount(){
@@ -420,51 +420,5 @@ namespace Vodovoz.Domain.Logistic
 		{
 		}
 	}
-
-	public class Wages{
-		public static Rates GetDriverRates(bool withForwarder=false){
-			return new Rates
-			{
-				PhoneServiceCompensationRate = 2,
-				FullBottleRate = withForwarder ? 10 : 15,
-				EmptyBottleRate = 5,
-				CoolerRate = withForwarder ? 20 : 30,
-				PaymentPerAddress = 50,
-				LargeOrderFullBottleRate = withForwarder ? 7 : 9,
-				LargeOrderEmptyBottleRate = 1,
-				LargeOrderMinimumBottles = 100,
-				SmallFullBottleRate = withForwarder ? 2 : 3,
-				ContractCancelationRate = withForwarder ? 20 : 30,
-			};
-		}
-
-		public static Rates GetForwarderRates(){
-			return new Rates
-			{
-				 PhoneServiceCompensationRate=0,
-				 FullBottleRate = 5,
-				 EmptyBottleRate = 5,
-				 CoolerRate = 10,
-				 LargeOrderFullBottleRate = 4,
-				 LargeOrderEmptyBottleRate = 1,
-				 SmallFullBottleRate = 1,
-				 LargeOrderMinimumBottles = 100,
-				 ContractCancelationRate = 10
-			};
-		}
-
-		public class Rates{
-			public decimal PhoneServiceCompensationRate;
-			public decimal FullBottleRate;
-			public decimal EmptyBottleRate;
-			public decimal CoolerRate;
-			public decimal PaymentPerAddress;
-			public decimal LargeOrderFullBottleRate;
-			public decimal LargeOrderEmptyBottleRate;
-			public int LargeOrderMinimumBottles;
-			public decimal SmallFullBottleRate;
-			public decimal ContractCancelationRate;
-		}
-	}				
 }
 
---
+++ Vodovoz/Domain/Wages.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Vodovoz.Domain
+{
+	public class Wages{
+		public static Rates GetDriverRates(bool withForwarder=false){
+			return new Rates
+			{
+				PhoneServiceCompensationRate = 2,
+				FullBottleRate = withForwarder ? 10 : 15,
+				EmptyBottleRate = 5,
+				CoolerRate = withForwarder ? 20 : 30,
+				PaymentPerAddress = 50,
+				LargeOrderFullBottleRate = withForwarder ? 7 : 9,
+				LargeOrderEmptyBottleRate = 1,
+				LargeOrderMinimumBottles = 100,
+				SmallFullBottleRate = withForwarder ? 2 : 3,
+				ContractCancelationRate = withForwarder ? 20 : 30,
+			};
+		}
+
+		public static Rates GetForwarderRates(){
+			return new Rates
+			{
+				PhoneServiceCompensationRate=0,
+				FullBottleRate = 5,
+				EmptyBottleRate = 5,
+				CoolerRate = 10,
+				LargeOrderFullBottleRate = 4,
+				LargeOrderEmptyBottleRate = 1,
+				SmallFullBottleRate = 1,
+				LargeOrderMinimumBottles = 100,
+				ContractCancelationRate = 10
+			};
+		}
+
+		public class Rates{
+			public decimal PhoneServiceCompensationRate;
+			public decimal FullBottleRate;
+			public decimal EmptyBottleRate;
+			public decimal CoolerRate;
+			public decimal PaymentPerAddress;
+			public decimal LargeOrderFullBottleRate;
+			public decimal LargeOrderEmptyBottleRate;
+			public int LargeOrderMinimumBottles;
+			public decimal SmallFullBottleRate;
+			public decimal ContractCancelationRate;
+		}
+	}		
+}
+
