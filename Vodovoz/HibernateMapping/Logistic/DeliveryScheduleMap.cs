52198fb960e885541afcd2e3af1bb17c8ffd2cf3
Andrey Gankov <gav@qsolution.ru>
2016-04-18 17:18:59 +0300

    Перенес мапинг времени доставки на флуент.

---
+++ Vodovoz/HibernateMapping/Logistic/DeliveryScheduleMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataAccess.NhibernateFixes;
+using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Logistic;
+
+namespace Vodovoz.HMap
+{
+	public class DeliveryScheduleMap : ClassMap<DeliverySchedule>
+	{
+		public DeliveryScheduleMap ()
+		{
+			Table("delivery_schedule");
+			Not.LazyLoad ();
+
+			Id(x => x.Id).Column ("id").GeneratedBy.Native();
+			Map(x => x.Name).Column ("name");
+			Map(x => x.From).Column ("from_time").CustomType<TimeAsTimeSpanTypeClone>();
+			Map(x => x.To).Column ("to_time").CustomType<TimeAsTimeSpanTypeClone>();
+		}
+	}
+}
+
