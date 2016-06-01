926924d6d51ba66641190403a74aa0f3ef2895d8
Andrey Gankov <gav@qsolution.ru>
2016-06-01 16:07:54 +0300

    Удалил не используемую и глупую функцию.

--- Vodovoz/Repositories/NomenclatureRepository.cs
+++ Vodovoz/Repositories/NomenclatureRepository.cs
@@ -8,10 +8,6 @@ namespace Vodovoz.Repository
 {
 	public static class NomenclatureRepository
 	{
-		public static Nomenclature GetDefaultBottle (IUnitOfWork uow)
-		{
-			return uow.GetById<Nomenclature> (32);
-		}
 
 		public static QueryOver<Nomenclature> NomenclatureForProductMaterialsQuery ()
 		{
