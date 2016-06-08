2a3a8d461de7cb11a86b0174f10d1003c467e704
Andrey Gankov <gav@qsolution.ru>
2016-06-08 16:52:46 +0300

    Исправил статусы клиентов и значимость.

---
+++ VodovozBusiness/HibernateMapping/Counterparty/CounterpartyStatusMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Client;
+
+namespace Vodovoz.HMap
+{
+	public class CounterpartyStatusMap : ClassMap<CounterpartyStatus>
+	{
+		public CounterpartyStatusMap ()
+		{
+			Table ("counterparty_status");
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+			Map (x => x.Name).Column ("name");
+		}
+	}
+}
