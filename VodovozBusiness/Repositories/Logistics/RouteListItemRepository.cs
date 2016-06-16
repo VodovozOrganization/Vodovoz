d7eb44f65c58e68c201509a27fe78418c03e4867
Andrei <anri.vin@gmail.com>
2016-06-16 18:31:23 +0300

    Added RouteListItem repository

---
+++ VodovozBusiness/Repositories/Logistics/RouteListItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Vodovoz.Domain.Logistic;
+using QSOrmProject;
+using Vodovoz.Domain.Orders;
+
+namespace Vodovoz.Repository.Logistics
+{
+	public static class RouteListItemRepository
+	{
+		public static RouteListItem GetRouteListItemForOrder(IUnitOfWork uow, Order order)
+		{
+			RouteListItem routeListItemAlias = null;
+
+			return uow.Session.QueryOver<RouteListItem> (() => routeListItemAlias)
+				.Where (() => routeListItemAlias.Order == order)
+				.SingleOrDefault ();
+		}
+	}
+}
+
