41be8fc294a055a0811e1fe27b179152d9eb792c
Andrey Gankov <gav@qsolution.ru>
2016-06-22 20:33:07 +0300

    Перенес значимост контрагента в пространство клиент. Добавил флуент мапинг.

---
+++ VodovozBusiness/HibernateMapping/Counterparty/SignificanceMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Client;
+
+namespace Vodovoz.HMap
+{
+	public class SignificanceMap : ClassMap<Significance>
+	{
+		public SignificanceMap ()
+		{
+			Table ("significance");
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+			Map (x => x.Name).Column ("name");
+		}
+	}
+}
