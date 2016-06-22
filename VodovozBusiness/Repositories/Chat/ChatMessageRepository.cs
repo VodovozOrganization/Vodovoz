3d6f628a801085f14c2f7bd7dd30ecc7d2e03566
Andrei <anri.vin@gmail.com>
2016-06-22 17:52:33 +0300

    Added chat message repo

---
+++ VodovozBusiness/Repositories/Chat/ChatMessageRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using QSOrmProject;
+using Vodovoz.Domain.Employees;
+using Vodovoz.Domain.Chat;
+using ChatClass = Vodovoz.Domain.Chat.Chat;
+using System.Collections.Generic;
+
+namespace Vodovoz.Repository.Chat
+{
+	public static class ChatMessageRepository
+	{
+		public static IList<ChatMessage> GetChatMessagesForPeriod(IUnitOfWork uow, ChatClass chat, int days) {
+			ChatMessage chatMessageAlias = null;
+
+			return uow.Session.QueryOver<ChatMessage> (() => chatMessageAlias)
+				.Where (() => chatMessageAlias.Chat.Id == chat.Id)
+				.Where (() => chatMessageAlias.DateTime.Date >= DateTime.Now.Date.AddDays(-days))
+				.OrderBy(() => chatMessageAlias.DateTime).Asc
+				.List();
+		}
+	}
+}
+
