b3e3cf426aa716d123665107669cae33ab74e411
Andrei <anri.vin@gmail.com>
2016-06-22 11:20:11 +0300

    Added chat repository

---
+++ VodovozBusiness/Repositories/Chat/ChatRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using QSOrmProject;
+using Vodovoz.Domain.Employees;
+using Vodovoz.Domain.Chat;
+using ChatClass = Vodovoz.Domain.Chat.Chat;
+
+namespace Vodovoz.Repository.Chat
+{
+	public static class ChatRepository
+	{
+		public static ChatClass GetChatForDriver(IUnitOfWork uow, Employee driver) {
+			ChatClass chatAlias = null;
+
+			return uow.Session.QueryOver<ChatClass> (() => chatAlias)
+				.Where (() => chatAlias.ChatType == ChatType.DriverAndLogists)
+				.Where (() => chatAlias.Driver.Id == driver.Id)
+				.SingleOrDefault();
+		}
+	}
+}
+
