da30ae67b30f726ee8d0e208282c40cdce998018
Andrey Gankov <gav@qsolution.ru>
2016-06-24 11:27:33 +0300

    Исправлена работа с серийными номерами.

--- Vodovoz/ViewWidgets/Store/EquipmentReceptionView.cs
+++ Vodovoz/ViewWidgets/Store/EquipmentReceptionView.cs
@@ -21,12 +21,12 @@ namespace Vodovoz
 	{
 		IList<ServiceClaim> serviceClaims;
 
-		GenericObservableList<ReceptionItemNode> ReceptionEquipmentList = new GenericObservableList<ReceptionItemNode>();
+		GenericObservableList<ReceptionEquipmentItemNode> ReceptionEquipmentList = new GenericObservableList<ReceptionEquipmentItemNode>();
 
 		MenuItem menuitemSelectFromClient;
 		MenuItem menuitemRegisterSerial;
 
-		ReceptionItemNode equipmentToSetSerial;
+		ReceptionEquipmentItemNode equipmentToSetSerial;
 
 		string colTitleServiceClaim = "Заявка на сервис";
 
@@ -34,15 +34,12 @@ namespace Vodovoz
 		{
 			this.Build();
 
-			ytreeEquipment.ColumnsConfig = Gamma.GtkWidgets.ColumnsConfigFactory.Create<ReceptionItemNode> ()
+			ytreeEquipment.ColumnsConfig = Gamma.GtkWidgets.ColumnsConfigFactory.Create<ReceptionEquipmentItemNode> ()
 				.AddColumn ("Номенклатура").AddTextRenderer (node => node.Name)
 				.AddColumn ("Серийный номер").AddTextRenderer (node => node.Serial)
 				.AddColumn ("Кол-во")
 				.AddToggleRenderer (node => node.Returned, false)						
-				.AddSetter ((cell, node) => cell.Visible = node.Trackable)
 				.AddNumericRenderer (node => node.Amount, false)
-				.Adjustment (new Gtk.Adjustment (0, 0, 9999, 1, 100, 0))
-				.AddSetter ((cell, node) => cell.Editable = !node.Trackable)
 				.AddColumn(colTitleServiceClaim)
 				.AddComboRenderer(node=>node.ServiceClaim)
 				.Editing()
@@ -91,8 +88,8 @@ namespace Vodovoz
 					serviceClaims = RouteList.Addresses
 					.SelectMany(address => address.Order.InitialOrderService)
 					.ToList();
-					var column = (ColumnMapping<ReceptionItemNode>)ytreeEquipment.ColumnsConfig.ConfiguredColumns.First(x => x.Title == colTitleServiceClaim);
-					var cell = (ComboRendererMapping<ReceptionItemNode>) column.ConfiguredRenderersGeneric.First();
+					var column = (ColumnMapping<ReceptionEquipmentItemNode>)ytreeEquipment.ColumnsConfig.ConfiguredColumns.First(x => x.Title == colTitleServiceClaim);
+					var cell = (ComboRendererMapping<ReceptionEquipmentItemNode>) column.ConfiguredRenderersGeneric.First();
 					cell.FillItems<ServiceClaim>(serviceClaims.Where(sc=>sc.Equipment==null).ToList());
 					FillListEquipmentFromRoute();
 				}	
@@ -107,7 +104,7 @@ namespace Vodovoz
 
 		void FillListEquipmentFromRoute(){
 			ReceptionEquipmentList.Clear();
-			ReceptionItemNode resultAlias = null;
+			ReceptionEquipmentItemNode resultAlias = null;
 			Vodovoz.Domain.Orders.Order orderAlias = null;
 			Equipment equipmentAlias = null;
 			OrderEquipment orderEquipmentAlias = null;
@@ -120,7 +117,7 @@ namespace Vodovoz
 				.JoinAlias(()=>equipmentAlias.Nomenclature,()=> equipNomenclatureAlias, NHibernate.SqlCommand.JoinType.LeftOuterJoin)
 				.JoinAlias(()=> orderEquipmentAlias.NewEquipmentNomenclature, ()=> newEqupNomenclatureAlias, NHibernate.SqlCommand.JoinType.LeftOuterJoin)
 				.SelectList (list => list
-					.Select (() => equipmentAlias.Id).WithAlias (() => resultAlias.Id)
+					.Select (() => equipmentAlias.Id).WithAlias (() => resultAlias.EquipmentId)
 					.Select (Projections.Conditional(
 						Restrictions.Where(() => equipNomenclatureAlias.Id == null),
 						Projections.Property(() => newEqupNomenclatureAlias.Id),
@@ -135,8 +132,9 @@ namespace Vodovoz
 						Projections.Constant(false)
 					)).WithAlias (() => resultAlias.IsNew)
 				)
-				.TransformUsing (Transformers.AliasToBean<ReceptionItemNode> ())
-				.List<ReceptionItemNode> ();
+				.TransformUsing (Transformers.AliasToBean<ReceptionEquipmentItemNode> ())
+				.List<ReceptionEquipmentItemNode> ();
+
 			foreach (var equipment in equipmentItems)
 				ReceptionEquipmentList.Add (equipment);		
 		}
@@ -146,7 +144,7 @@ namespace Vodovoz
 			buttonSelectSerial.Sensitive 
 			= ytreeEquipment.Selection.CountSelectedRows() > 0;
 
-			var item = ytreeEquipment.GetSelectedObject<ReceptionItemNode>();
+			var item = ytreeEquipment.GetSelectedObject<ReceptionEquipmentItemNode>();
 
 			if (item != null && menuitemSelectFromClient != null)
 				menuitemSelectFromClient.Sensitive = item.ServiceClaim != null;
@@ -156,16 +154,17 @@ namespace Vodovoz
 
 		void MenuitemSelectFromUnused_Activated (object sender, EventArgs e)
 		{
-			equipmentToSetSerial = ytreeEquipment.GetSelectedObject<ReceptionItemNode>();
+			equipmentToSetSerial = ytreeEquipment.GetSelectedObject<ReceptionEquipmentItemNode>();
 			var nomenclature = MyOrmDialog.UoW.GetById<Nomenclature>(equipmentToSetSerial.NomenclatureId);
 			var selectUnusedEquipment = new OrmReference(EquipmentRepository.GetUnusedEquipment(nomenclature));
+			selectUnusedEquipment.Mode = OrmReferenceMode.Select;
 			selectUnusedEquipment.ObjectSelected += SelectUnusedEquipment_ObjectSelected;
 			MyTab.TabParent.AddSlaveTab(MyTab, selectUnusedEquipment);
 		}
 
 		void MenuitemSelectFromClient_Activated (object sender, EventArgs e)
 		{
-			equipmentToSetSerial = ytreeEquipment.GetSelectedObject<ReceptionItemNode>();
+			equipmentToSetSerial = ytreeEquipment.GetSelectedObject<ReceptionEquipmentItemNode>();
 			var filter = new ClientBalanceFilter(UnitOfWorkFactory.CreateWithoutRoot());
 			filter.RestrictCounterparty = equipmentToSetSerial.ServiceClaim.Counterparty;
 			filter.RestrictNomenclature = filter.UoW.GetById<Nomenclature>(equipmentToSetSerial.NomenclatureId);
@@ -180,7 +179,7 @@ namespace Vodovoz
 		{
 			var equipment = MyOrmDialog.UoW.GetById<Equipment>((e.Subject as Equipment).Id);
 			equipmentToSetSerial.NewEquipment = equipment;
-			equipmentToSetSerial.Id = equipment.Id;
+			equipmentToSetSerial.EquipmentId = equipment.Id;
 			equipmentToSetSerial.Returned = true;
 			//OnEquipmentListChanged();
 		}
@@ -189,7 +188,7 @@ namespace Vodovoz
 		{
 			var equipment = MyOrmDialog.UoW.GetById<Equipment>(e.ObjectId);
 			equipmentToSetSerial.NewEquipment = equipment;
-			equipmentToSetSerial.Id = equipment.Id;
+			equipmentToSetSerial.EquipmentId = equipment.Id;
 			equipmentToSetSerial.Returned = true;
 			//OnEquipmentListChanged();
 		}
@@ -201,8 +200,8 @@ namespace Vodovoz
 
 		private void RegisterSerial()
 		{
-			var itemNode = ytreeEquipment.GetSelectedObject () as ReceptionItemNode;
-			if (itemNode.IsNew && itemNode.Id==0) {
+			var itemNode = ytreeEquipment.GetSelectedObject () as ReceptionEquipmentItemNode;
+			if (itemNode.IsNew && itemNode.EquipmentId==0) {
 				var dlg = EquipmentGenerator.CreateOne (itemNode.NomenclatureId);
 				dlg.EquipmentCreated += OnEquipmentRegistered;
 				if (!MyTab.TabParent.CheckClosingSlaveTabs (MyTab)) {					
@@ -215,11 +214,67 @@ namespace Vodovoz
 		protected void OnEquipmentRegistered(object o, EquipmentCreatedEventArgs args){
 			var equipment = MyOrmDialog.UoW.GetById<Equipment>(args.Equipment[0].Id);
 			equipmentToSetSerial.NewEquipment = equipment;
-			equipmentToSetSerial.Id = equipment.Id;
+			equipmentToSetSerial.EquipmentId = equipment.Id;
 			equipmentToSetSerial.Returned = true;
 			//OnEquipmentListChanged();
 		}
 
 	}
+
+	public class ReceptionEquipmentItemNode : PropertyChangedBase{
+
+		public NomenclatureCategory NomenclatureCategory{ get; set; }
+		public int NomenclatureId{ get; set; }
+		public string Name{get;set;}
+
+		int amount;
+
+		public virtual int Amount {
+			get{ return amount;}
+			set{
+				SetField(ref amount, value, ()=>Amount);
+			}
+		}
+
+		int equipmentId;
+		[PropertyChangedAlso ("Serial")]
+		public int EquipmentId
+		{
+			get
+			{
+				return equipmentId;
+			}
+			set
+			{
+				SetField (ref equipmentId, value, () => EquipmentId);
+			}
+		}
+
+		public string Serial{ get { 			
+				return EquipmentId > 0 ? EquipmentId.ToString () : "(не определен)";
+			}
+		}
+
+		ServiceClaim serviceClaim;
+
+		public virtual ServiceClaim ServiceClaim {
+			get{ return serviceClaim;}
+			set{
+				SetField(ref serviceClaim, value, ()=>ServiceClaim);
+			}
+		}
+
+		public bool IsNew{ get; set; }
+		public Equipment NewEquipment{get;set;}
+		public bool Returned {
+			get {
+				return Amount > 0;
+			}
+			set {
+				Amount = value ? 1 : 0;
+			}
+		}
+	}
+
 }
 
