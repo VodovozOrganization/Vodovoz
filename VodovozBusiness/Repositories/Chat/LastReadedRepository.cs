555ffe709a80234248afb25efe8c55744832c14e
Andrei <anri.vin@gmail.com>
2016-06-29 14:47:02 +0300

    Updated chat repository.

--- VodovozBusiness/Repositories/Chat/LastReadedRepository.cs
+++ VodovozBusiness/Repositories/Chat/LastReadedRepository.cs
@@ -8,13 +8,28 @@ namespace Vodovoz.Repository.Chat
 {
 	public static class LastReadedRepository
 	{
-		public static LastReadedMessage GetChatLastReadedMessageForEmloyee(IUnitOfWork uow, ChatClass chat, Employee employee) {
+		public static LastReadedMessage GetLastReadedMessageForEmloyee(IUnitOfWork uow, ChatClass chat, Employee employee) {
 			LastReadedMessage lastReadedAlias = null;
 
 			return uow.Session.QueryOver<LastReadedMessage> (() => lastReadedAlias)
 				.Where (() => lastReadedAlias.Chat.Id == chat.Id)
 				.Where (() => lastReadedAlias.Employee.Id == employee.Id)
 				.SingleOrDefault();
+		}
+
+		public static int GetLastReadedMessagesCountForEmployee(IUnitOfWork uow, ChatClass chat, Employee employee) {
+			LastReadedMessage lastReadedAlias = null;
+			ChatMessage chatMessageAlias = null;
+			var lastMessage = GetLastReadedMessageForEmloyee(uow, chat, employee);
+
+			var query = uow.Session.QueryOver<ChatMessage>(() => chatMessageAlias)
+				.Where(() => chatMessageAlias.Chat.Id == chat.Id);
+			if (lastMessage == null)
+				return query.RowCount();
+			else
+				return query
+					.Where(() => chatMessageAlias.DateTime > lastMessage.LastDateTime)
+					.RowCount();
 		}
 	}
 }
