9c5e5982f57e227757bfb8d1723313060c3e7c3a
Andrey Gankov <gav@qsolution.ru>
2016-09-14 19:50:00 +0300

    Добавлены дополннительные поля для договора.

--- VodovozBusiness/DocTemplates/ContractParser.cs
+++ VodovozBusiness/DocTemplates/ContractParser.cs
@@ -25,6 +25,29 @@ namespace Vodovoz.DocTemplates
 			AddField(x => x.Organization.KPP, PatternFieldType.FString);
 			AddField(x => x.Organization.JurAddress, PatternFieldType.FString);
 			AddField(x => x.Organization.OGRN, PatternFieldType.FString);
+			//Расчетный счет
+			AddField(x => x.Organization.DefaultAccount.Number, PatternFieldType.FString);
+			AddField(x => x.Organization.DefaultAccount.InBank.Bik, PatternFieldType.FString);
+			AddField(x => x.Organization.DefaultAccount.InBank.CorAccount, PatternFieldType.FString);
+			AddField(x => x.Organization.DefaultAccount.InBank.Name, PatternFieldType.FString);
+			//Директор организации
+			AddField(x => x.Organization.Leader.FullName, PatternFieldType.FString);
+			AddField(x => x.Organization.Leader.ShortName, PatternFieldType.FString);
+
+			//Клиент
+			AddField(x => x.Counterparty.FullName, PatternFieldType.FString);
+			AddField(x => x.Counterparty.Address, PatternFieldType.FString);
+			AddField(x => x.Counterparty.INN, PatternFieldType.FString);
+			AddField(x => x.Counterparty.KPP, PatternFieldType.FString);
+			AddField(x => x.Counterparty.JurAddress, PatternFieldType.FString);
+			//Расчетный счет
+			AddField(x => x.Counterparty.DefaultAccount.Number, PatternFieldType.FString);
+			AddField(x => x.Counterparty.DefaultAccount.InBank.Bik, PatternFieldType.FString);
+			AddField(x => x.Counterparty.DefaultAccount.InBank.CorAccount, PatternFieldType.FString);
+			AddField(x => x.Counterparty.DefaultAccount.InBank.Name, PatternFieldType.FString);
+			//Директор клиента
+			//AddField(x => x.Counterparty.Leader.FullName, PatternFieldType.FString);
+			//AddField(x => x.Counterparty.Leader.ShortName, PatternFieldType.FString);
 
 			SortFields();
 		}
