df6523bcb4626dae64aed1238419f3c8df11608d
Dmitriy <MrDmitriy65@mail.ru>
2016-10-10 14:29:29 +0300

    Добавлены операции с топливом.

---
+++ VodovozBusiness/Domain/Operations/FuelOperation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Vodovoz.Domain.Operations;
+using Vodovoz.Domain.Logistic;
+using Vodovoz.Domain.Employees;
+
+namespace Vodovoz
+{
+	[OrmSubject (Gender = QSProjectsLib.GrammaticalGender.Neuter,
+		NominativePlural = "операции с топливом",
+		Nominative = "операция с топливом")]
+	public partial class FuelOperation : OperationBase
+	{
+		private FuelType fuel;
+
+		public virtual FuelType Fuel {
+			get { return fuel; }
+			set {SetField(ref fuel, value, () => Fuel);}
+		}
+
+		private Employee driver;
+
+		public virtual Employee Driver {
+			get { return driver; }
+			set { SetField(ref driver, value, () => Driver); }
+		}
+
+		private decimal litersGived;
+
+		public virtual DateTime LitersGived {
+			get { return litersGived; }
+			set {SetField(ref litersGived, value, () => LitersGived);}
+		}
+
+		private decimal litersOutlayed;
+
+		public virtual DateTime LitersOutlayed {
+			get { return litersOutlayed; }
+			set {SetField(ref litersOutlayed, value, () => LitersOutlayed);}
+		}
+
+		public FuelOperation()
+		{
+		}
+	}
+}
+
---
+++ VodovozBusiness/HibernateMapping/Operations/FuelOperationMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Operations;
+
+namespace Vodovoz.HMap
+{
+	public class FuelOperationMap: ClassMap<FuelOperation>
+	{
+		public FuelOperationMap ()
+		{
+			Table ("deposit_operations");
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+
+			Map (x => x.OperationTime).Column ("date");
+			Map (x => x.LitersGived).Column ("liters_gived");
+			Map (x => x.LitersOutlayed).Column ("liters_outlayed");
+
+			References (x => x.Fuel).Column ("fuel_type_id");
+			References (x => x.Driver).Column ("driver_id");
+		}
+	}
+}
+
