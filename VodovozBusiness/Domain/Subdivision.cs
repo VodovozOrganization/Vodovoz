619bae0d96bd58adab6c47a0261b30e775c2e749
Delov Dmitriy <MrDmitriy65@mail.ru>
2016-12-06 11:50:30 +0300

    Добавление к предыдущему коммиту

--- VodovozBusiness/Domain/Subdivision.cs
+++ VodovozBusiness/Domain/Subdivision.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using QSOrmProject;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vodovoz
 {
-	public class Subdivision
+	public class Subdivision : PropertyChangedBase, IDomainObject
 	{
+		#region Свойства
+
+		public virtual int Id { get; set; }
+
+		private string name;
+
+		[Display(Name = "Название подразделения")]
+		[Required (ErrorMessage = "Название подразделения должно быть заполнено.")]
+		public virtual string Name
+		{
+			get { return name; }
+			set { SetField(ref name, value, () => Name); }
+		}
+
+		#endregion
+
 		public Subdivision()
 		{
 		}
