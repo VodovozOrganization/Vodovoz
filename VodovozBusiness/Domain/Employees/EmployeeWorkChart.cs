edfad13c631974531454153bd45555bd94615625
Delov Dmitriy <MrDmitriy65@mail.ru>
2016-12-06 17:42:47 +0300

    Добавлена доменная модель для графика работы сотрудников

---
+++ VodovozBusiness/Domain/Employees/EmployeeWorkChart.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Vodovoz.Domain.Employees;
+using System.ComponentModel.DataAnnotations;
+using QSOrmProject;
+
+namespace Vodovoz
+{
+	[OrmSubject (Gender = QSProjectsLib.GrammaticalGender.Masculine,
+		NominativePlural = "графики работы сотрудников",
+		Nominative = "график работы сотрудников")]
+	public class EmployeeWorkChart : PropertyChangedBase, IDomainObject
+	{
+		#region Свойства
+
+		public virtual int Id { get; set; }
+
+		private Employee employee;
+
+		[Display(Name = "Сотрудник")]
+		public virtual Employee Employee
+		{
+			get { return employee; }
+			set { SetField(ref employee, value, () => Employee); }
+		}
+
+		private DateTime date;
+
+		[Display(Name = "Дата")]
+		public virtual DateTime Date
+		{
+			get { return date; }
+			set { SetField(ref date, value, () => Date); }
+		}
+
+		private TimeSpan startTime;
+
+		[Display(Name = "Время начала")]
+		public virtual TimeSpan StartTime
+		{
+			get { return startTime; }
+			set { SetField(ref startTime, value, () => StartTime); }
+		}
+
+		private TimeSpan endTime;
+
+		[Display(Name = "Время окончания")]
+		public virtual TimeSpan EndTime
+		{
+			get { return endTime; }
+			set { SetField(ref endTime, value, () => EndTime); }
+		}
+
+		public int Month
+		{
+			get {
+				return Date.Month;
+			}
+		}
+
+		public int Day
+		{
+			get {
+				return Date.Day;
+			}
+		}
+		#endregion
+
+		public EmployeeWorkChart()
+		{
+		}
+	}
+}
+
