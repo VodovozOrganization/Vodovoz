55d66b1e1fb8ce13a37ddb06cedee1501628690a
Delov Dmitriy <MrDmitriy65@mail.ru>
2016-12-07 09:17:06 +0300

    Добавлен маппинг для графика сотрудников Изменено название таблицы для графика работы сотрудников

---
+++ VodovozBusiness/HibernateMapping/Employees/EmployeeWorkChartMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+
+namespace Vodovoz
+{
+	public class EmployeeWorkChartMap : ClassMap<EmployeeWorkChart>
+	{
+		public EmployeeWorkChartMap()
+		{
+			Table("employee_work_charts");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.Date)	 .Column("date");
+			Map(x => x.StartTime).Column("start_time");
+			Map(x => x.EndTime)	 .Column("end_time");
+
+			References(x => x.Employee).Columns("employee_id");
+		}
+	}
+}
+
