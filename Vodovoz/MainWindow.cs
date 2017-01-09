54a21905bd926a2bbfd444c52965ac80baa6daef
Andrey Gankov <gav@qsolution.ru>
2017-01-09 11:45:03 +0300

    Пернес обработку событий кнопок в общую библиотеку.

--- Vodovoz/MainWindow.cs
+++ Vodovoz/MainWindow.cs
@@ -21,7 +21,6 @@ using Vodovoz.ViewModel;
 
 public partial class MainWindow: Gtk.Window
 {
-	private static Gtk.Clipboard clipboard = Gtk.Clipboard.Get (Gdk.Atom.Intern ("CLIPBOARD", false));
 	private static Logger logger = LogManager.GetCurrentClassLogger ();
 	uint LastUiId;
 
@@ -35,7 +34,9 @@ public partial class MainWindow: Gtk.Window
 	{
 		Build ();
 		this.BuildToolbarActions ();
-		this.KeyReleaseEvent += HandleKeyReleaseEvent;
+		this.KeyReleaseEvent += ClipboardWorkaround.HandleKeyReleaseEvent;
+		TDIMain.MainNotebook = tdiMain;
+		this.KeyReleaseEvent += TDIMain.TDIHandleKeyReleaseEvent;
 		//Передаем лебл
 		QSMain.StatusBarLabel = labelStatus;
 		this.Title = MainSupport.GetTitle ();
@@ -128,90 +129,6 @@ public partial class MainWindow: Gtk.Window
 			infopanel.SetInfoProvider(currentTab as IInfoProvider);
 		else			
 			infopanel.SetInfoProvider(DefaultInfoProvider.Instance);
-	}
-
-	void HandleKeyReleaseEvent (object o, KeyReleaseEventArgs args)
-	{
-		int platform = (int)Environment.OSVersion.Platform;
-		int version = (int)Environment.OSVersion.Version.Major;
-		Gdk.ModifierType modifier;
-
-		//Kind of MacOSX
-		if ((platform == 4 || platform == 6 || platform == 128) && version > 8)
-			modifier = Gdk.ModifierType.MetaMask | Gdk.ModifierType.Mod1Mask;
-		//Kind of Windows or Unix
-		else
-			modifier = Gdk.ModifierType.ControlMask;
-
-		//CTRL+C	
-		if ((args.Event.Key == Gdk.Key.Cyrillic_es || args.Event.Key == Gdk.Key.Cyrillic_ES) && args.Event.State.HasFlag(modifier)) {
-			Widget w = (o as MainWindow).Focus;
-			CopyToClipboard (w);
-		}//CTRL+X
-		else if ((args.Event.Key == Gdk.Key.Cyrillic_che || args.Event.Key == Gdk.Key.Cyrillic_CHE) && args.Event.State.HasFlag(modifier)) {
-			Widget w = (o as MainWindow).Focus;
-			CutToClipboard (w);
-		}//CTRL+V
-		else if ((args.Event.Key == Gdk.Key.Cyrillic_em || args.Event.Key == Gdk.Key.Cyrillic_EM) && args.Event.State.HasFlag(modifier)) {
-			Widget w = (o as MainWindow).Focus;
-			PasteFromClipboard (w);
-		}//CTRL+S || CTRL+ENTER
-		else if ((args.Event.Key == Gdk.Key.S
-		         || args.Event.Key == Gdk.Key.s
-		         || args.Event.Key == Gdk.Key.Cyrillic_yeru
-		         || args.Event.Key == Gdk.Key.Cyrillic_YERU
-				|| args.Event.Key == Gdk.Key.Return) && args.Event.State.HasFlag(modifier)) {
-			var w = tdiMain.CurrentPageWidget;
-			if (w is QSTDI.TabVBox) {
-				var tab = (w as QSTDI.TabVBox).Tab;
-				if (tab is QSTDI.TdiSliderTab) {
-					var dialog = (tab as QSTDI.TdiSliderTab).ActiveDialog;
-					dialog.SaveAndClose ();
-				}
-				if(tab is ITdiDialog)
-				{
-					(tab as ITdiDialog).SaveAndClose();
-				}
-			}
-		}
-	}
-
-	void CopyToClipboard (Widget w)
-	{
-		int start, end;
-
-		if (w is Editable)
-			(w as Editable).CopyClipboard ();
-		else if (w is TextView)
-			(w as TextView).Buffer.CopyClipboard (clipboard);
-		else if (w is Label) {
-			(w as Label).GetSelectionBounds (out start, out end);
-			if (start != end)
-				clipboard.Text = (w as Label).Text.Substring (start, end - start);
-		}
-	}
-
-	void CutToClipboard (Widget w)
-	{
-		int start, end;
-
-		if (w is Editable)
-			(w as Editable).CutClipboard ();
-		else if (w is TextView)
-			(w as TextView).Buffer.CutClipboard (clipboard, true);
-		else if (w is Label) {
-			(w as Label).GetSelectionBounds (out start, out end);
-			if (start != end)
-				clipboard.Text = (w as Label).Text.Substring (start, end - start);
-		}
-	}
-
-	void PasteFromClipboard (Widget w)
-	{
-		if (w is Editable)
-			(w as Editable).PasteClipboard ();
-		else if (w is TextView)
-			(w as TextView).Buffer.PasteClipboard (clipboard);
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
