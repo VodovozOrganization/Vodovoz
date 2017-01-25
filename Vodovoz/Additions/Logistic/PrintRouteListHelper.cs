f28a49f2ffd6653649442a204d91d3524037ea9b
Delov Dmitriy <MrDmitriy65@mail.ru>
2017-01-25 11:06:11 +0300

    [I-22] Закрытие маршрутного листа   Изменено поле в запросе RDL   Добавлен статус для печати документа закрытия

--- Vodovoz/Additions/Logistic/PrintRouteListHelper.cs
+++ Vodovoz/Additions/Logistic/PrintRouteListHelper.cs
@@ -62,6 +62,7 @@ namespace Vodovoz.Additions.Logistic
 
 			switch (routeList.Status)
 			{
+				case RouteListStatus.OnClosing:
 				case RouteListStatus.MileageCheck:
 				case RouteListStatus.Closed:
 					documentName = "ClosedRouteList";
