756994a8ffb235a38e22ba1c44fe5072979b58ad
Delov Dmitriy <MrDmitriy65@mail.ru>
2017-02-17 12:52:04 +0300

    [I-68] Трек водителей   В диалоге проверки километража старый код открытия карты изменет на новый

--- Vodovoz/Dialogs/Logistic/RouteListMileageCheckDlg.cs
+++ Vodovoz/Dialogs/Logistic/RouteListMileageCheckDlg.cs
@@ -21,11 +21,6 @@ namespace Vodovoz
 	{
 		#region Поля
 
-		private Gtk.Window mapWindow = null;
-		private GMapControl gmapWidget = new GMap.NET.GtkSharp.GMapControl();
-		private readonly GMapOverlay tracksOverlay = new GMapOverlay("tracks");
-		private List<DistanceTextInfo> tracksDistance = new List<DistanceTextInfo>();
-
 		private bool editing = true;
 
 		List<RouteListKeepingItemNode> items;
@@ -103,20 +98,6 @@ namespace Vodovoz
 			} );
 
 			ytreeviewAddresses.ItemsDataSource = items;
-			ConfigureMap();
-		}
-
-		private void ConfigureMap()
-		{
-			gmapWidget.MapProvider = GMapProviders.OpenStreetMap;
-			gmapWidget.Position = new PointLatLng(59.93900, 30.31646);
-			gmapWidget.HeightRequest = 150;
-			gmapWidget.MinZoom = 0;
-			gmapWidget.MaxZoom = 24;
-			gmapWidget.Zoom = 9;
-			gmapWidget.MouseWheelZoomEnabled = true;
-			gmapWidget.Overlays.Add(tracksOverlay);
-			gmapWidget.ExposeEvent += GmapWidget_ExposeEvent;
 		}
 
 		#endregion
@@ -160,130 +141,11 @@ namespace Vodovoz
 
 		protected void OnButtonOpenMapClicked (object sender, EventArgs e)
 		{
-			ClearTracks();
-
-			if (mapWindow == null)
-			{
-				mapWindow = new Gtk.Window("Трек водителя");
-				mapWindow.SetDefaultSize(700, 600);
-				mapWindow.DeleteEvent += MapWindow_DeleteEvent;
-				mapWindow.Add(gmapWidget);
-
-				int pointsCount = LoadTracks();
-				if (pointsCount <= 0)
-					MessageDialogWorks.RunInfoDialog("Нет данных о треке");
-				LoadAddresses();
-
-				gmapWidget.Show();
-				mapWindow.Show();
-			}
-			else
-			{
-				mapWindow.Remove(gmapWidget);
-				mapWindow.Destroy();
-				mapWindow = null;
-			}
+			var trackWnd = new TrackMapWnd(Entity.Id);
+			trackWnd.Show();
 		}
+
 		#endregion
-
-		#region Методы
-		private void ClearTracks()
-		{
-			tracksOverlay.Clear();
-			tracksDistance.Clear();
-		}
-
-		private int LoadTracks()
-		{
-			var pointList = Repository.Logistics.TrackRepository.GetPointsForRouteList(UoW, Entity.Id);
-			if (pointList.Count == 0)
-				return 0;
-
-			var points = pointList.Select(p => new PointLatLng(p.Latitude, p.Longitude));
-
-			var route = new GMapRoute(points, Entity.Id.ToString());
-
-			route.Stroke = new System.Drawing.Pen(System.Drawing.Color.Red);
-			route.Stroke.Width = 4;
-			route.Stroke.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
-
-			tracksDistance.Add(MakeDistanceLayout(route));
-			tracksOverlay.Routes.Add(route);
-
-			return pointList.Count;
-		}
-
-		private void LoadAddresses()
-		{
-			foreach(var orderItem in Entity.Addresses)
-			{
-				var point = orderItem.Order.DeliveryPoint;
-				if(point.Latitude.HasValue && point.Longitude.HasValue)
-				{
-					GMarkerGoogleType type;
-					switch(orderItem.Status)
-					{
-						case RouteListItemStatus.Completed:
-							type = GMarkerGoogleType.green_small;
-							break;
-						case RouteListItemStatus.Canceled:
-							type = GMarkerGoogleType.purple_small;
-							break;
-						case RouteListItemStatus.Overdue:
-							type = GMarkerGoogleType.red_small;
-							break;
-						default:
-							type = GMarkerGoogleType.none;
-							break;
-					}
-					var addressMarker = new GMarkerGoogle(new PointLatLng((double)point.Latitude, (double)point.Longitude),	type);
-					addressMarker.ToolTipText = point.ShortAddress;
-					tracksOverlay.Markers.Add(addressMarker);
-				}
-			}
-		}
-
-		private DistanceTextInfo MakeDistanceLayout(GMapRoute route)
-		{
-			var layout = new Pango.Layout(mapWindow.PangoContext);
-			layout.Alignment = Pango.Alignment.Right;
-			var colTXT = System.Drawing.ColorTranslator.ToHtml(route.Stroke.Color);
-			layout.SetMarkup(String.Format("<span foreground=\"{1}\"><span font=\"Segoe UI Symbol\">⛽</span> {0:N1} км.</span>", route.Distance, colTXT));
-
-			return new DistanceTextInfo{
-				PangoLayout = layout
-			};
-		}
-		#endregion
-
-		#region Методы событий
-		void GmapWidget_ExposeEvent (object o, Gtk.ExposeEventArgs args)
-		{
-			if (tracksDistance.Count == 0)
-				return;
-			var g = args.Event.Window;
-			var area = args.Event.Area;
-			int layoutWidth, layoutHeight, voffset = 0;
-			var gc = gmapWidget.Style.TextGC(Gtk.StateType.Normal);
-
-			foreach(var distance in tracksDistance)
-			{
-				distance.PangoLayout.GetPixelSize(out layoutWidth, out layoutHeight);
-				g.DrawLayout(gc, area.Right - 6 - layoutWidth, area.Top + 6 + voffset, distance.PangoLayout);
-				voffset += 3 + layoutHeight;
-			}
-		}
-
-		void MapWindow_DeleteEvent (object o, Gtk.DeleteEventArgs args)
-		{
-			buttonOpenMap.Click();
-			args.RetVal = false;
-		}
-		#endregion
-
-		class DistanceTextInfo{
-			public Pango.Layout PangoLayout;
-		}
 	}
 }
 
