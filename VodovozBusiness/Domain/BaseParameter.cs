1276dc0a4719d8a3a1d996ca36e945ff1b2ae0d9
Andrey Gankov <gav@qsolution.ru>
2017-03-23 19:31:18 +0300

    Добавлены в доменную модель параметры базы. Чтобы запрашивать из сервиса, где не поднято обычное MySQL соединение.

---
+++ VodovozBusiness/Domain/BaseParameter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Vodovoz.Domain
+{
+	public class BaseParameter
+	{
+		public string Name { get; set; }
+
+		public string StrValue { get; set; }
+	}
+}
---
+++ VodovozBusiness/HibernateMapping/BaseParameterMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using Vodovoz.Domain;
+
+namespace Vodovoz.HibernateMapping
+{
+	public class BaseParameterMap : ClassMap<BaseParameter>
+	{
+		public BaseParameterMap()
+		{
+			Table("base_parameters");
+
+			Id(x => x.Name).Column("name").GeneratedBy.Assigned();
+
+			Map(x => x.StrValue).Column("str_value");
+		}
+	}
+}
