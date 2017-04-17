39d8171c921c917ac5030df3d63ac36801dca77a
Alex <all_reg@inbox.ru>
2017-04-17 12:47:56 +0300

    Убрана колонка - доплата экспедитора

---
+++ Vodovoz/gtk-gui/Vodovoz.Dialogs.Logistic.TransferAddressesBetweenRLDlg.cs
@@ -0,0 +1,14 @@
+
+namespace Vodovoz.Dialogs.Logistic
+{
+	public partial class TransferAddressesBetweenRLDlg
+	{
+		private Gtk.Button buttonCancel;
+
+		private Gtk.Button buttonOk;
+
+		private void Build()
+		{
+		}
+	}
+}
