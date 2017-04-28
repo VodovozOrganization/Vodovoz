a13cb5107c37dc43abd1dbbdaededd2250d2326e
Alex <all_reg@inbox.ru>
2017-04-28 10:15:28 +0300

    Fix, добавил по недосмотру...

--- Vodovoz/Dialogs/Logistic/NonDeliveryDlg1.cs
+++ Vodovoz/Dialogs/Logistic/NonDeliveryDlg1.cs
@@ -5,81 +5,81 @@ namespace Vodovoz.Dialogs.Logistic
 	public partial class Widget : Gtk.Bin
 	{
 
-		#region поля
+		//#region поля
 
-		//private static Logger logger = LogManager.GetCurrentClassLogger ();
+		////private static Logger logger = LogManager.GetCurrentClassLogger ();
 
-		private Track track = null;
-		private decimal balanceBeforeOp = default (decimal);
+		//private Track track = null;
+		//private decimal balanceBeforeOp = default (decimal);
 
-		//List<RouteListRepository.ReturnsNode> allReturnsToWarehouse;
-		int bottlesReturnedToWarehouse;
-		int bottlesReturnedTotal;
+		////List<RouteListRepository.ReturnsNode> allReturnsToWarehouse;
+		//int bottlesReturnedToWarehouse;
+		//int bottlesReturnedTotal;
 
-		//enum RouteListActions
+		////enum RouteListActions
+		////{
+		////	[Display (Name = "Новый штраф")]
+		////	CreateNewFine,
+		////	[Display (Name = "Перенести разгрузку в другой МЛ")]
+		////	TransferReceptionToAnotherRL,
+		////	[Display (Name = "Перенести разгрузку в этот МЛ")]
+		////	TransferReceptionToThisRL,
+		////	[Display (Name = "Перенести адреса в этот МЛ")]
+		////	TransferAddressesToThisRL,
+		////	[Display (Name = "Перенести адреса из этого МЛ")]
+		////	TransferAddressesToAnotherRL
+
+		////}
+
+		//#endregion
+
+		//#region Конструкторы и конфигурирование диалога
+
+		//public NonDeliveryDlg (NonDelivery nDeliveryList) : this (nDeliveryList.Id) { }
+
+		//public NonDeliveryDlg (int id)
 		//{
-		//	[Display (Name = "Новый штраф")]
-		//	CreateNewFine,
-		//	[Display (Name = "Перенести разгрузку в другой МЛ")]
-		//	TransferReceptionToAnotherRL,
-		//	[Display (Name = "Перенести разгрузку в этот МЛ")]
-		//	TransferReceptionToThisRL,
-		//	[Display (Name = "Перенести адреса в этот МЛ")]
-		//	TransferAddressesToThisRL,
-		//	[Display (Name = "Перенести адреса из этого МЛ")]
-		//	TransferAddressesToAnotherRL
-
+		//	this.Build ();
+		//	UoWGeneric = UnitOfWorkFactory.CreateForRoot<NonDelivery> (id);
+		//	TabName = String.Format ("Создание недовоза{0}", Entity.Id);
+		//	ConfigureDlg ();
 		//}
 
-		#endregion
+		//public override bool Save ()
+		//{
+		//	throw new NotImplementedException ();
+		//}
 
-		#region Конструкторы и конфигурирование диалога
+		//void ConfigureDlg ()
+		//{
+		//	//speccomboShift.ItemsList = GuilPerson.				
 
-		public NonDeliveryDlg (NonDelivery nDeliveryList) : this (nDeliveryList.Id) { }
+		//	//enumWarrantyType.Binding
+		//	//	.AddBinding (UoWGeneric.Root, equipmentType => equipmentType.WarrantyCardType, widget => widget.SelectedItem)
+		//	//	.InitializeFromSource ();
 
-		public NonDeliveryDlg (int id)
-		{
-			this.Build ();
-			UoWGeneric = UnitOfWorkFactory.CreateForRoot<NonDelivery> (id);
-			TabName = String.Format ("Создание недовоза{0}", Entity.Id);
-			ConfigureDlg ();
-		}
+		//	//speccomboShift.ItemsList =   DeliveryShiftRepository.ActiveShifts (UoW);
+		//	//speccomboShift.Binding.AddBinding (Entity, rl => rl.Guilt, widget => widget.SelectedItem).InitializeFromSource ();
+		//	//speccomboShift.Sensitive = editing;
 
-		public override bool Save ()
-		{
-			throw new NotImplementedException ();
-		}
+		//	//speccomboShift.Binding.AddBinding (Entity, e => e.Guilt, w => w.Text).InitializeFromSource ();
+		//	//dataentryRegNumber.Binding.AddBinding (Entity, e => e.RegistrationNumber, w => w.Text).InitializeFromSource ();
 
-		void ConfigureDlg ()
-		{
-			//speccomboShift.ItemsList = GuilPerson.				
+		//	//dataentryreferenceDriver.SubjectType = typeof (Employee);
+		//	//dataentryreferenceDriver.Binding.AddBinding (Entity, e => e.Driver, w => w.Subject).InitializeFromSource ();
 
-			//enumWarrantyType.Binding
-			//	.AddBinding (UoWGeneric.Root, equipmentType => equipmentType.WarrantyCardType, widget => widget.SelectedItem)
-			//	.InitializeFromSource ();
+		//	//dataentryFuelType.SubjectType = typeof (FuelType);
+		//	//dataentryFuelType.Binding.AddBinding (Entity, e => e.FuelType, w => w.Subject).InitializeFromSource ();
+		//	//radiobuttonMain.Active = true;
 
-			//speccomboShift.ItemsList =   DeliveryShiftRepository.ActiveShifts (UoW);
-			//speccomboShift.Binding.AddBinding (Entity, rl => rl.Guilt, widget => widget.SelectedItem).InitializeFromSource ();
-			//speccomboShift.Sensitive = editing;
+		//	//dataspinbutton1.Binding.AddBinding (Entity, e => e.FuelConsumption, w => w.Value).InitializeFromSource ();
 
-			//speccomboShift.Binding.AddBinding (Entity, e => e.Guilt, w => w.Text).InitializeFromSource ();
-			//dataentryRegNumber.Binding.AddBinding (Entity, e => e.RegistrationNumber, w => w.Text).InitializeFromSource ();
-
-			//dataentryreferenceDriver.SubjectType = typeof (Employee);
-			//dataentryreferenceDriver.Binding.AddBinding (Entity, e => e.Driver, w => w.Subject).InitializeFromSource ();
-
-			//dataentryFuelType.SubjectType = typeof (FuelType);
-			//dataentryFuelType.Binding.AddBinding (Entity, e => e.FuelType, w => w.Subject).InitializeFromSource ();
-			//radiobuttonMain.Active = true;
-
-			//dataspinbutton1.Binding.AddBinding (Entity, e => e.FuelConsumption, w => w.Value).InitializeFromSource ();
-
-			//photoviewCar.Binding.AddBinding (Entity, e => e.Photo, w => w.ImageFile).InitializeFromSource ();
-			//photoviewCar.GetSaveFileName = () => String.Format ("{0}({1})", Entity.Model, Entity.RegistrationNumber);
-		}
-		public Widget ()
-		{
-			this.Build ();
-		}
+		//	//photoviewCar.Binding.AddBinding (Entity, e => e.Photo, w => w.ImageFile).InitializeFromSource ();
+		//	//photoviewCar.GetSaveFileName = () => String.Format ("{0}({1})", Entity.Model, Entity.RegistrationNumber);
+		//}
+		//public Widget ()
+		//{
+		//	this.Build ();
+		//}
 	}
 }
