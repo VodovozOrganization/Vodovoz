7dae0df3e40b8c8b67e562525a55dd34bb4ad0da
Alex <all_reg@inbox.ru>
2017-05-24 09:42:55 +0300

    Скрыты уволенные сотрудники из диалога -отчет по балансу водителей. Добавлено итого в печатную форму.

--- Vodovoz/ReportsParameters/DriversWageBalanceReport.cs
+++ Vodovoz/ReportsParameters/DriversWageBalanceReport.cs
@@ -96,8 +96,8 @@ namespace Vodovoz.Reports
 			Employee employeeAlias = null;
 
 			driversList = UoW.Session.QueryOver<Employee>(() => employeeAlias)
-				.Where(() => employeeAlias.Category == EmployeeCategory.driver 
-			           || employeeAlias.Category == EmployeeCategory.forwarder)
+			                 .Where(() =>employeeAlias.IsFired == false && employeeAlias.Category == EmployeeCategory.driver
+					   || employeeAlias.Category == EmployeeCategory.forwarder )
 				.SelectList(list => list
 					.Select(() => employeeAlias.Id).WithAlias(() => resultAlias.Id)
 					.Select(() => employeeAlias.Name).WithAlias(() => resultAlias.Name)
