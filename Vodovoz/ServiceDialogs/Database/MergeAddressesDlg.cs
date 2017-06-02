dcfb0ee72b180f28ed884bfa489c292c557a1ed3
Andrey Gankov <gav@qsolution.ru>
2017-06-02 10:51:08 +0300

    Уменьшил расстояние разрыва до пол, километра.

---
+++ Vodovoz/ServiceDialogs/Database/MergeAddressesDlg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ServiceDialogs.Database
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class MergeAddressesDlg : Gtk.Bin
+	{
+		public MergeAddressesDlg()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ Vodovoz/gtk-gui/Vodovoz.ServiceDialogs.Database.MergeAddressesDlg.cs
@@ -0,0 +1,10 @@
+
+namespace Vodovoz.ServiceDialogs.Database
+{
+	public partial class MergeAddressesDlg
+	{
+		private void Build()
+		{
+		}
+	}
+}
