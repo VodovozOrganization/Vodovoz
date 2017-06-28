cc57603a37f262df7fe5cbd5e42777586690a9a6
Andrey Gankov <gav@qsolution.ru>
2017-06-28 13:36:34 +0300

    Отключил залоги ради ускорения. Зачем то делается куча запросов. Потом нужно будет разбираться.

--- VodovozBusiness/HibernateMapping/Order/OrderMap.cs
+++ VodovozBusiness/HibernateMapping/Order/OrderMap.cs
@@ -6,30 +6,30 @@ namespace Vodovoz.HMap
 {
 	public class OrderMap : ClassMap<Order>
 	{
-		public OrderMap ()
+		public OrderMap()
 		{
-			Table ("orders");
+			Table("orders");
 
-			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
 
-			Map (x => x.Comment)			.Column ("comment");
-			Map (x => x.DeliveryDate)		.Column ("delivery_date");
-			Map (x => x.SelfDelivery)		.Column ("self_delivery");
-			Map (x => x.BottlesReturn)		.Column ("bottles_return");
-			Map (x => x.ExtraMoney)	        .Column ("extra_money");
-			Map (x => x.SumToReceive)		.Column ("sum_to_receive");
-			Map (x => x.Shipped)			.Column ("shipped");
-			Map (x => x.SumDifferenceReason).Column ("sum_difference_reason");
-			Map (x => x.CollectBottles)		.Column ("collect_bottles");
-			Map (x => x.Code1c)				.Column ("code1c");
-			Map (x => x.Address1c)			.Column ("address_1c");
-			Map (x => x.Address1cCode)		.Column ("address_1c_code");
-			Map (x => x.FromClientText)		.Column ("from_client_text");
-			Map (x => x.ToClientText)		.Column ("to_client_text");
-			Map (x => x.DeliverySchedule1c)	.Column ("delivery_schedule_1c");
-			Map (x => x.DailyNumber1c)      .Column ("daily_number_1c");
-			Map (x => x.ClientPhone)        .Column ("client_phone");
-			Map (x => x.LastEditedTime)		.Column ("last_edited_time");
+			Map(x => x.Comment).Column("comment");
+			Map(x => x.DeliveryDate).Column("delivery_date");
+			Map(x => x.SelfDelivery).Column("self_delivery");
+			Map(x => x.BottlesReturn).Column("bottles_return");
+			Map(x => x.ExtraMoney).Column("extra_money");
+			Map(x => x.SumToReceive).Column("sum_to_receive");
+			Map(x => x.Shipped).Column("shipped");
+			Map(x => x.SumDifferenceReason).Column("sum_difference_reason");
+			Map(x => x.CollectBottles).Column("collect_bottles");
+			Map(x => x.Code1c).Column("code1c");
+			Map(x => x.Address1c).Column("address_1c");
+			Map(x => x.Address1cCode).Column("address_1c_code");
+			Map(x => x.FromClientText).Column("from_client_text");
+			Map(x => x.ToClientText).Column("to_client_text");
+			Map(x => x.DeliverySchedule1c).Column("delivery_schedule_1c");
+			Map(x => x.DailyNumber1c).Column("daily_number_1c");
+			Map(x => x.ClientPhone).Column("client_phone");
+			Map(x => x.LastEditedTime).Column("last_edited_time");
 			Map (x => x.CommentManager)		.Column ("comment_manager");
 			Map (x => x.ReturnedTare)		.Column ("returned_tare");
 			Map (x => x.InformationOnTara)	.Column ("information_on_tara");
@@ -42,18 +42,20 @@ namespace Vodovoz.HMap
 			Map (x => x.ReasonType)			.Column ("reason_type").CustomType<ReasonTypeStringType> (); 
 			Map (x => x.DriverCallType)	    .Column ("driver_call_type").CustomType<DriverCallTypeStringType>();
 
-			References (x => x.Client)					.Column ("client_id");
-			References (x => x.Contract)				.Column ("counterparty_contract_id");
-			References (x => x.Author)					.Column ("author_employee_id");
-			References (x => x.DeliveryPoint)			.Column ("delivery_point_id");
-			References (x => x.DeliverySchedule)		.Column ("delivery_schedule_id");
-			References (x => x.PreviousOrder)			.Column ("previous_order_id");
-			References (x => x.BottlesMovementOperation).Column ("bottles_movement_operation_id");
-			References (x => x.MoneyMovementOperation)	.Column ("money_movement_operation_id");
-			References (x => x.LastEditor)				.Column ("editor_employee_id");
+			References(x => x.Client).Column("client_id");
+			References(x => x.Contract).Column("counterparty_contract_id");
+			References(x => x.Author).Column("author_employee_id");
+			References(x => x.DeliveryPoint).Column("delivery_point_id");
+			References(x => x.DeliverySchedule).Column("delivery_schedule_id");
+			References(x => x.PreviousOrder).Column("previous_order_id");
+			References(x => x.BottlesMovementOperation).Column("bottles_movement_operation_id");
+			References(x => x.MoneyMovementOperation).Column("money_movement_operation_id");
+			References(x => x.LastEditor).Column("editor_employee_id");
 
-			HasMany (x => x.OrderDocuments)		.Cascade.AllDeleteOrphan ().LazyLoad ().Inverse ().KeyColumn ("attached_to_order_id");
+			HasMany(x => x.OrderDocuments).Cascade.AllDeleteOrphan().LazyLoad().Inverse().KeyColumn("attached_to_order_id");
+#if !SHORT
 			HasMany (x => x.OrderDepositItems)	.Cascade.AllDeleteOrphan ().LazyLoad ().Inverse ().KeyColumn ("order_id");
+#endif
 			HasMany (x => x.OrderItems)			.Cascade.AllDeleteOrphan  ().Inverse ().LazyLoad ().KeyColumn ("order_id");
 			HasMany (x => x.OrderEquipments)	.Cascade.AllDeleteOrphan  ().Inverse ().LazyLoad ().KeyColumn ("order_id");
 			HasMany (x => x.InitialOrderService).Cascade.None ().LazyLoad ().Inverse ().KeyColumn ("initial_order_id");
