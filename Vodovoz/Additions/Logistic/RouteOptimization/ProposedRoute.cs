0ce7936d19b31e3c9f887c3f7ed1d6117951542a
Andrey Gankov <gav@qsolution.ru>
2017-07-06 18:47:22 +0300

    Удалил весь старый код с рекурсией.

--- Vodovoz/Additions/Logistic/RouteOptimization/ProposedRoute.cs
+++ Vodovoz/Additions/Logistic/RouteOptimization/ProposedRoute.cs
@@ -10,108 +10,15 @@ namespace Vodovoz.Additions.Logistic.RouteOptimization
 		public List<Order> Orders = new List<Order>();
 		public AtWorkDriver Driver;
 
-		public List<FreeOrders> PossibleOrders;
-
 		public Car Car {
 			get {
 				return Driver.Car;
 			}
 		}
 
-		int? bottlesCache;
-
-		public int CurrentBottles{
-			get{
-				if(bottlesCache == null)
-					bottlesCache = Orders.SelectMany(x => x.OrderItems)
-							 .Where(x => x.Nomenclature.Category == Domain.Goods.NomenclatureCategory.water)
-							 .Sum(x => x.Count);
-				return bottlesCache.Value;
-			}
-		}
-
-		double? weightCache;
-
-		public double CurrentWeight {
-			get {
-				if(weightCache == null)
-					weightCache = Orders.SelectMany(x => x.OrderItems)
-							 .Sum(x => x.Nomenclature.Weight * x.Count);
-				return weightCache.Value;
-			}
-		}
-
-		double? volumeCache;
-
-		public double CurrentVolume {
-			get {
-				if(volumeCache == null)
-					volumeCache = Orders.SelectMany(x => x.OrderItems)
-							 .Sum(x => x.Nomenclature.Volume * x.Count);
-				return volumeCache.Value;
-			}
-		}
-
 		public ProposedRoute(AtWorkDriver driver)
 		{
 			Driver = driver;
-		}
-
-		public bool CanAdd(Order order)
-		{
-			if(Orders.Count >= Car.MaxRouteAddresses)
-				return false;
-
-			var bottles = CurrentBottles + order.OrderItems.Where(x => x.Nomenclature.Category == Domain.Goods.NomenclatureCategory.water)
-							 .Sum(x => x.Count);
-			if(bottles > Car.MaxBottles)
-				return false;
-
-			var weight = CurrentWeight + order.OrderItems.Sum(x => x.Nomenclature.Weight * x.Count);
-			if(weight > Car.MaxWeight)
-				return false;
-
-			var volume = CurrentVolume + order.OrderItems.Sum(x => x.Nomenclature.Volume * x.Count);
-			if(volume > Car.MaxVolume)
-				return false;
-
-			return true;
-		}
-
-		/// <summary>
-		/// Возвращает стоимость добавления адреса.
-		/// </summary>
-		public double AddOrder(Order order)
-		{
-			var cost = Orders.Count == 0
-				? DistanceCalculator.GetDistanceFromBase(order.DeliveryPoint)
-			                 : DistanceCalculator.GetDistance(Orders.Last().DeliveryPoint, order.DeliveryPoint);
-			Orders.Add(order);
-			RemoveFromPossible(order);
-			CleanCache();
-
-			return cost;
-		}
-
-		public void RemoveFromPossible(Order order)
-		{
-			PossibleOrders.ForEach(x => x.Orders.Remove(order));
-		}
-
-		public ProposedRoute Clone()
-		{
-			var propose = new ProposedRoute(Driver);
-			propose.Orders = Orders.ToList();
-			propose.PossibleOrders = PossibleOrders.Select(x => x.Clone()).ToList();
-
-			return propose;
-		}
-
-		private void CleanCache()
-		{
-			bottlesCache = null;
-			weightCache = null;
-			volumeCache = null;
 		}
 	}
 }
