3783c315e7bdf27ec446603eae725c514b77bf9e
Andrey Gankov <gav@qsolution.ru>
2017-08-10 15:13:15 +0300

    Уже давно не актуальный скрипт.

--- Base/Merge advance to 0.5.sql
+++
@@ -1,3 +0,0 @@
-UPDATE cash_advance_closing SET money = (SELECT money FROM cash_income WHERE id = cash_advance_closing.income_id) WHERE income_id IS NOT NULL;
-
-UPDATE cash_advance_closing SET money = (SELECT money FROM cash_expense WHERE id = cash_advance_closing.expense_id) WHERE advance_report_id IS NOT NULL;
