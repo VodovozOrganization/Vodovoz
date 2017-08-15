abc3a474c110aabcde1a6a0be5056e02393b6c54
Alex <all_reg@inbox.ru>
2017-08-15 13:33:07 +0300

    Архив. Новая дата по умолчанию.

--- Vodovoz/Dialogs/ResidueDlg.cs
+++ Vodovoz/Dialogs/ResidueDlg.cs
@@ -37,7 +37,7 @@ namespace Vodovoz
 
 		private void ConfigureDlg ()
 		{
-			Entity.Date = new DateTime(2017, 4, 24);
+			Entity.Date = new DateTime(2017, 4, 23);
 			ypickerDocDate.Binding.AddBinding(Entity, e => e.Date, w => w.Date).InitializeFromSource();
 			yreferenceClientSelector.RepresentationModel = new ViewModel.CounterpartyVM(UoW);
 			yreferenceClientSelector.Binding.AddBinding(Entity, e => e.Customer, w => w.Subject).InitializeFromSource();
