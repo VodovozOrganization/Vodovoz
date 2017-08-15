9d60ce810d5a7069edf289a1ace2abb5e53f4ca0
Alex <all_reg@inbox.ru>
2017-08-15 16:57:54 +0300

    Fix. Отчет по незакрытым МЛ.

--- Vodovoz/gtk-gui/Vodovoz.ReportsParameters.Logistic.RouteListsOnClosingReport.cs
+++ Vodovoz/gtk-gui/Vodovoz.ReportsParameters.Logistic.RouteListsOnClosingReport.cs
@@ -6,6 +6,8 @@ namespace Vodovoz.ReportsParameters.Logistic
 	{
 		private global::Gtk.VBox vbox1;
 
+		private global::Gtk.CheckButton buttonToDayRL;
+
 		private global::Gtk.Button buttonCreateReport;
 
 		protected virtual void Build()
@@ -19,19 +21,30 @@ namespace Vodovoz.ReportsParameters.Logistic
 			this.vbox1.Name = "vbox1";
 			this.vbox1.Spacing = 6;
 			// Container child vbox1.Gtk.Box+BoxChild
+			this.buttonToDayRL = new global::Gtk.CheckButton();
+			this.buttonToDayRL.CanFocus = true;
+			this.buttonToDayRL.Name = "buttonToDayRL";
+			this.buttonToDayRL.Label = global::Mono.Unix.Catalog.GetString("Убрать сегодняшние МЛ");
+			this.buttonToDayRL.DrawIndicator = true;
+			this.buttonToDayRL.UseUnderline = true;
+			this.vbox1.Add(this.buttonToDayRL);
+			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.buttonToDayRL]));
+			w1.Position = 0;
+			w1.Expand = false;
+			w1.Fill = false;
+			// Container child vbox1.Gtk.Box+BoxChild
 			this.buttonCreateReport = new global::Gtk.Button();
 			this.buttonCreateReport.CanFocus = true;
 			this.buttonCreateReport.Name = "buttonCreateReport";
 			this.buttonCreateReport.UseUnderline = true;
 			this.buttonCreateReport.Label = global::Mono.Unix.Catalog.GetString("Сформировать отчет");
 			this.vbox1.Add(this.buttonCreateReport);
-			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.buttonCreateReport]));
-			w1.Position = 1;
-			w1.Expand = false;
-			w1.Fill = false;
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.buttonCreateReport]));
+			w2.Position = 2;
+			w2.Expand = false;
+			w2.Fill = false;
 			this.Add(this.vbox1);
-			if ((this.Child != null))
-			{
+			if((this.Child != null)) {
 				this.Child.ShowAll();
 			}
 			this.Hide();
