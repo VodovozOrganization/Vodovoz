020a07bb55b12032f425e2b2a91da9d9722c419d
Andrey Gankov <gav@qsolution.ru>
2017-11-09 16:13:34 +0300

    Обновил скприт сборки. Теперь можно выбрать чем собрать xbuild или msbuild

--- build_win.sh
+++ build_win.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-xbuild /p:Configuration=DebugShortWin
+echo "Чем собрать проект?"
+echo "1) msbuild"
+echo "2) xbuild"
+echo "По умолчанию 1."
+read case;
+
+case $case in
+    2)
+xbuild /p:Configuration=DebugShortWin /p:Platform=x86
+;;
+    1|*)
+msbuild /p:Configuration=DebugShortWin /p:Platform=x86
+;;
+esac
