8f8c53986a272c05f515cc17fc42115d9f84824f
Vadim <enzogord@gmail.com>
2018-03-07 15:38:41 +0300

    (Исправлено включением отслеживания изменений) FIX Подтверждения заказа, когда не сохранялись изменения после редактирвоания какого либо списка в заказе. (reverted from commit c7571ea45214aef9b57ee296103180cd1aef899d)

--- Vodovoz/Dialogs/OrderDlg.cs
+++ Vodovoz/Dialogs/OrderDlg.cs
@@ -1029,7 +1029,6 @@ namespace Vodovoz
 
 		protected void OnButtonAcceptClicked(object sender, EventArgs e)
 		{
-			bool hasChanges = UoWGeneric.HasChanges;
 			if(UoWGeneric.Root.OrderStatus == OrderStatus.NewOrder) {
 				var valid = new QSValidator<Order>(UoWGeneric.Root,
 								new Dictionary<object, object> {
@@ -1081,7 +1080,6 @@ namespace Vodovoz
 				treeItems.Selection.UnselectAll();
 				treeEquipment.Selection.UnselectAll();
 				UpdateButtonState();
-				this.HasChanges = hasChanges;
 				buttonSave.Click();
 				return;
 			}
