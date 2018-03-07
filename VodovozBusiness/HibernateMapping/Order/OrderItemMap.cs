a426fe74fb4d792c43a005a12e9c0387b75a9eb5
Vadim <enzogord@gmail.com>
2018-03-07 11:19:53 +0300

    I-876 FIX Исправлено сохранение основания скидки в товаре

--- VodovozBusiness/HibernateMapping/Order/OrderItemMap.cs
+++ VodovozBusiness/HibernateMapping/Order/OrderItemMap.cs
@@ -26,6 +26,7 @@ namespace Vodovoz.HibernateMapping
 			References (x => x.Order)						 .Column ("order_id");
 			References (x => x.FreeRentEquipment)			 .Column ("free_rent_equipment_id").Cascade.All();
 			References (x => x.PaidRentEquipment)			 .Column ("paid_rent_equipment_id").Cascade.All();
+			References (x => x.DiscountReason)			 	 .Column ("discount_reason_id").Cascade.All();
 		}
 	}
 }
