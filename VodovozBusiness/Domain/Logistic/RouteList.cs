9778c1265e30aab2128fd14c527858ed2dd6aadc
opopve <opopve@gmail.com>
2018-07-31 16:40:53 +0300

    I-1329 Разовые водители - не вычитать моб свзяь Сделано

--- VodovozBusiness/Domain/Logistic/RouteList.cs
+++ VodovozBusiness/Domain/Logistic/RouteList.cs
@@ -391,7 +391,7 @@ namespace Vodovoz.Domain.Logistic
 			{
                 var count = Addresses.Where(item => item.RouteList.car.TypeOfUse == CarTypeOfUse.Truck || item.Order.IsService == true)
                                      .Select(item => item).Count();
-				if (count > 0)
+				if (count > 0 || Driver.WageCalcType == WageCalculationType.withoutPayment)
 					return 0;
 
 				return Wages.GetDriverRates(Date).PhoneServiceCompensationRate * UniqueAddressCount;
