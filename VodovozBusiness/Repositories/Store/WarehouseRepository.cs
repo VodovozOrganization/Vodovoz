98c6a84b47511bf805c5a5ad505fa64fd9194d49
Vadim Gordeev <enzogord@gmail.com>
2018-08-09 12:05:29 +0300

    I-1354 Исправлена проверка необходимо ли грузить МЛ

--- VodovozBusiness/Repositories/Store/WarehouseRepository.cs
+++ VodovozBusiness/Repositories/Store/WarehouseRepository.cs
@@ -24,8 +24,10 @@ namespace Vodovoz.Repository.Store
 		{
 			Vodovoz.Domain.Orders.Order orderAlias = null;
 			OrderItem orderItemsAlias = null;
-			OrderEquipment orderEquipmentAlias = null, orderNewEquipmentAlias = null;
-			Nomenclature OrderItemNomenclatureAlias = null, OrderEquipmentNomenclatureAlias = null, OrderNewEquipmentNomenclatureAlias = null, resultNomenclatureAlias = null;
+			OrderEquipment orderEquipmentAlias = null;
+			Nomenclature OrderItemNomenclatureAlias = null,
+				OrderEquipmentNomenclatureAlias = null, 
+				resultNomenclatureAlias = null;
 
 			var ordersQuery = QueryOver.Of<Vodovoz.Domain.Orders.Order>(() => orderAlias);
 
@@ -43,21 +45,14 @@ namespace Vodovoz.Repository.Store
 			var orderEquipmentSubquery = QueryOver.Of<OrderEquipment>(() => orderEquipmentAlias)
 				.WithSubquery.WhereProperty(i => i.Order.Id).In(ordersQuery)
 				.JoinAlias(() => orderEquipmentAlias.Order, () => orderAlias)
-				.JoinQueryOver(() => orderEquipmentAlias.Equipment)
 				.Where(() => orderEquipmentAlias.Direction == Direction.Deliver)
 				.JoinAlias(e => e.Nomenclature, () => OrderEquipmentNomenclatureAlias)
 				.SelectList(list => list.Select(() => OrderEquipmentNomenclatureAlias.Id));
-			var orderNewEquipmentSubquery = QueryOver.Of<OrderEquipment>(() => orderNewEquipmentAlias)
-				.WithSubquery.WhereProperty(i => i.Order.Id).In(ordersQuery)
-				.JoinAlias(() => orderNewEquipmentAlias.Order, () => orderAlias)
-				.JoinAlias(() => orderNewEquipmentAlias.Nomenclature, () => OrderNewEquipmentNomenclatureAlias)
-				.SelectList(list => list.Select(() => OrderNewEquipmentNomenclatureAlias.Id));
 
 			return uow.Session.QueryOver<Nomenclature>(() => resultNomenclatureAlias)
 				.Where(new Disjunction()
 					.Add(Subqueries.WhereProperty<Nomenclature>(n => n.Id).In(orderitemsSubqury))
 					.Add(Subqueries.WhereProperty<Nomenclature>(n => n.Id).In(orderEquipmentSubquery))
-				       .Add(Subqueries.WhereProperty<Nomenclature>(n => n.Id).In(orderNewEquipmentSubquery))
 				).Where(n => n.Warehouse != null)
 				.Select(Projections.Distinct(Projections.Property<Nomenclature>(n => n.Warehouse)))
 				.List<Warehouse>();
