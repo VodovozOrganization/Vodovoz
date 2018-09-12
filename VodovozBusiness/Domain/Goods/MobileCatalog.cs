046d60564d82c372f592e258581254d6c6e77c24
Denis <dmselin.ws@gmail.com>
2018-09-12 13:15:27 +0300

    I-1454 Пропущен файл к предыдущему комиту.

---
+++ VodovozBusiness/Domain/Goods/MobileCatalog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Goods
+{
+	public enum MobileCatalog
+	{
+		[Display(Name = "Не публиковать")]
+		None,
+		[Display(Name = "Вода")]
+		Water,
+		[Display(Name = "Товары.Оборудование")]
+		Goods_Equipment,
+		[Display(Name = "Товары.Чай, Кофе, Посуда")]
+		Goods_CoffeeTea,
+		[Display(Name = "Товары.Аксессуары")]
+		Goods_Accessory
+	}
+
+	public class MobileCatalogStringType : NHibernate.Type.EnumStringType
+	{
+		public MobileCatalogStringType() : base(typeof(MobileCatalog))
+		{
+		}
+	}
+
+}
