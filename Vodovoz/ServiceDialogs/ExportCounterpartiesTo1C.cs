a9127fd33470ac8d9b312ab35e0dfaeec8a8a225
opopve <opopve@gmail.com>
2018-10-02 15:44:21 +0300

    I-1183 Выгрузка для 1с8 - номера и почта Сделано

---
+++ Vodovoz/ServiceDialogs/ExportCounterpartiesTo1C.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using QSOrmProject;
+using QSProjectsLib;
+using Vodovoz.Repository;
+using Vodovoz.ServiceDialogs.ExportTo1c;
+
+namespace Vodovoz.ServiceDialogs
+{
+	public class ExportCounterpartiesTo1C
+	{
+		private readonly IUnitOfWork uow;
+		private IList<CounterpartyTo1CNode> counterparties;
+
+		public int Steps => counterparties.Count;
+		public ExportCounterpariesData Result { get; private set; }
+
+		public ExportCounterpartiesTo1C()
+		{
+			this.uow = UnitOfWorkFactory.CreateWithoutRoot();
+		}
+
+		public void Run(IWorker worker)
+		{
+			worker.OperationName = "Подготовка данных";
+			worker.ReportProgress(0, "Получение контрагентов");
+			counterparties = CounterpartyRepository.GetCounterpartiesWithInnAndAnyContact(uow);
+			worker.OperationName = "Выгрузка имён и контактных данных";
+			worker.StepsCount = Steps;
+			Result = new ExportCounterpariesData(uow);
+			int i = 0;
+			while(!worker.IsCancelled && i < Steps) {
+				worker.ReportProgress(i, "Контрагент");
+				Result.AddCounterparty(counterparties[i]);
+				i++;
+			}
+		}
+	}
+}
---
+++ Vodovoz/ServiceDialogs/ExportTo1c/ExportCounterpariesData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Gamma.Utilities;
+using QSOrmProject;
+using Vodovoz.ExportTo1c;
+using Vodovoz.Repository;
+
+namespace Vodovoz.ServiceDialogs.ExportTo1c
+{
+	public class ExportCounterpariesData : IXmlConvertable
+	{
+		public string Version { get; set; }
+		public DateTime ExportDate { get; set; }
+		public List<string> Errors = new List<string>();
+
+		public List<ObjectNode> Objects { get; private set; }
+
+		public readonly IUnitOfWork UoW;
+
+		XElement Xml;
+
+		public XElement ToXml() => Xml;
+
+		public ExportCounterpariesData(IUnitOfWork uow)
+		{
+			this.Objects = new List<ObjectNode>();
+			this.UoW = uow;
+
+			this.Version = "1.0";
+			this.ExportDate = DateTime.Now;
+			Xml	= new XElement(
+				"ExchangeFile",
+				new XAttribute("FormatVersion", Version),
+				new XAttribute("Generated", ExportDate.ToString("s"))
+			);
+		}
+
+		public void AddCounterparty(CounterpartyTo1CNode counterparty)
+		{
+			Xml.Add(
+				new XElement(
+					"Counterparty",
+					new XAttribute(
+						counterparty.GetPropertyName(x => x.Id),
+						counterparty.Id
+					),
+					new XElement(
+						counterparty.GetPropertyName(x => x.Name),
+						counterparty.Name
+					),
+					new XElement(
+						counterparty.GetPropertyName(x => x.Phones),
+						counterparty.Phones
+					),
+					new XElement(
+						counterparty.GetPropertyName(x => x.EMails),
+						counterparty.EMails
+					)
+				)
+			);
+		}
+	}
+}
