8b7cd1d450a4ab53d3527d3c8840276bbac43510
opopve <opopve@gmail.com>
2018-10-03 14:12:04 +0300

    I-1484 Фильтр по незакрытым МЛ Сделано

--- Vodovoz/ReportsParameters/Logistic/RouteListsOnClosingReport.cs
+++ Vodovoz/ReportsParameters/Logistic/RouteListsOnClosingReport.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using QSOrmProject;
+using Gamma.GtkWidgets;
 using QSReport;
 
 namespace Vodovoz.ReportsParameters.Logistic
@@ -11,19 +11,14 @@ namespace Vodovoz.ReportsParameters.Logistic
 		public RouteListsOnClosingReport()
 		{
 			this.Build();
+			ConfigureChkBtns();
 		}
 
 		#region IParametersWidget implementation
 
 		public event EventHandler<LoadReportEventArgs> LoadReport;
 
-		public string Title
-		{
-			get
-			{
-				return "Отчет по незакрытым МЛ";
-			}
-		}
+		public string Title => "Отчет по незакрытым МЛ";
 
 		#endregion
 
@@ -35,11 +30,12 @@ namespace Vodovoz.ReportsParameters.Logistic
 		private ReportInfo GetReportInfo()
 		{
 			var parameters = new Dictionary<string, object>();
-			if(buttonToDayRL.Active)
-				parameters.Add("todayRloff", 1);
-			else
-				parameters.Add("todayRloff", 0);
-			
+
+			parameters.Add("todayRloff", chkRemTodayRLs.Active);
+			parameters.Add("RemTruckRLs", chkRemTruckRLs.Active);
+			parameters.Add("RemServiceRLs", chkRemServiceRLs.Active);
+			parameters.Add("RemMercRLs", chkRemMercRLs.Active);
+
 			return new ReportInfo
 			{
 				Identifier = "Logistic.RouteListOnClosing",
@@ -51,5 +47,34 @@ namespace Vodovoz.ReportsParameters.Logistic
 		{
 			OnUpdate(true);
 		}
+
+		#region Настройка смены названий кнопок
+		Dictionary<yCheckButton, string> chkLabels = new Dictionary<yCheckButton, string>();
+
+		void ConfigureChkBtns()
+		{
+			AddChkBtnToListForRenaming(chkRemTodayRLs);
+			AddChkBtnToListForRenaming(chkRemTruckRLs);
+			AddChkBtnToListForRenaming(chkRemServiceRLs);
+			AddChkBtnToListForRenaming(chkRemMercRLs);
+		}
+
+		void AddChkBtnToListForRenaming(yCheckButton btn) { 
+			chkLabels.Add(btn, btn.Label);
+			btn.Toggled += OnChkBtnToggled;
+			SetChkBtnTitle(btn);
+		}
+
+		void SetChkBtnTitle(yCheckButton btn){
+			if(!chkLabels.ContainsKey(btn))
+				throw new NotImplementedException(String.Format("Переключатель {0} - {1} не найден в словаре", btn.Name, btn.Label));
+			btn.Label = String.Format("{0} {1}", btn.Active ? "Показать" : "Скрыть", chkLabels[btn]);
+		}
+
+		protected void OnChkBtnToggled(object sender, EventArgs e)
+		{
+			SetChkBtnTitle((yCheckButton)sender);
+		}
+		#endregion
 	}
 }
