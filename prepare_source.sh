6121784894c12eff2e7491d2ce17bb4c56c0344e
Andrey Gankov <gav@qsolution.ru>
2018-10-15 17:06:24 +0300

    Обновлен скрипт подоготовки исходников.

--- prepare_source.sh
+++ prepare_source.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 echo "Что делаем?"
-echo "1) nuget restore"
-echo "2) cleanup packages directories"
-echo "По умолчанию 1."
+echo "1) git pull"
+echo "2) nuget restore"
+echo "3) cleanup packages directories"
+echo "Можно вызывать вместе, например git+nuget=12"
 read case;
 
 case $case in
-    2)
+    *3*)
 rm -v -f -R ./packages/*
 rm -v -f -R ../QSProjects/packages/*
 rm -v -f -R ../My-FyiReporting/packages/*
 rm -v -f -R ../VodovozService/packages/*
-;;
-    1|*)
+;;&
+    *1*)
+git pull --autostash
+cd ../VodovozService
+git pull --autostash
+cd ../QSProjects
+git pull --autostash
+cd ../Gtk.DataBindings
+git pull --autostash
+cd ../GammaBinding
+git pull --autostash
+cd ../GMap.NET
+git pull --autostash
+cd ../Vodovoz
+;;&
+    *2*)
 nuget restore Vodovoz.sln;
 nuget restore ../QSProjects/QSProjectsLib.sln;
 nuget restore ../My-FyiReporting/MajorsilenceReporting-Linux-GtkViewer.sln
 nuget restore ../VodovozService/VodovozService.sln
-;;
+;;&
 esac
 
 
