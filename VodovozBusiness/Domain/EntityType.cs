5a841568dfa6d532a541f6e02fbeba705e8c88ac
opopve <opopve@gmail.com>
2019-01-24 15:31:22 +0300

    I-1669 иерархия складов - добавлена сущность "EntityType" и таблица для связи многие-ко-многим в моделе БД и коде.

---
+++ VodovozBusiness/Domain/EntityType.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Bindings.Utilities;
+using QS.DomainModel.Entity;
+
+namespace Vodovoz.Domain
+{
+	[Appellative(Gender = GrammaticalGender.Feminine,
+	NominativePlural = "типы документов",
+	Nominative = "тип документа")]
+	public class EntityType : PropertyChangedBase, IDomainObject, IValidatableObject
+	{
+		public EntityType() { }
+
+		#region Свойства
+
+		public virtual int Id { get; set; }
+
+		string customName;
+		[Display(Name = "Название документа")]
+		public virtual string CustomName {
+			get => customName;
+			set => SetField(ref customName, value, () => CustomName);
+		}
+
+		string type;
+		[Display(Name = "Тип документа")]
+		public virtual string Type {
+			get => type;
+			set => SetField(ref type, value, () => Type);
+		}
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if(string.IsNullOrWhiteSpace(CustomName))
+				yield return new ValidationResult(
+					"Название документа должно быть заполнено.",
+					new[] { this.GetPropertyName(o => o.CustomName) }
+				);
+
+			if(string.IsNullOrWhiteSpace(Type))
+				yield return new ValidationResult(
+					"Тип документа должен быть выбран.",
+					new[] { this.GetPropertyName(o => o.Type) }
+				);
+		}
+
+		#endregion
+	}
+}
