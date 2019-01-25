59f2770be3c2a20d8bd8adad576706c3ef52cd6d
Vadim Gordeev <enzogord@gmail.com>
2019-01-25 09:19:21 +0300

    Добавлен класс для хранения прав подразделений

---
+++ VodovozBusiness/HibernateMapping/Permissions/EntitySubdivisionPermissionMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NHibernate.Mapping;
+using Vodovoz.Domain.Permissions;
+using FluentNHibernate.Mapping;
+
+namespace Vodovoz.HibernateMapping.Permissions
+{
+	public class EntitySubdivisionPermissionMap //: ClassMap<EntitySubdivisionPermission>
+	{
+		/*public EntitySubdivisionPermissionMap()
+		{
+			Table("permission_entity_subdivision");
+			Not.LazyLoad();
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.EntityName).Column("entity_name");
+			Map(x => x.CanCreate).Column("can_create");
+			Map(x => x.CanRead).Column("can_read");
+			Map(x => x.CanUpdate).Column("can_update");
+			Map(x => x.CanDelete).Column("can_delete");
+
+			References(x => x.Subdivision).Column("subdivision_id");
+		}*/
+	}
+}
