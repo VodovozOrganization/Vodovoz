e98bc6610667cd48f997f27e5f530b8daec902dd
opopve <opopve@gmail.com>
2019-02-01 17:00:45 +0300

    I-1678 Изменения по логистике в связи с открытием второго склада - добавлена географическая группа для районов доставки - добавлен простой справочник для географической группы

---
+++ VodovozBusiness/Domain/Sale/GeographicGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QS.DomainModel.Entity;
+
+namespace Vodovoz.Domain.Sale
+{
+	public class GeographicGroup : BusinessObjectBase<ScheduleRestrictedDistrict>, IDomainObject
+	{
+		public virtual int Id { get; set; }
+
+		string name;
+
+		[Display(Name = "Название")]
+		public virtual string Name {
+			get => name;
+			set => SetField(ref name, value, () => Name);
+		}
+	}
+}
