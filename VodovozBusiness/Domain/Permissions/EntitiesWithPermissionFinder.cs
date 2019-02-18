46225422812846d67d0df07fa10c18a3aff1aea6
Vadim Gordeev <enzogord@gmail.com>
2019-02-18 11:48:17 +0300

    Подключена сборка с банками в поисковик сущностей для проверки прав

--- VodovozBusiness/Domain/Permissions/EntitiesWithPermissionFinder.cs
+++ VodovozBusiness/Domain/Permissions/EntitiesWithPermissionFinder.cs
@@ -5,6 +5,7 @@ using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
 using QS.Project.Domain;
 using Vodovoz.Domain.Orders;
+using QSBanks;
 
 namespace Vodovoz.Domain.Permissions
 {
@@ -12,9 +13,10 @@ namespace Vodovoz.Domain.Permissions
 	{
 		public IEnumerable<Type> FindTypes()
 		{
+			var qsBanksAssembly = Assembly.GetAssembly(typeof(Bank));
 			var qsDomainAssembly = Assembly.GetAssembly(typeof(EntityUserPermission));
 			var vodovozDomainAssembly = Assembly.GetAssembly(typeof(Order));
-			return DomainHelper.GetHavingAttributeEntityTypes<EntityPermissionAttribute>(x => x.IsClass && !x.IsAbstract, qsDomainAssembly, vodovozDomainAssembly);
+			return DomainHelper.GetHavingAttributeEntityTypes<EntityPermissionAttribute>(x => x.IsClass && !x.IsAbstract, qsDomainAssembly, vodovozDomainAssembly, qsBanksAssembly);
 		}
 	}
 }
