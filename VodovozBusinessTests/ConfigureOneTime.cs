22c6f60034f0447b1bf47c50f7286e3080adbdb8
Andrey Gankov <gav@qsolution.ru>
2019-02-20 17:17:30 +0300

    Исправлено тестирование удаления.

--- VodovozBusinessTests/ConfigureOneTime.cs
+++ VodovozBusinessTests/ConfigureOneTime.cs
@@ -18,7 +18,7 @@ namespace VodovozBusinessTests.Deletion
 			Console.WriteLine("Инициализация");
 			var db_config = FluentNHibernate.Cfg.Db.MySQLConfiguration.Standard
 								.Dialect<NHibernate.Spatial.Dialect.MySQL57SpatialDialect>()
-								.ConnectionString("server=vod.qsolution.ru;port=3306;database=Vodovoz_test;user id=test_only;password=7qqKWuNugQF2Y2W1;sslmode=None;");
+								.ConnectionString("server=vod.qsolution.ru;port=3306;database=test-test;user id=test_only;password=7qqKWuNugQF2Y2W1;sslmode=None;");
 
 			Console.WriteLine("ORM");
 			// Настройка ORM
