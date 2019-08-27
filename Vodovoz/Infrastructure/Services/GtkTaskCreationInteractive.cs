251dd4405381bd2bf785da7bc5d7ad7432ac5921
ZhuchkovMichael <Taenga.Zhu@yandex.ru>
2019-08-27 17:46:51 +0300

    Добавлен сервис для выбора даты при автоматическом создании задач

---
+++ Vodovoz/Infrastructure/Services/GtkTaskCreationInteractive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QS.Tdi;
+using Vodovoz.Tools.CallTasks;
+using Vodovoz.ViewWidgets;
+
+namespace Vodovoz.Infrastructure.Services
+{
+	public class GtkTaskCreationInteractive : ITaskCreationInteractive
+	{
+		public CreationTaskResult RunQuestion(ref DateTime? dateTime)
+		{
+			var questionWindow = new TaskCreationWindow();
+			questionWindow.Modal = true;
+			questionWindow.ShowAll();
+			dateTime = questionWindow.Date;
+			return questionWindow.creationTaskResult;
+		}
+	}
+}
---
+++ Vodovoz/ViewWidgets/TaskCreationWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Gtk;
+using QS.Widgets.GtkUI;
+using Vodovoz.Infrastructure.Services;
+using Vodovoz.Tools.CallTasks;
+
+namespace Vodovoz.ViewWidgets
+{
+	public partial class TaskCreationWindow : Gtk.Window
+	{
+		public CreationTaskResult creationTaskResult { get; set; } = CreationTaskResult.Cancel;
+
+		public DateTime? Date { get; set; }
+
+		public TaskCreationWindow() : base(Gtk.WindowType.Toplevel)
+		{
+			this.Build();
+		}
+
+		protected void OnButtonAutoClicked(object sender, EventArgs e)
+		{
+			creationTaskResult = CreationTaskResult.Auto;
+			Date = null;
+		}
+
+		protected void OnButtonPickDateClicked(object sender, EventArgs e)
+		{
+			creationTaskResult = CreationTaskResult.DatePick;
+
+			Window parentWin = (Window)this.Toplevel;
+			Dialog editDate = new Dialog("Укажите дату",
+				parentWin, DialogFlags.DestroyWithParent) {
+				Modal = true
+			};
+			editDate.AddButton("Отмена", ResponseType.Cancel);
+			editDate.AddButton("Ok", ResponseType.Ok);
+			Calendar SelectDate = new Calendar();
+			SelectDate.DisplayOptions = CalendarDisplayOptions.ShowHeading |
+				CalendarDisplayOptions.ShowDayNames |
+					CalendarDisplayOptions.ShowWeekNumbers;
+			SelectDate.DaySelectedDoubleClick += (send, args) => editDate.Respond(ResponseType.Ok);
+			SelectDate.Date = Date ?? DateTime.Now.Date;
+
+			editDate.VBox.Add(SelectDate);
+			editDate.ShowAll();
+			int response = editDate.Run();
+			if(response == (int)ResponseType.Ok)
+				Date = SelectDate.GetDate();
+			SelectDate.Destroy();
+			editDate.Destroy();
+
+		}
+	}
+}
