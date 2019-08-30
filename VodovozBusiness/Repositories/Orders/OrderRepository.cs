cf3408a79a136afe6e91abacbf3cd3007825032c
ZhuchkovMichael <Taenga.Zhu@yandex.ru>
2019-08-30 10:40:09 +0300

    Из OrderRepository удален дублирующий класс для оборудования

--- VodovozBusiness/Repositories/Orders/OrderRepository.cs
+++ VodovozBusiness/Repositories/Orders/OrderRepository.cs
@@ -174,12 +174,4 @@ namespace Vodovoz.Repositories.Orders
 		}
 
 	}
-
-	public class ClientEquipmentNode
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public string ShortName { get; set; }
-		public int Count { get; set; }
-	}
 }
