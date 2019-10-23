c877897659f66fc42c81790aba3084c3f8237bb3
Vadim Gordeev <enzogord@gmail.com>
2019-10-23 18:13:10 +0300

    Исправлен поиск параметра расчета зарплаты для наших автомобилей

--- VodovozBusiness/EntityRepositories/WageCalculation/WageSingletonRepository.cs
+++ VodovozBusiness/EntityRepositories/WageCalculation/WageSingletonRepository.cs
@@ -39,6 +39,7 @@ namespace Vodovoz.EntityRepositories.WageCalculation
 		public WageParameter GetActualParameterForOurCars(IUnitOfWork uow, DateTime dateTime)
 		{
 			return uow.Session.QueryOver<WageParameter>()
+				.Where(x => x.WageParameterTarget == WageParameterTargets.ForOurCars)
 				.Where(x => x.StartDate <= dateTime)
 				.OrderBy(x => x.StartDate).Desc
 				.Take(1)
