a0a46f574f898bba6390a1ed46e3db56cb795ecb
ZhuchkovMikhail <Taenga.Zhu@yandex.ru>
2019-11-07 17:44:14 +0300

    I-2259 Доп к з/п водителей - временные разграничения Добавил маппинг для дополнительных параметров расчета зп

---
+++ VodovozBusiness/HibernateMapping/WageCalculation/AdvancedWageParameters/AdvancedWageParameterMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using Vodovoz.Domain.WageCalculation.AdvancedWageParameters;
+
+namespace Vodovoz.HibernateMapping.WageCalculation.AdvancedWageParameters
+{
+	public class AdvancedWageParameterMap : ClassMap<AdvancedWageParameter>
+	{
+		public AdvancedWageParameterMap()
+		{
+			Table("advanced_wage_parameter");
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+			DiscriminateSubClassesOnColumn("type");
+			Map(x => x.AdvancedWageParameterType).Column("type").CustomType<AdvancedWageParameterStringType>();
+
+			Map(x => x.Wage).Column("wage");
+			References(x => x.ParentParameter).Column("parent_id");
+			References(x => x.WageRate).Column("wage_rate_id");
+		}
+	}
+}
