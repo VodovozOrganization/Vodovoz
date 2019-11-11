7a620e11d5cd991258d0f294995fe56700c86f29
ZhuchkovMikhail <Taenga.Zhu@yandex.ru>
2019-11-11 10:57:22 +0300

    doc: Перенес документацию методов в интерфейс

--- VodovozBusiness/EntityRepositories/Orders/IOrderRepository.cs
+++ VodovozBusiness/EntityRepositories/Orders/IOrderRepository.cs
@@ -12,31 +12,93 @@ namespace Vodovoz.EntityRepositories.Orders
 {
 	public interface IOrderRepository
 	{
+		/// <summary>
+		/// Кол-во 19л. воды в заказе
+		/// </summary>
+		/// <returns>Кол-во 19л. воды в заказе</returns>
+		/// <param name="uow">Uow.</param>
+		/// <param name="order">Заказ</param>
 		int Get19LWatterQtyForOrder(IUnitOfWork uow, Domain.Orders.Order order);
+
 		IList<Domain.Orders.Order> GetAcceptedOrdersForRegion(IUnitOfWork uow, DateTime date, ScheduleRestrictedDistrict district);
+
+		/// <summary>
+		/// Список МЛ для заказа, отсортированный в порядке владения этим заказом, в случае переносов
+		/// </summary>
+		/// <returns>Список МЛ</returns>
+		/// <param name="UoW">UoW</param>
+		/// <param name="order">Заказ</param>
 		IList<RouteList> GetAllRLForOrder(IUnitOfWork UoW, Domain.Orders.Order order);
+
 		IList<Domain.Orders.Order> GetCurrentOrders(IUnitOfWork UoW, Counterparty counterparty);
+
+		/// <summary>
+		/// Возврат отсортированного списка скидок
+		/// </summary>
+		/// <returns>Список скидок</returns>
+		/// <param name="UoW">UoW</param>
+		/// <param name="orderByDescending">Если <c>true</c>, то сортируется список по убыванию.</param>
 		IList<DiscountReason> GetDiscountReasons(IUnitOfWork UoW, bool orderByDescending = false);
+
+		/// <summary>
+		/// Оборудование заказа от клиента
+		/// </summary>
+		/// <returns>Список оборудования от клиенту</returns>
+		/// <param name="uow">Uow.</param>
+		/// <param name="order">Заказ</param>
 		IList<ClientEquipmentNode> GetEquipmentFromClientForOrder(IUnitOfWork uow, Domain.Orders.Order order);
+
 		IList<ClientEquipmentNode> GetEquipmentToClientForOrder(IUnitOfWork uow, Domain.Orders.Order order);
+
+		/// <summary>
+		/// Первый заказ контрагента, который можно считать выполненым.
+		/// </summary>
+		/// <returns>Первый заказ</returns>
+		/// <param name="uow">UoW</param>
+		/// <param name="counterparty">Контрагент</param>
 		Domain.Orders.Order GetFirstRealOrderForClientForActionBottle(IUnitOfWork uow, Counterparty counterparty);
+
 		OrderStatus[] GetGrantedStatusesToCreateSeveralOrders();
+
 		Domain.Orders.Order GetLatestCompleteOrderForCounterparty(IUnitOfWork UoW, Counterparty counterparty);
+
+		/// <summary>
+		/// Список последних заказов для точки доставки.
+		/// </summary>
+		/// <returns>Список последних заказов для точки доставки.</returns>
+		/// <param name="UoW">IUnitOfWork</param>
+		/// <param name="deliveryPoint">Точка доставки.</param>
+		/// <param name="count">Требуемое количество последних заказов.</param>
 		IList<Domain.Orders.Order> GetLatestOrdersForDeliveryPoint(IUnitOfWork UoW, DeliveryPoint deliveryPoint, int? count = null);
+
 		OrderStatus[] GetOnClosingOrderStatuses();
+
 		Domain.Orders.Order GetOrderOnDateAndDeliveryPoint(IUnitOfWork uow, DateTime date, DeliveryPoint deliveryPoint);
+
 		IList<Domain.Orders.Order> GetOrdersBetweenDates(IUnitOfWork UoW, DateTime startDate, DateTime endDate);
+
 		IList<Domain.Orders.Order> GetOrdersByCode1c(IUnitOfWork uow, string[] codes1c);
+
 		QueryOver<Domain.Orders.Order> GetOrdersForRLEditingQuery(DateTime date, bool showShipped);
+
 		IList<Domain.Orders.Order> GetOrdersToExport1c8(IUnitOfWork UoW, Export1cMode mode, DateTime startDate, DateTime endDate);
+
 		QueryOver<Domain.Orders.Order> GetSelfDeliveryOrdersForPaymentQuery();
+
 		OrderStatus[] GetStatusesForActualCount(Domain.Orders.Order order);
+
 		OrderStatus[] GetStatusesForOrderCancelation();
+
 		OrderStatus[] GetValidStatusesToUseActionBottle();
+
 		bool IsBottleStockExists(IUnitOfWork uow, Counterparty counterparty);
+
 		double GetAvgRandeBetwenOrders(IUnitOfWork uow, DeliveryPoint deliveryPoint, DateTime? startDate = null, DateTime? endDate = null);
+
 		double GetAvgRandeBetwenOrders(IUnitOfWork uow, DeliveryPoint deliveryPoint, out int? orderCount, DateTime? startDate = null, DateTime? endDate = null);
+
 		OrderStatus[] GetUndeliveryStatuses();
+
 		ReceiptForOrderNode[] GetShippedOrdersWithReceiptsForDates(IUnitOfWork uow, PaymentType paymentType, DateTime? startDate = null);
 	}
 
