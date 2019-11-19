48ae9255bf4ce018985f152dc27e1639d6996b80
ZhuchkovMikhail <Taenga.Zhu@yandex.ru>
2019-11-19 17:48:20 +0300

    I-2259 Доп к з/п водителей - временные разграничения Добавлен виджет для создания дополнительных прав

---
+++ Vodovoz/ViewWidgets/AdvancedWageParameterViews/AdvancedWageParametersView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.WageCalculation;
+
+namespace Vodovoz.ViewWidgets.AdvancedWageParameterViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AdvancedWageParametersView : TabViewBase<AdvancedWageParametersViewModel>
+	{
+		public AdvancedWageParametersView(AdvancedWageParametersViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
---
+++ VodovozViewModels/ViewModels/WageCalculation/AdvancedWageParametersViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using QS.DomainModel.UoW;
+using QS.Services;
+using QS.ViewModels;
+using Vodovoz.Infrastructure;
+
+namespace Vodovoz.ViewModels.WageCalculation
+{
+	public class AdvancedWageParametersViewModel : UoWTabViewModelBase
+	{
+		public IAdvancedWageWidgetFactory AdvancedWageWidgetFactory { get; }
+
+		public event Action<ViewModelBase> OnParameterViewModelChanged;
+
+		private ViewModelBase parameterViewModel;
+		public virtual ViewModelBase ParameterViewModel {
+			get => parameterViewModel;
+			set {
+				SetField(ref parameterViewModel, value);
+				OnParameterViewModelChanged?.Invoke(value);
+			}
+		}
+
+		public AdvancedWageParametersViewModel(IAdvancedWageWidgetFactory advancedWageWidgetFactory,IUnitOfWorkFactory unitOfWorkFactory, IInteractiveService interactiveService) : base(unitOfWorkFactory, interactiveService)
+		{
+			AdvancedWageWidgetFactory = advancedWageWidgetFactory ?? throw new ArgumentNullException(nameof(advancedWageWidgetFactory));
+		}
+	}
+}
