206f9f1d0cb1f7cd973724c7fc3d0e5bad2c1e1d
Vadim Gordeev <enzogord@gmail.com>
2020-01-20 12:50:39 +0300

    Добавлен проект для предоставления поиска с помощью Solr

---
+++ SolrSearch/SolrEntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SolrSearch
+{
+	public abstract class SolrEntityBase
+	{
+		public string SolrId { get; protected set; }
+		public string SolrEntityType { get; protected set; }
+	}
+}
