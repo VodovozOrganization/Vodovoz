2d6c724aaca76966b53ecab5a5e23badbfd7523d
Vadim Gordeev <enzogord@gmail.com>
2020-01-20 12:56:37 +0300

    Добавленая Solr сущность используемая для поиска по контрагентам

---
+++ VodovozViewModels/SolrMapping/CounterpartySolrMap.cs
@@ -0,0 +1,16 @@
+﻿using SolrSearch.Mapping;
+using Vodovoz.SolrModel;
+using Vodovoz.Domain.Client;
+namespace Vodovoz.SolrMapping
+{
+	public class CounterpartySolrMap : SolrOrmSourceClassMap<CounterpartySolrEntity, Counterparty>
+	{
+		public CounterpartySolrMap()
+		{
+			Map(se => se.Id, e => e.Id);
+			Map(se => se.Name, e => e.Name);
+			Map(se => se.FullName, e => e.FullName);
+			Map(se => se.Inn, e => e.INN);
+		}
+	}
+}
---
+++ VodovozViewModels/SolrModel/CounterpartySolrEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SolrSearch;
+
+namespace Vodovoz.SolrModel
+{
+	public class CounterpartySolrEntity : SolrEntityBase
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string FullName { get; set; }
+
+		public string Inn { get; set; }
+	}
+}
