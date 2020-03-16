a379491b539d04416ff7ad46d580451ad26c4d55
Zhuchkov Mikhail <Taenga.Zhu@yandex.ru>
2020-03-16 15:45:23 +0300

    I-2605 Еще один фильтр в журнале должников - неактуальные должники Сделано

--- Vodovoz/Representations/DebtorsJournalViewModel.cs
+++ Vodovoz/Representations/DebtorsJournalViewModel.cs
@@ -27,7 +27,6 @@ namespace Vodovoz.Representations
 	public class DebtorsJournalViewModel : FilterableSingleEntityJournalViewModelBase<Order, CallTaskDlg, DebtorJournalNode, DebtorsJournalFilterViewModel>
 	{
 
-
 		public DebtorsJournalViewModel(DebtorsJournalFilterViewModel filterViewModel, IUnitOfWorkFactory unitOfWorkFactory, ICommonServices commonServices, IEmployeeRepository employeeRepository) : base(filterViewModel, unitOfWorkFactory, commonServices)
 		{
 			TabName = "Журнал задолженности";
@@ -82,6 +81,8 @@ namespace Vodovoz.Representations
 			OrderItem orderItemAlias = null;
 			DiscountReason discountReasonAlias = null;
 			Nomenclature nomenclatureAlias = null;
+			Order orderFromAnotherDPAlias = null;
+
 
 			var ordersQuery = uow.Session.QueryOver(() => orderAlias);
 
@@ -147,6 +148,16 @@ namespace Vodovoz.Representations
 				.Where(() => orderItemAlias.Order.Id == orderAlias.Id)
 				.And(() => FilterViewModel.DiscountReason.Id == discountReasonAlias.Id);
 
+			var orderFromAnotherDP = QueryOver.Of(() => orderFromAnotherDPAlias)
+				.Select(Projections.Property(() => orderFromAnotherDPAlias.Id))
+				.Where(() => orderFromAnotherDPAlias.Client.Id == counterpartyAlias.Id)
+				.And(() => orderFromAnotherDPAlias.OrderStatus == OrderStatus.Closed)
+				.And(() => orderFromAnotherDPAlias.DeliveryDate >= orderAlias.DeliveryDate)
+				.And(new Disjunction().Add(() => orderFromAnotherDPAlias.DeliveryPoint.Id != deliveryPointAlias.Id)
+						.Add(() => orderFromAnotherDPAlias.SelfDelivery && !orderAlias.SelfDelivery)
+						.Add(() => !orderFromAnotherDPAlias.SelfDelivery && orderAlias.SelfDelivery)
+				);
+
 			#endregion LastOrder
 
 			ordersQuery = ordersQuery.WithSubquery.WhereProperty(p => p.Id).Eq(LastOrderIdQuery);
@@ -168,6 +179,8 @@ namespace Vodovoz.Representations
 					ordersQuery = ordersQuery.Where(() => orderAlias.DeliveryDate >= FilterViewModel.StartDate.Value);
 				if(FilterViewModel.EndDate != null)
 					ordersQuery = ordersQuery.Where(() => orderAlias.DeliveryDate <= FilterViewModel.EndDate.Value);
+				if(FilterViewModel.EndDate != null && FilterViewModel.HideActiveCounterparty)
+					ordersQuery = ordersQuery.WithSubquery.WhereNotExists(orderFromAnotherDP);
 				if(FilterViewModel.LastOrderNomenclature != null)
 					ordersQuery = ordersQuery.WithSubquery.WhereExists(LastOrderNomenclatures);
 				if(FilterViewModel.DiscountReason != null)
