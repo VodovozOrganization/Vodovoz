2e13d547833ddb5d677097407e9f940bf91c9ac2
gavr <gavr123456789@gmail.com>
2020-06-26 14:19:45 +0300

    Поиск по номерам теперь понимает номер введенный в любом виде, с тирешками, без, со скобками, с 8рки, с +7рки.

--- Vodovoz/JournalViewModels/CounterpartyJournalViewModel.cs
+++ Vodovoz/JournalViewModels/CounterpartyJournalViewModel.cs
@@ -83,13 +83,23 @@ namespace Vodovoz.JournalViewModels
 				.Left.JoinAlias(c => c.Phones, () => phoneAlias)
 				.Left.JoinAlias(() => counterpartyAlias.DeliveryPoints, () => deliveryPointAlias);
 
-			query.Where(GetSearchCriterion(
-				() => counterpartyAlias.Id,
-				() => counterpartyAlias.VodovozInternalId,
-				() => counterpartyAlias.Name,
-				() => counterpartyAlias.INN,
-				() => phoneAlias.DigitsNumber,
-				() => deliveryPointAlias.CompiledAddress
+
+			var searchHealperNew = new TempAdapters.SearchHelper(Search);
+
+			var idParam = new TempAdapters.SearchParameter(() => counterpartyAlias.Id, TempAdapters.SearchParametrType.Id);
+			var vodovozInternalIdParam = new TempAdapters.SearchParameter(() => counterpartyAlias.VodovozInternalId, TempAdapters.SearchParametrType.VodovozInternalId);
+			var nameParam = new TempAdapters.SearchParameter(() => counterpartyAlias.Name, TempAdapters.SearchParametrType.Name);
+			var INNParam = new TempAdapters.SearchParameter(() => counterpartyAlias.INN, TempAdapters.SearchParametrType.INN);
+			var digitNumberParam = new TempAdapters.SearchParameter(() => phoneAlias.DigitsNumber, TempAdapters.SearchParametrType.DigitsNumber);
+			var compiledAdressParam = new TempAdapters.SearchParameter(() => deliveryPointAlias.CompiledAddress, TempAdapters.SearchParametrType.CompiledAddress);
+
+			query.Where(searchHealperNew.GetSearchCriterionNew(
+				idParam,
+				vodovozInternalIdParam,
+				nameParam,
+				INNParam,
+				digitNumberParam,
+				compiledAdressParam
 			));
 
 			var counterpartyResultQuery = query.SelectList(list => list
---
+++ VodovozViewModels/TempAdapters/SearchParameter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Vodovoz.TempAdapters
+{
+	public enum SearchParametrType
+	{
+		/*DeliveryPoint,
+		BottlesMovementOperation,
+		ClientTask,
+		BusinessTaskJournalNode,
+		Counterparty,
+		Employee,
+		Phone,
+		Order,
+		*/
+		// пока что это все только для CounterParty поиска
+		Id,
+		VodovozInternalId,
+		Name,
+		INN,
+		DigitsNumber, // Phone это красивый вариант со скобками
+		CompiledAddress
+	}
+
+	public class  SearchParameter //<TRootEntity> пока все только для OCunterparty это не нужно
+	{
+		public SearchParametrType type;
+		public Expression<Func<object>> alias;
+
+		public SearchParameter(Expression<Func<object>> alias, SearchParametrType type)
+		{
+			this.type = type;
+			this.alias = alias;
+		}
+
+	}
+}
