440cf4a6bac7d56a4b3adbb4695c03e1903ef506
Andrey Shaklein <as1854@yandex.ru>
2020-08-17 17:02:43 +0300

    I-2782 Отчет Доходы и расходы - сделан фильтр по кассе; - в итоговой таблице баланс отображаются только выбранные кассы - если выбрано Все, то отображаются все доступные для пользователя кассы - в шапку выведена инфа о выбранных фильтрах.

--- Vodovoz/toolbars/ToolbarActions.cs
+++ Vodovoz/toolbars/ToolbarActions.cs
@@ -508,7 +508,7 @@ public partial class MainWindow : Window
 	{
 		tdiMain.OpenTab(
 			QSReport.ReportViewDlg.GenerateHashName<Vodovoz.Reports.CashFlow>(),
-			() => new QSReport.ReportViewDlg(new Vodovoz.Reports.CashFlow())
+			() => new QSReport.ReportViewDlg(new Vodovoz.Reports.CashFlow(new SubdivisionRepository(), ServicesConfig.CommonServices))
 		);
 	}
 
