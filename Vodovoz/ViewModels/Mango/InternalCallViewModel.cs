0a4ab01cb9b0081cfec94edd360e3664f3f0d761
DBalt8 <d.PotapovAnt@yandex.ru>
2020-08-19 09:36:02 +0300

    Добавлена view , viewmodel p-0
    Добавлена View , ViewModel для InternalCall- (только интерфейс , без логики)

---
+++ Vodovoz/ViewModels/Mango/InternalCallViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+namespace Vodovoz.ViewModels.MangoViewModel
+{
+	public class InternalCallViewModel : DialogViewModelBase
+	{
+		public InternalCallViewModel(INavigationManager navigation) : base(navigation)
+		{
+
+		}
+	}
+}
---
+++ Vodovoz/Views/Mango/InternalCallView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.Views.Dialog;
+using Vodovoz.ViewModels.MangoViewModel;
+
+namespace Vodovoz.Views.Mango
+{
+	public partial class InternalCallView : DialogViewBase<InternalCallViewModel>
+	{
+		public InternalCallView(InternalCallViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
