91865cd277f3417b96e1d79262c776bac317fa1e
Gankov <gav@qsolution.ru>
2020-08-21 09:55:28 +0300

    Минимальная реализация сервера подписки.
    Пока с тестовым кодом. Но клиенты подписываются и получают уведомления.

---
+++ Services/Application/VodovozMangoService/NotificationServiceImpl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace VodovozMangoService
+{
+	public class NotificationServiceImpl : NotificationService.NotificationServiceBase
+	{
+		public readonly Dictionary<uint, BlockingCollection<NotificationMessage>> Subscribers =
+			new Dictionary<uint, BlockingCollection<NotificationMessage>>();
+		public NotificationServiceImpl()
+		{
+		}
+
+		public override async Task Subscribe(NotificationSubscribeRequest request, IServerStreamWriter<NotificationMessage> responseStream, ServerCallContext context)
+		{
+			var queue = new BlockingCollection<NotificationMessage>();
+			lock (Subscribers)
+			{
+				Subscribers[request.Extension] = queue;
+			}
+
+			while (!context.CancellationToken.IsCancellationRequested)
+			{
+				var message = queue.Take(context.CancellationToken);
+				if(message != null)
+					await responseStream.WriteAsync(message);
+			}
+
+			lock (Subscribers)
+			{
+				if (Subscribers.ContainsValue(queue))
+					Subscribers.Remove(request.Extension);
+			}
+		}
+	}
+}
---
+++ Services/Library/ClientMangoService/MangoNotificationClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace ClientMangoService
+{
+	public class MangoNotificationClient
+	{
+		// public static string ServiceAddress = "mango.vod.qsolution.ru";
+		public static string ServiceAddress = "localhost";
+		public uint ServicePort = 7087;
+		private NotificationService.NotificationServiceClient client;
+
+		public MangoNotificationClient(uint extension, CancellationToken token)
+		{
+			Channel channel = new Channel($"{ServiceAddress}:{ServicePort}", ChannelCredentials.Insecure);
+			client = new NotificationService.NotificationServiceClient(channel);
+			
+			var request = new NotificationSubscribeRequest { Extension =  extension};
+			var response = client.Subscribe(request);
+
+
+			var responseReaderTask = Task.Run(async () =>
+			{
+				while(await response.ResponseStream.MoveNext()) {
+					var message = response.ResponseStream.Current;
+					Console.WriteLine($"extension:{extension} Received:{message.Number}");
+				}
+			});
+		}
+
+		public event EventHandler<IncomeCallEventArgs> IncomeCall;
+
+		protected virtual void OnIncomeCall(NotificationMessage message)
+		{
+			IncomeCall?.Invoke(this, new IncomeCallEventArgs{Message = message});
+		}
+	}
+	
+	public class IncomeCallEventArgs : EventArgs
+	{
+		public NotificationMessage Message { get; set; }
+	}
+}
