7a3eb44a754ff1872980096108f8ebea881e11c3
Danila <d.PotapovAnt@yandex.ru>
2020-08-24 09:51:37 +0300

    Набросок view-viewmodel для И-П

---
+++ Vodovoz/ViewModels/Mango/AdditionalCallViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Services;
+using QS.ViewModels;
+
+namespace Vodovoz.ViewModels.Mango
+{
+	public class AdditionalCallViewModel : DialogTabViewModelBase
+	{
+		public AdditionalCallViewModel(IUnitOfWorkFactory unitOfWorkFactory, IInteractiveService interactiveService, INavigationManager navigation) : base(unitOfWorkFactory, interactiveService, navigation)
+		{
+
+		}
+	}
+}
---
+++ Vodovoz/Views/Mango/AdditionalCallView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Vodovoz.Views.Mango
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AdditionalCallView : Gtk.Bin		
+	{
+		public AdditionalCallView()
+		{
+			this.Build();
+		}
+		void Configure()
+		{
+
+		}
+	}
+}
