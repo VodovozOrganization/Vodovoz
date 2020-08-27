3f1d8df2e369bf36bc491f13c446f7a10572dedf
Gankov <gav@qsolution.ru>
2020-08-27 09:59:03 +0300

    Реализован диалог входящего звонка.

--- Vodovoz/Views/Mango/IncomingCallView.cs
+++ Vodovoz/Views/Mango/IncomingCallView.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using QS.Views.Dialog;
+using Vodovoz.Infrastructure.Mango;
+using Vodovoz.ViewModels.Mango;
+
 namespace Vodovoz.Views.Mango
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class IncomingCallView : Gtk.Bin
+	public partial class IncomingCallView : DialogViewBase<IncomingCallViewModel>
 	{
-		public IncomingCallView()
+		public IncomingCallView(IncomingCallViewModel viewModel) : base(viewModel)
 		{
 			this.Build();
+			
+			labelName.Binding.AddBinding(viewModel.MangoManager, m => m.CallerName, w => w.Text).InitializeFromSource();
+			labelNumber.Binding.AddFuncBinding<MangoManager>(viewModel.MangoManager, m => "Телефон: " + m.CallerNumber, w => w.Text).InitializeFromSource();
+		}
+
+		protected void OnButtonDisconnectClicked(object sender, EventArgs e)
+		{
+			ViewModel.DeclineCall();
 		}
 	}
 }
