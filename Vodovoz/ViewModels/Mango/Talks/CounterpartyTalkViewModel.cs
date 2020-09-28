4a00cb1c55d50a15110bf37a7ed683b7d68b3889
Danila <d.PotapovAnt@yandex.ru>
2020-09-28 15:13:57 +0300

    Косметическая правка для корректного отображения номера

--- Vodovoz/ViewModels/Mango/Talks/CounterpartyTalkViewModel.cs
+++ Vodovoz/ViewModels/Mango/Talks/CounterpartyTalkViewModel.cs
@@ -78,7 +78,7 @@ namespace Vodovoz.ViewModels.Mango.Talks
 
 		public string GetPhoneNumber()
 		{
-			return "+7"+MangoManager.CallerNumber;
+			return "+"+MangoManager.CallerNumber;
 		}
 
 		public IDictionary<string, CounterpartyOrderView> GetCounterpartyViewModels()
