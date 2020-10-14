64cd3aeace5139d7e13ab937f9380483dd881fb7
Gankov <gav@qsolution.ru>
2020-10-14 15:26:30 +0300

    Надеюсь исправил работу свойства isTransfer

--- Services/Application/VodovozMangoService/Controllers/EventsController.cs
+++ Services/Application/VodovozMangoService/Controllers/EventsController.cs
@@ -51,7 +51,7 @@ namespace VodovozMangoService.Controllers
                 {
                     if (callsService.Calls.TryGetValue(message.from.taken_from_call_id, out var takenFrom))
                     {
-                        if(takenFrom.LastEvent.location == "abonent")
+                        if(takenFrom.LastEvent.call_state == "OnHold")
                             call.OnHoldCall = callsService.Calls[message.from.taken_from_call_id];
                     }
                 }
