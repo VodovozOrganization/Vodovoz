002f60857e37506dc5195c32ec018abaabc99bc5
Andrey Shaklein <as1854@yandex.ru>
2020-11-30 11:53:09 +0300

    Добавил возможность поиска в жернале сотрудников по ФИО

--- Vodovoz/JournalNodes/EmployeeJournalNode.cs
+++ Vodovoz/JournalNodes/EmployeeJournalNode.cs
@@ -5,13 +5,13 @@ namespace Vodovoz.JournalNodes
 {
 	public class EmployeeJournalNode : JournalEntityNodeBase<Employee>
 	{
-		public override string Title => $"{EmpLastName} {EmpFirstName} {EmpMiddleName}";
+		public override string Title => FullName;
 		public string EmpLastName { get; set; }
 		public string EmpFirstName { get; set; }
 		public string EmpMiddleName { get; set; }
 		public EmployeeCategory EmpCatEnum { get; set; }
 		public EmployeeStatus Status { get; set; }
-		public string FullName => string.Format("{0} {1} {2}", EmpLastName, EmpFirstName, EmpMiddleName);
+		public string FullName => $"{EmpLastName} {EmpFirstName} {EmpMiddleName}";
 		public string RowColor => Status == EmployeeStatus.IsFired ? "grey" : "black";
 	}
 }
