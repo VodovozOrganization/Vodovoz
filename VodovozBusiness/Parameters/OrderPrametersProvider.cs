b07a5183794b413503f3ac90337bfb1f9bf99a38
Vadim <enzogord@gmail.com>
2020-12-15 18:16:58 +0300

    Изменен подбор организации по карте и из приложения на ип сосновцена, остальные как прежде

---
+++ VodovozBusiness/Parameters/OrderPrametersProvider.cs
@@ -0,0 +1,17 @@
+using System;
+using Vodovoz.Services;
+
+namespace Vodovoz.Parameters
+{
+    public class OrderPrametersProvider : IOrderPrametersProvider
+    {
+        private readonly ParametersProvider parametersProvider;
+
+        public OrderPrametersProvider(ParametersProvider parametersProvider)
+        {
+            this.parametersProvider = parametersProvider ?? throw new ArgumentNullException(nameof(parametersProvider));
+        }
+
+        public int PaymentByCardFromMobileAppId => parametersProvider.GetIntValue("PaymentByCardFromMobileAppId");
+    }
+}
