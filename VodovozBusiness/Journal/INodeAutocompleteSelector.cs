98155a02407e487ddd9c161235474fbb104e4dc3
Sergey <efimov90@gmail.com>
2020-12-21 12:34:40 +0300

    Добавление колонок для журналов Журнал пользователей, Журнал объектов изменений, Журнал полей объектов изменений
    Созданы недостающие интерфейсы для автоподбора нод
    Созданы недостующие реализации интерфейсов

---
+++ VodovozBusiness/Journal/INodeAutocompleteSelector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+
+namespace Vodovoz.Journal
+{
+	public interface INodeAutocompleteSelector : INodeSelector
+	{
+		IList Items { get; }
+		void SearchValues(params string[] values);
+		event EventHandler ListUpdated;
+	}
+}
