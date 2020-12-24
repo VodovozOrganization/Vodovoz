9fcc8f5c302a387c1268c43ebf736dad06a07631
ShubArseniy <shub.ar@yandex.ru>
2020-12-24 11:03:11 +0300

    Теперь чеки не будут отправляться если форма оплаты По карте источник Сайт и организация ЮГ

--- Services/Application/VodovozSalesReceiptsService/ReceiptServiceStarter.cs
+++ Services/Application/VodovozSalesReceiptsService/ReceiptServiceStarter.cs
@@ -5,6 +5,7 @@ using Nini.Config;
 using NLog;
 using Vodovoz.Core.DataService;
 using Vodovoz.EntityRepositories.Orders;
+using Vodovoz.Parameters;
 
 namespace VodovozSalesReceiptsService
 {
@@ -33,13 +34,20 @@ namespace VodovozSalesReceiptsService
 
 			var fiscalizationWorker = new FiscalizationWorker(
 				OrderSingletonRepository.GetInstance(),
-				new SalesReceiptSender(baseAddress)
+				new SalesReceiptSender(baseAddress),
+				new OrderPrametersProvider(ParametersProvider.Instance),
+				new OrganizationParametersProvider(ParametersProvider.Instance)
 			);
 			fiscalizationWorker.Start();
 			
 			logger.Info("Служба фискализации запущена");
 
-			var salesReceiptsInstanceProvider = new SalesReceiptsInstanceProvider(new BaseParametersProvider(), OrderSingletonRepository.GetInstance());
+			var salesReceiptsInstanceProvider = new SalesReceiptsInstanceProvider(
+				new BaseParametersProvider(),
+				OrderSingletonRepository.GetInstance(),
+				new OrderPrametersProvider(ParametersProvider.Instance),
+				new OrganizationParametersProvider(ParametersProvider.Instance)
+			);
 			WebServiceHost salesReceiptsHost = new SalesReceiptsServiceHost(salesReceiptsInstanceProvider);
 			salesReceiptsHost.AddServiceEndpoint(
 				typeof(ISalesReceiptsService),
