f3ec7d21bff39f786a8f045930d3ed470a838d30
Сергей <efimov90@gmail.com>
2021-01-12 17:20:45 +0300

    Добавлено отображение организации у договора в диалоге заказа

--- Vodovoz/Dialogs/OrderWidgets/OrderDlg.cs
+++ Vodovoz/Dialogs/OrderWidgets/OrderDlg.cs
@@ -415,7 +415,7 @@ namespace Vodovoz
 			entryTrifle.ValidationMode = ValidationType.numeric;
 			entryTrifle.Binding.AddBinding(Entity, e => e.Trifle, w => w.Text, new IntToStringConverter()).InitializeFromSource();
 
-			ylabelContract.Binding.AddFuncBinding(Entity, e => e.Contract != null ? e.Contract.Title : string.Empty, w => w.Text).InitializeFromSource();
+			ylabelContract.Binding.AddFuncBinding(Entity, e => e.Contract != null ? e.Contract.Title + " (" + e.Contract.Organization.FullName + ")" : string.Empty, w => w.Text).InitializeFromSource();
 
 			OldFieldsConfigure();
 
