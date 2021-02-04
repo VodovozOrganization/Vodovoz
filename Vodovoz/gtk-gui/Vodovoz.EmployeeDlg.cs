24bc5e2abd3517b35dc5ef10fe03d8cef2c618a7
ShubArseniy <shub.ar@yandex.ru>
2021-02-04 16:36:20 +0300

    Расширено поле "Добавочный телефон Mango" до 5 символов

--- Vodovoz/gtk-gui/Vodovoz.EmployeeDlg.cs
+++ Vodovoz/gtk-gui/Vodovoz.EmployeeDlg.cs
@@ -661,7 +661,7 @@ namespace Vodovoz
 			this.dataentryInnerPhone.CanFocus = true;
 			this.dataentryInnerPhone.Name = "dataentryInnerPhone";
 			this.dataentryInnerPhone.IsEditable = true;
-			this.dataentryInnerPhone.MaxLength = 3;
+			this.dataentryInnerPhone.MaxLength = 5;
 			this.dataentryInnerPhone.InvisibleChar = '●';
 			this.hbox17.Add(this.dataentryInnerPhone);
 			global::Gtk.Box.BoxChild w35 = ((global::Gtk.Box.BoxChild)(this.hbox17[this.dataentryInnerPhone]));
