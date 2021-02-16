2fc3ee855903f6a5122e9e2222de92fbda0627e5
Andrey Shaklein <as1854@yandex.ru>
2021-02-16 10:38:37 +0300

    I-3261 Убрал привязку от баланса водителя при разгузке и закрытии МЛ

---
+++ VodovozBusiness/EntityRepositories/Store/ICarLoadDocumentRepository.cs
@@ -0,0 +1,9 @@
+﻿using QS.DomainModel.UoW;
+
+namespace Vodovoz.EntityRepositories.Store
+{
+    public interface ICarLoadDocumentRepository
+    {
+        bool HasTerminalLoaded(IUnitOfWork uow, int routelistId, int terminalId);
+    }
+}
