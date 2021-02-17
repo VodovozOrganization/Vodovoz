4d243578b6d643d230fcffbdfcc9769c2726f458
Сергей <efimov90@gmail.com>
2021-02-17 16:23:03 +0300

    Фильтр каналов сбыта и перемещение фильтра типов ответственных лиц

---
+++ Vodovoz/JournalViewers/Retail/SalesChannelJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.JournalViewers.Retail
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class SalesChannelJournalFilterView : FilterViewBase<SalesChannelJournalFilterViewModel>
+    {
+        public SalesChannelJournalFilterView(SalesChannelJournalFilterViewModel viewModel) : base(SalesChannelJournalFilterViewModel)
+        {
+            this.Build();
+        }
+    }
+}
