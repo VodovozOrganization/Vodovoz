c7ba5f46d5ff06600580de709bf78eed55209b10
Сергей <efimov90@gmail.com>
2021-02-19 11:29:21 +0300

    Журнал для RegisteredRM

---
+++ VodovozViewModels/ViewModels/Security/RegisteredRMViewModel.cs
@@ -0,0 +1,18 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using Vodovoz.Domain.Security;
+
+namespace Vodovoz.ViewModels.ViewModels.Security
+{
+    public class RegisteredRMViewModel : EntityTabViewModelBase<RegisteredRM>
+    {
+        public RegisteredRMViewModel(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, ICommonServices commonServices, INavigationManager navigation = null)
+            : base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
+        {
+
+        }
+    }
+}
