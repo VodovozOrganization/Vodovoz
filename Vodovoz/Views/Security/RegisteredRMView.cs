fbefc68674b686d7761b4e81b6d2dc2397ddfdca
Сергей <efimov90@gmail.com>
2021-02-19 12:10:39 +0300

    GUI для Зарегистрированных RM

---
+++ Vodovoz/Views/Security/RegisteredRMView.cs
@@ -0,0 +1,14 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Security;
+
+namespace Vodovoz.Views.Security
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class RegisteredRMView : TabViewBase<RegisteredRMViewModel>
+    {
+        public RegisteredRMView(RegisteredRMViewModel viewModel) : base(viewModel)
+        {
+            this.Build();
+        }
+    }
+}
