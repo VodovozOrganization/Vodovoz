3d22de35a811e4ecf8a1b8b2b62c2d8ef47eec6b
Сергей <efimov90@gmail.com>
2021-02-19 13:41:41 +0300

    Элементы UI для зарегистрированных RM

--- Vodovoz/Views/Security/RegisteredRMView.cs
+++ Vodovoz/Views/Security/RegisteredRMView.cs
@@ -9,6 +9,27 @@ namespace Vodovoz.Views.Security
         public RegisteredRMView(RegisteredRMViewModel viewModel) : base(viewModel)
         {
             this.Build();
+
+            ConfigureDlg();
+        }
+
+        void ConfigureDlg()
+        {
+            yentryUsername.Binding.AddBinding(ViewModel.Entity, e => e.Username, w => w.Text).InitializeFromSource();
+            yentryDomainame.Binding.AddBinding(ViewModel.Entity, e => e.Domain, w => w.Text).InitializeFromSource();
+            yentrySID.Binding.AddBinding(ViewModel.Entity, e => e.SID, w => w.Text).InitializeFromSource();
+            ycheckIsActive.Binding.AddBinding(ViewModel.Entity, e => e.IsActive, w => w.Active).InitializeFromSource();
+
+            buttonSave.Clicked += (sender, e) => ViewModel.SaveAndClose();
+            buttonCancel.Clicked += (sender, e) => ViewModel.Close(false, QS.Navigation.CloseSource.Cancel);
+        }
+
+        protected void OnButtonAddUserClicked(object sender, System.EventArgs e)
+        {
+        }
+
+        protected void OnButtonDeleteUserClicked(object sender, System.EventArgs e)
+        {
         }
     }
 }
