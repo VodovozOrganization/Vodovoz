ca053658009584439af0b1dae5ac860ae20be7b7
Сергей <efimov90@gmail.com>
2021-02-26 16:39:55 +0300

    Фикс опечатки

--- Vodovoz/gtk-gui/Vodovoz.Filters.GtkViews.ComplaintFilterView.cs
+++ Vodovoz/gtk-gui/Vodovoz.Filters.GtkViews.ComplaintFilterView.cs
@@ -163,7 +163,7 @@ namespace Vodovoz.Filters.GtkViews
 			// Container child table4.Gtk.Table+TableChild
 			this.label3 = new global::Gtk.Label();
 			this.label3.Name = "label3";
-			this.label3.LabelProp = global::Mono.Unix.Catalog.GetString("Автор рекламацииы");
+			this.label3.LabelProp = global::Mono.Unix.Catalog.GetString("Автор рекламации");
 			this.table4.Add(this.label3);
 			global::Gtk.Table.TableChild w8 = ((global::Gtk.Table.TableChild)(this.table4[this.label3]));
 			w8.TopAttach = ((uint)(1));
