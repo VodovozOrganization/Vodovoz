b745d65d1ad3fc3e70d0326722b1d521a44a9e2d
ShubArseniy <shub.ar@yandex.ru>
2021-03-01 17:36:38 +0300

    Реализованы Виды наёмных автомобилей, вьюшка и фильтр отчёта эксплуатации ТС. Изменён фильтр автомобилей. Вынесен отдельно энам с месяцами

---
+++ Vodovoz/Filters/GtkViews/DriverCarKindJournalFilterView.cs
@@ -0,0 +1,14 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Logistic;
+
+namespace Vodovoz.Filters.GtkViews
+{
+    public partial class DriverCarKindJournalFilterView : FilterViewBase<DriverCarKindJournalFilterViewModel>
+    {
+        public DriverCarKindJournalFilterView(DriverCarKindJournalFilterViewModel filterViewModel) : base(filterViewModel)
+        {
+            this.Build();
+            ycheckIncludeArchive.Binding.AddBinding(ViewModel, vm => vm.IncludeArchive, w => w.Active).InitializeFromSource();
+        }
+    }
+}
--- Vodovoz/Filters/ViewModels/CarJournalFilterViewModel.cs
+++ Vodovoz/Filters/ViewModels/CarJournalFilterViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using QS.Project.Filter;
 using QS.Project.Journal;
-using QS.Services;
+using Vodovoz.CommonEnums;
+using Vodovoz.Domain.Logistic;
 
 namespace Vodovoz.Filters.ViewModels
 {
@@ -9,15 +11,48 @@ namespace Vodovoz.Filters.ViewModels
 	{
 		public CarJournalFilterViewModel()
 		{
-			UpdateWith(
-				x => x.IncludeArchive
-			);
-		}
+			visitingMasters = AllYesNo.All;
+			raskat = AllYesNo.All;
 
+			RestrictedCarTypesOfUse = new List<CarTypeOfUse>((CarTypeOfUse[])Enum.GetValues(typeof(CarTypeOfUse)));
+			SetFilterSensetivity(true);
+		}
+		
 		private bool includeArchive;
 		public virtual bool IncludeArchive {
 			get => includeArchive;
-			set => SetField(ref includeArchive, value, () => IncludeArchive);
+			set => UpdateFilterField(ref includeArchive, value);
+		}
+		
+		private AllYesNo visitingMasters;
+		public AllYesNo VisitingMasters {
+			get => visitingMasters;
+			set => UpdateFilterField(ref visitingMasters, value);
+		}
+		
+		private AllYesNo raskat;
+		public AllYesNo Raskat {
+			get => raskat;
+			set => UpdateFilterField(ref raskat, value);
+		}
+		
+		private IList<CarTypeOfUse> restrictedCarTypesOfUse;
+		public IList<CarTypeOfUse> RestrictedCarTypesOfUse {
+			get => restrictedCarTypesOfUse;
+			set => UpdateFilterField(ref restrictedCarTypesOfUse, value);
+		}
+
+		public bool CanChangeIsArchive { get; private set; }
+		public bool CanChangeVisitingMasters { get; private set; }
+		public bool CanChangeRaskat { get; private set; }
+		public bool CanChangeRestrictedCarTypesOfUse { get; private set; }
+
+		public void SetFilterSensetivity(bool isSensitive)
+		{
+			CanChangeRaskat = isSensitive;
+			CanChangeIsArchive = isSensitive;
+			CanChangeVisitingMasters = isSensitive;
+			CanChangeRestrictedCarTypesOfUse = isSensitive;
 		}
 	}
 }
