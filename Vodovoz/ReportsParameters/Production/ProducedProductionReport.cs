11a9d16d69b0cdaac9d9989a53bdd9ce5dbf55b6
Art8m <9artbe@gmail.com>
2021-03-19 15:29:01 +0300

    Год в отчёте по производству

--- Vodovoz/ReportsParameters/Production/ProducedProductionReport.cs
+++ Vodovoz/ReportsParameters/Production/ProducedProductionReport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
+using System.Linq;
 using QS.Dialog.GtkUI;
 using QS.DomainModel.UoW;
 using QS.Project.Journal.EntitySelector;
@@ -30,9 +31,12 @@ namespace Vodovoz.ReportsParameters.Production
 			
 			UoW = UnitOfWorkFactory.CreateWithoutRoot();
 			yenumcomboboxMonths.ItemsEnum = typeof(Month);
-			yenumcomboboxMonths.SelectedItem = Month.September;
-			
-			ycomboboxProduction.SetRenderTextFunc<Warehouse>(x => x.Name);
+			yenumcomboboxMonths.SelectedItem = (Month)(DateTime.Now.AddMonths(-1).Month);
+
+            ylistcomboboxYear.ItemsList = Enumerable.Range(DateTime.Now.AddYears(-10).Year, DateTime.Now.AddYears(10).Year).ToList();
+            ylistcomboboxYear.SelectedItem = DateTime.Now.Year;
+
+            ycomboboxProduction.SetRenderTextFunc<Warehouse>(x => x.Name);
 			ycomboboxProduction.ItemsList = UoW.Session.QueryOver<Warehouse>().Where(x => x.TypeOfUse == WarehouseUsing.Production).List();
 
 			entryreferenceNomenclature.SetEntityAutocompleteSelectorFactory(
@@ -75,24 +79,27 @@ namespace Vodovoz.ReportsParameters.Production
 			CultureInfo ci = new CultureInfo("ru-RU");
 			DateTimeFormatInfo mfi = ci.DateTimeFormat;
 
-			var monthNum = MonthToDateTime_AddMonth((Month) yenumcomboboxMonths.SelectedItem, 0).Month;
+            var monthNum = (int)yenumcomboboxMonths.SelectedItem;
 			string strMonthName = mfi.GetMonthName(monthNum).ToString();
+
+            var year = (int)ylistcomboboxYear.SelectedItem;
+            var reportDate = new DateTime(year, monthNum, 1);
+
+            var monthNumMinus1 = reportDate.AddMonths(-1).Month;
+            string strMonthNameMinus1 = mfi.GetMonthName(monthNumMinus1).ToString();
 			
-			var monthNumMinus1 = MonthToDateTime_AddMonth((Month) yenumcomboboxMonths.SelectedItem, -1).Month;
-			string strMonthNameMinus1 = mfi.GetMonthName(monthNumMinus1).ToString();
-			
-			var monthNumMinus2 = MonthToDateTime_AddMonth((Month) yenumcomboboxMonths.SelectedItem, -2).Month;
+			var monthNumMinus2 = reportDate.AddMonths(-2).Month;
 			string strMonthNameMinus2 = mfi.GetMonthName(monthNumMinus2).ToString();
-			
-			return new ReportInfo {
+
+            return new ReportInfo {
 				Identifier = "Production.ProducedProduction",
 				Parameters = new Dictionary<string, object> {
-					{ "month_start",         MonthStart(MonthToDateTime_AddMonth( (Month) yenumcomboboxMonths.SelectedItem,  0 )) },
-					{ "month_end",             MonthEnd(MonthToDateTime_AddMonth( (Month) yenumcomboboxMonths.SelectedItem,  0 )) },
-					{ "month_minus_1_start", MonthStart(MonthToDateTime_AddMonth( (Month) yenumcomboboxMonths.SelectedItem, -1 )) },
-					{ "month_minus_1_end",     MonthEnd(MonthToDateTime_AddMonth( (Month) yenumcomboboxMonths.SelectedItem, -1 )) },
-					{ "month_minus_2_start", MonthStart(MonthToDateTime_AddMonth( (Month) yenumcomboboxMonths.SelectedItem, -2 )) },
-					{ "month_minus_2_end",     MonthEnd(MonthToDateTime_AddMonth( (Month) yenumcomboboxMonths.SelectedItem, -2 )) },
+					{ "month_start",         MonthStart(reportDate) },
+					{ "month_end",             MonthEnd(reportDate) },
+					{ "month_minus_1_start", MonthStart(reportDate.AddMonths(-1)) },
+					{ "month_minus_1_end",     MonthEnd(reportDate.AddMonths(-1)) },
+					{ "month_minus_2_start", MonthStart(reportDate.AddMonths(-2)) },
+					{ "month_minus_2_end",     MonthEnd(reportDate.AddMonths(-2)) },
 					{ "month_name",    strMonthName },
 					{ "month_name_minus_1",  strMonthNameMinus1  },
 					{ "month_name_minus_2",  strMonthNameMinus2  },
@@ -118,79 +125,33 @@ namespace Vodovoz.ReportsParameters.Production
 		{
 			return new DateTime(date.Year,date.Month, DateTime.DaysInMonth(date.Year,date.Month));
 		}
-		
-		private DateTime MonthToDateTime_AddMonth(Month month, int n)
-		{
-			switch (month)
-			{
-				case Month.September:
-					var now1 = DateTime.Now;
-					return new DateTime(now1.Year,9,now1.Day).AddMonths(n);
-				case Month.October:
-					var now2 = DateTime.Now;
-					return new DateTime(now2.Year,10,now2.Day).AddMonths(n);
-				case Month.November:
-					var now3 = DateTime.Now;
-					return new DateTime(now3.Year,11,now3.Day).AddMonths(n);
-				case Month.December:
-					var now4 = DateTime.Now;
-					return new DateTime(now4.Year,12,now4.Day).AddMonths(n);
-				case Month.January:
-					var now5 = DateTime.Now;
-					return new DateTime(now5.Year,1,now5.Day).AddMonths(n);
-				case Month.February:
-					var now6 = DateTime.Now;
-					return new DateTime(now6.Year,2,now6.Day).AddMonths(n);
-				case Month.March:
-					var now7 = DateTime.Now;
-					return new DateTime(now7.Year,3,now7.Day).AddMonths(n);
-				case Month.April:
-					var now8 = DateTime.Now;
-					return new DateTime(now8.Year,4,now8.Day).AddMonths(n);
-				case Month.May:
-					var now9 = DateTime.Now;
-					return new DateTime(now9.Year,5,now9.Day).AddMonths(n);
-				case Month.June:
-					var now10 = DateTime.Now;
-					return new DateTime(now10.Year,6,now10.Day).AddMonths(n);
-				case Month.July:
-					var now11 = DateTime.Now;
-					return new DateTime(now11.Year,7,now11.Day).AddMonths(n);
-				case Month.August:
-					var now12 = DateTime.Now;
-					return new DateTime(now12.Year,8,now12.Day).AddMonths(n);
-				default:
-					throw new ArgumentOutOfRangeException(nameof(month), month, null);
-			}
-		}
-		
 	}
 	
 	enum Month
 	{
 		[Display ( Name = "Сентябрь")]
-		September,
+		September = 9,
 		[Display ( Name = "Октябрь")]
-		October,
+		October = 10,
 		[Display ( Name = "Ноябрь")]
-		November,
+		November = 11,
 		[Display ( Name = "Декабрь")]
-		December,
+		December = 12,
 		[Display ( Name = "Январь")]
-		January,
+		January = 1,
 		[Display ( Name = "Февраль")]
-		February,
+		February = 2,
 		[Display ( Name = "Март")]
-		March,
+		March = 3,
 		[Display ( Name = "Апрель")]
-		April,
+		April = 4,
 		[Display ( Name = "Май")]
-		May,
+		May = 5,
 		[Display ( Name = "Июнь")]
-		June,
+		June = 6,
 		[Display ( Name = "Июль")]
-		July,
+		July = 7,
 		[Display ( Name = "Август")]
-		August
+		August = 8
 	}
 }
