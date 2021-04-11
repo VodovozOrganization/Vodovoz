ffdb1f163810135b527be62014b0f21ddc3934e5
Posox <Iamposox@yandex.ru>
2021-04-12 02:52:14 +0300

    Adding rules to retail for usersettings

--- VodovozViewModels/ViewModels/Users/UserSettingsViewModel.cs
+++ VodovozViewModels/ViewModels/Users/UserSettingsViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using QS.DomainModel.UoW;
+using QS.EntityRepositories;
 using QS.Project.Domain;
 using QS.Project.Journal.EntitySelector;
 using QS.Services;
 using QS.ViewModels;
 using Vodovoz.Domain.Employees;
+using Vodovoz.Domain.Permissions;
 using Vodovoz.Infrastructure.Services;
 using Vodovoz.Journals.JournalViewModels.Organization;
 using Vodovoz.Services;
@@ -15,6 +17,9 @@ namespace Vodovoz.ViewModels.Users
     {
         private readonly IEmployeeService employeeService;
         private readonly ISubdivisionService subdivisionService;
+        private int? subdivisionId;
+        private string rulesName = "user_have_access_to_retail";
+        
         public EntityAutocompleteSelectorFactory<SubdivisionsJournalViewModel> SubdivisionAutocompleteSelectorFactory { get; }
         public IEntityAutocompleteSelectorFactory CounterpartyAutocompleteSelectorFactory { get; }
 
@@ -26,11 +31,14 @@ namespace Vodovoz.ViewModels.Users
             this.subdivisionService = subdivisionService ?? throw new ArgumentNullException(nameof(subdivisionService));
             SubdivisionAutocompleteSelectorFactory = subdivisionAutocompleteSelectorFactory;
             CounterpartyAutocompleteSelectorFactory = counterpartySelectorFactory;
+            subdivisionId = this.employeeService.GetEmployeeForUser(UoW, CommonServices.UserService.CurrentUserId)?.Subdivision?.Id;
         }
 
-        public bool IsUserFromOkk => subdivisionService.GetOkkId() == employeeService.GetEmployeeForUser(UoW, CommonServices.UserService.CurrentUserId)?.Subdivision?.Id;
+        public bool IsUserFromOkk => subdivisionService.GetOkkId() == subdivisionId;
 
-        public bool IsUserFromRetail => subdivisionService.GetRetailSubdivisionId() == employeeService
-            .GetEmployeeForUser(UoW, CommonServices.UserService.CurrentUserId)?.Subdivision?.Id;
+        public bool IsUserFromRetail => UoW.Session.QueryOver<HierarchicalPresetUserPermission>().Where(x =>
+                        x.User.Id == Entity.User.Id && x.PermissionName == rulesName && x.Value).List().Count > 0 || 
+                   UoW.Session.QueryOver<HierarchicalPresetSubdivisionPermission>().Where(x =>
+                        x.Subdivision.Id == subdivisionId && x.PermissionName == rulesName && x.Value).List().Count > 0;
     }
 }
