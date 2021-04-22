c5f72544e3669a325520bc7a1cd781cd48a3ff54
ShubArseniy <shub.ar@yandex.ru>
2021-04-22 10:32:18 +0300

    Добавил проброс валидатора паролей в ChangePassword Создал фабрику настроек для этого валидатора

---
+++ VodovozBusiness/Tools/Validation/PasswordValidationSettingsFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using NHibernate;
+using QS.DomainModel.UoW;
+using QS.Validation;
+
+namespace Vodovoz.Tools.Validation
+{
+    public class PasswordValidationSettingsFactory
+    {
+        public PasswordValidationSettingsFactory(IUnitOfWorkFactory unitOfWorkFactory)
+        {
+            this.unitOfWorkFactory = unitOfWorkFactory ?? throw new ArgumentNullException(nameof(unitOfWorkFactory));
+        }
+
+        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+
+        public DefaultPasswordValidationSettings GetPasswordValidationSettings()
+        {
+            using(var uow = unitOfWorkFactory.CreateWithoutRoot()) {
+                return new DefaultPasswordValidationSettings {
+                    MinNumberCount = uow.Session.CreateSQLQuery("SELECT @@GLOBAL.simple_password_check_digits AS value")
+                        .AddScalar("value", NHibernateUtil.Int32)
+                        .List<int>().First(),
+                    MinLength = uow.Session.CreateSQLQuery("SELECT @@GLOBAL.simple_password_check_minimal_length AS value")
+                        .AddScalar("value", NHibernateUtil.Int32)
+                        .List<int>().First(),
+                    MinLetterSameCaseCount = uow.Session.CreateSQLQuery("SELECT @@GLOBAL.simple_password_check_letters_same_case AS value")
+                        .AddScalar("value", NHibernateUtil.Int32)
+                        .List<int>().First(),
+                    MinOtherCharactersCount = uow.Session.CreateSQLQuery("SELECT @@GLOBAL.simple_password_check_other_characters AS value")
+                        .AddScalar("value", NHibernateUtil.Int32)
+                        .List<int>().First(),
+                    AllowWhitespaces = false,
+                    ASCIIOnly = true,
+                    NotAllowedStrings = new[] { "'", Environment.NewLine }
+                };
+            }
+        }
+    }
+}
