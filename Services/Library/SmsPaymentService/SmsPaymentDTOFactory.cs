929b25c8ebc976ad80573aedfc4a4e8a2cfae32e
Art8m <9artbe@gmail.com>
2021-05-06 11:44:55 +0300

    3338 Исправление суммы, отправляемой в ЮКассу (распределение цен номенклатур)

--- Services/Library/SmsPaymentService/SmsPaymentDTOFactory.cs
+++ Services/Library/SmsPaymentService/SmsPaymentDTOFactory.cs
@@ -9,7 +9,8 @@ namespace SmsPaymentService
     {
         public SmsPaymentDTO CreateSmsPaymentDTO(SmsPayment smsPayment, Order order)
         {
-            var newSmsPaymentDTO = new SmsPaymentDTO {
+            var newSmsPaymentDTO = new SmsPaymentDTO
+            {
                 Recepient = smsPayment.Recepient.Name,
                 RecepientId = smsPayment.Recepient.Id,
                 PhoneNumber = smsPayment.PhoneNumber,
@@ -18,18 +19,52 @@ namespace SmsPaymentService
                 PaymentCreationDate = smsPayment.CreationDate,
                 Amount = smsPayment.Amount,
                 RecepientType = smsPayment.Recepient.PersonType,
-                Items = new List<SmsPaymentItemDTO>()
+                Items = GetCalculatedSmsPaymentItemDTOs(order.OrderItems)
             };
 
-            foreach(var orderItem in order.OrderItems) {
-                newSmsPaymentDTO.Items.Add(new SmsPaymentItemDTO {
-                    Name = orderItem.Nomenclature.OfficialName,
-                    Quantity = orderItem.CurrentCount,
-                    Price = orderItem.Sum / orderItem.Count
-                });
+            return newSmsPaymentDTO;
+        }
+
+        private List<SmsPaymentItemDTO> GetCalculatedSmsPaymentItemDTOs(IList<OrderItem> itemList)
+        {
+            List<SmsPaymentItemDTO> smsPaymentDTOList = new List<SmsPaymentItemDTO>();
+
+            foreach (var item in itemList)
+            {
+                decimal price = decimal.Round(item.Sum / item.Count, 2, MidpointRounding.AwayFromZero);
+                bool isDevided = item.Sum == price * item.Count;
+
+                if (isDevided)
+                {
+                    smsPaymentDTOList.Add(
+                        new SmsPaymentItemDTO()
+                        {
+                            Name = item.Nomenclature.OfficialName,
+                            Quantity = item.CurrentCount,
+                            Price = price
+                        });
+                }
+                else
+                {
+                    smsPaymentDTOList.Add(
+                        new SmsPaymentItemDTO()
+                        {
+                            Name = item.Nomenclature.OfficialName,
+                            Quantity = item.CurrentCount - 1,
+                            Price = price
+                        });
+
+                    smsPaymentDTOList.Add(
+                        new SmsPaymentItemDTO()
+                        {
+                            Name = item.Nomenclature.OfficialName,
+                            Quantity = 1,
+                            Price = item.Sum - (price * (item.Count - 1))
+                        });
+                }
             }
 
-            return newSmsPaymentDTO;
+            return smsPaymentDTOList;
         }
     }
 }
