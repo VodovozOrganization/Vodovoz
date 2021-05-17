1d50456b84cda6d69b439f668471fd118a562183
Сергей <efimov90@gmail.com>
2021-05-17 17:32:25 +0300

    Подключение Autofac

--- DriverAPI/Program.cs
+++ DriverAPI/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -20,6 +21,7 @@ namespace DriverAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
