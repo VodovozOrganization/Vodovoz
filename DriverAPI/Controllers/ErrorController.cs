5f2d8bd7039657860e8faf3b7e94e2abbdb51546
Сергей <efimov90@gmail.com>
2021-05-19 09:19:24 +0300

    Логгирование через контроллер ошибок

--- DriverAPI/Controllers/ErrorController.cs
+++ DriverAPI/Controllers/ErrorController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DriverAPI.Controllers
 {
@@ -9,13 +10,29 @@ namespace DriverAPI.Controllers
     [ApiController]
     public class ErrorController : ControllerBase
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
         [HttpGet]
         [Route("/api/error")]
         public ErrorResponseModel Error()
         {
             var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
-            var exception = context?.Error ?? new System.Exception("Вызван обработчик ошибок без ошибки");
+            var exception = context?.Error;
             var code = 500;
+
+            if (exception != null)
+            {
+                logger.LogError(exception, exception.Message);
+            }
+            else
+            {
+                exception = new System.Exception("Вызван обработчик ошибок без ошибки");
+            }
 
             Response.StatusCode = code;
 
