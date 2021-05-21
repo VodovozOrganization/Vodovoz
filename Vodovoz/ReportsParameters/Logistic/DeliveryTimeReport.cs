6d8ea72f353cde1d4afa8c6c3159ff1f7357a9f8
ShubArseniy <shub.ar@yandex.ru>
2021-05-21 15:24:14 +0300

    Изменения в отчёте "Время доставки" Заменил виджет выбора частей города, добавил выбор принадлежности МЛ, заменил виджет выбора времени Также исправил немного вью другого отчёта OrdersByDistrictsAndDeliverySchedulesReport

--- Vodovoz/ReportsParameters/Logistic/DeliveryTimeReport.cs
+++ Vodovoz/ReportsParameters/Logistic/DeliveryTimeReport.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Bindings.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using Gamma.ColumnConfig;
+using QS.Dialog;
 using QS.Dialog.GtkUI;
 using QS.DomainModel.UoW;
 using QS.Report;
+using QS.Services;
 using QSReport;
 using Vodovoz.Domain.Sale;
 
@@ -12,23 +15,18 @@ namespace Vodovoz.ReportsParameters.Logistic
 {
 	public partial class DeliveryTimeReport : SingleUoWWidgetBase, IParametersWidget
 	{
-		GenericObservableList<GeographicGroup> geographicGroups;
+		private readonly IInteractiveService interactiveService;
 
-		public DeliveryTimeReport ()
+		public DeliveryTimeReport(IUnitOfWorkFactory unitOfWorkFactory, IInteractiveService interactiveService)
 		{
-			this.Build();
+			if(unitOfWorkFactory == null)
+			{
+				throw new ArgumentNullException(nameof(unitOfWorkFactory));
+			}
+			this.interactiveService = interactiveService ?? throw new ArgumentNullException(nameof(interactiveService));
+			UoW = unitOfWorkFactory.CreateWithoutRoot();
+			Build();
 			ConfigureDlg();
-		}
-
-		void ConfigureDlg()
-		{
-			UoW = UnitOfWorkFactory.CreateWithoutRoot();
-			geograficGroup.UoW = UoW;
-			geograficGroup.Label = "Часть города:";
-			geographicGroups = new GenericObservableList<GeographicGroup>();
-			geograficGroup.Items = geographicGroups;
-			foreach(var gg in UoW.Session.QueryOver<GeographicGroup>().List())
-				geographicGroups.Add(gg);
 		}
 
 		#region IParametersWidget implementation
@@ -39,35 +37,92 @@ namespace Vodovoz.ReportsParameters.Logistic
 
 		#endregion
 
-		private int[] GetResultIds(IEnumerable<int> ids)
+		private void ConfigureDlg()
 		{
-			return ids.Any() ? ids.ToArray() : new int[] { 0 };
+			ytreeGeoGroups.HeadersVisible = false;
+			ytreeGeoGroups.ColumnsConfig = FluentColumnsConfig<SelectableParameter>.Create()
+				.AddColumn("").AddToggleRenderer(x => x.IsSelected)
+				.AddColumn("").AddTextRenderer(x => x.GeographicGroup.Name)
+				.Finish();
+			ytreeGeoGroups.ItemsDataSource =
+				UoW.GetAll<GeographicGroup>().Select(x => new SelectableParameter { GeographicGroup = x, IsSelected = true }).ToList();
+
+			ytreeRouteListTypeOfUse.HeadersVisible = false;
+			ytreeRouteListTypeOfUse.ColumnsConfig = FluentColumnsConfig<SelectableParameter>.Create()
+				.AddColumn("").AddToggleRenderer(x => x.IsSelected)
+				.AddColumn("").AddEnumRenderer(x => x.RouteListTypeOfUse)
+				.Finish();
+			ytreeRouteListTypeOfUse.ItemsDataSource = Enum.GetValues(typeof(RouteListTypeOfUse)).Cast<RouteListTypeOfUse>()
+				.Select(x => new SelectableParameter { RouteListTypeOfUse = x, IsSelected = x == RouteListTypeOfUse.Logistics}).ToList();
 		}
 
-		void OnUpdate (bool hide = false)
-		{
-			if (LoadReport != null) {
-				LoadReport (this, new LoadReportEventArgs (GetReportInfo (), hide));
-			}
-		}
+		private void OnUpdate(bool hide = false) => LoadReport?.Invoke(this, new LoadReportEventArgs(GetReportInfo(), hide));
 
-		private ReportInfo GetReportInfo ()
+		private ReportInfo GetReportInfo()
 		{
-			return new ReportInfo {
+			return new ReportInfo
+			{
 				Identifier = "Logistic.DeliveryTime",
 				Parameters = new Dictionary<string, object>
 				{
 					{ "beforeTime", ytimeDelivery.Text },
-					{ "geographic_groups", GetResultIds(geographicGroups.Select(g => g.Id)) }
+					{ "geographic_groups", GetSelectedGeoGroupIds() },
+					{ "rl_type_of_use", GetSelectedRouteListTypeOfUses() }
 				}
 			};
 		}
 
-		protected void OnButtonCreateReportClicked(object sender, EventArgs e) => OnUpdate(true);
-
-		protected void OnYtimeDeliveryChanged (object sender, EventArgs e)
+		private void OnButtonCreateReportClicked(object sender, EventArgs e)
 		{
-			buttonCreateReport.Sensitive = ytimeDelivery.Time != default (TimeSpan);
+			if(!GetSelectedGeoGroupIds().Any())
+			{
+				interactiveService.ShowMessage(ImportanceLevel.Warning, "Не выбрана ни одна часть города");
+				return;
+			}
+			if(!GetSelectedRouteListTypeOfUses().Any())
+			{
+				interactiveService.ShowMessage(ImportanceLevel.Warning, "Не выбрана ни одна принадлежность МЛ");
+				return;
+			}
+			OnUpdate(true);
+		}
+
+		private IEnumerable<RouteListTypeOfUse> GetSelectedRouteListTypeOfUses()
+		{
+			return (ytreeRouteListTypeOfUse.ItemsDataSource as IEnumerable<SelectableParameter>)
+				?.Where(x => x.IsSelected)
+				.Select(x => x.RouteListTypeOfUse)
+				?? new List<RouteListTypeOfUse>();
+		}
+
+		private IEnumerable<int> GetSelectedGeoGroupIds()
+		{
+			return (ytreeGeoGroups.ItemsDataSource as IEnumerable<SelectableParameter>)
+				?.Where(x => x.IsSelected)
+				.Select(x => x.GeographicGroup.Id)
+				?? new List<int>();
+		}
+
+		private void OnYtimeDeliveryChanged(object sender, EventArgs e)
+		{
+			buttonCreateReport.Sensitive = ytimeDelivery.Time != default(TimeSpan);
+		}
+
+		private class SelectableParameter
+		{
+			public bool IsSelected { get; set; }
+			public GeographicGroup GeographicGroup { get; set; }
+			public RouteListTypeOfUse RouteListTypeOfUse { get; set; }
+		}
+
+		private enum RouteListTypeOfUse
+		{
+			[Display(Name = "Логистика")]
+			Logistics,
+			[Display(Name = "СЦ")]
+			ServiceCenter,
+			[Display(Name = "Фуры")]
+			CompanyTrucks
 		}
 	}
 }
