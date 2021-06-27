bbf55d2fc1e766bca4b268e7a35474687c08cb6b
Vadim <enzogord@gmail.com>
2021-06-28 02:12:41 +0300

    New user dialog

---
+++ Vodovoz/Views/Users/UserView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.Dialogs.Users;
+namespace Vodovoz.Views.Users
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class UserView : TabViewBase<UserViewModel>
+	{
+		public UserView(UserViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+
+			ConfigureDialog();
+		}
+
+		private void ConfigureDialog()
+		{
+
+		}
+	}
+}
