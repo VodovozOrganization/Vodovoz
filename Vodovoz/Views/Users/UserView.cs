ece5535fef61e4a76da2f8244eb3709e5e70289a
Vadim <enzogord@gmail.com>
2021-06-28 04:04:57 +0300

    Доработка диалога

--- Vodovoz/Views/Users/UserView.cs
+++ Vodovoz/Views/Users/UserView.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using QS.Views.GtkUI;
-using Vodovoz.Dialogs.Users;
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels;
+using QS.Widgets.GtkUI;
+using Vodovoz.Core.Permissions;
+
 namespace Vodovoz.Views.Users
 {
 	[System.ComponentModel.ToolboxItem(true)]
@@ -15,7 +17,55 @@ namespace Vodovoz.Views.Users
 
 		private void ConfigureDialog()
 		{
+			var presetPermissionWidget = new UserPresetPermissionWidget();
+			var documentPermissionWidget = new UserEntityPermissionWidget();
+			var specialDocumentPermissionWidget = new SubdivisionForUserEntityPermissionWidget();
 
+			//Отключены, так как это простейший вариант диалога, вводимый из-за срочности ввода новых прав на склады
+			tableRoles.Visible = false;
+			ybuttonSetNewPassword.Visible = false;
+			ybuttonResetPassword.Visible = false;
+			PasswordWarning.Visible = false;
+			ycheckRequirePasswordChange.Visible = false;
+
+			ycheckIsAdmin.Binding.AddBinding(ViewModel.Entity, e => e.IsAdmin, w => w.Active).InitializeFromSource();
+			ycheckUserDisabled.Binding.AddBinding(ViewModel.Entity, e => e.Deactivated, w => w.Active).InitializeFromSource();
+			ytextviewComment.Binding.AddBinding(ViewModel.Entity, e => e.Description, w => w.Buffer.Text).InitializeFromSource();
+			ylabelIdValue.Binding.AddBinding(ViewModel.Entity, e => e.Id, w => w.LabelProp).InitializeFromSource();
+			ylabelDisplayName.Binding.AddBinding(ViewModel.Entity, e => e.Name, w => w.LabelProp).InitializeFromSource();
+			ylabelLogin.Binding.AddBinding(ViewModel.Entity, e => e.Login, w => w.LabelProp).InitializeFromSource();
+
+			buttonSave.Clicked += (sender, e) => {
+				presetPermissionWidget.Save();
+				documentPermissionWidget.Save();
+				specialDocumentPermissionWidget.Save();
+				ViewModel.SaveCommand.Execute();
+			};
+
+			buttonCancel.Clicked += (sender, e) => ViewModel.CancelCommand.Execute();
+
+			ybuttonPresetPrivileges.Sensitive = false;
+			ybuttonDocumentPrivileges.Sensitive = false;
+			ybuttonWarehousePrivileges.Sensitive = false;
+			ybuttonSpecialDocumentPrivileges.Sensitive = false;
+
+			if(ViewModel.Entity.Id != 0)
+			{
+				presetPermissionWidget.ConfigureDlg(ViewModel.UoW, ViewModel.Entity);
+				vboxPresetPrivileges.Add(presetPermissionWidget);
+				presetPermissionWidget.Show();
+				ybuttonPresetPrivileges.Sensitive = true;
+
+				documentPermissionWidget.ConfigureDlg(ViewModel.UoW, ViewModel.Entity);
+				vboxDocumentPrivileges.Add(documentPermissionWidget);
+				documentPermissionWidget.Show();
+				ybuttonDocumentPrivileges.Sensitive = true;
+
+				specialDocumentPermissionWidget.ConfigureDlg(ViewModel.UoW, ViewModel.Entity);
+				vboxSpecialDocumentPrivileges.Add(specialDocumentPermissionWidget);
+				specialDocumentPermissionWidget.Show();
+				ybuttonSpecialDocumentPrivileges.Sensitive = true;
+			}
 		}
 	}
 }
---
+++ VodovozViewModels/Dialogs/Users/UserViewModel.cs
@@ -0,0 +1,48 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using Vodovoz.Domain.Employees;
+using QS.Commands;
+
+namespace Vodovoz.ViewModels
+{
+    public class UserViewModel : EntityTabViewModelBase<User>
+    {
+        public UserViewModel(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, ICommonServices commonServices, INavigationManager navigation = null) 
+            : base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
+        {
+		}
+
+		private DelegateCommand _saveCommand;
+		public DelegateCommand SaveCommand
+		{
+			get
+			{
+				if(_saveCommand == null)
+				{
+					_saveCommand = new DelegateCommand(() => {
+						UoW.Save();
+					});
+				}
+				return _saveCommand;
+			}
+		}
+
+		private DelegateCommand _cancelCommand;
+		public DelegateCommand CancelCommand
+		{
+			get
+			{
+				if(_cancelCommand == null)
+				{
+					_cancelCommand = new DelegateCommand(() => {
+						Close(true, CloseSource.Cancel);
+					});
+				}
+				return _cancelCommand;
+			}
+		}
+	}
+}
