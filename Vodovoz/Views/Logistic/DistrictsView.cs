7497ad7cecbf3e3c3baafa951f9f2270919f4612
Posox <Iamposox@yandex.ru>
2021-07-02 17:57:12 +0300

    Adding View Districts

---
+++ Vodovoz/Views/Logistic/DistrictsView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gamma.ColumnConfig;
+using QS.Dialog;
+using QS.DomainModel.UoW;
+using QS.Views.GtkUI;
+using Vodovoz.Domain.Logistic;
+using Vodovoz.ViewModels.Logistic;
+
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DistrictsView : TabViewBase<DistrictViewModel>, ISingleUoWDialog
+	{
+		public DistrictsView(DistrictViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+
+		public IUnitOfWork UoW { get; set; }
+	}
+}
---
+++ VodovozBusiness/Domain/Logistic/Districts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.DomainModel.Entity;
+
+namespace Vodovoz.Domain.Logistic
+{
+	public class Districts : PropertyChangedBase, IDomainObject
+	{
+		public Districts()
+		{
+		}
+
+		public int Id { get; set; }
+	}
+}
