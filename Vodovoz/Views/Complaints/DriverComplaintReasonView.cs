082d65738917f67bb83e23da1d3d353e7b044665
Сергей <efimov90@gmail.com>
2021-07-07 15:52:16 +0300

    Пустой диалог для DriverComplainReason

---
+++ Vodovoz/Views/Complaints/DriverComplaintReasonView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Complaints;
+
+namespace Vodovoz.Views.Complaints
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DriverComplaintReasonView : TabViewBase<DriverComplaintReasonViewModel>
+	{
+		public DriverComplaintReasonView(DriverComplaintReasonViewModel driverComplaintReasonViewModel)
+			: base(driverComplaintReasonViewModel)
+		{
+			this.Build();
+		}
+	}
+}
