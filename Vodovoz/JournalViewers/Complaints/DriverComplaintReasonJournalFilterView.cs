2380bb1598e4456dada585796337301ad357a9b8
Сергей <efimov90@gmail.com>
2021-07-09 10:14:28 +0300

    GUI фильтра

---
+++ Vodovoz/JournalViewers/Complaints/DriverComplaintReasonJournalFilterView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Complaints;
+
+namespace Vodovoz.JournalViewers.Complaints
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DriverComplaintReasonJournalFilterView : FilterViewBase<DriverComplaintReasonJournalFilterViewModel>
+	{
+		public DriverComplaintReasonJournalFilterView(DriverComplaintReasonJournalFilterViewModel viewModel)
+			: base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
