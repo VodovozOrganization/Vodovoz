017d70c919b941a775d06157ba25336481ee4799
kozlov-d <sprunk97@gmail.com>
2021-07-13 14:04:23 +0700

    +Документы терминала водителя и маппинг

---
+++ VodovozBusiness/Domain/Documents/DriverTerminal/DriverAttachedTerminalReturnDocument.cs
@@ -0,0 +1,40 @@
+﻿using QS.DomainModel.Entity;
+using QS.DomainModel.Entity.EntityPermissions;
+using Vodovoz.Domain.Goods;
+using Vodovoz.Domain.Operations;
+using Vodovoz.Domain.Store;
+
+namespace Vodovoz.Domain.Documents.DriverTerminal
+{
+	[Appellative(Gender = GrammaticalGender.Masculine,
+		Nominative = "документ возврата терминала водителя",
+		NominativePlural = "документы возврата терминалов водителей")]
+	[EntityPermission]
+	public class DriverAttachedTerminalReturnDocument : DriverAttachedTerminalDocumentBase
+	{
+		public virtual string Title =>
+			$"Возврат терминала {CreationDate.ToShortDateString()} в {CreationDate.ToShortTimeString()}\r\n " +
+			$"на склад {WarehouseMovementOperation?.IncomingWarehouse?.Name}";
+
+		public override void CreateMovementOperations(Warehouse incomeWarehouse, Nomenclature terminal)
+		{
+			WarehouseMovementOperation = new WarehouseMovementOperation
+			{
+				WriteoffWarehouse = null,
+				IncomingWarehouse = incomeWarehouse,
+				Amount = 1,
+				Equipment = null,
+				Nomenclature = terminal,
+				OperationTime = CreationDate
+			};
+
+			EmployeeNomenclatureMovementOperation = new EmployeeNomenclatureMovementOperation
+			{
+				Amount = -1,
+				Employee = Driver,
+				Nomenclature = terminal,
+				OperationTime = CreationDate
+			};
+		}
+	}
+}
