625b5beacf1f8885ed287a0bc31f4bcfb600e535
DobriyMisha <dobriymisha@gmail.com>
2021-07-28 13:54:52 +0300

    New notification class for undelivery

---
+++ VodovozBusiness/Domain/Sms/UndeliveryNotApprovedSmsNotification.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QS.DomainModel.Entity;
+using QS.DomainModel.Entity.EntityPermissions;
+using QS.HistoryLog;
+using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Orders;
+
+namespace Vodovoz.Domain.Sms
+{
+	//FEDOS
+	[Appellative(Gender = GrammaticalGender.Masculine,
+		NominativePlural = "смс уведомления при н/согл автопереносе в недовозе при переносе",
+		Nominative = "смс уведомление при н/согл автопереносе в недовозе при переносе")]
+	[EntityPermission]
+	[HistoryTrace]
+	public class UndeliveryNotApprovedSmsNotification : SmsNotification
+	{
+		public override SmsNotificationType SmsNotificationType => SmsNotificationType.UndeliveryNotApproved;
+
+		private UndeliveredOrder undeliveredOrder;
+		[Display(Name = "Недовоз")]
+		public virtual UndeliveredOrder UndeliveredOrder {
+			get => undeliveredOrder;
+			set => SetField(ref undeliveredOrder, value, () => UndeliveredOrder);
+		}
+
+		private Counterparty counterparty;
+		[Display(Name = "Контрагент")]
+		public virtual Counterparty Counterparty {
+			get => counterparty;
+			set => SetField(ref counterparty, value, () => Counterparty);
+		}
+	}
+}
