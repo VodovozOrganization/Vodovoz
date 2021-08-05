2ab34f9e94af2e46945d7f90847d4735a3407dee
kozlov-d <sprunk97@gmail.com>
2021-08-05 19:26:12 +0700

    Счетчики выполнения, улучшения запроса

--- VodovozViewModels/ViewModels/Suppliers/WarehousesBalanceSummaryViewModel.cs
+++ VodovozViewModels/ViewModels/Suppliers/WarehousesBalanceSummaryViewModel.cs
@@ -33,14 +33,15 @@ namespace Vodovoz.ViewModels.ViewModels.Suppliers
 	public class BalanceSummaryReport
 	{
 		public List<string> WarehousesTitles { get; set; }
-		public List<BalanceSummaryRow> SummaryRows { get; set; }
+		public BalanceSummaryRow[] SummaryRows { get; set; }
 	}
 
 	public class BalanceBean
 	{
 		public int NomId { get; set; }
 		public int WarehouseId { get; set; }
-		public decimal? Amount { get; set; }
+		public decimal Amount { get; set; }
+		public decimal MinCount { get; set; }
 	}
 
 	public class WarehousesBalanceSummaryViewModel : DialogTabViewModelBase
@@ -61,6 +62,8 @@ namespace Vodovoz.ViewModels.ViewModels.Suppliers
 		}
 
 		#region Свойства
+
+		public double AlgoTime { get; set; }
 
 		public DateTime? EndDate { get; set; } = DateTime.Today;
 
@@ -114,12 +117,6 @@ namespace Vodovoz.ViewModels.ViewModels.Suppliers
 
 		public async Task<BalanceSummaryReport> ActionGenerateReport(CancellationToken cancellationToken)
 		{
-			/*if(!SelectedIndicators.Any())
-			{
-				_interactiveService.ShowMessage(ImportanceLevel.Warning, "Не выбрано ни 1 показателя");
-				return null;
-			}*/
-
 			try
 			{
 				return await Generate(EndDate, cancellationToken);
@@ -137,115 +134,119 @@ namespace Vodovoz.ViewModels.ViewModels.Suppliers
 			cancellationToken.ThrowIfCancellationRequested();
 			endDate = endDate?.AddHours(23).AddMinutes(59).AddSeconds(59);
 
-			var report = new BalanceSummaryReport {WarehousesTitles = new List<string>(), SummaryRows = new List<BalanceSummaryRow>()};
-			var wars = _warsFilter.ParameterSets.FirstOrDefault(ps => ps.ParameterName == "warehouses")?.GetIncludedParameters().ToList();
-
-			report.WarehousesTitles = wars?.Select(x => x.Title).ToList();
 			var nomsSet = _nomsFilter.ParameterSets.FirstOrDefault(x => x.ParameterName == "nomenclatures");
 			var noms = nomsSet?.GetIncludedParameters().ToList();
-			var groupsSet = _nomsFilter.ParameterSets.FirstOrDefault(x => x.ParameterName == "product_groups");
-			var groupsIds = groupsSet?.GetSelectedValues().ToList();
-			var includeGroups = groupsSet?.FilterType == SelectableFilterType.Include;
-//FIXME учесть группы
-//FIXME учесть ДАТУ
+			var groups = _nomsFilter.ParameterSets.FirstOrDefault(x => x.ParameterName == "product_groups")?
+				.GetIncludedParameters().ToList();
+			var wars = _warsFilter.ParameterSets
+				.FirstOrDefault(ps => ps.ParameterName == "warehouses")?.GetIncludedParameters().ToList();
+
+			var report = new BalanceSummaryReport
+			{
+				WarehousesTitles = new List<string>(),
+				SummaryRows = new BalanceSummaryRow[noms?.Count ?? 0]
+			};
+			report.WarehousesTitles = wars?.Select(x => x.Title).ToList();
 
 			Nomenclature nomAlias = null;
-			Warehouse warAlias = null;
 			WarehouseMovementOperation inAlias = null;
 			WarehouseMovementOperation woAlias = null;
 			BalanceBean resultAlias = null;
 			var nomsIds = noms?.Select(x => (int) x.Value).ToList();
 			var warsIds = wars?.Select(x => (int) x.Value).ToList();
+			var groupsIds = groups?.Select(x => (int) x.Value).ToList();
 
 			var incomeQuery = UoW.Session.QueryOver(() => inAlias)
 				.Fetch(SelectMode.Fetch, () => inAlias.Nomenclature)
 				.Fetch(SelectMode.Fetch, () => inAlias.IncomingWarehouse)
-				.Where(() => inAlias.OperationTime < endDate)
+				.Where(() => inAlias.OperationTime <= endDate)
 				.Left.JoinAlias(() => inAlias.Nomenclature, () => nomAlias)
-				.Left.JoinAlias(() => inAlias.IncomingWarehouse, () => warAlias)
 				.Where(Restrictions.In(Projections.Property(() => nomAlias.Id), nomsIds))
-				.Where(Restrictions.In(Projections.Property(() => warAlias.Id), warsIds))
+				//.Where(Restrictions.In(Projections.Property(() => nomAlias.ProductGroup.Id), groupsIds))
+				.Where(Restrictions.In(Projections.Property(() => inAlias.IncomingWarehouse.Id), warsIds))
 				.SelectList(list => list
-					.SelectGroup(() => warAlias.Id).WithAlias(() => resultAlias.WarehouseId)
+					.SelectGroup(() => inAlias.IncomingWarehouse.Id).WithAlias(() => resultAlias.WarehouseId)
 					.SelectGroup(() => nomAlias.Id).WithAlias(() => resultAlias.NomId)
+					.Select(() => nomAlias.MinStockCount).WithAlias(() => resultAlias.MinCount)
 					.Select(Projections.Sum(Projections.Property(() => inAlias.Amount))).WithAlias(() => resultAlias.Amount)
-				).TransformUsing(Transformers.AliasToBean<BalanceBean>());
+				)
+				.OrderBy(() => inAlias.Nomenclature.Id).Asc
+				.ThenBy(() => inAlias.IncomingWarehouse.Id).Asc
+				.TransformUsing(Transformers.AliasToBean<BalanceBean>());
 
 			var writeoffQuery = UoW.Session.QueryOver(() => woAlias)
 				.Fetch(SelectMode.Fetch, () => woAlias.Nomenclature)
 				.Fetch(SelectMode.Fetch, () => woAlias.WriteoffWarehouse)
-				.Where(() => woAlias.OperationTime < endDate)
+				.Where(() => woAlias.OperationTime <= endDate)
 				.Left.JoinAlias(() => woAlias.Nomenclature, () => nomAlias)
-				.Left.JoinAlias(() => woAlias.WriteoffWarehouse, () => warAlias)
-				.Where(Restrictions.In(Projections.Property(() => nomAlias.Id), nomsIds))
-				.Where(Restrictions.In(Projections.Property(() => warAlias.Id), warsIds))
+				.Where(Restrictions.In(Projections.Property(() => woAlias.Nomenclature.Id), nomsIds))
+				//.Where(Restrictions.In(Projections.Property(() => nomAlias.ProductGroup.Id), groupsIds))
+				.Where(Restrictions.In(Projections.Property(() => woAlias.WriteoffWarehouse.Id), warsIds))
 				.SelectList(list => list
-					.SelectGroup(() => warAlias.Id).WithAlias(() => resultAlias.WarehouseId)
+					.SelectGroup(() => woAlias.WriteoffWarehouse.Id).WithAlias(() => resultAlias.WarehouseId)
 					.SelectGroup(() => nomAlias.Id).WithAlias(() => resultAlias.NomId)
 					.Select(Projections.Sum(Projections.Property(() => woAlias.Amount))).WithAlias(() => resultAlias.Amount)
-				).TransformUsing(Transformers.AliasToBean<BalanceBean>());
+				)
+				.OrderBy(() => nomAlias.Id).Asc
+				.ThenBy(() => woAlias.WriteoffWarehouse.Id).Asc
+				.TransformUsing(Transformers.AliasToBean<BalanceBean>());
 
 			var inResult = await incomeQuery.ListAsync<BalanceBean>(cancellationToken);
 			var woResult = await writeoffQuery.ListAsync<BalanceBean>(cancellationToken);
 
-			/*var incomes = incomeQuery.List();
-			UoW.Session.Clear();
-			var writeoffs = writeoffQuery.List();*/
-
-			foreach(var nom in noms)
+			//Кол-во списаний != кол-во начислений, используется два счетчика
+			var addedCounter = 0;
+			var removedCounter = 0;
+			var start = DateTime.Now;
+			for(var nomsCounter = 0; nomsCounter < noms?.Count; nomsCounter++)
 			{
 				cancellationToken.ThrowIfCancellationRequested();
-				var row = new BalanceSummaryRow();
-				row.NomId = (int) nom.Value;
-				row.NomTitle = nom.Title;
-				row.Separate = new decimal[wars?.Count ?? 0];
-				row.Min = UoW.Session.QueryOver<Nomenclature>().Where(n => n.Id == row.NomId).List().Select(x => x.MinStockCount).First();
-
-				var i = 0;
-
-				foreach(var war in wars)
+				var row = new BalanceSummaryRow
 				{
-					cancellationToken.ThrowIfCancellationRequested();
-					/*var added = incomes
-						.Where(x => x.Nomenclature.Id == (int)nom.Value && x.IncomingWarehouse.Id == (int)war.Value)
-						.Sum(x => x.Amount);
-					var removed = writeoffs
-						.Where(x => x.Nomenclature.Id == (int)nom.Value && x.WriteoffWarehouse.Id == (int)war.Value)
-						.Sum(x => x.Amount);*/
-					/*var added = incomeQuery.List<decimal?>().FirstOrDefault();
-					var removed = writeoffQuery.List<decimal?>().FirstOrDefault();*/
-
-					/*var incomeQuery = UoW.Session.QueryOver(() => inAlias)
-						.Left.JoinAlias(() => inAlias.Nomenclature, () => nomAlias)
-						.Left.JoinAlias(() => inAlias.IncomingWarehouse, () => warAlias)
-						.Where(Restrictions.Eq(Projections.Property(() => nomAlias.Id), nom.Value))
-						.Where(Restrictions.Eq(Projections.Property(() => warAlias.Id), war.Value));
-					var writeoffQuery = UoW.Session.QueryOver(() => woAlias)
-						.Left.JoinAlias(() => woAlias.Nomenclature, () => nomAlias)
-						.Left.JoinAlias(() => woAlias.WriteoffWarehouse, () => warAlias)
-						.Where(Restrictions.Eq(Projections.Property(() => nomAlias.Id), nom.Value))
-						.Where(Restrictions.Eq(Projections.Property(() => warAlias.Id), war.Value));
-
-					incomeQuery.Select(Projections.Sum(Projections.Property(() => inAlias.Amount)));
-					writeoffQuery.Select(Projections.Sum(Projections.Property(() => woAlias.Amount)));
-					
-					var added = incomeQuery.List<decimal?>().FirstOrDefault();
-					var removed = writeoffQuery.List<decimal?>().FirstOrDefault();
-
-					row.Separate[i] = (added ?? 0) - (removed ?? 0);*/
-
-					var added = inResult
-						.Where(x => x.NomId == (int)nom.Value && x.WarehouseId == (int)war.Value)
-						.Select(x => x.Amount).FirstOrDefault();
-					var removed = woResult
-						.Where(x => x.NomId == (int)nom.Value && x.WarehouseId == (int)war.Value)
-						.Select(x => x.Amount).FirstOrDefault();
-					row.Separate[i] = (added ?? 0) - (removed ?? 0);
-					i++;
+					NomId = (int) noms[nomsCounter].Value,
+					NomTitle = noms[nomsCounter].Title,
+					Separate = new decimal[wars?.Count ?? 0]
+				};
+				//Т.к. для ТМЦ может не быть incomingWarMovOps, то не будет MinStockCount. Дозагрузка сильно увеличивает время формирования
+				//при этом дозагрузка зачастую бессмысленна, т.к. поле MinStockCount != 0 у малого кол-ва номенклатур.
+				if(addedCounter < inResult.Count && inResult[addedCounter].NomId == row.NomId)
+				{
+					row.Min = inResult[addedCounter].MinCount;
+				}
+				else
+				{
+					row.Min = 0;
 				}
 
-				report.SummaryRows.Add(row);
+				for(var warsCounter = 0; warsCounter < wars?.Count; warsCounter++)
+				{//Т.к. данные запроса упорядочены, тут реализован доступ по индексам
+					if(addedCounter == inResult.Count || removedCounter == woResult.Count)
+					{
+						/*Кол-во данных в запросе может не совпадать с кол-вом выбранных номенклатур. Несмотря на то, что счетчики
+						 * увеличиваются лишь при совпадении, иногда кидается OutOfBounds, так что пришлось вставить break.
+						 * На итоговые данные это не влияет */
+						break;
+					}
+					var warId = (int) wars[warsCounter].Value;
+					var tempIn = inResult[addedCounter];
+					if(tempIn.WarehouseId == warId && tempIn.NomId == row.NomId)
+					{
+						row.Separate[warsCounter] = tempIn.Amount;
+						addedCounter++;
+					}
+
+					var tempWo = woResult[removedCounter];
+					if(tempWo.WarehouseId == warId && tempWo.NomId == row.NomId)
+					{
+						row.Separate[warsCounter] -= tempWo.Amount;
+						removedCounter++;
+					}
+				}
+
+				report.SummaryRows[nomsCounter] = row;
 			}
+			AlgoTime = (DateTime.Now - start).TotalSeconds;
+
 
 			return await new ValueTask<BalanceSummaryReport>(report);
 		}
@@ -254,6 +255,8 @@ namespace Vodovoz.ViewModels.ViewModels.Suppliers
 		{
 			ShowWarningMessage(message);
 		}
+
+		#region Настройка фильтров
 
 		private SelectableParameterReportFilterViewModel CreateNomsViewModel()
 		{
@@ -348,5 +351,7 @@ namespace Vodovoz.ViewModels.ViewModels.Suppliers
 
 			return new SelectableParameterReportFilterViewModel(_warsFilter);
 		}
+
+		#endregion
 	}
 }
