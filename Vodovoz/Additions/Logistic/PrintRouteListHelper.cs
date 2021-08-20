6c7c82b54a1da5c8003997236506dd6bb4c64891
Сергей <efimov90@gmail.com>
2021-08-20 15:54:12 +0300

    чистка кода

--- Vodovoz/Additions/Logistic/PrintRouteListHelper.cs
+++ Vodovoz/Additions/Logistic/PrintRouteListHelper.cs
@@ -21,17 +21,19 @@ namespace Vodovoz.Additions.Logistic
 {
 	public static class PrintRouteListHelper
 	{
-		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+		private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 		private static readonly IRouteColumnRepository _routeColumnRepository = new RouteColumnRepository();
 		private const string _orderCommentTagName = "OrderComment";
 		private const string _orderPrioritizedTagName = "prioritized";
 
 		public static ReportInfo GetRDLTimeList(int routeListId)
 		{
-			return new ReportInfo {
-				Title = String.Format("Лист времени для МЛ № {0}", routeListId),
+			return new ReportInfo
+			{
+				Title = $"Лист времени для МЛ № { routeListId }",
 				Identifier = "Documents.TimeList",
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "route_list_id", routeListId }
 				}
 			};
@@ -39,10 +41,12 @@ namespace Vodovoz.Additions.Logistic
 
 		public static ReportInfo GetRDLDailyList(int routeListId)
 		{
-			return new ReportInfo {
-				Title = String.Format("Ежедневные номера МЛ № {0}", routeListId),
+			return new ReportInfo
+			{
+				Title = $"Ежедневные номера МЛ № { routeListId }",
 				Identifier = "Logistic.AddressesByDailyNumber",
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "route_list", routeListId }
 				}
 			};
@@ -53,19 +57,22 @@ namespace Vodovoz.Additions.Logistic
 			var RouteColumns = _routeColumnRepository.ActiveColumns(uow);
 
 			if(RouteColumns.Count < 1)
+			{
 				MessageDialogHelper.RunErrorDialog("В справочниках не заполнены колонки маршрутного листа. Заполните данные и повторите попытку.");
+			}
 
 			string documentName = "RouteList";
 			bool isClosed = false;
 
 			if(routeList.PrintAsClosed())
 			{
-				documentName = "ClosedRouteList"; 
+				documentName = "ClosedRouteList";
 				isClosed = true;
 			}
 
-			string RdlText = String.Empty;
-			using(var rdr = new StreamReader(System.IO.Path.Combine(Environment.CurrentDirectory, "Reports/Logistic/" + documentName + ".rdl"))) {
+			string RdlText = string.Empty;
+			using(var rdr = new StreamReader(Path.Combine(Environment.CurrentDirectory, "Reports/Logistic/" + documentName + ".rdl")))
+			{
 				RdlText = rdr.ReadToEnd();
 			}
 			//Для уникальности номеров Textbox.
@@ -80,40 +87,43 @@ namespace Vodovoz.Additions.Logistic
 				"<BorderStyle><Default>Solid</Default></BorderStyle><BorderColor /><BorderWidth /><FontSize>8pt</FontSize>" +
 				"<TextAlign>Center</TextAlign><Format>{2}</Format><VerticalAlign>Middle</VerticalAlign>";
 
-			if(isClosed) {
+			if(isClosed)
+			{
 				numericCellTemplate += "<BackgroundColor>=Iif((Fields!Status.Value = \"EnRoute\") or (Fields!Status.Value = \"Completed\"), White, Lightgrey)</BackgroundColor>";
 			}
 			numericCellTemplate += "<PaddingTop>10pt</PaddingTop><PaddingBottom>10pt</PaddingBottom></Style>" +
-			                       "<CanGrow>true</CanGrow></Textbox></ReportItems></TableCell>";
+								   "<CanGrow>true</CanGrow></Textbox></ReportItems></TableCell>";
 
 			//Расширяем требуемые колонки на нужную ширину
-			RdlText = RdlText.Replace("<!--colspan-->", String.Format("<ColSpan>{0}</ColSpan>", RouteColumns.Count));
+			RdlText = RdlText.Replace("<!--colspan-->", $"<ColSpan>{ RouteColumns.Count }</ColSpan>");
 
 			//Расширяем таблицу
 			string columnsXml = "<TableColumn><Width>18pt</Width></TableColumn>";
-			string columns = String.Empty;
-			
+			string columns = string.Empty;
+
 			columns += "<TableColumn><Width>100pt</Width></TableColumn>"; // Первая колонка шире тк кк это коммент
-			for(int i = 1; i < RouteColumns.Count; i++) {
+			for(int i = 1; i < RouteColumns.Count; i++)
+			{
 				columns += columnsXml;
 			}
 			RdlText = RdlText.Replace("<!--table_column-->", columns);
 
 			//Создаем колонки, дополняем запрос и тд.
-			string CellColumnHeader = String.Empty;
-			string CellColumnValue = String.Empty;
-			string CellColumnStock = String.Empty;
-			string CellColumnTotal = String.Empty;
-			string SqlSelect = String.Empty;
-			string SqlSelectSubquery = String.Empty;
-			string Fields = String.Empty;
+			string CellColumnHeader = string.Empty;
+			string CellColumnValue = string.Empty;
+			string CellColumnStock = string.Empty;
+			string CellColumnTotal = string.Empty;
+			string SqlSelect = string.Empty;
+			string SqlSelectSubquery = string.Empty;
+			string Fields = string.Empty;
 			string TotalSum = "= 0";
 
 			bool isFirstColumn = true;
-			
-			foreach(var column in RouteColumns) {
+
+			foreach(var column in RouteColumns)
+			{
 				//Заголовки колонок
-				CellColumnHeader += String.Format(
+				CellColumnHeader += string.Format(
 					"<TableCell><ReportItems>" +
 					"<Textbox Name=\"Textbox{0}\">" +
 					"<Value>{1}</Value>" +
@@ -124,43 +134,55 @@ namespace Vodovoz.Additions.Logistic
 					TextBoxNumber++, column.Name);
 				//Формула для колонки с водой для информации из запроса
 				//'' + {{Water_fact{0}}} + '(' + ({{Water_fact{0}}} - {{Water{0}}}) + ')'
-				
-				if (isFirstColumn) {
-					CellColumnValue += String.Format(numericCellTemplate, TextBoxNumber++, $"=Iif({{{ _orderPrioritizedTagName }}}, \'Приоритет! \', \"\") + {{{ _orderCommentTagName }}}", "0");
+
+				if(isFirstColumn)
+				{
+					CellColumnValue += string.Format(numericCellTemplate, TextBoxNumber++, $"=Iif({{{ _orderPrioritizedTagName }}}, \'Приоритет! \', \"\") + {{{ _orderCommentTagName }}}", "0");
 				}
-				else {
-					if (isClosed)
-						CellColumnValue += String.Format(numericCellTemplate,
+				else
+				{
+					if(isClosed)
+						CellColumnValue += string.Format(numericCellTemplate,
 							TextBoxNumber++,
-							String.Format(
-								"=Iif({{Water{0}}} + {{Water_fact{0}}} = 0, \"\", Iif( {{Water{0}}} = {{Water_fact{0}}}, Format({{Water{0}}}, '0'), '' + {{Water_fact{0}}} + '(' + Iif({{Water_fact{0}}} - {{Water{0}}} > 0, '+', '') + ({{Water_fact{0}}} - {{Water{0}}}) + ')'))",
+							string.Format(
+								"=Iif({{Water{0}}} + {{Water_fact{0}}} = 0, \"\", Iif( {{Water{0}}} = {{Water_fact{0}}}, Format({{Water{0}}}, '0'), ''" +
+								" + {{Water_fact{0}}} + '(' + Iif({{Water_fact{0}}} - {{Water{0}}} > 0, '+', '') + ({{Water_fact{0}}} - {{Water{0}}}) + ')'))",
 								column.Id),
-							String.Format("=Iif({{Water{0}}} = {{Water_fact{0}}}, \"0\", \"\")", column.Id)
+							string.Format("=Iif({{Water{0}}} = {{Water_fact{0}}}, \"0\", \"\")", column.Id)
 						);
 					else
-						CellColumnValue += String.Format(numericCellTemplate,
-							TextBoxNumber++, String.Format("=Iif({{Water{0}}} = 0, \"\", {{Water{0}}})", column.Id),
+					{
+						CellColumnValue += string.Format(numericCellTemplate,
+							TextBoxNumber++, $"=Iif({{Water{ column.Id }}} = 0, \"\", {{Water{ column.Id }}})",
 							'C');
+					}
 				}
 				//Ячейка с запасом. Пока там пусто
-				CellColumnStock += String.Format(numericCellTemplate, TextBoxNumber++, "", 0);
+				CellColumnStock += string.Format(numericCellTemplate, TextBoxNumber++, "", 0);
 
 				//Ячейка с суммой по бутылям.
-				if (isFirstColumn) {
-					CellColumnTotal += String.Format(numericCellTemplate, TextBoxNumber++, "", 0);
+				if(isFirstColumn)
+				{
+					CellColumnTotal += string.Format(numericCellTemplate, TextBoxNumber++, "", 0);
 				}
-				else {
+				else
+				{
 					string formula;
-					if (isClosed)
-						formula = String.Format("=Iif(Sum({{Water_fact{0}}}) = 0, \"\", Sum({{Water_fact{0}}}))",
-							column.Id);
+					if(isClosed)
+					{
+						formula = $"=Iif(Sum({{Water_fact{ column.Id }}}) = 0, \"\", Sum({{Water_fact{ column.Id }}}))";
+					}
 					else
-						formula = String.Format("=Iif(Sum({{Water{0}}}) = 0, \"\", Sum({{Water{0}}}))", column.Id);
-					CellColumnTotal += String.Format(numericCellTemplate, TextBoxNumber++, formula, 0);
+					{
+						formula = $"=Iif(Sum({{Water{ column.Id }}}) = 0, \"\", Sum({{Water{ column.Id }}}))";
+					}
+
+					CellColumnTotal += string.Format(numericCellTemplate, TextBoxNumber++, formula, 0);
 				}
 
 				//Запрос..
-				if (isFirstColumn) {
+				if(isFirstColumn)
+				{
 					SqlSelect += $", orders.comment AS { _orderCommentTagName }" +
 						$", (SELECT EXISTS (" +
 						$"SELECT * FROM undelivered_orders uo" +
@@ -182,33 +204,35 @@ namespace Vodovoz.Additions.Logistic
 
 					isFirstColumn = false;
 				}
-				else {
-					SqlSelect += String.Format(", IFNULL(wt_qry.Water{0}, 0) AS Water{0}", column.Id.ToString());
-					SqlSelectSubquery += String.Format(
+				else
+				{
+					SqlSelect += string.Format(", IFNULL(wt_qry.Water{0}, 0) AS Water{0}", column.Id.ToString());
+					SqlSelectSubquery += string.Format(
 						", SUM(IF(nomenclature_route_column.id = {0}, cast(order_items.count as DECIMAL), 0)) AS {1}",
 						column.Id, "Water" + column.Id.ToString());
-					if (isClosed) {
+					if(isClosed)
+					{
 						SqlSelect +=
-							String.Format(
+							string.Format(
 								", IF(route_list_addresses.status = 'Transfered', 0, cast(IFNULL(wt_qry.Water_fact{0}, 0) as DECIMAL)) AS Water_fact{0}",
 								column.Id.ToString());
-						SqlSelectSubquery += String.Format(
+						SqlSelectSubquery += string.Format(
 							", SUM(IF(nomenclature_route_column.id = {0}, cast(IFNULL(order_items.actual_count, 0) as DECIMAL), 0)) AS {1}",
 							column.Id, "Water_fact" + column.Id.ToString());
 					}
 
 					//Линкуем запрос на переменные RDL
-					Fields += String.Format("" +
-					                        "<Field Name=\"{0}\">" +
-					                        "<DataField>{0}</DataField>" +
-					                        "<TypeName>System.Decimal</TypeName>" +
-					                        "</Field>", "Water" + column.Id.ToString());
-					if (isClosed) {
-						Fields += String.Format("" +
-						                        "<Field Name=\"{0}\">" +
-						                        "<DataField>{0}</DataField>" +
-						                        "<TypeName>System.Decimal</TypeName>" +
-						                        "</Field>", "Water_fact" + column.Id.ToString());
+					Fields += string.Format("" +
+											"<Field Name=\"{0}\">" +
+											"<DataField>{0}</DataField>" +
+											"<TypeName>System.Decimal</TypeName>" +
+											"</Field>", "Water" + column.Id.ToString());
+					if(isClosed)
+					{
+						Fields += string.Format("<Field Name=\"{0}\">" +
+												"<DataField>{0}</DataField>" +
+												"<TypeName>System.Decimal</TypeName>" +
+												"</Field>", "Water_fact" + column.Id.ToString());
 					}
 				}
 				//Формула итоговой суммы по всем бутылям.
@@ -216,11 +240,11 @@ namespace Vodovoz.Additions.Logistic
 				{
 					if(isClosed)
 					{
-						TotalSum += $"+ Sum(Iif(Fields!Status.Value = \"Completed\", {{Water_fact{column.Id}}}, 0))";
+						TotalSum += $"+ Sum(Iif(Fields!Status.Value = \"Completed\", {{Water_fact{ column.Id }}}, 0))";
 					}
 					else
 					{
-						TotalSum += $"+ Sum({{Water{column.Id}}})";
+						TotalSum += $"+ Sum({{Water{ column.Id }}})";
 					}
 				}
 			}
@@ -233,21 +257,24 @@ namespace Vodovoz.Additions.Logistic
 			RdlText = RdlText.Replace("<!--fields-->", Fields);
 			RdlText = RdlText.Replace("<!--table_cell_total_without_stock-->", TotalSum);
 
-			var TempFile = System.IO.Path.GetTempFileName();
-			using(StreamWriter sw = new StreamWriter(TempFile)) {
+			var TempFile = Path.GetTempFileName();
+			using(StreamWriter sw = new StreamWriter(TempFile))
+			{
 				sw.Write(RdlText);
 			}
 #if DEBUG
 			Console.WriteLine(RdlText);
 #endif
 
-			string printDatestr = string.Format("Дата печати: {0}", DateTime.Now.ToString("g"));
+			string printDatestr = $"Дата печати: { DateTime.Now.ToString("g") }";
 			var needTerminal = routeList.Addresses.Any(x => x.Order.PaymentType == PaymentType.Terminal);
-			
-			return new ReportInfo {
-				Title = String.Format("Маршрутный лист № {0}", routeList.Id),
+
+			return new ReportInfo
+			{
+				Title = $"Маршрутный лист № { routeList.Id }",
 				Path = TempFile,
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "RouteListId", routeList.Id },
 					{ "Print_date", printDatestr},
 					{ "RouteListDate", routeList.Date},
@@ -263,19 +290,23 @@ namespace Vodovoz.Additions.Logistic
 			XmlDocument rdlText = new XmlDocument();
 			XmlNamespaceManager namespaces = new XmlNamespaceManager(rdlText.NameTable);
 			namespaces.AddNamespace("r", "http://schemas.microsoft.com/sqlserver/reporting/2005/01/reportdefinition");
-			rdlText.Load(System.IO.Path.Combine(Environment.CurrentDirectory, "Reports/Logistic/" + documentName + ".rdl"));
+			rdlText.Load(Path.Combine(Environment.CurrentDirectory, "Reports/Logistic/" + documentName + ".rdl"));
 			var imageData = rdlText.DocumentElement.SelectSingleNode("/r:Report/r:EmbeddedImages/r:EmbeddedImage[@Name=\"map\"]/r:ImageData", namespaces);
 
-			var map = new GMapControl();
-			map.MapProvider = GMapProviders.OpenCycleMap;
-			map.MaxZoom = 18;
-			map.RoutesEnabled = true;
-			map.MarkersEnabled = true;
+			var map = new GMapControl
+			{
+				MapProvider = GMapProviders.OpenCycleMap,
+				MaxZoom = 18,
+				RoutesEnabled = true,
+				MarkersEnabled = true
+			};
 
 			GMapOverlay routeOverlay = new GMapOverlay("route");
-			using(var calc = new RouteGeometryCalculator(DistanceProvider.Osrm)) {
+			using(var calc = new RouteGeometryCalculator(DistanceProvider.Osrm))
+			{
 				MapDrawingHelper.DrawRoute(routeOverlay, routeList, calc);
 			}
+
 			GMapOverlay addressesOverlay = new GMapOverlay("addresses");
 			MapDrawingHelper.DrawAddressesOfRoute(addressesOverlay, routeList);
 			map.Overlays.Add(routeOverlay);
@@ -283,8 +314,10 @@ namespace Vodovoz.Additions.Logistic
 			map.SetFakeAllocationSize(new Gdk.Rectangle(0, 0, 900, 900));
 			map.ZoomAndCenterRoutes("route");
 			byte[] img;
-			using(var bitmap = map.ToBitmap((int count) => logger.Info("Загружаем плитки карты(осталось {0})...", count), !batchPrint)) {
-				using(MemoryStream stream = new MemoryStream()) {
+			using(var bitmap = map.ToBitmap((int count) => _logger.Info("Загружаем плитки карты(осталось {0})...", count), !batchPrint))
+			{
+				using(MemoryStream stream = new MemoryStream())
+				{
 					bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
 					img = stream.ToArray();
 				}
@@ -293,10 +326,12 @@ namespace Vodovoz.Additions.Logistic
 			string base64image = Convert.ToBase64String(img);
 			imageData.InnerText = base64image;
 
-			return new ReportInfo {
-				Title = String.Format("Карта маршрута № {0}", routeList.Id),
+			return new ReportInfo
+			{
+				Title = $"Карта маршрута № { routeList.Id }",
 				Source = rdlText.InnerXml,
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "route_id", routeList.Id }
 				}
 			};
@@ -304,10 +339,12 @@ namespace Vodovoz.Additions.Logistic
 
 		public static ReportInfo GetRDLLoadDocument(int routeListId)
 		{
-			return new ReportInfo {
-				Title = String.Format("Документ погрузки для МЛ № {0}", routeListId),
+			return new ReportInfo
+			{
+				Title = $"Документ погрузки для МЛ № { routeListId }",
 				Identifier = "RouteList.CarLoadDocument",
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "route_list_id", routeListId },
 				},
 				UseUserVariables = true
@@ -316,10 +353,12 @@ namespace Vodovoz.Additions.Logistic
 
 		public static ReportInfo GetRDLLoadSofiyskaya(int routeListId)
 		{
-			return new ReportInfo {
-				Title = String.Format("Погрузка Софийская для МЛ № {0}", routeListId),
+			return new ReportInfo
+			{
+				Title = $"Погрузка Софийская для МЛ № { routeListId }",
 				Identifier = "RouteList.CarLoadDocSofiyskaya",
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "id", routeListId },
 				},
 				UseUserVariables = true
@@ -329,10 +368,12 @@ namespace Vodovoz.Additions.Logistic
 		public static ReportInfo GetRDLFine(RouteList routeList)
 		{
 
-			return new ReportInfo {
-				Title = String.Format("Штрафы сотрудника {0}", routeList.Driver.LastName),
+			return new ReportInfo
+			{
+				Title = $"Штрафы сотрудника { routeList.Driver.LastName }",
 				Identifier = "Employees.Fines",
-				Parameters = new Dictionary<string, object> {
+				Parameters = new Dictionary<string, object>
+				{
 					{ "drivers", routeList.Driver.Id },
 					{ "startDate", routeList.Date },
 					{ "endDate", routeList.Date },
@@ -345,7 +386,8 @@ namespace Vodovoz.Additions.Logistic
 
 		public static ReportInfo GetRDL(RouteList routeList, RouteListPrintableDocuments type, IUnitOfWork uow = null, bool batchPrint = false)
 		{
-			switch(type) {
+			switch(type)
+			{
 				case RouteListPrintableDocuments.LoadDocument:
 					return GetRDLLoadDocument(routeList.Id);
 				case RouteListPrintableDocuments.LoadSofiyskaya:
@@ -389,25 +431,39 @@ namespace Vodovoz.Additions.Logistic
 
 	public class RouteListPrintableDocs : IPrintableRDLDocument
 	{
+		IUnitOfWork _uow;
+		public RouteList routeList;
+		RouteListPrintableDocuments _type;
+
 		public RouteListPrintableDocs(IUnitOfWork uow, RouteList routeList, RouteListPrintableDocuments type)
 		{
-			this.uow = uow;
+			_uow = uow;
 			this.routeList = routeList;
-			this.type = type;
+			_type = type;
 			CopiesToPrint = DefaultCopies;
 		}
 
 		#region IPrintableRDLDocument implementation 
-		public ReportInfo GetReportInfo() => PrintRouteListHelper.GetRDL(routeList, type, uow, IsBatchPrint);
+		public ReportInfo GetReportInfo() => PrintRouteListHelper.GetRDL(routeList, _type, _uow, IsBatchPrint);
 		public Dictionary<object, object> Parameters { get; set; } = new Dictionary<object, object>();
 		#endregion
 
 		#region IPrintableDocument implementation 
 		public PrinterType PrintType => PrinterType.RDL;
 
-		public DocumentOrientation Orientation {
-			get {
-				switch(type) {
+		public string Name => _type.GetEnumTitle();
+
+		public int CopiesToPrint { get; set; }
+		#endregion
+		
+		bool IsBatchPrint => Parameters.ContainsKey("IsBatchPrint") && (bool)Parameters["IsBatchPrint"];
+		
+		public DocumentOrientation Orientation
+		{
+			get
+			{
+				switch(_type)
+				{
 					case RouteListPrintableDocuments.RouteList:
 					case RouteListPrintableDocuments.OrderOfAddresses:
 						return DocumentOrientation.Landscape;
@@ -417,9 +473,12 @@ namespace Vodovoz.Additions.Logistic
 			}
 		}
 
-		int DefaultCopies {
-			get {
-				switch(type) {
+		int DefaultCopies
+		{
+			get
+			{
+				switch(_type)
+				{
 					case RouteListPrintableDocuments.RouteList:
 					case RouteListPrintableDocuments.RouteMap:
 					case RouteListPrintableDocuments.DailyList:
@@ -433,16 +492,5 @@ namespace Vodovoz.Additions.Logistic
 				}
 			}
 		}
-
-		public string Name => type.GetEnumTitle();
-
-		public int CopiesToPrint { get; set; }
-		#endregion
-
-		IUnitOfWork uow;
-		public RouteList routeList;
-		RouteListPrintableDocuments type;
-		bool IsBatchPrint => Parameters.ContainsKey("IsBatchPrint") && (bool)Parameters["IsBatchPrint"];
 	}
 }
-
