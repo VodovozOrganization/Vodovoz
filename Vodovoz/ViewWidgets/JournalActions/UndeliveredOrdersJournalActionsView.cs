9f3228499fa8354fc61dfe1faf28cf808af254b2
Andrey Shaklein <as1854@yandex.ru>
2021-08-25 09:46:34 +0300

    2538 Создал вьюху для экшенов журнала недовозов

---
+++ Vodovoz/ViewWidgets/JournalActions/UndeliveredOrdersJournalActionsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.JournalActions
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class UndeliveredOrdersJournalActionsView : Gtk.Bin
+	{
+		public UndeliveredOrdersJournalActionsView()
+		{
+			this.Build();
+		}
+	}
+}
