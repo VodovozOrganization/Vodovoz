2c49c398c9b2ffee31700c9216a94d38ba7d24af
MikhailFedosov <dobriymisha@gmail.com>
2021-08-31 22:57:25 +0300

    UI base widget for analytics for undelivery report

---
+++ Vodovoz/ReportsParameters/Logistic/AnalyticsForUndeliveryReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QS.Dialog.GtkUI;
+using QSReport;
+
+namespace Vodovoz.ReportsParameters.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AnalyticsForUndeliveryReport : SingleUoWWidgetBase, IParametersWidget
+	{
+		public AnalyticsForUndeliveryReport()
+		{
+			this.Build();
+		}
+
+		public string Title => throw new NotImplementedException();
+
+		public event EventHandler<LoadReportEventArgs> LoadReport;
+	}
+}
