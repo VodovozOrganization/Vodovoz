bb79583850dcf1dade22335f2a4dfb70a0057895
kozlov-d <sprunk97@gmail.com>
2021-09-03 15:23:37 +0700

    Автоформат. Переименование

--- Vodovoz/Commons/FileChooser.cs
+++ Vodovoz/Commons/FileChooser.cs
@@ -3,66 +3,69 @@ using VodovozInfrastructure.Interfaces;
 
 namespace Vodovoz
 {
-    public class FileChooser: Gtk.FileChooserDialog, IFileChooserProvider
-    {
-        private FileChooserDialog _fileChooser;
-        private readonly string _fileName;
-        public FileChooser(string fileName = null)
-        {
-	        _fileName = fileName ?? string.Empty;
-        }
-        
-        public string GetExportFilePath(string fileName = null)
-        {
-            //Создается здесь а не в конструкторе, потому что единственный способ
-            //закрыть это destroy
-            _fileChooser =
-                new Gtk.FileChooserDialog("Выберите где сохранить файл",
-                    this,
-                    FileChooserAction.Save,
-                    "Отмена", ResponseType.Cancel,
-                    "Сохранить", ResponseType.Accept);
-            _fileChooser.CurrentName = string.IsNullOrWhiteSpace(fileName) ? _fileName : fileName;
-            
-            var result = _fileChooser.Run();
-            if (result == (int)ResponseType.Accept)
-                return _fileChooser.Filename;
-            else
-            {
-                CloseWindow();
-                return "";
-            }
-        }
+	public class FileChooser : Gtk.FileChooserDialog, IFileChooserProvider
+	{
+		private FileChooserDialog _fileChooser;
+		private string _predefinedFileName;
 
-        public string GetExportFolderPath()
-        {
-            //Создается здесь а не в конструкторе, потому что единственный способ
-            //закрыть это destroy
-            _fileChooser =
-                new Gtk.FileChooserDialog("Выберите где сохранить файл",
-                    this,
-                    FileChooserAction.SelectFolder,
-                    "Отмена", ResponseType.Cancel,
-                    "Сохранить", ResponseType.Accept);
-            _fileChooser.CurrentName = string.Empty;
+		public FileChooser(string fileName = null)
+		{
+			_predefinedFileName = fileName ?? string.Empty;
+		}
 
-            var result = _fileChooser.Run();
-            if (result == (int)ResponseType.Accept)
-            {
-                var path = _fileChooser.Filename;
-                CloseWindow();
-                return path;
-            }
-            else
-            {
-                CloseWindow();
-                return "";
-            }
-        }
+		public string GetExportFilePath(string fileName = null)
+		{
+			//Создается здесь а не в конструкторе, потому что единственный способ
+			//закрыть это destroy
+			_fileChooser =
+				new Gtk.FileChooserDialog("Выберите где сохранить файл",
+					this,
+					FileChooserAction.Save,
+					"Отмена", ResponseType.Cancel,
+					"Сохранить", ResponseType.Accept);
+			_fileChooser.CurrentName = string.IsNullOrWhiteSpace(fileName) ? _predefinedFileName : fileName;
 
-        public void CloseWindow()
-        {
-            _fileChooser.Destroy();
-        }
-    }
+			var result = _fileChooser.Run();
+			if(result == (int)ResponseType.Accept)
+			{
+				return _fileChooser.Filename;
+			}
+			else
+			{
+				CloseWindow();
+				return "";
+			}
+		}
+
+		public string GetExportFolderPath()
+		{
+			//Создается здесь а не в конструкторе, потому что единственный способ
+			//закрыть это destroy
+			_fileChooser =
+				new Gtk.FileChooserDialog("Выберите где сохранить файл",
+					this,
+					FileChooserAction.SelectFolder,
+					"Отмена", ResponseType.Cancel,
+					"Сохранить", ResponseType.Accept);
+			_fileChooser.CurrentName = string.Empty;
+
+			var result = _fileChooser.Run();
+			if(result == (int)ResponseType.Accept)
+			{
+				var path = _fileChooser.Filename;
+				CloseWindow();
+				return path;
+			}
+			else
+			{
+				CloseWindow();
+				return "";
+			}
+		}
+
+		public void CloseWindow()
+		{
+			_fileChooser.Destroy();
+		}
+	}
 }
