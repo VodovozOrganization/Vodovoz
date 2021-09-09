76073f3264eca44f36512cef28ca3a1fffd15932
Posox <Iamposox@yandex.ru>
2021-09-09 13:45:43 +0300

    Add HistoryTrace

--- VodovozBusiness/Domain/Permissions/Warehouses/SubdivisionWarehousePermission.cs
+++ VodovozBusiness/Domain/Permissions/Warehouses/SubdivisionWarehousePermission.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
+using QS.HistoryLog;
 
 namespace Vodovoz.Domain.Permissions.Warehouses
 {
     [EntityPermission]
+    [HistoryTrace]
     public class SubdivisionWarehousePermission: WarehousePermissionBase
     {
         public override PermissionType PermissionType => PermissionType.Subdivision;
--- VodovozBusiness/Domain/Permissions/Warehouses/UserWarehousePermission.cs
+++ VodovozBusiness/Domain/Permissions/Warehouses/UserWarehousePermission.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
+using QS.HistoryLog;
 using Vodovoz.Domain.Employees;
 
 namespace Vodovoz.Domain.Permissions.Warehouses
 {
     [EntityPermission]
+    [HistoryTrace]
     public class UserWarehousePermission : WarehousePermissionBase
     {
         public override PermissionType PermissionType => PermissionType.User;
