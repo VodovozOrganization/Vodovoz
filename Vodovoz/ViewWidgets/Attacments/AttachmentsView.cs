ded8ff7f0a3c770f59c9c90537b7cc63ef84a62c
Andrey Shaklein <as1854@yandex.ru>
2021-09-09 15:29:40 +0300

    2809 Создал вьюху AttachmentsView

---
+++ Vodovoz/ViewWidgets/Attacments/AttachmentsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Attacments
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AttachmentsView : Gtk.Bin
+	{
+		public AttachmentsView()
+		{
+			this.Build();
+		}
+	}
+}
