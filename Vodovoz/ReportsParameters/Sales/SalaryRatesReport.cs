6495cc0262e01e1f9b10828b51eb991e6a905812
Posox <Iamposox@yandex.ru>
2021-09-10 16:21:38 +0300

    FillItems

--- Vodovoz/ReportsParameters/Sales/SalaryRatesReport.cs
+++ Vodovoz/ReportsParameters/Sales/SalaryRatesReport.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gamma.ColumnConfig;
+using Gamma.Utilities;
 using QS.Dialog.GtkUI;
 using QSReport;
+using Vodovoz.Domain.WageCalculation;
 
 namespace Vodovoz.ReportsParameters.Sales
 {
@@ -9,11 +14,35 @@ namespace Vodovoz.ReportsParameters.Sales
 	{
 		public SalaryRatesReport()
 		{
-			this.Build();
+			Build();
+			var arrayOfWageRate = Enum.GetValues(typeof(WageRateTypes));
+			var salaryRateFilterNode = new List<SalaryRateFilterNode>();
+			
+			foreach (var rate in arrayOfWageRate)
+				salaryRateFilterNode.Add(new SalaryRateFilterNode((WageRateTypes)rate));
+			
+			treeViewSalaryProperties.ColumnsConfig = FluentColumnsConfig<SalaryRateFilterNode>.Create()
+				.AddColumn("Название").AddTextRenderer(x => x.Title)
+				.AddColumn("").AddToggleRenderer(x => x.Selected)
+				.Finish();
+			
+			treeViewSalaryProperties.ItemsDataSource = salaryRateFilterNode;
 		}
 
 		public string Title => "Ставки для водителей";
 
 		public event EventHandler<LoadReportEventArgs> LoadReport;
+	}
+
+	public class SalaryRateFilterNode
+	{
+		public SalaryRateFilterNode(WageRateTypes wageRateTypes)
+		{
+			Title = wageRateTypes.GetEnumTitle();
+			Selected = false;
+		}
+		public string Title { get; set; }
+		
+		public bool Selected { get; set; }
 	}
 }
