ee760f57439796ae35b5105fc9efc49b95126a42
kozlov-d <sprunk97@gmail.com>
2021-09-13 19:09:33 +0700

    Добавиль фильтр по частям города, добавил текстбокс с выбранными фильтрами

--- Vodovoz/ReportsParameters/OrderStatisticByWeekReport.cs
+++ Vodovoz/ReportsParameters/OrderStatisticByWeekReport.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Bindings.Collections.Generic;
+using System.Linq;
+using Gamma.ColumnConfig;
+using MoreLinq;
+using QS.Dialog.GtkUI;
+using QS.DomainModel.UoW;
 using QS.Report;
 using QSReport;
+using Vodovoz.Domain.Sale;
+using Vodovoz.ViewModels.Logistic;
 
 namespace Vodovoz.ReportsParameters
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class OrderStatisticByWeekReport : Gtk.Bin, IParametersWidget
+	public partial class OrderStatisticByWeekReport : SingleUoWWidgetBase, IParametersWidget
 	{
+		private readonly GenericObservableList<GeographicGroupNode> _geographicGroupNodes;
+
         public OrderStatisticByWeekReport()
 		{
 			this.Build();
+			UoW = UnitOfWorkFactory.CreateWithoutRoot();
+
 			dateperiodpicker.StartDate = new DateTime(DateTime.Today.Year, 1, 1);
 			dateperiodpicker.EndDate = DateTime.Today;
 
@@ -20,6 +32,17 @@ namespace Vodovoz.ReportsParameters
             }.ForEach(comboboxReportMode.AppendText);
 
             comboboxReportMode.Active = 0;
+
+			_geographicGroupNodes = new GenericObservableList<GeographicGroupNode>(
+				UoW.GetAll<GeographicGroup>().Select(gg => new GeographicGroupNode(gg)).ToList());
+			ytreeviewGeographicGroup.ColumnsConfig = FluentColumnsConfig<GeographicGroupNode>
+				.Create()
+					.AddColumn("Выбрать").AddToggleRenderer(ggn => ggn.Selected).Editing()
+					.AddColumn("Район города").AddTextRenderer(ggn => ggn.ToString())
+				.Finish();
+
+			ytreeviewGeographicGroup.ItemsDataSource = _geographicGroupNodes;
+			ytreeviewGeographicGroup.HeadersVisible = false;
         }
 
 		#region IParametersWidget implementation
@@ -39,6 +62,7 @@ namespace Vodovoz.ReportsParameters
 
 		private ReportInfo GetReportInfo()
 		{
+			var selectedGeoGroupsIds = _geographicGroupNodes.Where(ggn => ggn.Selected).Select(ggn => ggn.GeographicGroup.Id).ToArray();
 			return new ReportInfo {
 				Identifier = "Logistic.OrderStatisticByWeek",
                 Parameters = new Dictionary<string, object>
@@ -46,8 +70,28 @@ namespace Vodovoz.ReportsParameters
 					{ "start_date", dateperiodpicker.StartDate },
 					{ "end_date", dateperiodpicker.EndDate.AddDays(1).AddTicks(-1) },
                     { "report_mode", comboboxReportMode.Active },
+					{ "geographic_group_id", selectedGeoGroupsIds },
+					{ "selected_filters", GetSelectedFilters() }
 				}
 			};
+		}
+
+		private string GetSelectedFilters()
+		{
+			var result = "Фильтры: части города -";
+			_geographicGroupNodes.Where(ggn => ggn.Selected).Select(ggn => ggn.ToString()).ForEach(ggName => result += $" {ggName},");
+			result += " тип значений -";
+			switch(comboboxReportMode.Active)
+			{
+				case 0:
+					result += " план.";
+					break;
+				case 1:
+					result += " факт.";
+					break;
+			}
+
+			return result;
 		}
 
 		protected void OnDateperiodpickerPeriodChanged(object sender, EventArgs e) => 
