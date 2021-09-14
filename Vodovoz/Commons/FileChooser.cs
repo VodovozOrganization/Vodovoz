ab393ce8632da584b0acd40a5b80cbc7084e2bb0
Andrey Shaklein <as1854@yandex.ru>
2021-09-14 10:05:58 +0300

    Spaces to Tabs

--- Vodovoz/Commons/FileChooser.cs
+++ Vodovoz/Commons/FileChooser.cs
@@ -3,15 +3,15 @@ using VodovozInfrastructure.Interfaces;
 
 namespace Vodovoz
 {
-    public class FileChooser: FileChooserDialog, IFileChooserProvider
-    {
-        private FileChooserDialog fileChooser;
+	public class FileChooser: FileChooserDialog, IFileChooserProvider
+	{
+		private FileChooserDialog fileChooser;
 		private string _predefinedFileName;
 
 		public FileChooser(string fileName = null)
-        {
+		{
 			_predefinedFileName = fileName;
-        }
+		}
 		
 		private void CreateNewFileChooser(string title, FileChooserAction fileChooserAction, params object[] buttonData)
 		{
@@ -20,83 +20,83 @@ namespace Vodovoz
 				DoOverwriteConfirmation = true;
 			}
 		}
-        
-        public string GetExportFilePath(string fileName = null)
-        {
-            //Создается здесь а не в конструкторе, потому что единственный способ
-            //закрыть это destroy
-            object[] buttonData = {
-	            "Отмена",
-	            ResponseType.Cancel,
-	            "Сохранить",
-	            ResponseType.Accept
-            };
-            
-            CreateNewFileChooser("Выберите где сохранить файл", FileChooserAction.Save, buttonData);
+		
+		public string GetExportFilePath(string fileName = null)
+		{
+			//Создается здесь а не в конструкторе, потому что единственный способ
+			//закрыть это destroy
+			object[] buttonData = {
+				"Отмена",
+				ResponseType.Cancel,
+				"Сохранить",
+				ResponseType.Accept
+			};
+			
+			CreateNewFileChooser("Выберите где сохранить файл", FileChooserAction.Save, buttonData);
 
-            fileChooser.CurrentName = string.IsNullOrWhiteSpace(fileName) ? _predefinedFileName : fileName;
-            
-            var result = fileChooser.Run();
-           
+			fileChooser.CurrentName = string.IsNullOrWhiteSpace(fileName) ? _predefinedFileName : fileName;
+			
+			var result = fileChooser.Run();
+		   
 			if(result == (int)ResponseType.Accept)
 			{
 				return fileChooser.Filename;
 			}
 			
 			CloseWindow();
-	        return "";
-        }
-        
-        public string GetAttachedFileName()
-        {
-	        object[] buttonData = {
-		        "Отмена",
-		        ResponseType.Cancel,
-		        "Прикрепить",
-		        ResponseType.Accept
-	        };
-	        
-	        CreateNewFileChooser("Выберите файл для прикрепления...", FileChooserAction.Open, buttonData);
+			return "";
+		}
+		
+		public string GetAttachedFileName()
+		{
+			object[] buttonData = {
+				"Отмена",
+				ResponseType.Cancel,
+				"Прикрепить",
+				ResponseType.Accept
+			};
+			
+			CreateNewFileChooser("Выберите файл для прикрепления...", FileChooserAction.Open, buttonData);
 
-	        if(fileChooser.Run() == (int)ResponseType.Accept)
-	        {
+			if(fileChooser.Run() == (int)ResponseType.Accept)
+			{
 				HideWindow();
 				return fileChooser.Filename;
-	        }
-	        
-	        CloseWindow();
-	        return "";
-        }
+			}
+			
+			CloseWindow();
+			return "";
+		}
 
-        public string GetExportFolderPath()
-        {
-            //Создается здесь а не в конструкторе, потому что единственный способ
-            //закрыть это destroy
-            object[] buttonData = {
-	            "Отмена",
-	            ResponseType.Cancel,
-	            "Сохранить",
-	            ResponseType.Accept
-            };
-	        
-            CreateNewFileChooser("Выберите где сохранить файл", FileChooserAction.SelectFolder, buttonData);
-            
-            fileChooser.CurrentName = _predefinedFileName;
+		public string GetExportFolderPath()
+		{
+			//Создается здесь а не в конструкторе, потому что единственный способ
+			//закрыть это destroy
+			object[] buttonData = {
+				"Отмена",
+				ResponseType.Cancel,
+				"Сохранить",
+				ResponseType.Accept
+			};
+			
+			CreateNewFileChooser("Выберите где сохранить файл", FileChooserAction.SelectFolder, buttonData);
+			
+			fileChooser.CurrentName = _predefinedFileName;
 
-            var result = fileChooser.Run();
-            if(result == (int)ResponseType.Accept)
-            {
-                var path = fileChooser.Filename;
-                CloseWindow();
-                return path;
-            }
-            
-            CloseWindow();
-	        return "";
-        }
+			var result = fileChooser.Run();
+			if(result == (int)ResponseType.Accept)
+			{
+				var path = fileChooser.Filename;
+				CloseWindow();
+				return path;
+			}
+			
+			CloseWindow();
+			return "";
+		}
 
-        public void CloseWindow() => fileChooser.Destroy();
+		public void CloseWindow() => fileChooser.Destroy();
 
-        public void HideWindow() => fileChooser.Hide();
-    }
+		public void HideWindow() => fileChooser.Hide();
+	}
 }
