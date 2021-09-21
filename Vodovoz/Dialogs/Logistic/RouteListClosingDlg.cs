700de60622265c0153dc7e1bb1f32de3d4d84d39
Сергей <efimov90@gmail.com>
2021-09-21 16:32:27 +0300

    Добавил сообщение об отсутствии куда распределять наличку

--- Vodovoz/Dialogs/Logistic/RouteListClosingDlg.cs
+++ Vodovoz/Dialogs/Logistic/RouteListClosingDlg.cs
@@ -1141,7 +1141,14 @@ namespace Vodovoz
 			Expense cashExpense = null;
 
 			var inputCashOrder = (decimal)spinCashOrder.Value;
-			messages.AddRange(Entity.ManualCashOperations(ref cashIncome, ref cashExpense, inputCashOrder, _categoryRepository));
+			try
+			{
+				messages.AddRange(Entity.ManualCashOperations(ref cashIncome, ref cashExpense, inputCashOrder, _categoryRepository));
+			}
+			catch(MissingOrdersWithCashlessPaymentTypeException)
+			{
+				MessageDialogHelper.RunInfoDialog($"В МЛ отсутствуют заказы с типом оплыта 'Наличная'");
+			}
 
 			if (cashIncome != null) UoW.Save(cashIncome);
 			if (cashExpense != null) UoW.Save(cashExpense);
---
+++ VodovozBusiness/Domain/Cash/MissingOrdersWithCashlessPaymentTypeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Vodovoz.Domain.Cash
+{
+	[Serializable]
+	public class MissingOrdersWithCashlessPaymentTypeException : Exception
+	{
+		public MissingOrdersWithCashlessPaymentTypeException()
+		{
+		}
+
+		public MissingOrdersWithCashlessPaymentTypeException(string message) : base(message)
+		{
+		}
+
+		public MissingOrdersWithCashlessPaymentTypeException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected MissingOrdersWithCashlessPaymentTypeException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
