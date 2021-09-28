2b1e6a9c418eb8a8da8733e12643ed534805d886
kozlov-d <sprunk97@gmail.com>
2021-09-28 19:26:39 +0700

    Добавил отправку СМС на второй диалог недовоза

--- Vodovoz/Dialogs/UndeliveryOnOrderCloseDlg.cs
+++ Vodovoz/Dialogs/UndeliveryOnOrderCloseDlg.cs
@@ -3,10 +3,14 @@ using System.Linq;
 using Gtk;
 using QS.DomainModel.UoW;
 using QS.Validation;
+using Vodovoz.Core.DataService;
 using Vodovoz.Domain.Orders;
+using Vodovoz.Domain.Sms;
 using Vodovoz.EntityRepositories.Employees;
 using Vodovoz.EntityRepositories.Orders;
 using Vodovoz.EntityRepositories.Undeliveries;
+using Vodovoz.Parameters;
+using Vodovoz.Services;
 
 namespace Vodovoz.Dialogs
 {
@@ -15,6 +19,7 @@ namespace Vodovoz.Dialogs
 		private readonly IEmployeeRepository _employeeRepository = new EmployeeRepository();
 		private readonly IUndeliveredOrdersRepository _undeliveredOrdersRepository = new UndeliveredOrdersRepository();
 		private readonly IOrderRepository _orderRepository = new OrderRepository();
+		private readonly ISmsNotifierParametersProvider _baseParametersProvider = new BaseParametersProvider(new ParametersProvider());
 
 		UndeliveredOrder undelivery;
 		Order order;
@@ -50,20 +55,37 @@ namespace Vodovoz.Dialogs
 
 		protected void OnButtonSaveClicked(object sender, EventArgs e)
 		{
+			if(Save() && undelivery.NewOrder != null
+			          && undelivery.OrderTransferType == TransferType.AutoTransferNotApproved
+			          && undelivery.NewOrder.OrderStatus != OrderStatus.Canceled)
+			{
+				ProcessSmsNotification();
+			}
+			DlgSaved?.Invoke(this, new UndeliveryOnOrderCloseEventArgs(undelivery));
+		}
+
+		private bool Save()
+		{
 			var valid = new QSValidator<UndeliveredOrder>(undelivery);
 			if(valid.RunDlgIfNotValid((Window)this.Toplevel))
-				return;
+			{
+				return false;
+			}
+
 			undeliveryView.BeforeSaving();
 			if(!CanCreateUndelivery()) {
-				this.OnCloseTab(false);
-				if(DlgSaved != null)
-					DlgSaved(this, new UndeliveryOnOrderCloseEventArgs(undelivery));
-				return;
+				OnCloseTab(false);
+				return false;
 			}
 			UoW.Save(undelivery);
 			OnCloseTab(false);
-			if(DlgSaved != null)
-				DlgSaved(this, new UndeliveryOnOrderCloseEventArgs(undelivery));
+			return true;
+		}
+
+		private void ProcessSmsNotification()
+		{
+			var smsNotifier = new SmsNotifier(_baseParametersProvider);
+			smsNotifier.NotifyUndeliveryAutoTransferNotApproved(undelivery);
 		}
 
 		/// <summary>
