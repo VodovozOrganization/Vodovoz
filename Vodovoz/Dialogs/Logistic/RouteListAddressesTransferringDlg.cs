0ceeb6fe7bf426b7da772300f8fd0bc6516fb2cf
Andrey Shaklein <as1854@yandex.ru>
2021-09-28 09:11:15 +0300

    3795 Исправил ошибку с некорректным возвратом адреса при переносе в исходный МЛ - сделал колонку Нужна загрузка нередактируемой в правой панели; - удалил неиспользуемый параметр из метода FirstFillClosing;

--- Vodovoz/Dialogs/Logistic/RouteListAddressesTransferringDlg.cs
+++ Vodovoz/Dialogs/Logistic/RouteListAddressesTransferringDlg.cs
@@ -203,7 +203,7 @@ namespace Vodovoz
 			if(isRightPanel)
 			{
 				config.AddColumn("Нужна загрузка").AddToggleRenderer(node => node.NeedToReload)
-					  .AddSetter((c, n) => c.Sensitive = n.WasTransfered);
+					.Editing(false);
 			}
 			else
 			{
@@ -411,7 +411,7 @@ namespace Vodovoz
 
 				if(routeListTo.ClosingFilled)
 				{
-					newItem.FirstFillClosing(UoW, _wageParameterService);
+					newItem.FirstFillClosing(_wageParameterService);
 				}
 
 				UoW.Save(item);
@@ -491,29 +491,16 @@ namespace Vodovoz
 
 				if(pastPlace != null)
 				{
-					previousRouteList.SetAddressStatusWithoutOrderChange(pastPlace.Id, address.Status);
-					pastPlace.DriverBottlesReturned = address.DriverBottlesReturned;
-					previousRouteList.TransferAddressTo(pastPlace.Id, null);
-
-					if(pastPlace.RouteList.ClosingFilled)
-					{
-						pastPlace.FirstFillClosing(UoW, _wageParameterService);
-					}
-
+					previousRouteList.RevertTransferAddress(_wageParameterService, pastPlace, address);
 					UpdateTranferDocuments(pastPlace.RouteList, address.RouteList);
+					pastPlace.RecalculateTotalCash();
 					UoW.Save(pastPlace);
-					UoW.Save(previousRouteList);
 				}
 
 				address.RouteList.ObservableAddresses.Remove(address);
 				UoW.Save(address.RouteList);
 			}
-
-			foreach (var routeListItem in toRevert)
-			{
-				routeListItem.RecalculateTotalCash();
-			}
-
+			
 			UoW.Commit();
 			UpdateNodes();
 		}
@@ -688,20 +675,8 @@ namespace Vodovoz
 		public string Address => RouteListItem.Order.DeliveryPoint?.ShortAddress ?? "Нет адреса";
 		public RouteListItemStatus Status => RouteListItem.Status;
 
-		public bool NeedToReload
-		{
-			get => RouteListItem.NeedToReload;
-			set
-			{
-				if(RouteListItem.WasTransfered)
-				{
-					RouteListItem.NeedToReload = value;
-					RouteListItem.RouteList.UoW.Save(RouteListItem);
-					RouteListItem.RouteList.UoW.Commit();
-				}
-			}
-		}
-
+		public bool NeedToReload => RouteListItem.NeedToReload;
+		
 		bool _leftNeedToReload;
 		public bool LeftNeedToReload
 		{
