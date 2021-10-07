a1a594a2a1e266a63080e7fc0d7cd58f372f55a7
Andrey Shaklein <as1854@yandex.ru>
2021-10-07 09:54:11 +0300

    3810 Поставил для всех entityViewModelEntry для выбора скалда в фильтре складских остатков; - убрал из нового журнала складов зашитую выборку складов с правами;

--- Vodovoz/JournalViewModels/WarehouseJournalViewModel.cs
+++ Vodovoz/JournalViewModels/WarehouseJournalViewModel.cs
@@ -1,43 +1,39 @@
 ﻿using System;
-using System.Linq;
 using NHibernate;
-using NHibernate.Criterion;
 using NHibernate.Transform;
 using QS.DomainModel.UoW;
 using QS.Project.Domain;
 using QS.Project.Journal;
 using QS.Services;
-using Vodovoz.Core;
 using Vodovoz.Domain.Store;
 using Vodovoz.EntityRepositories.Subdivisions;
-using Vodovoz.Infrastructure.Permissions;
 using Vodovoz.ViewModels.Journals.JournalNodes;
 using Vodovoz.ViewModels.Warehouses;
 
 namespace Vodovoz.JournalViewModels
 {
-    public class WarehouseJournalViewModel
-        : SingleEntityJournalViewModelBase<Warehouse, WarehouseViewModel, WarehouseJournalNode>
+    public class WarehouseJournalViewModel : SingleEntityJournalViewModelBase<Warehouse, WarehouseViewModel, WarehouseJournalNode>
     {
-        public WarehouseJournalViewModel(
+		private readonly ISubdivisionRepository _subdivisionRepository;
+		private readonly WarehouseJournalFilterViewModel _filterViewModel;
+		
+		public WarehouseJournalViewModel(
             IUnitOfWorkFactory unitOfWorkFactory,
             ICommonServices commonServices,
-            ISubdivisionRepository subdivisionRepository)
-                : base(unitOfWorkFactory, commonServices)
+            ISubdivisionRepository subdivisionRepository,
+			WarehouseJournalFilterViewModel filterViewModel = null)
+				: base(unitOfWorkFactory, commonServices)
         {
             TabName = "Журнал складов";
-            this.subdivisionRepository = subdivisionRepository ?? throw new ArgumentNullException(nameof(subdivisionRepository));
-            warehousePermissions = new[] { WarehousePermissions.WarehouseView };
+            _subdivisionRepository = subdivisionRepository ?? throw new ArgumentNullException(nameof(subdivisionRepository));
+			_filterViewModel = filterViewModel;
 
             UpdateOnChanges(
                 typeof(Warehouse)
             );
         }
 
-        private readonly ISubdivisionRepository subdivisionRepository;
-        private WarehousePermissions[] warehousePermissions;
-
-        protected override void CreateNodeActions()
+		protected override void CreateNodeActions()
         {
             NodeActionsList.Clear();
             CreateDefaultSelectAction();
@@ -50,15 +46,13 @@ namespace Vodovoz.JournalViewModels
             WarehouseJournalNode warehouseNodeAlias = null;
 
             var query = uow.Session.QueryOver<Warehouse>(() => warehouseAlias).WhereNot(w => w.IsArchive);
-            var disjunction = new Disjunction();
 
-            foreach (var p in warehousePermissions)
-            {
-                disjunction.Add<Warehouse>(w => w.Id.IsIn(CurrentPermissions.Warehouse.Allowed(p).Select(x => x.Id).ToArray()));
-            }
-            query.Where(disjunction);
+			if(_filterViewModel?.ExcludeWarehousesIds != null)
+			{
+				query.WhereRestrictionOn(x => x.Id).Not.IsIn(_filterViewModel.ExcludeWarehousesIds);
+			}
 
-            query.Where(GetSearchCriterion(
+			query.Where(GetSearchCriterion(
                 () => warehouseAlias.Id,
                 () => warehouseAlias.Name
             ));
@@ -74,14 +68,19 @@ namespace Vodovoz.JournalViewModels
             EntityUoWBuilder.ForCreate(),
             QS.DomainModel.UoW.UnitOfWorkFactory.GetDefaultFactory,
             commonServices,
-            subdivisionRepository
+            _subdivisionRepository
         );
 
         protected override Func<WarehouseJournalNode, WarehouseViewModel> OpenDialogFunction => node => new WarehouseViewModel(
             EntityUoWBuilder.ForOpen(node.Id),
             QS.DomainModel.UoW.UnitOfWorkFactory.GetDefaultFactory,
             commonServices,
-            subdivisionRepository
+            _subdivisionRepository
         );
     }
+
+	public class WarehouseJournalFilterViewModel
+	{
+		public int[] ExcludeWarehousesIds { get; set; }
+	}
 }
