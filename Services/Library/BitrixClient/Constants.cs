fda5cfa89f5641fb2310846534459d2b72f1b72f
Даниил Козлов <sprunk97@gmail.com>
2021-10-12 15:30:25 +0700

    Текущие статусы

--- Services/Library/BitrixClient/Constants.cs
+++ Services/Library/BitrixClient/Constants.cs
@@ -4,13 +4,16 @@
 	{
 		public const string ApiUrl = "https://vodovoz.bitrix24.ru";
 
+		//Статусы сделок для разных направлений(досок) имеют свой id.
+		//На данный момент тут находятся статусы для доски "заказы сайт" (C24 - по сути id направления/категории).
+		//Для доски категории 0 (ИМ оплата нал/сайт) вроде этого префикса нет
 		/// <summary>
 		/// Id статуса сделки "Завести в ДВ"
 		/// </summary>
-		public const string CreateInDVDealStageId = "13";
-		public const string InProgressDealStageId = "НЕОБХОДИМО УТОЧНИТЬ";
-		public const string DVErrorDealStageId = "НЕОБХОДИМО УТОЧНИТЬ";
-		public const string FailDealStageId = "НЕОБХОДИМО УТОЧНИТЬ";
-		public const string SuccessDealStageId = "НЕОБХОДИМО УТОЧНИТЬ";
+		public const string CreateInDVDealStageId = "C24:FINAL_INVOICE";//13
+		public const string InProgressDealStageId = "C24:1";//уточнить
+		public const string DVErrorDealStageId = "C24:2";//уточнить
+		public const string FailDealStageId = "C24:LOSE";//7
+		public const string SuccessDealStageId = "C24:WON";
 	}
 }
