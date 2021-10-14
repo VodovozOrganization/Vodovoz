5b9ca245ce70bfbbfacbeb93c606684027d48027
Andrey Shaklein <as1854@yandex.ru>
2021-10-14 17:58:11 +0300

    spaces to tabs

--- VodovozBusiness/Parameters/RouteListParametersProvider.cs
+++ VodovozBusiness/Parameters/RouteListParametersProvider.cs
@@ -3,22 +3,22 @@ using Vodovoz.Services;
 
 namespace Vodovoz.Parameters
 {
-    public class RouteListParametersProvider : IRouteListParametersProvider
-    {
-        private readonly IParametersProvider parametersProvider;
+	public class RouteListParametersProvider : IRouteListParametersProvider
+	{
+		private readonly IParametersProvider parametersProvider;
 
-        public RouteListParametersProvider(IParametersProvider parametersProvider)
-        {
-            this.parametersProvider = parametersProvider ?? throw new ArgumentNullException(nameof(parametersProvider));
-        }
-        
-        public int CashSubdivisionSofiiskayaId => parametersProvider.GetIntValue("cashsubdivision_sofiiskaya_id");
-        public int CashSubdivisionParnasId => parametersProvider.GetIntValue("cashsubdivision_parnas_id");
-        public int WarehouseSofiiskayaId => parametersProvider.GetIntValue("warehouse_sofiiskaya_id");
-        public int WarehouseParnasId => parametersProvider.GetIntValue("warehouse_parnas_id");
+		public RouteListParametersProvider(IParametersProvider parametersProvider)
+		{
+			this.parametersProvider = parametersProvider ?? throw new ArgumentNullException(nameof(parametersProvider));
+		}
+		
+		public int CashSubdivisionSofiiskayaId => parametersProvider.GetIntValue("cashsubdivision_sofiiskaya_id");
+		public int CashSubdivisionParnasId => parametersProvider.GetIntValue("cashsubdivision_parnas_id");
+		public int WarehouseSofiiskayaId => parametersProvider.GetIntValue("warehouse_sofiiskaya_id");
+		public int WarehouseParnasId => parametersProvider.GetIntValue("warehouse_parnas_id");
 		
 		//Склад Бугры
-        public int WarehouseHillocksId => parametersProvider.GetIntValue("warehouse_hillocks_id");
-        public int SouthGeographicGroupId => parametersProvider.GetIntValue("south_geographic_group_id");
-    }
+		public int WarehouseHillocksId => parametersProvider.GetIntValue("warehouse_hillocks_id");
+		public int SouthGeographicGroupId => parametersProvider.GetIntValue("south_geographic_group_id");
+	}
 }
--- VodovozBusiness/Services/IRouteListParametersProvider.cs
+++ VodovozBusiness/Services/IRouteListParametersProvider.cs
@@ -1,14 +1,14 @@
 namespace Vodovoz.Services
 {
-    public interface IRouteListParametersProvider
-    {
-        int CashSubdivisionSofiiskayaId { get; }
-        int CashSubdivisionParnasId { get; }
-        int WarehouseSofiiskayaId { get; }
-        int WarehouseParnasId { get; }
+	public interface IRouteListParametersProvider
+	{
+		int CashSubdivisionSofiiskayaId { get; }
+		int CashSubdivisionParnasId { get; }
+		int WarehouseSofiiskayaId { get; }
+		int WarehouseParnasId { get; }
 		
 		//Склад Бугры
 		int WarehouseHillocksId { get; }
 		int SouthGeographicGroupId { get; }
-    }
+	}
 }
