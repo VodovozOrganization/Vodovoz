5ea4922f825ac915bd51abc9b071b07c1708d31c
Сергей <efimov90@gmail.com>
2021-11-01 08:45:36 +0300

    Blank widget

---
+++ Vodovoz/Views/Logistic/CarView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Logistic;
+
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarView : TabViewBase<CarViewModel>
+	{
+		public CarView(CarViewModel viewModel) : base(viewModel)
+		{
+			Build();
+		}
+	}
+}
