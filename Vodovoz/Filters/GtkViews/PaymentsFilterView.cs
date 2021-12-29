e953acc3a91de43718f331d3c7e81902944198d2
Andrey Shaklein <as1854@yandex.ru>
2021-12-29 10:53:46 +0300

    3859 добавил в фильтр платежей из банк клиента возможность выборки вручную созданных платежей

--- Vodovoz/Filters/GtkViews/PaymentsFilterView.cs
+++ Vodovoz/Filters/GtkViews/PaymentsFilterView.cs
@@ -7,19 +7,29 @@ namespace Vodovoz.Filters.GtkViews
 	[System.ComponentModel.ToolboxItem(true)]
 	public partial class PaymentsFilterView : FilterViewBase<PaymentsJournalFilterViewModel>
 	{
-		public PaymentsFilterView(PaymentsJournalFilterViewModel paymentsJournalFilterVM) : base(paymentsJournalFilterVM)
+		public PaymentsFilterView(PaymentsJournalFilterViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
 			Configure();
 		}
 
 		void Configure()
 		{
-			dateRangeFilter.Binding.AddBinding(ViewModel, vm => vm.StartDate, w => w.StartDateOrNull).InitializeFromSource();
-			dateRangeFilter.Binding.AddBinding(ViewModel, vm=> vm.EndDate, w => w.EndDateOrNull).InitializeFromSource();
-			yenumcomboPaymentState.Binding.AddBinding(ViewModel, vm => vm.PaymentState, w => w.SelectedItemOrNull).InitializeFromSource();
+			dateRangeFilter.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.StartDate, w => w.StartDateOrNull)
+				.AddBinding(vm=> vm.EndDate, w => w.EndDateOrNull)
+				.InitializeFromSource();
+			yenumcomboPaymentState.Binding
+				.AddBinding(ViewModel, vm => vm.PaymentState, w => w.SelectedItemOrNull)
+				.InitializeFromSource();
 			yenumcomboPaymentState.ItemsEnum = typeof(PaymentState);
-			ycheckbtnHideCompleted.Binding.AddBinding(ViewModel, vm => vm.HideCompleted, w => w.Active).InitializeFromSource();
+			ycheckbtnHideCompleted.Binding
+				.AddBinding(ViewModel, vm => vm.HideCompleted, w => w.Active)
+				.InitializeFromSource();
+			chkIsManualCreate.Binding
+				.AddBinding(ViewModel, vm => vm.IsManualCreate, w => w.Active)
+				.InitializeFromSource();
 		}
 	}
 }
