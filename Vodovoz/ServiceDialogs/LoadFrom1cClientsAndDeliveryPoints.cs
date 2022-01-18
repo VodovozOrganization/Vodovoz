8a72bc9c6560d26ccd2a1586d1117c15714d9b66
Andrey Shaklein <as1854@yandex.ru>
2022-01-18 14:42:07 +0300

    Правки после обновления библиотек

--- Vodovoz/ServiceDialogs/LoadFrom1cClientsAndDeliveryPoints.cs
+++ Vodovoz/ServiceDialogs/LoadFrom1cClientsAndDeliveryPoints.cs
@@ -8,7 +8,7 @@ using Gtk;
 using QS.Dialog.GtkUI;
 using QS.DomainModel.UoW;
 using QSBanks;
-using QSBanks.Repositories;
+using QS.Banks.Repositories;
 using Vodovoz.Domain.Contacts;
 using QSProjectsLib;
 using Vodovoz.Domain.Client;
