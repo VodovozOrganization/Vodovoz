4988b2134b56437ea4032559bebe2bf47d528ea6
Andrey Shaklein <as1854@yandex.ru>
2022-01-27 12:44:07 +0300

    3858 создал окно для распределения баланса контрагентов

---
+++ Vodovoz/Views/Payments/AutomaticallyAllocationBalanceWindowView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.Views.Dialog;
+using Vodovoz.ViewModels.Payments;
+
+namespace Vodovoz.Views.Payments
+{
+	public partial class AutomaticallyAllocationBalanceWindowView : DialogViewBase<AutomaticallyAllocationBalanceWindowViewModel>
+	{
+		public AutomaticallyAllocationBalanceWindowView(AutomaticallyAllocationBalanceWindowViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
---
+++ VodovozViewModels/ViewModels/Payments/AutomaticallyAllocationBalanceWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using QS.ViewModels.Dialog;
+using QS.Navigation;
+namespace Vodovoz.ViewModels.Payments
+{
+	public class AutomaticallyAllocationBalanceWindowViewModel : WindowDialogViewModelBase
+	{
+		public AutomaticallyAllocationBalanceWindowViewModel(INavigationManager navigationManager) : base(navigationManager)
+		{
+		}
+	}
+}
