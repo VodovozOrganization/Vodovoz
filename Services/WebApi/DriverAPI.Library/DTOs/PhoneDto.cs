9b590a630a17f140bc1782e90c3466d1c5048040
Art8m <9artbe@gmail.com>
2022-04-08 00:39:09 +0300

    Дополнения в API для мобильного приложения "Водитель"

---
+++ Services/WebApi/DriverAPI.Library/DTOs/PhoneDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DriverAPI.Library.DTOs
+{
+	public class PhoneDto
+	{
+		public string Number { get; set; }
+		public PhoneDtoType PhoneType { get; set; }
+	}
+}
---
+++ Services/WebApi/DriverAPI.Library/DTOs/PhoneDtoType.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DriverAPI.Library.DTOs
+{
+	[JsonConverter(typeof(JsonStringEnumConverter))]
+	public enum PhoneDtoType
+	{
+		DeliveryPoint,
+		Counterparty
+	}
+}
