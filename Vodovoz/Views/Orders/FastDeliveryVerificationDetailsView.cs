1a430bdd6fcf6de81ae51fad2b0bf4e38b510e6b
Andrey Shaklein <as1854@yandex.ru>
2022-04-19 14:44:39 +0300

    3974 Добавил новое окно с детализацией по проверке на добавление быстрой доставки

---
+++ Vodovoz/Views/Orders/FastDeliveryVerificationDetailsView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.Views.Dialog;
+using Vodovoz.ViewModels.ViewModels.Orders;
+
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FastDeliveryVerificationDetailsView : DialogViewBase<FastDeliveryVerificationDetailsViewModel>
+	{
+		public FastDeliveryVerificationDetailsView(FastDeliveryVerificationDetailsViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
---
+++ VodovozViewModels/ViewModels/Orders/FastDeliveryVerificationDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.ViewModels.Dialog;
+using QS.Navigation;
+
+namespace Vodovoz.ViewModels.ViewModels.Orders
+{
+	public class FastDeliveryVerificationDetailsViewModel : WindowDialogViewModelBase
+	{
+		public FastDeliveryVerificationDetailsViewModel(
+			INavigationManager navigationManager
+		) : base(navigationManager)
+		{
+		}
+	}
+}
