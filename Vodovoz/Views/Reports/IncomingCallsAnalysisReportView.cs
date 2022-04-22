ca1411c7637eb80072cf2a752cb9f50932d91e83
Andrey Shaklein <as1854@yandex.ru>
2022-04-22 12:00:56 +0300

    3950 добавил вью/вью модель для отчета по анализу входящих звонков

---
+++ Vodovoz/Views/Reports/IncomingCallsAnalysisReportView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.Views.Dialog;
+using Vodovoz.ViewModels.ViewModels.Reports;
+
+namespace Vodovoz.Views.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class IncomingCallsAnalysisReportView : DialogViewBase<IncomingCallsAnalysisReportViewModel>
+	{
+		public IncomingCallsAnalysisReportView(IncomingCallsAnalysisReportViewModel viewModel) : base(viewModel)
+		{
+			Build();
+		}
+	}
+}
---
+++ VodovozViewModels/ViewModels/Reports/IncomingCallsAnalysisReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using QS.ViewModels.Dialog;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+
+namespace Vodovoz.ViewModels.ViewModels.Reports
+{
+	public class IncomingCallsAnalysisReportViewModel : UowDialogViewModelBase
+	{
+		public IncomingCallsAnalysisReportViewModel(IUnitOfWorkFactory uowFactory, INavigationManager navigationManager)
+			: base(uowFactory, navigationManager)
+		{
+		}
+	}
+}
