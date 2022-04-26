2c3c1dbd7ed5bd2bdc6099b4e84f41ac07dbb971
Andrey Shaklein <as1854@yandex.ru>
2022-04-26 17:42:18 +0300

    3889 создал проект с платежной страницей

---
+++ Services/WebApi/PayPageAPI/Controllers/PayController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using PayPageAPI.Models;
+
+namespace PayPageAPI.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IAvangardFastPaymentModel _avangardFastPaymentModel;
+		private readonly IPayViewModelFactory _payViewModelFactory;
+
+		public HomeController(
+			ILogger<HomeController> logger,
+			IAvangardFastPaymentModel avangardFastPaymentModel,
+			IPayViewModelFactory payViewModelFactory)
+		{
+			_logger = logger;
+			_avangardFastPaymentModel = avangardFastPaymentModel ?? throw new ArgumentNullException(nameof(avangardFastPaymentModel));
+			_payViewModelFactory = payViewModelFactory ?? throw new ArgumentNullException(nameof(payViewModelFactory));
+		}
+
+		[Route("~/{fastPaymentGuid}")]
+		public IActionResult Index(Guid fastPaymentGuid)
+		{
+			_logger.LogInformation("Поступил запрос на открытие главной страницы");
+
+			var fastPayment = _avangardFastPaymentModel.GetFastPaymentByGuid(fastPaymentGuid);
+
+			if(fastPayment == null)
+			{
+				_logger.LogError($"Запрос пришел с несуществующим платежом: guid {fastPaymentGuid}");
+				return new BadRequestResult();
+			}
+			
+			_logger.LogInformation($"Загружаем страницу с сессией: {fastPayment.Ticket}");
+			return View(_payViewModelFactory.CreateNewPayViewModel(fastPayment));
+		}
+
+		[Route("~/Oferta")]
+		public IActionResult Oferta()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Services/WebApi/PayPageAPI/Models/PayViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Gamma.Utilities;
+using QS.Utilities;
+using Vodovoz.Domain.FastPayments;
+using Vodovoz.Parameters;
+
+namespace PayPageAPI.Models
+{
+	public class PayViewModel
+	{
+		private readonly decimal _orderSum;
+		private readonly FastPaymentStatus _fastPaymentStatus;
+
+		private readonly IFastPaymentParametersProvider _fastPaymentParametersProvider;
+		
+		public PayViewModel(IFastPaymentParametersProvider fastPaymentParametersProvider, FastPayment fastPayment)
+		{
+			_fastPaymentParametersProvider =
+				fastPaymentParametersProvider ?? throw new ArgumentNullException(nameof(fastPaymentParametersProvider));
+
+			if(fastPayment == null)
+			{
+				throw new ArgumentNullException(nameof(fastPayment));
+			}
+			
+			OrderNum = fastPayment.Order.Id;
+			OrderDate = fastPayment.Order.DeliveryDate;
+			_orderSum = fastPayment.Order.OrderSum;
+			Ticket = fastPayment.Ticket;
+			_fastPaymentStatus = fastPayment.FastPaymentStatus;
+		}
+
+		public int OrderNum { get; }
+		public DateTime? OrderDate { get; }
+		public string Ticket { get; }
+
+		public string PayUrl => $"{_fastPaymentParametersProvider.GetAvangardFastPayBaseUrl}?ticket={Ticket}";
+		public string SumString => _orderSum.ToShortCurrencyString();
+		public string StatusString => _fastPaymentStatus.GetEnumTitle();
+		public bool IsNotPayable => _fastPaymentStatus != FastPaymentStatus.Processing;
+	}
+}
