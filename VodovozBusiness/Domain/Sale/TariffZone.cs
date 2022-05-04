1281b76c3aa4bab2b019b98c7d572574a9ea2914
Art8m <9artbe@gmail.com>
2022-05-04 12:38:40 +0300

    Время ограничения для доставки за час по тарифным зонам

--- VodovozBusiness/Domain/Sale/TariffZone.cs
+++ VodovozBusiness/Domain/Sale/TariffZone.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
 
@@ -17,6 +18,22 @@ namespace Vodovoz.Domain.Sale
 		public virtual string Name {
 			get => name;
 			set => SetField(ref name, value, () => Name);
+		}
+
+		private TimeSpan from;
+		[Display(Name = "От часа")]
+		public virtual TimeSpan From
+		{
+			get => from;
+			set => SetField(ref @from, value, () => From);
+		}
+
+		private TimeSpan to;
+		[Display(Name = "До часа")]
+		public virtual TimeSpan To
+		{
+			get => to;
+			set => SetField(ref to, value, () => To);
 		}
 	}
 }
--- VodovozBusiness/HibernateMapping/Sale/TariffZoneMap.cs
+++ VodovozBusiness/HibernateMapping/Sale/TariffZoneMap.cs
@@ -1,4 +1,5 @@
 ﻿using FluentNHibernate.Mapping;
+using NHibernate.Type;
 using Vodovoz.Domain.Sale;
 
 namespace Vodovoz.HibernateMapping.Sale
@@ -12,6 +13,8 @@ namespace Vodovoz.HibernateMapping.Sale
 			Id(x => x.Id).Column("id").GeneratedBy.Native();
 
 			Map(x => x.Name).Column("name");
+			Map(x => x.From).Column("from_time").CustomType<TimeAsTimeSpanType>();
+			Map(x => x.To).Column("to_time").CustomType<TimeAsTimeSpanType>();
 		}
 	}
 }
