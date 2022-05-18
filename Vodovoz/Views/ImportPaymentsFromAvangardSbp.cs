6372087cbf5de7cf64266f932318aa37b37b6909
Andrey Shaklein <as1854@yandex.ru>
2022-05-18 13:05:19 +0300

    4007 добавил вьюху для загрузки реестра оплат из Авангарда

---
+++ Vodovoz/Views/ImportPaymentsFromAvangardSbp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Vodovoz.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ImportPaymentsFromAvangardSbp : Gtk.Bin
+	{
+		public ImportPaymentsFromAvangardSbp()
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
