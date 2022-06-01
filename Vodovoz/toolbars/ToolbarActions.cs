bb2324a434b661772b4fd96c839a9097904654b9
Ruslan <rusvas012@gmail.com>
2022-06-01 12:37:54 +0300

    убрал пробел

--- Vodovoz/toolbars/ToolbarActions.cs
+++ Vodovoz/toolbars/ToolbarActions.cs
@@ -503,7 +503,7 @@ public partial class MainWindow : Window
 		tdiMain.OpenTab(
 			"CRM",
 			() => new TasksView(
-								new EmployeeJournalFactory(), 
+								new EmployeeJournalFactory(),
 								new DeliveryPointRepository()), null
 		);
 	}
