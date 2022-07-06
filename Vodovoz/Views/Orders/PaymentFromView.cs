32e39d963fb5405eda282d0a661e50c4941caf56
Andrey Shaklein <as1854@yandex.ru>
2022-07-06 16:23:14 +0300

    4043 добавил вьюху источника оплаты

---
+++ Vodovoz/Views/Orders/PaymentFromView.cs
@@ -0,0 +1,34 @@
+﻿using QS.Navigation;
+using QS.Views;
+using Vodovoz.ViewModels.ViewModels.Orders;
+
+namespace Vodovoz.Views.Orders
+{
+	public partial class PaymentFromView : ViewBase<PaymentFromViewModel>
+	{
+		public PaymentFromView(PaymentFromViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			ConfigureView();
+		}
+
+		private void ConfigureView()
+		{
+			btnSave.Clicked += (sender, e) => ViewModel.SaveAndClose();
+			btnCancel.Clicked += (sender, e) => ViewModel.Close(false, CloseSource.Cancel);
+
+			btnSave.Binding
+				.AddBinding(ViewModel, vm => vm.CanEdit, w => w.Sensitive)
+				.InitializeFromSource();
+
+			entryName.Binding
+				.AddBinding(ViewModel, vm => vm.CanEdit, w => w.Sensitive)
+				.InitializeFromSource();
+
+			entryOrganizationForAvangardPayments.SetEntityAutocompleteSelectorFactory(ViewModel.OrganizationSelectorFactory);
+			entryOrganizationForAvangardPayments.Binding
+				.AddBinding(ViewModel, vm => vm.CanEdit, w => w.Sensitive)
+				.InitializeFromSource();
+		}
+	}
+}
