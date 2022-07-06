56c0c7ce5a2606fbc48986f44fc61345e7b62daf
Ruslan <rusvas012@gmail.com>
2022-07-06 09:35:04 +0300

    4042 Возмещение затрат при эксплуатации ТС

--- VodovozViewModels/ViewModels/Logistic/CarEventViewModel.cs
+++ VodovozViewModels/ViewModels/Logistic/CarEventViewModel.cs
@@ -1,16 +1,24 @@
 ﻿using QS.Commands;
 using QS.DomainModel.UoW;
 using QS.Project.Domain;
+using QS.Project.Journal;
 using QS.Project.Journal.EntitySelector;
 using QS.Services;
+using QS.Tdi;
 using QS.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Vodovoz.Domain.Employees;
 using Vodovoz.Domain.Logistic;
+using Vodovoz.EntityRepositories.Undeliveries;
+using Vodovoz.FilterViewModels.Employees;
 using Vodovoz.Infrastructure.Services;
+using Vodovoz.Journals.JournalViewModels.Employees;
 using Vodovoz.Parameters;
 using Vodovoz.Services;
 using Vodovoz.TempAdapters;
+using Vodovoz.ViewModels.Employees;
 using Vodovoz.ViewModels.Journals.JournalFactories;
 using Vodovoz.ViewModels.TempAdapters;
 
@@ -21,6 +29,17 @@ namespace Vodovoz.ViewModels.ViewModels.Logistic
 		private readonly ICarEventSettings _carEventSettingsSettings = new CarEventSettings(new ParametersProvider());
 		private DelegateCommand _changeDriverCommand;
 		private DelegateCommand _changeEventTypeCommand;
+		private IUndeliveredOrdersJournalOpener _undeliveryViewOpener;
+		private IEntitySelectorFactory _employeeSelectorFactory;
+		private IEmployeeSettings _employeeSettings;
+
+		public bool CanEdit => PermissionResult.CanUpdate;
+		public bool CanAddFine => CanEdit;
+		public bool CanAttachFine => CanEdit;
+		public IEmployeeService EmployeeService { get; }
+		public IEmployeeJournalFactory EmployeeJournalFactory { get; }
+		public IList<FineItem> FineItems => Entity.Fines.SelectMany(x => x.Items).OrderByDescending(x => x.Id).ToList();
+
 		public CarEventViewModel(
 			IEntityUoWBuilder uowBuilder,
 			IUnitOfWorkFactory unitOfWorkFactory,
@@ -28,13 +47,23 @@ namespace Vodovoz.ViewModels.ViewModels.Logistic
 			ICarJournalFactory carJournalFactory,
 			ICarEventTypeJournalFactory carEventTypeJournalFactory,
 			IEmployeeService employeeService,
-			IEmployeeJournalFactory employeeJournalFactory)
+			IEmployeeJournalFactory employeeJournalFactory,
+			IUndeliveredOrdersJournalOpener undeliveryViewOpener,
+			IEmployeeSettings employeeSettings
+			)
 			: base(uowBuilder, unitOfWorkFactory, commonServices)
 		{
+			_undeliveryViewOpener = undeliveryViewOpener ?? throw new ArgumentNullException(nameof(undeliveryViewOpener));
+			EmployeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+			EmployeeJournalFactory = employeeJournalFactory ?? throw new ArgumentNullException(nameof(employeeJournalFactory));
+			_employeeSelectorFactory = EmployeeJournalFactory.CreateEmployeeAutocompleteSelectorFactory();
+			_employeeSettings = employeeSettings ?? throw new ArgumentNullException(nameof(employeeSettings));
+
 			if(employeeService == null)
 			{
 				throw new ArgumentNullException(nameof(employeeService));
 			}
+			CreateCommands();
 
 			CarSelectorFactory = carJournalFactory.CreateCarAutocompleteSelectorFactory();
 			CarEventTypeSelectorFactory = carEventTypeJournalFactory.CreateCarEventTypeAutocompleteSelectorFactory();
@@ -78,5 +107,70 @@ namespace Vodovoz.ViewModels.ViewModels.Logistic
 				},
 				() => true
 			));
+
+		private void CreateCommands()
+		{
+			CreateAttachFineCommand();
+			CreateAddFineCommand();
+		}
+
+		public DelegateCommand<ITdiTab> AddFineCommand { get; private set; }
+
+		private void CreateAddFineCommand()
+		{
+			AddFineCommand = new DelegateCommand<ITdiTab>(
+				t => {
+					FineViewModel fineViewModel = new FineViewModel(
+						EntityUoWBuilder.ForCreate(),
+						QS.DomainModel.UoW.UnitOfWorkFactory.GetDefaultFactory,
+						_undeliveryViewOpener,
+						EmployeeService,
+						_employeeSelectorFactory,
+						_employeeSettings,
+						CommonServices
+					);
+					fineViewModel.FineReasonString = Entity.GetFineReason();
+					fineViewModel.EntitySaved += (sender, e) => {
+						Entity.AddFine(e.Entity as Fine);
+					};
+					t.TabParent.AddSlaveTab(t, fineViewModel);
+				},
+				t => CanAddFine
+			);
+			AddFineCommand.CanExecuteChangedWith(this, x => CanAddFine);
+		}
+
+		public DelegateCommand AttachFineCommand { get; private set; }
+
+		private void CreateAttachFineCommand()
+		{
+			AttachFineCommand = new DelegateCommand(
+				() => {
+					var fineFilter = new FineFilterViewModel();
+					fineFilter.ExcludedIds = Entity.Fines.Select(x => x.Id).ToArray();
+					var fineJournalViewModel = new FinesJournalViewModel(
+						fineFilter,
+						_undeliveryViewOpener,
+						EmployeeService,
+						_employeeSelectorFactory,
+						QS.DomainModel.UoW.UnitOfWorkFactory.GetDefaultFactory,
+						_employeeSettings,
+						CommonServices
+					);
+					fineJournalViewModel.SelectionMode = JournalSelectionMode.Single;
+					fineJournalViewModel.OnEntitySelectedResult += (sender, e) => {
+						var selectedNode = e.SelectedNodes.FirstOrDefault();
+						if(selectedNode == null)
+						{
+							return;
+						}
+						Entity.AddFine(UoW.GetById<Fine>(selectedNode.Id));
+					};
+					TabParent.AddSlaveTab(this, fineJournalViewModel);
+				},
+				() => CanAttachFine
+			);
+			AttachFineCommand.CanExecuteChangedWith(this, x => CanAttachFine);
+		}
 	}
 }
