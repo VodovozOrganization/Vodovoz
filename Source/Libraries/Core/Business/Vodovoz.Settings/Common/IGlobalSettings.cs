135872742dcee953dca72236f8674af6bb074286
Vadim Gordeev <enzogord@gmail.com>
2022-08-03 15:26:41 +0300

    Connections settings
    # Conflicts:
    #	Source/Applications/Desktop/Vodovoz/Infrastructure/ConnectionStringProvider.cs
    #	Source/Applications/Desktop/Vodovoz/Vodovoz.csproj
    #	Source/Libraries/Core/Business/Vodovoz.Reports/ReportFactory.cs
    #	Vodovoz/CreateProjectParam.cs
    #	Vodovoz/MainWindow.cs
    #	Vodovoz/ReportsParameters/Sales/SalesReport.cs
    #	VodovozBusiness/Parameters/GlobalSettings.cs
    #	VodovozInfrastructure/VodovozInfrastructure.csproj
    #	VodovozReports/VodovozReports.csproj

--- Source/Libraries/Core/Business/Vodovoz.Settings/Common/IGlobalSettings.cs
+++ Source/Libraries/Core/Business/Vodovoz.Settings/Common/IGlobalSettings.cs
@@ -2,10 +2,23 @@
 {
 	public interface IGlobalSettings
 	{
+		#region Open Source Routing Machine (OSRM)
+
 		/// <summary>
 		/// Ссылка на сервис расчета растояний
 		/// </summary>
 		string OsrmServiceUrl { get; }
 		bool ExcludeToll { get; }
+
+		#endregion Open Source Routing Machine (OSRM)
+
+		#region Connection
+
+		bool SlaveConnectionEnabled { get; }
+		string SlaveConnectionEnabledForThisDatabase { get; }
+		string SlaveConnectionHost { get; }
+		int SlaveConnectionPort { get; }
+
+		#endregion Connection
 	}
 }
---
+++ VodovozBusiness/Parameters/GlobalSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Vodovoz.Services;
+
+namespace Vodovoz.Parameters
+{
+	public class GlobalSettings : IGlobalSettings
+	{
+		private readonly IParametersProvider _parametersProvider;
+
+		public GlobalSettings(IParametersProvider parametersProvider)
+		{
+			_parametersProvider = parametersProvider ?? throw new ArgumentNullException(nameof(parametersProvider));
+		}
+
+		#region Open Source Routing Machine (OSRM)
+
+		public string OsrmServiceUrl => _parametersProvider.GetStringValue("osrm_url");
+
+		public bool ExcludeToll =>  _parametersProvider.GetBoolValue("osrm_exclude_toll");
+
+		#endregion Open Source Routing Machine (OSRM)
+
+		#region Connection
+
+		public bool SlaveConnectionEnabled => _parametersProvider.GetBoolValue("slave_connection_enabled");
+
+		public string SlaveConnectionEnabledForThisDatabase => _parametersProvider.GetStringValue("slave_connection_enabled_for_this_database");
+
+		public string SlaveConnectionHost => _parametersProvider.GetStringValue("slave_connection_host");
+
+		public int SlaveConnectionPort => _parametersProvider.GetIntValue("slave_connection_port");
+
+		#endregion Connection
+	}
+}
