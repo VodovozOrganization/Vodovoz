75b8f6823e35a197d614efba0f4d478cbe70f1c0
Ruslan <rusvas012@gmail.com>
2022-08-08 04:34:47 -0700

    update ui

--- Vodovoz/Dialogs/Client/CounterpartyDlg.cs
+++ Vodovoz/Dialogs/Client/CounterpartyDlg.cs
@@ -203,6 +203,8 @@ namespace Vodovoz
 
 		public Counterparty Counterparty => UoWGeneric.Root;
 
+		public bool HasOgrn => Counterparty.CounterpartyType == CounterpartyType.Dealer;
+
 		private bool CanEdit => permissionResult.CanUpdate || permissionResult.CanCreate && Entity.Id == 0;
 
 		public override bool HasChanges
@@ -667,7 +669,13 @@ namespace Vodovoz
 
 		private void ConfigureTabRequisites()
 		{
-			validatedINN.ValidationMode = validatedKPP.ValidationMode = QSWidgetLib.ValidationType.numeric;
+			validatedOGRN.ValidationMode = validatedINN.ValidationMode = validatedKPP.ValidationMode = QSWidgetLib.ValidationType.numeric;
+
+			validatedOGRN.Binding
+				.AddBinding(Entity, e => e.OGRN, w => w.Text)
+				.InitializeFromSource();
+			validatedOGRN.IsEditable = CanEdit;
+
 			validatedINN.Binding
 				.AddBinding(Entity, e => e.INN, w => w.Text)
 				.InitializeFromSource();
@@ -1258,6 +1266,7 @@ namespace Vodovoz
 		private void OnEnumCounterpartyTypeChanged(object sender, EventArgs e)
 		{
 			rbnPrices.Visible = Entity.CounterpartyType == CounterpartyType.Supplier;
+			validatedOGRN.Visible = labelOGRN.Visible = HasOgrn;
 		}
 
 		private void OnEnumCounterpartyTypeChangedByUser(object sender, EventArgs e)
