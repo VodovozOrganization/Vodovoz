edd7b5756e74224924eabea034442366cc31fd2f
Andrey Shaklein <as1854@yandex.ru>
2022-08-15 13:33:40 +0300

    4085 создал шаблон виджета выбора месяца и шаблон диалога констант рентабельности

---
+++ Vodovoz/ViewWidgets/Profitability/MonthPickerView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Profitability
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class MonthPickerView : Gtk.Bin
+	{
+		public MonthPickerView()
+		{
+			Build();
+		}
+	}
+}
---
+++ Vodovoz/Views/Profitability/ProfitabilityConstantsView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vodovoz.Views.Profitability
+{
+	public partial class ProfitabilityConstantsView : Gtk.Bin
+	{
+		public ProfitabilityConstantsView()
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			//throw new NotImplementedException();
+		}
+	}
+}
