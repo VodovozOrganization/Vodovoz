8b5b81d9c9200104ae07f6709dd1f7caaff42929
Vadim Gordeev <enzogord@gmail.com>
2022-08-18 17:04:23 +0300

    Добавлен диалог группового изменения цены

---
+++ Vodovoz/Views/Goods/GroupNomenclaturePriceView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class GroupNomenclaturePriceView : Gtk.Bin
+	{
+		public GroupNomenclaturePriceView()
+		{
+			this.Build();
+		}
+	}
+}
