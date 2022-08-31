328321bd592387d2b8f50b9d73a8ae1c0fd784cd
Andrey Shaklein <as1854@yandex.ru>
2022-08-31 09:03:34 +0300

    4085 разделил на виджеты: основной с выбором даты и виджет с данными констант

---
+++ Vodovoz/Views/Profitability/ProfitabilityConstantsDataView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Profitability
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ProfitabilityConstantsDataView : Gtk.Bin
+	{
+		public ProfitabilityConstantsDataView()
+		{
+			this.Build();
+		}
+	}
+}
