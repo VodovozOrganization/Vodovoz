f706a182b48cbd885bcd087ddc633e59f6709665
Andrey Shaklein <as1854@yandex.ru>
2022-09-04 22:12:23 +0300

    4085 удалил "экспериментальный" интерфейс

--- VodovozViewModels/Infrastructure/Report/SelectableParametersFilter/IParametersFactory.cs
+++ VodovozViewModels/Infrastructure/Report/SelectableParametersFilter/IParametersFactory.cs
@@ -11,25 +11,4 @@ namespace Vodovoz.Infrastructure.Report.SelectableParametersFilter
 		bool IsRecursiveFactory { get; }
 		IList<SelectableParameter> GetParameters(IEnumerable<Func<ICriterion>> filterRelations);
 	}
-
-	public interface IParametersFactory<TEntity> : IParametersFactory
-		where TEntity: class, IDomainObject
-	{
-		IList<ISelectableParameter<TEntity>> GetEntityParameters(IEnumerable<Func<ICriterion>> filterRelations);
-	}
-
-	public interface ISelectableParameter
-	{
-		bool Selected { get; set; }
-		string Title { get; }
-		object Entity { get; }
-		SelectableParameter Parent { get; set; }
-		GenericObservableList<SelectableParameter> Children { get; }
-	}
-	
-	public interface ISelectableParameter<out TEntity> : ISelectableParameter
-		where TEntity: class, IDomainObject
-	{
-		TEntity Entity { get; }
-	}
 }
