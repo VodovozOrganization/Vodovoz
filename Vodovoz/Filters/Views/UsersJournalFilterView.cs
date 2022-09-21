ab42d912dff93ed149af148034679ddbae839771
Andrey Shaklein <as1854@yandex.ru>
2022-09-21 15:39:19 +0300

    2851 добавил вьюху фильтра журнала пользователей

---
+++ Vodovoz/Filters/Views/UsersJournalFilterView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Vodovoz.Filters.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class UsersJournalFilterView : Gtk.Bin
+	{
+		public UsersJournalFilterView()
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
