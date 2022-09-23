930476e7009f8424bf1dde4f3980b0acface5ed9
Vadim Gordeev <enzogord@gmail.com>
2022-09-23 04:53:11 +0300

    Добавлен сервис для периодического запуска обработчика

---
+++ Services/WebApi/RoboAtsService/Workers/TimerService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoboAtsService.Workers
+{
+	public abstract class TimerService : IHostedService, IAsyncDisposable
+	{
+		private readonly Task _completedTask = Task.CompletedTask;
+		private Timer? _timer;
+
+		protected abstract void DoWork();
+		protected abstract TimeSpan Interval { get; }
+
+		public Task StartAsync(CancellationToken stoppingToken)
+		{
+			_timer = new Timer((state) => DoWork(), null, TimeSpan.Zero, Interval);
+			OnStartService();
+			return _completedTask;
+		}
+
+		protected virtual void OnStartService()
+		{
+
+		}
+
+		public Task StopAsync(CancellationToken stoppingToken)
+		{
+			_timer?.Change(Timeout.Infinite, 0);
+			OnStopService();
+			return _completedTask;
+		}
+
+		protected virtual void OnStopService()
+		{
+
+		}
+
+		public virtual async ValueTask DisposeAsync()
+		{
+			if(_timer is IAsyncDisposable timer)
+			{
+				await timer.DisposeAsync();
+			}
+
+			_timer = null;
+		}
+	}
+}
