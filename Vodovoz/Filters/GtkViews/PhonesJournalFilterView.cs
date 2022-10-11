efb77ea42518f8ad0b60b9c93b33b4a2db7d2b61
Art8m <9artbe@gmail.com>
2022-10-11 04:58:16 -0700

    ui

---
+++ Vodovoz/Filters/GtkViews/PhonesJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PhonesJournalFilterView : Gtk.Bin
+	{
+		public PhonesJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
