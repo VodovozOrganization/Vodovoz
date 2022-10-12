59c8afc3b5292a129c6a105b50ad41e3af522021
Andrey Shaklein <as1854@yandex.ru>
2022-10-12 10:50:13 +0300

    Добавил форматирование в json уведомление о смене статуса оплаты

--- Services/WebApi/FastPaymentsAPI.Library/Services/MobileAppNotificationService.cs
+++ Services/WebApi/FastPaymentsAPI.Library/Services/MobileAppNotificationService.cs
@@ -9,15 +9,17 @@ namespace FastPaymentsAPI.Library.Services
 	public class MobileAppNotificationService : IMobileAppNotificationService
 	{
 		private readonly HttpClient _httpClient;
+		private JsonSerializerOptions _jsonOptions;
 
 		public MobileAppNotificationService(HttpClient client)
 		{
 			_httpClient = client ?? throw new ArgumentNullException(nameof(client));
+			ConfigureJsonOptions();
 		}
 
 		public async Task NotifyOfFastPaymentStatusChangedAsync(FastPaymentStatusChangeNotificationDto paymentNotificationDto, string url)
 		{
-			var json = JsonSerializer.Serialize(paymentNotificationDto);
+			var json = JsonSerializer.Serialize(paymentNotificationDto, _jsonOptions);
 			var response = await _httpClient.PostAsJsonAsync(url, json);
 
 			if(response.IsSuccessStatusCode)
@@ -25,6 +27,14 @@ namespace FastPaymentsAPI.Library.Services
 				return;
 			}
 			throw new Exception(response.ReasonPhrase);
+		}
+		
+		private void ConfigureJsonOptions()
+		{
+			_jsonOptions = new JsonSerializerOptions
+			{
+				WriteIndented = true
+			};
 		}
 	}
 }
--- Services/WebApi/FastPaymentsAPI.Library/Services/VodovozSiteNotificationService.cs
+++ Services/WebApi/FastPaymentsAPI.Library/Services/VodovozSiteNotificationService.cs
@@ -11,16 +11,18 @@ namespace FastPaymentsAPI.Library.Services
 	{
 		private readonly HttpClient _httpClient;
 		private readonly IConfiguration _configuration;
+		private JsonSerializerOptions _jsonOptions;
 
 		public VodovozSiteNotificationService(HttpClient client, IConfiguration configuration)
 		{
 			_httpClient = client ?? throw new ArgumentNullException(nameof(client));
 			_configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+			ConfigureJsonOptions();
 		}
 
 		public async Task NotifyOfFastPaymentStatusChangedAsync(FastPaymentStatusChangeNotificationDto paymentNotificationDto)
 		{
-			var json = JsonSerializer.Serialize(paymentNotificationDto);
+			var json = JsonSerializer.Serialize(paymentNotificationDto, _jsonOptions);
 			var response = await _httpClient.PostAsJsonAsync(
 				_configuration.GetSection("VodovozSiteNotificationService")
 					.GetValue<string>("NotifyOfFastPaymentStatusChangedURI"), json);
@@ -30,6 +32,14 @@ namespace FastPaymentsAPI.Library.Services
 				return;
 			}
 			throw new Exception(response.ReasonPhrase);
+		}
+		
+		private void ConfigureJsonOptions()
+		{
+			_jsonOptions = new JsonSerializerOptions
+			{
+				WriteIndented = true
+			};
 		}
 	}
 }
