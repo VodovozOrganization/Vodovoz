d57c7f9868aa1ffc575c1884551613085e67c060
Andrey Shaklein <as1854@yandex.ru>
2022-10-21 09:40:38 +0300

    4093 добавил сущности для регистрации, их версий, журнал под них, маппинг

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Employees/EmployeeRegistration.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Gamma.Utilities;
+using QS.DomainModel.Entity;
+
+namespace Vodovoz.Domain.Employees
+{
+	[Appellative(Gender = GrammaticalGender.Neuter,
+		Nominative = "оформление сотрудника",
+		NominativePlural = "оформления сотрудников")]
+	public class EmployeeRegistration : PropertyChangedBase, IDomainObject, IEmployeeRegistration
+	{
+		private RegistrationType _registrationType;
+		private PaymentForm _paymentForm;
+		private decimal _taxRate;
+
+		public virtual int Id { get; set; }
+
+		[Display(Name = "Вид оформления")]
+		public virtual RegistrationType RegistrationType
+		{
+			get => _registrationType;
+			set => SetField(ref _registrationType, value);
+		}
+
+		[Display(Name = "Форма оплаты")]
+		public virtual PaymentForm PaymentForm
+		{
+			get => _paymentForm;
+			set => SetField(ref _paymentForm, value);
+		}
+
+		[Display(Name = "Ставка налога")]
+		public virtual decimal TaxRate
+		{
+			get => _taxRate;
+			set => SetField(ref _taxRate, value);
+		}
+
+		public override string ToString() =>
+			$"Вид оформления: {RegistrationType}, форма оплаты: {PaymentForm.GetEnumTitle()} ставка налога: {TaxRate}";
+	}
+
+	public enum PaymentForm
+	{
+		[Display(Name = "Нал")]
+		Cash,
+		[Display(Name = "Безнал")]
+		Cashless
+	}
+	
+	/*public enum RegistrationType
+	{
+		[Display(Name = "Самозанятый")]
+		SelfEmployed,
+		[Display(Name = "ИП")]
+		PrivateBusinessman,
+		[Display(Name = "ГПК")]
+		Contract,
+		[Display(Name = "ТК РФ")]
+		LaborCode
+	}*/
+}
---
+++ Source/Libraries/Core/Business/VodovozBusiness/Factories/EmployeeRegistrationVersionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Vodovoz.Domain.Employees;
+
+namespace Vodovoz.Factories
+{
+	public class EmployeeRegistrationVersionFactory : IEmployeeRegistrationVersionFactory
+	{
+		private readonly IEmployeeRegistrationFactory _employeeRegistrationFactory; 
+		
+		public EmployeeRegistrationVersionFactory(IEmployeeRegistrationFactory employeeRegistrationFactory)
+		{
+			_employeeRegistrationFactory = employeeRegistrationFactory ?? throw new ArgumentNullException(nameof(employeeRegistrationFactory));
+		}
+		
+		public EmployeeRegistrationVersion CreateEmployeeRegistrationVersion(Employee employee) =>
+			new EmployeeRegistrationVersion
+			{
+				Employee = employee,
+				EmployeeRegistration = _employeeRegistrationFactory.CreateEmployeeRegistration()
+			};
+	}
+}
