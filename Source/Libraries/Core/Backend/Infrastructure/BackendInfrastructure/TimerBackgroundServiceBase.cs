768784a9cec4dcc8f313d8f9df30f1f5aee8d179
Sergey Efimov <efimov90@gmail.com>
2022-12-13 13:41:19 +0300

    Таймер бекграунд сервис

---
+++ Source/Libraries/Core/Backend/Infrastructure/BackendInfrastructure/TimerBackgroundServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vodovoz.Infrastructure
+{
+	public abstract class TimerBackgroundServiceBase : BackgroundService
+	{
+		protected abstract TimeSpan Interval { get; }
+
+		protected abstract Task DoWork(CancellationToken stoppingToken);
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while(!stoppingToken.IsCancellationRequested)
+			{
+				await DoWork(stoppingToken);
+				await Task.Delay(Interval, stoppingToken);
+			}
+		}
+
+		public override async Task StartAsync(CancellationToken cancellationToken)
+		{
+			await base.StartAsync(cancellationToken);
+		}
+
+		public override async Task StopAsync(CancellationToken cancellationToken)
+		{
+			await base.StopAsync(cancellationToken);
+		}
+	}
+}
