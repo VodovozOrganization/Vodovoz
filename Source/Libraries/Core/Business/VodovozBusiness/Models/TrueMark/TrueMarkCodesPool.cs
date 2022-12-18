12e53c3c1890f2c9d5f99fae7bdce55e04a1e6b2
Vadim Gordeev <enzogord@gmail.com>
2022-12-18 10:01:09 +0300

    Пул кодов

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Models/TrueMark/TrueMarkCodesPool.cs
@@ -0,0 +1,47 @@
+﻿using QS.DomainModel.UoW;
+using System;
+using System.Data;
+
+namespace Vodovoz.Models.TrueMark
+{
+	public class TrueMarkCodesPool
+	{
+		private readonly IUnitOfWorkFactory _uowFactory;
+
+		public TrueMarkCodesPool(IUnitOfWorkFactory uowFactory)
+		{
+			_uowFactory = uowFactory ?? throw new ArgumentNullException(nameof(uowFactory));
+		}
+
+		public void PutCode(string code)
+		{
+			using(var uow = _uowFactory.CreateWithoutRoot())
+			using(var transaction = uow.Session.BeginTransaction(IsolationLevel.RepeatableRead))
+			{
+				var sql = $@"INSERT INTO true_mark_codes_pool (code) VALUES(':code');";
+				uow.Session.CreateSQLQuery(sql)
+					.SetParameter("code", code)
+					.ExecuteUpdate();
+				uow.Commit();
+			}
+		}
+
+		public string TakeCode()
+		{
+			using(var uow = _uowFactory.CreateWithoutRoot())
+			using(var transaction = uow.Session.BeginTransaction(IsolationLevel.RepeatableRead))
+			{
+				var sql = $@"
+					DELETE FROM true_mark_codes_pool
+					ORDER BY adding_time DESC 
+					LIMIT 1
+					RETURNING code
+					;";
+				var query = uow.Session.CreateSQLQuery(sql);
+				var result = query.UniqueResult<string>();
+				uow.Commit();
+				return result;
+			}
+		}
+	}
+}
