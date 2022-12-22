89c611841f65330be2eb81e7acf8e13a8544cc6e
Vadim Gordeev <enzogord@gmail.com>
2022-12-22 10:19:35 +0300

    Формат

--- Source/Applications/Backend/Workers/IIS/RoboatsCallsWorker/Program.cs
+++ Source/Applications/Backend/Workers/IIS/RoboatsCallsWorker/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace RoboatsCallsWorker
 {
 	public class Program
-    {
+	{
 		public static async Task Main(string[] args)
 		{
 			await CreateHostBuilder(args).Build().RunAsync();
