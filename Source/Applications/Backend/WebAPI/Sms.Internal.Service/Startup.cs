daaa1f02c19c81c5d289c91031683430c2b3f4ca
Vadim Gordeev <enzogord@gmail.com>
2022-12-22 15:23:20 +0300

    Форматирование

--- Source/Applications/Backend/WebAPI/Sms.Internal.Service/Startup.cs
+++ Source/Applications/Backend/WebAPI/Sms.Internal.Service/Startup.cs
@@ -18,7 +18,7 @@ using Vodovoz.Settings.Database;
 namespace Sms.Internal.Service
 {
 	public class Startup
-    {
+	{
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
@@ -30,8 +30,8 @@ namespace Sms.Internal.Service
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
+		{
+			services.AddGrpc();
 			CreateBaseConfig();
 		}
 
@@ -48,20 +48,20 @@ namespace Sms.Internal.Service
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		{
+			if(env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseRouting();
+			app.UseRouting();
 
 			app.UseMiddleware<ApiKeyMiddleware>();
 
 			app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<SmsService>();
-            });
+			{
+				endpoints.MapGrpcService<SmsService>();
+			});
 		}
 
 		private ISmsRuConfiguration GetSmsRuSettings()
--- Source/Libraries/Core/Business/Vodovoz.Settings.Database/Setting.cs
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/Setting.cs
@@ -2,28 +2,32 @@
 
 namespace Vodovoz.Settings.Database
 {
-    public class Setting
-    {
-        #region MappedProperties
+	public class Setting
+	{
+		#region MappedProperties
 
-        public virtual string Name { get; set; }
-        public virtual string StrValue { get; set; }
-        public virtual TimeSpan? CacheTimeout { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string StrValue { get; set; }
+		public virtual TimeSpan? CacheTimeout { get; set; }
 
-        #endregion
+		#endregion
 
-        public virtual DateTime? CachedTime { get; set; }
+		public virtual DateTime? CachedTime { get; set; }
 
-        public virtual bool IsExpired {
-            get {
-                if(CacheTimeout == null) {
-                    return true;
-                }
-                if(CachedTime == null) {
-                    return false;
-                }
-                return DateTime.Now.Subtract(CachedTime.Value) > CacheTimeout;
-            }
-        }
-    }
+		public virtual bool IsExpired
+		{
+			get
+			{
+				if(CacheTimeout == null)
+				{
+					return true;
+				}
+				if(CachedTime == null)
+				{
+					return false;
+				}
+				return DateTime.Now.Subtract(CachedTime.Value) > CacheTimeout;
+			}
+		}
+	}
 }
