8f5dfaaa5f2bac39c259f408bb11c985f16a66ff
Andrey Shaklein <as1854@yandex.ru>
2022-12-26 11:49:16 +0300

    добавил в фильтр журнала заказов новый параметр и обернул его в ScrollWindow

--- Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrderFilterView.cs
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrderFilterView.cs
@@ -107,6 +107,9 @@ namespace Vodovoz.Filters.GtkViews
 				.AddBinding(ViewModel, vm => vm.SortDeliveryDate, w => w.Active, new NullableBooleanToBooleanConverter())
 				.AddBinding(ViewModel, vm => vm.SortDeliveryDateVisibility, w => w.Visible)
 				.InitializeFromSource();
+			ycheckExcludeClosingDocumentDeliverySchedule.Binding
+				.AddBinding(ViewModel, vm => vm.ExcludeClosingDocumentDeliverySchedule, w => w.Active)
+				.InitializeFromSource();
 
 			yenumcomboboxPaymentOrder.ItemsEnum = typeof(PaymentOrder);
 			yenumcomboboxPaymentOrder.Binding.AddBinding(ViewModel, vm => vm.PaymentOrder, w => w.SelectedItemOrNull)
