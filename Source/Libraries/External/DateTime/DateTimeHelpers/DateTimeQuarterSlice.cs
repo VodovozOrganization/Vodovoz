4344b623458ba4fd2c7f8bd2396de80823f7f22b
Sergey Efimov <efimov90@gmail.com>
2022-12-26 13:21:22 +0300

    DateTimeHelpersLibrary

---
+++ Source/Libraries/External/DateTime/DateTimeHelpers/DateTimeQuarterSlice.cs
@@ -0,0 +1,7 @@
+﻿namespace DateTimeHelpers
+{
+	public sealed class DateTimeQuarterSlice : DateTimeSlice
+	{
+		public override string ToString() => $"{Quarter}кв{StartDate:yy}";
+	}
+}
---
+++ Source/Libraries/External/DateTime/DateTimeHelpers/IDateTimeSlice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateTimeHelpers
+{
+	public interface IDateTimeSlice
+	{
+		DateTime EndDate { get; set; }
+		int Quarter { get; }
+		DateTimeSliceType SliceType { get; set; }
+		DateTime StartDate { get; set; }
+		int WeekNumber { get; }
+
+		string ToString();
+	}
+}
