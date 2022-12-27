fc6b316b72c928a356d809ebafa177d11a0ae050
Andrey Shaklein <as1854@yandex.ru>
2022-12-27 12:01:31 +0300

    4199 добавил окно для смены родительской группы товаров

---
+++ Source/Applications/Desktop/Vodovoz/Views/ProductGroups/EditParentProductGroupWindowView.cs
@@ -0,0 +1,26 @@
+﻿using QS.Project.Dialogs.GtkUI;
+using QS.Views.Dialog;
+using Vodovoz.Filters.ViewModels;
+using Vodovoz.Representations;
+using Vodovoz.ViewModels.ViewModels.Goods;
+
+namespace Vodovoz.Views.ProductGroups
+{
+	public partial class EditParentProductGroupWindowView : DialogViewBase<EditParentProductGroupWindowViewModel>
+	{
+		public EditParentProductGroupWindowView(EditParentProductGroupWindowViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			btnMove.Clicked += (sender, e) => ViewModel.MoveToParentGroupCommand.Execute();
+
+			entryParentProductGroup.JournalButtons = Buttons.None;
+			entryParentProductGroup.RepresentationModel = new ProductGroupVM(ViewModel.UoW, new ProductGroupFilterViewModel());
+			entryParentProductGroup.Binding.AddBinding(ViewModel, vm => vm.ParentProductGroup, w => w.Subject).InitializeFromSource();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Goods/EditParentProductGroupWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using QS.Dialog;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+using QS.Commands;
+using Vodovoz.Domain.Goods;
+using QS.DomainModel.UoW;
+
+namespace Vodovoz.ViewModels.ViewModels.Goods
+{
+	public class EditParentProductGroupWindowViewModel : WindowDialogViewModelBase, IDisposable
+	{
+		private readonly IInteractiveService _interactiveService;
+		private DelegateCommand _moveToParentGroupCommand;
+		private ProductGroup _parentProductGroup;
+
+		public EditParentProductGroupWindowViewModel(
+			IInteractiveService interactiveService,
+			INavigationManager navigationManager,
+			IUnitOfWork uow) : base(navigationManager)
+		{
+			_interactiveService = interactiveService ?? throw new ArgumentNullException(nameof(interactiveService));
+			UoW = uow ?? throw new ArgumentNullException(nameof(uow));
+		}
+
+		public IUnitOfWork UoW { get; }
+
+		public ProductGroup ParentProductGroup
+		{
+			get => _parentProductGroup;
+			set => SetField(ref _parentProductGroup, value);
+		}
+
+		public DelegateCommand MoveToParentGroupCommand => _moveToParentGroupCommand ?? (
+			_moveToParentGroupCommand = new DelegateCommand(
+				() => { }));
+				
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
