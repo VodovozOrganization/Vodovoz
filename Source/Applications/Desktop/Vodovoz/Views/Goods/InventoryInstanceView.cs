5904e307dc63db252e81be94db62b389b4d29f9a
Andrey Shaklein <as1854@yandex.ru>
2022-12-30 10:15:00 +0300

    4189 добавил вью/вью модель для экземпляра инвентарной номенклатуры

---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/InventoryInstanceView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using QS.Views;
+using Vodovoz.ViewModels.ViewModels.Goods;
+
+namespace Vodovoz.Views.Goods
+{
+	public partial class InventoryInstanceView : ViewBase<InventoryInstanceViewModel>
+	{
+		public InventoryInstanceView(InventoryInstanceViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Goods/InventoryInstanceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using QS.ViewModels;
+using Vodovoz.Domain.Documents;
+using QS.Project.Domain;
+using QS.Services;
+using QS.Navigation;
+using QS.DomainModel.UoW;
+namespace Vodovoz.ViewModels.ViewModels.Goods
+{
+	public class InventoryInstanceViewModel : EntityTabViewModelBase<InventoryDocument>
+	{
+		public InventoryInstanceViewModel(
+			IEntityUoWBuilder entityUoWBuilder,
+			IUnitOfWorkFactory unitOfWorkFactory,
+			ICommonServices commonServices,
+			INavigationManager navigationManager) : base(entityUoWBuilder, unitOfWorkFactory, commonServices, navigationManager)
+		{
+
+		}
+	}
+}
