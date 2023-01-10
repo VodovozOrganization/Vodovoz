0181149aeacd3d82d4793e82a7ee8831ba947844
Sergey Efimov <efimov90@gmail.com>
2023-01-11 00:11:27 +0300

    View

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/CarsMonitoringView.cs
@@ -0,0 +1,14 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Logistic;
+
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarsMonitoringView : TabViewBase<CarsMonitoringViewModel>
+	{
+		public CarsMonitoringView(CarsMonitoringViewModel viewModel) : base(viewModel)
+		{
+			Build();
+		}
+	}
+}
