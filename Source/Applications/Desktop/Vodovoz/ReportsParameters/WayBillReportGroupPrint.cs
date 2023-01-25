03641f6402fc6df7c84a682036a46b45cf12be9b
N-Sem <nikiforov.semen@gmail.com>
2023-01-25 16:08:39 +0300

    Поправил ошибки в окне, теперь компилируется

--- Source/Applications/Desktop/Vodovoz/ReportsParameters/WayBillReportGroupPrint.cs
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/WayBillReportGroupPrint.cs
@@ -17,53 +17,11 @@ namespace Vodovoz.ReportsParameters
 
 		public WayBillReportGroupPrint(IEmployeeJournalFactory employeeJournalFactory, ICarJournalFactory carJournalFactory)
 		{
-			_employeeJournalFactory = employeeJournalFactory ?? throw new ArgumentNullException(nameof(employeeJournalFactory));
-			_carJournalFactory = carJournalFactory ?? throw new ArgumentNullException(nameof(carJournalFactory));
-
 			Build();
-			UoW = UnitOfWorkFactory.CreateWithoutRoot();
-			Configure();
 		}
 
-		private void Configure()
-		{
-			datepicker.Date = DateTime.Today;
-			timeHourEntry.Text = DateTime.Now.Hour.ToString("00.##");
-			timeMinuteEntry.Text = DateTime.Now.Minute.ToString("00.##");
-
-			entryDriver.SetEntityAutocompleteSelectorFactory(
-				_employeeJournalFactory.CreateWorkingDriverEmployeeAutocompleteSelectorFactory());
-
-			entryCar.SetEntityAutocompleteSelectorFactory(_carJournalFactory.CreateCarAutocompleteSelectorFactory());
-		}
-
-		#region IParametersWidget implementation
+		public string Title => throw new NotImplementedException();
 
 		public event EventHandler<LoadReportEventArgs> LoadReport;
-
-		public string Title => "Путевой лист";
-
-		#endregion
-
-		private ReportInfo GetReportInfo()
-		{
-			return new ReportInfo
-			{
-				Identifier = "Logistic.WayBillReport",
-				Parameters = new Dictionary<string, object>
-				{
-					{ "date", datepicker.Date },
-					{ "driver_id", (entryDriver?.Subject as Employee)?.Id ?? -1 },
-					{ "car_id", (entryCar?.Subject as Car)?.Id ?? -1 },
-					{ "time", timeHourEntry.Text + ":" + timeMinuteEntry.Text },
-					{ "need_date", !datepicker.IsEmpty }
-				}
-			};
-		}
-
-		protected void OnButtonCreateRepotClicked(object sender, EventArgs e)
-		{
-			LoadReport?.Invoke(this, new LoadReportEventArgs(GetReportInfo(), true));
-		}
 	}
 }
