5ebb84c66982c5e3e20c66b0bc2b3631a7ee94a6
N-Sem <nikiforov.semen@gmail.com>
2023-01-25 17:24:34 +0300

    Промежуточный результат

--- Source/Applications/Desktop/Vodovoz/ReportsParameters/WayBillReportGroupPrint.cs
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/WayBillReportGroupPrint.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using QS.Report;
-using QSReport;
-using QS.Dialog.GtkUI;
-using Vodovoz.Domain.Employees;
-using QS.DomainModel.UoW;
-using Vodovoz.Domain.Logistic.Cars;
-using Vodovoz.TempAdapters;
-using Vodovoz.ViewModels.TempAdapters;
 namespace Vodovoz.ReportsParameters
 {
-	public partial class WayBillReportGroupPrint : SingleUoWWidgetBase, IParametersWidget
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class WayBillReportGroupPrint : Gtk.Bin
 	{
-		private readonly IEmployeeJournalFactory _employeeJournalFactory;
-		private readonly ICarJournalFactory _carJournalFactory;
-
-		public WayBillReportGroupPrint(IEmployeeJournalFactory employeeJournalFactory, ICarJournalFactory carJournalFactory)
+		public WayBillReportGroupPrint()
 		{
-			Build();
+			this.Build();
 		}
-
-		public string Title => throw new NotImplementedException();
-
-		public event EventHandler<LoadReportEventArgs> LoadReport;
 	}
 }
