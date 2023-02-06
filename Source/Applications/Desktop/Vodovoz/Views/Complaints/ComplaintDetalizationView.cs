f874f4a9d2f4bc51339a5f3fa9efedd705b24c93
Sergey Efimov <efimov90@gmail.com>
2023-02-07 00:25:42 +0300

    ComplaintDetaizationView

---
+++ Source/Applications/Desktop/Vodovoz/Views/Complaints/ComplaintDetalizationView.cs
@@ -0,0 +1,14 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Complaints;
+
+namespace Vodovoz.Views.Complaints
+{
+	public partial class ComplaintDetalizationView : TabViewBase<ComplaintDetalizationViewModel>
+	{
+		public ComplaintDetalizationView(ComplaintDetalizationViewModel viewModel)
+			: base(viewModel)
+		{
+			Build();
+		}
+	}
+}
