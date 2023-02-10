d3d9cad3ee438088c394dfb122f1867fe363e584
Sergey Efimov <efimov90@gmail.com>
2023-02-10 11:56:10 +0300

    чистка InventoryDocument

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Documents/InventoryDocument.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Documents/InventoryDocument.cs
@@ -1,79 +1,95 @@
+﻿using Gamma.Utilities;
+using QS.DomainModel.Entity;
+using QS.DomainModel.Entity.EntityPermissions;
+using QS.DomainModel.UoW;
+using QS.HistoryLog;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Bindings.Collections.Generic;
 using System.Linq;
-using Gamma.Utilities;
-using QS.DomainModel.Entity;
-using QS.DomainModel.Entity.EntityPermissions;
-using QS.DomainModel.UoW;
-using QS.HistoryLog;
+using System.Linq.Expressions;
 using Vodovoz.Domain.Goods;
 using Vodovoz.Domain.Store;
 using Vodovoz.EntityRepositories.Stock;
 
 namespace Vodovoz.Domain.Documents
 {
-	[Appellative (Gender = GrammaticalGender.Feminine,
+	[Appellative(Gender = GrammaticalGender.Feminine,
 		NominativePlural = "инвентаризации",
 		Nominative = "инвентаризация",
 		Prepositional = "инвентаризации")]
 	[EntityPermission]
 	[HistoryTrace]
-	public class InventoryDocument: Document, IValidatableObject
+	public class InventoryDocument : Document, IValidatableObject
 	{
-		public override DateTime TimeStamp {
+		public override DateTime TimeStamp
+		{
 			get { return base.TimeStamp; }
-			set {
+			set
+			{
 				base.TimeStamp = value;
-				foreach (var item in Items) {
-					if (item.WarehouseChangeOperation != null && item.WarehouseChangeOperation.OperationTime != TimeStamp)
+				foreach(var item in Items)
+				{
+					if(item.WarehouseChangeOperation != null && item.WarehouseChangeOperation.OperationTime != TimeStamp)
+					{
 						item.WarehouseChangeOperation.OperationTime = TimeStamp;
+					}
 				}
 			}
 		}
 
-		string comment;
+		string _comment;
 
-		[Display (Name = "Комментарий")]
-		public virtual string Comment {
-			get { return comment; }
-			set { SetField (ref comment, value, () => Comment); }
+		[Display(Name = "Комментарий")]
+		public virtual string Comment
+		{
+			get { return _comment; }
+			set { SetField(ref _comment, value); }
 		}
 
-		Warehouse warehouse;
+		Warehouse _warehouse;
 
-		[Display (Name = "Склад")]
+		[Display(Name = "Склад")]
 		[Required(ErrorMessage = "Склад должен быть указан.")]
-		public virtual Warehouse Warehouse {
-			get { return warehouse; }
-			set {
-				SetField (ref warehouse, value, () => Warehouse);
+		public virtual Warehouse Warehouse
+		{
+			get { return _warehouse; }
+			set
+			{
+				SetField(ref _warehouse, value);
 			}
 		}
 
-		IList<InventoryDocumentItem> items = new List<InventoryDocumentItem> ();
+		IList<InventoryDocumentItem> _items = new List<InventoryDocumentItem>();
 
-		[Display (Name = "Строки")]
-		public virtual IList<InventoryDocumentItem> Items {
-			get { return items; }
-			set {
-				SetField (ref items, value, () => Items);
-				observableItems = null;
+		[Display(Name = "Строки")]
+		public virtual IList<InventoryDocumentItem> Items
+		{
+			get { return _items; }
+			set
+			{
+				SetField(ref _items, value);
+				_observableItems = null;
 			}
 		}
 
-		GenericObservableList<InventoryDocumentItem> observableItems;
+		GenericObservableList<InventoryDocumentItem> _observableItems;
 		//FIXME Кослыль пока не разберемся как научить hibernate работать с обновляемыми списками.
-		public virtual GenericObservableList<InventoryDocumentItem> ObservableItems {
-			get {
-				if (observableItems == null)
-					observableItems = new GenericObservableList<InventoryDocumentItem> (Items);
-				return observableItems;
+		public virtual GenericObservableList<InventoryDocumentItem> ObservableItems
+		{
+			get
+			{
+				if(_observableItems == null)
+				{
+					_observableItems = new GenericObservableList<InventoryDocumentItem>(Items);
+				}
+
+				return _observableItems;
 			}
 		}
 
-		public virtual string Title => String.Format("Инвентаризация №{0} от {1:d}", Id, TimeStamp);
+		public virtual string Title => $"Инвентаризация №{Id} от {TimeStamp:d}";
 
 		#region Функции
 
@@ -97,20 +113,23 @@ namespace Vodovoz.Domain.Documents
 		public virtual void AddItem(Nomenclature nomenclature, decimal amountInDB, decimal amountInFact)
 		{
 			var item = new InventoryDocumentItem()
-			{ 
+			{
 				Nomenclature = nomenclature,
 				AmountInDB = amountInDB,
 				AmountInFact = amountInFact,
 				Document = this
 			};
-			ObservableItems.Add (item);
+			ObservableItems.Add(item);
 		}
 
-		public virtual void FillItemsFromStock(IUnitOfWork uow, Dictionary<int, decimal> selectedNomenclature){
+		public virtual void FillItemsFromStock(IUnitOfWork uow, Dictionary<int, decimal> selectedNomenclature)
+		{
 			var inStock = selectedNomenclature;
 
-			if (inStock.Count == 0)
+			if(inStock.Count == 0)
+			{
 				return;
+			}
 
 			var nomenclatures = uow.GetById<Nomenclature>(inStock.Select(p => p.Key).ToArray());
 
@@ -118,12 +137,13 @@ namespace Vodovoz.Domain.Documents
 			foreach(var itemInStock in inStock)
 			{
 				ObservableItems.Add(
-					new InventoryDocumentItem(){
-					Nomenclature = nomenclatures.First(x => x.Id == itemInStock.Key),
-					AmountInDB = itemInStock.Value,
-					AmountInFact = 0,
-					Document = this
-				}
+					new InventoryDocumentItem()
+					{
+						Nomenclature = nomenclatures.First(x => x.Id == itemInStock.Key),
+						AmountInDB = itemInStock.Value,
+						AmountInFact = 0,
+						Document = this
+					}
 				);
 			}
 		}
@@ -139,7 +159,9 @@ namespace Vodovoz.Domain.Documents
 			int[] productGroupToExclude)
 		{
 			if(Warehouse == null)
+			{
 				return;
+			}
 
 			var selectedNomenclature = stockRepository.NomenclatureInStock(
 				uow,
@@ -150,7 +172,7 @@ namespace Vodovoz.Domain.Documents
 				nomenclatureTypeToExclude,
 				productGroupToInclude,
 				productGroupToExclude);
-			
+
 			FillItemsFromStock(uow, selectedNomenclature);
 		}
 
@@ -165,7 +187,9 @@ namespace Vodovoz.Domain.Documents
 			int[] productGroupToExclude)
 		{
 			if(Warehouse == null)
+			{
 				return;
+			}
 
 			var inStock = stockRepository.NomenclatureInStock(
 				uow,
@@ -181,8 +205,10 @@ namespace Vodovoz.Domain.Documents
 			foreach(var itemInStock in inStock)
 			{
 				var item = Items.FirstOrDefault(x => x.Nomenclature.Id == itemInStock.Key);
-				if (item != null)
+				if(item != null)
+				{
 					item.AmountInDB = itemInStock.Value;
+				}
 				else
 				{
 					ObservableItems.Add(
@@ -197,12 +223,16 @@ namespace Vodovoz.Domain.Documents
 			}
 
 			var itemsToRemove = new List<InventoryDocumentItem>();
-			foreach(var item in Items) {
+			foreach(var item in Items)
+			{
 				if(!inStock.ContainsKey(item.Nomenclature.Id))
+				{
 					itemsToRemove.Add(item);
+				}
 			}
 
-			foreach(var item in itemsToRemove) {
+			foreach(var item in itemsToRemove)
+			{
 				ObservableItems.Remove(item);
 			}
 		}
@@ -228,12 +258,14 @@ namespace Vodovoz.Domain.Documents
 						item.CreateOperation(Warehouse, TimeStamp);
 					}
 				}
-				if(item.AmountInDB == 0 && item.AmountInFact == 0) {
+				if(item.AmountInDB == 0 && item.AmountInFact == 0)
+				{
 					itemsToDelete.Add(item);
 				}
 			}
 
-			foreach(var item in itemsToDelete) {
+			foreach(var item in itemsToDelete)
+			{
 				uow.Delete(item);
 				Items.Remove(item);
 			}
@@ -241,15 +273,19 @@ namespace Vodovoz.Domain.Documents
 
 		#endregion
 
-		public virtual IEnumerable<ValidationResult> Validate (ValidationContext validationContext)
+		public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
 		{
 			if(Items.Count == 0)
-				yield return new ValidationResult (String.Format("Табличная часть документа пустая."),
-					new[] { this.GetPropertyName (o => o.Items) });
-			
-			if(TimeStamp == default(DateTime))
-				yield return new ValidationResult (String.Format("Дата документа должна быть указана."),
-					new[] { this.GetPropertyName (o => o.TimeStamp) });
+			{
+				yield return new ValidationResult("Табличная часть документа пустая.",
+					new[] { this.GetPropertyName(o => o.Items) });
+			}
+
+			if(TimeStamp == default)
+			{
+				yield return new ValidationResult("Дата документа должна быть указана.",
+					new[] { this.GetPropertyName(o => o.TimeStamp) });
+			}
 
 			foreach(var item in Items)
 			{
@@ -278,7 +314,7 @@ namespace Vodovoz.Domain.Documents
 			}
 		}
 
-		public InventoryDocument ()
+		public InventoryDocument()
 		{
 		}
 
