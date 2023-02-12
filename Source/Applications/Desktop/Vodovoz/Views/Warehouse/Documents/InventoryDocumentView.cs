a6394e0f7850e00c8d9c4234564c0a0e62c7471b
Sergey Efimov <efimov90@gmail.com>
2023-02-12 10:45:58 +0300

    Болванка View

---
+++ Source/Applications/Desktop/Vodovoz/Views/Warehouse/Documents/InventoryDocumentView.cs
@@ -0,0 +1,21 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Warehouses.Documents;
+
+namespace Vodovoz.Views.Warehouse.Documents
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class InventoryDocumentView : TabViewBase<InventoryDocumentViewModel>
+	{
+		public InventoryDocumentView(InventoryDocumentViewModel viewModel)
+			: base(viewModel)
+		{
+			Build();
+			ConfigureView();
+		}
+
+		private void ConfigureView()
+		{
+
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Warehouses/Documents/InventoryDocumentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using Vodovoz.Domain.Documents;
+
+namespace Vodovoz.ViewModels.ViewModels.Warehouses.Documents
+{
+	public class InventoryDocumentViewModel : EntityTabViewModelBase<InventoryDocument>
+	{
+		public InventoryDocumentViewModel(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, ICommonServices commonServices, INavigationManager navigation = null)
+		 : base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
+		{
+		}
+	}
+}
