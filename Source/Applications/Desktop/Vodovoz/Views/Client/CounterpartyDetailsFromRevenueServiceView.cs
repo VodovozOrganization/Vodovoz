055cd883ab2da8703e4fbe3f92c3767afed939d6
Art8m <9artbe@gmail.com>
2023-02-20 05:22:15 -0800

    stetic

---
+++ Source/Applications/Desktop/Vodovoz/Views/Client/CounterpartyDetailsFromRevenueServiceView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Client
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CounterpartyDetailsFromRevenueServiceView : Gtk.Bin
+	{
+		public CounterpartyDetailsFromRevenueServiceView()
+		{
+			this.Build();
+		}
+	}
+}
