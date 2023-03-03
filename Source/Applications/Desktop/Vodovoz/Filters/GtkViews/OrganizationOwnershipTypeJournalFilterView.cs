21733bfc69e2bc9def70f26aab0667bf950c030c
N-Sem <nikiforov.semen@gmail.com>
2023-03-03 12:35:21 +0300

    Изменения во вьюшке фильтра

--- Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrganizationOwnershipTypeJournalFilterView.cs
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrganizationOwnershipTypeJournalFilterView.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using QS.Views.GtkUI;
+using Vodovoz.Filters.ViewModels;
+
 namespace Vodovoz.Filters.GtkViews
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class OrganizationOwnershipTypeJournalFilterView : Gtk.Bin
+	public partial class OrganizationOwnershipTypeJournalFilterView : FilterViewBase<OrganizationOwnershipTypeJournalFilterViewModel>
 	{
-		public OrganizationOwnershipTypeJournalFilterView()
+		public OrganizationOwnershipTypeJournalFilterView(OrganizationOwnershipTypeJournalFilterViewModel organizationOwnershipTypeJournalFilterViewModel) : base(сlientCameFromFilterViewModel)
 		{
 			this.Build();
+			Configure();
+		}
+
+		void Configure()
+		{
+			yChkShowArchive.Binding.AddBinding(ViewModel, vm => vm.RestrictArchive, w => w.Active).InitializeFromSource();
 		}
 	}
 }
