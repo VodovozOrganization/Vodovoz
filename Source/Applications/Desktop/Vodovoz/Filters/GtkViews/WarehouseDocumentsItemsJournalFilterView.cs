b3ba7ace940f3ca2afea34ca68ed0dfed94c7979
Sergey Efimov <efimov90@gmail.com>
2023-03-05 18:07:27 +0300

    Фильтр

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/WarehouseDocumentsItemsJournalFilterView.cs
@@ -0,0 +1,15 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Store;
+
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class WarehouseDocumentsItemsJournalFilterView : FilterViewBase<WarehouseDocumentsItemsJournalFilterViewModel>
+	{
+		public WarehouseDocumentsItemsJournalFilterView(WarehouseDocumentsItemsJournalFilterViewModel filterViewModel)
+			: base(filterViewModel)
+		{
+			Build();
+		}
+	}
+}
