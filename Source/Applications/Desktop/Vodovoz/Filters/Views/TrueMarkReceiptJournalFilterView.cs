214e437f81ca51fbd4556bd8562e5dbb82f6bd0e
Vadim Gordeev <enzogord@gmail.com>
2023-03-06 17:52:24 +0300

    Добавлен виджет фильтра

---
+++ Source/Applications/Desktop/Vodovoz/Filters/Views/TrueMarkReceiptJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class TrueMarkReceiptJournalFilterView : Gtk.Bin
+	{
+		public TrueMarkReceiptJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
