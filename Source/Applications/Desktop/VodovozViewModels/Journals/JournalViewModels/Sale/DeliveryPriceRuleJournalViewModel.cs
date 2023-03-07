6bce66f866fe8efb7ec03a87214f0aa69b058509
Semen <34092262+N-Sem@users.noreply.github.com>
2023-03-07 12:54:57 +0300

    Откорректировал вью-модели и журнал, добавил регистрацию колонок в отдельном файле

--- Source/Applications/Desktop/VodovozViewModels/Journals/JournalViewModels/Sale/DeliveryPriceRuleJournalViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Journals/JournalViewModels/Sale/DeliveryPriceRuleJournalViewModel.cs
@@ -8,6 +8,7 @@ using System;
 using Vodovoz.Domain.Sale;
 using Vodovoz.ViewModels.ViewModels.Sale;
 using Vodovoz.EntityRepositories.Sale;
+using Vodovoz.ViewModels.Journals.JournalNodes;
 
 namespace Vodovoz.ViewModels.Journals.JournalViewModels.Sale
 {
@@ -78,23 +79,5 @@ namespace Vodovoz.ViewModels.Journals.JournalViewModels.Sale
 			CreateDefaultAddActions();
 			CreateDefaultEditAction();
 		}
-	}
-
-	public class DeliveryPriceRuleJournalNode : JournalEntityNodeBase<DeliveryPriceRule>
-	{
-		public override string Title => $"{Name} {Water19LCount}";
-		public int Water19LCount { get; set; }
-		public int Water6LCount { get; set; }
-		public int Water1500mlCount { get; set; }
-		public int Water600mlCount { get; set; }
-		public int Water500mlCount { get; set; }
-		public string Name { get; set; }
-		public decimal OrderMinSumEShopGoods { get; set; }
-
-		public string Description => $"Если " +
-			$"19л б. < {Water19LCount}шт. " +
-			$"или 6л б. < {Water6LCount}шт. " +
-			$"или 1500мл б. < {Water1500mlCount}шт. " +
-			$"или 500мл б. < {Water500mlCount}шт.";
 	}
 }
