831672d6e23c59a2dc56a5bddc8bae193f524301
Sergey Efimov <efimov90@gmail.com>
2023-03-16 09:52:32 +0300

    IProfitabilityConstantsViewModelHandler

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Controllers/IProfitabilityConstantsViewModelHandler.cs
@@ -0,0 +1,12 @@
+﻿using QS.DomainModel.UoW;
+using System;
+using Vodovoz.Domain;
+
+namespace Vodovoz.Controllers
+{
+	public interface IProfitabilityConstantsViewModelHandler
+	{
+		IUnitOfWorkGeneric<ProfitabilityConstants> GetLastCalculatedProfitabilityConstants();
+		IUnitOfWorkGeneric<ProfitabilityConstants> GetProfitabilityConstantsByCalculatedMonth(IUnitOfWork uow, DateTime calculatedMonth);
+	}
+}
