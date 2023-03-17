a7c09eb259e015afcc1395e9fce860a9ed56843c
Andrey Shaklein <as1854@yandex.ru>
2023-03-17 14:48:27 +0300

    добавил вьюхи фильтра и карточки ручного сопоставления контакта

---
+++ Source/Applications/Desktop/Vodovoz/Filters/Views/ExternalCounterpartiesMatchingJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ExternalCounterpartiesMatchingJournalFilterView : Gtk.Bin
+	{
+		public ExternalCounterpartiesMatchingJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/Views/Client/ExternalCounterpartyMatchingView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Vodovoz.Views.Client
+{
+	public partial class ExternalCounterpartyMatchingView : Gtk.Bin
+	{
+		public ExternalCounterpartyMatchingView()
+		{
+			Build();
+		}
+	}
+}
