c97a1394021d7c6214300227429a82b25a258f83
Sergey Efimov <efimov90@gmail.com>
2023-03-23 17:59:07 +0300

    Болванка отчета и вспомогательный класс

---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Reports/Sales/SalesBySubdivisionsAnalitycsReport.cs
@@ -0,0 +1,81 @@
+﻿using DateTimeHelpers;
+using System;
+
+namespace Vodovoz.ViewModels.ViewModels.Reports.Sales
+{
+	public class SalesBySubdivisionsAnalitycsReport
+	{
+		private SalesBySubdivisionsAnalitycsReport(
+			DateTimePeriod firstPeriod,
+			DateTimePeriod secondPeriod,
+			bool splitByNomenclatures,
+			bool splitBySubdivisions,
+			bool splitByWarehouses)
+		{
+			FirstPeriod = firstPeriod;
+			SecondPeriod = secondPeriod;
+			SplitByNomenclatures = splitByNomenclatures;
+			SplitBySubdivisions = splitBySubdivisions;
+			SplitByWarehouses = splitByWarehouses;
+		}
+
+		public DateTimePeriod FirstPeriod { get; }
+
+		public DateTimePeriod SecondPeriod { get; }
+
+		public bool SplitByNomenclatures { get; }
+
+		public bool SplitBySubdivisions { get; }
+
+		public bool SplitByWarehouses { get; }
+
+		public static SalesBySubdivisionsAnalitycsReport Create(
+			DateTimePeriod firstPeriod,
+			DateTimePeriod secondPeriod,
+			bool splitByNomenclatures,
+			bool splitBySubdivisions,
+			bool splitByWarehouses)
+		{
+			ValidateParameters(
+				firstPeriod,
+				secondPeriod,
+				splitByNomenclatures,
+				splitBySubdivisions,
+				splitByWarehouses);
+
+			return new SalesBySubdivisionsAnalitycsReport(
+				firstPeriod,
+				secondPeriod,
+				splitByNomenclatures,
+				splitBySubdivisions,
+				splitByWarehouses);
+		}
+
+		private static void ValidateParameters(
+			DateTimePeriod firstPeriod,
+			DateTimePeriod secondPeriod,
+			bool splitByNomenclatures,
+			bool splitBySubdivisions,
+			bool splitByWarehouses)
+		{
+			if(firstPeriod == null)
+			{
+				throw new ArgumentNullException("Нельзя создать отчет не указав период",
+					nameof(firstPeriod));
+			}
+
+			if(splitByWarehouses && secondPeriod != null)
+			{
+				throw new ArgumentException("Нельзя выбрать разбивку по складам для отчета с двумя периодами",
+					nameof(splitByWarehouses));
+			}
+
+			if(splitByWarehouses
+				&& (firstPeriod.EndDateTime - firstPeriod.StartDateTime).TotalDays > 1)
+			{
+				throw new ArgumentException("Нельзя выбрать разбивку по складам для отчета с интервалом более одного дня",
+					nameof(splitByWarehouses));
+			}
+		}
+	}
+}
---
+++ Source/Libraries/External/DateTime/DateTimeHelpers/DateTimePeriod.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DateTimeHelpers
+{
+	public class DateTimePeriod
+	{
+		public DateTime StartDateTime { get; set; }
+
+		public DateTime EndDateTime { get; set; }
+	}
+}
