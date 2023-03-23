b63572765f30c777a246998be6a7bbe737a82ae4
Sergey Efimov <efimov90@gmail.com>
2023-03-23 18:50:20 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Views/Reports/SalesBySubdivisionsAnalitycsReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class SalesBySubdivisionsAnalitycsReportView : Gtk.Bin
+	{
+		public SalesBySubdivisionsAnalitycsReportView()
+		{
+			this.Build();
+		}
+	}
+}
