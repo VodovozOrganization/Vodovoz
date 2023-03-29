54d589b49282e68f320ed9f68ce60ce3bd2b3a67
Vadim Gordeev <enzogord@gmail.com>
2023-03-29 13:55:58 +0300

    Фикс ссылок на сборки

--- Source/Applications/Backend/WebAPI/CashReceiptApi/Startup.cs
+++ Source/Applications/Backend/WebAPI/CashReceiptApi/Startup.cs
@@ -2,7 +2,6 @@
 using CashReceiptApi.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
