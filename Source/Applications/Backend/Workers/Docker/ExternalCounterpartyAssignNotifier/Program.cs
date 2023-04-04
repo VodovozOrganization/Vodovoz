2ad5976c924f5b3ff2a960d93f0d03da5d6660d5
Andrey Shaklein <as1854@yandex.ru>
2023-04-04 18:39:47 +0300

    добавил воркер по отправке сопоставленных данных в ИПЗ

--- Source/Applications/Backend/Workers/Docker/ExternalCounterpartyAssignNotifier/Program.cs
+++ Source/Applications/Backend/Workers/Docker/ExternalCounterpartyAssignNotifier/Program.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using ExternalCounterpartyAssignNotifier.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
