55fdd1f7d210cf7b5a36b7fbc8b4008acfe13254
Semen <34092262+N-Sem@users.noreply.github.com>
2023-04-13 15:23:24 +0300

    Изменены заголовки столбцов

--- Source/Applications/Desktop/Vodovoz/Views/Goods/FixedPricesView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/FixedPricesView.cs
@@ -32,30 +32,30 @@ namespace Vodovoz.Views.Goods
 				.AddColumn("Номенклатура").AddTextRenderer(x => x.Name)
 				.Finish();
 
-			ytreeviewNomenclatures.Selection.Changed += NomenclatureSelection_Changed;
+			ytreeviewNomenclatures.Selection.Changed += OnNomenclatureSelectionChanged;
 			ytreeviewNomenclatures.Binding.AddBinding(ViewModel, vm => vm.FixedPriceNomenclatures, w => w.ItemsDataSource).InitializeFromSource();
 
 			ViewModel.DiffFormatter = new PangoDiffFormater();
 			ytreeviewFixedPricesChanges.ColumnsConfig = FluentColumnsConfig<FieldChange>.Create()
-				.AddColumn("Время изменения").AddTextRenderer(x => x.Entity.ChangeTimeText)
+				.AddColumn("Время\nизменения").AddTextRenderer(x => x.Entity.ChangeTimeText)
 				.AddColumn("Пользователь").AddTextRenderer(x => x.Entity.ChangeSet.UserName)
-				.AddColumn("Старое значение").AddTextRenderer(x => x.OldFormatedDiffText, useMarkup: true)
-				.AddColumn("Новое значение").AddTextRenderer(x => x.NewFormatedDiffText, useMarkup: true)
+				.AddColumn("Старое\nзначение").AddTextRenderer(x => x.OldFormatedDiffText, useMarkup: true)
+				.AddColumn("Новое\nзначение").AddTextRenderer(x => x.NewFormatedDiffText, useMarkup: true)
 				.Finish();
 
 			ytreeviewFixedPricesChanges.Binding.AddBinding(ViewModel, vm => vm.SelectedPriceChanges, w => w.ItemsDataSource).InitializeFromSource();
 
 			ytreeviewFixedPriceAndCount.ColumnsConfig = FluentColumnsConfig<FixedPriceItemViewModel>.Create()
-				.AddColumn("Номенклатура").AddTextRenderer(x => x.NomenclatureTitle)
-				.AddColumn("Фиксированная цена").AddNumericRenderer(x => x.FixedPrice).Editing().Digits(2)
-					.AddSetter((cell, node) => cell.Editable = ViewModel.CanEdit)
+				//.AddColumn("Номенклатура").AddTextRenderer(x => x.NomenclatureTitle)
+				.AddColumn("Минимальное\nколичество").AddNumericRenderer(x => x.MinCount).Editing()
 					.Adjustment(new Gtk.Adjustment(0, 0, 1e6, 1, 10, 10))
-				.AddColumn("Минимальное количество").AddNumericRenderer(x => x.MinCount).Editing()
+				.AddColumn("Фиксированная\nцена").AddNumericRenderer(x => x.FixedPrice).Editing().Digits(2)
+					.AddSetter((cell, node) => cell.Editable = ViewModel.CanEdit)
 					.Adjustment(new Gtk.Adjustment(0, 0, 1e6, 1, 10, 10))
 				.Finish();
 
 			ytreeviewFixedPriceAndCount.Binding.AddBinding(ViewModel, vm => vm.FixedPricesByNomenclature, w => w.ItemsDataSource).InitializeFromSource();
-			ytreeviewFixedPriceAndCount.Selection.Changed += PriceSelection_Changed;
+			ytreeviewFixedPriceAndCount.Selection.Changed += OnPriceSelectionChanged;
 
 			buttonAdd.Clicked += (s, e) => ViewModel.AddFixedPriceCommand.Execute();
 			ViewModel.AddFixedPriceCommand.CanExecuteChanged += (sender, e) => buttonAdd.Sensitive = ViewModel.AddFixedPriceCommand.CanExecute();
@@ -66,13 +66,13 @@ namespace Vodovoz.Views.Goods
 			buttonDel.Sensitive = ViewModel.RemoveFixedPriceCommand.CanExecute();
 		}
 
-		void NomenclatureSelection_Changed(object sender, EventArgs e)
+		void OnNomenclatureSelectionChanged(object sender, EventArgs e)
 		{
 			var selectedNomenclature = ytreeviewNomenclatures.GetSelectedObject() as Nomenclature;
 			ViewModel.SelectedNomenclature = selectedNomenclature;
 		}
 
-		void PriceSelection_Changed(object sender, EventArgs e)
+		void OnPriceSelectionChanged(object sender, EventArgs e)
 		{
 			var selectedPrice = ytreeviewFixedPriceAndCount.GetSelectedObject() as FixedPriceItemViewModel;
 			ViewModel.SelectedFixedPrice = selectedPrice;
