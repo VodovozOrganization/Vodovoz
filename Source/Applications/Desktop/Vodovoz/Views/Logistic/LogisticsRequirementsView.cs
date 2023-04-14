d28117201c97d302f1493f07097ba94fbc3fef4e
N-Sem <nikiforov.semen@gmail.com>
2023-04-14 16:06:38 +0300

    Добавлена новая вью с чекбоксами требований к водителям

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/LogisticsRequirementsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class LogisticsRequirementsView : Gtk.Bin
+	{
+		public LogisticsRequirementsView()
+		{
+			this.Build();
+		}
+	}
+}
