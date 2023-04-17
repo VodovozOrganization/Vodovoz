a3941358bad92537da67d3d33985e4b71d7e81ef
Sergey Efimov <efimov90@gmail.com>
2023-04-17 14:04:48 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/SidePanel/InfoViews/CarsMonitoringInfoPanelView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Vodovoz.SidePanel.InfoProviders;
+
+namespace Vodovoz.SidePanel.InfoViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarsMonitoringInfoPanelView : Gtk.Bin, IPanelView
+	{
+		public CarsMonitoringInfoPanelView()
+		{
+			Build();
+		}
+
+		public IInfoProvider InfoProvider { get; set; }
+
+		public bool VisibleOnPanel => true;
+
+		public void OnCurrentObjectChanged(object changedObject) => Refresh();
+
+		public void Refresh()
+		{
+			return;
+		}
+	}
+}
