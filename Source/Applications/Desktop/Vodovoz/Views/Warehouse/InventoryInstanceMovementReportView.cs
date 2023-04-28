260fb46cca5e2b4f76e8409106f9bea399b6db8a
Andrey Shaklein <as1854@yandex.ru>
2023-04-28 13:49:46 +0300

    4189_добавил вьюху и вьюмодель для нового отчета

---
+++ Source/Applications/Desktop/Vodovoz/Views/Warehouse/InventoryInstanceMovementReportView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Warehouses;
+
+namespace Vodovoz.Views.Warehouse
+{
+	public partial class InventoryInstanceMovementReportView : TabViewBase<InventoryInstanceMovementReportViewModel>
+	{
+		public InventoryInstanceMovementReportView(InventoryInstanceMovementReportViewModel viewModel) : base(viewModel)
+		{
+			Build();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Warehouses/InventoryInstanceMovementReportViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QS.Dialog;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Services.FileDialog;
+using QS.ViewModels;
+namespace Vodovoz.ViewModels.ViewModels.Warehouses
+{
+	public class InventoryInstanceMovementReportViewModel : DialogTabViewModelBase
+	{
+		public InventoryInstanceMovementReportViewModel(
+			IUnitOfWorkFactory uowFactory,
+			IInteractiveService interactiveService,
+			INavigationManager navigation,
+			IFileDialogService fileDialogService) :base(uowFactory, interactiveService, navigation)
+		{
+		}
+	}
+}
