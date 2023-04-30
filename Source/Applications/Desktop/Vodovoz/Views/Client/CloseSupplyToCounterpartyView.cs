ef0d6bec99b7b0c425e347d9ff60065ac3f7204a
Semen <nikiforov.semen@gmail.com>
2023-04-30 22:05:26 +0300

    Добавлена вью-модель для диалога

--- Source/Applications/Desktop/Vodovoz/Views/Client/CloseSupplyToCounterpartyView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Client/CloseSupplyToCounterpartyView.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using QS.Views.GtkUI;
+using System;
+using Vodovoz.ViewModels.Dialogs.Counterparty;
+
 namespace Vodovoz.Views.Client
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class CloseSupplyToCounterpartyView : Gtk.Bin
+	public partial class CloseSupplyToCounterpartyView : WidgetViewBase<CounterpartyFilesViewModel>
 	{
 		public CloseSupplyToCounterpartyView()
 		{
 			this.Build();
+		}
+
+		protected override void ConfigureWidget()
+		{
+			if(ViewModel == null)
+			{
+				return;
+			}
+
+
 		}
 	}
 }
---
+++ Source/Applications/Desktop/VodovozViewModels/Dialogs/Counterparty/CloseSupplyToCounterpartyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using QS.Tdi;
+using QS.ViewModels.Extension;
+using QS.ViewModels;
+using QS.DomainModel.UoW;
+using QS.Project.Services.FileDialog;
+using QS.Services;
+using Vodovoz.EntityRepositories;
+
+namespace Vodovoz.ViewModels.Dialogs.Counterparty
+{
+	public class CloseSupplyToCounterpartyViewModel : EntityWidgetViewModelBase<Domain.Client.Counterparty>, ITDICloseControlTab, IAskSaveOnCloseViewModel
+	{
+		public CloseSupplyToCounterpartyViewModel(
+			Domain.Client.Counterparty entity,
+			ICommonServices commonServices,
+			IUnitOfWork uow) : base(entity, commonServices)
+		{
+			UoW = uow ?? throw new System.ArgumentNullException(nameof(uow));
+		}
+		
+		public bool AskSaveOnClose => throw new System.NotImplementedException();
+
+		public bool CanClose()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
