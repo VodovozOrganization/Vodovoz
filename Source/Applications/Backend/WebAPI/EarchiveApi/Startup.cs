0aab8ed2c659ec3fce354918b134ca7fa625f6fa
Semen <nikiforov.semen@gmail.com>
2023-05-12 15:49:56 +0300

    Добавлено чтение параметров БД из конфигурации

--- Source/Applications/Backend/WebAPI/EarchiveApi/Startup.cs
+++ Source/Applications/Backend/WebAPI/EarchiveApi/Startup.cs
@@ -8,15 +8,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace EarchiveApi
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+
+			var connectionStrBuilder = new MySqlConnectionStringBuilder();
+
+			var domainDbConfig = Configuration.GetSection("DomainDB");
+
+			connectionStrBuilder.Server = domainDbConfig.GetValue<string>("Server");
+			connectionStrBuilder.Port = domainDbConfig.GetValue<uint>("Port");
+			connectionStrBuilder.Database = domainDbConfig.GetValue<string>("Database");
+			connectionStrBuilder.UserID = domainDbConfig.GetValue<string>("UserID");
+			connectionStrBuilder.Password = domainDbConfig.GetValue<string>("Password");
+			connectionStrBuilder.SslMode = MySqlSslMode.None;
+
+			var connectionString = connectionStrBuilder.GetConnectionString(true);
+
 			services.AddGrpc();
 		}
 
