de46bed49f1d7ee2edb39609e1bf99c0e11f9756
Sergey Efimov <efimov90@gmail.com>
2023-05-30 17:52:03 +0300

    Annotations

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Cash/FinancialCategoriesGroups/TargetDocument.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Cash/FinancialCategoriesGroups/TargetDocument.cs
@@ -1,9 +1,14 @@
-﻿namespace Vodovoz.Domain.Cash.FinancialCategoriesGroups
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Cash.FinancialCategoriesGroups
 {
 	public enum TargetDocument
 	{
+		[Display(Name = "Ордер")]
 		Invoice,
+		[Display(Name = "Ордер для самовывоза")]
 		SelfDelivery,
+		[Display(Name = "Ордер для документа перемещения ДС")]
 		Transfer
 	}
 }
