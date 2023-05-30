06b65903f9ca9e4aa5e02061cb18c1ee89b1510c
Sergey Efimov <efimov90@gmail.com>
2023-05-30 11:11:21 +0300

    Views

--- Source/Applications/Desktop/Vodovoz/Cash/FinancialCategoriesGroups/FinancialExpenseCategoryView.cs
+++ Source/Applications/Desktop/Vodovoz/Cash/FinancialCategoriesGroups/FinancialExpenseCategoryView.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using QS.Views.GtkUI;
+using System;
+using Vodovoz.ViewModels.Cash.FinancialCategoriesGroups;
+
 namespace Vodovoz.Cash.FinancialCategoriesGroups
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class FinancialExpenseCategoryView : Gtk.Bin
+	public partial class FinancialExpenseCategoryView : TabViewBase<FinancialExpenseCategoryViewModel>
 	{
-		public FinancialExpenseCategoryView()
+		public FinancialExpenseCategoryView(FinancialExpenseCategoryViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			labelIdValue.Binding
+				.AddSource(ViewModel.Entity)
+				.AddFuncBinding(ec => ec.Id != 0 ? ec.Id.ToString() : string.Empty, label => label.Text)
+				.InitializeFromSource();
+
+			yentryName.Binding
+				.AddBinding(ViewModel.Entity, e => e.Name, (widget) => widget.Text)
+				.InitializeFromSource();
+
+			yentryNumbering.Binding
+				.AddBinding(ViewModel.Entity, e => e.Numbering, (widget) => widget.Text)
+				.InitializeFromSource();
+
+			entryParentGroup.ViewModel = ViewModel.ParentFinancialCategoriesGroupViewModel;
+
+			entryParentGroup.Visible = false;
+
+			labelParent.Visible = false;
+
+			#region ParentEntityviewmodelentry
+			ParentEntityviewmodelentry.SetEntityAutocompleteSelectorFactory(ViewModel.ExpenseCategoryAutocompleteSelectorFactory);
+			ParentEntityviewmodelentry.Binding.AddBinding(ViewModel.Entity, s => s.Parent, w => w.Subject).InitializeFromSource();
+			ParentEntityviewmodelentry.CanEditReference = true;
+			#endregion
+
+			#region SubdivisionEntityviewmodelentry
+
+			SubdivisionEntityviewmodelentry.SetEntityAutocompleteSelectorFactory(ViewModel.SubdivisionAutocompleteSelectorFactory);
+			SubdivisionEntityviewmodelentry.Binding.AddBinding(ViewModel.Entity, s => s.Subdivision, w => w.Subject).InitializeFromSource();
+
+			#endregion
+
+			ycheckArchived.Binding.AddBinding(ViewModel, e => e.IsArchive, w => w.Active).InitializeFromSource();
+
+			yenumTypeDocument.ItemsEnum = typeof(ExpenseInvoiceDocumentType);
+			yenumTypeDocument.Binding.AddBinding(ViewModel.Entity, e => e.ExpenseDocumentType, w => w.SelectedItem).InitializeFromSource();
+
+			buttonSave.Clicked += (sender, e) => { ViewModel.SaveAndClose(); };
+			buttonCancel.Clicked += (sender, e) => { ViewModel.Close(true, QS.Navigation.CloseSource.Cancel); };
+
+			btnCopyEntityId1.Sensitive = ViewModel.Entity.Id > 0;
+			btnCopyEntityId1.Clicked += OnBtnCopyEntityIdClicked;
+		}
+
+		protected void OnBtnCopyEntityIdClicked(object sender, EventArgs e)
+		{
+			if(ViewModel.Entity.Id > 0)
+			{
+				GetClipboard(Selection.Clipboard).Text = ViewModel.Entity.Id.ToString();
+			}
 		}
 	}
 }
