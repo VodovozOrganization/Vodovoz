7d802ba23b7c71772857f7b3229750024917042d
Sergey Efimov <efimov90@gmail.com>
2023-06-02 19:17:41 +0300

    Право удаления

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Permissions/Logistic.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Permissions
+{
+	public static partial class Logistic
+	{
+		public static class RouteList
+		{
+			[Display(
+				Name = "Удаление заказов и маршрутных листов",
+				Description = "Пользователь может удалять заказы и маршрутные листы в журналах.")]
+			public static string CanDelete => "can_delete";
+		}
+	}
+}
