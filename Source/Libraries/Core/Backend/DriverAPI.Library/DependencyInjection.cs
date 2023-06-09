1f44c68354cff3c4c1706de1fd880547c125f23b
Sergey Efimov <efimov90@gmail.com>
2023-06-09 11:34:27 +0300

    Remove V1

--- Source/Libraries/Core/Backend/DriverAPI.Library/DependencyInjection.cs
+++ Source/Libraries/Core/Backend/DriverAPI.Library/DependencyInjection.cs
@@ -2,18 +2,6 @@
 using DriverAPI.Library.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
-using ActionTimeHelper = DriverAPI.Library.Helpers.ActionTimeHelper;
-using DeprecatedActionTimeHelper = DriverAPI.Library.Deprecated.Helpers.ActionTimeHelper;
-using DeprecatedOrderModel = DriverAPI.Library.Deprecated.Models.OrderModel;
-using DeprecatedTrackPointsModel = DriverAPI.Library.Deprecated.Models.TrackPointsModel;
-using IActionTimeHelper = DriverAPI.Library.Helpers.IActionTimeHelper;
-using IDeprecatedActionTimeHelper = DriverAPI.Library.Deprecated.Helpers.IActionTimeHelper;
-using IDeprecatedOrderModel = DriverAPI.Library.Deprecated.Models.IOrderModel;
-using IDeprecatedTrackPointsModel = DriverAPI.Library.Deprecated.Models.ITrackPointsModel;
-using IOrderModel = DriverAPI.Library.Models.IOrderModel;
-using ITrackPointsModel = DriverAPI.Library.Models.ITrackPointsModel;
-using OrderModel = DriverAPI.Library.Models.OrderModel;
-using TrackPointsModel = DriverAPI.Library.Models.TrackPointsModel;
 
 namespace DriverAPI.Library
 {
@@ -46,11 +34,6 @@ namespace DriverAPI.Library
 			services.AddScoped<ISmsPaymentModel, SmsPaymentModel>();
 			services.AddScoped<IDriverComplaintModel, DriverComplaintModel>();
 			services.AddScoped<IFastPaymentModel, FastPaymentModel>();
-
-			// Deprecated
-			services.AddScoped<IDeprecatedOrderModel, DeprecatedOrderModel>();
-			services.AddScoped<IDeprecatedTrackPointsModel, DeprecatedTrackPointsModel>();
-			services.AddScoped<IDeprecatedActionTimeHelper, DeprecatedActionTimeHelper>();
 
 			return services;
 		}
