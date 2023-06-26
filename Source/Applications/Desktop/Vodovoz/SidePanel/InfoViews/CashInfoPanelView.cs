d4c3d9fbc9f9ef2e78a4a83b61a97c596196253d
Sergey Efimov <efimov90@gmail.com>
2023-06-26 19:03:47 +0300

    Remove old filter

--- Source/Applications/Desktop/Vodovoz/SidePanel/InfoViews/CashInfoPanelView.cs
+++ Source/Applications/Desktop/Vodovoz/SidePanel/InfoViews/CashInfoPanelView.cs
@@ -57,48 +57,11 @@ namespace Vodovoz.SidePanel.InfoViews
 
 		public void Refresh()
 		{
-			if(InfoProvider is ICashInfoProvider infoProvider)
-			{
-				var filter = infoProvider.CashFilter;
-				labelInfo.Text = $"{GetAllCashSummaryInfo(filter)}";
-			}
-
 			if(InfoProvider is IDocumentsInfoProvider documentsInfoProvider)
 			{
 				var filter = documentsInfoProvider.DocumentsFilterViewModel;
 				labelInfo.Text = $"{GetAllCashSummaryInfo(filter)}";
 			}
-		}
-
-		private string GetAllCashSummaryInfo(CashDocumentsFilter filter)
-		{
-			if(filter == null)
-			{
-				return "";
-			}
-
-			decimal totalCash = 0;
-			var allCashString = "";
-			var distinctBalances = _cashRepository
-				.CurrentCashForGivenSubdivisions(_uow, filter.SelectedSubdivisions.Select(x => x.Id).ToArray()).ToList();
-
-			var inTransferring = _cashRepository.GetCashInTransferring(_uow);
-
-			if(filter.SelectedSubdivisions.Count() > 1)
-			{
-				distinctBalances = distinctBalances.OrderBy(x => _sortedSubdivisionsIds.IndexOf(x.Id)).ToList();
-			}
-
-			foreach(var node in distinctBalances)
-			{
-				totalCash += node.Balance;
-				allCashString += $"\r\n{node.Name}: {CurrencyWorks.GetShortCurrencyString(node.Balance)}";
-			}
-
-			var total = $"Денег в кассе: {CurrencyWorks.GetShortCurrencyString(totalCash)}. ";
-			var separatedCash = filter.SelectedSubdivisions.Any() ? $"\r\n\tИз них: {allCashString}" : "";
-			var cashInTransferringMessage = $"\n\nВ сейфе инкассатора: {CurrencyWorks.GetShortCurrencyString(inTransferring)}";
-			return total + separatedCash + cashInTransferringMessage;
 		}
 
 		private string GetAllCashSummaryInfo(DocumentsFilterViewModel filter)
