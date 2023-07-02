8c8d7f46862f3c01b43c50205d40ab1a166601ec
N-Sem <nikiforov.semen@gmail.com>
2023-07-02 20:20:39 +0300

    Добавлена вью выбора документов на переотправку

---
+++ Source/Applications/Desktop/Vodovoz/Views/Client/ResendCounterpartyEdoDocumentsView.cs
@@ -0,0 +1,14 @@
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Dialogs.Counterparty;
+
+namespace Vodovoz.Views.Client
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ResendCounterpartyEdoDocumentsView : TabViewBase<ResendCounterpartyEdoDocumentsViewModel>
+	{
+		public ResendCounterpartyEdoDocumentsView(ResendCounterpartyEdoDocumentsViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
