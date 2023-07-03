fe9ee46bc37cd5bdd067c00d23f78ba18178ae65
Semen <nikiforov.semen@gmail.com>
2023-07-03 22:12:22 +0300

    Изменена служба Taxcom

--- Source/Applications/Backend/WebAPI/TaxcomEdoApi/Controllers/TaxcomEdoController.cs
+++ Source/Applications/Backend/WebAPI/TaxcomEdoApi/Controllers/TaxcomEdoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using TaxcomEdoApi.Services;
 
 namespace TaxcomEdoApi.Controllers
 {
@@ -8,17 +10,27 @@ namespace TaxcomEdoApi.Controllers
 	public class TaxcomEdoController : ControllerBase
 	{
 		private readonly ILogger<TaxcomEdoController> _logger;
+		private readonly DocumentFlowService _documentFlowService;
 
-		public TaxcomEdoController(ILogger<TaxcomEdoController> logger)
+		public TaxcomEdoController(ILogger<TaxcomEdoController> logger, DocumentFlowService documentFlowService)
 		{
 			_logger = logger;
+			_documentFlowService = documentFlowService;
 		}
 
 		[HttpGet]
 		[Route("/Login")]
 		public void Login()
 		{
-			
+
+		}
+
+		[HttpGet]
+		[Route("/SendUpdByOrder")]
+		public async Task SendUpdByOrder(int orderId)
+		{
+			_logger.LogInformation("Запрос на формирование и отправку УПД для заказа №{OrderId}", orderId);
+			await _documentFlowService.CreateAndSendUpdByOrderAsync(orderId);
 		}
 	}
 }
