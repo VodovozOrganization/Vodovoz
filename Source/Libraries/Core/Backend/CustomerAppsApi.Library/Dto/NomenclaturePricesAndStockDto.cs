504da55ffd1699e70c1efbe76398c6f7df6dcdef
Andrey Shaklein <as1854@yandex.ru>
2023-07-07 16:40:34 +0300

    4409 добавил конвертер для перечислений в строки

--- Source/Libraries/Core/Backend/CustomerAppsApi.Library/Dto/NomenclaturePricesAndStockDto.cs
+++ Source/Libraries/Core/Backend/CustomerAppsApi.Library/Dto/NomenclaturePricesAndStockDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Vodovoz.Domain.Goods.NomenclaturesOnlineParameters;
 
 namespace CustomerAppsApi.Library.Dto
@@ -6,7 +7,9 @@ namespace CustomerAppsApi.Library.Dto
 	public class NomenclaturePricesAndStockDto
 	{
 		public int NomenclatureErpId { get; set; }
+		[JsonConverter(typeof(JsonStringEnumConverter))]
 		public NomenclatureOnlineAvailability? AvailableForSale { get; set; }
+		[JsonConverter(typeof(JsonStringEnumConverter))]
 		public NomenclatureOnlineMarker? Marker { get; set; }
 		public decimal? PercentDiscount { get; set; }
 		public IList<NomenclaturePricesDto> Prices { get; set; }
