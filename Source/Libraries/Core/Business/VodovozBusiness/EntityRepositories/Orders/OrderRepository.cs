a3acfd3089144129f47e198ae02c4a4e9e538a2f
Semen <nikiforov.semen@gmail.com>
2023-07-09 22:25:31 +0300

    Исправлен запрос получения списка заказов для формирования УПД

--- Source/Libraries/Core/Business/VodovozBusiness/EntityRepositories/Orders/OrderRepository.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/EntityRepositories/Orders/OrderRepository.cs
@@ -861,6 +861,7 @@ namespace Vodovoz.EntityRepositories.Orders
 			CounterpartyContract counterpartyContractAlias = null;
 			VodovozOrder orderAlias = null;
 			EdoContainer edoContainerAlias = null;
+			OrderEdoTrueMarkDocumentsActions orderEdoTrueMarkDocumentsActionsAlias = null;
 
 			var orderStatuses = new[] { OrderStatus.OnTheWay, OrderStatus.Shipped, OrderStatus.UnloadingOnStock, OrderStatus.Closed };
 
@@ -868,7 +869,9 @@ namespace Vodovoz.EntityRepositories.Orders
 				.Left.JoinAlias(o => o.Client, () => counterpartyAlias)
 				.JoinAlias(o => o.Contract, () => counterpartyContractAlias)
 				.JoinEntityAlias(() => edoContainerAlias,
-				() => orderAlias.Id == edoContainerAlias.Order.Id && edoContainerAlias.Type == Type.Upd, JoinType.LeftOuterJoin);
+				() => orderAlias.Id == edoContainerAlias.Order.Id && edoContainerAlias.Type == Type.Upd, JoinType.LeftOuterJoin)
+				.JoinEntityAlias(() => orderEdoTrueMarkDocumentsActionsAlias,
+				() => orderAlias.Id == orderEdoTrueMarkDocumentsActionsAlias.Order.Id, JoinType.LeftOuterJoin);
 
 			if(startDate.HasValue)
 			{
@@ -878,7 +881,10 @@ namespace Vodovoz.EntityRepositories.Orders
 			var result = query.Where(() => counterpartyAlias.PersonType == PersonType.legal)
 				.And(() => orderAlias.PaymentType == PaymentType.Cashless)
 				.And(() => counterpartyContractAlias.Organization.Id == organizationId)
-				.And(Restrictions.IsNull(Projections.Property(() => edoContainerAlias.Id)))
+				.And(Restrictions.Disjunction()
+					.Add(Restrictions.IsNull(Projections.Property(() => edoContainerAlias.Id)))
+					.Add(Restrictions.Eq(Projections.Property(() => orderEdoTrueMarkDocumentsActionsAlias.IsNeedToResendEdoUpd), true))
+				)
 				.And(Restrictions.Disjunction()
 					.Add(() => (counterpartyAlias.RegistrationInChestnyZnakStatus == RegistrationInChestnyZnakStatus.InProcess
 								|| counterpartyAlias.RegistrationInChestnyZnakStatus == RegistrationInChestnyZnakStatus.Registered)
@@ -886,7 +892,7 @@ namespace Vodovoz.EntityRepositories.Orders
 					.Add(() => counterpartyAlias.ReasonForLeaving == ReasonForLeaving.ForOwnNeeds
 						&& counterpartyAlias.ConsentForEdoStatus == ConsentForEdoStatus.Agree))
 				.WhereRestrictionOn(() => orderAlias.OrderStatus).IsIn(orderStatuses)
-				.TransformUsing(Transformers.RootEntity)
+				.TransformUsing(Transformers.DistinctRootEntity)
 				.List();
 
 			return result;
