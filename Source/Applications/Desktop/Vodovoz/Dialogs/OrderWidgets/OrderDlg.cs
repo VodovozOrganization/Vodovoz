7dbd19732ee202533ce1704c03e2fd375d1545c5
N-Sem <nikiforov.semen@gmail.com>
2023-07-13 12:52:35 +0300

    Добалвена привязка для отображенич поддтипа терминала в диалоге заказа

--- Source/Applications/Desktop/Vodovoz/Dialogs/OrderWidgets/OrderDlg.cs
+++ Source/Applications/Desktop/Vodovoz/Dialogs/OrderWidgets/OrderDlg.cs
@@ -711,6 +711,14 @@ namespace Vodovoz
 			ySpecPaymentFrom.ItemsList = paymentFromItemsQuery.List();
 			ySpecPaymentFrom.Binding.AddBinding(Entity, e => e.PaymentByCardFrom, w => w.SelectedItem).InitializeFromSource();
 
+			yenumcomboboxTerminalSubtype.ItemsEnum = typeof(PaymentByTerminalSource);
+			yenumcomboboxTerminalSubtype.Sensitive = false;
+			yenumcomboboxTerminalSubtype.Binding
+				.AddSource(Entity)
+				.AddBinding(s => s.PaymentByTerminalSource, w => w.SelectedItem)
+				.AddFuncBinding(s => s.PaymentType == PaymentType.Terminal, w => w.Visible)
+				.InitializeFromSource();
+
 			enumTax.ItemsEnum = typeof(TaxType);
 			Enum[] hideTaxTypeEnums = { TaxType.None };
 			enumTax.AddEnumToHideList(hideTaxTypeEnums);
