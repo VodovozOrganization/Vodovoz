9022f8534a4bde39f11ced85a5941d2481f4c02a
Sergey Efimov <efimov90@gmail.com>
2023-07-14 11:37:48 +0300

    Право генерации отчета ДДС

--- Source/Libraries/Core/Business/VodovozBusiness/Permissions/Cash.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Permissions/Cash.cs
@@ -1,7 +1,15 @@
 ﻿namespace Vodovoz.Permissions
 {
+	/// <summary>
+	/// Права кассы
+	/// </summary>
 	public static partial class Cash
 	{
 		internal static string CanEditExpenseAndIncomeDate => "can_edit_cash_income_expense_date";
+
+		/// <summary>
+		/// Доступен отчет ДДС
+		/// </summary>
+		public static string CanGenerateCashFlowDdsReport => "can_generate_cash_flow_dds_report";
 	}
 }
