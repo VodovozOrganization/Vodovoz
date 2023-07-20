7fa6ec37735ffcb2fb441ea9a4897f3f4dfc4d70
N-Sem <nikiforov.semen@gmail.com>
2023-07-20 13:50:20 +0300

    Добавлен новый диалог переноса заказа

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/FastDeliveryOrderTransfer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FastDeliveryOrderTransfer : Gtk.Bin
+	{
+		public FastDeliveryOrderTransfer()
+		{
+			this.Build();
+		}
+	}
+}
