9062db9d06efa45d3903f6f70396d95c16d9491e
Sergey Efimov <efimov90@gmail.com>
2023-07-20 19:08:08 +0300

    Рефакторинг (не работают группы как надо)

--- Source/Applications/Desktop/Vodovoz/ReportsParameters/CashFlow.CashFlowDdsReportRenderer.cs
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/CashFlow.CashFlowDdsReportRenderer.cs
@@ -1,7 +1,6 @@
 ﻿using ClosedXML.Excel;
 using System;
 using System.Linq;
-using Vodovoz.Domain.Cash;
 
 namespace Vodovoz.Reports
 {
@@ -9,11 +8,33 @@ namespace Vodovoz.Reports
 	{
 		public class CashFlowDdsReportRenderer
 		{
-			private readonly XLColor _subTotalsBGColor = XLColor.FromArgb(253, 233, 216);
+			private readonly XLColor _subTotalsBGColor = XLColor.FromArgb(249, 191, 143);
+
+			private const int _splitterRowsCount = 1;
+
+			private const string _headerTitle = "Отчет по бюджету";
+			private const int _headerRow = 1;
+			private const int _headerFontSize = 13;
+
+			private const int _subHeaderRow = 2;
+			private const int _subHeaderFontSize = 13;
 
 			private const int _mainCategoryColumn = 1;
 			private const int _categoryTitleColumn = 2;
-			private const int _categoryMoney = 3;
+			private const int _categoryFontSize = 13;
+			private const int _moneyColumn = 3;
+
+			private const string _moneyNumericFormat = "# ### ### ##0.00;-# ### ### ##0.00";
+
+			private const string _incomesHeaderTitle = "Доходы";
+			private const int _incomesHeaderFontSize = 13;
+
+			private const string _expensesHeaderTitle = "Расходы";
+			private const int _expensesHeaderFontSize = 13;
+
+			private const string _totalHeaderTitle = "Прибыль";
+
+			private const int _totalHeaderFontSize = 13;
 
 			public IXLWorkbook Render(CashFlowDdsReport cashFlowDdsReport)
 			{
@@ -23,136 +44,182 @@ namespace Vodovoz.Reports
 
 				var reportSheet = result.Worksheets.First();
 
-				var firstCell = reportSheet.Cell("A1");
-				firstCell.Value = "Отчет по бюджету";
-				firstCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-				firstCell.Style.Font.Bold = true;
-				firstCell.Style.Font.FontSize = 13;
-				reportSheet.Range("A1:C1").Row(1).Merge();
+				RenderHeader(reportSheet);
 
-				var secondCell = reportSheet.Cell("A2");
-				secondCell.Value = $"с {cashFlowDdsReport.StartDate:dd.MM.yyyy} по {cashFlowDdsReport.EndDate:dd.MM.yyyy}";
-				secondCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-				secondCell.Style.Font.Bold = true;
-				secondCell.Style.Font.FontSize = 13;
-				reportSheet.Range("A2:C2").Row(1).Merge();
+				RenderSubheader(reportSheet, cashFlowDdsReport.StartDate, cashFlowDdsReport.EndDate);
 
-				reportSheet.Cell("B4").Active = true;
+				var incomesStartLine = 4;
 
-				var summaryRow = reportSheet.ActiveCell.Address.RowNumber;
+				var incomesSum = cashFlowDdsReport.IncomesGroupLines.Sum(x => x.Money);
 
-				var summaryCell = reportSheet.Cell(summaryRow, 2);
-				summaryCell.Value = "Прибыль";
-				summaryCell.Style.Font.Bold = true;
-				summaryCell.Style.Font.FontSize = 13;
+				var insertedIncomesGroupRows = RenderIncomesLines(reportSheet, cashFlowDdsReport, incomesStartLine, incomesSum);
 
-				var summaryTotalCell = reportSheet.Cell(summaryRow, 3);
-				summaryTotalCell.Style.Font.Bold = true;
-				summaryTotalCell.Style.Font.FontSize = 13;
+				var expensesSum = cashFlowDdsReport.ExpensesGroupLines.Sum(x => x.Money);
 
-				GoToNextRow(reportSheet);
+				int insertedExpensesLines = RenderExpensesLines(reportSheet, cashFlowDdsReport, incomesStartLine + insertedIncomesGroupRows + 1, expensesSum);
 
-				GoToNextRow(reportSheet);
+				RenderSubSubheader(
+					reportSheet,
+					incomesStartLine + insertedIncomesGroupRows + insertedExpensesLines + 2 * _splitterRowsCount,
+					_totalHeaderTitle,
+					_totalHeaderFontSize,
+					incomesSum - expensesSum);
 
-				var incomesStartLine = reportSheet.ActiveCell.Address.RowNumber;
+				reportSheet.Column(_mainCategoryColumn).AdjustToContents();
+				reportSheet.Column(_categoryTitleColumn).AdjustToContents();
+				reportSheet.Column(_moneyColumn).AdjustToContents();
 
-				var incomesCell = reportSheet.Cell(incomesStartLine, _mainCategoryColumn);
+				//reportSheet.CollapseRows();
 
-				reportSheet.Range(incomesStartLine, 1, incomesStartLine, 3).AddConditionalFormat().WhenNotBlank().Fill.BackgroundColor = _subTotalsBGColor;
-				reportSheet.Range(incomesStartLine, 1, incomesStartLine, 3).AddConditionalFormat().WhenIsBlank().Fill.BackgroundColor = _subTotalsBGColor;
+				return result;
+			}
 
-				incomesCell.Style.Font.Bold = true;
-				incomesCell.Value = "Доходы";
+			private int RenderIncomesLines(IXLWorksheet reportSheet, CashFlowDdsReport cashFlowDdsReport, int row, decimal incomesSum)
+			{
+				RenderSubSubheader(
+					reportSheet,
+					row,
+					_incomesHeaderTitle,
+					_incomesHeaderFontSize,
+					incomesSum);
+
+				var incomegroupsStartAt = row;
+
+				var insertedIncomesGroupRows = 0;
 
 				foreach(var incomeGroup in cashFlowDdsReport.IncomesGroupLines)
 				{
-					RenderIncomeGroup(
+					insertedIncomesGroupRows += RenderIncomeGroup(
 						reportSheet,
+						incomegroupsStartAt + insertedIncomesGroupRows,
 						incomeGroup,
 						1,
 						string.Empty);
 				}
 
-				GoToNextRow(reportSheet);
+				return insertedIncomesGroupRows;
+			}
 
-				var expensesStartLine = reportSheet.ActiveCell.Address.RowNumber;
+			private int RenderExpensesLines(IXLWorksheet reportSheet, CashFlowDdsReport cashFlowDdsReport, int row, decimal sum)
+			{
+				RenderSubSubheader(
+					reportSheet,
+					row,
+					_expensesHeaderTitle,
+					_expensesHeaderFontSize,
+					sum);
 
-				var expenseCell = reportSheet.Cell(expensesStartLine, _mainCategoryColumn);
+				var expensesStartAt = row;
 
-				reportSheet.Range(expensesStartLine, 1, expensesStartLine, 3).AddConditionalFormat().WhenNotBlank().Fill.BackgroundColor = _subTotalsBGColor;
-				reportSheet.Range(expensesStartLine, 1, expensesStartLine, 3).AddConditionalFormat().WhenIsBlank().Fill.BackgroundColor = _subTotalsBGColor;
-
-				expenseCell.Style.Font.Bold = true;
-				expenseCell.Value = "Расходы";
+				var insertedExpensesGroupRows = 0;
 
 				foreach(var expenseGroup in cashFlowDdsReport.ExpensesGroupLines)
 				{
-					RenderExpenseGroup(
+					insertedExpensesGroupRows += RenderExpenseGroup(
 						reportSheet,
+						expensesStartAt + insertedExpensesGroupRows,
 						expenseGroup,
 						1,
 						string.Empty);
 				}
 
-				reportSheet.Column(1).AdjustToContents();
-				reportSheet.Column(2).AdjustToContents();
-				reportSheet.Column(3).AdjustToContents();
-
-				RenderMoney(reportSheet, summaryRow, cashFlowDdsReport.IncomesGroupLines.Sum(x => x.Money) - cashFlowDdsReport.ExpensesGroupLines.Sum(x => x.Money), true);
-
-				reportSheet.CollapseRows();
-
-				return result;
+				return insertedExpensesGroupRows;
 			}
 
-			private void RenderIncomeGroup(
+			private void RenderSubSubheader(IXLWorksheet xLWorksheet, int row, string title, int fontSize, decimal money, bool bold = true)
+			{
+				var subheaderCell = xLWorksheet.Cell(row, _mainCategoryColumn);
+
+				ApplyBGColorConditionalFormat(
+					xLWorksheet.Range(row, _mainCategoryColumn, row, _moneyColumn),
+					_subTotalsBGColor);
+
+				subheaderCell.Style.Font.Bold = bold;
+				subheaderCell.Value = title;
+
+				RenderMoney(xLWorksheet,
+					row,
+					money,
+					fontSize,
+					bold);
+			}
+
+			private void RenderHeader(IXLWorksheet xLWorksheet)
+			{
+				var firstCell = xLWorksheet.Cell(_headerRow, 1);
+				firstCell.Value = _headerTitle;
+				firstCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+				firstCell.Style.Font.Bold = true;
+				firstCell.Style.Font.FontSize = _headerFontSize;
+				xLWorksheet.Range(_headerRow, 1, _headerRow, 3).Merge();
+			}
+
+			private void RenderSubheader(IXLWorksheet reportSheet, DateTime startDate, DateTime endDate)
+			{
+				var subheaderFirstCell = reportSheet.Cell(_subHeaderRow, 1);
+				subheaderFirstCell.Value = $"с {startDate:dd.MM.yyyy} по {endDate:dd.MM.yyyy}";
+				subheaderFirstCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+				subheaderFirstCell.Style.Font.Bold = true;
+				subheaderFirstCell.Style.Font.FontSize = _subHeaderFontSize;
+				reportSheet.Range(_subHeaderRow, 1, _subHeaderRow, 3).Merge();
+			}
+
+			private void ApplyBGColorConditionalFormat(IXLRange range, XLColor color)
+			{
+				range.AddConditionalFormat().WhenNotBlank().Fill.BackgroundColor = color;
+				range.AddConditionalFormat().WhenIsBlank().Fill.BackgroundColor = color;
+			}
+
+			private int RenderIncomeGroup(
 				IXLWorksheet xLWorksheet,
+				int startRow,
 				CashFlowDdsReport.IncomesGroupLine incomeGroup,
 				int groupLevel,
 				string numberPrefix)
 			{
-				var activeAtStartCellRow = xLWorksheet.ActiveCell.Address.RowNumber;
+				var inserted = 0;
 
 				if(groupLevel > 1)
 				{
-					RenderGroupTitle(xLWorksheet, xLWorksheet.ActiveCell.Address.RowNumber, incomeGroup.Title, numberPrefix);
+					RenderGroupTitle(xLWorksheet, startRow, incomeGroup.Title, numberPrefix, incomeGroup.Money);
 				}
 
-				GoToNextRow(xLWorksheet);
-
-				var categoriesEndRowNumber = incomeGroup.IncomeCategories.Count + activeAtStartCellRow;
-
-				if(incomeGroup.IncomeCategories.Any())
-				{
-					foreach(var category in incomeGroup.IncomeCategories)
-					{
-						RenderIncomeCategory(category, xLWorksheet);
-					}
-				}
+				inserted++;
 
 				var incomeGroupsCount = incomeGroup.Groups.Count;
 
-				if(incomeGroup.Groups.Any())
+				if(incomeGroupsCount != 0)
 				{
 					for(int i = 0; i < incomeGroupsCount; i++)
 					{
-						RenderIncomeGroup(xLWorksheet, incomeGroup.Groups[i], groupLevel + 1, $"{numberPrefix}{i + 1}. ");
+						inserted += RenderIncomeGroup(xLWorksheet, startRow + inserted, incomeGroup.Groups[i], groupLevel + 1, $"{numberPrefix}{i + 1}. ");
 					}
 				}
 
-				int groupEndRowNumber = xLWorksheet.ActiveCell.Address.RowNumber - 1;
+				int groupEndRowNumber = startRow + inserted;
 
-				var categoriesStartRowNumber = activeAtStartCellRow + 1;
+				var categoriesEndRowNumber = incomeGroup.IncomeCategories.Count + startRow + inserted;
 
-				if(incomeGroup.Groups.Any())
+				if(incomeGroup.IncomeCategories.Count != 0)
+				{
+					foreach(var category in incomeGroup.IncomeCategories)
+					{
+						RenderIncomeCategory(xLWorksheet, startRow + inserted, category);
+						inserted++;
+					}
+				}
+
+				var categoriesStartRowNumber = groupEndRowNumber + 1;
+
+				if(incomeGroup.Groups.Count != 0)
 				{
 					if(groupLevel > 1)
 					{
-						xLWorksheet.Rows(categoriesStartRowNumber, groupEndRowNumber).Group();
+						xLWorksheet.Rows(startRow + 1, categoriesEndRowNumber).Group();
 					}
 				}
 
-				if(incomeGroup.IncomeCategories.Any())
+				if(incomeGroup.IncomeCategories.Count != 0)
 				{
 					if(groupLevel > 1)
 					{
@@ -160,57 +227,59 @@ namespace Vodovoz.Reports
 					}
 				}
 
-				RenderMoney(xLWorksheet, activeAtStartCellRow, incomeGroup.Money, true);
+				return inserted;
 			}
 
-			private void RenderExpenseGroup(
+			private int RenderExpenseGroup(
 				IXLWorksheet xLWorksheet,
+				int startRow,
 				CashFlowDdsReport.ExpensesGroupLine expenseGroup,
 				int groupLevel,
 				string numberPrefix)
 			{
-				var activeAtStartCellRow = xLWorksheet.ActiveCell.Address.RowNumber;
+				var inserted = 0;
 
 				if(groupLevel > 1)
 				{
-					RenderGroupTitle(xLWorksheet, xLWorksheet.ActiveCell.Address.RowNumber, expenseGroup.Title, numberPrefix);
+					RenderGroupTitle(xLWorksheet, startRow, expenseGroup.Title, numberPrefix, expenseGroup.Money);
 				}
 
-				GoToNextRow(xLWorksheet);
+				inserted++;
 
 				var expenseGroupsCount = expenseGroup.Groups.Count;
 
-				if(expenseGroup.Groups.Any())
+				if(expenseGroupsCount != 0)
 				{
 					for(int i = 0; i < expenseGroupsCount; i++)
 					{
-						RenderExpenseGroup(xLWorksheet, expenseGroup.Groups[i], groupLevel + 1, $"{numberPrefix}{i + 1}. ");
+						inserted += RenderExpenseGroup(xLWorksheet, startRow + inserted, expenseGroup.Groups[i], groupLevel + 1, $"{numberPrefix}{i + 1}. ");
 					}
 				}
+				
+				int groupEndRowNumber = startRow + inserted;
 
-				var categoriesEndRowNumber = expenseGroup.ExpenseCategories.Count + activeAtStartCellRow;
+				var categoriesEndRowNumber = expenseGroup.ExpenseCategories.Count + startRow + inserted;
 
-				if(expenseGroup.ExpenseCategories.Any())
+				if(expenseGroup.ExpenseCategories.Count != 0)
 				{
 					foreach(var category in expenseGroup.ExpenseCategories)
 					{
-						RenderExpenseCategory(category, xLWorksheet);
+						RenderExpenseCategory(xLWorksheet, startRow + inserted, category);
+						inserted++;
 					}
 				}
 
-				int groupEndRowNumber = xLWorksheet.ActiveCell.Address.RowNumber - 1;
+				var categoriesStartRowNumber = groupEndRowNumber + 1;
 
-				var categoriesStartRowNumber = activeAtStartCellRow + 1;
-
-				if(expenseGroup.Groups.Any())
+				if(expenseGroup.Groups.Count != 0)
 				{
 					if(groupLevel > 1)
 					{
-						xLWorksheet.Rows(categoriesStartRowNumber, groupEndRowNumber).Group();
+						xLWorksheet.Rows(startRow + 1, categoriesEndRowNumber).Group();
 					}
 				}
 
-				if(expenseGroup.ExpenseCategories.Any())
+				if(expenseGroup.ExpenseCategories.Count != 0)
 				{
 					if(groupLevel > 1)
 					{
@@ -218,23 +287,23 @@ namespace Vodovoz.Reports
 					}
 				}
 
-				RenderMoney(xLWorksheet, activeAtStartCellRow, expenseGroup.Money, true);
+				return inserted;
 			}
 
 			private void RenderIncomeCategory(
-				CashFlowDdsReport.FinancialIncomeCategoryLine category,
-				IXLWorksheet xLWorksheet)
+				IXLWorksheet xLWorksheet,
+				int row,
+				CashFlowDdsReport.FinancialIncomeCategoryLine category)
 			{
-				RenderCategoryRow(xLWorksheet, xLWorksheet.ActiveCell.Address.RowNumber, category.Title, category.Money);
-				GoToNextRow(xLWorksheet);
+				RenderCategoryRow(xLWorksheet, row, category.Title, category.Money);
 			}
 
 			private void RenderExpenseCategory(
-				CashFlowDdsReport.FinancialExpenseCategoryLine category,
-				IXLWorksheet xLWorksheet)
+				IXLWorksheet xLWorksheet,
+				int row,
+				CashFlowDdsReport.FinancialExpenseCategoryLine category)
 			{
-				RenderCategoryRow(xLWorksheet, xLWorksheet.ActiveCell.Address.RowNumber, category.Title, category.Money);
-				GoToNextRow(xLWorksheet);
+				RenderCategoryRow(xLWorksheet, row, category.Title, category.Money);
 			}
 
 			private void GoToNextRow(IXLWorksheet xLWorksheet)
@@ -244,11 +313,13 @@ namespace Vodovoz.Reports
 					xLWorksheet.ActiveCell.Address.ColumnNumber).Active = true;
 			}
 
-			private void RenderGroupTitle(IXLWorksheet xLWorksheet, int row, string title, string numberPrefix)
+			private void RenderGroupTitle(IXLWorksheet xLWorksheet, int row, string title, string numberPrefix, decimal money)
 			{
 				var leveledCell = xLWorksheet.Cell(row, _categoryTitleColumn);
 				leveledCell.Style.Font.Bold = true;
 				leveledCell.Value = $"{numberPrefix}{title}";
+
+				RenderMoney(xLWorksheet, row, money, _categoryFontSize, true);
 			}
 
 			private void RenderCategoryRow(IXLWorksheet xLWorksheet, int row, string title, decimal money, bool right = false)
@@ -262,20 +333,21 @@ namespace Vodovoz.Reports
 					titleCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
 				}
 
-				RenderMoney(xLWorksheet, row, money);
+				RenderMoney(xLWorksheet, row, money, _categoryFontSize);
 			}
 
-			private void RenderMoney(IXLWorksheet xLWorksheet, int row, decimal money, bool bold = false)
+			private void RenderMoney(IXLWorksheet xLWorksheet, int row, decimal money, int fontSize, bool bold = false)
 			{
-				var moneyCell = xLWorksheet.Cell(row, _categoryMoney);
+				var moneyCell = xLWorksheet.Cell(row, _moneyColumn);
 
 				if(bold)
 				{
 					moneyCell.Style.Font.Bold = true;
 				}
 
-				moneyCell.Style.NumberFormat.Format = "# ### ### ##0.00 ₽;-# ### ### ##0.00 ₽";
+				moneyCell.Style.NumberFormat.Format = _moneyNumericFormat;
 				moneyCell.DataType = XLDataType.Number;
+				moneyCell.Style.Font.FontSize = fontSize;
 				moneyCell.Value = money;
 			}
 		}
