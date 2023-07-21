064e0901718d4efe131ce630619d06a6b41a50a4
Semen <nikiforov.semen@gmail.com>
2023-07-21 12:39:16 +0300

    DataNode инфо-панели выделен в отдельный файл

---
+++ Source/Applications/Desktop/Vodovoz/SidePanel/InfoViews/CarsMonitoringInfoPanelView.DataNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vodovoz.SidePanel.InfoViews
+{
+	public partial class CarsMonitoringInfoPanelView
+	{
+		sealed class DataNode
+		{
+			public string DriverName { get; set; }
+			public int RouteListId { get; set; }
+			public string CarNumber { get; set; }
+			public string Address { get; set; }
+			public int RouteListItemId { get; set; }
+			public bool IsFastDeliveryOrder { get; set; }
+			public string DeliveryType { get; set; }
+			public DateTime DeliveryBefore { get; set; }
+		}
+	}
+}
--- Source/Applications/Desktop/Vodovoz/SidePanel/InfoViews/CarsMonitoringInfoPanelView.cs
+++ Source/Applications/Desktop/Vodovoz/SidePanel/InfoViews/CarsMonitoringInfoPanelView.cs
@@ -371,18 +371,6 @@ namespace Vodovoz.SidePanel.InfoViews
 			base.Destroy();
 		}
 
-		internal class DataNode
-		{
-			public string DriverName { get; set; }
-			public int RouteListId { get; set; }
-			public string CarNumber { get; set; }
-			public string Address { get; set; }
-			public int RouteListItemId { get; set; }
-			public bool IsFastDeliveryOrder { get; set; }
-			public string DeliveryType { get; set; }
-			public DateTime DeliveryBefore { get; set; }
-		}
-
 		public class FastDeliveryMonitoringNode
 		{
 			public string Column1 { get; set; }
