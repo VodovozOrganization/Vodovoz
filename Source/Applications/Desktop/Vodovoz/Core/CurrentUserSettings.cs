5081e03b0e35ec829f2b840e60aa3efd50f2e1ef
Vadim Gordeev <enzogord@gmail.com>
2023-07-27 19:33:04 +0300

    Перенос из библиотеки

--- Source/Applications/Desktop/Vodovoz/Core/CurrentUserSettings.cs
+++ Source/Applications/Desktop/Vodovoz/Core/CurrentUserSettings.cs
@@ -1,4 +1,8 @@
-﻿using QSOrmProject.Users;
+﻿using QS.DomainModel.Entity;
+using QS.DomainModel.UoW;
+using QSOrmProject;
+using System;
+using System.Linq;
 using Vodovoz.Domain.Employees;
 using Vodovoz.EntityRepositories;
 using Vodovoz.Services;
@@ -27,6 +31,80 @@ namespace Vodovoz
 	public class UserSettingsGetter : IUserSettings
 	{
 		public UserSettings Settings => CurrentUserSettings.Settings;
+	}
+
+
+	public class UserSettingsManager<TUserSettings> where TUserSettings : IDomainObject
+	{
+		static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public Func<IUnitOfWork, TUserSettings> LoadUserSettings;
+		public Func<IUnitOfWork, TUserSettings> CreateUserSettings;
+
+		public UserSettingsManager()
+		{
+		}
+
+		IUnitOfWork uow;
+
+		IUnitOfWork UoW
+		{
+			get
+			{
+				if(uow == null || !uow.IsAlive)
+					uow = UnitOfWorkFactory.CreateWithoutRoot();
+				return uow;
+			}
+		}
+
+		TUserSettings settings;
+		public TUserSettings Settings
+		{
+			get
+			{
+				if(settings != null && UoW != null && UoW.IsAlive)
+					return settings;
+
+				ReloadSettings();
+
+				var map = OrmMain.GetObjectDescription<TUserSettings>();
+				if(map != null)
+				{
+					map.ObjectUpdatedGeneric -= Map_ObjectUpdatedGeneric;
+					map.ObjectUpdatedGeneric += Map_ObjectUpdatedGeneric;
+				}
+				else
+					logger.Warn($"Класс {typeof(TUserSettings)} не добавлен в {nameof(OrmMain.AddObjectDescription)}, поэтому подписка на измененя настроек не возможна.");
+
+				return settings;
+			}
+		}
+
+		private void ReloadSettings()
+		{
+			logger.Info("Обновляем настройки пользователя...");
+			settings = LoadUserSettings(UoW);
+
+			if(settings == null)
+			{
+				logger.Info("Настроек пользователя нет, создаем новые.");
+				settings = CreateUserSettings(UoW);
+				SaveSettings();
+			}
+		}
+
+		void Map_ObjectUpdatedGeneric(object sender, QSOrmProject.UpdateNotification.OrmObjectUpdatedGenericEventArgs<TUserSettings> e)
+		{
+			if(e.UpdatedSubjects.Any(x => x.Id == Settings.Id))
+				UoW.Session.Refresh(Settings);
+		}
+
+		public void SaveSettings()
+		{
+			UoW.Save(settings);
+			UoW.Commit();
+		}
+
 	}
 }
 
