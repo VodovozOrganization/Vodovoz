b35f02611c6b0ac3ae2ad07886b40de5a84d3d8e
Vadim Gordeev <enzogord@gmail.com>
2023-07-31 19:23:37 +0300

    Загрузка всего списка настроек по таймауту любой из настроек

---
+++ Source/Libraries/Core/Business/Vodovoz.Settings/SettingException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Vodovoz.Settings
+{
+	public class SettingException : Exception
+	{
+		public SettingException()
+		{
+		}
+
+		public SettingException(string message) : base(message)
+		{
+		}
+
+		public SettingException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected SettingException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
