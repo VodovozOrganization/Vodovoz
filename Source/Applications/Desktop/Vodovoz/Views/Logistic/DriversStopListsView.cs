078228994af96456186f33c886af4902ee9bacd3
N-Sem <nikiforov.semen@gmail.com>
2023-08-03 18:16:01 +0300

    Добавлен диалог снятия стоп-листов

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/DriversStopListsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DriversStopListsView : Gtk.Bin
+	{
+		public DriversStopListsView()
+		{
+			this.Build();
+		}
+	}
+}
