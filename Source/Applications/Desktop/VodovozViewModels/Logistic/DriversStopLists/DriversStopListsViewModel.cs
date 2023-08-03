32fe6e6452fc19ac9207536f2c4ba660731495a3
Semen <nikiforov.semen@gmail.com>
2023-08-03 16:51:18 +0300

    Добавлена вью-модель нового диалога

---
+++ Source/Applications/Desktop/VodovozViewModels/Logistic/DriversStopLists/DriversStopListsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using QS.Dialog;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.ViewModels;
+
+namespace Vodovoz.ViewModels.Logistic.DriversStopLists
+{
+	public class DriversStopListsViewModel : DialogTabViewModelBase
+	{
+		public DriversStopListsViewModel(
+			IUnitOfWorkFactory unitOfWorkFactory,
+			IInteractiveService interactiveService,
+			INavigationManager navigation
+			) : base(unitOfWorkFactory, interactiveService, navigation)
+		{
+			
+		}
+	}
+}
