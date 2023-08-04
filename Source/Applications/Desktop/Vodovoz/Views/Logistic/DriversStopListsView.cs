f2e61b290a7cea8df95659b3c0d1a6a98125a1d3
Semen <nikiforov.semen@gmail.com>
2023-08-04 18:41:40 +0300

    Добавлены привязки во вью диалога стоп-листов

--- Source/Applications/Desktop/Vodovoz/Views/Logistic/DriversStopListsView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/DriversStopListsView.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using Gamma.ColumnConfig;
+using QS.Views.GtkUI;
+using QSProjectsLib;
+using Vodovoz.Domain.Employees;
+using Vodovoz.Domain.Logistic;
+using Vodovoz.Domain.Logistic.Cars;
+using Vodovoz.ViewModels.Logistic.DriversStopLists;
+using static Vodovoz.ViewModels.Logistic.DriversStopLists.DriversStopListsViewModel;
+
 namespace Vodovoz.Views.Logistic
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class DriversStopListsView : Gtk.Bin
+	public partial class DriversStopListsView : TabViewBase<DriversStopListsViewModel>
 	{
-		public DriversStopListsView()
+		public DriversStopListsView(DriversStopListsViewModel viewModel) : base(viewModel)
 		{
 			this.Build();
+			ConfigureDlg();
+		}
+
+		private void ConfigureDlg()
+		{
+			yenumcomboStatus.ItemsEnum = typeof(EmployeeStatus);
+			yenumcomboStatus.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.FilterEmployeeStatus, w => w.SelectedItemOrNull)
+				.InitializeFromSource();
+
+			yenumcomboCarTypeOfUse.ItemsEnum = typeof(CarTypeOfUse);
+			yenumcomboCarTypeOfUse.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.FilterCarTypeOfUse, w => w.SelectedItemOrNull)
+				.InitializeFromSource();
+
+			yenumcomboCarOwnType.ItemsEnum = typeof(CarOwnType);
+			yenumcomboCarOwnType.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.FilterCarOwnType, w => w.SelectedItemOrNull)
+				.InitializeFromSource();
+
+			ytreeviewCurrent.ColumnsConfig = FluentColumnsConfig<DriverNode>
+				.Create()
+					.AddColumn("Водитель").AddTextRenderer(x => x.DriverFullName)
+					.AddColumn("Автомобиль").AddTextRenderer(x => x.CarRegistrationNumber)
+					.AddColumn("Общийд долг по МЛ").AddTextRenderer(x => x.RouteListsDebtsSum.ToShortCurrencyString())
+					.AddColumn("Кол-во незакрытых МЛ").AddTextRenderer(x => x.UnclosedRouteListsWithDebtCount.ToString())
+					.AddColumn("Стоп-лист").AddToggleRenderer(d => d.IsDriverInStopList).Editing(false)
+				.Finish();
+
+			ytreeviewCurrent.Binding.AddBinding(ViewModel, vm => vm.CurrentDriversList, w => w.ItemsDataSource).InitializeFromSource();
+
+			ytreeviewHystory.ColumnsConfig = FluentColumnsConfig<DriverStopListRemoval>
+				.Create()
+					.AddColumn("Водитель").AddTextRenderer(x => x.Driver.FullName)
+					.AddColumn("Снят с").AddTextRenderer(x => x.DateFrom.ToString("dd.MM.yyyy HH:mm"))
+					.AddColumn("Снят по").AddTextRenderer(x => x.DateTo.ToString("dd.MM.yyyy HH:mm"))
+					.AddColumn("Комментарий").AddTextRenderer(x => x.Comment)
+					.AddColumn("ФИО снявшего").AddTextRenderer(x => x.Author.FullName)
+				.Finish();
+
+			ytreeviewHystory.Binding.AddBinding(ViewModel, vm => vm.StopListsRemovalHistory, w => w.ItemsDataSource).InitializeFromSource();
 		}
 	}
 }
