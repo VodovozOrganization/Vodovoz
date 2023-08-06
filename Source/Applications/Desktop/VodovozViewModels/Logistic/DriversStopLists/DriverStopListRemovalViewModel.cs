6bebcb4432fbc1794c46e91ad9f53c9f54dd25ac
Semen <nikiforov.semen@gmail.com>
2023-08-06 17:48:11 +0300

    Добавлена вью-модель диалога снятия стоп-листа

---
+++ Source/Applications/Desktop/VodovozViewModels/Logistic/DriversStopLists/DriverStopListRemovalViewModel.cs
@@ -0,0 +1,18 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace Vodovoz.ViewModels.Logistic.DriversStopLists
+{
+	public class DriverStopListRemovalViewModel : UowDialogViewModelBase
+	{
+		public DriverStopListRemovalViewModel(
+			IUnitOfWorkFactory unitOfWorkFactory,
+			INavigationManager navigation,
+			int driverId
+			) : base(unitOfWorkFactory, navigation)
+		{
+
+		}
+	}
+}
