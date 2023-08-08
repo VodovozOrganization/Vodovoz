2edfe569f0a3237385a0b49991aa612672cc58f7
Vadim Gordeev <enzogord@gmail.com>
2023-08-08 18:44:54 +0300

    Реализация автоматического биндинга для IObservableList

--- Source/Libraries/Core/Infrastructure/VodovozInfrastructure/Observable/ObservableListBinding.cs
+++ Source/Libraries/Core/Infrastructure/VodovozInfrastructure/Observable/ObservableListBinding.cs
@@ -1,20 +1,22 @@
-﻿using System;
+﻿using QS.Extensions.Observable.Collections.List;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Data.Bindings;
-using System.Data.Bindings.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 
 namespace VodovozInfrastructure.Observable
 {
 	public class ObservableListBinding<TSourceElement> : IReadyObservableListBinding
 	{
-		private IListEvents _source;
-		private object _dest;
+		private IObservableList<TSourceElement> _source;
 
 		private Action<TSourceElement, int> _insertAction;
 		private Action<int> _removeAction;
 		private Action<TSourceElement, int> _setAction;
+		private Action _resetAction;
 
-		internal ObservableListBinding<TSourceElement> Bind(GenericObservableList<TSourceElement> sourceElements)
+		internal ObservableListBinding<TSourceElement> Bind(IObservableList<TSourceElement> sourceElements)
 		{
 			_source = sourceElements;
 			return this;
@@ -32,8 +34,6 @@ namespace VodovozInfrastructure.Observable
 				throw new ArgumentNullException(nameof(destElementFactory));
 			}
 
-			_dest = destElements;
-
 			foreach(var sourceElement in (IEnumerable<TSourceElement>)_source)
 			{
 				var destElement = destElementFactory(sourceElement);
@@ -43,66 +43,81 @@ namespace VodovozInfrastructure.Observable
 			_insertAction = (sourceElement, index) => destElements.Insert(index, destElementFactory(sourceElement));
 			_setAction = (sourceElement, index) => destElements[index] = destElementFactory(sourceElement);
 			_removeAction = (index) => destElements.RemoveAt(index);
+			_resetAction = () => {
+				destElements.Clear();
+				foreach(var sourceElement in _source.Reverse())
+				{
+					_insertAction.Invoke(sourceElement, 0);
+				}
+			};
 
-			_source.ElementAdded += ObservableVersions_ElementAdded;
-			_source.ElementRemoved += ObservableVersions_ElementRemoved;
-			_source.ElementChanged += ObservableVersions_ElementChanged;
+			_source.CollectionChanged += Source_CollectionChanged;
 
 			return this;
 		}
 
 		void IReadyObservableListBinding.Clear()
 		{
-			_source.ElementAdded -= ObservableVersions_ElementAdded;
-			_source.ElementRemoved -= ObservableVersions_ElementRemoved;
-			_source.ElementChanged -= ObservableVersions_ElementChanged;
+			Clear();
+		}
+
+		private void Clear()
+		{
+			_source.CollectionChanged -= Source_CollectionChanged;
 			_source = null;
-			_dest = null;
 			_insertAction = null;
 			_removeAction = null;
 			_setAction = null;
 		}
 
-		private void ObservableVersions_ElementAdded(object aList, int[] aIdx)
+		private void Source_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
 		{
-			var eventSource = aList as GenericObservableList<TSourceElement>;
-			if(eventSource != _source)
+			switch(e.Action)
 			{
-				eventSource.ElementAdded -= ObservableVersions_ElementAdded;
-			}
-
-			foreach(var index in aIdx)
-			{
-				_insertAction.Invoke(eventSource[index], index);
+				case NotifyCollectionChangedAction.Add:
+					AddElement(e.NewItems, e.NewStartingIndex);
+					break;
+				case NotifyCollectionChangedAction.Move:
+					throw new NotSupportedException("Перемещение элемента внутри коллекции не поддерживается");
+				case NotifyCollectionChangedAction.Remove:
+					RemoveElement(e.OldStartingIndex);
+					break;
+				case NotifyCollectionChangedAction.Replace:
+					ReplaceElement(e.NewStartingIndex);
+					break;
+				case NotifyCollectionChangedAction.Reset:
+					break;
 			}
 		}
 
-		private void ObservableVersions_ElementRemoved(object aList, int[] aIdx, object aObject)
+		private void AddElement(IList addedElements, int index)
 		{
-			var eventSource = aList as GenericObservableList<TSourceElement>;
-			if(eventSource != _source)
+			var elements = addedElements as IEnumerable<TSourceElement>;
+			if(elements == null)
 			{
-				eventSource.ElementRemoved -= ObservableVersions_ElementRemoved;
+				return;
 			}
 
-			foreach(var index in aIdx)
+			foreach(var element in elements.Reverse())
 			{
-				_removeAction.Invoke(index);
+				_insertAction.Invoke(element, index);
 			}
 		}
 
-		private void ObservableVersions_ElementChanged(object aList, int[] aIdx)
+		private void RemoveElement(int index)
 		{
-			var eventSource = aList as GenericObservableList<TSourceElement>;
-			if(eventSource != null && eventSource != _source)
-			{
-				eventSource.ElementChanged -= ObservableVersions_ElementChanged;
-			}
+			_removeAction.Invoke(index);
+		}
 
-			foreach(var index in aIdx)
-			{
-				_setAction.Invoke(eventSource[index], index);
-			}
+		private void ReplaceElement(int index)
+		{
+			_setAction.Invoke(_source[index], index);
+		}
+
+		private void ResetCollection()
+		{
+
+			_resetAction.Invoke();
 		}
 	}
 }
