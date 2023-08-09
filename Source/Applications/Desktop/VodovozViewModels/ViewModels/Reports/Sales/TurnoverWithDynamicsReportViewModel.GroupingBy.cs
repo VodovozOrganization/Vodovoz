08ce9ebf317ede824e8b09c2a98043b5c1f11237
Semen <nikiforov.semen@gmail.com>
2023-08-09 16:12:05 +0300

    Добавлены комментарии в enum

--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Reports/Sales/TurnoverWithDynamicsReportViewModel.GroupingBy.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Reports/Sales/TurnoverWithDynamicsReportViewModel.GroupingBy.cs
@@ -4,13 +4,16 @@ namespace Vodovoz.ViewModels.Reports.Sales
 {
 	public partial class TurnoverWithDynamicsReportViewModel
 	{
+		/// <summary>
+		/// Варианты группировки и отображения отчета
+		/// </summary>
 		public enum GroupingByEnum
 		{
-			[Display(Name = "Номенклатура")]
+			[Display(Name = "Группировать по номенклатуре")]
 			Nomenclature,
-			[Display(Name = "Контрагент")]
+			[Display(Name = "Группировать по контрагенту, не показывать контакты")]
 			Counterparty,
-			[Display(Name = "Контрагент, показать контакты")]
+			[Display(Name = "Группировать по контрагенту, показать контакты")]
 			CounterpartyShowContacts
 		}
 	}
