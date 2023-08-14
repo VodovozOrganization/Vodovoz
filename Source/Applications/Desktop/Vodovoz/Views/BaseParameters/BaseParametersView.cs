d1832d6e675ee9ca7aaa5ff9c2772c97215fb27f
N-Sem <nikiforov.semen@gmail.com>
2023-08-14 10:47:59 +0300

    Добавлена view диалога BaseParameters

---
+++ Source/Applications/Desktop/Vodovoz/Views/BaseParameters/BaseParametersView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.BaseParameters
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class BaseParametersView : Gtk.Bin
+	{
+		public BaseParametersView()
+		{
+			this.Build();
+		}
+	}
+}
