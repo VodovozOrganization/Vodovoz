5c1a390f7443c671cc99f04c8f073182ddb1c778
Sergey Efimov <efimov90@gmail.com>
2023-08-15 12:23:17 +0300

    Bindings

--- Source/Applications/Desktop/Vodovoz/Counterparties/SubtypeView.cs
+++ Source/Applications/Desktop/Vodovoz/Counterparties/SubtypeView.cs
@@ -1,13 +1,32 @@
-﻿using System.ComponentModel;
+﻿using QS.Views.GtkUI;
+using System.ComponentModel;
+using Vodovoz.ViewModels.Counterparties;
 
 namespace Vodovoz.Counterparties
 {
 	[ToolboxItem(true)]
-	public partial class SubtypeView : Gtk.Bin
+	public partial class SubtypeView : TabViewBase<SubtypeViewModel>
 	{
-		public SubtypeView()
+		public SubtypeView(SubtypeViewModel viewModel)
+			: base(viewModel)
 		{
 			Build();
+
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			ylabelIdValue.Binding
+				.AddBinding(ViewModel.Entity, e => e.Id, w => w.Text)
+				.InitializeFromSource();
+
+			yentryName.Binding
+				.AddBinding(ViewModel.Entity, e => e.Name, w => w.Text)
+				.InitializeFromSource();
+
+			buttonSave.Clicked += (s, e) => ViewModel.SaveCommand.Execute();
+			buttonCancel.Clicked += (s, e) => ViewModel.CloseCommand.Execute();
 		}
 	}
 }
