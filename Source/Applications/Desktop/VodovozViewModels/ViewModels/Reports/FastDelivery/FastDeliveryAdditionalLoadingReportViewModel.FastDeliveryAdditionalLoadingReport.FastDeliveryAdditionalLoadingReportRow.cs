6df7b06a3baf41b120bc869057b9f70af682c1a4
Sergey Efimov <efimov90@gmail.com>
2023-08-16 15:56:27 +0300

    Рефакторинг отчета

---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Reports/FastDelivery/FastDeliveryAdditionalLoadingReportViewModel.FastDeliveryAdditionalLoadingReport.FastDeliveryAdditionalLoadingReportRow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Vodovoz.ViewModels.ViewModels.Reports.FastDelivery
+{
+	public partial class FastDeliveryAdditionalLoadingReportViewModel
+	{
+		public partial class FastDeliveryAdditionalLoadingReport
+		{
+			public class FastDeliveryAdditionalLoadingReportRow
+			{
+				public DateTime RouteListDate { get; set; }
+				public int RouteListId { get; set; }
+				public int OwnOrdersCount { get; set; }
+				public string AdditionaLoadingNomenclature { get; set; }
+				public decimal AdditionaLoadingAmount { get; set; }
+				public string RouteListDateString => RouteListDate.ToShortDateString();
+			}
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Reports/FastDelivery/FastDeliveryAdditionalLoadingReportViewModel.FastDeliveryAdditionalLoadingReport.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Vodovoz.ViewModels.ViewModels.Reports.FastDelivery
+{
+	public partial class FastDeliveryAdditionalLoadingReportViewModel
+	{
+		public partial class FastDeliveryAdditionalLoadingReport
+		{
+			public IList<FastDeliveryAdditionalLoadingReportRow> Rows { get; set; }
+		}
+	}
+}
