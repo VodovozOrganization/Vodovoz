0d2848ef305237d1539b5b054917fa3bc01bf03c
Semen <nikiforov.semen@gmail.com>
2023-08-16 12:39:44 +0300

    Добавлены привязки во вью

--- Source/Applications/Desktop/Vodovoz/ReportsParameters/Cash/MovementsPaymentControlView.cs
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Cash/MovementsPaymentControlView.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using QS.Views;
+using Vodovoz.ViewModels.Cash.Reports;
+
 namespace Vodovoz.ReportsParameters.Cash
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class MovementsPaymentControlView : Gtk.Bin
+	public partial class MovementsPaymentControlView : ViewBase<MovementsPaymentControlViewModel>
 	{
-		public MovementsPaymentControlView()
+		public MovementsPaymentControlView(MovementsPaymentControlViewModel viewModel) : base(viewModel)
 		{
 			this.Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			if(ViewModel == null)
+			{
+				return;
+			}
+
+			dateperiodpicker.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.StartDate, w => w.StartDate)
+				.AddBinding(vm => vm.EndDate, w => w.EndDate)
+				.InitializeFromSource();
+
+			buttonCreateReport.Clicked += (s, e) => ViewModel.CreateReportCommand?.Execute();
 		}
 	}
 }
