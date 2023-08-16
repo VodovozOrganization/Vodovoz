cec8f6fa39e24f24208239390ed8262020462251
N-Sem <nikiforov.semen@gmail.com>
2023-08-16 12:06:51 +0300

    Добавлена вью диалога отчета

---
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Cash/MovementsPaymentControlView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ReportsParameters.Cash
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class MovementsPaymentControlView : Gtk.Bin
+	{
+		public MovementsPaymentControlView()
+		{
+			this.Build();
+		}
+	}
+}
