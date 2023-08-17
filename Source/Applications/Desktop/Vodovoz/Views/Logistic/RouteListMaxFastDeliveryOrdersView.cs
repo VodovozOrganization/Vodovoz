2722cbe90c4e093be928fcad02fd3010f7204074
Art8m <9artbe@gmail.com>
2023-08-17 08:02:56 -0700

    stetic

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/RouteListMaxFastDeliveryOrdersView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RouteListMaxFastDeliveryOrdersView : Gtk.Bin
+	{
+		public RouteListMaxFastDeliveryOrdersView()
+		{
+			this.Build();
+		}
+	}
+}
