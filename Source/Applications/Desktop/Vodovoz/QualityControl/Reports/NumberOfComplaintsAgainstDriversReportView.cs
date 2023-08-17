a10ff8807c3a727f048dd0d94b3e080488d42bd8
Sergey Efimov <efimov90@gmail.com>
2023-08-17 16:42:30 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/QualityControl/Reports/NumberOfComplaintsAgainstDriversReportView.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Vodovoz.QualityControl.Reports
+{
+	[ToolboxItem(true)]
+	public partial class NumberOfComplaintsAgainstDriversReportView : Gtk.Bin
+	{
+		public NumberOfComplaintsAgainstDriversReportView()
+		{
+			Build();
+		}
+	}
+}
