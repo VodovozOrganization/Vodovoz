fe626d4ec0fd52f47025dde2fdd243159ecd7fc4
Sergey Efimov <efimov90@gmail.com>
2023-08-17 15:14:00 +0300

    Логика отчета

---
+++ Source/Applications/Desktop/VodovozViewModels/QualityControl/Reports/NumberOfComplaintsAgainstDriversReportViewModel.NumberOfComplaintsAgainstDriversReport.cs
@@ -0,0 +1,53 @@
+﻿using QS.DomainModel.Entity;
+using QS.DomainModel.UoW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vodovoz.Domain.Complaints;
+using Vodovoz.Domain.Employees;
+
+namespace Vodovoz.ViewModels.QualityControl.Reports
+{
+	public partial class NumberOfComplaintsAgainstDriversReportViewModel
+	{
+		[Appellative(Nominative = "Jтчет по количеству рекламаций на водителей")]
+		public partial class NumberOfComplaintsAgainstDriversReport
+		{
+			private NumberOfComplaintsAgainstDriversReport(DateTime startDate, DateTime endDate, List<Row> rows)
+			{
+				CreatedAt = DateTime.Now;
+				StartDate = startDate;
+				EndDate = endDate;
+				Rows = rows;
+			}
+
+			public DateTime	CreatedAt { get; }
+			public DateTime StartDate { get; }
+			public DateTime EndDate { get; }
+			public List<Row> Rows { get; }
+
+			public static NumberOfComplaintsAgainstDriversReport Generate(IUnitOfWork unitOfWork, DateTime startDate, DateTime endDate)
+			{
+				var complaints = (from complaint in unitOfWork.Session.Query<Complaint>()
+								  join complaintKind in unitOfWork.Session.Query<ComplaintKind>()
+								  on complaint.ComplaintKind.Id equals complaintKind.Id
+								  join driver in unitOfWork.Session.Query<Employee>()
+								  on complaint.Driver.Id equals driver.Id
+								  where complaint.CreationDate >= startDate
+									&& complaint.CreationDate <= endDate
+								  group complaint by driver.Id into complaintsGroup
+								  let driver = complaintsGroup.FirstOrDefault().Driver
+								  let driverFullName = $"{driver.Name} {driver.LastName} {driver.Patronymic}"
+								  let complaintsList = string.Join(",\n", complaintsGroup.SelectMany(x => $"{x.Id} - {x.ComplaintKind.Name}"))
+								  select new Row
+								  {
+									  DriverFullName = driverFullName,
+									  ComplaintsCount = complaintsGroup.Count(),
+									  ComplaintsList = complaintsList
+								  }).ToList();
+
+				return new NumberOfComplaintsAgainstDriversReport(startDate, endDate, complaints);
+			}
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/QualityControl/Reports/NumberOfComplaintsAgainstDriversReportViewModel.cs
@@ -0,0 +1,62 @@
+﻿using QS.Commands;
+using QS.Dialog;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.ViewModels;
+using System;
+using Vodovoz.Tools;
+
+namespace Vodovoz.ViewModels.QualityControl.Reports
+{
+	public partial class NumberOfComplaintsAgainstDriversReportViewModel : DialogTabViewModelBase
+	{
+		private readonly IInteractiveService _interactiveService;
+		private NumberOfComplaintsAgainstDriversReport _report;
+		private DateTime? _startDate;
+		private DateTime? _endDate;
+
+		public NumberOfComplaintsAgainstDriversReportViewModel(
+			IUnitOfWorkFactory unitOfWorkFactory,
+			IInteractiveService interactiveService,
+			INavigationManager navigation)
+			: base(unitOfWorkFactory, interactiveService, navigation)
+		{
+			_interactiveService = interactiveService ?? throw new ArgumentNullException(nameof(interactiveService));
+
+			TabName = typeof(NumberOfComplaintsAgainstDriversReport).GetClassUserFriendlyName().Nominative;
+
+			GenerateReportCommand = new DelegateCommand(GenerateReport);
+		}
+
+		public DateTime? StartDate
+		{
+			get => _startDate;
+			set => SetField(ref _startDate, value);
+		}
+
+		public DateTime? EndDate
+		{
+			get => _endDate;
+			set => SetField(ref _endDate, value);
+		}
+
+		public NumberOfComplaintsAgainstDriversReport Report
+		{
+			get => _report;
+			private set => SetField(ref _report, value);
+		}
+
+		public DelegateCommand GenerateReportCommand { get; }
+
+		private void GenerateReport()
+		{
+			if(StartDate is null || EndDate is null)
+			{
+				_interactiveService.ShowMessage(ImportanceLevel.Warning, "Не указан период");
+				return;
+			}
+
+			Report = NumberOfComplaintsAgainstDriversReport.Generate(UoW, StartDate.Value, EndDate.Value);
+		}
+	}
+}
