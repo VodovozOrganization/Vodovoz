1bddd82601bb6bbdb09c7811e9c26737ac5f5ef6
Sergey Efimov <efimov90@gmail.com>
2023-08-18 09:41:11 +0300

    Убрал старое подключение енама

--- Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/Drivers/DriverMobileAppActionRecordMap.cs
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/Drivers/DriverMobileAppActionRecordMap.cs
@@ -11,19 +11,12 @@ namespace Vodovoz.Data.NHibernate.HibernateMapping.Logistic.Drivers
 
 			Id(x => x.Id).Column("id").GeneratedBy.Native();
 
-			Map(x => x.Action).Column("type").CustomType<DriverMobileAppActionStringType>();
+			Map(x => x.Action).Column("type");
 			Map(x => x.Result).Column("result");
 			Map(x => x.ActionDatetime).Column("action_datetime");
 			Map(x => x.RecievedDatetime).Column("recieved_datetime");
 
 			References(x => x.Driver).Column("employee_id");
-		}
-	}
-
-	public class DriverMobileAppActionStringType : NHibernate.Type.EnumStringType
-	{
-		public DriverMobileAppActionStringType() : base(typeof(DriverMobileAppActionType))
-		{
 		}
 	}
 }
