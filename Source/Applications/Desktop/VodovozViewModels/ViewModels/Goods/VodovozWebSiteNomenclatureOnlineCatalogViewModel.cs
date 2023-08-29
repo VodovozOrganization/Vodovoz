4e12764ab8a870fbce8d37bb077839ac2afdc438
Andrey Shaklein <as1854@yandex.ru>
2023-08-29 17:06:08 +0300

    4485 добавил сущности онлайн каталогов и вью модели журналов и диалогов

---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Goods/VodovozWebSiteNomenclatureOnlineCatalogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using Vodovoz.Domain.Goods;
+
+namespace Vodovoz.ViewModels.ViewModels.Goods
+{
+	public class VodovozWebSiteNomenclatureOnlineCatalogViewModel : EntityTabViewModelBase<VodovozWebSiteNomenclatureOnlineCatalog>
+	{
+		public VodovozWebSiteNomenclatureOnlineCatalogViewModel(
+			IEntityUoWBuilder uowBuilder,
+			IUnitOfWorkFactory uowFactory,
+			ICommonServices commonServices,
+			INavigationManager navigationManager) : base(uowBuilder, uowFactory, commonServices, navigationManager)
+		{
+
+		}
+	}
+}
