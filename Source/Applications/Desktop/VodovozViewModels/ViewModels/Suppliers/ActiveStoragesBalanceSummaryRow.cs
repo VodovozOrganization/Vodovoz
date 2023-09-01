55923f32106ce8876206f6212347c920d938a1ee
Andrey Shaklein <as1854@yandex.ru>
2023-09-01 15:22:04 +0300

    добавил порядковый номер хранилища в отчет

--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Suppliers/ActiveStoragesBalanceSummaryRow.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Suppliers/ActiveStoragesBalanceSummaryRow.cs
@@ -2,8 +2,15 @@
 {
 	public class ActiveStoragesBalanceSummaryRow
 	{
+		/// <summary>
+		/// Порядковый номер хранилища
+		/// </summary>
+		public int? RowNumberStorage { get; set; }
 		public int StorageId { get; set; }
 		public string Storage { get; set; }
+		/// <summary>
+		/// Порядковый номер ТМЦ на балансе хранилища
+		/// </summary>
 		public int RowNumberFromStorage { get; set; }
 		public int EntityId { get; set; }
 		public string Entity { get; set; }
