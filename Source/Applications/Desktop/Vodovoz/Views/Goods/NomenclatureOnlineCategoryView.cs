accb360c8a73158115fdf60477857e5d2adcbd03
Andrey Shaklein <as1854@yandex.ru>
2023-09-06 12:29:02 +0300

    добавил вьюхи онлайн групп и категорий номенклатур

---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/NomenclatureOnlineCategoryView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class NomenclatureOnlineCategoryView : Gtk.Bin
+	{
+		public NomenclatureOnlineCategoryView()
+		{
+			this.Build();
+		}
+	}
+}
