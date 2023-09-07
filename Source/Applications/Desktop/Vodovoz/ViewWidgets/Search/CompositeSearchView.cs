55e2b28e53095f044cd59a4f694cb0acd7f3a845
N-Sem <nikiforov.semen@gmail.com>
2023-09-07 16:08:33 +0300

    Добавлена вью виджета поиска

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Search/CompositeSearchView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Search
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CompositeSearchView : Gtk.Bin
+	{
+		public CompositeSearchView()
+		{
+			this.Build();
+		}
+	}
+}
