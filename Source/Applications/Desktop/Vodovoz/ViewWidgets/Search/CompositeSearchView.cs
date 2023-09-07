5fc93913609fa401f2df7ee713ed7a984ffb1f3e
Semen <nikiforov.semen@gmail.com>
2023-09-07 17:08:08 +0300

    Добавлены биндинги в представлении

--- Source/Applications/Desktop/Vodovoz/ViewWidgets/Search/CompositeSearchView.cs
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Search/CompositeSearchView.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Widgets.Search;
+
 namespace Vodovoz.ViewWidgets.Search
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class CompositeSearchView : Gtk.Bin
+	public partial class CompositeSearchView : WidgetViewBase<CompositeSearchViewModel>
 	{
-		public CompositeSearchView()
+		public CompositeSearchView(CompositeSearchViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			ylabelSearchInfo.Binding
+				.AddBinding(ViewModel, vm => vm.SearchInfoLabelText, w => w.Text)
+				.InitializeFromSource();
+
+			entrySearch1.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.EntrySearchText1, w => w.Text)
+				.InitializeFromSource();
+
+			entrySearch2.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.EntrySearchText2, w => w.Text)
+				.AddFuncBinding(vm => vm.SearchEntryShownCount > 1, w => w.Visible)
+				.InitializeFromSource();
+
+			entrySearch3.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.EntrySearchText3, w => w.Text)
+				.AddFuncBinding(vm => vm.SearchEntryShownCount > 2, w => w.Visible)
+				.InitializeFromSource();
+
+			entrySearch4.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.EntrySearchText4, w => w.Text)
+				.AddFuncBinding(vm => vm.SearchEntryShownCount > 3, w => w.Visible)
+				.InitializeFromSource();
+
+			buttonAddAnd.Binding
+				.AddSource(ViewModel)
+				.AddFuncBinding(vm => vm.SearchEntryShownCount > 3, w => w.Sensitive)
+				.InitializeFromSource();
+
+			buttonAddAnd.Clicked += (o, e) => ViewModel.AddSearchEntryCommand.Execute();
+			ybutton2.Clicked += (o, e) => ViewModel.ClearSerarchEntriesTextCommand.Execute();
 		}
 	}
 }
