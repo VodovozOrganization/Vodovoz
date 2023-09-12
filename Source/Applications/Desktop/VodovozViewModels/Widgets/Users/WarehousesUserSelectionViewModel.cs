b8f586d865f86991df5da91782b20d5e51bc10f7
Semen <nikiforov.semen@gmail.com>
2023-09-12 12:28:52 +0300

    Добавлена часть логики во вью-модель

--- Source/Applications/Desktop/VodovozViewModels/Widgets/Users/WarehousesUserSelectionViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Widgets/Users/WarehousesUserSelectionViewModel.cs
@@ -1,27 +1,121 @@
-﻿using QS.ViewModels;
+﻿using QS.Commands;
+using QS.DomainModel.Entity;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Journal;
+using QS.Services;
+using QS.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Data.Bindings.Collections.Generic;
-using Vodovoz.Domain.Orders;
+using System.Linq;
 using Vodovoz.Domain.Store;
+using Vodovoz.ViewModels.Journals.JournalViewModels.Store;
 
 namespace Vodovoz.ViewModels.Widgets.Users
 {
 	public class WarehousesUserSelectionViewModel : WidgetViewModelBase
 	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly ICommonServices _commonServices;
+		private readonly INavigationManager _navigationManager;
+		private Warehouse _selectedWarehouse;
+		private DelegateCommand _addWarehouseCommand;
+		private DelegateCommand _removeWarehouseCommand;
+
+		public WarehousesUserSelectionViewModel(
+			IUnitOfWork unitOfWork,
+			ICommonServices commonServices,
+			INavigationManager navigationManager,
+			IEnumerable<int> warehousesIds)
+		{
+			_unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+			_commonServices = commonServices ?? throw new ArgumentNullException(nameof(commonServices));
+			_navigationManager = navigationManager ?? throw new ArgumentNullException(nameof(navigationManager));
+			ObservableWarehouses = new GenericObservableList<Warehouse>(GetWarehousesByIds(warehousesIds));
+		}
+
 		#region Свойства
 
-		private GenericObservableList<Warehouse> _selectedWarehouses;
-		public virtual GenericObservableList<Warehouse> SelectedWarehouses
+		public virtual GenericObservableList<Warehouse> ObservableWarehouses { get; set; }
+
+		[PropertyChangedAlso(nameof(CanRemoveWarehouse))]
+		public Warehouse SelectedWarehouse
+		{
+			get => _selectedWarehouse;
+			set => SetField(ref _selectedWarehouse, value);
+		}
+
+		#endregion
+
+		private List<Warehouse> GetWarehousesByIds(IEnumerable<int> warehousesIds)
+		{
+			var warehouses = _unitOfWork.GetAll<Warehouse>()
+				.Where(w => warehousesIds.Contains(w.Id))
+				.ToList();
+
+			return warehouses;
+		}
+
+		#region Commands
+
+		#region Add waarehouse
+		public DelegateCommand AddWarehouseCommand
 		{
 			get
 			{
-				if(_selectedWarehouses == null)
+				if(_addWarehouseCommand == null)
 				{
-					_selectedWarehouses = new GenericObservableList<Warehouse>();
+					_addWarehouseCommand = new DelegateCommand(AddWarehouse, () => CanAddWarehouse);
+					_addWarehouseCommand.CanExecuteChangedWith(this, x => x.CanAddWarehouse);
 				}
-
-				return _selectedWarehouses;
+				return _addWarehouseCommand;
 			}
 		}
+
+		public bool CanAddWarehouse => true;
+
+		private void AddWarehouse()
+		{
+			var warehouseJournalVM = _navigationManager.OpenViewModel<WarehouseJournalViewModel>(null).ViewModel;
+
+			warehouseJournalVM.SelectionMode = JournalSelectionMode.Single;
+			warehouseJournalVM.OnEntitySelectedResult += (s, e) =>
+			{
+				if(s is Warehouse warehouseToAdd)
+				{
+					ObservableWarehouses.Add(warehouseToAdd);
+				}
+			};
+		}
+		#endregion Add warehouse
+
+		#region Remove warehouse
+
+		public DelegateCommand RemoveWarehouseCommand
+		{
+			get
+			{
+				if(_removeWarehouseCommand == null)
+				{
+					_removeWarehouseCommand = new DelegateCommand(RemoveWarehouse, () => CanRemoveWarehouse);
+					_removeWarehouseCommand.CanExecuteChangedWith(this, x => x.CanRemoveWarehouse);
+				}
+				return _removeWarehouseCommand;
+			}
+		}
+
+		public bool CanRemoveWarehouse => SelectedWarehouse != null;
+
+		private void RemoveWarehouse()
+		{
+			if(SelectedWarehouse != null && ObservableWarehouses.Contains(SelectedWarehouse))
+			{
+				ObservableWarehouses.Remove(SelectedWarehouse);
+			}
+		}
+
+		#endregion Remove warehouse
 
 		#endregion
 	}
