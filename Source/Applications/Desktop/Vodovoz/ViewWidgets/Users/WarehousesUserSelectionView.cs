304a9f376cb69f70254f8f13f52450f940b1499a
N-Sem <nikiforov.semen@gmail.com>
2023-09-12 09:28:18 +0300

    Добавлен вид виджета выбора склада

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Users/WarehousesUserSelectionView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Users
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class WarehousesUserSelectionView : Gtk.Bin
+	{
+		public WarehousesUserSelectionView()
+		{
+			this.Build();
+		}
+	}
+}
