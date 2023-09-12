9d36e392c1f13df9accb113964badbc101749986
Semen <nikiforov.semen@gmail.com>
2023-09-12 09:48:10 +0300

    Добавлен класс вью-модели виджета

---
+++ Source/Applications/Desktop/VodovozViewModels/Widgets/Users/WarehousesUserSelectionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using QS.ViewModels;
+using System.Data.Bindings.Collections.Generic;
+using Vodovoz.Domain.Orders;
+using Vodovoz.Domain.Store;
+
+namespace Vodovoz.ViewModels.Widgets.Users
+{
+	public class WarehousesUserSelectionViewModel : WidgetViewModelBase
+	{
+		#region Свойства
+
+		private GenericObservableList<Warehouse> _selectedWarehouses;
+		public virtual GenericObservableList<Warehouse> SelectedWarehouses
+		{
+			get
+			{
+				if(_selectedWarehouses == null)
+				{
+					_selectedWarehouses = new GenericObservableList<Warehouse>();
+				}
+
+				return _selectedWarehouses;
+			}
+		}
+
+		#endregion
+	}
+}
