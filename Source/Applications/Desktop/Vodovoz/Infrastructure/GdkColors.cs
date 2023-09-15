bd4df7a0279f66bb2128df1bec5cbbc064a085ce
Sergey Efimov <efimov90@gmail.com>
2023-09-15 09:13:19 +0300

    Комментарии к недовозам

--- Source/Applications/Desktop/Vodovoz/Infrastructure/GdkColors.cs
+++ Source/Applications/Desktop/Vodovoz/Infrastructure/GdkColors.cs
@@ -32,7 +32,7 @@ namespace Vodovoz.Infrastructure
 		#region AdditionalColors
 
 		public static Color Black { get; } = new Color(0, 0, 0);
-		public static Color Red { get; } = new Color(0xff, 0, 0);
+		public static Color Red { get; } = IsLight ? new Color(0xff, 0, 0) : new Color(204, 0, 0);
 		public static Color Red2 { get; } = new Color(0xfe, 0x5c, 0x5c);
 		public static Color LightRed { get; } = new Color(0xff, 0x66, 0x66);
 		public static Color Pink { get; } = IsLight ? new Color(0xff, 0xc0, 0xc0) : new Color(164, 123, 123);
@@ -45,7 +45,7 @@ namespace Vodovoz.Infrastructure
 		public static Color DarkGray { get; } = new Color(0x80, 0x80, 0x80);
 		public static Color Green { get; } = new Color(14, 135, 14);
 		public static Color LightGreen { get; } = new Color(0xc0, 0xff, 0xc0);
-		public static Color Blue { get; } = IsLight ? new Color(0x00, 0x18, 0xf9) : new Color(0, 0, 97);
+		public static Color Blue { get; } = IsLight ? new Color(0x00, 0x18, 0xf9) : new Color(0, 35, 164);
 		public static Color BabyBlue { get; } = new Color(0x89, 0xcf, 0xef);
 		public static Color LightPurple { get; } = new Color(199, 206, 255);
 		public static Color LightCoral { get; } = new Color(240, 128, 128);
