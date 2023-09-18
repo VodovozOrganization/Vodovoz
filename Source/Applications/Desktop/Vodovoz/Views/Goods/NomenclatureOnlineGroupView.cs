1dc17f1d4ac742c9ca97ef90f8f1717a7e318d52
Andrey Shaklein <as1854@yandex.ru>
2023-09-18 17:15:35 +0300

    4485 добавил вьюхи онлайн групп и категорий

---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/NomenclatureOnlineGroupView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class NomenclatureOnlineGroupView : Gtk.Bin
+	{
+		public NomenclatureOnlineGroupView()
+		{
+			this.Build();
+		}
+	}
+}
