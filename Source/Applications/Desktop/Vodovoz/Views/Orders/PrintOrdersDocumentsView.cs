272d4f5881509266557560643c5cabeed56389fe
Semen <nikiforov.semen@gmail.com>
2023-09-18 16:28:08 +0300

    Добавлены привязки в представлении

--- Source/Applications/Desktop/Vodovoz/Views/Orders/PrintOrdersDocumentsView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/PrintOrdersDocumentsView.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using Gamma.GtkWidgets;
+using Gtk;
+using QS.Views.Dialog;
+using Vodovoz.Domain.Orders;
+using Vodovoz.ViewModels.Dialogs.Orders;
+
 namespace Vodovoz.Views.Orders
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class PrintOrdersDocumentsView : Gtk.Bin
+	public partial class PrintOrdersDocumentsView : DialogViewBase<PrintOrdersDocumentsViewModel>
 	{
-		public PrintOrdersDocumentsView()
+		public PrintOrdersDocumentsView(PrintOrdersDocumentsViewModel viewModel) : base(viewModel)
 		{
 			this.Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			ybuttonPrint.Clicked += (sender, e) => ViewModel.PrintCommand?.Execute();
+			ybuttonPrint.Binding
+				.AddBinding(ViewModel, vm => vm.CanPrintDocuments, w => w.Sensitive)
+				.InitializeFromSource();
+
+			ycheckbuttonBill.Binding
+				.AddBinding(ViewModel, vm => vm.IsPrintBill, w => w.Active)
+				.InitializeFromSource();
+
+			ycheckbuttonUpd.Binding
+				.AddBinding(ViewModel, vm => vm.IsPrintUpd, w => w.Active)
+				.InitializeFromSource();
+
+			ytreeviewOrders.ColumnsConfig = ColumnsConfigFactory.Create<Order>()
+				.AddColumn("Заказ").AddTextRenderer(o => o.Id.ToString())
+				.AddColumn("Дата").AddTextRenderer(o => o.DeliveryDate.ToString())
+				.AddColumn("")
+				.Finish();
+
+			ytreeviewOrders.ItemsDataSource = ViewModel.Orders;
+
+			ytreeviewWarnings.HeadersVisible = false;
+			ytreeviewWarnings.ColumnsConfig = ColumnsConfigFactory.Create<string>()
+				.AddColumn("")
+					.AddTextRenderer(x => x)
+				.RowCells()
+					.AddSetter<CellRendererText>((c, n) => c.Foreground = "red")
+				.Finish();
+
+			ytreeviewWarnings.ItemsDataSource = ViewModel.Warnings;
 		}
 	}
 }
