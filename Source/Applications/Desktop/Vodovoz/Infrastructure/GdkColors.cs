756af94c5ee0e8a3eecf51374b7954659d259c81
Sergey Efimov <efimov90@gmail.com>
2023-09-18 12:30:54 +0300

    Доп секции, перемещение цветов

--- Source/Applications/Desktop/Vodovoz/Infrastructure/GdkColors.cs
+++ Source/Applications/Desktop/Vodovoz/Infrastructure/GdkColors.cs
@@ -26,6 +26,36 @@ namespace Vodovoz.Infrastructure
 
 		#endregion Primary
 
+		#region Success
+
+		public static Color Green { get; } = new Color(14, 135, 14);
+
+		public static Color LightGreen { get; } = new Color(0xc0, 0xff, 0xc0);
+
+		#endregion Success
+
+		#region Danger
+
+		public static Color Red { get; } = IsLight ? new Color(0xff, 0, 0) : new Color(204, 0, 0);
+
+		public static Color LightRed { get; } = new Color(0xff, 0x66, 0x66);
+
+		#endregion Danger
+
+		#region Warning
+
+		public static Color Yellow { get; } = new Color(255, 255, 40);
+
+		public static Color LightYellow { get; } = new Gdk.Color(0xe1, 0xd6, 0x70);
+
+		#endregion Warning
+
+		#region Info
+
+		public static Color Blue { get; } = IsLight ? new Color(0x00, 0x18, 0xf9) : new Color(0, 35, 164);
+
+		#endregion Info
+
 		#region Insensitive
 
 		public static Color InsensitiveBG => Rc.GetStyle(_dwfaultWidget).Background(StateType.Insensitive);
@@ -37,24 +67,14 @@ namespace Vodovoz.Infrastructure
 
 		#region AdditionalColors
 
-		public static Color Black { get; } = new Color(0, 0, 0);
-		public static Color Red { get; } = IsLight ? new Color(0xff, 0, 0) : new Color(204, 0, 0);
 		public static Color Red2 { get; } = new Color(0xfe, 0x5c, 0x5c);
-
 		public static Color DarkRed { get; } = new Color(110, 19, 0);
-		public static Color LightRed { get; } = new Color(0xff, 0x66, 0x66);
 		public static Color Pink { get; } = IsLight ? new Color(0xff, 0xc0, 0xc0) : new Color(164, 123, 123);
-		public static Color Yellow { get; } = new Color(255, 255, 40);
-		public static Color LightYellow { get; } = new Gdk.Color(0xe1, 0xd6, 0x70);
 		public static Color LightYellow2 { get; } = new Color(255, 243, 199);
-		public static Color White { get; } = new Color(0xff, 0xff, 0xff);
 		public static Color Orange { get; } = new Color(0xfc, 0x66, 0x00);
 		public static Color LightGray { get; } = new Color(0xcc, 0xcc, 0xcc);
 		public static Color DarkGray { get; } = new Color(0x80, 0x80, 0x80);
-		public static Color Green { get; } = new Color(14, 135, 14);
 		public static Color DarkGreen { get; } = new Color(32, 100, 17);
-		public static Color LightGreen { get; } = new Color(0xc0, 0xff, 0xc0);
-		public static Color Blue { get; } = IsLight ? new Color(0x00, 0x18, 0xf9) : new Color(0, 35, 164);
 		public static Color CadetBlue { get; } = IsLight ? new Color(95, 158, 160) : new Color(140, 178, 179);
 		public static Color BabyBlue { get; } = new Color(0x89, 0xcf, 0xef);
 		public static Color LightPurple { get; } = new Color(199, 206, 255);
