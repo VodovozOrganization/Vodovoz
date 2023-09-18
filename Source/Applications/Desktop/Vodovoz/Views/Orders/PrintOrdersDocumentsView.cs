25fe685956f7f19f8802b1a5117ad271a7ddc80b
N-Sem <nikiforov.semen@gmail.com>
2023-09-18 13:40:07 +0300

    Добавлен новый диалог

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/PrintOrdersDocumentsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PrintOrdersDocumentsView : Gtk.Bin
+	{
+		public PrintOrdersDocumentsView()
+		{
+			this.Build();
+		}
+	}
+}
