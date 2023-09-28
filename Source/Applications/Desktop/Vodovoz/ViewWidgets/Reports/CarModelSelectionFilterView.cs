af668aab8e15e54953720c13db9b7cfdc36c9730
N-Sem <nikiforov.semen@gmail.com>
2023-09-28 18:05:45 +0300

    Добавлена вью нового виджета выбора модели авто

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Reports/CarModelSelectionFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarModelSelectionFilterView : Gtk.Bin
+	{
+		public CarModelSelectionFilterView()
+		{
+			this.Build();
+		}
+	}
+}
