1b2c1c86aa58a7d6ba3d649d09883b0f31a9edb6
Sergey Efimov <efimov90@gmail.com>
2023-09-29 11:31:56 +0300

    Рефакторинг, чистка подразделений

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Subdivisions/SubdivisionType.cs
@@ -0,0 +1,18 @@
+﻿using QS.DomainModel.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz
+{
+	[Appellative(Gender = GrammaticalGender.Masculine
+		Nominative = "тип подразделения",
+		NominativePlural = "типы подразделений")]
+	public enum SubdivisionType
+	{
+		[Display(Name = "Стандартный")]
+		Default,
+		[Display(Name = "Логистика")]
+		Logistic,
+		[Display(Name = "Офис")]
+		Office
+	}
+}
