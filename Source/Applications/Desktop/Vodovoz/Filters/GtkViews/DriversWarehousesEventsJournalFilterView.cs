1aba6ab619b3184f2a8463b26a5ed9f9b204dce6
Andrey Shaklein <as1854@yandex.ru>
2023-10-11 12:55:25 +0300

    4546 добавил вьюху фильтра журнала событий

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/DriversWarehousesEventsJournalFilterView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Logistic;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DriversWarehousesEventsJournalFilterView : FilterViewBase<DriversWarehousesEventsJournalFilterViewModel>
+	{
+		public DriversWarehousesEventsJournalFilterView(DriversWarehousesEventsJournalFilterViewModel viewModel) : base(viewModel)
+		{
+			Build();
+		}
+	}
+}
