1b9f3a34144f6b5b7dc98661bd2e76b685859885
Sergey Efimov <efimov90@gmail.com>
2023-10-17 14:14:04 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/FastDeliveryOrderTransferFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FastDeliveryOrderTransferFilterView : Gtk.Bin
+	{
+		public FastDeliveryOrderTransferFilterView()
+		{
+			this.Build();
+		}
+	}
+}
