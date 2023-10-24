75352e74d912f742627c9a3c9e9e5f0129212ce3
Semen <nikiforov.semen@gmail.com>
2023-10-24 17:11:15 +0300

    Добавлена вью-модель всплывающего окна

---
+++ Source/Applications/Desktop/VodovozViewModels/Counterparties/CounterpartyClassification/CounterpartyClassificationCalculationSettingsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using QS.Navigation;
+using QS.ViewModels.Dialog;
+using System;
+
+namespace Vodovoz.ViewModels.Counterparties.CounterpartyClassification
+{
+	public class CounterpartyClassificationCalculationSettingsViewModel : WindowDialogViewModelBase
+	{
+		private string _userEmail;
+		private string _emailForReportCopy;
+
+		public CounterpartyClassificationCalculationSettingsViewModel(
+			INavigationManager navigation,
+			string userEmail
+			) : base(navigation)
+		{
+			if(string.IsNullOrWhiteSpace(userEmail))
+			{
+				throw new ArgumentException($"'{nameof(userEmail)}' cannot be null or whitespace.", nameof(userEmail));
+			}
+
+			_userEmail = userEmail;
+		}
+
+		public string UserEmail
+		{
+			get => _userEmail;
+			set => SetField(ref _userEmail, value);
+		}
+
+		public string EmailForReportCopy
+		{
+			get => _emailForReportCopy;
+			set => SetField(ref _emailForReportCopy, value);
+		}
+	}
+}
