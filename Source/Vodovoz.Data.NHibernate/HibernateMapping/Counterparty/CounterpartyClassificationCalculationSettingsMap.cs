65c3adb9229733976aee4c5df34644ca6f347424
Semen <nikiforov.semen@gmail.com>
2023-10-24 16:03:15 +0300

    Добавлен мап-класс для класса настроек расчета классификации

---
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Counterparty/CounterpartyClassificationCalculationSettingsMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Client.CounterpartyClassification;
+
+namespace Vodovoz.Data.NHibernate.HibernateMapping.Counterparty
+{
+	public class CounterpartyClassificationCalculationSettingsMap : ClassMap<CounterpartyClassificationCalculationSettings>
+	{
+		public CounterpartyClassificationCalculationSettingsMap()
+		{
+			Table("counterparty_classification_settings");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.PeriodInMonths).Column("period_in_months");
+			Map(x => x.BottlesCountAClassificationFrom).Column("bottles_count_a_classification_from");
+			Map(x => x.BottlesCountCClassificationTo).Column("bottles_count_c_classification_to");
+			Map(x => x.OrdersCountXClassificationFrom).Column("orders_count_x_classification_from");
+			Map(x => x.OrdersCountZClassificationTo).Column("orders_count_z_classification_to");
+			Map(x => x.SettingsCreationDate).Column("settings_creation_date");
+		}
+	}
+}
