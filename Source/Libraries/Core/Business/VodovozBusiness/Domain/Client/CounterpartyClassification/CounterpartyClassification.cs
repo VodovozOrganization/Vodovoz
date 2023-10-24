9248feae88da212987c0d497347d67af5e7598ad
Semen <34092262+N-Sem@users.noreply.github.com>
2023-10-24 15:05:00 +0300

    Добавлены новые классы классификации КА и настройка классификации

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Client/CounterpartyClassification/CounterpartyClassification.cs
@@ -0,0 +1,83 @@
+﻿using QS.DomainModel.Entity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Client.CounterpartyClassification
+{
+	[Appellative(Gender = GrammaticalGender.Masculine,
+		NominativePlural = "классификации контрагентов",
+		Nominative = "классификация контрагента")]
+	public class CounterpartyClassification : PropertyChangedBase, IDomainObject
+	{
+		private ClientClassificationByBottlesCount _classificationByBottlesCount;
+		private ClientClassificationByOrdersCount _classificationByOrdersCount;
+		private int _bottlesPerMonthAverageCount;
+		private int _ordersPerMonthAverageCount;
+		private decimal _moneyTurnoverPerMonthAverageSum;
+		private DateTime _classificationCalculationDate;
+
+		public virtual int Id { get; }
+
+		[Display(Name = "Классификация по среднему количеству бутылей")]
+		public virtual ClientClassificationByBottlesCount ClassificationByBottlesCount
+		{
+			get => _classificationByBottlesCount;
+			set => SetField(ref _classificationByBottlesCount, value);
+		}
+
+		[Display(Name = "Классификация по среднему количеству заказов")]
+		public virtual ClientClassificationByOrdersCount ClassificationByOrdersCount
+		{
+			get => _classificationByOrdersCount;
+			set => SetField(ref _classificationByOrdersCount, value);
+		}
+
+		[Display(Name = "Среднее количество бутылей в месяц")]
+		public virtual int BottlesPerMonthAverageCount
+		{
+			get => _bottlesPerMonthAverageCount;
+			set => SetField(ref _bottlesPerMonthAverageCount, value);
+		}
+
+		[Display(Name = "Среднее количество заказов в месяц")]
+		public virtual int OrdersPerMonthAverageCount
+		{
+			get => _ordersPerMonthAverageCount;
+			set => SetField(ref _ordersPerMonthAverageCount, value);
+		}
+
+		[Display(Name = "Средний оборот (сумма всех заказов) в месяц")]
+		public virtual decimal MoneyTurnoverPerMonthAverageSum
+		{
+			get => _moneyTurnoverPerMonthAverageSum;
+			set => SetField(ref _moneyTurnoverPerMonthAverageSum, value);
+		}
+
+		[Display(Name = "Дата выполнения расчета классификации")]
+		public virtual DateTime ClassificationCalculationDate
+		{
+			get => _classificationCalculationDate;
+			set => SetField(ref _classificationCalculationDate, value);
+		}
+	}
+
+	public enum ClientClassificationByBottlesCount
+	{
+		[Display(Name = "A")]
+		A,
+		[Display(Name = "B")]
+		B,
+		[Display(Name = "C")]
+		C
+	}
+
+	public enum ClientClassificationByOrdersCount
+	{
+		[Display(Name = "X")]
+		X,
+		[Display(Name = "Y")]
+		Y,
+		[Display(Name = "Z")]
+		Z
+	}
+}
---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Client/CounterpartyClassification/CounterpartyClassificationCalculationSettings.cs
@@ -0,0 +1,101 @@
+﻿using QS.DomainModel.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Client.CounterpartyClassification
+{
+	[Appellative(Gender = GrammaticalGender.Masculine,
+		NominativePlural = "параметры расчета классификаций контрагентов",
+		Nominative = "параметры расчета классификации контрагента")]
+	public class CounterpartyClassificationCalculationSettings : PropertyChangedBase, IDomainObject, IValidatableObject
+	{
+		private int _periodInMonths;
+		private int _bottlesCountAClassificationFrom;
+		private int _bottlesCountCClassificationTo;
+		private int _ordersCountXClassificationFrom;
+		private int _ordersCountZClassificationTo;
+		private DateTime _settingsCreationDate;
+
+		public virtual int Id { get; }
+
+		[Display(Name = "Значение периода в месяцах для выполнения расчета")]
+		public virtual int PeriodInMonths
+		{
+			get => _periodInMonths;
+			set => SetField(ref _periodInMonths, value);
+		}
+
+		//Если у контрагента среднее кол-во бутылей в месяц больше BottlesCountCClassificationTo, но меньше BottlesCountAClassificationFrom,
+		//то контрагенту присваивается категория 'B'
+
+		[Display(Name = "Среднее кол-во бутылей в месяц, начиная от которого присваивается категория 'A'")]
+		public virtual int BottlesCountAClassificationFrom
+		{
+			get => _bottlesCountAClassificationFrom;
+			set => SetField(ref _bottlesCountAClassificationFrom, value);
+		}
+
+		[Display(Name = "Среднее кол-во бутылей в месяц, до которого присваивается категория 'C'")]
+		public virtual int BottlesCountCClassificationTo
+		{
+			get => _bottlesCountCClassificationTo;
+			set => SetField(ref _bottlesCountCClassificationTo, value);
+		}
+
+		//Если у контрагента среднее кол-во заказов в месяц больше OrdersCountZClassificationTo, но меньше OrdersCountXClassificationFrom,
+		//то контрагенту присваивается категория 'Y'
+
+		[Display(Name = "Среднее кол-во заказов в месяц, начиная от которого присваивается категория 'X'")]
+		public virtual int OrdersCountXClassificationFrom
+		{
+			get => _ordersCountXClassificationFrom;
+			set => SetField(ref _ordersCountXClassificationFrom, value);
+		}
+
+		[Display(Name = "Среднее кол-во заказов в месяц, до которого присваивается категория 'Z'")]
+		public virtual int OrdersCountZClassificationTo
+		{
+			get => _ordersCountZClassificationTo;
+			set => SetField(ref _ordersCountZClassificationTo, value);
+		}
+
+		[Display(Name = "Дата создания настройки параметров для расчета классификации контрагентов")]
+		public virtual DateTime SettingsCreationDate
+		{
+			get => _settingsCreationDate;
+			set => SetField(ref _settingsCreationDate, value);
+		}
+
+		#region IValidatableObject implementation
+		public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if(PeriodInMonths < 1)
+			{
+				yield return new ValidationResult(string.Format("Введенное значение периода не должно быть меньше 1"));
+			}
+
+			if(BottlesCountAClassificationFrom < 0
+				|| BottlesCountCClassificationTo < 0
+				|| OrdersCountXClassificationFrom < 0
+				|| OrdersCountZClassificationTo < 0)
+			{
+				yield return new ValidationResult(string.Format("Введенные значения среднего кол-ва бутылей и среднего кол-ва заказов в месяц " +
+					"не должно быть меньше 0"));
+			}
+
+			if(BottlesCountAClassificationFrom <= BottlesCountCClassificationTo)
+			{
+				yield return new ValidationResult(string.Format("Значение среднего кол-ва бутылей в месяц для присвоения классификации 'A' " +
+					"должно быть больше среднего значения для присвоения классификации 'С'"));
+			}
+
+			if(OrdersCountXClassificationFrom <= OrdersCountZClassificationTo)
+			{
+				yield return new ValidationResult(string.Format("Значение среднего кол-ва заказов в месяц для присвоения классификации 'X' " +
+					"должно быть больше среднего значения для присвоения классификации 'Z'"));
+			}
+		}
+		#endregion
+	}
+}
