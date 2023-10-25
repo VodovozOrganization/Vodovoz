c81ae7e415ed7cfa83b2cf9e4c9107af8a284f8c
N-Sem <nikiforov.semen@gmail.com>
2023-10-25 15:38:33 +0300

    Создан диалог запуска расчета

---
+++ Source/Applications/Desktop/Vodovoz/Views/Client/CounterpartyClassification/CounterpartyClassificationCalculationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Client.CounterpartyClassification
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CounterpartyClassificationCalculationView : Gtk.Bin
+	{
+		public CounterpartyClassificationCalculationView()
+		{
+			this.Build();
+		}
+	}
+}
