0330cf02869717098ecaaae260979fda48e144ab
N-Sem <nikiforov.semen@gmail.com>
2023-10-26 11:12:53 +0300

    Добавлен диалог для ввода email  для отправки отчета

---
+++ Source/Applications/Desktop/Vodovoz/Views/Client/CounterpartyClassification/CounterpartyClassificationCalculationEmailSettingsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Client.CounterpartyClassification
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CounterpartyClassificationCalculationEmailSettingsView : Gtk.Bin
+	{
+		public CounterpartyClassificationCalculationEmailSettingsView()
+		{
+			this.Build();
+		}
+	}
+}
