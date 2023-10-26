aa3237ec33f79998a63dc978f9b7600a35c15b6f
Vadim Gordeev <enzogord@gmail.com>
2023-10-26 14:45:41 +0300

    Панель для СКУД и Манго

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Pacs/PacsPanelViewModel.cs
@@ -0,0 +1,151 @@
+﻿using QS.Commands;
+using QS.DomainModel.Entity;
+using QS.Navigation;
+using QS.ViewModels;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vodovoz.Presentation.ViewModels.Pacs
+{
+	public class PacsPanelViewModel : WidgetViewModelBase
+	{
+		private readonly IOperatorClient _operatorClient;
+		private readonly INavigationManager _navigationManager;
+
+		private bool _pacsEnabled;
+		private object _operatorState;
+		private BreakState _breakState;
+		private PacsState _pacsState;
+		private MangoState _mangoState;
+
+		public DelegateCommand BreakCommand { get; }
+		public DelegateCommand RefreshCommand { get; }
+		public DelegateCommand OpenPacsDialogCommand { get; }
+		public DelegateCommand OpenMangoDialogCommand { get; }
+
+		public PacsPanelViewModel(IOperatorClient operatorClient, INavigationManager navigationManager)
+		{
+			_operatorClient = operatorClient ?? throw new ArgumentNullException(nameof(operatorClient));
+			_navigationManager = navigationManager ?? throw new ArgumentNullException(nameof(navigationManager));
+
+			BreakCommand = new DelegateCommand(Break);
+			BreakCommand.CanExecuteChangedWith(this, x => x.CanBreak);
+
+			RefreshCommand = new DelegateCommand(Refresh);
+			RefreshCommand.CanExecuteChangedWith(this, x => x.CanRefresh);
+
+			OpenPacsDialogCommand = new DelegateCommand(OpenPacsDialog);
+			OpenPacsDialogCommand.CanExecuteChangedWith(this, x => x.CanOpenPacsDialog);
+
+			OpenMangoDialogCommand = new DelegateCommand(OpenMangoDialog);
+			OpenMangoDialogCommand.CanExecuteChangedWith(this, x => x.CanOpenMangoDialog);
+
+			_operatorClient.StateChanged += OperatorStateChanged;
+		}
+
+		public virtual bool PacsEnabled
+		{
+			get => _pacsEnabled;
+			set => SetField(ref _pacsEnabled, value);
+		}
+
+		private void OperatorStateChanged(object sender, object state)
+		{
+			_operatorState = state;
+		}
+
+
+		[PropertyChangedAlso(nameof(CanBreak))]
+		public virtual BreakState BreakState
+		{
+			get => _breakState;
+			set => SetField(ref _breakState, value);
+		}
+
+		public bool CanBreak =>
+			BreakState == BreakState.CanStartBreak ||
+			BreakState == BreakState.CanEndBreak;
+
+		private void Break()
+		{
+			_operatorClient.StartBreak();
+		}
+
+
+		public bool CanRefresh { get; }
+
+		private void Refresh()
+		{
+			_operatorState = _operatorClient.GetState();
+		}
+
+		[PropertyChangedAlso(nameof(CanOpenPacsDialog))]
+		public virtual PacsState PacsState
+		{
+			get => _pacsState;
+			set => SetField(ref _pacsState, value);
+		}
+
+
+		public bool CanOpenPacsDialog { get; set; }
+
+		private void OpenPacsDialog()
+		{
+
+		}
+
+		[PropertyChangedAlso(nameof(CanOpenMangoDialog))]
+		public virtual MangoState MangoState
+		{
+			get => _mangoState;
+			set => SetField(ref _mangoState, value);
+		}
+
+
+		public bool CanOpenMangoDialog { get; set; }
+
+		private void OpenMangoDialog()
+		{
+
+		}
+
+	}
+
+	public enum BreakState
+	{
+		BreakDenied,
+		CanStartBreak,
+		CanEndBreak	
+	}
+
+	public enum PacsState
+	{
+		Disconnected,
+		Connected,
+		WorkShift,
+		Break,
+		Talk
+	}
+
+	public enum MangoState
+	{
+		Disable,
+		Disconnected,
+		Connected,
+		Ring,
+		Talk
+	}
+
+	public interface IOperatorClient
+	{
+		event EventHandler<object> StateChanged;
+		object GetState();
+
+		void StartWorkshift(string phoneNumber);
+		void ChangeNumber(string phoneNumber);
+		void EndWorkshift();
+		void StartBreak();
+		void EndBreak();
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/Views/Pacs/PacsPanelView.cs
@@ -0,0 +1,190 @@
+﻿using Gtk;
+using QS.Views.GtkUI;
+using System;
+using System.ComponentModel;
+using Vodovoz.Presentation.ViewModels.Pacs;
+
+namespace Vodovoz.Views.Pacs
+{
+	[ToolboxItem(true)]
+	public partial class PacsPanelView : WidgetViewBase<PacsPanelViewModel>
+	{
+		private IconSize _iconSize = IconSize.Dnd;
+
+		public IconSize IconSize
+		{
+			get => _iconSize; 
+			set
+			{
+				if(_iconSize == value)
+				{
+					return;
+				}
+
+				_iconSize = value;
+				UpdateBreakButtonImage();
+				UpdateRefreshButtonImage();
+				UpdatePacsButtonImage();
+				UpdateMangoButtonImage();
+			}
+		}
+
+		public PacsPanelView()
+		{
+			this.Build();
+
+			UpdateBreakButtonImage();
+			UpdateRefreshButtonImage();
+			UpdatePacsButtonImage();
+			UpdateMangoButtonImage();
+		}
+
+		protected override void ConfigureWidget()
+		{
+			base.ConfigureWidget();
+			if(ViewModel == null)
+			{
+				return;
+			}
+
+			buttonBreak.BindCommand(ViewModel.BreakCommand);
+			buttonBreak.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.PacsEnabled, w => w.Visible)
+				.InitializeFromSource();
+
+			buttonRefresh.BindCommand(ViewModel.RefreshCommand);
+			buttonRefresh.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.PacsEnabled, w => w.Visible)
+				.InitializeFromSource();
+
+			buttonPacs.BindCommand(ViewModel.OpenPacsDialogCommand);
+			buttonPacs.Binding.AddSource(ViewModel)
+				.AddBinding(vm => vm.PacsEnabled, w => w.Visible)
+				.InitializeFromSource();
+
+			buttonMango.BindCommand(ViewModel.OpenMangoDialogCommand);
+
+			ViewModel.PropertyChanged += ViewModelPropertyChanged;
+		}
+
+		private void ViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			switch(e.PropertyName)
+			{
+				case nameof(PacsPanelViewModel.BreakState):
+					UpdateBreakButtonImage();
+					break;
+				case nameof(PacsPanelViewModel.PacsState):
+					UpdatePacsButtonImage();
+					break;
+				case nameof(PacsPanelViewModel.MangoState):
+					UpdateMangoButtonImage();
+					break;
+			}
+		}
+
+		private void UpdateBreakButtonImage()
+		{
+			var image = new Image();
+
+			switch(ViewModel.BreakState)
+			{
+				case BreakState.BreakDenied:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "coffee-break-denied", GetSmallButtonsSize());
+					break;
+				case BreakState.CanStartBreak:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "coffee-break-allowed", GetSmallButtonsSize());
+					break;
+				case BreakState.CanEndBreak:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "coffee-break-default", GetSmallButtonsSize());
+					break;
+				default:
+					throw new InvalidOperationException("Неизвестное состояние кнопки перерыва");
+			}
+
+			this.buttonBreak.Image.Destroy();
+			this.buttonBreak.Image = image;
+		}
+
+		private void UpdateRefreshButtonImage()
+		{
+			var image = new Image();
+			image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "gtk-refresh", GetSmallButtonsSize());
+			
+			this.buttonRefresh.Image.Destroy();
+			this.buttonRefresh.Image = image;
+		}
+
+		private IconSize GetSmallButtonsSize()
+		{
+			switch(_iconSize)
+			{
+				case IconSize.Dialog:
+					return IconSize.LargeToolbar;
+				case IconSize.Dnd:
+				case IconSize.LargeToolbar:
+					return IconSize.SmallToolbar;
+				case IconSize.SmallToolbar:
+				case IconSize.Menu:
+				case IconSize.Button:
+				case IconSize.Invalid:
+				default:
+					return IconSize.Menu;
+			}
+		}
+
+		private void UpdatePacsButtonImage()
+		{
+			var image = new Image();
+
+			switch(ViewModel.PacsState)
+			{
+				case PacsState.Disconnected:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "pacs-disabled", _iconSize);
+					break;
+				case PacsState.Break:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "coffee-break-active", _iconSize);
+					break;
+				case PacsState.Talk:
+				case PacsState.Connected:
+				case PacsState.WorkShift:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "pacs-active", _iconSize);
+					break;
+				default:
+					throw new InvalidOperationException("Неизвестное состояние кнопки скуд");
+			}
+
+			this.buttonPacs.Image.Destroy();
+			this.buttonPacs.Image = image;
+		}
+
+		private void UpdateMangoButtonImage()
+		{
+			var image = new Image();
+
+			switch(ViewModel.MangoState)
+			{
+				case MangoState.Disable:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "phone-disable", _iconSize);
+					break;
+				case MangoState.Disconnected:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "phone-disconnected", _iconSize);
+					break;
+				case MangoState.Connected:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "phone-connected", _iconSize);
+					break;
+				case MangoState.Ring:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "phone-ring", _iconSize);
+					break;
+				case MangoState.Talk:
+					image.Pixbuf = Stetic.IconLoader.LoadIcon(this, "phone-talk", _iconSize);
+					break;
+				default:
+					throw new InvalidOperationException("Неизвестное состояние кнопки манго");
+			}
+
+			this.buttonPacs.Image.Destroy();
+			this.buttonPacs.Image = image;
+		}
+	}
+}
