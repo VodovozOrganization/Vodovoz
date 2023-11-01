4bead25f47796eb15ba8d281509ef50549aa92f8
Sergey Efimov <efimov90@gmail.com>
2023-11-01 18:14:15 +0300

    Чистка Payment и PaymentItem

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Payments/PaymentState.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Payments
+{
+	public enum PaymentState
+	{
+		[Display(Name = "Нераспределен")]
+		undistributed,
+		[Display(Name = "Распределен")]
+		distributed,
+		[Display(Name = "Завершен")]
+		completed,
+		[Display(Name = "Отменен")]
+		Cancelled
+	}
+}
