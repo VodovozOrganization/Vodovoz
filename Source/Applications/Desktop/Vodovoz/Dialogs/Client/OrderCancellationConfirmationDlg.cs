b9f463013ba7dc45811e8ed735b8a87bfa2c4953
N-Sem <nikiforov.semen@gmail.com>
2023-11-10 09:32:35 +0300

    Создано новый диалог подтверждения заказа

---
+++ Source/Applications/Desktop/Vodovoz/Dialogs/Client/OrderCancellationConfirmationDlg.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Vodovoz.Dialogs.Client
+{
+	public partial class OrderCancellationConfirmationDlg : Gtk.Dialog
+	{
+		public OrderCancellationConfirmationDlg()
+		{
+			this.Build();
+		}
+	}
+}
