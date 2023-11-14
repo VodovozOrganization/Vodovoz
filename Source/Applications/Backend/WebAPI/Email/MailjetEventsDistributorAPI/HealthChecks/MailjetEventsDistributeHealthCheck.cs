737c8e25b0c7879042cf3aef7fe20f900c983ad7
Art8m <9artbe@gmail.com>
2023-11-14 17:29:03 +0300

    MailjetEventDistributor

---
+++ Source/Applications/Backend/WebAPI/Email/MailjetEventsDistributorAPI/HealthChecks/MailjetEventsDistributeHealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using VodovozHealthCheck;
+using VodovozHealthCheck.Dto;
+using VodovozHealthCheck.Utils;
+
+namespace MailjetEventsDistributorAPI.HealthChecks
+{
+	public class MailjetEventsDistributeHealthCheck : VodovozHealthCheckBase
+	{
+		protected override async Task<VodovozHealthResultDto> GetHealthResult()
+		{
+			var isHealthy = UrlExistsChecker.UrlExists("https://localhost:5001/Test");
+
+			return new VodovozHealthResultDto
+			{
+				IsHealthy = isHealthy
+			};
+		}
+	}
+}
