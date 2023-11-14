3a2051fc3295f987b90fa95630fa6b761409dcf0
Art8m <9artbe@gmail.com>
2023-11-14 17:04:50 +0300

    EArchive

---
+++ Source/Applications/Backend/WebAPI/EarchiveApi/HealthChecks/EarchiveApiHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using VodovozHealthCheck;
+using VodovozHealthCheck.Dto;
+
+namespace EarchiveApi.HealthChecks
+{
+	public class EarchiveApiHealthCheck : VodovozHealthCheckBase
+	{
+		protected override async Task<VodovozHealthResultDto> GetHealthResult()
+		{
+			var serviceAddress = $"https://localhost:7101/";
+
+			var grpcChannelOptions = new GrpcChannelOptions
+			{
+				HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+			};
+
+			var channel = GrpcChannel.ForAddress(serviceAddress, grpcChannelOptions);
+
+			var earchiveUpdClient = new EarchiveApiTestClient.EarchiveUpd.EarchiveUpdClient(channel);
+
+			var response = earchiveUpdClient.GetAddresses(new EarchiveApiTestClient.CounterpartyInfo { Id = 2 });
+
+			await response.ResponseStream.MoveNext(CancellationToken.None);
+
+			var address = response.ResponseStream.Current;
+
+			return new VodovozHealthResultDto { IsHealthy = address != null };
+		}
+	}
+}
