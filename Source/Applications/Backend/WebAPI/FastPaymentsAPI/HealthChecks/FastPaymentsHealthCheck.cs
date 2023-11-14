21a92e0d3b1fe6d756d85c2bd7d767818f8b5f13
Art8m <9artbe@gmail.com>
2023-11-14 14:29:43 +0300

    FastPayments

---
+++ Source/Applications/Backend/WebAPI/FastPaymentsAPI/HealthChecks/FastPaymentsHealthCheck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FastPaymentsAPI.Library.DTO_s;
+using FastPaymentsAPI.Library.DTO_s.Requests;
+using VodovozHealthCheck;
+using VodovozHealthCheck.Dto;
+using VodovozHealthCheck.Utils;
+
+namespace FastPaymentsAPI.HealthChecks
+{
+	public class FastPaymentsHealthCheck : VodovozHealthCheckBase
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public FastPaymentsHealthCheck(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+		}
+
+		protected override async Task<VodovozHealthResultDto> GetHealthResult()
+		{
+			var healthResult = new VodovozHealthResultDto();
+
+			var fastPaymentControllerResult = await ResponseHelper.GetJsonByUri<OrderDTO>(
+				"https://localhost:7290/api/GetOrderId?orderId=3279687",
+				_httpClientFactory);
+
+			var fastPaymentControllerIsHealthy = fastPaymentControllerResult != null;
+
+			if(!fastPaymentControllerIsHealthy)
+			{
+				healthResult.AdditionalUnhealthyResults.Add("FastPaymentController не прошёл проверку.");
+			}
+
+			var paymentStatusControllerResult = await ResponseHelper.GetJsonByUri<FastPaymentStatusDto>(
+				"https://localhost:7290/api/GetCheckPaymentStatus?orderId=109654126",
+				_httpClientFactory);
+
+			var paymentStatusControllerIsHealthy = paymentStatusControllerResult?.PaymentStatus == RequestPaymentStatus.Performed;
+
+			if(!paymentStatusControllerIsHealthy)
+			{
+				healthResult.AdditionalUnhealthyResults.Add("PaymentStatusController не прошёл проверку.");
+			}
+
+			healthResult.IsHealthy = fastPaymentControllerIsHealthy && paymentStatusControllerIsHealthy;
+
+			return healthResult;
+		}
+	}
+}
