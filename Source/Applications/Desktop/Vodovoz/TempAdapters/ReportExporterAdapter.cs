ce02e680500de4e845dd7f13674496a86e1a70bc
Sergey Efimov <efimov90@gmail.com>
2023-11-15 12:55:23 +0300

    Фикс

--- Source/Applications/Desktop/Vodovoz/TempAdapters/ReportExporterAdapter.cs
+++ Source/Applications/Desktop/Vodovoz/TempAdapters/ReportExporterAdapter.cs
@@ -3,18 +3,22 @@ using QS.Report;
 using QSProjectsLib;
 using RdlEngine;
 using System.IO;
-using Vodovoz.Domain.Orders.Documents;
 using Vodovoz.ViewModels.TempAdapters;
 
 namespace Vodovoz.TempAdapters
 {
 	public class ReportExporterAdapter : IReportExporter 
 	{
+		private const string _hideSignatureParameterName = "hide_signature";
+
 		public void ExportReport(IPrintableRDLDocument printableRDLDocument, string path, bool hideSignature)
 		{
 			var reportInfo = printableRDLDocument.GetReportInfo(QSMain.ConnectionString);
 
-			(printableRDLDocument as ISignableDocument).HideSignature = hideSignature;
+			if(reportInfo.Parameters.ContainsKey(_hideSignatureParameterName))
+			{
+				reportInfo.Parameters[_hideSignatureParameterName] = hideSignature;
+			}
 
 			using(MemoryStream stream = ReportExporter.ExportToMemoryStream(reportInfo.GetReportUri(), reportInfo.GetParametersString(), QSMain.ConnectionString, OutputPresentationType.PDF, overwriteSubreportCon:true))
 			{
