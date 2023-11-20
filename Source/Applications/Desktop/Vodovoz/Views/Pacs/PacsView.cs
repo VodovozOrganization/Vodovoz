a4f98f44a22fa69bea8af7562aa3d0491d64ac17
Vadim Gordeev <enzogord@gmail.com>
2023-11-20 09:10:14 +0300

    Дополнения UI

---
+++ Source/Applications/Desktop/Vodovoz/Views/Pacs/PacsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Pacs
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PacsView : Gtk.Bin
+	{
+		public PacsView()
+		{
+			this.Build();
+		}
+	}
+}
