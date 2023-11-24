3b80c9b9de6873e25d47b2613f0a1118b8654db0
N-Sem <nikiforov.semen@gmail.com>
2023-11-24 09:55:56 +0300

    Добавлен фильтра для журнала геогрупп

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/GeoGroupJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class GeoGroupJournalFilterView : Gtk.Bin
+	{
+		public GeoGroupJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
