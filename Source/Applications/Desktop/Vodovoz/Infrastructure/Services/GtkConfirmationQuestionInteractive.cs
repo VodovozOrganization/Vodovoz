8d48be2b8ad615b524d33c6d0eac25d2a25c03a7
Semen <nikiforov.semen@gmail.com>
2023-11-27 17:59:32 +0300

    Реализовано открытие диалога

--- Source/Applications/Desktop/Vodovoz/Infrastructure/Services/GtkConfirmationQuestionInteractive.cs
+++ Source/Applications/Desktop/Vodovoz/Infrastructure/Services/GtkConfirmationQuestionInteractive.cs
@@ -1,13 +1,19 @@
-﻿using QS.Dialog.GtkUI;
+﻿using Gtk;
+using Vodovoz.Dialogs.Interactive;
 using Vodovoz.Tools.Interactive.ConfirmationQuestion;
 
 namespace Vodovoz.Infrastructure.Services
 {
 	public class GtkConfirmationQuestionInteractive : IConfirmationQuestionInteractive
 	{
-		public bool Question(ConfirmationQuestionDialogInfo dialogInfo, ConfirmationQuestion question1, ConfirmationQuestion question2, ConfirmationQuestion question3)
+		public bool Question(ConfirmationQuestionDialogInfo dialogInfo, ConfirmationQuestion question1, ConfirmationQuestion question2 = null, ConfirmationQuestion question3 = null)
 		{
-			throw new System.NotImplementedException();
+			var dlg = new ConfirmationQuestionDlg(dialogInfo, question1, question2, question3);
+			dlg.SetPosition(WindowPosition.CenterAlways);
+			var result = dlg.Run() == (int)ResponseType.Yes;
+			dlg.Destroy();
+
+			return result;
 		}
 	}
 }
