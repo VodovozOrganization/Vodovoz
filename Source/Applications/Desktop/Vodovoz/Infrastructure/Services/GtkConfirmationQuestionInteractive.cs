612894fc34c5aa7051fc4f684bd165abdd6522da
Semen <34092262+N-Sem@users.noreply.github.com>
2023-11-27 16:34:44 +0300

    Базовая gtk реализация интерфейса

---
+++ Source/Applications/Desktop/Vodovoz/Infrastructure/Services/GtkConfirmationQuestionInteractive.cs
@@ -0,0 +1,13 @@
+﻿using QS.Dialog.GtkUI;
+using Vodovoz.Tools.Interactive.ConfirmationQuestion;
+
+namespace Vodovoz.Infrastructure.Services
+{
+	public class GtkConfirmationQuestionInteractive : IConfirmationQuestionInteractive
+	{
+		public bool Question(ConfirmationQuestionDialogInfo dialogInfo, ConfirmationQuestion question1, ConfirmationQuestion question2, ConfirmationQuestion question3)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
