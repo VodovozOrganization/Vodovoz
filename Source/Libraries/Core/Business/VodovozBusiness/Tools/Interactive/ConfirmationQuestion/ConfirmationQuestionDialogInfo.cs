c317e700da2d1946e84a3b6f59fe4387b14dfc3d
Semen <34092262+N-Sem@users.noreply.github.com>
2023-11-27 16:19:16 +0300

    Добавлен новый интерфейс диалога с подтверждением

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Tools/Interactive/ConfirmationQuestion/ConfirmationQuestionDialogInfo.cs
@@ -0,0 +1,12 @@
+﻿using QS.Dialog;
+
+namespace Vodovoz.Tools.Interactive.ConfirmationQuestion
+{
+	public class ConfirmationQuestionDialogInfo
+	{
+		public string Title { get; set; }
+		public string TopText { get; set; }
+		public string BottomText { get; set; }
+		public ImportanceLevel ImportanceLevel { get; set; }
+	}
+}
---
+++ Source/Libraries/Core/Business/VodovozBusiness/Tools/Interactive/ConfirmationQuestion/IConfirmationQuestionInteractive.cs
@@ -0,0 +1,7 @@
+﻿namespace Vodovoz.Tools.Interactive.ConfirmationQuestion
+{
+	public interface IConfirmationQuestionInteractive
+	{
+		bool Question(ConfirmationQuestionDialogInfo dialogInfo, ConfirmationQuestion question1, ConfirmationQuestion question2, ConfirmationQuestion question3);
+	}
+}
