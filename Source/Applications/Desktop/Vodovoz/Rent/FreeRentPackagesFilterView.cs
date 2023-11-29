b1153f0baa729a06b277209ab41e5ed8938e9c81
Sergey Efimov <efimov90@gmail.com>
2023-11-29 16:13:49 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Rent/FreeRentPackagesFilterView.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using QS.Project.Filter;
+using QS.Views.GtkUI;
+
+namespace Vodovoz.Rent
+{
+	[ToolboxItem(true)]
+	public partial class FreeRentPackagesFilterView
+	 : FilterViewBase<FreeRentPackagesFilterViewModel>
+	{
+		public FreeRentPackagesFilterView(FreeRentPackagesFilterViewModel viewModel)
+			: base(viewModel)
+		{
+			Build();
+		}
+	}
+
+	public class FreeRentPackagesFilterViewModel : FilterViewModelBase<FreeRentPackagesFilterViewModel>
+	{
+	}
+}
