e47d832cedfd3267349b31cbcd3440fa76cccb01
Semen <34092262+N-Sem@users.noreply.github.com>
2023-11-29 16:24:44 +0300

    Изменен диалог

--- Source/Applications/Desktop/Vodovoz/Dialogs/Interactive/ConfirmationQuestionDlg.cs
+++ Source/Applications/Desktop/Vodovoz/Dialogs/Interactive/ConfirmationQuestionDlg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Vodovoz.Tools.Interactive.ConfirmationQuestion;
+using static Vodovoz.Tools.Interactive.ConfirmationQuestion.ConfirmationQuestionDialogSettings;
 
 namespace Vodovoz.Dialogs.Interactive
 {
@@ -11,7 +12,6 @@ namespace Vodovoz.Dialogs.Interactive
 		public ConfirmationQuestionDlg(ConfirmationQuestionDialogSettings dialogInfo, params ConfirmationQuestion[] questions)
 		{
 			_dialogInfo = dialogInfo ?? throw new ArgumentNullException(nameof(dialogInfo));
-
 			_questions = questions ?? new ConfirmationQuestion[0];
 
 			Build();
@@ -20,50 +20,85 @@ namespace Vodovoz.Dialogs.Interactive
 
 		private void Configure()
 		{
+			Title = _dialogInfo.Title ?? string.Empty;
+
+			if(!string.IsNullOrWhiteSpace(_dialogInfo.TopText))
+			{
+				ylabelTopText.Text = _dialogInfo.TopText;
+				ylabelTopText.Visible = true;
+			}
+
+			if(!string.IsNullOrWhiteSpace(_dialogInfo.BottomText))
+			{
+				ylabelBottomText.Text = _dialogInfo.BottomText;
+				ylabelBottomText.Visible = true;
+			}
+
 			ShowImage();
 
-			Title = _dialogInfo.Title;
+			UpdateButtonsSensitive();
 
-			//buttonOk.Sensitive = false;
-			//buttonCancel.Sensitive = true;
+			UpdateQuestions();
+		}
 
+		private void ShowImage()
+		{
+			switch(_dialogInfo.Purpose)
+			{
+				case (DialogPurpose.Question):
+					imageQuestion.Visible = true;
+					break;
+				case DialogPurpose.Warning:
+					imageWarning.Visible = true;
+					break;
+				case DialogPurpose.Error:
+					imageError.Visible = true;
+					break;
+				case DialogPurpose.Info:
+					imageInfo.Visible = true;
+					break;
+				default:
+					throw new ArgumentException("Тип не известен");
+			}
+		}
+
+		private void UpdateQuestions()
+		{
 			if(_questions.Length > 0)
 			{
 				yvboxQuestion1.Visible = true;
 				ylabelQuestion1.Text = _questions[0].QuestionText;
 				ycheckbuttonConfirmation1.Label = _questions[0].ConfirmationText;
-				ycheckbuttonConfirmation1.Clicked += (s, e) => UpdateButtonCancelSensitive();
+				ycheckbuttonConfirmation1.Clicked += (s, e) => UpdateButtonsSensitive();
 			}
-
 
 			if(_questions.Length > 1)
 			{
 				yvboxQuestion2.Visible = true;
 				ylabelQuestion2.Text = _questions[1].QuestionText;
 				ycheckbuttonConfirmation2.Label = _questions[1].ConfirmationText;
-				ycheckbuttonConfirmation2.Clicked += (s, e) => UpdateButtonCancelSensitive();
+				ycheckbuttonConfirmation2.Clicked += (s, e) => UpdateButtonsSensitive();
 			}
 
-			if(_questions.Length > 3)
+			if(_questions.Length > 2)
 			{
 				yvboxQuestion3.Visible = true;
 				ylabelQuestion3.Text = _questions[2].QuestionText;
 				ycheckbuttonConfirmation3.Label = _questions[2].ConfirmationText;
-				ycheckbuttonConfirmation3.Clicked += (s, e) => UpdateButtonCancelSensitive();
+				ycheckbuttonConfirmation3.Clicked += (s, e) => UpdateButtonsSensitive();
 			}
 		}
 
-		private void ShowImage()
+		private void UpdateButtonsSensitive()
 		{
-			imageQuestion.Visible = true;
-		}
+			bool isCheckbuttonsActivated =
+				(_questions.Length < 1 || ycheckbuttonConfirmation1.Active)
+				&& (_questions.Length < 2 || ycheckbuttonConfirmation2.Active)
+				&& (_questions.Length < 3 || ycheckbuttonConfirmation3.Active);
 
-		private void UpdateButtonCancelSensitive()
-		{
-			//buttonOk.Sensitive = true;
-				//ycheckbuttonConfirmation1.Active
-				//&& (_question2 == null || ycheckbuttonConfirmation2.Active)
-				//&& (_question3 == null || ycheckbuttonConfirmation3.Active);
+			buttonYes.Sensitive = _dialogInfo.IsYesAvailableByDefault || isCheckbuttonsActivated;
+
+			buttonNo.Sensitive = _dialogInfo.IsNoAvailableByDefault || isCheckbuttonsActivated;			
 		}
 	}
 }
