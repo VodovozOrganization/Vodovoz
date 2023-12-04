34a01b8707a7edfa5620189f42bd143a380b880d
Semen <34092262+N-Sem@users.noreply.github.com>
2023-12-04 16:14:08 +0300

    Добавлен класс указателя копии района

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Sale/DistrictCopyItem.cs
@@ -0,0 +1,26 @@
+﻿using QS.DomainModel.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Sale
+{
+	public class DistrictCopyItem : PropertyChangedBase, IDomainObject
+	{
+		private District _district;
+		private District _copiedToDistrict;
+		public virtual int Id { get; set; }
+
+		[Display(Name = "Район, который был скопирован")]
+		public virtual District District
+		{ 
+			get => _district; 
+			set => SetField(ref _district, value);
+		}
+
+		[Display(Name = "Копия района")]
+		public virtual District CopiedToDistrict
+		{
+			get => _copiedToDistrict;
+			set => SetField(ref _copiedToDistrict, value);
+		}
+	}
+}
