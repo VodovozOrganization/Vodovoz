850c35e1f38314b7071b30db95790a3b87b1c04d
Semen <34092262+N-Sem@users.noreply.github.com>
2023-12-04 16:47:12 +0300

    DriverDistrictPriorityHelper изменен класс

--- Source/Libraries/Core/Business/VodovozBusiness/Tools/Logistic/DriverDistrictPriorityHelper.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Tools/Logistic/DriverDistrictPriorityHelper.cs
@@ -4,29 +4,35 @@ using Vodovoz.Domain.Sale;
 
 namespace Vodovoz.Tools.Logistic
 {
-    public static class DriverDistrictPriorityHelper
-    {
-        public static DriverDistrictPrioritySet CopyPrioritySetWithActiveDistricts(DriverDistrictPrioritySet districtPrioritySet, out IList<DriverDistrictPriority> notCopiedPriorities)
-        {
-            var copy = (DriverDistrictPrioritySet)districtPrioritySet.Clone();
-            notCopiedPriorities = new List<DriverDistrictPriority>();
-            
-            for(int i = 0; i < copy.DriverDistrictPriorities.Count; i++) {
-                if(copy.DriverDistrictPriorities[i].District.IsActive) {
-                    continue;
-                }
-                var activeDistrict = District.GetDistrictFromActiveDistrictsSetOrNull(copy.DriverDistrictPriorities[i].District);
-                if(activeDistrict != null) {
-                    copy.DriverDistrictPriorities[i].District = activeDistrict;
-                }
-                else {
-                    notCopiedPriorities.Add(districtPrioritySet.DriverDistrictPriorities[i]);
-                    copy.DriverDistrictPriorities.RemoveAt(i);
-                    i--;
-                }
-            }
-            copy.CheckAndFixDistrictsPriorities();
-            return copy;
-        }
-    }
+	public static class DriverDistrictPriorityHelper
+	{
+		public static DriverDistrictPrioritySet CopyPrioritySetWithActiveDistricts(DriverDistrictPrioritySet districtPrioritySet, out IList<DriverDistrictPriority> notCopiedPriorities)
+		{
+			var copy = (DriverDistrictPrioritySet)districtPrioritySet.Clone();
+			notCopiedPriorities = new List<DriverDistrictPriority>();
+
+			for(int i = 0; i < copy.DriverDistrictPriorities.Count; i++)
+			{
+				if(copy.DriverDistrictPriorities[i].District.IsActive)
+				{
+					continue;
+				}
+
+				var activeDistrict = District.GetDistrictFromActiveDistrictsSetOrNull(copy.DriverDistrictPriorities[i].District);
+
+				if(activeDistrict != null)
+				{
+					copy.DriverDistrictPriorities[i].District = activeDistrict;
+				}
+				else
+				{
+					notCopiedPriorities.Add(districtPrioritySet.DriverDistrictPriorities[i]);
+					copy.DriverDistrictPriorities.RemoveAt(i);
+					i--;
+				}
+			}
+			copy.CheckAndFixDistrictsPriorities();
+			return copy;
+		}
+	}
 }
