52a821a4f24aa913557b00d69c915432212a0d9b
Art8m <9artbe@gmail.com>
2023-12-04 22:19:49 +0300

    Убрал лишнее для фильтра по статусу документооборота в ЭДО

--- Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrderFilterView.cs
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrderFilterView.cs
@@ -146,8 +146,7 @@ namespace Vodovoz.Filters.GtkViews
 			searchByAddressView.Show();
 
 			enumCmbEdoDocFlowStatus.ItemsEnum = typeof(EdoDocFlowStatus);
-			enumCmbEdoDocFlowStatus.SelectedItemStrictTyped = false;
-			enumCmbEdoDocFlowStatus.Binding.AddBinding(ViewModel, vm => vm.EdoDocFlowStatus, w => w.SelectedItemOrNull).InitializeFromSource();
+			enumCmbEdoDocFlowStatus.Binding.AddBinding(ViewModel, vm => vm.EdoDocFlowStatus, w => w.SelectedItem).InitializeFromSource();
 		}
 
 		private void InitializeRestrictions()
