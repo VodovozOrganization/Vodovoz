afa38c1740ad7310aba4703cb6b88fea9dad3c35
Andrey Shaklein <as1854@yandex.ru>
2023-12-07 21:52:17 +0300

    новый диалог для поиска расхождений в платежах

---
+++ Source/Applications/Desktop/Vodovoz/Views/Payments/PaymentsDiscrepanciesAnalysisView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using QS.Views.Dialog;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Payments;
+
+namespace Vodovoz.Views.Payments
+{
+	public partial class PaymentsDiscrepanciesAnalysisView : DialogViewBase<PaymentsDiscrepanciesAnalysisViewModel>
+	{
+		public PaymentsDiscrepanciesAnalysisView(PaymentsDiscrepanciesAnalysisViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/PaymentsDiscrepanciesAnalysisViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.ViewModels.Dialog;
+using QS.Navigation;
+namespace Vodovoz.ViewModels.ViewModels.Payments
+{
+	public class PaymentsDiscrepanciesAnalysisViewModel : DialogViewModelBase
+	{
+		public PaymentsDiscrepanciesAnalysisViewModel(
+			INavigationManager navigationManager) : base(navigationManager)
+		{
+
+		}
+	}
+}
