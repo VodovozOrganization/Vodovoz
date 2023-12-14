420a8fd8aa1d6df31b41348315d5b24b5466ef49
Andrey Shaklein <as1854@yandex.ru>
2023-12-15 00:32:47 +0300

    фикс фабрики

--- Source/Applications/Desktop/Vodovoz/Factories/PhonesViewModelFactory.cs
+++ Source/Applications/Desktop/Vodovoz/Factories/PhonesViewModelFactory.cs
@@ -1,13 +1,5 @@
 ﻿using Autofac;
-using QS.Dialog.GtkUI.FileDialog;
 using QS.DomainModel.UoW;
-using QS.Project.Services;
-using QS.Project.Services.FileDialog;
-using System;
-using Vodovoz.EntityRepositories;
-using Vodovoz.Parameters;
-using Vodovoz.TempAdapters;
-using Vodovoz.Tools;
 using Vodovoz.ViewModels.Journals.JournalFactories;
 using Vodovoz.ViewModels.ViewModels.Contacts;
 
@@ -16,6 +8,8 @@ namespace Vodovoz.Factories
 	public class PhonesViewModelFactory : IPhonesViewModelFactory
 	{
 		public PhonesViewModel CreateNewPhonesViewModel(ILifetimeScope lifetimeScope, IUnitOfWork uow) =>
-			lifetimeScope.Resolve<PhonesViewModel>(new TypedParameter(typeof(IUnitOfWork), uow));
+			lifetimeScope.Resolve<PhonesViewModel>(
+				new TypedParameter(typeof(IUnitOfWork), uow),
+				new TypedParameter(typeof(RoboatsJournalsFactory), lifetimeScope.Resolve<RoboatsJournalsFactory>()));
 	}
 }
