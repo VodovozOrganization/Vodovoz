24182aa33e7b754eae22b0d0529f0135fbe2fefa
Art8m <9artbe@gmail.com>
2023-12-26 00:30:51 +0300

    Изменение отчета по произведенной продукции

--- Source/Applications/Desktop/Vodovoz/ReportsParameters/Production/ProducedProductionReport.cs
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Production/ProducedProductionReport.cs
@@ -1,16 +1,23 @@
-﻿using QS.Dialog.GtkUI;
+﻿using Autofac;
+using NHibernate.Linq;
+using QS.Dialog.GtkUI;
 using QS.DomainModel.UoW;
+using QS.Project.Services;
 using QS.Report;
 using QSReport;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using Autofac;
+using System.Linq.Expressions;
 using Vodovoz.CommonEnums;
 using Vodovoz.Domain.Goods;
 using Vodovoz.Domain.Store;
+using Vodovoz.EntityRepositories;
+using Vodovoz.Extensions;
+using Vodovoz.Presentation.ViewModels.Common;
 using Vodovoz.TempAdapters;
+using Vodovoz.ViewWidgets.Reports;
 
 namespace Vodovoz.ReportsParameters.Production
 {
@@ -19,9 +26,15 @@ namespace Vodovoz.ReportsParameters.Production
 	{
 		private ILifetimeScope _lifetimeScope = Startup.AppDIContainer.BeginLifetimeScope();
 		private readonly INomenclatureJournalFactory _nomenclatureJournalFactory;
+		private readonly IGenericRepository<Nomenclature> _nomenclatureRepository;
+		private readonly IGenericRepository<Warehouse> _warehouseRepository;
+		private readonly IncludeExludeFiltersViewModel _filterViewModel;
 
 		public ProducedProductionReport(INomenclatureJournalFactory nomenclatureJournalFactory)
-		{
+		{		
+			_nomenclatureRepository = _lifetimeScope.Resolve<IGenericRepository<Nomenclature>>();
+			_warehouseRepository = _lifetimeScope.Resolve<IGenericRepository<Warehouse>>();
+
 			_nomenclatureJournalFactory = nomenclatureJournalFactory ?? throw new ArgumentNullException(nameof(nomenclatureJournalFactory));
 			Build();
 			
@@ -32,15 +45,93 @@ namespace Vodovoz.ReportsParameters.Production
             ylistcomboboxYear.ItemsList = Enumerable.Range(DateTime.Now.AddYears(-10).Year, 21).Reverse();
             ylistcomboboxYear.SelectedItem = DateTime.Today.Year;
 
-            /*ycomboboxProduction.SetRenderTextFunc<Warehouse>(x => x.Name);
-			ycomboboxProduction.ItemsList = UoW.Session.QueryOver<Warehouse>().Where(x => x.TypeOfUse == WarehouseUsing.Production).List();
+			_filterViewModel = CreateReportIncludeExcludeFilter(UoW);
 
-			entryreferenceNomenclature.SetEntityAutocompleteSelectorFactory(
-				_nomenclatureJournalFactory.GetDefaultNomenclatureSelectorFactory(_lifetimeScope));*/
+			var filterView = new IncludeExludeFiltersView(_filterViewModel);
+			vboxParameters.Add(filterView);
+			filterView.Show();
+
 			buttonCreateReport.Sensitive = true;
 			buttonCreateReport.Clicked += OnButtonCreateReportClicked;
 		}
-		
+
+		private IncludeExludeFiltersViewModel CreateReportIncludeExcludeFilter(IUnitOfWork unitOfWork)
+		{
+			var includeExcludeFiltersViewModel = new IncludeExludeFiltersViewModel(ServicesConfig.InteractiveService);
+
+			includeExcludeFiltersViewModel.AddFilter(unitOfWork, _nomenclatureRepository, config =>
+			{
+				config.Title = "Номенклатура";
+
+				config.RefreshFunc = (IncludeExcludeEntityFilter<Nomenclature> filter) =>
+				{
+					Expression<Func<Nomenclature, bool>> specificationExpression = null;
+
+					Expression<Func<Nomenclature, bool>> searchInFullNameSpec = nomenclature =>
+						(string.IsNullOrWhiteSpace(includeExcludeFiltersViewModel.CurrentSearchString)
+						|| nomenclature.Name.ToLower().Like($"%{includeExcludeFiltersViewModel.CurrentSearchString.ToLower()}%"))
+						&& nomenclature.Category == NomenclatureCategory.water
+						&& (includeExcludeFiltersViewModel.ShowArchived || !nomenclature.IsArchive);
+
+					specificationExpression = specificationExpression.CombineWith(searchInFullNameSpec);
+
+					var elementsToAdd = _nomenclatureRepository.Get(
+							unitOfWork,
+							specificationExpression,
+							limit: IncludeExludeFiltersViewModel.DefaultLimit)
+						.Select(x => new IncludeExcludeElement<int, Nomenclature>
+						{
+							Id = x.Id,
+							Title = x.Name
+						});
+
+					filter.FilteredElements.Clear();
+
+					foreach(var element in elementsToAdd)
+					{
+						filter.FilteredElements.Add(element);
+					}
+				};
+			});
+
+			includeExcludeFiltersViewModel.AddFilter(unitOfWork, _warehouseRepository, config =>
+			{
+				config.Title = "Производство";
+
+				config.RefreshFunc = (IncludeExcludeEntityFilter<Warehouse> filter) =>
+				{
+					Expression<Func<Warehouse, bool>> specificationExpression = null;
+
+					Expression<Func<Warehouse, bool>> searchInFullNameSpec = warehouse =>
+						(string.IsNullOrWhiteSpace(includeExcludeFiltersViewModel.CurrentSearchString)
+						|| warehouse.Name.ToLower().Like($"%{includeExcludeFiltersViewModel.CurrentSearchString.ToLower()}%"))
+						&& warehouse.TypeOfUse == WarehouseUsing.Production
+						&& (includeExcludeFiltersViewModel.ShowArchived || !warehouse.IsArchive);
+
+					specificationExpression = specificationExpression.CombineWith(searchInFullNameSpec);
+
+					var elementsToAdd = _warehouseRepository.Get(
+							unitOfWork,
+							specificationExpression,
+							limit: IncludeExludeFiltersViewModel.DefaultLimit)
+						.Select(x => new IncludeExcludeElement<int, Warehouse>
+						{
+							Id = x.Id,
+							Title = x.Name
+						});
+
+					filter.FilteredElements.Clear();
+
+					foreach(var element in elementsToAdd)
+					{
+						filter.FilteredElements.Add(element);
+					}
+				};
+			});
+
+			return includeExcludeFiltersViewModel;
+		}
+
 		void OnButtonCreateReportClicked (object sender, EventArgs e)
 		{
 			OnUpdate (true);
@@ -55,8 +146,7 @@ namespace Vodovoz.ReportsParameters.Production
 		#endregion
 
 		private ReportInfo GetReportInfo()
-		{
-			
+		{			
 			CultureInfo ci = new CultureInfo("ru-RU");
 			DateTimeFormatInfo mfi = ci.DateTimeFormat;
 
@@ -72,29 +162,65 @@ namespace Vodovoz.ReportsParameters.Production
 			var monthNumMinus2 = reportDate.AddMonths(-2).Month;
 			string strMonthNameMinus2 = mfi.GetMonthName(monthNumMinus2).ToString();
 
-            return new ReportInfo {
-				Identifier = "Production.ProducedProduction",
-				Parameters = new Dictionary<string, object> {
-					{ "month_start",         MonthStart(reportDate) },
-					{ "month_end",             MonthEnd(reportDate) },
-					{ "month_minus_1_start", MonthStart(reportDate.AddMonths(-1)) },
-					{ "month_minus_1_end",     MonthEnd(reportDate.AddMonths(-1)) },
-					{ "month_minus_2_start", MonthStart(reportDate.AddMonths(-2)) },
-					{ "month_minus_2_end",     MonthEnd(reportDate.AddMonths(-2)) },
-					{ "month_name",    strMonthName },
-					{ "month_name_minus_1",  strMonthNameMinus1  },
-					{ "month_name_minus_2",  strMonthNameMinus2  },
-					
-					{ "nomenclature_id", (entryreferenceNomenclature.Subject as Nomenclature)?.Id ?? -1},
-					{ "warehouse_id", (ycomboboxProduction.SelectedItem as Warehouse)?.Id ?? -1 },
-					{ "creation_date", DateTime.Now},
-					
-					{ "nomenclature_footer_name", (entryreferenceNomenclature.Subject as Nomenclature)?.Name ?? ""},
-					{ "warehouse_footer_name", (ycomboboxProduction.SelectedItem as Warehouse)?.Name ?? "" },
-					
-				}
-			};
+            var strYearNameMinus1 = $"{strMonthName} {year - 1}";
+
+            var includedNomenclatureIds = _filterViewModel
+				.GetIncludedElements<Nomenclature>()
+				.Select(x => (x as IncludeExcludeElement<int, Nomenclature>).Id)
+				.ToArray();
+
+			var excludeNomenclatureIds = _filterViewModel
+				.GetExcludedElements<Nomenclature>()
+				.Select(x => (x as IncludeExcludeElement<int, Nomenclature>).Id)
+				.ToArray();
+
+			var includedWarehouseIds = _filterViewModel
+				.GetIncludedElements<Warehouse>()
+				.Select(x => (x as IncludeExcludeElement<int, Warehouse>).Id)
+				.ToArray();
+
+			var excludeWarehouseIds = _filterViewModel
+				.GetExcludedElements<Warehouse>()
+				.Select(x => (x as IncludeExcludeElement<int, Warehouse>).Id)
+				.ToArray();
+
+
+            Dictionary<string, object> parameters = _filterViewModel.GetReportParametersSet();
+
+            parameters.Add( "month_start", MonthStart(reportDate) );
+
+            parameters.Add("month_end", MonthEnd(reportDate));
+            parameters.Add("month_minus_1_start", MonthStart(reportDate.AddMonths(-1)));
+            parameters.Add("month_minus_1_end", MonthEnd(reportDate.AddMonths(-1)));
+            parameters.Add("month_name", strMonthName);
+            parameters.Add("month_name_minus_1", strMonthNameMinus1);            
+            parameters.Add("creation_date", DateTime.Now);
+
+            var identifier = string.Empty;
+
+            if(IsForYearMode)
+            {
+                identifier = "Production.ProducedProductionYear";
+                parameters.Add("year_name_minus_1", strYearNameMinus1);
+                parameters.Add("year_minus_1_start", MonthStart(reportDate.AddYears(-1)));
+                parameters.Add("year_minus_1_end", MonthEnd(reportDate.AddYears(-1)));
+            }
+			else
+			{
+                identifier = "Production.ProducedProduction";
+                parameters.Add("month_name_minus_2", strMonthNameMinus2);
+                parameters.Add("month_minus_2_start", MonthStart(reportDate.AddMonths(-2)));
+                parameters.Add("month_minus_2_end", MonthEnd(reportDate.AddMonths(-2)));
+            }
+
+            return new ReportInfo
+            {
+                Identifier = identifier,
+                Parameters = parameters
+            };
 		}
+
+        public bool IsForYearMode { get; } = true;
 
 		void OnUpdate(bool hide = false) => LoadReport?.Invoke(this, new LoadReportEventArgs(GetReportInfo(), hide));
 
