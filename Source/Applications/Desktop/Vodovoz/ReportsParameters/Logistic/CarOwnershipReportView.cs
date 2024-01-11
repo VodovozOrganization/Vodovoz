d047140b8dfba95a4ba6fc11b5b5033fb235b8b1
N-Sem <nikiforov.semen@gmail.com>
2024-01-11 17:59:22 +0300

    Добавлена вью отчета

---
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Logistic/CarOwnershipReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ReportsParameters.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarOwnershipReportView : Gtk.Bin
+	{
+		public CarOwnershipReportView()
+		{
+			this.Build();
+		}
+	}
+}
