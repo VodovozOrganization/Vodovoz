dd0752c516c4127d0e486d591607e251f84af489
Semen <nikiforov.semen@gmail.com>
2024-01-12 13:48:14 +0300

    Добавлены привязки во вью отчета

--- Source/Applications/Desktop/Vodovoz/ReportsParameters/Logistic/CarOwnershipReportView.cs
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Logistic/CarOwnershipReportView.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Gamma.Widgets.Additions;
+using QS.Views;
+using Vodovoz.Domain.Logistic.Cars;
+using Vodovoz.ViewModels.ReportsParameters.Logistic.CarOwnershipReport;
 namespace Vodovoz.ReportsParameters.Logistic
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class CarOwnershipReportView : Gtk.Bin
+	public partial class CarOwnershipReportView : ViewBase<CarOwnershipReportViewModel>
 	{
-		public CarOwnershipReportView()
+		public CarOwnershipReportView(CarOwnershipReportViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			enumcheckCarTypeOfUse.EnumType = typeof(CarTypeOfUse);
+			enumcheckCarTypeOfUse.Binding
+				.AddBinding(ViewModel, vm => vm.SelectedCarTypesOfUse, w => w.SelectedValuesList, new EnumsListConverter<CarTypeOfUse>())
+				.InitializeFromSource();
+
+			enumcheckCarOwnType.EnumType = typeof(CarOwnType);
+			enumcheckCarOwnType.Binding
+				.AddBinding(ViewModel, vm => vm.SelectedCarOwnTypes, w => w.SelectedValuesList, new EnumsListConverter<CarOwnType>())
+				.InitializeFromSource();
+
+			yradiobuttonCarOwnTypeOnDate.Binding
+				.AddBinding(ViewModel, vm => vm.IsOneDayReportSelected, w => w.Active)
+				.InitializeFromSource();
+
+			yradiobuttonCarOwnTypeOnPeriod.Binding
+				.AddBinding(ViewModel, vm => vm.IsPeriodReportSelected, w => w.Active)
+				.InitializeFromSource();
+
+			datepickerDate.Binding
+				.AddBinding(ViewModel, vm => vm.DateInOneDayReport, w => w.DateOrNull)
+				.InitializeFromSource();
+			datepickerDate.IsEditable = true;
+
+			daterangepickerPeriod.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.StartDateInPeriodReport, w => w.StartDateOrNull)
+				.AddBinding(vm => vm.EndDateInPeriodReport, w => w.EndDateOrNull)
+				.InitializeFromSource();
+
+			yenumcomboboxStep.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.PeriodReportModes, w => w.ItemsEnum)
+				.AddBinding(vm => vm.SelectedPeriodReportMode, w => w.SelectedItem)
+				.InitializeFromSource();
+
+			yhboxDateSettings.Binding
+				.AddBinding(ViewModel, vm => vm.IsOneDayReportSelected, w => w.Sensitive)
+				.InitializeFromSource();
+
+			ytablePeriodSettings.Binding
+				.AddBinding(ViewModel, vm => vm.IsPeriodReportSelected, w => w.Sensitive)
+				.InitializeFromSource();
+
+			ybuttonCreateReport.Clicked += (s, e) => ViewModel.GenerateReportCommand.Execute();
 		}
 	}
 }
