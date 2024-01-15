100eedb7f03979523208fed70b61593ff46d3b64
Sergey Efimov <efimov90@gmail.com>
2024-01-15 14:09:05 +0300

    Фикс прав
    Новое право
    И документирование старых прав заказа

--- Source/Libraries/Core/Business/VodovozBusiness/Permissions/Order.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Permissions/Order.cs
@@ -1,18 +1,45 @@
-﻿namespace Vodovoz.Permissions
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Permissions
 {
 	/// <summary>
 	/// Права заказы
 	/// </summary>
 	public static partial class Order
 	{
+		/// <summary>
+		/// Может активировать акцию скидка на второй заказ
+		/// </summary>
+		[Display(
+			Name = "Может активировать акцию скидка на второй заказ",
+			Description = "Может активировать акцию скидка на второй заказ")]
 		public static string CanActivateClientsSecondOrderDiscount => "can_activate_clients_second_order_discount";
 
 		/// <summary>
 		/// Пользователь может формировать заказ для ликвидированного контрагента
 		/// </summary>
+		[Display(
+			Name = "Пользователь может формировать заказ для ликвидированного контрагента",
+			Description = "Пользователь может формировать заказ для ликвидированного контрагента")]
 		public static string CanFormOrderWithLiquidatedCounterparty =>
 			"can_form_order_with_liquidated_counterparty";
 
+		/// <summary>
+		/// Настройка для редактирование поля "Ожидает до" в заказах<br/>
+		/// Разрешить управлять настройкой для редактирования поля "Ожидает до" в заказах
+		/// </summary>
+		[Display(
+			Name = "Настройка для редактирование поля \"Ожидает до\" в заказах",
+			Description = "Разрешить управлять настройкой для редактирования поля \"Ожидает до\" в заказах")]
 		public static string CanEditOrderWaitUntil => nameof(CanEditOrderWaitUntil);
+
+		/// <summary>
+		/// Изменение заказа при наличии кассового чека<br/>
+		/// Пользователь может изменять товары, цены, контрагента в заказе даже при наличии кассового чека по данному заказу
+		/// </summary>
+		[Display(
+			Name = "Изменение заказа при наличии кассового чека",
+			Description = "Пользователь может изменять товары, цены, контрагента в заказе даже при наличии кассового чека по данному заказу")]
+		public static string CanChangeOrderAfterRecieptCreated => nameof(CanChangeOrderAfterRecieptCreated);
 	}
 }
