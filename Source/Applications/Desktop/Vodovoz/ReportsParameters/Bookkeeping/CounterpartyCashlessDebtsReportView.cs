2b6af208babdba1401ffa1ce87e44f07bab7b923
N-Sem <nikiforov.semen@gmail.com>
2024-01-22 16:32:01 +0300

    Новая вью отчета

---
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Bookkeeping/CounterpartyCashlessDebtsReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ReportsParameters.Bookkeeping
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CounterpartyCashlessDebtsReportView : Gtk.Bin
+	{
+		public CounterpartyCashlessDebtsReportView()
+		{
+			this.Build();
+		}
+	}
+}
