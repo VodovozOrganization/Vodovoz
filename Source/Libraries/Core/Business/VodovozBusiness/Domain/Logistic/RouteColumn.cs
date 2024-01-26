a0ac31f4dbc99ea4c3728c503100b6cd174fc5a3
Semen <nikiforov.semen@gmail.com>
2024-01-26 16:35:43 +0300

    Добавлены новые свойства в колонки номенклатур

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Logistic/RouteColumn.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Logistic/RouteColumn.cs
@@ -10,21 +10,37 @@ namespace Vodovoz.Domain.Logistic
 		Nominative = "колонка маршрутного листа")]
 	public class RouteColumn : PropertyChangedBase, IDomainObject
 	{
+		private string _name;
+		private string _shortName;
+		private bool _isHighlighted;
 		public virtual int Id { get; set; }
-
-		string name;
 
 		[Display (Name = "Название")]
 		[Required (ErrorMessage = "Название номенклатуры должно быть заполнено.")]
 		[StringLength(20)]
 		public virtual string Name {
-			get { return name; }
-			set { SetField (ref name, value, () => Name); }
+			get => _name;
+			set => SetField(ref _name, value);
+		}
+
+		[Display(Name = "Название")]
+		[StringLength(3)]
+		public virtual string ShortName
+		{
+			get => _shortName;
+			set => SetField(ref _shortName, value);
+		}
+
+		[Display(Name = "Ячейка выделена")]
+		public virtual bool IsHighlighted
+		{
+			get => _isHighlighted;
+			set => SetField(ref _isHighlighted, value);
 		}
 
 		public RouteColumn ()
 		{
-			Name = String.Empty;
+			Name = string.Empty;
 		}
 	}
 }
