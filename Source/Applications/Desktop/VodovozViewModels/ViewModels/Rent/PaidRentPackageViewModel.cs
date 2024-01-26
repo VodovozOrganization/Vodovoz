33e1b50b633705361ee4e11a2af20e82d2d5cffc
Sergey Efimov <efimov90@gmail.com>
2024-01-26 14:07:48 +0300

    Пакеты платной аренды

--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Rent/PaidRentPackageViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Rent/PaidRentPackageViewModel.cs
@@ -1,43 +1,75 @@
-﻿using NHibernate;
+﻿using Autofac;
+using NHibernate;
 using NHibernate.Criterion;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
 using QS.Services;
 using QS.ViewModels;
+using QS.ViewModels.Control.EEVM;
 using System;
 using Vodovoz.Domain;
 using Vodovoz.Domain.Goods;
 using Vodovoz.EntityRepositories.RentPackages;
+using Vodovoz.ViewModels.Dialogs.Goods;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Goods;
+using Vodovoz.ViewModels.Journals.JournalViewModels.Goods;
 
 namespace Vodovoz.ViewModels.ViewModels.Rent
 {
 	public class PaidRentPackageViewModel : EntityTabViewModelBase<PaidRentPackage>
-    {
+	{
 		private readonly IRentPackageRepository _rentPackageRepository;
 
 		public PaidRentPackageViewModel(
-            IEntityUoWBuilder uowBuilder,
-            IUnitOfWorkFactory unitOfWorkFactory,
-            ICommonServices commonServices,
+			IEntityUoWBuilder uowBuilder,
+			IUnitOfWorkFactory unitOfWorkFactory,
+			ICommonServices commonServices,
 			INavigationManager navigationManager,
-            IRentPackageRepository rentPackageRepository)
+			IRentPackageRepository rentPackageRepository,
+			ILifetimeScope lifetimeScope)
 			: base(uowBuilder, unitOfWorkFactory, commonServices, navigationManager)
-        {
+		{
 			_rentPackageRepository = rentPackageRepository ?? throw new ArgumentNullException(nameof(rentPackageRepository));
 
-	        NomenclatureCriteria = UoW.Session.CreateCriteria<Nomenclature>();
-	        DepositNomenclatureCriteria = NomenclatureCriteria.Add(Restrictions.Eq("Category", NomenclatureCategory.deposit));
+			NomenclatureCriteria = UoW.Session.CreateCriteria<Nomenclature>();
+			DepositNomenclatureCriteria = NomenclatureCriteria.Add(Restrictions.Eq("Category", NomenclatureCategory.deposit));
 
-	        ConfigureValidateContext();
-        }
-        
-        public ICriteria DepositNomenclatureCriteria { get; }
-        public ICriteria NomenclatureCriteria { get; }
+			ConfigureValidateContext();
+
+			DepositServiceNomenclatureViewModel = new CommonEEVMBuilderFactory<PaidRentPackage>(this, Entity, UoW, NavigationManager, lifetimeScope)
+				.ForProperty(x => x.DepositService)
+				.UseViewModelJournalAndAutocompleter<NomenclaturesJournalViewModel, NomenclatureFilterViewModel>(filter =>
+				{
+					filter.RestrictCategory = NomenclatureCategory.deposit;
+				})
+				.UseViewModelDialog<NomenclatureViewModel>()
+				.Finish();
+
+			DailyRentServiceNomenclatureViewModel = new CommonEEVMBuilderFactory<PaidRentPackage>(this, Entity, UoW, NavigationManager, lifetimeScope)
+				.ForProperty(x => x.RentServiceDaily)
+				.UseViewModelJournalAndAutocompleter<NomenclaturesJournalViewModel>()
+				.UseViewModelDialog<NomenclatureViewModel>()
+				.Finish();
+
+
+			LongTermRentNomenclatureServiceViewModel = new CommonEEVMBuilderFactory<PaidRentPackage>(this, Entity, UoW, NavigationManager, lifetimeScope)
+				.ForProperty(x => x.RentServiceMonthly)
+				.UseViewModelJournalAndAutocompleter<NomenclaturesJournalViewModel>()
+				.UseViewModelDialog<NomenclatureViewModel>()
+				.Finish();
+		}
+
+		public ICriteria DepositNomenclatureCriteria { get; }
+		public ICriteria NomenclatureCriteria { get; }
+
+		public IEntityEntryViewModel DepositServiceNomenclatureViewModel { get; }
+		public IEntityEntryViewModel DailyRentServiceNomenclatureViewModel { get; }
+		public IEntityEntryViewModel LongTermRentNomenclatureServiceViewModel { get; }
 
 		private void ConfigureValidateContext()
-        {
-	        ValidationContext.ServiceContainer.AddService(typeof(IRentPackageRepository), _rentPackageRepository);
-        }
+		{
+			ValidationContext.ServiceContainer.AddService(typeof(IRentPackageRepository), _rentPackageRepository);
+		}
 	}
 }
