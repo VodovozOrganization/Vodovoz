9f97c3357b96a1ba39f45c450256c2299819f95d
Sergey Efimov <efimov90@gmail.com>
2024-01-26 12:24:50 +0300

    Фикс

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Store/ReceptionItemNode.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QS.DomainModel.Entity;
+using Vodovoz.Domain.Documents;
+using Vodovoz.Domain.Goods;
+using Vodovoz.Domain.Operations;
+using Vodovoz.Domain.Service;
+
+namespace Vodovoz
+{
+	public class ReceptionItemNode : PropertyChangedBase
+	{
+		private decimal _amount;
+		private decimal _expectedAmount;
+
+		public NomenclatureCategory NomenclatureCategory { get; set; }
+		public int NomenclatureId { get; set; }
+		public string Name { get; set; }
+
+		public virtual decimal Amount {
+			get => _amount;
+			set => SetField(ref _amount, value, () => Amount);
+		}
+
+		public virtual decimal ExpectedAmount {
+			get => _expectedAmount;
+			set => SetField(ref _expectedAmount, value, () => ExpectedAmount);
+		}
+
+		int equipmentId;
+		[PropertyChangedAlso("Serial")]
+		public int EquipmentId {
+			get => equipmentId;
+			set => SetField(ref equipmentId, value, () => EquipmentId);
+		}
+
+		[Display(Name = "№ кулера")]
+		public string Redhead {
+			get => CarUnloadDocumentItem.Redhead;
+			set {
+				if(value != CarUnloadDocumentItem.Redhead)
+					CarUnloadDocumentItem.Redhead = value;
+			}
+		}
+
+		ServiceClaim serviceClaim;
+
+		public virtual ServiceClaim ServiceClaim {
+			get => serviceClaim;
+			set => SetField(ref serviceClaim, value, () => ServiceClaim);
+		}
+
+		public Equipment NewEquipment { get; set; }
+		public bool Returned {
+			get => Amount > 0;
+			set => Amount = value ? 1 : 0;
+		}
+
+		GoodsAccountingOperation movementOperation = new GoodsAccountingOperation();
+
+		public virtual GoodsAccountingOperation MovementOperation {
+			get => movementOperation;
+			set => SetField(ref movementOperation, value, () => MovementOperation);
+		}
+
+		public ReceptionItemNode(Nomenclature nomenclature, int amount)
+		{
+			Name = nomenclature.Name;
+			NomenclatureId = nomenclature.Id;
+			NomenclatureCategory = nomenclature.Category;
+			_amount = amount;
+		}
+
+		public ReceptionItemNode(GoodsAccountingOperation movementOperation) : this(movementOperation.Nomenclature, (int)movementOperation.Amount)
+		{
+			this.movementOperation = movementOperation;
+		}
+
+		CarUnloadDocumentItem carUnloadDocumentItem = new CarUnloadDocumentItem();
+
+		public virtual CarUnloadDocumentItem CarUnloadDocumentItem {
+			get => carUnloadDocumentItem;
+			set => SetField(ref carUnloadDocumentItem, value, () => CarUnloadDocumentItem);
+		}
+
+		public ReceptionItemNode(CarUnloadDocumentItem carUnloadDocumentItem) : this(carUnloadDocumentItem.GoodsAccountingOperation)
+		{
+			this.carUnloadDocumentItem = carUnloadDocumentItem;
+		}
+
+		public ReceptionItemNode() { }
+	}
+}
+
--- Source/Applications/Desktop/Vodovoz/ViewWidgets/Store/ReturnsReceptionView.cs
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Store/ReturnsReceptionView.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Data.Bindings.Collections.Generic;
 using System.Linq;
 using Autofac;
 using NHibernate.Criterion;
 using NHibernate.Transform;
-using QS.DomainModel.Entity;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Journal;
@@ -17,7 +15,6 @@ using Vodovoz.Domain.Goods;
 using Vodovoz.Domain.Logistic;
 using Vodovoz.Domain.Operations;
 using Vodovoz.Domain.Orders;
-using Vodovoz.Domain.Service;
 using Vodovoz.Domain.Store;
 using Vodovoz.EntityRepositories.Logistic;
 using Vodovoz.EntityRepositories.Stock;
@@ -355,89 +352,6 @@ namespace Vodovoz
 			_lifetimeScope?.Dispose();
 			_lifetimeScope = null;
 		}
-	}
-
-	public class ReceptionItemNode : PropertyChangedBase
-	{
-		private decimal _amount;
-		private decimal _expectedAmount;
-
-		public NomenclatureCategory NomenclatureCategory { get; set; }
-		public int NomenclatureId { get; set; }
-		public string Name { get; set; }
-
-		public virtual decimal Amount {
-			get => _amount;
-			set => SetField(ref _amount, value, () => Amount);
-		}
-
-		public virtual decimal ExpectedAmount {
-			get => _expectedAmount;
-			set => SetField(ref _expectedAmount, value, () => ExpectedAmount);
-		}
-
-		int equipmentId;
-		[PropertyChangedAlso("Serial")]
-		public int EquipmentId {
-			get => equipmentId;
-			set => SetField(ref equipmentId, value, () => EquipmentId);
-		}
-
-		[Display(Name = "№ кулера")]
-		public string Redhead {
-			get => CarUnloadDocumentItem.Redhead;
-			set {
-				if(value != CarUnloadDocumentItem.Redhead)
-					CarUnloadDocumentItem.Redhead = value;
-			}
-		}
-
-		ServiceClaim serviceClaim;
-
-		public virtual ServiceClaim ServiceClaim {
-			get => serviceClaim;
-			set => SetField(ref serviceClaim, value, () => ServiceClaim);
-		}
-
-		public Equipment NewEquipment { get; set; }
-		public bool Returned {
-			get => Amount > 0;
-			set => Amount = value ? 1 : 0;
-		}
-
-		GoodsAccountingOperation movementOperation = new GoodsAccountingOperation();
-
-		public virtual GoodsAccountingOperation MovementOperation {
-			get => movementOperation;
-			set => SetField(ref movementOperation, value, () => MovementOperation);
-		}
-
-		public ReceptionItemNode(Nomenclature nomenclature, int amount)
-		{
-			Name = nomenclature.Name;
-			NomenclatureId = nomenclature.Id;
-			NomenclatureCategory = nomenclature.Category;
-			_amount = amount;
-		}
-
-		public ReceptionItemNode(GoodsAccountingOperation movementOperation) : this(movementOperation.Nomenclature, (int)movementOperation.Amount)
-		{
-			this.movementOperation = movementOperation;
-		}
-
-		CarUnloadDocumentItem carUnloadDocumentItem = new CarUnloadDocumentItem();
-
-		public virtual CarUnloadDocumentItem CarUnloadDocumentItem {
-			get => carUnloadDocumentItem;
-			set => SetField(ref carUnloadDocumentItem, value, () => CarUnloadDocumentItem);
-		}
-
-		public ReceptionItemNode(CarUnloadDocumentItem carUnloadDocumentItem) : this(carUnloadDocumentItem.GoodsAccountingOperation)
-		{
-			this.carUnloadDocumentItem = carUnloadDocumentItem;
-		}
-
-		public ReceptionItemNode() { }
 	}
 }
 
