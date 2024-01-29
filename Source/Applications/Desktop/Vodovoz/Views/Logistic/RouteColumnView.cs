152713f02ae468d6ac412be003b53cffbd7b46f6
N-Sem <nikiforov.semen@gmail.com>
2024-01-29 09:31:30 +0300

    Добавлена вью редактирования колонки МЛ

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/RouteColumnView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RouteColumnView : Gtk.Bin
+	{
+		public RouteColumnView()
+		{
+			this.Build();
+		}
+	}
+}
