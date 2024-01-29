c8b248257db7e4ebd1b999abe80e1494ceef5cdf
Semen <nikiforov.semen@gmail.com>
2024-01-29 14:11:14 +0300

    Изменен журнал колонок МЛ

--- Source/Applications/Desktop/VodovozViewModels/Journals/JournalViewModels/Logistic/RouteColumnJournalViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Journals/JournalViewModels/Logistic/RouteColumnJournalViewModel.cs
@@ -1,66 +1,54 @@
-using NHibernate;
+﻿using NHibernate;
 using NHibernate.Transform;
 using QS.DomainModel.UoW;
 using QS.Navigation;
-using QS.Project.Domain;
 using QS.Project.Journal;
+using QS.Project.Services;
 using QS.Services;
-using System;
 using Vodovoz.Domain.Logistic;
 using Vodovoz.ViewModels.ViewModels.Logistic;
 using static Vodovoz.ViewModels.Journals.JournalViewModels.Logistic.RouteColumnJournalViewModel;
 
 namespace Vodovoz.ViewModels.Journals.JournalViewModels.Logistic
 {
-	public class RouteColumnJournalViewModel : SingleEntityJournalViewModelBase<RouteColumn, RouteColumnViewModel, RouteColumnJournalNode>
+	public class RouteColumnJournalViewModel : EntityJournalViewModelBase<RouteColumn, RouteColumnViewModel, RouteColumnJournalNode>
 	{
 		public RouteColumnJournalViewModel(
 			IUnitOfWorkFactory unitOfWorkFactory,
 			ICommonServices commonServices,
 			INavigationManager navigationManager,
-			bool hideJournalForOpenDialog = false,
-			bool hideJournalForCreateDialog = false)
-			: base(unitOfWorkFactory, commonServices, hideJournalForOpenDialog, hideJournalForCreateDialog, navigationManager)
+			IDeleteEntityService deleteEntityService,
+			ICurrentPermissionService currentPermissionService)
+			: base(unitOfWorkFactory, commonServices.InteractiveService, navigationManager, deleteEntityService, currentPermissionService)
 		{
 			TabName = "Колонки в маршрутном листе";
 
 			UpdateOnChanges(typeof(RouteColumn));
+
+			UseSlider = true;
 		}
 
-		protected override Func<IUnitOfWork, IQueryOver<RouteColumn>> ItemsSourceQueryFunction => (uow) =>
+		protected override IQueryOver<RouteColumn> ItemsQuery(IUnitOfWork unitOfWork)
 		{
 			RouteColumnJournalNode resultAlias = null;
 
-			var query = uow.Session.QueryOver<RouteColumn>()
-				.SelectList(list => list
+			var query = unitOfWork.Session.QueryOver<RouteColumn>();
+
+			query.Where(
+				GetSearchCriterion<RouteColumn>(
+					x => x.Id,
+					x => x.Name,
+					x => x.ShortName
+					)
+				);
+
+			return query.SelectList(list => list
 				.Select(x => x.Id).WithAlias(() => resultAlias.Id)
 				.Select(x => x.Name).WithAlias(() => resultAlias.Name)
 				.Select(x => x.ShortName).WithAlias(() => resultAlias.ShortName)
 				.Select(x => x.IsHighlighted).WithAlias(() => resultAlias.IsHighlighted))
 				.TransformUsing(Transformers.AliasToBean<RouteColumnJournalNode>()).OrderBy(x => x.Name).Asc;
-
-			query.Where(
-			GetSearchCriterion<RouteColumn>(
-				x => x.Id,
-				x => x.Name,
-				x => x.ShortName
-				)
-			);
-
-			return query;
-		};
-
-		protected override Func<RouteColumnViewModel> CreateDialogFunction => () =>
-			new RouteColumnViewModel(EntityUoWBuilder.ForCreate(), UnitOfWorkFactory, commonServices);
-
-		protected override Func<RouteColumnJournalNode, RouteColumnViewModel> OpenDialogFunction => node =>
-			new RouteColumnViewModel(EntityUoWBuilder.ForOpen(node.Id), UnitOfWorkFactory, commonServices);
-
-		//protected override Func<RouteColumnViewModel> CreateDialogFunction => () =>
-		//	NavigationManager.OpenViewModel<RouteColumnViewModel, IEntityUoWBuilder>(null, EntityUoWBuilder.ForCreate()).ViewModel;
-
-		//protected override Func<RouteColumnJournalNode, RouteColumnViewModel> OpenDialogFunction => selectedNode =>
-		//	NavigationManager.OpenViewModel<RouteColumnViewModel, IEntityUoWBuilder>(null, EntityUoWBuilder.ForOpen(selectedNode.Id)).ViewModel;
+		}
 
 		public class RouteColumnJournalNode : JournalEntityNodeBase<RouteColumn>
 		{
