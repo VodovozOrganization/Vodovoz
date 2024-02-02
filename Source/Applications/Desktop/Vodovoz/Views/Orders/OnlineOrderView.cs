c8e0f473a131aeff06adf569a8f8feded505dfb6
Andrey Shaklein <as1854@yandex.ru>
2024-02-02 16:18:47 +0300

    4755 вьюха онлайн заказа

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/OnlineOrderView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using QS.Views.GtkUI;
+using QS.Navigation;
+using Vodovoz.ViewModels.ViewModels.Orders;
+
+namespace Vodovoz.Views.Orders
+{
+	public partial class OnlineOrderView : TabViewBase<OnlineOrderViewModel>
+	{
+		public OnlineOrderView(OnlineOrderViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			btnCancel.Clicked += (sender, e) => ViewModel.Close(false, CloseSource.Cancel);
+		}
+	}
+}
