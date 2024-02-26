c67657b94170a01b27952a2942809d466d9d99ed
Semen <34092262+N-Sem@users.noreply.github.com>
2024-02-26 16:16:49 +0300

    Добавлена вью-модель диалога

---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/PaymentsDiscrepanciesAnalysisViewModel.cs
@@ -0,0 +1,13 @@
+﻿using QS.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace Vodovoz.ViewModels.ViewModels.Payments
+{
+	public class PaymentsDiscrepanciesAnalysisViewModel : DialogViewModelBase
+	{
+		public PaymentsDiscrepanciesAnalysisViewModel(
+			INavigationManager navigation) : base(navigation)
+		{
+		}
+	}
+}
