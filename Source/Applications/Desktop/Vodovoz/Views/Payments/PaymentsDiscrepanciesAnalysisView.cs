458ae1beddb9f62f20f9c34a54543dec85002c46
N-Sem <nikiforov.semen@gmail.com>
2024-02-26 16:01:51 +0300

    Добавлен диалог для анализа платежей

---
+++ Source/Applications/Desktop/Vodovoz/Views/Payments/PaymentsDiscrepanciesAnalysisView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Payments
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PaymentsDiscrepanciesAnalysisView : Gtk.Bin
+	{
+		public PaymentsDiscrepanciesAnalysisView()
+		{
+			this.Build();
+		}
+	}
+}
