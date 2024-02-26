fff048c3bb01b970c553169f3e24a88b2ec3efab
Andrey Shaklein <as1854@yandex.ru>
2024-02-26 16:27:48 +0300

    добавил вьюху причины отмены онлайн заказа

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/OnlineOrderCancellationReasonView.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Vodovoz.Views.Orders
+{
+	[ToolboxItem(true)]
+	public partial class OnlineOrderCancellationReasonView : Gtk.Bin
+	{
+		public OnlineOrderCancellationReasonView()
+		{
+			Build();
+		}
+	}
+}
