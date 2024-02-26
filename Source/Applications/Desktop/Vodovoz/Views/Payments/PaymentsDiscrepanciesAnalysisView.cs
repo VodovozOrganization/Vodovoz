202e9996898643dadf0900c7a9377603a2cb7889
Semen <34092262+N-Sem@users.noreply.github.com>
2024-02-26 17:59:33 +0300

    Добавлены привязки

--- Source/Applications/Desktop/Vodovoz/Views/Payments/PaymentsDiscrepanciesAnalysisView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Payments/PaymentsDiscrepanciesAnalysisView.cs
@@ -1,12 +1,155 @@
-﻿using System;
+﻿using Gamma.ColumnConfig;
+using Gamma.Utilities;
+using Gtk;
+using QS.Views.Dialog;
+using Vodovoz.Infrastructure;
+using Vodovoz.ViewModels.ViewModels.Payments;
+using static Vodovoz.ViewModels.ViewModels.Payments.PaymentsDiscrepanciesAnalysisViewModel;
+
 namespace Vodovoz.Views.Payments
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class PaymentsDiscrepanciesAnalysisView : Gtk.Bin
+	public partial class PaymentsDiscrepanciesAnalysisView : DialogViewBase<PaymentsDiscrepanciesAnalysisViewModel>
 	{
-		public PaymentsDiscrepanciesAnalysisView()
+		private const string _xmlPattern = "*.xml";
+		private const string _csvPattern = "*.csv";
+
+		public PaymentsDiscrepanciesAnalysisView(PaymentsDiscrepanciesAnalysisViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			ynotebook.ShowTabs = false;
+			ynotebook.Binding
+				.AddBinding(ViewModel, vm => vm.SelectedCheckMode, w => w.CurrentPage)
+				.InitializeFromSource();
+
+			ycheckbuttonClientDiscrepanciesOnly.Binding
+				.AddFuncBinding(ViewModel, vm => vm.SelectedCheckMode == DiscrepancyCheckMode.ByCounterparty, w => w.Active)
+				.InitializeFromSource();
+			ycheckbuttonClientDiscrepanciesOnly.Toggled += (s, e) => ViewModel.SetByCounterpartyCheckModeCommand.Execute();
+
+			ycheckbuttonCommonMode.Binding
+				.AddFuncBinding(ViewModel, vm => vm.SelectedCheckMode == DiscrepancyCheckMode.CommonReconciliation, w => w.Active)
+				.InitializeFromSource();
+			ycheckbuttonCommonMode.Toggled += (s, e) => ViewModel.SetCommonReconciliationCheckModeCommand.Execute();
+
+			ConfigureFileChooser();
+			ConfigureOrdersTree();
+			ConfigurePaymentsTree();
+			ConfigureCounterpartiesTree();
+		}
+
+		private void ConfigureFileChooser()
+		{
+			var xmlFilter = new FileFilter();
+			xmlFilter.AddPattern(_xmlPattern);
+			xmlFilter.Name = $"Файлы XML ({_xmlPattern})";
+			var csvFilter = new FileFilter();
+			csvFilter.AddPattern(_csvPattern);
+			csvFilter.Name = $"Файлы CSV ({_csvPattern})";
+
+			yfilechooserbutton.AddFilter(csvFilter);
+			yfilechooserbutton.AddFilter(xmlFilter);
+
+			yfilechooserbutton.Binding
+				.AddBinding(ViewModel, vm => vm.SelectedFileName, w => w.Filename)
+				.InitializeFromSource();
+		}
+
+		private void ConfigureOrdersTree()
+		{
+			ytreeviewOrdersData.ColumnsConfig = FluentColumnsConfig<OrderDiscrepanciesNode>.Create()
+				.AddColumn("№ заказа")
+					.AddNumericRenderer(n => n.OrderId)
+				.AddColumn("Дата заказа")
+					.AddTextRenderer(n =>
+						n.OrderDeliveryDate.HasValue ?
+							n.OrderDeliveryDate.Value.ToShortDateString()
+							: string.Empty)
+				.AddColumn("Статус заказа")
+					.AddTextRenderer(n => n.OrderStatus.HasValue ? n.OrderStatus.GetEnumTitle() : string.Empty)
+				.AddColumn("Сумма по акту")
+					.AddNumericRenderer(n => n.DocumentOrderSum)
+				.AddColumn("Сумма по ДВ")
+					.AddNumericRenderer(n => n.ProgramOrderSum)
+					.AddSetter((spin, node) =>
+					{
+						spin.ForegroundGdk = GdkColors.PrimaryText;
+
+						if(node.OrderSumDiscrepancy)
+						{
+							spin.ForegroundGdk = GdkColors.DangerText;
+						}
+					})
+				.AddColumn("Распределенная сумма")
+					.AddNumericRenderer(n => n.AllocatedSum)
+				.AddColumn("Статус оплаты заказа")
+					.AddTextRenderer(n => n.OrderPaymentStatus.HasValue
+						? n.OrderPaymentStatus.GetEnumTitle()
+						: string.Empty)
+				/*.RowCells()
+				.AddSetter<CellRenderer>((c, n) =>
+				{
+					c.CellBackgroundGdk = GdkColors.PrimaryBase;
+
+					if(n.IsMissingFromDocument)
+					{
+						c.CellBackgroundGdk = GdkColors.Orange;
+					}
+				})*/
+				.AddColumn("")
+				.Finish();
+
+			ytreeviewOrdersData.ItemsDataSource = ViewModel.OrdersNodes;
+		}
+
+		private void ConfigurePaymentsTree()
+		{
+			ytreeviewPaymentsData.ColumnsConfig = FluentColumnsConfig<PaymentDiscrepanciesNode>.Create()
+				.AddColumn("№ платежа")
+					.AddNumericRenderer(n => n.PaymentNum)
+				.AddColumn("Дата платежа")
+					.AddTextRenderer(n => n.PaymentDate.ToShortDateString())
+				.AddColumn("Сумма по акту")
+					.AddNumericRenderer(n => n.DocumentPaymentSum)
+				.AddColumn("Сумма по ДВ")
+					.AddNumericRenderer(n => n.ProgramPaymentSum)
+				.AddColumn("Распределено на клиента")
+					.AddTextRenderer(n => n.CounterpartyId.ToString())
+					.AddTextRenderer(n => n.CounterpartyName)
+					.AddSetter((spin, node) =>
+					{
+						spin.ForegroundGdk = GdkColors.PrimaryText;
+
+						if(node.CounterpartyInn != ViewModel.SelectedClient.INN)
+						{
+							spin.ForegroundGdk = GdkColors.DangerText;
+						}
+					})
+				.AddColumn("Назначение платежа")
+					.AddTextRenderer(n => n.PaymentPurpose)
+				.AddColumn("")
+				.Finish();
+
+			ytreeviewPaymentsData.ItemsDataSource = ViewModel.PaymentsNodes;
+		}
+
+		private void ConfigureCounterpartiesTree()
+		{
+			ytreeviewCounterpartiesData.ColumnsConfig = FluentColumnsConfig<CounterpartyBalanceNode>.Create()
+				.AddColumn("Наименование")
+					.AddTextRenderer(n => n.CounterpartyName)
+				.AddColumn("Баланс по ДВ")
+					.AddNumericRenderer(n => n.CounterpartyBalance)
+				.AddColumn("Баланс по 1С")
+					.AddNumericRenderer(n => n.CounterpartyBalance1C)
+				.AddColumn("")
+				.Finish();
+
+			ytreeviewCounterpartiesData.ItemsDataSource = ViewModel.CounterpartyBalanceNodes;
 		}
 	}
 }
--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/PaymentsDiscrepanciesAnalysisViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/PaymentsDiscrepanciesAnalysisViewModel.cs
@@ -1,13 +1,121 @@
-﻿using QS.Navigation;
+﻿using QS.Commands;
+using QS.Navigation;
 using QS.ViewModels.Dialog;
+using System;
+using System.Data.Bindings.Collections.Generic;
+using Vodovoz.Domain.Orders;
 
 namespace Vodovoz.ViewModels.ViewModels.Payments
 {
 	public class PaymentsDiscrepanciesAnalysisViewModel : DialogViewModelBase
 	{
+		private DiscrepancyCheckMode _selectedCheckMode;
+		private string _selectedFileName;
+		private Domain.Client.Counterparty _selectedClient;
+
 		public PaymentsDiscrepanciesAnalysisViewModel(
 			INavigationManager navigation) : base(navigation)
 		{
+			Title = "Поиск расхождений в оплатах клиента";
+
+			SetByCounterpartyCheckModeCommand = new DelegateCommand(SetByCounterpartyCheckMode);
+			SetCommonReconciliationCheckModeCommand = new DelegateCommand(SetCommonReconciliationCheckMode);
+		}
+
+		#region Settings
+
+		public DiscrepancyCheckMode SelectedCheckMode
+		{
+			get => _selectedCheckMode;
+			set => SetField(ref _selectedCheckMode, value);
+		}
+
+		public string SelectedFileName
+		{
+			get => _selectedFileName;
+			set
+			{
+				if(SetField(ref _selectedFileName, value))
+				{
+					OnPropertyChanged(nameof(CanReadFile));
+				}
+			}
+		}
+
+		public Domain.Client.Counterparty SelectedClient
+		{
+			get => _selectedClient;
+			set => SetField(ref _selectedClient, value);
+		}
+
+		public bool CanReadFile => !string.IsNullOrWhiteSpace(_selectedFileName);
+
+		public GenericObservableList<OrderDiscrepanciesNode> OrdersNodes { get; } =
+			new GenericObservableList<OrderDiscrepanciesNode>();
+
+		public GenericObservableList<PaymentDiscrepanciesNode> PaymentsNodes { get; } =
+			new GenericObservableList<PaymentDiscrepanciesNode>();
+
+		public GenericObservableList<CounterpartyBalanceNode> CounterpartyBalanceNodes { get; } =
+			new GenericObservableList<CounterpartyBalanceNode>();
+
+		#endregion Settings
+
+		#region Commands
+
+		public DelegateCommand SetByCounterpartyCheckModeCommand { get; }
+		public DelegateCommand SetCommonReconciliationCheckModeCommand { get; }
+
+		#endregion Commands
+
+		private void SetByCounterpartyCheckMode()
+		{
+			SelectedCheckMode = DiscrepancyCheckMode.ByCounterparty;
+		}
+
+		private void SetCommonReconciliationCheckMode()
+		{
+			SelectedCheckMode = DiscrepancyCheckMode.CommonReconciliation;
+		}
+
+
+		public class OrderDiscrepanciesNode
+		{
+			public int OrderId { get; set; }
+			public DateTime? OrderDeliveryDate { get; set; }
+			public OrderStatus? OrderStatus { get; set; }
+			public OrderPaymentStatus? OrderPaymentStatus { get; set; }
+			public decimal DocumentOrderSum { get; set; }
+			public decimal ProgramOrderSum { get; set; }
+			public decimal AllocatedSum { get; set; }
+			public bool IsMissingFromDocument { get; set; }
+			public bool OrderSumDiscrepancy => ProgramOrderSum != DocumentOrderSum;
+		}
+
+		public class PaymentDiscrepanciesNode
+		{
+			public int PaymentNum { get; set; }
+			public DateTime PaymentDate { get; set; }
+			public decimal DocumentPaymentSum { get; set; }
+			public decimal ProgramPaymentSum { get; set; }
+			public int CounterpartyId { get; set; }
+			public string CounterpartyName { get; set; }
+			public string CounterpartyInn { get; set; }
+			public bool IsManuallyCreated { get; set; }
+			public string PaymentPurpose { get; set; }
+		}
+
+		public class CounterpartyBalanceNode
+		{
+			public string CounterpartyName { get; set; }
+			public decimal CounterpartyBalance { get; set; }
+			public decimal CounterpartyBalance1C { get; set; }
+		}
+
+		public enum DiscrepancyCheckMode
+		{
+			ByCounterparty,
+			CommonReconciliation
 		}
 	}
 }
