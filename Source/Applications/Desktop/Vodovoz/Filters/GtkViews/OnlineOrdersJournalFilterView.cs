fcd7a46e8a123fe2c8bf0af4c8010b3b41faaa47
Andrey Shaklein <as1854@yandex.ru>
2024-02-27 16:27:52 +0300

    4755 добавил вьюху фильтра журнала онлайн заказов

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OnlineOrdersJournalFilterView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Orders;
+
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OnlineOrdersJournalFilterView : FilterViewBase<OnlineOrdersJournalFilterViewModel>
+	{
+		public OnlineOrdersJournalFilterView(OnlineOrdersJournalFilterViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+
+		}
+	}
+}
