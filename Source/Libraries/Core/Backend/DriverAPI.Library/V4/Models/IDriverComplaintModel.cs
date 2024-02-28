65432491d52d34caf1c07e369d5d04331e97b178
Sergey Efimov <efimov90@gmail.com>
2024-02-28 09:31:41 +0300

    Убрал ненужное

--- Source/Libraries/Core/Backend/DriverAPI.Library/V4/Models/IDriverComplaintModel.cs
+++ Source/Libraries/Core/Backend/DriverAPI.Library/V4/Models/IDriverComplaintModel.cs
@@ -3,15 +3,8 @@ using System.Collections.Generic;
 
 namespace DriverAPI.Library.V4.Models
 {
-	/// <summary>
-	/// 
-	/// </summary>
 	public interface IDriverComplaintModel
 	{
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <returns></returns>
 		IEnumerable<DriverComplaintReasonDto> GetPinnedComplaintReasons();
 	}
 }
