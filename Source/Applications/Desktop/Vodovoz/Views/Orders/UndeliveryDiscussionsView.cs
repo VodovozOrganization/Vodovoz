74cebf8ca889a3390211107356081936d577f09a
Art8m <9artbe@gmail.com>
2024-03-05 12:22:39 +0300

    Подготовка под MVVM

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/UndeliveryDiscussionsView.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data.Bindings.Collections.Generic;
+using Gamma.GtkWidgets;
+using QS.DomainModel.UoW;
+using QS.Views.GtkUI;
+using Vodovoz.Domain.Employees;
+using Vodovoz.Domain.Orders;
+using Vodovoz.EntityRepositories.Employees;
+using Vodovoz.EntityRepositories.Undeliveries;
+using Vodovoz.Extensions;
+using Vodovoz.Infrastructure;
+using Vodovoz.ViewModels.Orders;
+
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class UndeliveryDiscussionsView : WidgetViewBase<UndeliveryDiscussionsViewModel>
+	{
+		private readonly IEmployeeRepository _employeeRepository = new EmployeeRepository();
+		private readonly IUndeliveredOrderCommentsRepository _undeliveredOrderCommentsRepository = new UndeliveredOrderCommentsRepository();
+		
+		public UndeliveryDiscussionsView()
+		{
+			this.Build();
+		}
+
+		IUnitOfWork UoW { get; set; }
+		UndeliveredOrderComment Comment { get; set; }
+		GenericObservableList<UndeliveredOrderCommentsNode> Comments { get; } = new GenericObservableList<UndeliveredOrderCommentsNode>();
+
+		CommentedFields Field { get; set; }
+		UndeliveredOrder UndeliveredOrder { get; set; }
+		Employee Employee { get; set; }
+
+		public event EventHandler<EventArgs> CommentAdded;
+
+		void OnCommentAdded()
+		{
+			if(CommentAdded != null)
+			{
+				CommentAdded(this, new EventArgs());
+			}
+		}
+
+		public void Configure(IUnitOfWork uow, UndeliveredOrder undeliveredOrder, CommentedFields field)
+		{
+			UoW = uow;
+			UndeliveredOrder = undeliveredOrder;
+			Employee = _employeeRepository.GetEmployeeForCurrentUser(UoW);
+			Field = field;
+			
+			UpdateCommentsTreeview();
+		}
+
+		void CreateComment()
+		{
+			Comment = new UndeliveredOrderComment();
+			Comment.UndeliveredOrder = UndeliveredOrder;
+			Comment.CommentedField = Field;
+			Comment.Employee = Employee;
+			Comment.CommentDate = DateTime.Now;
+			Comment.Comment = txtAddComment.Buffer.Text;
+			txtAddComment.Buffer.Text = string.Empty;
+		}
+
+		void UpdateCommentsTreeview()
+		{
+			var comments = _undeliveredOrderCommentsRepository.GetCommentNodes(UoW, UndeliveredOrder, Field);
+
+			Comments.Clear();
+
+			foreach(var comment in comments)
+			{
+				Comments.Add(comment);
+			}
+
+			yTreeComments.ColumnsConfig = ColumnsConfigFactory.Create<UndeliveredOrderCommentsNode>()
+				.AddColumn("Дата - Имя")
+					.AddTextRenderer(c => "", useMarkup: true)
+					.AddSetter((c, n) =>
+					{
+						var color = Comments.IndexOf(n) % 2 == 0 ? GdkColors.InfoText.ToHtmlColor() : GdkColors.DangerText.ToHtmlColor();
+						c.Markup = $"<span foreground=\"{color}\"><b>{n.UserDateAndName}</b></span>";
+					})
+				.AddColumn("Комментарий")
+					.AddTextRenderer(n => "", useMarkup: true)
+					.AddSetter((c, n) =>
+					{
+						var color = Comments.IndexOf(n) % 2 == 0 ? GdkColors.InfoText.ToHtmlColor() : GdkColors.DangerText.ToHtmlColor();
+						c.Markup = $"<span foreground=\"{color}\"><b>{n.Comment}</b></span>";
+					})
+					.WrapWidth(450).WrapMode(Pango.WrapMode.WordChar)
+				.Finish();
+
+			yTreeComments.ItemsDataSource = Comments;
+		}
+
+		protected void OnBtnAddCommentClicked(object sender, EventArgs e)
+		{
+			if(string.IsNullOrWhiteSpace(txtAddComment.Buffer.Text))
+			{
+				return;
+			}
+
+			CreateComment();
+			UoW.Save(Comment);
+			UoW.Commit();
+			UpdateCommentsTreeview();
+			Comment = null;
+			OnCommentAdded();
+		}
+
+		protected void OnBtnShowHideClicked(object sender, EventArgs e)
+		{
+			mainBox.Visible = !mainBox.Visible;
+			btnShowHide.Label = mainBox.Visible ? "<" : ">";
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Orders/UndeliveryDiscussionsViewModel.cs
@@ -0,0 +1,204 @@
+﻿using QS.Commands;
+using QS.Dialog;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Journal;
+using QS.Project.Services.FileDialog;
+using QS.Services;
+using QS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Bindings.Collections.Generic;
+using System.Linq;
+using QS.ViewModels.Dialog;
+using Vodovoz.Domain.Complaints;
+using Vodovoz.EntityRepositories;
+using Vodovoz.FilterViewModels.Organization;
+using Vodovoz.Journals.JournalNodes;
+using Vodovoz.Journals.JournalViewModels.Organizations;
+using Vodovoz.Services;
+using Vodovoz.Domain.Orders;
+
+namespace Vodovoz.ViewModels.Orders
+{
+	public class UndeliveryDiscussionsViewModel : EntityWidgetViewModelBase<UndeliveredOrder>, IDisposable
+	{
+		private readonly IFileDialogService _fileDialogService;
+		private readonly IEmployeeService _employeeService;
+		private readonly IUserRepository _userRepository;
+		private readonly INavigationManager _navigationManager;
+		private DialogViewModelBase _parentTab;
+
+		public UndeliveryDiscussionsViewModel(
+			UndeliveredOrder entity,
+			IUnitOfWork uow,
+			DialogViewModelBase parentTab,
+			IFileDialogService fileDialogService,
+			IEmployeeService employeeService,
+			ICommonServices commonServices,
+			IUserRepository userRepository,
+			INavigationManager navigationManager) : base(entity, commonServices)
+		{
+			_parentTab = parentTab ?? throw new ArgumentNullException(nameof(parentTab));
+			_fileDialogService = fileDialogService ?? throw new ArgumentNullException(nameof(fileDialogService));
+			_employeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+			_userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+			_navigationManager = navigationManager;
+
+			UoW = uow;
+			//CreateCommands();
+			ConfigureEntityPropertyChanges();
+			FillDiscussionsViewModels();
+		}
+
+		public bool CanEdit => PermissionResult.CanUpdate;
+
+		//private Dictionary<int, ComplaintDiscussionViewModel> viewModelsCache = new Dictionary<int, ComplaintDiscussionViewModel>();
+
+		private void ConfigureEntityPropertyChanges()
+		{
+			//Entity.ObservableComplaintDiscussions.ElementAdded += ObservableComplaintDiscussions_ElementAdded;
+			//Entity.ObservableComplaintDiscussions.ElementRemoved += ObservableComplaintDiscussions_ElementRemoved;
+		}
+
+		void ObservableComplaintDiscussions_ElementAdded(object aList, int[] aIdx)
+		{
+			FillDiscussionsViewModels();
+		}
+
+		void ObservableComplaintDiscussions_ElementRemoved(object aList, int[] aIdx, object aObject)
+		{
+			FillDiscussionsViewModels();
+		}
+
+		private void FillDiscussionsViewModels()
+		{
+			//foreach(ComplaintDiscussion discussion in Entity.ObservableComplaintDiscussions) {
+			//	var discussionViewModel = GetDiscussionViewModel(discussion);
+			//	if(!ObservableComplaintDiscussionViewModels.Contains(discussionViewModel)) {
+			//		ObservableComplaintDiscussionViewModels.Add(discussionViewModel);
+			//	}
+			//}
+		}
+
+		//private ComplaintDiscussionViewModel GetDiscussionViewModel(ComplaintDiscussion complaintDiscussion)
+		//{
+		//	int subdivisionId = complaintDiscussion.Subdivision.Id;
+
+		//	if(viewModelsCache.ContainsKey(subdivisionId))
+		//	{
+		//		return viewModelsCache[subdivisionId];
+		//	}
+
+		//	var viewModel =
+		//		new ComplaintDiscussionViewModel(
+		//			complaintDiscussion, _fileDialogService, _employeeService, CommonServices, UoW, _userRepository);
+
+		//	viewModelsCache.Add(subdivisionId, viewModel);
+		//	return viewModel;
+		//}
+
+		//GenericObservableList<ComplaintDiscussionViewModel> observableComplaintDiscussionViewModels = new GenericObservableList<ComplaintDiscussionViewModel>();
+
+		////FIXME Кослыль пока не разберемся как научить hibernate работать с обновляемыми списками.
+		//public virtual GenericObservableList<ComplaintDiscussionViewModel> ObservableComplaintDiscussionViewModels {
+		//	get => observableComplaintDiscussionViewModels;
+		//	set => SetField(ref observableComplaintDiscussionViewModels, value, () => ObservableComplaintDiscussionViewModels);
+		//}
+
+		//#region Commands
+
+		//private void CreateCommands()
+		//{
+		//	CreateAttachSubdivisionCommand();
+		//	CreateAttachSubdivisionByComplaintKindCommand();
+		//}
+
+		//#region AttachSubdivisionCommand
+
+		//public bool CanAttachSubdivision => CanEdit;
+
+		//public DelegateCommand AttachSubdivisionCommand { get; private set; }
+
+		//private void CreateAttachSubdivisionCommand()
+		//{
+		//	AttachSubdivisionCommand = new DelegateCommand(
+		//		() =>
+		//		{
+		//			var page = _navigationManager.OpenViewModel<SubdivisionsJournalViewModel, Action<SubdivisionFilterViewModel>>(
+		//				_parentTab,
+		//				filter =>
+		//					filter.ExcludedSubdivisionsIds = Entity.ObservableComplaintDiscussions.Select(x => x.Subdivision.Id).ToArray(),
+		//				OpenPageOptions.AsSlave,
+		//				vm => vm.SelectionMode = JournalSelectionMode.Single);
+
+		//			page.ViewModel.OnSelectResult += (s, e) =>
+		//			{
+		//				var selected = e.SelectedObjects.OfType<SubdivisionJournalNode>().FirstOrDefault();
+
+		//				if(selected is null)
+		//				{
+		//					return;
+		//				}
+
+		//				var subdivision = UoW.GetById<Subdivision>(selected.Id);
+		//				Entity.AttachSubdivisionToDiscussions(subdivision);
+		//			};
+		//		},
+		//		() => CanAttachSubdivision
+		//	);
+		//	AttachSubdivisionCommand.CanExecuteChangedWith(this, x => x.CanAttachSubdivision);
+		//}
+
+		//#endregion AttachSubdivisionCommand
+
+		//#region AttachSubdivisionByComplaintKindCommand
+
+		//public DelegateCommand AttachSubdivisionByComplaintKindCommand { get; private set; }
+
+		//private void CreateAttachSubdivisionByComplaintKindCommand()
+		//{
+		//	AttachSubdivisionByComplaintKindCommand = new DelegateCommand(
+		//		() =>
+		//		{
+		//			if(Entity.ComplaintKind == null)
+		//			{
+		//				CommonServices.InteractiveService.ShowMessage(ImportanceLevel.Warning, $"Не выбран вид рекламаций");
+		//				return;
+		//			}
+
+		//			if(!Entity.ComplaintKind.Subdivisions.Any())
+		//			{
+		//				CommonServices.InteractiveService.ShowMessage(ImportanceLevel.Warning,
+		//					$"У вида рекламации {Entity.ComplaintKind.Name} отсутствуют подключаемые отделы.");
+		//				return;
+		//			}
+
+		//			string subdivisionString = string.Join(", ", Entity.ComplaintKind.Subdivisions.Select(s => s.Name));
+
+		//			if(CommonServices.InteractiveService.Question(
+		//				$"Будут подключены следующие отделы: { subdivisionString }.",
+		//				"Подключить?")
+		//			)
+		//			{
+		//				foreach(var subdivision in Entity.ComplaintKind.Subdivisions)
+		//				{
+		//					Entity.AttachSubdivisionToDiscussions(subdivision);
+		//				}
+		//			}
+		//		},
+		//		() => CanAttachSubdivision
+		//	);
+		//	AttachSubdivisionByComplaintKindCommand.CanExecuteChangedWith(this, x => x.CanAttachSubdivision);
+		//}
+
+		//#endregion AttachSubdivisionByComplaintKindCommand
+
+		//#endregion Commands
+
+		public void Dispose()
+		{
+			_parentTab = null;
+		}
+	}
+}
