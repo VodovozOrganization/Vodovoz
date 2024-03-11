4e11a2d97669c2200d17b5701ec8edce594a9659
Sergey Efimov <efimov90@gmail.com>
2024-03-11 14:19:20 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Search/CompositeAlgebraicSearchView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Search
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CompositeAlgebraicSearchView : Gtk.Bin
+	{
+		public CompositeAlgebraicSearchView()
+		{
+			this.Build();
+		}
+	}
+}
