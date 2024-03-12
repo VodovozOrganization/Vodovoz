93b41064994e625620c6c2c6f8ee45e7f9c00889
Art8m <9artbe@gmail.com>
2024-03-12 01:56:31 -0700

    stetic UndeliveryView

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/UndeliveryView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class UndeliveryView : Gtk.Bin
+	{
+		public UndeliveryView()
+		{
+			this.Build();
+		}
+	}
+}
