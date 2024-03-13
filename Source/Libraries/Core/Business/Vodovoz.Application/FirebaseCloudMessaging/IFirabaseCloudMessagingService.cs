a435f12e852fee352bd3b4e4aff5821a16f75218
Sergey Efimov <efimov90@gmail.com>
2024-03-13 14:44:32 +0300

    Добавлен интерфейс сервиса IFirebaseCloudMessagingService

---
+++ Source/Libraries/Core/Business/Vodovoz.Application/FirebaseCloudMessaging/IFirabaseCloudMessagingService.cs
@@ -0,0 +1,9 @@
+﻿using Vodovoz.Errors;
+
+namespace Vodovoz.Application.FirebaseCloudMessaging
+{
+	public interface IFirabaseCloudMessagingService
+	{
+		Result SendMessage(string recipientToken, string title, string body, object data = null);
+	}
+}
