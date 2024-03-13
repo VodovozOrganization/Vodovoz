32c7a62e7a68180133715762fe54a2086fe2d2d9
N-Sem <nikiforov.semen@gmail.com>
2024-03-13 17:58:54 +0300

    Добавлена viewmodel и привязки

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Logistic/DeliveryScheduleSelectionViewModel.cs
@@ -0,0 +1,73 @@
+﻿using QS.Commands;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+using System;
+using System.Collections.Generic;
+using Vodovoz.Domain.Logistic;
+
+namespace Vodovoz.Presentation.ViewModels.Logistic
+{
+	public class DeliveryScheduleSelectionViewModel : WindowDialogViewModelBase
+	{
+		public event EventHandler<DeliveryScheduleSelectedEventArgs> DeliveryScheduleSelected;
+		public event EventHandler SelectEntityFromJournalSelected;
+
+		public DeliveryScheduleSelectionViewModel(
+			INavigationManager navigation,
+			List<DeliverySchedule> deliverySchedules,
+			DateTime deliveryDate,
+			bool isUserCanSelectAnyDeliveryScheduleFromJournal = false
+			) : base(navigation)
+		{
+			DeliverySchedules = deliverySchedules;
+			DeliveryDate = deliveryDate;
+			IsUserCanSelectAnyDeliveryScheduleFromJournal = isUserCanSelectAnyDeliveryScheduleFromJournal;
+			DeliveryDay = GetDeliveryDay();
+
+			SelectDeliveryScheduleCommand = new DelegateCommand<DeliverySchedule>(SelectDeliverySchedule);
+			SelectEntityFromJournalCommand = new DelegateCommand(SelectEntityFromJournal);
+		}
+
+		public DateTime DeliveryDate { get; }
+		public string DeliveryDay { get; }
+		public bool IsUserCanSelectAnyDeliveryScheduleFromJournal { get; }
+		public List<DeliverySchedule> DeliverySchedules { get; }
+		public string NoDeliverySchedulesMessage =>
+			"На данный день\nинтервалы\nдоставки\nотсутствуют";
+
+		public DelegateCommand<DeliverySchedule> SelectDeliveryScheduleCommand { get; }
+		public DelegateCommand SelectEntityFromJournalCommand { get; }
+
+		private string GetDeliveryDay()
+		{
+			return DeliveryDate.ToString();
+		}
+
+		private void SelectDeliverySchedule(DeliverySchedule deliverySchedule)
+		{
+			DeliveryScheduleSelected?.Invoke(this, new DeliveryScheduleSelectedEventArgs(deliverySchedule));
+			CloseWindow();
+		}
+
+		private void SelectEntityFromJournal()
+		{
+			SelectEntityFromJournalSelected?.Invoke(this, EventArgs.Empty);
+			CloseWindow();
+		}
+
+		private void CloseWindow()
+		{
+			Close(false, CloseSource.Self);
+		}
+
+		public class DeliveryScheduleSelectedEventArgs : EventArgs
+		{
+			public DeliveryScheduleSelectedEventArgs(DeliverySchedule deliverySchedule)
+			{
+				DeliverySchedule = deliverySchedule;
+			}
+
+			public DeliverySchedule DeliverySchedule { get; }
+		}
+	}
+}
--- Source/Applications/Desktop/Vodovoz/Views/Logistic/DeliveryScheduleSelectionView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/DeliveryScheduleSelectionView.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Gtk;
+using QS.Views.Dialog;
+using Vodovoz.Domain.Logistic;
+using Vodovoz.Extensions;
+using Vodovoz.Infrastructure;
+using Vodovoz.Presentation.ViewModels.Logistic;
+
 namespace Vodovoz.Views.Logistic
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class DeliveryScheduleSelectionView : Gtk.Bin
+	public partial class DeliveryScheduleSelectionView : DialogViewBase<DeliveryScheduleSelectionViewModel>
 	{
-		public DeliveryScheduleSelectionView()
+		private readonly string _dangerTextHtmlColor = GdkColors.DangerText.ToHtmlColor();
+		public DeliveryScheduleSelectionView(DeliveryScheduleSelectionViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			SetLabelsContent();
+			SetContainersVisibitity();
+			ConfigureManualScheduleSelectionEvent();
+			AddButtons();
+		}
+
+		private void SetLabelsContent()
+		{
+			ylabelDayOfWeek.Markup = $"<b>На {ViewModel.DeliveryDay}<b>";
+			ylabelDate.Markup = $"{ViewModel.DeliveryDate.ToShortDateString()}";
+
+			ylabelNoSchedules.Markup = $"<span foreground='{_dangerTextHtmlColor}'><b>{ViewModel.NoDeliverySchedulesMessage}</b></span>";
+		}
+
+		private void SetContainersVisibitity()
+		{
+			var isViewModelHasDeliverySchedules = ViewModel.DeliverySchedules.Count > 0;
+
+			yvboxButtonsLeftColumn.Visible = isViewModelHasDeliverySchedules;
+			yvboxButtonsRightColumn.Visible = isViewModelHasDeliverySchedules;
+			yvboxNoSchedulesInfo.Visible = !isViewModelHasDeliverySchedules;
+		}
+
+		private void ConfigureManualScheduleSelectionEvent()
+		{
+			ybuttonManualScheduleSelection.Visible = ViewModel.IsUserCanSelectAnyDeliveryScheduleFromJournal;
+			ybuttonManualScheduleSelection.Clicked += (s, e) => ViewModel.SelectEntityFromJournalCommand.Execute();
+		}
+
+		private void AddButtons()
+		{
+			for(int i = 0; i < ViewModel.DeliverySchedules.Count; i++)
+			{
+				var deliverySchedule = ViewModel.DeliverySchedules[i];
+
+				if(i % 2 == 0)
+				{
+					yvboxButtonsLeftColumn.Add(GetButton(deliverySchedule));
+
+					continue;
+				}
+
+				yvboxButtonsRightColumn.Add(GetButton(deliverySchedule));
+			}
+		}
+
+		private Button GetButton(DeliverySchedule deliverySchedule)
+		{
+			var button = new Button
+			{
+				Label = deliverySchedule.Name
+			};
+
+			button.Clicked += (s, e) => ViewModel.SelectDeliveryScheduleCommand.Execute(deliverySchedule);
+
+			return button;
 		}
 	}
 }
