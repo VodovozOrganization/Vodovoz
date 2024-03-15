9e2015245c1365007b2640904467994d3621adfd
Sergey Efimov <efimov90@gmail.com>
2024-03-15 11:38:57 +0300

    Revert "Все контракты Api с документацией"
    This reverts commit 513364ef305191c264fa748188f60c037961c59a.

--- Source/Libraries/Core/Backend/Firebase.Client/Requests/Notification.cs
+++ Source/Libraries/Core/Backend/Firebase.Client/Requests/Notification.cs
@@ -1,28 +1,15 @@
-﻿namespace Vodovoz.FirebaseCloudMessaging.Client.Requests
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace FirebaseCloudMessaging.Client.Requests
 {
-	/// <summary>
-	/// Уведомление<br/>
-	/// <see href="https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages?hl=ru#notification"/>
-	/// </summary>
+	[Serializable]
 	public class Notification
 	{
-		/// <summary>
-		/// Название уведомления.
-		/// </summary>
+		[JsonPropertyName("title")]
 		public string Title { get; set; }
 
-		/// <summary>
-		/// Текст уведомления.
-		/// </summary>
+		[JsonPropertyName("body")]
 		public string Body { get; set; }
-
-		/// <summary>
-		/// Содержит URL-адрес изображения, которое будет загружено на устройство и отображено в уведомлении.<br/>
-		/// JPEG, PNG, BMP полностью поддерживаются на всех платформах.<br/>
-		/// Анимированные GIF-файлы и видео работают только на iOS. WebP и HEIF имеют разные уровни поддержки в зависимости от платформы и версии платформы.<br/>
-		/// Android имеет ограничение на размер изображения в 1 МБ.<br/>
-		/// Использование квоты и последствия/затраты на размещение изображений в Firebase Storage: https://firebase.google.com/pricing.
-		/// </summary>
-		public string Image { get; set; }
 	}
 }
