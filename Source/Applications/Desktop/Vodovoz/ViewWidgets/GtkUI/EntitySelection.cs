fc720ed4952e4a2977ce2f712cb0da4b8769e61c
N-Sem <nikiforov.semen@gmail.com>
2024-03-15 16:30:51 +0300

    Контрол для выбора объекта

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/GtkUI/EntitySelection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.GtkUI
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EntitySelection : Gtk.Bin
+	{
+		public EntitySelection()
+		{
+			this.Build();
+		}
+	}
+}
