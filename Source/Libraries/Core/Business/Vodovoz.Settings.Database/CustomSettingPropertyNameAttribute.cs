b07b3f6f0a0310957ab2a3b8be1414c0e26f06ac
Sergey Efimov <efimov90@gmail.com>
2024-03-15 18:05:13 +0300

    Поддержка атрибута

---
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/CustomSettingPropertyNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Vodovoz.Settings.Database
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class CustomSettingPropertyNameAttribute : Attribute
+	{
+		public CustomSettingPropertyNameAttribute(string customPropertyName)
+		{
+			CustomPropertyName = customPropertyName;
+		}
+
+		public string CustomPropertyName { get; }
+	}
+}
