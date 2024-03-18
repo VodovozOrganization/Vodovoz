37cd74dbd768fa79e78a1090d2f8cb668d317d3d
Art8m <9artbe@gmail.com>
2024-03-18 06:32:04 -0700

    Вынес виджет принадлежность МЛ

---
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Logistic/RouteListOwnView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ReportsParameters.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RouteListOwnView : Gtk.Bin
+	{
+		public RouteListOwnView()
+		{
+			this.Build();
+		}
+	}
+}
