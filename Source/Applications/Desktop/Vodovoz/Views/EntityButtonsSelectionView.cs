f1682dd24331942473c16a3a5a379a4660e64195
N-Sem <nikiforov.semen@gmail.com>
2024-03-20 09:00:52 +0300

    Добавлен вью для окна выбора

---
+++ Source/Applications/Desktop/Vodovoz/Views/EntityButtonsSelectionView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EntityButtonsSelectionView : Gtk.Bin
+	{
+		public EntityButtonsSelectionView()
+		{
+			this.Build();
+		}
+	}
+}
