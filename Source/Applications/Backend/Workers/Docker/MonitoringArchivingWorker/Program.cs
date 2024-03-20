2ca78a3052e03f87796d70041de30c19f9a71f51
Vadim Gordeev <enzogord@gmail.com>
2024-03-20 20:13:38 +0700

    Перенос регистрации сборки с настройками в метод где регистрируются сами настройки и соответствующие Изменения в QS

--- Source/Applications/Backend/Workers/Docker/MonitoringArchivingWorker/Program.cs
+++ Source/Applications/Backend/Workers/Docker/MonitoringArchivingWorker/Program.cs
@@ -49,8 +49,7 @@ namespace MonitoringArchivingWorker
 							typeof(QS.HistoryLog.HistoryMain).Assembly,
 							typeof(QS.Project.Domain.TypeOfEntity).Assembly,
 							typeof(QS.Attachments.Domain.Attachment).Assembly,
-							typeof(EmployeeWithLoginMap).Assembly,
-							typeof(Vodovoz.Settings.Database.AssemblyFinder).Assembly
+							typeof(EmployeeWithLoginMap).Assembly
 						)
 						.AddDatabaseConnection()
 						.AddCore()
