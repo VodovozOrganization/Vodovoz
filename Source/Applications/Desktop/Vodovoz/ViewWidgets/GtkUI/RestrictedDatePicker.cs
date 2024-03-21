c220e13d2856b001853bb74719c4557ab90dcd1e
N-Sem <nikiforov.semen@gmail.com>
2024-03-21 21:56:42 +0300

    Новый вид календаря

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/GtkUI/RestrictedDatePicker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.GtkUI
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RestrictedDatePicker : Gtk.Bin
+	{
+		public RestrictedDatePicker()
+		{
+			this.Build();
+		}
+
+		protected void OnButtonEditDateClicked(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
