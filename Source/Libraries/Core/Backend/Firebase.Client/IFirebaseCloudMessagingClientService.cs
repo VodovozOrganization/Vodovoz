d135871c5e162538d0a1076a78036e8dd1088385
Sergey Efimov <efimov90@gmail.com>
2024-03-25 13:27:04 +0300

    Мусор, убран из проекта не убран из папки

--- Source/Libraries/Core/Backend/Firebase.Client/IFirebaseCloudMessagingClientService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace FirebaseCloudMessaging.Client
-{
-	public interface IFirebaseCloudMessagingClientService
-	{
-		Task SendMessage(string recipientToken, string title, string body, object data = null);
-	}
-}
