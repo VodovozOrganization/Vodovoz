a5be619f5549749b7d996c677721760e049a6e29
Andrey <as1854@yandex.ru>
2024-03-26 09:58:51 +0300

    4757 добавлен виджет причины оценки заказа

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/OrderRatingReasonView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrderRatingReasonView : Gtk.Bin
+	{
+		public OrderRatingReasonView()
+		{
+			this.Build();
+		}
+	}
+}
