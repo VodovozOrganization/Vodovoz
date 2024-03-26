d595d96d33a9f4c7657dbd0c65785e3862c54a8d
Andrey <as1854@yandex.ru>
2024-03-26 14:26:04 +0300

    4757 добавил виджет оценки заказа

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/OrderRatingView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrderRatingView : Gtk.Bin
+	{
+		public OrderRatingView()
+		{
+			this.Build();
+		}
+	}
+}
