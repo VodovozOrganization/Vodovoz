a1f4f5dda4286da1a6e1ca232a949eb502858887
Andrey <as1854@yandex.ru>
2024-03-26 13:05:23 +0300

    4757 добавил виджет фильтра журнала оценок заказов

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OrdersRatingsJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrdersRatingsJournalFilterView : Gtk.Bin
+	{
+		public OrdersRatingsJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
