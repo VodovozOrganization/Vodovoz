e42b8ce768ff5f2418085732a733342563c17a0f
Sergey Efimov <efimov90@gmail.com>
2024-03-27 13:09:50 +0300

    Небольшой рефакторинг

--- Source/Applications/Backend/WebAPI/DeliveryRulesService/Startup.cs
+++ Source/Applications/Backend/WebAPI/DeliveryRulesService/Startup.cs
@@ -1,129 +1,30 @@
-﻿using Autofac;
-using DeliveryRulesService.Cache;
-using DeliveryRulesService.HealthChecks;
-using Fias.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using NLog.Web;
-using QS.Project.Core;
-using QS.Services;
-using System.Linq;
-using System.Reflection;
-using Vodovoz;
-using Vodovoz.Core.Data.NHibernate;
-using Vodovoz.Core.Data.NHibernate.Mappings;
-using Vodovoz.Tools;
-using Vodovoz.Tools.CallTasks;
 using VodovozHealthCheck;
 
 namespace DeliveryRulesService
 {
 	public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-			services.AddMvc().AddControllersAsServices();
-			services.AddControllers().AddJsonOptions(j =>
-			{
-				//Необходимо для сериализации свойств как PascalCase
-				j.JsonSerializerOptions.PropertyNamingPolicy = null;
-			});
-
-			services
-				.AddLogging(logging =>
-				{
-					logging.ClearProviders();
-					logging.AddNLogWeb();
-					logging.AddConfiguration(Configuration.GetSection("NLog"));
-				})
-
-				.AddMappingAssemblies(
-					typeof(QS.Project.HibernateMapping.UserBaseMap).Assembly,
-					typeof(Vodovoz.Data.NHibernate.AssemblyFinder).Assembly,
-					typeof(QS.Banks.Domain.Bank).Assembly,
-					typeof(QS.HistoryLog.HistoryMain).Assembly,
-					typeof(QS.Project.Domain.TypeOfEntity).Assembly,
-					typeof(QS.Attachments.Domain.Attachment).Assembly,
-					typeof(EmployeeWithLoginMap).Assembly
-				)
-				.AddDatabaseConnection()
-				.AddCore()
-				.AddTrackedUoW()
-
-				.ConfigureHealthCheckService<DeliveryRulesServiceHealthCheck>()
-				.AddHttpClient()
-				.AddFiasClient()
-				;
-
-				Vodovoz.Data.NHibernate.DependencyInjection.AddStaticScopeForEntity(services);
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
 		}
 
-		public void ConfigureContainer(ContainerBuilder builder)
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
 		{
-			ErrorReporter.Instance.AutomaticallySendEnabled = false;
-			ErrorReporter.Instance.SendedLogRowCount = 100;
-
-			builder.RegisterType<DistrictCache>().AsSelf().AsImplementedInterfaces();
-			
-			builder.RegisterType<CallTaskWorker>()
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			builder.RegisterType<UserService>()
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			builder.RegisterInstance(ErrorReporter.Instance).AsImplementedInterfaces();
-
-			builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
-				.Where(t => t.Name.EndsWith("Handler"))
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			var vodovozBusinessAssembly = typeof(VodovozBusinessAssemblyFinder).Assembly;
-
-			builder.RegisterAssemblyTypes(vodovozBusinessAssembly)
-				.Where(t => t.Name.EndsWith("Provider"))
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			builder.RegisterAssemblyTypes(vodovozBusinessAssembly)
-				.Where(t => t.Name.EndsWith("Model"))
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			builder.RegisterAssemblyTypes(vodovozBusinessAssembly)
-				.Where(t => t.Name.EndsWith("Repository"))
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			builder.RegisterAssemblyTypes(vodovozBusinessAssembly, Assembly.GetExecutingAssembly())
-				.Where(t => t.Name.EndsWith("Factory"))
-				.AsSelf()
-				.AsImplementedInterfaces();
-
-			builder.RegisterAssemblyTypes(vodovozBusinessAssembly)
-				.Where(t => t.Name.EndsWith("Controller"))
-				.AsSelf()
-				.AsImplementedInterfaces();
+			services.AddDeliveryRulesService();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+		{
 			if(env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
@@ -141,6 +42,6 @@ namespace DeliveryRulesService
 			});
 
 			app.ConfigureHealthCheckApplicationBuilder();
-        }
+		}
 	}
 }
