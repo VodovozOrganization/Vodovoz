ca4d205a32c44732113697d9ff412d745852a54f
Sergey Efimov <efimov90@gmail.com>
2024-03-28 13:13:18 +0300

    Чистка кода и изменение маппинга

--- Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/DistrictMap.cs
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/DistrictMap.cs
@@ -55,30 +55,8 @@ namespace Vodovoz.Data.NHibernate.HibernateMapping.Logistic
 				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
 				.Where($"week_day = '{WeekDayName.Sunday}'");
 
-			HasMany(x => x.TodayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Today}'");
-			HasMany(x => x.MondayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Monday}'");
-			HasMany(x => x.TuesdayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Tuesday}'");
-			HasMany(x => x.WednesdayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Wednesday}'");
-			HasMany(x => x.ThursdayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Thursday}'");
-			HasMany(x => x.FridayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Friday}'");
-			HasMany(x => x.SaturdayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Saturday}'");
-			HasMany(x => x.SundayDeliveryScheduleRestrictions)
-				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id")
-				.Where($"week_day = '{WeekDayName.Sunday}'");
+			HasMany(x => x.AllDeliveryScheduleRestrictions)
+				.Cascade.AllDeleteOrphan().Inverse().KeyColumn("district_id");
 		}
 	}
 }
