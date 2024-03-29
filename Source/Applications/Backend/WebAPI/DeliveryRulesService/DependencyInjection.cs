3488462777047b35685483ca5ae8efa3cf6e1422
Sergey Efimov <efimov90@gmail.com>
2024-03-29 10:00:25 +0300

    Переработка

--- Source/Applications/Backend/WebAPI/DeliveryRulesService/DependencyInjection.cs
+++ Source/Applications/Backend/WebAPI/DeliveryRulesService/DependencyInjection.cs
@@ -1,10 +1,16 @@
-﻿using DeliveryRulesService.HealthChecks;
+﻿using DeliveryRulesService.Cache;
+using DeliveryRulesService.HealthChecks;
 using DeliveryRulesService.Workers;
 using Fias.Client;
 using Microsoft.Extensions.DependencyInjection;
 using QS.Project.Core;
+using QS.Services;
+using System.Linq;
+using System.Reflection;
 using Vodovoz.Core.Data.NHibernate;
 using Vodovoz.Core.Data.NHibernate.Mappings;
+using Vodovoz.Tools;
+using Vodovoz.Tools.CallTasks;
 using VodovozHealthCheck;
 
 namespace DeliveryRulesService
@@ -47,7 +53,22 @@ namespace DeliveryRulesService
 
 			Vodovoz.Data.NHibernate.DependencyInjection.AddStaticScopeForEntity(services);
 
-			services.AddHostedServices();
+			services
+				.AddSingleton<DistrictCacheService>()
+				.AddScoped<IErrorReporter, ErrorReporter>(_ => ErrorReporter.Instance)
+				.AddScoped<IUserService, UserService>()
+				.AddScoped<ICallTaskWorker, CallTaskWorker>()
+				.AddHostedServices();
+
+			return services;
+		}
+
+		public static IServiceCollection AddHandlers(this IServiceCollection services)
+		{
+			var typesToRegister = Assembly
+				.GetExecutingAssembly()
+				.GetTypes()
+				.Where(type => type.Name.EndsWith("Handler"));
 
 			return services;
 		}
--- Source/Applications/Backend/WebAPI/DeliveryRulesService/Startup.cs
+++ Source/Applications/Backend/WebAPI/DeliveryRulesService/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Vodovoz.Tools;
 using VodovozHealthCheck;
 
 namespace DeliveryRulesService
@@ -19,6 +20,8 @@ namespace DeliveryRulesService
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			ErrorReporter.Instance.AutomaticallySendEnabled = false;
+			ErrorReporter.Instance.SendedLogRowCount = 100;
 			services.AddDeliveryRulesService();
 		}
 
