3d23475fb0e4df190ee612cf261b4ac146f8683d
Sergey Efimov <efimov90@gmail.com>
2024-04-11 12:37:55 +0300

    Изменение формата запроса

---
+++ Source/Libraries/Core/Backend/RoboAtsService.Contracts/Responses/GetContactPhoneHasOrdersForDeliveryTodayResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace RoboAtsService.Contracts.Responses
+{
+	/// <summary>
+	/// Ответ есть ли заказы с контактным телефоном
+	/// </summary>
+	public class GetContactPhoneHasOrdersForDeliveryTodayResponse
+	{
+		/// <summary>
+		/// Статус наличия заказов с контактным номером из запроса
+		/// </summary>
+		public bool Status { get; set; }
+	}
+}
