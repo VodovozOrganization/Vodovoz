379a4ac0ca636341277df40363b8044aa64100dc
N-Sem <nikiforov.semen@gmail.com>
2024-04-12 16:30:24 +0300

    Добавлен журнал и вью-модель диалога топливных карт

---
+++ Source/Applications/Desktop/VodovozViewModels/Fuel/FuelCards/FuelCardJournalFilterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using QS.Project.Filter;
+
+namespace Vodovoz.ViewModels.Fuel.FuelCards
+{
+	public class FuelCardJournalFilterViewModel : FilterViewModelBase<FuelCardJournalFilterViewModel>
+	{
+		private bool _isExcludeArchived;
+
+		public bool IsExcludeArchived
+		{
+			get => _isExcludeArchived;
+			set => UpdateFilterField(ref _isExcludeArchived, value);
+		}
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/Fuel/FuelCards/FuelCardViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using Vodovoz.Domain.Fuel;
+
+namespace Vodovoz.ViewModels.Fuel.FuelCards
+{
+	public class FuelCardViewModel : EntityTabViewModelBase<FuelCard>
+	{
+		private readonly ILogger<FuelCardViewModel> _logger;
+
+		public FuelCardViewModel(
+			ILogger<FuelCardViewModel> logger,
+			IEntityUoWBuilder uowBuilder,
+			IUnitOfWorkFactory unitOfWorkFactory,
+			ICommonServices commonServices,
+			INavigationManager navigation = null)
+			: base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
+		{
+			_logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+		}
+	}
+}
