a3104beb6799fbc82c7aa69c41593722814ce5d5
N-Sem <nikiforov.semen@gmail.com>
2024-04-12 17:46:11 +0300

    Добавлен вью топливной карты

---
+++ Source/Applications/Desktop/Vodovoz/Views/Fuel/FuelCardView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Fuel
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FuelCardView : Gtk.Bin
+	{
+		public FuelCardView()
+		{
+			this.Build();
+		}
+	}
+}
