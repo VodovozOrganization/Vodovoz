2d39eb50d7d5ee5a3880e94f8935a90bc975b9f6
N-Sem <nikiforov.semen@gmail.com>
2024-04-15 08:31:56 +0300

    Удалена запись в БД дата очистки истории проверок экспресс-доставки

--- Source/Libraries/Core/Business/Vodovoz.Settings.Database/Delivery/FastDeliveryAvailabilityHistorySettings.cs
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/Delivery/FastDeliveryAvailabilityHistorySettings.cs
@@ -6,19 +6,13 @@ namespace Vodovoz.Settings.Database.Delivery
 {
 	public class FastDeliveryAvailabilityHistorySettings : IFastDeliveryAvailabilityHistorySettings
 	{
-		private const string _fastDeliveryHistoryClearDate = "fast_delivery_availability_history_clear_date";
 		private readonly ISettingsController _settingsController;
 
 		public FastDeliveryAvailabilityHistorySettings(ISettingsController settingsController)
 		{
 			_settingsController = settingsController ?? throw new ArgumentNullException(nameof(settingsController));
 		}
-		public void UpdateFastDeliveryHistoryClearDate(string value)
-		{
-			_settingsController.CreateOrUpdateSetting(_fastDeliveryHistoryClearDate, value);
-		}
 
 		public int FastDeliveryHistoryStorageDays => _settingsController.GetValue<int>("fast_delivery_availability_history_storage_days");
-		public DateTime FastDeliveryHistoryClearDate => _settingsController.GetValue<DateTime>(_fastDeliveryHistoryClearDate);
 	}
 }
