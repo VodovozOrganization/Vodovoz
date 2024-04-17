98a4795d7b3e98e364c6bc6069fa490c17f64238
Sergey Efimov <efimov90@gmail.com>
2024-04-17 14:55:56 +0300

    Новый виджет

---
+++ Source/Applications/Desktop/Vodovoz/Logistic/DistrictSetDiffReportConfirmationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DistrictSetDiffReportConfirmationView : Gtk.Bin
+	{
+		public DistrictSetDiffReportConfirmationView()
+		{
+			this.Build();
+		}
+	}
+}
