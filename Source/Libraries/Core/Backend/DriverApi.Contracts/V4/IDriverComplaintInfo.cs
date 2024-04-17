da3309dd80209695e802683c929399a3ba52b96e
Sergey Efimov <efimov90@gmail.com>
2024-04-17 10:35:35 +0300

    Удалена версия 4
    Отключен старый Middleware обработки ошибок
    Поправлены зависимости

--- Source/Libraries/Core/Backend/DriverApi.Contracts/V4/IDriverComplaintInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DriverApi.Contracts.V4
-{
-	public interface IDriverComplaintInfo
-	{
-		int Rating { get; }
-		int DriverComplaintReasonId { get; }
-		string OtherDriverComplaintReasonComment { get; }
-	}
-}
--- Source/Libraries/Core/Backend/DriverApi.Contracts/V4/IDriverCompleteOrderInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DriverApi.Contracts.V4
-{
-	public interface IDriverOrderShipmentInfo : ITrueMarkOrderScannedInfo
-	{
-		int OrderId { get; }
-		int BottlesReturnCount { get; }
-		string DriverComment { get; }
-	}
-}
