dc601f6315064b00609ce5905e88ba8f5d5a8db3
N-Sem <nikiforov.semen@gmail.com>
2024-04-18 13:01:42 +0300

    Добавлены классы для сохранения данных запросов

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Fuel/FuelApiRequest.cs
@@ -0,0 +1,45 @@
+﻿using QS.DomainModel.Entity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Fuel
+{
+	public class FuelApiRequest : IDomainObject
+	{
+		public virtual int Id { get; set; }
+		public virtual DateTime RequestDateTime { get; set; }
+		public virtual FuelApiRequestFunction RequestFunction { get; set; }
+		public virtual FuelApiResponseResult ResponseResult { get; set; }
+		public virtual string ErrorResponseMessage { get; set; }
+	}
+
+	public enum FuelApiRequestFunction
+	{
+		[Display(Name = "Не указано")]
+		None,
+		[Display(Name = "Авторизация")]
+		Login,
+		[Display(Name = "Список карт")]
+		FuelCardsData,
+		[Display(Name = "Информация по карте")]
+		FuelCardDetails,
+		[Display(Name = "Список лимитов карты")]
+		FuelCardsLimitsData,
+		[Display(Name = "Удаление лимитов карты")]
+		FuelCardsLimitsDelete,
+		[Display(Name = "Создание лимита карты")]
+		FuelCardsLimitCreate,
+		[Display(Name = "Список транзакций")]
+		TransactionsData
+	}
+
+	public enum FuelApiResponseResult
+	{
+		[Display(Name = "Не указано")]
+		None,
+		[Display(Name = "Успех")]
+		Success,
+		[Display(Name = "Ошибка")]
+		Error
+	}
+}
