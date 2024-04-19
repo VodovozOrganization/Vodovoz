074a0eeb76db6030e9a98c6cc5bd9309c0e0912a
N-Sem <nikiforov.semen@gmail.com>
2024-04-19 16:43:56 +0300

    Добавлены классы списания километража и мапинги

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Logistic/Cars/MileageWriteOffReason.cs
@@ -0,0 +1,62 @@
+﻿using QS.DomainModel.Entity;
+using QS.DomainModel.Entity.EntityPermissions;
+using QS.HistoryLog;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Logistic.Cars
+{
+	[Appellative(Gender = GrammaticalGender.Feminine,
+		NominativePlural = "причины списаний киломатража",
+		Nominative = "причина списания километража")]
+	[EntityPermission]
+	[HistoryTrace]
+	public class MileageWriteOffReason : PropertyChangedBase, IDomainObject, IValidatableObject
+	{
+		private const int _nameMaxLength = 100;
+		private const int _descriptionMaxLength = 500;
+
+		private string _name;
+		private string _description;
+
+		public virtual int Id { get; set; }
+
+		[Display(Name = "Наименование")]
+		public virtual string Name
+		{
+			get => _name;
+			set => SetField(ref _name, value);
+		}
+
+		[Display(Name = "Описание")]
+		public virtual string Description
+		{
+			get => _description;
+			set => SetField(ref _description, value);
+		}
+
+		public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if(string.IsNullOrWhiteSpace(Name))
+			{
+				yield return new ValidationResult(
+					"Наименование должно быть обязательно указано",
+					new[] { nameof(Name) });
+			}
+
+			if(Name?.Length > _nameMaxLength)
+			{
+				yield return new ValidationResult(
+					$"Наименование не должно быть длиннее {_nameMaxLength} символов",
+					new[] { nameof(Name) });
+			}
+
+			if(Description?.Length > _descriptionMaxLength)
+			{
+				yield return new ValidationResult(
+					$"Описание не должно быть длиннее {_descriptionMaxLength} символов",
+					new[] { nameof(Name) });
+			}
+		}
+	}
+}
