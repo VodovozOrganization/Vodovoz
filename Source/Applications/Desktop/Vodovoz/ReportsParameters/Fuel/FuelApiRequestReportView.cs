0f14c3dcfecdeab5910ad4766c247dd37a8d602a
N-Sem <nikiforov.semen@gmail.com>
2024-04-19 11:06:22 +0300

    Добавлена вью отчета по запросам

---
+++ Source/Applications/Desktop/Vodovoz/ReportsParameters/Fuel/FuelApiRequestReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ReportsParameters.Fuel
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FuelApiRequestReportView : Gtk.Bin
+	{
+		public FuelApiRequestReportView()
+		{
+			this.Build();
+		}
+	}
+}
