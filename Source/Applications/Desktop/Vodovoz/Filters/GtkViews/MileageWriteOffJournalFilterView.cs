fabf1c8c9917646d088ce2dc2e83522561247aac
N-Sem <nikiforov.semen@gmail.com>
2024-04-22 17:15:27 +0300

    Фильтр журнала списаний километража

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/MileageWriteOffJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class MileageWriteOffJournalFilterView : Gtk.Bin
+	{
+		public MileageWriteOffJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
