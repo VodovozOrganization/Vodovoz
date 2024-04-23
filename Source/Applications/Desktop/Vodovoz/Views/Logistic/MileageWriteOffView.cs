e5c8dc5b1bf292f60ee8f5d939caac8e2847a8cc
N-Sem <nikiforov.semen@gmail.com>
2024-04-23 15:26:40 +0300

    Логика диалога списания и привязки

--- Source/Applications/Desktop/Vodovoz/Views/Logistic/MileageWriteOffView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/MileageWriteOffView.cs
@@ -1,10 +1,46 @@
-﻿namespace Vodovoz.Views.Logistic
+﻿using QS.Views.GtkUI;
+using Vodovoz.ViewModels.Logistic.MileagesWriteOff;
+
+namespace Vodovoz.Views.Logistic
 {
-	public partial class MileageWriteOffView : Gtk.Bin
+	public partial class MileageWriteOffView : TabViewBase<MileageWriteOffViewModel>
 	{
-		public MileageWriteOffView()
+		public MileageWriteOffView(MileageWriteOffViewModel viewModel) : base(viewModel)
 		{
 			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			yvboxMain.Sensitive = ViewModel.CanCreateOrUpdate;
+
+			datepickerCreateDate.Binding
+				.AddBinding(ViewModel.Entity, e => e.CreationDate, w => w.DateOrNull)
+				.InitializeFromSource();
+
+			datepickerWriteOffDate.Binding
+				.AddBinding(ViewModel.Entity, e => e.WriteOffDate, w => w.DateOrNull)
+				.InitializeFromSource();
+
+			yspinbuttonDistance.Binding
+				.AddBinding(ViewModel.Entity, e => e.DistanceKm, w => w.ValueAsDecimal)
+				.InitializeFromSource();
+
+			entityentryAuthor.ViewModel = ViewModel.AuthorEntryViewModel;
+
+			entityentryCar.ViewModel = ViewModel.CarEntryViewModel;
+			entityentryCar.ViewModel.ChangedByUser += (s, e) => ViewModel.CarChangedByUserCommand.Execute();
+
+			entityentryDriver.ViewModel = ViewModel.DriverEntryViewModel;
+			//entityentryWriteOffReason
+			
+			ytextviewComment.Binding
+				.AddBinding(ViewModel.Entity, e => e.Comment, w => w.Buffer.Text)
+				.InitializeFromSource();
+
+			ybuttonSave.BindCommand(ViewModel.SaveCommand);
+			ybuttonCancel.BindCommand(ViewModel.CancelCommand);
 		}
 	}
 }
--- Source/Applications/Desktop/VodovozViewModels/Logistic/MileagesWriteOff/MileageWriteOffViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Logistic/MileagesWriteOff/MileageWriteOffViewModel.cs
@@ -1,23 +1,46 @@
-﻿using QS.Commands;
+﻿using Autofac;
+using QS.Commands;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
 using QS.Services;
 using QS.ViewModels;
+using QS.ViewModels.Control.EEVM;
+using System;
+using System.Collections.Generic;
+using Vodovoz.Core.Domain.Employees;
 using Vodovoz.Domain.Logistic.Cars;
+using Vodovoz.EntityRepositories.Employees;
 using Vodovoz.Tools;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Employees;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Logistic;
+using Vodovoz.ViewModels.Journals.JournalViewModels.Employees;
+using Vodovoz.ViewModels.Journals.JournalViewModels.Logistic;
+using Vodovoz.ViewModels.ViewModels.Employees;
+using Vodovoz.ViewModels.ViewModels.Logistic;
 
 namespace Vodovoz.ViewModels.Logistic.MileagesWriteOff
 {
 	public class MileageWriteOffViewModel : EntityTabViewModelBase<MileageWriteOff>
 	{
+		private readonly ILifetimeScope _lifetimeScope;
+
 		public MileageWriteOffViewModel(
 			IEntityUoWBuilder uowBuilder,
 			IUnitOfWorkFactory unitOfWorkFactory,
 			ICommonServices commonServices,
-			INavigationManager navigation)
+			INavigationManager navigation,
+			ILifetimeScope lifetimeScope,
+			IEmployeeRepository employeeRepository)
 			: base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
 		{
+			if(employeeRepository is null)
+			{
+				throw new ArgumentNullException(nameof(employeeRepository));
+			}
+
+			_lifetimeScope = lifetimeScope ?? throw new ArgumentNullException(nameof(lifetimeScope));
+
 			if(!CanRead)
 			{
 				AbortOpening("У вас недостаточно прав для просмотра");
@@ -28,12 +51,24 @@ namespace Vodovoz.ViewModels.Logistic.MileagesWriteOff
 				? $"Диалог создания {Entity.GetType().GetClassUserFriendlyName().Genitive}"
 				: $"{Entity.GetType().GetClassUserFriendlyName().Nominative.CapitalizeSentence()} №{Entity.Title}";
 
-			SaveCommand = new DelegateCommand(() => Save(true));
+			SaveCommand = new DelegateCommand(() => Save(true), () => CanCreateOrUpdate);
 			CancelCommand = new DelegateCommand(() => Close(false, CloseSource.Cancel));
+			CarChangedByUserCommand = new DelegateCommand(OnCarChangedByUser);
+
+			if(UoW.IsNew)
+			{
+				Entity.CreationDate = DateTime.Today;
+				Entity.Author = employeeRepository.GetEmployeeForCurrentUser(UoW);
+			}
+
+			CarEntryViewModel = CreateCarEntryViewModel();
+			DriverEntryViewModel = CreateDriverEntryViewModel();
+			AuthorEntryViewModel = CreateAuthorEntryViewModell();
 		}
 
 		public DelegateCommand SaveCommand { get; }
 		public DelegateCommand CancelCommand { get; }
+		public DelegateCommand CarChangedByUserCommand { get; }
 
 		#region Permissions
 
@@ -45,5 +80,67 @@ namespace Vodovoz.ViewModels.Logistic.MileagesWriteOff
 		public bool CanCreateOrUpdate => Entity.Id == 0 ? CanCreate : CanUpdate;
 
 		#endregion
+
+		public IEntityEntryViewModel CarEntryViewModel { get; }
+		public IEntityEntryViewModel DriverEntryViewModel { get; }
+		public IEntityEntryViewModel AuthorEntryViewModel { get; }
+
+		private IEntityEntryViewModel CreateCarEntryViewModel()
+		{
+			var viewModel =
+				new CommonEEVMBuilderFactory<MileageWriteOff>(this, Entity, UoW, NavigationManager, _lifetimeScope)
+				.ForProperty(x => x.Car)
+				.UseViewModelDialog<CarViewModel>()
+				.UseViewModelJournalAndAutocompleter<CarJournalViewModel, CarJournalFilterViewModel>(filter =>
+				{
+					filter.Archive = false;
+					filter.RestrictedCarOwnTypes = new List<CarOwnType> { CarOwnType.Company };
+				})
+				.Finish();
+
+			viewModel.CanViewEntity = false;
+
+			return viewModel;
+		}
+
+		private IEntityEntryViewModel CreateDriverEntryViewModel()
+		{
+			var viewModel =
+				new CommonEEVMBuilderFactory<MileageWriteOff>(this, Entity, UoW, NavigationManager, _lifetimeScope)
+				.ForProperty(x => x.Driver)
+				.UseViewModelDialog<EmployeeViewModel>()
+				.UseViewModelJournalAndAutocompleter<EmployeesJournalViewModel, EmployeeFilterViewModel>(filter =>
+				{
+					filter.Category = EmployeeCategory.driver;
+					filter.Status = EmployeeStatus.IsWorking;
+				})
+				.Finish();
+
+			viewModel.CanViewEntity = false;
+
+			return viewModel;
+		}
+
+		private IEntityEntryViewModel CreateAuthorEntryViewModell()
+		{
+			var viewModel =
+				new CommonEEVMBuilderFactory<MileageWriteOff>(this, Entity, UoW, NavigationManager, _lifetimeScope)
+				.ForProperty(x => x.Author)
+				.UseViewModelJournalAndAutocompleter<EmployeesJournalViewModel, EmployeeFilterViewModel>(filter =>
+				{
+					filter.Category = EmployeeCategory.office;
+					filter.Status = EmployeeStatus.IsWorking;
+				})
+				.Finish();
+
+			viewModel.CanViewEntity = false;
+
+			return viewModel;
+		}
+
+		private void OnCarChangedByUser()
+		{
+			Entity.Driver = Entity.Car?.Driver;
+		}
 	}
 }
