6944c161a4dbdf906411a098f0bcb437586e93b4
Andrey <as1854@yandex.ru>
2024-04-24 15:51:10 +0300

    4756 добавил вьюхи заявки на звоно и ее причины закрытия

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/RequestForCallView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Vodovoz.Views.Orders
+{
+	[ToolboxItem(true)]
+	public partial class RequestForCallView : Gtk.Bin
+	{
+		public RequestForCallView()
+		{
+			Build();
+		}
+	}
+}
