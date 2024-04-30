3ba0fd0ab38f625d6a6ce60bec276559d1c76397
N-Sem <nikiforov.semen@gmail.com>
2024-04-30 18:01:25 +0300

    Вью-модель виджета управления версиями топливных карт

---
+++ Source/Applications/Desktop/VodovozViewModels/Widgets/Cars/CarFuelCardVersionViewModel.cs
@@ -0,0 +1,171 @@
+﻿using Autofac;
+using QS.Commands;
+using QS.Navigation;
+using QS.Services;
+using QS.ViewModels;
+using QS.ViewModels.Control.EEVM;
+using QS.ViewModels.Dialog;
+using System;
+using System.Linq;
+using Vodovoz.Controllers;
+using Vodovoz.Domain.Fuel;
+using Vodovoz.Domain.Logistic.Cars;
+using Vodovoz.ViewModels.Fuel.FuelCards;
+
+namespace Vodovoz.ViewModels.Widgets.Cars
+{
+	public class CarFuelCardVersionViewModel : EntityWidgetViewModelBase<Car>
+	{
+		private DateTime? _selectedDate;
+		private FuelCard _selectedFuelCard;
+		private FuelCardVersion _selectedVersion;
+		private DialogViewModelBase _parentDialog;
+
+		private readonly ICarFuelCardVersionController _fuelCardVersionController;
+		private readonly INavigationManager _navigationManager;
+		private readonly ILifetimeScope _lifetimeScope;
+
+		public CarFuelCardVersionViewModel(
+			Car entity,
+			ICommonServices commonServices,
+			ICarFuelCardVersionController fuelCardVersionController,
+			INavigationManager navigationManager,
+			ILifetimeScope lifetimeScope)
+			: base(entity, commonServices)
+		{
+			_fuelCardVersionController = fuelCardVersionController ?? throw new System.ArgumentNullException(nameof(fuelCardVersionController));
+			_navigationManager = navigationManager;
+			_lifetimeScope = lifetimeScope;
+			CanRead = PermissionResult.CanRead;
+			CanCreate = PermissionResult.CanCreate && Entity.Id == 0
+				&& commonServices.CurrentPermissionService.ValidatePresetPermission(Vodovoz.Permissions.Logistic.Car.CanChangeFuelCardNumber);
+			CanEdit = commonServices.CurrentPermissionService.ValidatePresetPermission(Vodovoz.Permissions.Logistic.Car.CanChangeFuelCardNumber);
+
+			if(IsNewCar)
+			{
+				SelectedDate = DateTime.Now.Date;
+			}
+
+			AddNewVersionCommand = new DelegateCommand(AddNewVersion, () => CanAddNewVersion);
+			ChangeVersionStartDateCommand = new DelegateCommand(ChangeVersionStartDate, () => CanChangeVersionStartDate);
+		}
+
+		public DelegateCommand AddNewVersionCommand { get; }
+		public DelegateCommand ChangeVersionStartDateCommand { get; }
+
+		public IEntityEntryViewModel FuelCardEntryViewModel { get; private set; }
+
+		public bool IsNewCar => Entity.Id == 0;
+		public DialogViewModelBase ParentDialog
+		{
+			get => _parentDialog;
+			set
+			{
+				SetField(ref _parentDialog, value);
+
+				if(_parentDialog == null || FuelCardEntryViewModel != null)
+				{
+					return;
+				}
+
+				FuelCardEntryViewModel = GetFuelCardViewModel();
+			}
+		}
+
+		public virtual DateTime? SelectedDate
+		{
+			get => _selectedDate;
+			set
+			{
+				if(SetField(ref _selectedDate, value))
+				{
+					OnPropertyChanged(nameof(CanAddNewVersion));
+					OnPropertyChanged(nameof(CanChangeVersionStartDate));
+				}
+			}
+		}
+
+		public virtual FuelCard SelectedFuelCard
+		{
+			get => _selectedFuelCard;
+			set
+			{
+				if(SetField(ref _selectedFuelCard, value))
+				{
+					OnPropertyChanged(nameof(CanAddNewVersion));
+				}
+			}
+		}
+
+		public virtual FuelCardVersion SelectedVersion
+		{
+			get => _selectedVersion;
+			set
+			{
+				if(SetField(ref _selectedVersion, value))
+				{
+					OnPropertyChanged(nameof(CanChangeVersionStartDate));
+				}
+			}
+		}
+
+		public virtual bool CanRead { get; }
+		public virtual bool CanCreate { get; }
+		public virtual bool CanEdit { get; }
+		public virtual bool CanCreateOrUpdate => CanCreate || CanEdit;
+
+		public bool CanAddNewVersion =>
+			CanCreate
+			&& SelectedDate.HasValue
+			&& SelectedFuelCard != null
+			&& Entity.FuelCardVersions.All(x => x.Id != 0)
+			&& _fuelCardVersionController.IsValidDateForNewCarVersion(SelectedDate.Value);
+
+		public bool CanChangeVersionStartDate =>
+			SelectedDate.HasValue
+			&& SelectedVersion != null
+			&& (CanEdit || SelectedVersion.Id == 0)
+			&& _fuelCardVersionController.IsValidDateForVersionStartDateChange(SelectedVersion, SelectedDate.Value);
+
+		private void AddNewVersion()
+		{
+			if(SelectedDate == null)
+			{
+				return;
+			}
+			_fuelCardVersionController.CreateAndAddVersion(SelectedFuelCard, SelectedDate);
+
+			OnPropertyChanged(nameof(CanAddNewVersion));
+			OnPropertyChanged(nameof(CanChangeVersionStartDate));
+		}
+
+		private void ChangeVersionStartDate()
+		{
+			if(SelectedDate == null || SelectedVersion == null)
+			{
+				return;
+			}
+			_fuelCardVersionController.ChangeVersionStartDate(SelectedVersion, SelectedDate.Value);
+
+			OnPropertyChanged(nameof(CanAddNewVersion));
+			OnPropertyChanged(nameof(CanChangeVersionStartDate));
+		}
+
+		private IEntityEntryViewModel GetFuelCardViewModel()
+		{
+			var viewModel = new CommonEEVMBuilderFactory<CarFuelCardVersionViewModel>(ParentDialog, this, UoW, _navigationManager, _lifetimeScope)
+				.ForProperty(x => x.SelectedFuelCard)
+				.UseViewModelDialog<FuelCardViewModel>()
+				.UseViewModelJournalAndAutocompleter<FuelCardJournalViewModel, FuelCardJournalFilterViewModel>(filter =>
+				{
+					filter.IsShowArchived = false;
+				})
+				.Finish();
+
+			viewModel.IsEditable = CanCreateOrUpdate;
+			viewModel.CanViewEntity = CommonServices.CurrentPermissionService.ValidateEntityPermission(typeof(FuelCard)).CanRead;
+
+			return viewModel;
+		}
+	}
+}
