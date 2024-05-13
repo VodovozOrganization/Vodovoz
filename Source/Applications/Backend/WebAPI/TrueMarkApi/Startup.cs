f791c16b0fbdee2571c6f59383d187c1036eca4c
Sergey Efimov <efimov90@gmail.com>
2024-05-13 10:24:03 +0300

    Фикс

--- Source/Applications/Backend/WebAPI/TrueMarkApi/Startup.cs
+++ Source/Applications/Backend/WebAPI/TrueMarkApi/Startup.cs
@@ -1,11 +1,13 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using NLog.Web;
 using QS.HistoryLog;
 using QS.Project.Core;
 using QS.Services;
