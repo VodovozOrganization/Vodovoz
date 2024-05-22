a69e4f96b147ad22f5c86aa834a983fee9f0c3d2
Art8m <9artbe@gmail.com>
2024-05-22 18:09:24 +0300

    VodovozZabbixSender

---
+++ Source/Libraries/Core/Backend/Vodovoz.Zabbix.Sender/VodovozZabbixSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Vodovoz.Settings.Metrics;
+using ZabbixAsyncSender = ZabbixSender.Async.Sender;
+
+namespace Vodovoz.Zabbix.Sender
+{
+	public class VodovozZabbixSender : IZabbixSender
+	{
+		private readonly ZabbixAsyncSender _sender;
+		private readonly string _workerName;
+		private readonly IMetricSettings _metricSettings;
+
+		public VodovozZabbixSender(string workerName, IMetricSettings metricSettings)
+		{
+			_workerName = workerName ?? throw new ArgumentNullException(nameof(workerName));
+			_metricSettings = metricSettings?? throw new ArgumentNullException(nameof(metricSettings));
+			_sender = new ZabbixAsyncSender(metricSettings.ZabbixHost);
+		}
+
+		public async Task SendIsHealthyAsync(bool isHealthy)
+		{
+			var response = await _sender.Send(_workerName, _metricSettings.ZabbixHealthMetricName, isHealthy.ToString());
+		}
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/Metric/MetricSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Vodovoz.Settings.Metrics;
+
+namespace Vodovoz.Settings.Database.Metric
+{
+	public class MetricSettings : IMetricSettings
+	{
+		private readonly ISettingsController _settingsController;
+
+		public MetricSettings(ISettingsController settingsController)
+		{
+			_settingsController = settingsController ?? throw new ArgumentNullException(nameof(settingsController));
+		}
+
+		public string ZabbixHost => _settingsController.GetStringValue(nameof(ZabbixHost));
+
+		public string ZabbixHealthMetricName => _settingsController.GetStringValue(nameof(ZabbixHealthMetricName));
+	}
+}
