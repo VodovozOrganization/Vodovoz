904f6572a8b8814d7c007eed7f4e4f9dd743d4de
N-Sem <nikiforov.semen@gmail.com>
2024-05-22 16:24:29 +0300

    Добавлены виджеты для отображения данных страховок

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/CarInsuranceVersionEditingView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarInsuranceVersionEditingView : Gtk.Bin
+	{
+		public CarInsuranceVersionEditingView()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/CarInsuranceVersionView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarInsuranceVersionView : Gtk.Bin
+	{
+		public CarInsuranceVersionView()
+		{
+			this.Build();
+		}
+	}
+}
