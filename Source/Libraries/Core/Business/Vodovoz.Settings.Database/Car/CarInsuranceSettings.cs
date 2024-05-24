f75c25ce68d6f8a9568d368df31e50d2d5416acd
N-Sem <nikiforov.semen@gmail.com>
2024-05-24 11:33:06 +0300

    Добавлены параметры кол-ва дней для появления уведомлений

---
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/Car/CarInsuranceSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Vodovoz.Settings.Database.Car
+{
+	public class CarInsuranceSettings
+	{
+		private readonly string _parametersPrefix = "CarInsurance.";
+		private readonly ISettingsController _settingsController;
+
+		public CarInsuranceSettings(ISettingsController settingsController)
+		{
+			_settingsController = settingsController ?? throw new ArgumentNullException(nameof(settingsController));
+		}
+
+		public int OsagoEndingNotifyDaysBefore =>
+			_settingsController.GetValue<int>($"{_parametersPrefix}{nameof(OsagoEndingNotifyDaysBefore)}");
+
+		public int KaskoEndingNotifyDaysBefore =>
+			_settingsController.GetValue<int>($"{_parametersPrefix}{nameof(KaskoEndingNotifyDaysBefore)}");
+
+		public void SetOsagoEndingNotifyDaysBefore(string value)
+		{
+			_settingsController.CreateOrUpdateSetting($"{_parametersPrefix}{nameof(OsagoEndingNotifyDaysBefore)}", value);
+		}
+
+		public void SetKaskoEndingNotifyDaysBefore(string value)
+		{
+			_settingsController.CreateOrUpdateSetting($"{_parametersPrefix}{nameof(KaskoEndingNotifyDaysBefore)}", value);
+		}
+	}
+}
