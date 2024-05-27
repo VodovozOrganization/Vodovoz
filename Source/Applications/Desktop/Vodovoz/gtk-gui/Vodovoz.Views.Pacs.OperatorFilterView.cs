6704567acc4bdf04fc9bd5d6932d67a84747100b
Sergey Efimov <efimov90@gmail.com>
2024-05-27 18:35:40 +0300

    фильтр и фикс стетика

---
+++ Source/Applications/Desktop/Vodovoz/gtk-gui/Vodovoz.Views.Pacs.OperatorFilterView.cs
@@ -0,0 +1,18 @@
+
+namespace Vodovoz.Views.Pacs
+{
+	public partial class OperatorFilterView
+	{
+		private Gtk.HBox hbox1;
+
+		private Gamma.GtkWidgets.yRadioButton yradiobuttonAll;
+
+		private Gamma.GtkWidgets.yRadioButton yradiobuttonEnabled;
+
+		private Gamma.GtkWidgets.yRadioButton yradiobuttonDisabled;
+
+		private void Build()
+		{
+		}
+	}
+}
