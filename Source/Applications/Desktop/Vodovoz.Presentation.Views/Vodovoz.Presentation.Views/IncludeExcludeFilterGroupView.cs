99c1b02bd03c458b828997129364dbac7734048e
Sergey Efimov <efimov90@gmail.com>
2024-05-28 14:48:15 +0300

    Попытка добавить View проект

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.Views/Vodovoz.Presentation.Views/IncludeExcludeFilterGroupView.cs
@@ -0,0 +1,11 @@
+﻿namespace Vodovoz.Presentation.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class IncludeExcludeFilterGroupView : Gtk.Bin
+	{
+		public IncludeExcludeFilterGroupView()
+		{
+			Build();
+		}
+	}
+}
