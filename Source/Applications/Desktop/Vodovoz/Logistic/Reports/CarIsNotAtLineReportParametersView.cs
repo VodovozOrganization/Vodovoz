f8077199053145d369fdea696e96d2410b51546b
Sergey Efimov <efimov90@gmail.com>
2024-05-28 14:19:11 +0300

    Simple WidgetLayout

---
+++ Source/Applications/Desktop/Vodovoz/Logistic/Reports/CarIsNotAtLineReportParametersView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Logistic.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarIsNotAtLineReportParametersView : Gtk.Bin
+	{
+		public CarIsNotAtLineReportParametersView()
+		{
+			this.Build();
+		}
+	}
+}
