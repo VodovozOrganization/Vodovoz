e86a2ee44729a7c4960dad211511b495bc0ff9a9
N-Sem <nikiforov.semen@gmail.com>
2024-06-02 12:17:56 +0300

    Добавлен виджет редактирования версии

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/CarVersionEditingView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CarVersionEditingView : Gtk.Bin
+	{
+		public CarVersionEditingView()
+		{
+			this.Build();
+		}
+	}
+}
