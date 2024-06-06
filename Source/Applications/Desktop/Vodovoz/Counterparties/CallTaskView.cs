9112ffe4311d87738ddd75e305fe993199090add
Sergey Efimov <efimov90@gmail.com>
2024-06-06 16:26:36 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Counterparties/CallTaskView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Counterparties
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CallTaskView : Gtk.Bin
+	{
+		public CallTaskView()
+		{
+			this.Build();
+		}
+	}
+}
