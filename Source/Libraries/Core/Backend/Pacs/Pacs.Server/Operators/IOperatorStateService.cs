7e5b00b40d6339a75552dea630bb457976548e22
Sergey Efimov <efimov90@gmail.com>
2024-06-19 10:45:11 +0300

    Замена вызовов и интерфейса

--- Source/Libraries/Core/Backend/Pacs/Pacs.Server/Operators/IOperatorStateService.cs
+++ Source/Libraries/Core/Backend/Pacs/Pacs.Server/Operators/IOperatorStateService.cs
@@ -1,8 +1,24 @@
-﻿namespace Pacs.Server.Operators
+﻿using Pacs.Core.Messages.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Pacs.Server.Operators
 {
 	public interface IOperatorStateService
 	{
+		Task<OperatorResult> ChangePhone(int operatorId, string phoneNumber);
+		Task<OperatorResult> Connect(int operatorId);
+		Task<OperatorResult> Disconnect(int operatorId);
+		Task<OperatorResult> StartBreak(int operatorId, OperatorBreakType breakType);
+		Task<OperatorResult> EndBreak(int operatorId);
+		Task<OperatorResult> StartWorkShift(int operatorId, string phoneNumber);
+		Task<OperatorResult> EndWorkShift(int operatorId, string reason);
+		Task KeepAlive(int operatorId);
+
+		[Obsolete("Старый метод, нужно заменить логику в месте использования", true)]
 		OperatorController GetOperatorController(int operatorId);
+
+		[Obsolete("Старый метод, нужно заменить логику в месте использования", true)]
 		OperatorController GetOperatorController(string phoneNumber);
 	}
 }
