5600c6622ae94a0386a84a1ecfdebaa28b20e879
Andrey Shaklein <as1854@yandex.ru>
2024-06-19 17:05:56 +0300

    пропущенные юзинги

--- Source/Libraries/Core/Business/Vodovoz.Application/Orders/Services/OrderFromOnlineOrderCreator.cs
+++ Source/Libraries/Core/Business/Vodovoz.Application/Orders/Services/OrderFromOnlineOrderCreator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using QS.DomainModel.UoW;
+using Vodovoz.Core.Domain.Clients;
 using Vodovoz.Domain.Client;
 using Vodovoz.Domain.Employees;
 using Vodovoz.Domain.Orders;
