be8067d2bdaadeed9a05c4d71ce7eff4ec87d3fa
Sergey Efimov <efimov90@gmail.com>
2024-06-19 11:09:16 +0300

    Реализация методов оператора
    обозначение методов устаревшими с ошибкой
    прогрев кэша вынесен в метод
    исправлена ошибка в прогреве

--- Source/Libraries/Core/Backend/Pacs/Pacs.Server/Operators/OperatorStateService.cs
+++ Source/Libraries/Core/Backend/Pacs/Pacs.Server/Operators/OperatorStateService.cs
@@ -1,7 +1,9 @@
 ﻿using Pacs.Core;
+using Pacs.Core.Messages.Commands;
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Threading.Tasks;
 using Vodovoz.Core.Data.Repositories;
 
 namespace Pacs.Server.Operators
@@ -13,28 +15,125 @@ namespace Pacs.Server.Operators
 		private readonly IOperatorRepository _operatorRepository;
 		private readonly ConcurrentDictionary<int, OperatorController> _operatorControllers;
 
-		public OperatorStateService(IOperatorControllerFactory operatorControllerFactory, IPacsRepository pacsRepository, IOperatorRepository operatorRepository)
+		private readonly TimeSpan _warmUpOperatorStatesFromNodTimeSpan = TimeSpan.FromHours(-15);
+
+		public OperatorStateService(
+			IOperatorControllerFactory operatorControllerFactory,
+			IPacsRepository pacsRepository,
+			IOperatorRepository operatorRepository)
 		{
-			_operatorControllerFactory = operatorControllerFactory ?? throw new ArgumentNullException(nameof(operatorControllerFactory));
-			_pacsRepository = pacsRepository;
-			_operatorRepository = operatorRepository ?? throw new ArgumentNullException(nameof(operatorRepository));
+			_operatorControllerFactory = operatorControllerFactory
+				?? throw new ArgumentNullException(nameof(operatorControllerFactory));
+			_pacsRepository = pacsRepository
+				?? throw new ArgumentNullException(nameof(pacsRepository));
+			_operatorRepository = operatorRepository
+				?? throw new ArgumentNullException(nameof(operatorRepository));
 
 			_operatorControllers = new ConcurrentDictionary<int, OperatorController>();
 
-			var lastOperators = _pacsRepository.GetOperators(DateTime.Now.Add(TimeSpan.FromHours(15)));
+			WarmUpOperatorStates();
+		}
 
-			if(!lastOperators.Any())
-			{
-				return;
-			}
+		/// <summary>
+		/// Смена телефона оператора
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <param name="phoneNumber">Новый номер телефона</param>
+		/// <returns></returns>
+		public async Task<OperatorResult> ChangePhone(int operatorId, string phoneNumber)
+		{
+			return await GetOperatorController(operatorId).ChangePhone(phoneNumber);
+		}
 
-			foreach(var op in lastOperators)
+		/// <summary>
+		/// Подключение оператора
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <returns></returns>
+		public async Task<OperatorResult> Connect(int operatorId)
+		{
+			return await GetOperatorController(operatorId).Connect();
+		}
+
+		/// <summary>
+		/// Отключение оператора
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <returns></returns>
+		public async Task<OperatorResult> Disconnect(int operatorId)
+		{
+			return await GetOperatorController(operatorId).Disconnect();
+		}
+
+		/// <summary>
+		/// Начало перерыва
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <param name="breakType"><see cref="OperatorBreakType">Тип перерыва</see></param>
+		/// <returns></returns>
+		public async Task<OperatorResult> StartBreak(int operatorId, OperatorBreakType breakType)
+		{
+			return await GetOperatorController(operatorId).StartBreak(breakType);
+		}
+
+		/// <summary>
+		/// Завершение перерыва
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <returns></returns>
+		public async Task<OperatorResult> EndBreak(int operatorId)
+		{
+			return await GetOperatorController(operatorId).EndBreak();
+		}
+
+		/// <summary>
+		/// Начало смены
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <param name="phoneNumber">Номер телефона</param>
+		/// <returns></returns>
+		public async Task<OperatorResult> StartWorkShift(int operatorId, string phoneNumber)
+		{
+			return await GetOperatorController(operatorId).StartWorkShift(phoneNumber);
+		}
+
+		/// <summary>
+		/// Завершение смены оператора
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <param name="reason">Причина</param>
+		/// <returns></returns>
+		public async Task<OperatorResult> EndWorkShift(int operatorId, string reason)
+		{
+			return await GetOperatorController(operatorId).EndWorkShift(reason);
+		}
+
+		/// <summary>
+		/// Поддержание состояния подключения
+		/// </summary>
+		/// <param name="operatorId">Идентификатор оператора</param>
+		/// <returns></returns>
+		public async Task KeepAlive(int operatorId)
+		{
+			await GetOperatorController(operatorId).KeepAlive();
+		}
+
+		private void ControllerOnDisconnect(object sender, int operatorId)
+		{
+			if(_operatorControllers.TryRemove(operatorId, out var controller))
 			{
-				if(!_operatorControllers.TryGetValue(op.Id, out var _))
-				{
-					_operatorControllers.TryAdd(op.Id, _operatorControllerFactory.CreateOperatorController(op.Id));
-				}
+				controller.Dispose();
+				controller.OnDisconnect -= ControllerOnDisconnect;
 			}
+		}
+
+		public OperatorController GetOperatorController(string phoneNumber)
+		{
+			var controller = _operatorControllers
+				.Select(x => x.Value)
+				.FirstOrDefault(x => x.AssignedToPhone(phoneNumber));
+
+			return controller;
 		}
 
 		public OperatorController GetOperatorController(int operatorId)
@@ -63,18 +162,24 @@ namespace Pacs.Server.Operators
 			return controller;
 		}
 
-		public OperatorController GetOperatorController(string phoneNumber)
+		/// <summary>
+		/// Прогрев кэша
+		/// </summary>
+		private void WarmUpOperatorStates()
 		{
-			var controller = _operatorControllers.Select(x => x.Value).FirstOrDefault(x => x.AssignedToPhone(phoneNumber));
-			return controller;
-		}
+			var lastOperators = _pacsRepository.GetOperatorStatesFrom(DateTime.Now.Add(_warmUpOperatorStatesFromNodTimeSpan));
 
-		private void ControllerOnDisconnect(object sender, int operatorId)
-		{
-			if(_operatorControllers.TryRemove(operatorId, out var controller))
+			if(!lastOperators.Any())
 			{
-				controller.Dispose();
-				controller.OnDisconnect -= ControllerOnDisconnect;
+				return;
+			}
+
+			foreach(var operatorState in lastOperators)
+			{
+				if(!_operatorControllers.TryGetValue(operatorState.OperatorId, out var _))
+				{
+					_operatorControllers.TryAdd(operatorState.OperatorId, _operatorControllerFactory.CreateOperatorController(operatorState.OperatorId));
+				}
 			}
 		}
 	}
