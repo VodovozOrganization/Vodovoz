c47733e0e228f29ea65efab7330e78cc1d3fd0a1
Vadim Gordeev <enzogord@gmail.com>
2024-06-20 08:17:53 +0300

    Order entity refactoring

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Orders/OrderEnums/OrderAddressType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Orders
+{
+	public enum OrderAddressType
+	{
+		[Display(Name = "Обычная доставка")]
+		Delivery,
+		[Display(Name = "Сервисное обслуживание")]
+		Service,
+		[Display(Name = "Сетевой магазин")]
+		ChainStore,
+		[Display(Name = "Складская логистика")]
+		StorageLogistics
+	}
+}
