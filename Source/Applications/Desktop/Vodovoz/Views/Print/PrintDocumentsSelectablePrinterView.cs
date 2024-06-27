5a6fc0fd302dd2988faf528dfd9bba69718d949a
N-Sem <nikiforov.semen@gmail.com>
2024-06-27 16:32:54 +0300

    Новый диалог печати документов

---
+++ Source/Applications/Desktop/Vodovoz/Views/Print/PrintDocumentsSelectablePrinterView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Vodovoz.Views.Print
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PrintDocumentsSelectablePrinterView : Gtk.Bin
+	{
+		public PrintDocumentsSelectablePrinterView()
+		{
+			this.Build();
+		}
+
+		protected void OnZoomInActionActivated(object sender, EventArgs e)
+		{
+		}
+
+		protected void OnZoomOutActionActivated(object sender, EventArgs e)
+		{
+		}
+
+		protected void OnPrintActionActivated(object sender, EventArgs e)
+		{
+		}
+
+		protected void OnRefreshActionActivated(object sender, EventArgs e)
+		{
+		}
+
+		protected void OnPdfActionActivated(object sender, EventArgs e)
+		{
+		}
+	}
+}
