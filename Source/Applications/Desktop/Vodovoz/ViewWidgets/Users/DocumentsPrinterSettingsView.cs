1b0eac8a5e9ca0af511b75ddfa9460908bee1b2e
N-Sem <nikiforov.semen@gmail.com>
2024-06-27 17:54:23 +0300

    Добавлен виджет добавления настроек принтера

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Users/DocumentsPrinterSettingsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Users
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DocumentsPrinterSettingsView : Gtk.Bin
+	{
+		public DocumentsPrinterSettingsView()
+		{
+			this.Build();
+		}
+	}
+}
