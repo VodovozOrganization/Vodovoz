6c037462c12824ab3b37ff5d0f1cc3c1200e9128
Sergey Efimov <efimov90@gmail.com>
2024-06-28 15:53:17 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Counterparties/CallTaskMassEditView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Counterparties
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CallTaskMassEditView : Gtk.Bin
+	{
+		public CallTaskMassEditView()
+		{
+			this.Build();
+		}
+	}
+}
