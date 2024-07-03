cabd281be4245a160a8ae55d80f251ba4dcf85ad
N-Sem <nikiforov.semen@gmail.com>
2024-07-03 23:51:05 +0300

    Добавлен новый тип и интерфейс

---
+++ Source/Libraries/Core/Business/VodovozBusiness/PrintableDocuments/CustomPrinterPrintDocumentType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.PrintableDocuments
+{
+	public enum CustomPrinterPrintDocumentType
+	{
+		[Display(Name = "Талон погрузки (склад воды)")]
+		WaterCarLoadDocument,
+		[Display(Name = "Талон погрузки (склад оборудования)")]
+		EquipmentCarLoadDocument
+	}
+}
---
+++ Source/Libraries/Core/Business/VodovozBusiness/PrintableDocuments/ICustomPrinterPrintDocument.cs
@@ -0,0 +1,9 @@
+﻿using QS.Report;
+
+namespace Vodovoz.PrintableDocuments
+{
+	public interface ICustomPrinterPrintDocument : IPrintableRDLDocument
+	{
+		CustomPrinterPrintDocumentType DocumentType { get; }
+	}
+}
