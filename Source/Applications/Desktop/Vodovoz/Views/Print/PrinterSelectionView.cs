b7b916f1ee24cfb5e2206ba9988c1626b3cdfcd4
N-Sem <nikiforov.semen@gmail.com>
2024-07-05 14:16:12 +0300

    Добавлен диалог выбора принтера

---
+++ Source/Applications/Desktop/Vodovoz/Views/Print/PrinterSelectionView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Print
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PrinterSelectionView : Gtk.Bin
+	{
+		public PrinterSelectionView()
+		{
+			this.Build();
+		}
+	}
+}
