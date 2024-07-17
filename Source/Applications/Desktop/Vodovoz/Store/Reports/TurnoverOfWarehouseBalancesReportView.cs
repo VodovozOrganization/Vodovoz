4f654ed4ab1f471d335c946212461df5ab46eaca
Sergey Efimov <efimov90@gmail.com>
2024-07-17 08:20:14 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Store/Reports/TurnoverOfWarehouseBalancesReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Store.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class TurnoverOfWarehouseBalancesReportView : Gtk.Bin
+	{
+		public TurnoverOfWarehouseBalancesReportView()
+		{
+			this.Build();
+		}
+	}
+}
