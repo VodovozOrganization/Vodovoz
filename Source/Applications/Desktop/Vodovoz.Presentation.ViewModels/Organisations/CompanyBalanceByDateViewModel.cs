c2aa47c7a0f763e5b78c92a4a98a50f525695ec7
Andrey <as1854@yandex.ru>
2024-07-21 13:15:47 +0300

    4956 добавил вью/вью модель для баланса компании на день

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Organisations/CompanyBalanceByDateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace Vodovoz.Presentation.ViewModels.Organisations
+{
+	public class CompanyBalanceByDateViewModel : UowDialogViewModelBase
+	{
+		public CompanyBalanceByDateViewModel(
+			IUnitOfWorkFactory unitOfWorkFactory,
+			INavigationManager navigation) : base(unitOfWorkFactory, navigation)
+		{
+
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/Views/Organization/CompanyBalanceByDateView.cs
@@ -0,0 +1,19 @@
+﻿using QS.Views.Dialog;
+using Vodovoz.Presentation.ViewModels.Organisations;
+
+namespace Vodovoz.Views.Organization
+{
+	public partial class CompanyBalanceByDateView : DialogViewBase<CompanyBalanceByDateViewModel>
+	{
+		public CompanyBalanceByDateView(CompanyBalanceByDateViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+
+		}
+	}
+}
