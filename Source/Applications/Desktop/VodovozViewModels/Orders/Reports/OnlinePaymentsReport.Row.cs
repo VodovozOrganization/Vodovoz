047e729599f76104fe6ab7f4904804810d33bc78
Sergey Efimov <efimov90@gmail.com>
2024-07-28 15:12:05 +0300

    Temp

---
+++ Source/Applications/Desktop/VodovozViewModels/Orders/Reports/OnlinePaymentsReport.Row.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Orders;
+
+namespace Vodovoz.ViewModels.Orders.Reports
+{
+	public partial class OnlinePaymentsReport
+	{
+		public class Row
+		{
+			public DateTime? DeliveryDate { get; internal set; }
+			public int OrderId { get; internal set; }
+			public string CcounterpartyFullName { get; internal set; }
+			public string Address { get; internal set; }
+			public int? OnlineOrderId { get; internal set; }
+			public int TotalSumFromBank { get; internal set; }
+			public decimal OrderTotalSum { get; internal set; }
+			public OrderStatus OrderStatus { get; internal set; }
+			public string Author { get; internal set; }
+			public string PaymentDateTimeOrError { get; internal set; }
+			public PaymentType OrderPaymentType { get; internal set; }
+			public bool IsFutureOrder { get; internal set; }
+			public string NumberAndShop { get; internal set; }
+		}
+	}
+}
--- Source/Applications/Desktop/VodovozViewModels/Orders/Reports/OnlinePaymentsReport.cs
+++ Source/Applications/Desktop/VodovozViewModels/Orders/Reports/OnlinePaymentsReport.cs
@@ -1,6 +1,7 @@
 ﻿using NHibernate.Linq;
 using QS.DomainModel.UoW;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,8 +13,10 @@ using Vodovoz.Presentation.ViewModels.Errors;
 
 namespace Vodovoz.ViewModels.Orders.Reports
 {
-	public class OnlinePaymentsReport
+	public partial class OnlinePaymentsReport
 	{
+		private static readonly int[] _avangardPayments = new int[] { 10, 11, 12, 13 };
+
 		private OnlinePaymentsReport(
 			DateTime startDate,
 			DateTime endDate,
@@ -37,69 +40,56 @@ namespace Vodovoz.ViewModels.Orders.Reports
 		{
 			var startTime = DateTime.Now;
 
-			var avangardPayments = new int[] { 10, 11, 12, 13 };
+			if(cancellationToken.IsCancellationRequested)
+			{
+				return await Task.FromResult(Result.Failure<OnlinePaymentsReport>(Report.CreateAborted));
+			}
+
+			IQueryable<Row> ordersQuery = GetOrdersQuery(startDate, endDate, unitOfWork);
+
+			var orders = await ordersQuery.ToListAsync();
+
+			var onlineOrdersIds = orders.Select(x => x.OnlineOrderId);
 
 			if(cancellationToken.IsCancellationRequested)
 			{
 				return await Task.FromResult(Result.Failure<OnlinePaymentsReport>(Report.CreateAborted));
 			}
 
-			var orders = await (from order in unitOfWork.Session.Query<Order>()
-						  join counterparty in unitOfWork.Session.Query<Counterparty>()
-						  on order.Client.Id equals counterparty.Id
-						  join deliveryPoint in unitOfWork.Session.Query<DeliveryPoint>()
-						  on order.DeliveryPoint.Id equals deliveryPoint.Id
-						  where order.PaymentType == PaymentType.PaidOnline
-							  && order.OnlineOrder != null
-							  && !avangardPayments.Contains(order.PaymentByCardFrom.Id)
-							  && order.DeliveryDate >= startDate
-							  && order.DeliveryDate <= endDate
-						  let address = order.SelfDelivery ? "Самовывоз" : deliveryPoint.ShortAddress
-						  let orderTotalSum = (from orderItem in unitOfWork.Session.Query<OrderItem>()
-											   where orderItem.Order.Id == order.Id
-											   select orderItem.ActualSum).Sum(x => x as decimal? == null ? 0m : x)
-						  select new
-						  {
-							  order.DeliveryDate,
-							  order.Id,
-							  counterparty.FullName,
-							  Address = address,
-							  order.OnlineOrder,
-							  TotalSumFromBank = 0,
-							  OrderTotalSum = orderTotalSum,
-							  order.OrderStatus,
-							  Author = order.Author.ShortName,
-							  PaymentDateTimeOrError = "Оплата не найдена",
-							  order.PaymentType,
-							  IsFutureOrder = order.DeliveryDate > endDate,
-							  NumberAndShop = ""
-						  }).ToListAsync();
+			IQueryable<PaymentByCardOnline> onlinePaymentsQuery = GetOnlinePaymentsQuery(unitOfWork, onlineOrdersIds);
 
-			var onlineOrdersIds = orders.Select(x => x.OnlineOrder);
+			var onlinePayments = await onlinePaymentsQuery.ToListAsync();
+
+			var ordersIds = orders.Select(x => x.OrderId);
 
 			if(cancellationToken.IsCancellationRequested)
 			{
 				return await Task.FromResult(Result.Failure<OnlinePaymentsReport>(Report.CreateAborted));
 			}
 
-			var payments = await (from payment in unitOfWork.Session.Query<PaymentByCardOnline>()
-							where payment.PaymentByCardFrom != PaymentByCardOnlineFrom.FromSMS
-								 && onlineOrdersIds.Contains(payment.PaymentNr)
-							select payment)
-							.ToListAsync();
+			IQueryable<PaymentByCardOnline> smsPymentsQuery = GetSmsPaymentsQuery(unitOfWork, ordersIds);
 
-			var ordersIds = orders.Select(x => x.Id);
+			var smsPayments = await smsPymentsQuery.ToListAsync();
 
-			if(cancellationToken.IsCancellationRequested)
+			foreach(var payment in onlinePayments)
 			{
-				return await Task.FromResult(Result.Failure<OnlinePaymentsReport>(Report.CreateAborted));
+				var orderRowToUpdate = orders.Where(x => x.OnlineOrderId == payment.PaymentNr).FirstOrDefault();
+
+				if(orderRowToUpdate != null)
+				{
+					
+				}
 			}
 
-			var paymentsBySms = (from payment in unitOfWork.Session.Query<PaymentByCardOnline>()
-								 where payment.PaymentByCardFrom == PaymentByCardOnlineFrom.FromSMS
-									  && ordersIds.Contains(payment.PaymentNr)
-								 select payment)
-								.ToListAsync();
+			foreach(var payment in smsPayments)
+			{
+				var orderRowToUpdate = orders.Where(x => x.OrderId == payment.PaymentNr).FirstOrDefault();
+
+				if(orderRowToUpdate != null)
+				{
+
+				}
+			}
 
 			var generatedInMilliseconds = (DateTime.Now - startTime).TotalMilliseconds;
 
@@ -110,5 +100,49 @@ namespace Vodovoz.ViewModels.Orders.Reports
 						endDate,
 						selectedShop)));
 		}
+
+		private static IQueryable<PaymentByCardOnline> GetSmsPaymentsQuery(IUnitOfWork unitOfWork, IEnumerable<int> ordersIds) =>
+			from payment in unitOfWork.Session.Query<PaymentByCardOnline>()
+			where payment.PaymentByCardFrom == PaymentByCardOnlineFrom.FromSMS
+				 && ordersIds.Contains(payment.PaymentNr)
+			select payment;
+
+		private static IQueryable<PaymentByCardOnline> GetOnlinePaymentsQuery(IUnitOfWork unitOfWork, IEnumerable<int?> onlineOrdersIds) =>
+			from payment in unitOfWork.Session.Query<PaymentByCardOnline>()
+			where payment.PaymentByCardFrom != PaymentByCardOnlineFrom.FromSMS
+				 && onlineOrdersIds.Contains(payment.PaymentNr)
+			select payment;
+
+		private static IQueryable<Row> GetOrdersQuery(DateTime startDate, DateTime endDate, IUnitOfWork unitOfWork) =>
+			from order in unitOfWork.Session.Query<Order>()
+			join counterparty in unitOfWork.Session.Query<Counterparty>()
+			on order.Client.Id equals counterparty.Id
+			join deliveryPoint in unitOfWork.Session.Query<DeliveryPoint>()
+			on order.DeliveryPoint.Id equals deliveryPoint.Id
+			where order.PaymentType == PaymentType.PaidOnline
+				&& order.OnlineOrder != null
+				&& !_avangardPayments.Contains(order.PaymentByCardFrom.Id)
+				&& order.DeliveryDate >= startDate
+				&& order.DeliveryDate <= endDate
+			let address = order.SelfDelivery ? "Самовывоз" : deliveryPoint.ShortAddress
+			let orderTotalSum = (from orderItem in unitOfWork.Session.Query<OrderItem>()
+								 where orderItem.Order.Id == order.Id
+								 select orderItem.ActualSum).Sum(x => x as decimal? == null ? 0m : x)
+			select new Row
+			{
+				DeliveryDate = order.DeliveryDate,
+				OrderId = order.Id,
+				CcounterpartyFullName = counterparty.FullName,
+				Address = address,
+				OnlineOrderId = order.OnlineOrder,
+				TotalSumFromBank = 0,
+				OrderTotalSum = orderTotalSum,
+				OrderStatus = order.OrderStatus,
+				Author = order.Author.ShortName,
+				PaymentDateTimeOrError = "Оплата не найдена",
+				OrderPaymentType = order.PaymentType,
+				IsFutureOrder = order.DeliveryDate > endDate,
+				NumberAndShop = ""
+			};
 	}
 }
