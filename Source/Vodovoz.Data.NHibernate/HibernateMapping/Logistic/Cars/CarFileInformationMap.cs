c3920edd3c92d1f8c2a34689388bf0b1c582c725
Sergey Efimov <efimov90@gmail.com>
2024-08-15 16:39:29 +0300

    Временно отключил маппинги

--- Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/Cars/CarFileInformationMap.cs
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/Cars/CarFileInformationMap.cs
@@ -3,16 +3,17 @@ using VodovozBusiness.Domain.Logistic.Cars;
 
 namespace Vodovoz.Data.NHibernate.HibernateMapping.Logistic.Cars
 {
-	public class CarFileInformationMap : ClassMap<CarFileInformation>
-	{
-		public CarFileInformationMap()
-		{
-			Table("car_file_informations");
+	// TODO: Отключено до реализации 4963, мешает сборке
+	//public class CarFileInformationMap : ClassMap<CarFileInformation>
+	//{
+	//	public CarFileInformationMap()
+	//	{
+	//		Table("car_file_informations");
 
-			Id(x => x.Id).Column("id").GeneratedBy.Native();
+	//		Id(x => x.Id).Column("id").GeneratedBy.Native();
 
-			Map(x => x.CarId).Column("car_id");
-			Map(x => x.FileName).Column("file_name");
-		}
-	}
+	//		Map(x => x.CarId).Column("car_id");
+	//		Map(x => x.FileName).Column("file_name");
+	//	}
+	//}
 }
