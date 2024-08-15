636727d53958654635e22a98bab764fb66b2c259
Sergey Efimov <efimov90@gmail.com>
2024-08-16 02:50:39 +0300

    Возврат отключенных файлов

--- Source/Libraries/Infrastructure/Vodovoz.Infrastructure.FileStorage/CarFileStorageService.cs
+++ Source/Libraries/Infrastructure/Vodovoz.Infrastructure.FileStorage/CarFileStorageService.cs
@@ -8,78 +8,77 @@ using Vodovoz.Errors;
 
 namespace Vodovoz.Infrastructure.FileStorage
 {
-	// TODO: Отключено до реализации 4963, мешает сборке
-	//internal sealed class CarFileStorageService : FileStorageByS3Base, IFileStorageService, ICarFileStorageService
-	//{
-	//	public CarFileStorageService(IS3FileStorageService s3FileStorageService) : base(s3FileStorageService)
-	//	{
-	//	}
+	internal sealed class CarFileStorageService : FileStorageByS3Base, IFileStorageService, ICarFileStorageService
+	{
+		public CarFileStorageService(IS3FileStorageService s3FileStorageService) : base(s3FileStorageService)
+		{
+		}
 
-	//	protected override string BucketName => "vodovoz-car-attachments";
+		protected override string BucketName => "vodovoz-car-attachments";
 
-	//	public Task<Result> CreatePhotoAsync(Car entity, string filename, Stream inputStream, CancellationToken cancellationToken)
-	//	{
-	//		if(entity.AttachedFileInformations.Any(ati => ati.FileName == filename))
-	//		{
-	//			return Task.FromResult(Result.Failure(Application.Errors.FileStorage.PhotoMatchesAttachedFileFileName));
-	//		}
+		public Task<Result> CreatePhotoAsync(Car entity, string filename, Stream inputStream, CancellationToken cancellationToken)
+		{
+			if(entity.AttachedFileInformations.Any(ati => ati.FileName == filename))
+			{
+				return Task.FromResult(Result.Failure(Application.Errors.FileStorage.PhotoMatchesAttachedFileFileName));
+			}
 
-	//		return CreateFileAsync($"{entity.Id}/{filename}", inputStream, cancellationToken);
-	//	}
+			return CreateFileAsync($"{entity.Id}/{filename}", inputStream, cancellationToken);
+		}
 
-	//	public Task<Result<Stream>> GetPhotoAsync(Car entity, CancellationToken cancellationToken)
-	//		=> GetFileAsync($"{entity.Id}/{entity.PhotoFileName}", cancellationToken);
+		public Task<Result<Stream>> GetPhotoAsync(Car entity, CancellationToken cancellationToken)
+			=> GetFileAsync($"{entity.Id}/{entity.PhotoFileName}", cancellationToken);
 
-	//	public Task<Result> UpdatePhotoAsync(Car entity, string filename, Stream inputStream, CancellationToken cancellationToken)
-	//	{
-	//		if(entity.AttachedFileInformations.Any(ati => ati.FileName == filename))
-	//		{
-	//			return Task.FromResult(Result.Failure(Application.Errors.FileStorage.PhotoMatchesAttachedFileFileName));
-	//		}
+		public Task<Result> UpdatePhotoAsync(Car entity, string filename, Stream inputStream, CancellationToken cancellationToken)
+		{
+			if(entity.AttachedFileInformations.Any(ati => ati.FileName == filename))
+			{
+				return Task.FromResult(Result.Failure(Application.Errors.FileStorage.PhotoMatchesAttachedFileFileName));
+			}
 
-	//		if(string.IsNullOrWhiteSpace(entity.PhotoFileName))
-	//		{
-	//			return CreatePhotoAsync(entity, filename, inputStream, cancellationToken);
-	//		}
+			if(string.IsNullOrWhiteSpace(entity.PhotoFileName))
+			{
+				return CreatePhotoAsync(entity, filename, inputStream, cancellationToken);
+			}
 
-	//		if(entity.PhotoFileName == filename)
-	//		{
-	//			return UpdateFileAsync($"{entity.Id}/{filename}", inputStream, cancellationToken);
-	//		}
+			if(entity.PhotoFileName == filename)
+			{
+				return UpdateFileAsync($"{entity.Id}/{filename}", inputStream, cancellationToken);
+			}
 
-	//		DeletePhotoAsync(entity, cancellationToken);
+			DeletePhotoAsync(entity, cancellationToken);
 
-	//		return CreatePhotoAsync(entity, filename, inputStream, cancellationToken);
-	//	}
+			return CreatePhotoAsync(entity, filename, inputStream, cancellationToken);
+		}
 
-	//	public Task<Result> DeletePhotoAsync(Car entity, CancellationToken cancellationToken)
-	//		=> DeleteFileAsync($"{entity.Id}/{entity.PhotoFileName}", cancellationToken);
+		public Task<Result> DeletePhotoAsync(Car entity, CancellationToken cancellationToken)
+			=> DeleteFileAsync($"{entity.Id}/{entity.PhotoFileName}", cancellationToken);
 
-	//	public Task<Result> CreateFileAsync(Car entity, string fileName, Stream inputStream, CancellationToken cancellationToken)
-	//	{
-	//		if(entity.PhotoFileName == fileName)
-	//		{
-	//			return Task.FromResult(Result.Failure(Application.Errors.FileStorage.AttachedFileMatchesPhotoFileName));
-	//		}
+		public Task<Result> CreateFileAsync(Car entity, string fileName, Stream inputStream, CancellationToken cancellationToken)
+		{
+			if(entity.PhotoFileName == fileName)
+			{
+				return Task.FromResult(Result.Failure(Application.Errors.FileStorage.AttachedFileMatchesPhotoFileName));
+			}
 
-	//		return CreateFileAsync($"{entity.Id}/{fileName}", inputStream, cancellationToken);
-	//	}
+			return CreateFileAsync($"{entity.Id}/{fileName}", inputStream, cancellationToken);
+		}
 
-	//	public Task<Result<Stream>> GetFileAsync(Car entity, string fileName, CancellationToken cancellationToken)
-	//		=> GetFileAsync($"{entity.Id}/{fileName}", cancellationToken);
+		public Task<Result<Stream>> GetFileAsync(Car entity, string fileName, CancellationToken cancellationToken)
+			=> GetFileAsync($"{entity.Id}/{fileName}", cancellationToken);
 
-	//	public Task<Result> UpdateFileAsync(Car entity, string fileName, Stream inputStream, CancellationToken cancellationToken)
-	//	{
-	//		if(entity.PhotoFileName == fileName)
-	//		{
-	//			return Task.FromResult(Result.Failure(Application.Errors.FileStorage.AttachedFileMatchesPhotoFileName));
-	//		}
+		public Task<Result> UpdateFileAsync(Car entity, string fileName, Stream inputStream, CancellationToken cancellationToken)
+		{
+			if(entity.PhotoFileName == fileName)
+			{
+				return Task.FromResult(Result.Failure(Application.Errors.FileStorage.AttachedFileMatchesPhotoFileName));
+			}
 
-	//		return UpdateFileAsync($"{entity.Id}/{fileName}", inputStream, cancellationToken);
-	//	}
+			return UpdateFileAsync($"{entity.Id}/{fileName}", inputStream, cancellationToken);
+		}
 
-	//	public Task<Result> DeleteFileAsync(Car entity, string fileName, CancellationToken cancellationToken)
-	//		=> DeleteFileAsync($"{entity.Id}/{fileName}", cancellationToken);
+		public Task<Result> DeleteFileAsync(Car entity, string fileName, CancellationToken cancellationToken)
+			=> DeleteFileAsync($"{entity.Id}/{fileName}", cancellationToken);
 
-	//}
+	}
 }
--- Source/Libraries/Infrastructure/Vodovoz.Infrastructure.FileStorage/ComplaintDiscussionCommentFileStorageService.cs
+++ Source/Libraries/Infrastructure/Vodovoz.Infrastructure.FileStorage/ComplaintDiscussionCommentFileStorageService.cs
@@ -4,16 +4,15 @@ using VodovozBusiness.Domain.Complaints;
 
 namespace Vodovoz.Infrastructure.FileStorage
 {
-	// TODO: Отключено до реализации 4963, мешает сборке
-	//internal sealed class ComplaintDiscussionCommentFileStorageService
-	//	: AttachedFilesOnlyFileStorageByS3Base<ComplaintDiscussionComment, ComplaintDiscussionCommentFileInformation>,
-	//	IComplaintDiscussionCommentFileStorageService
-	//{
-	//	public ComplaintDiscussionCommentFileStorageService(IS3FileStorageService s3FileStorageService)
-	//		: base(s3FileStorageService)
-	//	{
-	//	}
+	internal sealed class ComplaintDiscussionCommentFileStorageService
+		: AttachedFilesOnlyFileStorageByS3Base<ComplaintDiscussionComment, ComplaintDiscussionCommentFileInformation>,
+		IComplaintDiscussionCommentFileStorageService
+	{
+		public ComplaintDiscussionCommentFileStorageService(IS3FileStorageService s3FileStorageService)
+			: base(s3FileStorageService)
+		{
+		}
 
-	//	protected override string BucketName => "vodovoz-complaint-discussion-comment-attachments";
-	//}
+		protected override string BucketName => "vodovoz-complaint-discussion-comment-attachments";
+	}
 }
