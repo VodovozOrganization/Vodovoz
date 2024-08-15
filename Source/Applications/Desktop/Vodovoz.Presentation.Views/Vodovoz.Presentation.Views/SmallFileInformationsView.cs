b1070bb1de31daee2c6db179d45636cbc7c97d88
Sergey Efimov <efimov90@gmail.com>
2024-08-15 15:26:19 +0300

    SmallFileInformationsView UI

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.Views/Vodovoz.Presentation.Views/SmallFileInformationsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Presentation.Views
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class SmallFileInformationsView : Gtk.Bin
+	{
+		public SmallFileInformationsView()
+		{
+			this.Build();
+		}
+	}
+}
