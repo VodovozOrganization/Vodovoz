9594c6374ded7da1ab75d5068654b74dc35d3c00
Sergey Efimov <efimov90@gmail.com>
2024-08-16 16:58:22 +0300

    Fix

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Employees/EmployeeFileInformation.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Employees/EmployeeFileInformation.cs
@@ -12,7 +12,6 @@ namespace VodovozBusiness.Domain.Employees
 		private int _employeeId;
 
 		[Display(Name = "Идентификатор сотрудника")]
-
 		public virtual int EmployeeId
 		{
 			get => _employeeId;
