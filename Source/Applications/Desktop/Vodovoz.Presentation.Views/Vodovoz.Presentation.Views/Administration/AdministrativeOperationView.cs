87ed8557f47927cd7be25a1bc4d4a0cdc9c90da0
Sergey Efimov <efimov90@gmail.com>
2024-09-02 05:32:17 +0300

    Административный виджет

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.Views/Vodovoz.Presentation.Views/Administration/AdministrativeOperationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Presentation.Views.Administration
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AdministrativeOperationView : Gtk.Bin
+	{
+		public AdministrativeOperationView()
+		{
+			this.Build();
+		}
+	}
+}
