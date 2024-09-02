320868253e75abed8a376abb6f1679cd32855229
Art8m <9artbe@gmail.com>
2024-09-02 05:59:06 -0700

    stetic

---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/NomenclatureMinimumBalanceByWarehouseEditView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class NomenclatureMinimumBalanceByWarehouseEditView : Gtk.Bin
+	{
+		public NomenclatureMinimumBalanceByWarehouseEditView()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/NomenclatureMinimumBalanceByWarehouseView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class NomenclatureMinimumBalanceByWarehouseView : Gtk.Bin
+	{
+		public NomenclatureMinimumBalanceByWarehouseView()
+		{
+			this.Build();
+		}
+	}
+}
