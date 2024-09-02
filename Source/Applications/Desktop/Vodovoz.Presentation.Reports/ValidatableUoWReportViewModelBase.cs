674b33c59c26c4a4aad4916df9339b81fb69fc21
Vadim Gordeev <enzogord@gmail.com>
2024-09-02 18:31:43 +0300

    Добавлена базовая ViewModel для отчетов с UoW

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.Reports/ValidatableUoWReportViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using QS.DomainModel.UoW;
+using QS.Report;
+using QS.Report.ViewModels;
+using QS.Validation;
+using System;
+
+namespace Vodovoz.Presentation.Reports
+{
+	public abstract class ValidatableUoWReportViewModelBase : ValidatableReportViewModelBase, IDisposable
+	{
+		public ValidatableUoWReportViewModelBase(
+			RdlViewerViewModel rdlViewerViewModel,
+			IReportInfoFactory reportInfoFactory,
+			IValidator validator
+			) : base(rdlViewerViewModel, reportInfoFactory, validator)
+		{
+		}
+
+		protected virtual IUnitOfWork UoW { get; set; }
+
+		public void Dispose()
+		{
+			UoW?.Dispose();
+		}
+	}
+}
