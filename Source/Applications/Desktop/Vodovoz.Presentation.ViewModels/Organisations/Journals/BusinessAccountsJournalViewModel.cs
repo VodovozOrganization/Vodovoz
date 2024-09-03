8f5f5b747242fa7a9ee5542643a59606ad7c7bbd
Andrey <as1854@yandex.ru>
2024-09-03 16:04:26 +0300

    добавлена логика фильтрования в журналы

--- Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Organisations/Journals/BusinessAccountsJournalViewModel.cs
+++ Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Organisations/Journals/BusinessAccountsJournalViewModel.cs
@@ -1,4 +1,5 @@
-﻿using NHibernate;
+﻿using System;
+using NHibernate;
 using NHibernate.Transform;
 using QS.Dialog;
 using QS.DomainModel.UoW;
@@ -12,14 +13,19 @@ namespace Vodovoz.Presentation.ViewModels.Organisations.Journals
 	public class BusinessAccountsJournalViewModel :
 		EntityJournalViewModelBase<BusinessAccount, BusinessAccountViewModel, BusinessAccountJournalNode>
 	{
+		private readonly BusinessAccountsFilterViewModel _filterViewModel;
+
 		public BusinessAccountsJournalViewModel(
+			BusinessAccountsFilterViewModel filterViewModel,
 			IUnitOfWorkFactory unitOfWorkFactory,
 			IInteractiveService interactiveService,
 			INavigationManager navigationManager,
 			ICurrentPermissionService currentPermissionService)
 			: base(unitOfWorkFactory, interactiveService, navigationManager, currentPermissionService: currentPermissionService)
 		{
-			
+			_filterViewModel = filterViewModel ?? throw new ArgumentNullException(nameof(filterViewModel));
+			JournalFilter = _filterViewModel;
+			_filterViewModel.OnFiltered += OnFilterFiltered;
 		}
 
 		protected override IQueryOver<BusinessAccount> ItemsQuery(IUnitOfWork uow)
@@ -30,20 +36,31 @@ namespace Vodovoz.Presentation.ViewModels.Organisations.Journals
 
 			var query = uow.Session.QueryOver<BusinessAccount>()
 				.JoinAlias(ba => ba.BusinessActivity, () => businessActivityAlias)
-				.JoinAlias(ba => ba.Funds, () => fundsAlias)
-				.SelectList(list => list
-					.Select(ba => ba.Id).WithAlias(() => resultAlias.Id)
-					.Select(ba => ba.Name).WithAlias(() => resultAlias.Name)
-					.Select(ba => ba.Bank).WithAlias(() => resultAlias.Bank)
-					.Select(ba => ba.Number).WithAlias(() => resultAlias.Number)
-					.Select(ba => ba.IsArchive).WithAlias(() => resultAlias.IsArchive)
-					.Select(ba => ba.AccountFillType).WithAlias(() => resultAlias.AccountFillType)
-					.Select(() => businessActivityAlias.Name).WithAlias(() => resultAlias.BusinessActivity)
-					.Select(() => fundsAlias.Name).WithAlias(() => resultAlias.Funds)
-				)
-				.TransformUsing(Transformers.AliasToBean<BusinessAccountJournalNode>());
+				.JoinAlias(ba => ba.Funds, () => fundsAlias);
+
+			if(_filterViewModel != null && !_filterViewModel.ShowArchived)
+			{
+				query.Where(ba => !ba.IsArchive);
+			}
+
+			query.SelectList(list => list
+				.Select(ba => ba.Id).WithAlias(() => resultAlias.Id)
+				.Select(ba => ba.Name).WithAlias(() => resultAlias.Name)
+				.Select(ba => ba.Bank).WithAlias(() => resultAlias.Bank)
+				.Select(ba => ba.Number).WithAlias(() => resultAlias.Number)
+				.Select(ba => ba.IsArchive).WithAlias(() => resultAlias.IsArchive)
+				.Select(ba => ba.AccountFillType).WithAlias(() => resultAlias.AccountFillType)
+				.Select(() => businessActivityAlias.Name).WithAlias(() => resultAlias.BusinessActivity)
+				.Select(() => fundsAlias.Name).WithAlias(() => resultAlias.Funds)
+			)
+			.TransformUsing(Transformers.AliasToBean<BusinessAccountJournalNode>());
 
 			return query;
+		}
+
+		private void OnFilterFiltered(object sender, EventArgs e)
+		{
+			Refresh();
 		}
 	}
 }
