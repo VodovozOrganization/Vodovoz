11ffe910a81980102777162d6cc2e9650ae87b10
N-Sem <nikiforov.semen@gmail.com>
2024-09-04 12:51:30 +0300

    Логика и привязки фильтра

--- Source/Applications/Desktop/Vodovoz/Filters/GtkViews/WarehouseDocumentsJournalFilterView.cs
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/WarehouseDocumentsJournalFilterView.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using QS.Views.GtkUI;
+using System.ComponentModel;
+using Vodovoz.Domain.Documents;
+using Vodovoz.Domain.Documents.MovementDocuments;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Store;
+
 namespace Vodovoz.Filters.GtkViews
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class WarehouseDocumentsJournalFilterView : Gtk.Bin
+	[ToolboxItem(true)]
+	public partial class WarehouseDocumentsJournalFilterView : FilterViewBase<WarehouseDocumentsJournalFilterViewModel>
 	{
-		public WarehouseDocumentsJournalFilterView()
+		public WarehouseDocumentsJournalFilterView(WarehouseDocumentsJournalFilterViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			yenumcomboboxDocumentType.ItemsEnum = typeof(DocumentType);
+			yenumcomboboxDocumentType.HiddenItems = new[] { DocumentType.DeliveryDocument as object };
+			yenumcomboboxDocumentType.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.Warehouse, w => w.SelectedItemOrNull)
+				.AddBinding(vm => vm.CanChangeRestrictedDocumentType, w => w.Sensitive)
+				.InitializeFromSource();
+
+			yenumcomboboxMovementStatus.ItemsEnum = typeof(MovementDocumentStatus);
+			yenumcomboboxMovementStatus.Binding
+				.AddBinding(ViewModel, vm => vm.MovementDocumentStatus, w => w.SelectedItemOrNull)
+				.InitializeFromSource();
+
+			daterangepickerPeriod.Binding
+				.AddSource(ViewModel)
+				.AddBinding(vm => vm.StartDate, w => w.StartDateOrNull)
+				.AddBinding(vm => vm.EndDate, w => w.EndDateOrNull)
+				.InitializeFromSource();
+
+			entityentryWarehouse.ViewModel = ViewModel.WarehouseEntityEntryViewModel;
+
+			entityentryDriver.ViewModel = ViewModel.DriverEntityEntryViewModel;
 		}
 	}
 }
--- Source/Applications/Desktop/VodovozViewModels/Journals/FilterViewModels/Store/WarehouseDocumentsJournalFilterViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Journals/FilterViewModels/Store/WarehouseDocumentsJournalFilterViewModel.cs
@@ -1,8 +1,165 @@
-﻿using QS.Project.Filter;
+﻿using QS.DomainModel.UoW;
+using QS.Project.Filter;
+using QS.Services;
+using QS.ViewModels.Control.EEVM;
+using System;
+using System.Linq;
+using Vodovoz.Core.Domain.Common;
+using Vodovoz.Domain.Documents;
+using Vodovoz.Domain.Documents.MovementDocuments;
+using Vodovoz.Domain.Employees;
+using Vodovoz.Domain.Store;
+using Vodovoz.Services;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Employees;
+using Vodovoz.ViewModels.Journals.JournalViewModels.Employees;
+using Vodovoz.ViewModels.Journals.JournalViewModels.Store;
+using Vodovoz.ViewModels.ViewModels.Employees;
+using Vodovoz.ViewModels.Warehouses;
 
 namespace Vodovoz.ViewModels.Journals.FilterViewModels.Store
 {
 	public class WarehouseDocumentsJournalFilterViewModel : FilterViewModelBase<WarehouseDocumentsJournalFilterViewModel>
 	{
+		private readonly ICommonServices _commonServices;
+		private readonly IUserSettingsService _userSettingsService;
+		private readonly IGenericRepository<Warehouse> _warehouseRepository;
+		private readonly ViewModelEEVMBuilder<Warehouse> _warehouseEEVMBuilder;
+		private readonly ViewModelEEVMBuilder<Employee> _driverEEVMBuilder;
+
+		private WarehouseDocumentsJournalViewModel _journal;
+
+		private DocumentType? _documentType;
+		private Warehouse _warehouse;
+		private MovementDocumentStatus? _movementDocumentStatus;
+		private DateTime? _startDate;
+		private DateTime? _endDate;
+		private Employee _driver;
+		private bool _canChangeRestrictedDocumentType = true;
+
+		public WarehouseDocumentsJournalFilterViewModel(
+			ICommonServices commonServices,
+			IUserSettingsService userSettingsService,
+			IGenericRepository<Warehouse> warehouseRepository,
+			ViewModelEEVMBuilder<Warehouse> warehouseEEVMBuilder,
+			ViewModelEEVMBuilder<Employee> driverEEVMBuilder)
+		{
+			_commonServices = commonServices ?? throw new ArgumentNullException(nameof(commonServices));
+			_userSettingsService = userSettingsService ?? throw new ArgumentNullException(nameof(userSettingsService));
+			_warehouseRepository = warehouseRepository ?? throw new ArgumentNullException(nameof(warehouseRepository));
+			_warehouseEEVMBuilder = warehouseEEVMBuilder ?? throw new ArgumentNullException(nameof(warehouseEEVMBuilder));
+			_driverEEVMBuilder = driverEEVMBuilder ?? throw new ArgumentNullException(nameof(driverEEVMBuilder));
+
+
+			StartDate = DateTime.Today.AddDays(-7);
+			EndDate = DateTime.Today.AddDays(1);
+		}
+
+		public bool CanSelectWarehouse =>
+			!_commonServices.CurrentPermissionService.ValidatePresetPermission("user_have_access_only_to_warehouse_and_complaints")
+			|| _commonServices.UserService.GetCurrentUser().IsAdmin;
+
+		public IEntityEntryViewModel WarehouseEntityEntryViewModel { get; private set; }
+		public IEntityEntryViewModel DriverEntityEntryViewModel { get; private set; }
+
+		public WarehouseDocumentsJournalViewModel Journal
+		{
+			get => _journal;
+			set
+			{
+				if(_journal != null)
+				{
+					return;
+				}
+
+				SetField(ref _journal, value);
+
+				var warehouseEntityEntryViewModel = _driverEEVMBuilder
+					.SetViewModel(value)
+					.SetUnitOfWork(value.UoW)
+					.ForProperty(this, x => x.Driver)
+					.UseViewModelJournalAndAutocompleter<EmployeesJournalViewModel, EmployeeFilterViewModel>(filter =>
+					{
+					})
+					.UseViewModelDialog<EmployeeViewModel>()
+					.Finish();
+
+				warehouseEntityEntryViewModel.CanViewEntity =
+					CanSelectWarehouse
+					&& _commonServices.CurrentPermissionService.ValidateEntityPermission(typeof(Warehouse)).CanUpdate;
+
+				WarehouseEntityEntryViewModel = warehouseEntityEntryViewModel;
+
+				SetDefaultWarehouse(value.UoW);
+
+				var driverEntityEntryViewModel = _warehouseEEVMBuilder
+					.SetViewModel(value)
+					.SetUnitOfWork(value.UoW)
+					.ForProperty(this, x => x.Warehouse)
+					.UseViewModelJournalAndAutocompleter<WarehouseJournalViewModel, WarehouseJournalFilterViewModel>(filter =>
+					{
+					})
+					.UseViewModelDialog<WarehouseViewModel>()
+					.Finish();
+
+				driverEntityEntryViewModel.CanViewEntity =
+					_commonServices.CurrentPermissionService.ValidateEntityPermission(typeof(Employee)).CanUpdate;
+
+				DriverEntityEntryViewModel = driverEntityEntryViewModel;
+			}
+		}
+
+		public DocumentType? DocumentType
+		{
+			get => _documentType;
+			set => UpdateFilterField(ref _documentType, value);
+		}
+
+		public Warehouse Warehouse
+		{
+			get => _warehouse;
+			set => UpdateFilterField(ref _warehouse, value);
+		}
+
+		public MovementDocumentStatus? MovementDocumentStatus
+		{
+			get => _movementDocumentStatus;
+			set => UpdateFilterField(ref _movementDocumentStatus, value);
+		}
+
+		public DateTime? StartDate
+		{
+			get => _startDate;
+			set => UpdateFilterField(ref _startDate, value);
+		}
+
+		public DateTime? EndDate
+		{
+			get => _endDate;
+			set => UpdateFilterField(ref _endDate, value);
+		}
+
+		public Employee Driver
+		{
+			get => _driver;
+			set => UpdateFilterField(ref _driver, value);
+		}
+
+		public bool CanChangeRestrictedDocumentType
+		{
+			get => _canChangeRestrictedDocumentType;
+			set => SetField(ref _canChangeRestrictedDocumentType, value);
+		}
+
+		private void SetDefaultWarehouse(IUnitOfWork uow)
+		{
+			var defaultWarehouse = _userSettingsService.Settings.DefaultWarehouse;
+
+			if(defaultWarehouse == null)
+			{
+				return;
+			}
+
+			Warehouse = _warehouseRepository.Get(uow, w => w.Id == defaultWarehouse.Id).FirstOrDefault();
+		}
 	}
 }
