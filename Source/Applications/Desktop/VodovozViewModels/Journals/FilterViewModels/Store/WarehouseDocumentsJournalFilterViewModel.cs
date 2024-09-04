b15b41a8d3a6441c61dc28646c1cbbd77e70900f
N-Sem <nikiforov.semen@gmail.com>
2024-09-04 09:59:29 +0300

    Добавлены основные классы для журнала

---
+++ Source/Applications/Desktop/VodovozViewModels/Journals/FilterViewModels/Store/WarehouseDocumentsJournalFilterViewModel.cs
@@ -0,0 +1,8 @@
+﻿using QS.Project.Filter;
+
+namespace Vodovoz.ViewModels.Journals.FilterViewModels.Store
+{
+	public class WarehouseDocumentsJournalFilterViewModel : FilterViewModelBase<WarehouseDocumentsJournalFilterViewModel>
+	{
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/Journals/JournalViewModels/Store/WarehouseDocumentsJournalViewModel.cs
@@ -0,0 +1,21 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Journal;
+using QS.Services;
+using Vodovoz.ViewModels.Journals.FilterViewModels.Store;
+using Vodovoz.ViewModels.Journals.JournalNodes.Store;
+
+namespace Vodovoz.ViewModels.Journals.JournalViewModels.Store
+{
+	public class WarehouseDocumentsJournalViewModel : FilterableMultipleEntityJournalViewModelBase<WarehouseDocumentsJournalNode, WarehouseDocumentsJournalFilterViewModel>
+	{
+		public WarehouseDocumentsJournalViewModel(
+			WarehouseDocumentsJournalFilterViewModel filterViewModel,
+			IUnitOfWorkFactory unitOfWorkFactory,
+			ICommonServices commonServices,
+			INavigationManager navigationManager = null)
+			: base(filterViewModel, unitOfWorkFactory, commonServices, navigationManager)
+		{
+		}
+	}
+}
