4a84191b37dadb89d96d1930a402b6ab624b396e
N-Sem <nikiforov.semen@gmail.com>
2024-09-04 09:10:33 +0300

    Добавлена вью для фильтра журнала документов склада

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/WarehouseDocumentsJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class WarehouseDocumentsJournalFilterView : Gtk.Bin
+	{
+		public WarehouseDocumentsJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
