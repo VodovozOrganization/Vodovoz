281fa5a6847d1aec24d3b743429d52258bcfe67f
Vadim Gordeev <enzogord@gmail.com>
2024-09-05 09:41:14 +0300

    Перевод в MVVM - CompanyTrucksReport

--- Source/Applications/Desktop/Vodovoz/MainWindow.Navigation.Reports.cs
+++ Source/Applications/Desktop/Vodovoz/MainWindow.Navigation.Reports.cs
@@ -934,9 +934,7 @@ public partial class MainWindow
 	/// <param name="e"></param>
 	protected void OnActionCompanyTrucksActivated(object sender, EventArgs e)
 	{
-		tdiMain.OpenTab(
-			QSReport.ReportViewDlg.GenerateHashName<CompanyTrucksReport>(),
-			() => new QSReport.ReportViewDlg(new CompanyTrucksReport()));
+		NavigationManager.OpenViewModel<RdlViewerViewModel, Type>(null, typeof(CompanyTrucksReportViewModel));
 	}
 
 	/// <summary>
