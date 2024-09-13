d29ed3c6c11d3f7285005f9fef88cfccd9894b96
Art8m <9artbe@gmail.com>
2024-09-13 07:11:37 -0700

    Фильтр стетик

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/ServiceDistrictsSetJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ServiceDistrictsSetJournalFilterView : Gtk.Bin
+	{
+		public ServiceDistrictsSetJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
