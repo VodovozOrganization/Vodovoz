ca7c0de982cbaae68a80b397889cd5d1fc9a9a70
Art8m <9artbe@gmail.com>
2024-09-15 01:01:58 -0700

    Виджет активации версии сервисных раонов

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/ServiceDistrictsSetActivationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ServiceDistrictsSetActivationView : Gtk.Bin
+	{
+		public ServiceDistrictsSetActivationView()
+		{
+			this.Build();
+		}
+	}
+}
