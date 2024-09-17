88eb8efab076913070646e398111b8fbc639ec65
N-Sem <nikiforov.semen@gmail.com>
2024-09-17 12:18:41 +0300

    Добавлена вью отчета

---
+++ Source/Applications/Desktop/Vodovoz/Views/Reports/OksDailyReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OksDailyReportView : Gtk.Bin
+	{
+		public OksDailyReportView()
+		{
+			this.Build();
+		}
+	}
+}
