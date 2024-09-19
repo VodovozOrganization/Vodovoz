4c11b399d339f70f2b6fe26a7414ea873c0e065e
Sergey Efimov <efimov90@gmail.com>
2024-09-19 15:25:10 +0300

    ViewModel

---
+++ Source/Applications/Desktop/VodovozViewModels/Accounting/Payments/PaymentWriteOffViewModel.cs
@@ -0,0 +1,101 @@
+﻿using DynamicData;
+using QS.Commands;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using QS.ViewModels.Control.EEVM;
+using System;
+using System.Windows.Input;
+using Vodovoz.Domain.Cash.FinancialCategoriesGroups;
+using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Organizations;
+using Vodovoz.ViewModels.Cash.FinancialCategoriesGroups;
+using VodovozBusiness.Domain.Payments;
+
+namespace Vodovoz.ViewModels.Accounting.Payments
+{
+	public class PaymentWriteOffViewModel : EntityTabViewModelBase<PaymentWriteOff>
+	{
+		private readonly IPermissionResult _permissionResult;
+		private Counterparty _counterparty;
+		private Organization _organization;
+
+		public PaymentWriteOffViewModel(
+			IEntityUoWBuilder uowBuilder,
+			IUnitOfWorkFactory unitOfWorkFactory,
+			ICommonServices commonServices,
+			INavigationManager navigation,
+			ICurrentPermissionService currentPermissionService,
+			ViewModelEEVMBuilder<FinancialExpenseCategory> financialExpenseCategoryViewModelEEVMBuilder)
+			: base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
+		{
+			if(currentPermissionService is null)
+			{
+				throw new ArgumentNullException(nameof(currentPermissionService));
+			}
+
+			_permissionResult = currentPermissionService.ValidateEntityPermission(typeof(PaymentWriteOff));
+
+			if(!_permissionResult.CanRead)
+			{
+				throw new AbortCreatingPageException("У вас нет доступа к этому документу", "Доступ запрещён");
+			}
+
+			if(financialExpenseCategoryViewModelEEVMBuilder is null)
+			{
+				throw new ArgumentNullException(nameof(financialExpenseCategoryViewModelEEVMBuilder));
+			}
+
+			FinancialExpenseCategoryViewModel = financialExpenseCategoryViewModelEEVMBuilder
+				.SetUnitOfWork(UoW)
+				.SetViewModel(this)
+				.UseViewModelJournalAndAutocompleter<FinancialCategoriesGroupsJournalViewModel, FinancialCategoriesJournalFilterViewModel>(filter =>
+				{
+					filter.RestrictFinancialSubtype = FinancialSubType.Expense;
+					filter.RestrictNodeSelectTypes.Add(typeof(FinancialExpenseCategory));
+
+					foreach(var includedId in new int[] { })
+					{
+						filter.IncludeExpenseCategoryIds.Add(includedId);
+					}
+				})
+				.UseViewModelDialog<FinancialExpenseCategoryViewModel>()
+				.Finish();
+
+			SaveCommand = new DelegateCommand(SaveHandler, () => CanSave);
+			CancelCommand = new DelegateCommand(CancelHandler);
+		}
+
+		public bool CanSave => (Entity.Id == 0 && _permissionResult.CanCreate) || _permissionResult.CanUpdate;
+
+		public Counterparty Counterparty
+		{
+			get => _counterparty;
+			set => SetField(ref _counterparty, value);
+		}
+
+		public Organization Organization
+		{
+			get => _organization;
+			set => SetField(ref _organization, value);
+		}
+
+		public IEntityEntryViewModel CounterpartyViewModel { get; set; }
+		public IEntityEntryViewModel OrganizationViewModel { get; set; }
+		public IEntityEntryViewModel FinancialExpenseCategoryViewModel { get; }
+		public ICommand SaveCommand { get; }
+		public ICommand CancelCommand { get; }
+
+		private void SaveHandler()
+		{
+			SaveAndClose();
+		}
+
+		private void CancelHandler()
+		{
+			Close(true, CloseSource.Cancel);
+		}
+	}
+}
