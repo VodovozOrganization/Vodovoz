9896d80289ffa785271e880a7228e0156065ee42
Vadim Gordeev <enzogord@gmail.com>
2024-09-19 10:35:22 +0300

    Edo OrderDocuments

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Data.NHibernate/Mapping/Edo/OrderEdoTaskMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Core.Domain.TrueMark;
+
+namespace Vodovoz.Core.Data.NHibernate.Mapping.Edo
+{
+	public class OrderEdoTaskMap : SubclassMap<OrderEdoTask>
+	{
+		public OrderEdoTaskMap()
+		{
+			DiscriminatorValue(nameof(EdoTaskType.Order));
+
+			Map(x => x.OrderId)
+				.Column("order_id");
+		}
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/TrueMark/EdoTaskItem.cs
@@ -0,0 +1,49 @@
+﻿using QS.DomainModel.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Core.Domain.TrueMark
+{
+	public class EdoTaskItem : PropertyChangedBase, IDomainObject
+	{
+		private int _id;
+		private int _orderEdoTaskId;
+		private int _orderItemId;
+		private int _codeId;
+		private int _transferEdoTaskId;
+
+		[Display(Name = "Код")]
+		public virtual int Id
+		{
+			get => _id;
+			set => SetField(ref _id, value);
+		}
+
+		[Display(Name = "Код ЭДО задачи заказа")]
+		public virtual int OrderEdoTaskId
+		{
+			get => _orderEdoTaskId;
+			set => SetField(ref _orderEdoTaskId, value);
+		}
+
+		[Display(Name = "Код товара в заказе")]
+		public virtual int OrderItemId
+		{
+			get => _orderItemId;
+			set => SetField(ref _orderItemId, value);
+		}
+
+		[Display(Name = "Код ЧЗ")]
+		public virtual int CodeId
+		{
+			get => _codeId;
+			set => SetField(ref _codeId, value);
+		}
+
+		[Display(Name = "Код ЭДО задачи перемещения")]
+		public virtual int TransferEdoTaskId
+		{
+			get => _transferEdoTaskId;
+			set => SetField(ref _transferEdoTaskId, value);
+		}
+	}
+}
