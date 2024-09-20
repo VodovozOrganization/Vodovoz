06df966f6b515339eb6d1de5db6823454dfbba09
Sergey Efimov <efimov90@gmail.com>
2024-09-20 15:17:44 +0300

    Фикс пробросов

--- Source/Applications/Desktop/VodovozViewModels/Accounting/Payments/PaymentWriteOffViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Accounting/Payments/PaymentWriteOffViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using QS.Commands;
+using QS.DomainModel.Entity;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
@@ -12,6 +13,7 @@ using Vodovoz.Domain.Client;
 using Vodovoz.Domain.Organizations;
 using Vodovoz.Settings.Common;
 using Vodovoz.ViewModels.Cash.FinancialCategoriesGroups;
+using Vodovoz.ViewModels.Extensions;
 using VodovozBusiness.Domain.Payments;
 
 namespace Vodovoz.ViewModels.Accounting.Payments
@@ -22,6 +24,7 @@ namespace Vodovoz.ViewModels.Accounting.Payments
 		private readonly IGeneralSettings _generalSettings;
 		private Counterparty _counterparty;
 		private Organization _organization;
+		private FinancialExpenseCategory _financialExpenseCategory;
 
 		public PaymentWriteOffViewModel(
 			IEntityUoWBuilder uowBuilder,
@@ -55,6 +58,7 @@ namespace Vodovoz.ViewModels.Accounting.Payments
 			FinancialExpenseCategoryViewModel = financialExpenseCategoryViewModelEEVMBuilder
 				.SetUnitOfWork(UoW)
 				.SetViewModel(this)
+				.ForProperty(this, x => x.FinancialExpenseCategory)
 				.UseViewModelJournalAndAutocompleter<FinancialCategoriesGroupsJournalViewModel, FinancialCategoriesJournalFilterViewModel>(filter =>
 				{
 					filter.RestrictFinancialSubtype = FinancialSubType.Expense;
@@ -70,20 +74,43 @@ namespace Vodovoz.ViewModels.Accounting.Payments
 
 			SaveCommand = new DelegateCommand(SaveHandler, () => CanSave);
 			CancelCommand = new DelegateCommand(CancelHandler);
+
+			SetPropertyChangeRelation(
+				x => x.CounterpartyId,
+				() => Counterparty);
+
+			SetPropertyChangeRelation(
+				x => x.OrganizationId,
+				() => Organization);
+
+			SetPropertyChangeRelation(
+				x => x.FinancialExpenseCategoryId,
+				() => FinancialExpenseCategory);
+
+			if(Entity.Id == 0)
+			{
+				Entity.Date = DateTime.Now;
+			}
 		}
 
 		public bool CanSave => (Entity.Id == 0 && _permissionResult.CanCreate) || _permissionResult.CanUpdate;
 
 		public Counterparty Counterparty
 		{
-			get => _counterparty;
-			set => SetField(ref _counterparty, value);
+			get => this.GetIdRefField(ref _counterparty, Entity.CounterpartyId);
+			set => this.SetIdRefField(SetField, ref _counterparty, () => Entity.CounterpartyId, value);
 		}
 
 		public Organization Organization
 		{
-			get => _organization;
-			set => SetField(ref _organization, value);
+			get => this.GetIdRefField(ref _organization, Entity.OrganizationId);
+			set => this.SetIdRefField(SetField, ref _organization, () => Entity.OrganizationId, value);
+		}
+
+		public FinancialExpenseCategory FinancialExpenseCategory
+		{
+			get => this.GetIdRefField(ref _financialExpenseCategory, Entity.FinancialExpenseCategoryId);
+			set => this.SetIdRefField(SetField, ref _financialExpenseCategory, () => Entity.FinancialExpenseCategoryId, value);
 		}
 
 		public IEntityEntryViewModel CounterpartyViewModel { get; set; }
