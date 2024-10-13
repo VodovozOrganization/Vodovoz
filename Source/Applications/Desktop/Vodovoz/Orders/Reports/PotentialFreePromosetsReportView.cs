908256e1221ee969994428699fcfc10d2dff12d7
N-Sem <nikiforov.semen@gmail.com>
2024-10-13 14:23:06 +0300

    Добавлена вью нового диалога отчета

---
+++ Source/Applications/Desktop/Vodovoz/Orders/Reports/PotentialFreePromosetsReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Orders.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PotentialFreePromosetsReportView : Gtk.Bin
+	{
+		public PotentialFreePromosetsReportView()
+		{
+			this.Build();
+		}
+	}
+}
