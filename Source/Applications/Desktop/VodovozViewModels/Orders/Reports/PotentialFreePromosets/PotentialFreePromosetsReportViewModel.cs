be70117e0a2a61e407eaf902452605ac3a1179e7
N-Sem <nikiforov.semen@gmail.com>
2024-10-15 17:03:22 +0300

    Запрос по дублям по номеру телефона

--- Source/Applications/Desktop/VodovozViewModels/Orders/Reports/PotentialFreePromosets/PotentialFreePromosetsReportViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Orders/Reports/PotentialFreePromosets/PotentialFreePromosetsReportViewModel.cs
@@ -16,7 +16,6 @@ using Vodovoz.Domain.Orders;
 using Vodovoz.Presentation.ViewModels.Extensions;
 using Vodovoz.Presentation.ViewModels.Factories;
 using static Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets.PotentialFreePromosetsReport;
-using static Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets.PotentialFreePromosetsReportViewModel;
 
 namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 {
@@ -231,7 +230,7 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 				.Distinct()
 				.ToList();
 
-			var authorNames = 
+			var authorNames =
 				(from author in uow.Session.Query<Employee>()
 				 where orderAuthorIds.Contains(author.Id)
 				 select new { AuthorId = author.Id, AuthorName = author.ShortName })
@@ -358,48 +357,101 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 
 		private IList<PromosetReportRow> MakeQueries()
 		{
-			var rows = GetAddressDuplicates(_uow);
+			var rows = new List<PromosetReportRow>();
 
-			return rows;
+			var duplicatesByAddress = GetAddressDuplicates(_uow);
+			rows.AddRange(duplicatesByAddress);
 
 			var selectedPromosets = GetSelectedPromotionalSets();
 
-			//Все строки номеров телефонов в карочках точек доставки,
-			//на которые оформлялись доставленные заказы с промонабором за период
-			var deliveryPointPhoneDigitNumbersHavingPromotionalSetsForPeriod =
+			var ordersWithPhonesHavingPromotionalSetsByDeliveryPointPhoneDigitNumbersForPeriod =
 				(from order in _uow.Session.Query<Order>()
 				 join orderItem in _uow.Session.Query<OrderItem>() on order.Id equals orderItem.Order.Id
 				 join phone in _uow.Session.Query<Phone>() on order.DeliveryPoint.Id equals phone.DeliveryPoint.Id
+				 join dp in _uow.Session.Query<DeliveryPoint>() on order.DeliveryPoint.Id equals dp.Id into deliveryPoints
+				 from deliveryPoint in deliveryPoints.DefaultIfEmpty()
+				 join cl in _uow.Session.Query<Counterparty>() on order.Client.Id equals cl.Id into clients
+				 from client in clients.DefaultIfEmpty()
+				 join dpc in _uow.Session.Query<DeliveryPointCategory>() on deliveryPoint.Category.Id equals dpc.Id into deliveryPointCategories
+				 from deliveryPointCategory in deliveryPointCategories.DefaultIfEmpty()
+				 join author in _uow.Session.Query<Employee>() on order.Author.Id equals author.Id
+				 join promoset in _uow.Session.Query<PromotionalSet>() on orderItem.PromoSet.Id equals promoset.Id
 				 where
 				 order.CreateDate >= StartDate.Value.Date
 				 && order.CreateDate < EndDate.Value.Date.AddDays(1)
-				 && !phone.IsArchive
 				 && selectedPromosets.Contains(orderItem.PromoSet.Id)
+				 && !phone.IsArchive
 				 && !_notDeliveredOrderStatuses.Contains(order.OrderStatus)
-				 && phone.DigitsNumber != null
-				 select phone.DigitsNumber).Distinct().ToList();
+				 select new OrderWithPhoneDataNode
+				 {
+					 OrderId = order.Id,
+					 ClientId = order.Client.Id,
+					 DeliveryPointId = order.DeliveryPoint.Id,
+					 OrderCreateDate = order.CreateDate,
+					 OrderDeliveryDate = order.DeliveryDate,
+					 AuthorId = order.Author.Id,
+					 AuthorName = author.ShortName,
+					 PhoneNumber = phone.Number,
+					 PhoneDigitNumber = phone.DigitsNumber,
+					 ClientName = client.FullName,
+					 DeliveryPointAddress = deliveryPoint.ShortAddress,
+					 DeliveryPointCategory = deliveryPointCategory.Name,
+					 PromosetId = promoset.Id,
+					 PromosetName = promoset.Name,
+					 IsRoot = true
+				 }).Distinct().ToList();
 
-			//Все строки номеров телефонов в карточках контаргентов,
-			//которым оформлялись доставленные заказы с промонабором за период
-			var clientPhoneDigitNumbersHavingPromotionalSetsForPeriod =
+			var ordersWithPhonesHavingPromotionalSetsByClientPhoneDigitNumbersForPeriod =
 				(from order in _uow.Session.Query<Order>()
 				 join orderItem in _uow.Session.Query<OrderItem>() on order.Id equals orderItem.Order.Id
 				 join phone in _uow.Session.Query<Phone>() on order.Client.Id equals phone.Counterparty.Id
+				 join dp in _uow.Session.Query<DeliveryPoint>() on order.DeliveryPoint.Id equals dp.Id into deliveryPoints
+				 from deliveryPoint in deliveryPoints.DefaultIfEmpty()
+				 join cl in _uow.Session.Query<Counterparty>() on order.Client.Id equals cl.Id into clients
+				 from client in clients.DefaultIfEmpty()
+				 join dpc in _uow.Session.Query<DeliveryPointCategory>() on deliveryPoint.Category.Id equals dpc.Id into deliveryPointCategories
+				 from deliveryPointCategory in deliveryPointCategories.DefaultIfEmpty()
+				 join author in _uow.Session.Query<Employee>() on order.Author.Id equals author.Id
+				 join promoset in _uow.Session.Query<PromotionalSet>() on orderItem.PromoSet.Id equals promoset.Id
 				 where
 				 order.CreateDate >= StartDate.Value.Date
 				 && order.CreateDate < EndDate.Value.Date.AddDays(1)
-				 && !phone.IsArchive
 				 && selectedPromosets.Contains(orderItem.PromoSet.Id)
+				 && !phone.IsArchive
 				 && !_notDeliveredOrderStatuses.Contains(order.OrderStatus)
-				 && phone.DigitsNumber != null
-				 select phone.DigitsNumber).Distinct().ToList();
+				 select new OrderWithPhoneDataNode
+				 {
+					 OrderId = order.Id,
+					 ClientId = order.Client.Id,
+					 DeliveryPointId = order.DeliveryPoint.Id,
+					 OrderCreateDate = order.CreateDate,
+					 OrderDeliveryDate = order.DeliveryDate,
+					 AuthorId = order.Author.Id,
+					 AuthorName = author.ShortName,
+					 PhoneNumber = phone.Number,
+					 PhoneDigitNumber = phone.DigitsNumber,
+					 ClientName = client.FullName,
+					 DeliveryPointAddress = deliveryPoint.ShortAddress,
+					 DeliveryPointCategory = deliveryPointCategory.Name,
+					 PromosetId = promoset.Id,
+					 PromosetName = promoset.Name,
+					 IsRoot = true
+				 })
+				 .Distinct()
+				 .ToList();
 
-			//Все строки номеров телефонов в карточке клиента и карточке точек доставки,
-			//на которые оформлялись доставленные заказы с промонабором за период
+			var ordersWithPhonesHavingPromotionalSetsByAllDigitNumbersForPeriod =
+				ordersWithPhonesHavingPromotionalSetsByDeliveryPointPhoneDigitNumbersForPeriod
+				.Union(ordersWithPhonesHavingPromotionalSetsByClientPhoneDigitNumbersForPeriod)
+				.DistinctBy(o => new { o.OrderId, o.PhoneDigitNumber, o.PromosetId })
+				.OrderBy(o => o.OrderId)
+				.ToList();
+
 			var allDigitNumbersHavingPromotionalSetsForPeriod =
-				deliveryPointPhoneDigitNumbersHavingPromotionalSetsForPeriod
-				.Union(clientPhoneDigitNumbersHavingPromotionalSetsForPeriod)
+				ordersWithPhonesHavingPromotionalSetsByAllDigitNumbersForPeriod
+				.Select(o => o.PhoneDigitNumber)
 				.Distinct()
+				.OrderBy(p => p)
 				.ToList();
 
 			var ordersWithPhonesHavingPromotionalSetsByDeliveryPointPhoneDigitNumbers =
@@ -412,6 +464,8 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 				 from client in clients.DefaultIfEmpty()
 				 join dpc in _uow.Session.Query<DeliveryPointCategory>() on deliveryPoint.Category.Id equals dpc.Id into deliveryPointCategories
 				 from deliveryPointCategory in deliveryPointCategories.DefaultIfEmpty()
+				 join author in _uow.Session.Query<Employee>() on order.Author.Id equals author.Id
+				 join promoset in _uow.Session.Query<PromotionalSet>() on orderItem.PromoSet.Id equals promoset.Id
 				 where
 				 orderItem.PromoSet.Id != null
 				 && !phone.IsArchive
@@ -425,12 +479,17 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 					 OrderCreateDate = order.CreateDate,
 					 OrderDeliveryDate = order.DeliveryDate,
 					 AuthorId = order.Author.Id,
+					 AuthorName = author.ShortName,
 					 PhoneNumber = phone.Number,
 					 PhoneDigitNumber = phone.DigitsNumber,
 					 ClientName = client.FullName,
 					 DeliveryPointAddress = deliveryPoint.ShortAddress,
-					 DeliveryPointCategory = deliveryPointCategory.Name
-				 }).Distinct().ToList();
+					 DeliveryPointCategory = deliveryPointCategory.Name,
+					 PromosetId = promoset.Id,
+					 PromosetName = promoset.Name
+				 })
+				 .Distinct()
+				 .ToList();
 
 			var ordersWithPhonesHavingPromotionalSetsByClientPhoneDigitNumbers =
 				(from order in _uow.Session.Query<Order>()
@@ -442,6 +501,8 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 				 from client in clients.DefaultIfEmpty()
 				 join dpc in _uow.Session.Query<DeliveryPointCategory>() on deliveryPoint.Category.Id equals dpc.Id into deliveryPointCategories
 				 from deliveryPointCategory in deliveryPointCategories.DefaultIfEmpty()
+				 join author in _uow.Session.Query<Employee>() on order.Author.Id equals author.Id
+				 join promoset in _uow.Session.Query<PromotionalSet>() on orderItem.PromoSet.Id equals promoset.Id
 				 where
 				 orderItem.PromoSet.Id != null
 				 && !phone.IsArchive
@@ -455,47 +516,99 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 					 OrderCreateDate = order.CreateDate,
 					 OrderDeliveryDate = order.DeliveryDate,
 					 AuthorId = order.Author.Id,
+					 AuthorName = author.ShortName,
 					 PhoneNumber = phone.Number,
 					 PhoneDigitNumber = phone.DigitsNumber,
 					 ClientName = client.FullName,
 					 DeliveryPointAddress = deliveryPoint.ShortAddress,
-					 DeliveryPointCategory = deliveryPointCategory.Name
-				 }).Distinct().ToList();
+					 DeliveryPointCategory = deliveryPointCategory.Name,
+					 PromosetId = promoset.Id,
+					 PromosetName = promoset.Name
+				 })
+				 .Distinct()
+				 .ToList();
 
-			var ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers = new List<OrderWithPhoneDataNode>();
-			ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers.AddRange(ordersWithPhonesHavingPromotionalSetsByDeliveryPointPhoneDigitNumbers);
-			ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers.AddRange(ordersWithPhonesHavingPromotionalSetsByClientPhoneDigitNumbers);
-
-			var unioned =
+			var ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers =
 				ordersWithPhonesHavingPromotionalSetsByDeliveryPointPhoneDigitNumbers
-				.Union(ordersWithPhonesHavingPromotionalSetsByClientPhoneDigitNumbers).ToList();
-
-			var distincted = ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers
-				.Select(o => o).Distinct().ToList();
-
-			var filtered =
-				distincted.GroupBy(d => d.PhoneDigitNumber)
-				.Where(g => g.Select(o => o.OrderId).Distinct().Count() > 1)
-				.SelectMany(g => g)
-				.DistinctBy(o => o.OrderId)
+				.Union(ordersWithPhonesHavingPromotionalSetsByClientPhoneDigitNumbers)
+				.DistinctBy(o => new { o.OrderId, o.PhoneDigitNumber, o.PromosetId })
+				.OrderBy(o => o.OrderId)
+				.ThenBy(o => o.IsRoot)
 				.ToList();
 
-			//var ordersHavignPromosetsAndSameDiditPhoneNumber =
-			//	from order in _uow.Session.Query<Order>()
-			//	join orderItem in _uow.Session.Query<OrderItem>() on order.Id equals orderItem.Order.Id
-			//	join cp1 in _uow.Session.Query<Phone>()
-			//	on new { ClientId = order.Client.Id, IsPhoneNotArchived = true } equals new { ClientId = cp1.Counterparty.Id, IsPhoneNotArchived = cp1.IsArchive }
-			//	into clientPhones
-			//	from clientPhone in clientPhones.DefaultIfEmpty()
-			//	join cp2 in _uow.Session.Query<Phone>()
-			//	on new { DeliveryPointId = order.DeliveryPoint.Id, IsPhoneNotArchived = true } equals new { DeliveryPointId = cp2.DeliveryPoint.Id, IsPhoneNotArchived = cp2.IsArchive }
-			//	into deliveryPointPhones
-			//	from deliveryPointPhone in deliveryPointPhones.DefaultIfEmpty()
-			//	join ph in ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers on clientPhone.DigitsNumber equals ph.PhoneDigitNumber
-			//	//where (ph.PhoneDigitNumber == clientPhone.DigitsNumber || ph.PhoneDigitNumber == deliveryPointPhone.DigitsNumber) && ph.OrderId != order.Id
-			//	select order;
+			var phonesByClientAndDeliveryPoint =
+				ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers
+				.GroupBy(o => (o.ClientId, o.DeliveryPointId))
+				.ToDictionary(g => g.Key, g => g.Select(o => o.PhoneNumber).Distinct().ToList());
 
-			//var oo = ordersHavignPromosetsAndSameDiditPhoneNumber.ToList();
+			var ff =
+				(from orderForPeriod in ordersWithPhonesHavingPromotionalSetsByAllDigitNumbersForPeriod
+				 join orderDuplicate in ordersWithPhonesHavingPromotionalSetsByAllDigitNumbers on orderForPeriod.PhoneDigitNumber equals orderDuplicate.PhoneDigitNumber
+				 where orderForPeriod.OrderId != orderDuplicate.OrderId
+				 select new { OrderForPeriod = orderForPeriod, OrderDuplicate = orderDuplicate })
+				.GroupBy(o => o.OrderForPeriod)
+				.OrderBy(g => g.Key.OrderId)
+				.ToDictionary(g => g.Key, g => g.Select(o => o.OrderDuplicate).DistinctBy(o => o.OrderId).ToList())
+				.DistinctBy(g => new { g.Key.OrderId, g.Key.DeliveryPointId, g.Key.PromosetId });
+
+
+			var promosetReportRows = new List<PromosetReportRow>();
+			var sequenceNumber = duplicatesByAddress.Count + 1;
+
+			foreach(var item in ff)
+			{
+				var rootOrder = item.Key;
+				var rootOrderPhones =
+					phonesByClientAndDeliveryPoint.TryGetValue((rootOrder.ClientId, rootOrder.DeliveryPointId), out var rootPhoneNumbers)
+					? string.Join(", ", rootPhoneNumbers)
+					: rootOrder.PhoneNumber;
+
+				var rootRow = new PromosetReportRow
+				{
+					SequenceNumber = sequenceNumber++,
+					Address = rootOrder.DeliveryPointAddress,
+					AddressCategory = rootOrder.DeliveryPointCategory,
+					Phone = rootOrderPhones,
+					Client = rootOrder.ClientName,
+					Order = rootOrder.OrderId,
+					OrderCreationDate = rootOrder.OrderCreateDate,
+					OrderDeliveryDate = rootOrder.OrderDeliveryDate,
+					Promoset = rootOrder.PromosetName,
+					Author = rootOrder.AuthorName,
+					IsRootRow = true
+				};
+
+				promosetReportRows.Add(rootRow);
+
+				foreach(var duplicateOrder in item.Value)
+				{
+					var duplicateOrderPhones =
+						phonesByClientAndDeliveryPoint.TryGetValue((duplicateOrder.ClientId, duplicateOrder.DeliveryPointId), out var duplicatePhoneNumbers)
+						? string.Join(", ", duplicatePhoneNumbers)
+						: duplicateOrder.PhoneNumber;
+
+					var duplicateRow = new PromosetReportRow
+					{
+						SequenceNumber = sequenceNumber++,
+						Address = duplicateOrder.DeliveryPointAddress,
+						AddressCategory = duplicateOrder.DeliveryPointCategory,
+						Phone = duplicateOrderPhones,
+						Client = duplicateOrder.ClientName,
+						Order = duplicateOrder.OrderId,
+						OrderCreationDate = duplicateOrder.OrderCreateDate,
+						OrderDeliveryDate = duplicateOrder.OrderDeliveryDate,
+						Promoset = duplicateOrder.PromosetName,
+						Author = duplicateOrder.AuthorName,
+						IsRootRow = duplicateOrder.IsRoot
+					};
+
+					promosetReportRows.Add(duplicateRow);
+				}
+			}
+
+			rows.AddRange(promosetReportRows);
+
+			return rows;
 		}
 
 		private IEnumerable<int> GetSelectedPromotionalSets()
@@ -522,12 +635,15 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 		public DateTime? OrderCreateDate { get; set; }
 		public DateTime? OrderDeliveryDate { get; set; }
 		public int AuthorId { get; set; }
+		public string AuthorName { get; set; }
 		public string PhoneNumber { get; set; }
 		public string PhoneDigitNumber { get; set; }
 		public string ClientName { get; set; }
 		public string DeliveryPointAddress { get; set; }
 		public string DeliveryPointCategory { get; set; }
+		public int PromosetId { get; set; }
 		public string PromosetName { get; set; }
+		public bool IsRoot { get; set; }
 	}
 
 	public class OrderDeliveryPointDataNode
@@ -541,7 +657,7 @@ namespace Vodovoz.ViewModels.Orders.Reports.PotentialFreePromosets
 		public AddressDataNode AddressDataNode { get; set; }
 		public string DeliveryPointCompiledAddress { get; set; }
 		public int? DeliveryPointAddressCategoryId { get; set; }
-}
+	}
 
 	public class AddressDataNode
 	{
