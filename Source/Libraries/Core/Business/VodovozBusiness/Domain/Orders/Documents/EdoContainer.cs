38746b017ced8f8e3a96cc326d5c2f85d904fcaa
Andrey <as1854@yandex.ru>
2024-10-21 10:35:46 +0300

    вынес EdoDocumentType в отдельный файл

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/EdoContainer.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/EdoContainer.cs
@@ -155,21 +155,4 @@ namespace Vodovoz.Domain.Orders.Documents
 		[Display(Name = "Подготовка к отправке")]
 		PreparingToSend
 	}
-
-	/// <summary>
-	/// WS - используется потому, что enum в NHibernate не может быть более 36 символов для 1 значения
-	/// </summary>
-	public enum EdoDocumentType
-	{
-		[Display(Name = "УПД")]
-		Upd,
-		[Display(Name = "Счёт")]
-		Bill,
-		[Display(Name = "Счет без отгрузки на предоплату")]
-		BillWSForAdvancePayment,
-		[Display(Name = "Cчет без отгрузки на долг")]
-		BillWSForDebt,
-		[Display(Name = "Cчет без отгрузки на постоплату")]
-		BillWSForPayment
-	}
 }
---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/EdoDocumentType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Orders.Documents
+{
+	/// <summary>
+	/// WS - используется потому, что enum в NHibernate не может быть более 36 символов для 1 значения
+	/// </summary>
+	public enum EdoDocumentType
+	{
+		[Display(Name = "УПД")]
+		Upd,
+		[Display(Name = "Счёт")]
+		Bill,
+		[Display(Name = "Счет без отгрузки на предоплату")]
+		BillWSForAdvancePayment,
+		[Display(Name = "Cчет без отгрузки на долг")]
+		BillWSForDebt,
+		[Display(Name = "Cчет без отгрузки на постоплату")]
+		BillWSForPayment
+	}
+}
