420e0fa7db48881897ee05441fe38e64e80fe889
Andrey <as1854@yandex.ru>
2024-10-24 10:00:59 +0300

    добавил док блоки

--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/ManualPaymentMatchingViewModelAllocatedNode.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/ManualPaymentMatchingViewModelAllocatedNode.cs
@@ -4,16 +4,46 @@ using Vodovoz.Domain.Payments;
 
 namespace Vodovoz.ViewModels.ViewModels.Payments
 {
+	/// <summary>
+	/// Информация о распределении
+	/// </summary>
 	public class ManualPaymentMatchingViewModelAllocatedNode
 	{
+		/// <summary>
+		/// Id распределения(строки платежа)
+		/// </summary>
 		public int PaymentItemId { get; set; }
+		/// <summary>
+		/// Id заказа
+		/// </summary>
 		public int OrderId { get; set; }
+		/// <summary>
+		/// Статус заказа
+		/// </summary>
 		public OrderStatus OrderStatus { get; set; }
+		/// <summary>
+		/// Дата заказа
+		/// </summary>
 		public DateTime OrderDate { get; set; }
+		/// <summary>
+		/// Сумма заказа
+		/// </summary>
 		public decimal OrderSum { get; set; }
+		/// <summary>
+		/// Распределенная сумма
+		/// </summary>
 		public decimal AllocatedSum { get; set; }
+		/// <summary>
+		/// Общая распределенная сумма(со всех платежей)
+		/// </summary>
 		public decimal AllAllocatedSum { get; set; }
+		/// <summary>
+		/// Статус оплаты заказа
+		/// </summary>
 		public OrderPaymentStatus OrderPaymentStatus { get; set; }
+		/// <summary>
+		/// Статус распределения
+		/// </summary>
 		public AllocationStatus PaymentItemStatus { get; set; }
 	}
 }
--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/ManualPaymentMatchingViewModelNode.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Payments/ManualPaymentMatchingViewModelNode.cs
@@ -5,17 +5,47 @@ using Vodovoz.Domain.Orders;
 
 namespace Vodovoz.ViewModels.ViewModels.Payments
 {
+	/// <summary>
+	/// Информация для распределения платежа
+	/// </summary>
 	public class ManualPaymentMatchingViewModelNode : JournalEntityNodeBase<Order>
 	{
 		public override string Title => $"{EntityType.GetSubjectNames()} №{Id}";
+		/// <summary>
+		/// Статус заказа
+		/// </summary>
 		public OrderStatus OrderStatus { get; set; }
+		/// <summary>
+		/// Дата заказа
+		/// </summary>
 		public DateTime OrderDate { get; set; }
+		/// <summary>
+		/// Текущая сумма заказа
+		/// </summary>
 		public decimal ActualOrderSum { get; set; }
+		/// <summary>
+		/// Прошлые распределения
+		/// </summary>
 		public decimal LastPayments { get; set; }
+		/// <summary>
+		/// Прошлое распределение(для корректного расчета текущего распределения)
+		/// </summary>
 		public decimal OldCurrentPayment { get; set; }
+		/// <summary>
+		/// Текущее распределение
+		/// </summary>
 		public decimal CurrentPayment { get; set; }
+		/// <summary>
+		/// Флаг для расчета или перерасчета суммы распределения
+		/// </summary>
 		public bool Calculate { get; set; }
+		/// <summary>
+		/// Статус оплаты заказа
+		/// </summary>
 		public OrderPaymentStatus OrderPaymentStatus { get; set; }
+		/// <summary>
+		/// Заказ Закр док
+		/// </summary>
 		public bool IsClosingDocumentsOrder { get; set; }
 	}
 }
