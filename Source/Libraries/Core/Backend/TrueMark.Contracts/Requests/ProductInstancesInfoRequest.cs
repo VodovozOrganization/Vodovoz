da515905a6b4100e03ffc3cf0b574874aa0755ea
Sergey Efimov <efimov90@gmail.com>
2024-10-29 10:35:55 +0300

    Contracts Change

--- Source/Libraries/Core/Backend/TrueMark.Contracts/Requests/ProductInstancesInfoRequest.cs
+++ Source/Libraries/Core/Backend/TrueMark.Contracts/Requests/ProductInstancesInfoRequest.cs
@@ -1,9 +1,11 @@
-﻿namespace TrueMark.Contracts.Requests
+﻿using System.Collections.Generic;
+
+namespace TrueMark.Contracts.Requests
 {
 	/// <summary>
 	/// Запрос информации о продукте
 	/// </summary>
-	public class ProductInstanceInfoRequest
+	public class ProductInstancesInfoRequest
 	{
 		/// <summary>
 		/// Токен для запроса в Честный знак
@@ -11,8 +13,8 @@
 		public string Bearer { get; set; }
 
 		/// <summary>
-		/// Код продукта для проверки
+		/// Коды продуктов для проверки
 		/// </summary>
-		public string ProductCode { get; set; }
+		public IEnumerable<string> ProductCodes { get; set; }
 	}
 }
