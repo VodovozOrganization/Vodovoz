cbb06dffb6a384d5bf9021bb01585dc7ece2f85a
Sergey Efimov <efimov90@gmail.com>
2024-11-02 11:18:32 +0300

    Подключение клиента

--- Source/TrueMark.ProductInstanceInfoCheck.Worker/DependencyInjection.cs
+++ Source/TrueMark.ProductInstanceInfoCheck.Worker/DependencyInjection.cs
@@ -1,6 +1,20 @@
-﻿namespace TrueMark.ProductInstanceInfoCheck.Worker;
+﻿using System.Net.Http.Headers;
+
+namespace TrueMark.ProductInstanceInfoCheck.Worker;
 public static class DependencyInjection
 {
 	public static IServiceCollection AddProductInstanceInfoCheckWorker(this IServiceCollection services)
-		=> services;
+	{
+		services.AddHttpClient<ProductInstanceInfoRequestConsumer>((serviceProvider, client) =>
+		{
+			var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+			var baseUri = configuration.GetValue<string>("ExternalTrueMarkBaseUrl")
+				?? throw new InvalidOperationException("Не найдена настройка \"ExternalTrueMarkBaseUrl\" в конфигурации");
+
+			client.BaseAddress = new Uri(baseUri);
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+		});
+
+		return services;
+	}
 }
