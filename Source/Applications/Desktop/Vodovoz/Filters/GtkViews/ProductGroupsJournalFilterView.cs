d5d680796621424a13edd646531b7044e1392fd3
N-Sem <nikiforov.semen@gmail.com>
2024-11-15 11:59:27 +0300

    Добавлен вью фильтра нового журнала

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/ProductGroupsJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ProductGroupsJournalFilterView : Gtk.Bin
+	{
+		public ProductGroupsJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
