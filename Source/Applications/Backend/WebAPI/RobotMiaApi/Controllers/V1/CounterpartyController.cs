82b0544dcefacc0e632060d83ccf294c10ddc52f
Sergey Efimov <efimov90@gmail.com>
2024-11-22 09:55:22 +0300

    Предварительная версия

---
+++ Source/Applications/Backend/WebAPI/RobotMiaApi/Controllers/V1/CounterpartyController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QS.DomainModel.UoW;
+using RobotMiaApi.Contracts.Responses.V1;
+using RobotMiaApi.Extensions.Mapping;
+using RobotMiaApi.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Vodovoz.Core.Domain.Extensions;
+using Vodovoz.Presentation.WebApi.Common;
+using Vodovoz.Services;
+
+namespace RobotMiaApi.Controllers.V1
+{
+	/// <summary>
+	/// Контроллер контрагентов
+	/// </summary>
+	public class CounterpartyController : VersionedController
+	{
+		private readonly IncomingCallCallService _incomingCallCallService;
+		private readonly ICounterpartyService _counterpartyService;
+
+		/// <summary>
+		/// Констркутор
+		/// </summary>
+		/// <param name="logger"></param>
+		/// <param name="incomingCallCallService"></param>
+		/// <param name="counterpartyService"></param>
+		public CounterpartyController(
+			ILogger<ApiControllerBase> logger,
+			IncomingCallCallService incomingCallCallService,
+			ICounterpartyService counterpartyService)
+			: base(logger)
+		{
+			_incomingCallCallService = incomingCallCallService
+				?? throw new ArgumentNullException(nameof(incomingCallCallService));
+			_counterpartyService = counterpartyService
+				?? throw new ArgumentNullException(nameof(counterpartyService));
+		}
+
+		/// <summary>
+		/// Получение контрагента по номеру телефона
+		/// </summary>
+		/// <param name="callId">Идентификатор звонка</param>
+		/// <param name="phoneNumber">Номер телефона</param>
+		/// <param name="unitOfWork"></param>
+		/// <returns></returns>
+		[HttpGet]
+		[Consumes(MediaTypeNames.Application.Json)]
+		[Produces(MediaTypeNames.Application.Json)]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<CounterpartyDto>))]
+		public async Task<IActionResult> GetAsync(
+			[FromQuery(Name = "call_id"), Required] Guid callId,
+			[FromQuery(Name = "phone_number"), Required] string phoneNumber,
+			[FromServices] IUnitOfWork unitOfWork)
+		{
+			phoneNumber = phoneNumber.NormalizePhone();
+
+			var possibleCounterparties = _counterpartyService
+				.GetByNormalizedPhoneNumber(unitOfWork, phoneNumber)
+				.ToList();
+
+			if(possibleCounterparties.Count != 1)
+			{
+				await _incomingCallCallService.RegisterCallAsync(callId, phoneNumber, null, unitOfWork);
+				await unitOfWork.CommitAsync();
+
+				return Problem("Клиент с данным телефоном не найден", statusCode: StatusCodes.Status404NotFound);
+			}
+			else
+			{
+				await _incomingCallCallService.RegisterCallAsync(callId, phoneNumber, possibleCounterparties.First().Id, unitOfWork);
+				await unitOfWork.CommitAsync();
+			}
+
+			return Ok(possibleCounterparties.MapToCounterpartyDtoV1());
+		}
+	}
+}
---
+++ Source/Applications/Backend/WebAPI/RobotMiaApi/Services/OrderService.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Logging;
+using QS.DomainModel.UoW;
+using RobotMiaApi.Contracts.Responses.V1;
+using RobotMiaApi.Extensions.Mapping;
+using RobotMiaApi.Specifications;
+using System;
+using System.Linq;
+using Vodovoz.Core.Domain.Repositories;
+using Vodovoz.Core.Domain.Specifications;
+using Vodovoz.Domain.Orders;
+using Vodovoz.Settings.Nomenclature;
+using Vodovoz.Settings.Roboats;
+using VodovozBusiness.Specifications.Orders;
+
+namespace RobotMiaApi.Services
+{
+	/// <summary>
+	/// Сервис заказов Api робота Мия
+	/// </summary>
+	public class OrderService
+	{
+		private static readonly OrderStatus[] _lastOrderCompletedStatuses = new OrderStatus[]
+		{
+			OrderStatus.Shipped,
+			OrderStatus.Closed,
+			OrderStatus.UnloadingOnStock
+		};
+		
+		private readonly ILogger<OrderService> _logger;
+		private readonly INomenclatureSettings _nomenclatureSettings;
+		private readonly IRoboatsSettings _roboatsSettings;
+		private readonly IGenericRepository<Order> _orderRepository;
+		private readonly IUnitOfWork _unitOfWork;
+
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		/// <param name="logger"></param>
+		/// <param name="nomenclatureSettings"></param>
+		/// <param name="roboatsSettings"></param>
+		/// <param name="orderRepository"></param>
+		/// <param name="unitOfWork"></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public OrderService(
+			ILogger<OrderService> logger,
+			INomenclatureSettings nomenclatureSettings,
+			IRoboatsSettings roboatsSettings,
+			IGenericRepository<Order> orderRepository,
+			IUnitOfWork unitOfWork)
+		{
+			_logger = logger
+				?? throw new ArgumentNullException(nameof(logger));
+			_nomenclatureSettings = nomenclatureSettings
+				?? throw new ArgumentNullException(nameof(nomenclatureSettings));
+			_roboatsSettings = roboatsSettings
+				?? throw new ArgumentNullException(nameof(roboatsSettings));
+			_orderRepository = orderRepository
+				?? throw new ArgumentNullException(nameof(orderRepository));
+			_unitOfWork = unitOfWork
+				?? throw new ArgumentNullException(nameof(unitOfWork));
+		}
+
+		/// <summary>
+		/// Получение последнего заказа контрагента
+		/// </summary>
+		/// <param name="counterpartyId"></param>
+		/// <returns></returns>
+		public LastOrderResponse GetLastOrderByCounterpartyId(int counterpartyId)
+		{
+			return _orderRepository
+				.Get(
+					_unitOfWork,
+					OrderSpecification
+						.CreateForCounterpartyId(counterpartyId)
+						.And(LastOrderSpecification.CreateForValidRobotMiaLastOrders(
+							_lastOrderCompletedStatuses,
+							DateTime.Today.AddMonths(-_roboatsSettings.OrdersInMonths),
+							_nomenclatureSettings.PaidDeliveryNomenclatureId,
+							_nomenclatureSettings.FastDeliveryNomenclatureId)))
+				.FirstOrDefault()?
+				.MapToLastOrderResponseV1();
+		}
+
+		/// <summary>
+		/// Получение последнего заказа точки доставки
+		/// </summary>
+		/// <param name="deliveryPointId"></param>
+		/// <returns></returns>
+		public LastOrderResponse GetLastOrderByDeliveryPointId(int deliveryPointId)
+		{
+			return _orderRepository
+				.Get(
+					_unitOfWork,
+					OrderSpecification
+						.CreateForDeliveryPointId(deliveryPointId)
+						.And(LastOrderSpecification.CreateForValidRobotMiaLastOrders(
+							_lastOrderCompletedStatuses,
+							DateTime.Today.AddMonths(-_roboatsSettings.OrdersInMonths),
+							_nomenclatureSettings.PaidDeliveryNomenclatureId,
+							_nomenclatureSettings.FastDeliveryNomenclatureId)))
+				.FirstOrDefault()?
+				.MapToLastOrderResponseV1();
+		}
+	}
+}
