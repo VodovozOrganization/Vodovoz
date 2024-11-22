83db4b84dbb5e95502b3557497414fd7263bd20e
Sergey Efimov <efimov90@gmail.com>
2024-11-22 09:34:01 +0300

    Dockblock

--- Source/Libraries/Core/Business/VodovozBusiness/Extensions/Mapping/RoboAtsOrderPaymentExtensions.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Extensions/Mapping/RoboAtsOrderPaymentExtensions.cs
@@ -4,8 +4,17 @@ using Vodovoz.Models.Orders;
 
 namespace VodovozBusiness.Extensions.Mapping
 {
+	/// <summary>
+	/// Расширение функционала <see cref="RoboAtsOrderPayment"/>
+	/// </summary>
 	public static class RoboAtsOrderPaymentExtensions
 	{
+		/// <summary>
+		/// Маппинг запроса на создание заказа в <see cref="PaymentType"/>
+		/// </summary>
+		/// <param name="roboAtsOrderPaymentType"></param>
+		/// <returns></returns>
+		/// <exception cref="NotSupportedException"></exception>
 		public static PaymentType MapToPaymentType(this RoboAtsOrderPayment roboAtsOrderPaymentType)
 		{
 			switch(roboAtsOrderPaymentType)
