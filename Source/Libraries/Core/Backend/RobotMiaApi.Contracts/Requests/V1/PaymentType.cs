cdf361003f662a284cd8f289e80ac828e6611fe6
Sergey Efimov <efimov90@gmail.com>
2024-11-22 09:55:22 +0300

    Предварительная версия

---
+++ Source/Libraries/Core/Backend/RobotMiaApi.Contracts/Requests/V1/PaymentType.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RobotMiaApi.Contracts.Requests.V1
+{
+	/// <summary>
+	/// Тип оплаты
+	/// </summary>
+	[JsonConverter(typeof(JsonStringEnumConverter))]
+	public enum PaymentType
+	{
+		/// <summary>
+		/// Наличные
+		/// </summary>
+		Cash,
+		/// <summary>
+		/// Qr-код
+		/// </summary>
+		SmsQR
+	}
+}
---
+++ Source/Libraries/Core/Backend/RobotMiaApi.Contracts/Responses/V1/CalculatePriceResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RobotMiaApi.Contracts.Responses.V1
+{
+	/// <summary>
+	/// Результат вычисления цены заказа
+	/// </summary>
+	public class CalculatePriceResponse
+	{
+		/// <summary>
+		/// Цена доставки
+		/// </summary>
+		[JsonPropertyName("delivery_price")]
+		public decimal DeliveryPrice { get; set; }
+
+		/// <summary>
+		/// Цена заказа
+		/// </summary>
+		[JsonPropertyName("order_price")]
+		public decimal OrderPrice { get; set; }
+	}
+}
