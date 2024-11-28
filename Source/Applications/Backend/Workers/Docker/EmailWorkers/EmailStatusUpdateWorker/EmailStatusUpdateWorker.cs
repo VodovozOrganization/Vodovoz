940d9e7d23a83e44c5c6bca228222dddc7380d0d
Sergey Efimov <efimov90@gmail.com>
2024-11-28 12:07:57 +0300

    Рефакторинг, фиксы, фиксы логирования

--- Source/Applications/Backend/Workers/Docker/EmailWorkers/EmailStatusUpdateWorker/EmailStatusUpdateWorker.cs
+++ Source/Applications/Backend/Workers/Docker/EmailWorkers/EmailStatusUpdateWorker/EmailStatusUpdateWorker.cs
@@ -1,8 +1,9 @@
-﻿using Mailjet.Api.Abstractions.Events;
+﻿using EmailStatusUpdateWorker.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using QS.DomainModel.UoW;
+using QS.Services;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.MailSending;
@@ -10,8 +11,6 @@ using System;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using QS.Services;
-using Vodovoz.Core.Data.NHibernate.Mappings;
 using Vodovoz.Domain.StoredEmails;
 using Vodovoz.EntityRepositories;
 
@@ -56,7 +55,11 @@ namespace EmailStatusUpdateWorker
 
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
-			_channel.BasicConsume(_storedEmailStatusUpdatingQueueId, false, _consumer);
+			_channel.BasicConsume(
+				_storedEmailStatusUpdatingQueueId,
+				false,
+				_consumer);
+
 			await Task.Delay(0, stoppingToken);
 		}
 
@@ -74,84 +77,115 @@ namespace EmailStatusUpdateWorker
 
 		private async Task MessageRecieved(object sender, BasicDeliverEventArgs e)
 		{
+			var body = e.Body;
+
+			UpdateStoredEmailStatusMessage message;
+
 			try
 			{
-				var body = e.Body;
-
-				var message = JsonSerializer.Deserialize<UpdateStoredEmailStatusMessage>(body.Span);
-
-				_logger.LogInformation($"Recieved message to update status for stored email with id: { message.EventPayload.Id }" +
-					$" to status: { message.Status }, request recieved at: { message.RecievedAt }");
-
-				if(message.EventPayload.Trackable)
-				{
-					using(var unitOfWork = _uowFactory.CreateWithoutRoot("Status update worker"))
-					{
-						var storedEmail = _emailRepository.GetById(unitOfWork, message.EventPayload.Id);
-
-						if(storedEmail != null)
-						{
-							_logger.LogInformation($"Found Email: { storedEmail.Id }, externalId { storedEmail.ExternalId }, status { storedEmail.State }");
-
-							if(storedEmail.StateChangeDate < message.RecievedAt)
-							{
-								try
-								{
-									var newStatus = ConvertFromMailjetStatus(message.Status);
-
-									if(newStatus == StoredEmailStates.Undelivered || newStatus == StoredEmailStates.SendingError)
-									{
-										storedEmail.AddDescription(message.ErrorInfo);
-									}
-
-									storedEmail.State = newStatus;
-									storedEmail.StateChangeDate = message.RecievedAt;
-									storedEmail.ExternalId = message.MailjetMessageId;
-
-									unitOfWork.Save(storedEmail);
-									unitOfWork.Commit();
-
-									_logger.LogInformation($"Email: { storedEmail.Id }, externalId { storedEmail.ExternalId }, status changed to { storedEmail.State }");
-								}
-								catch(ArgumentOutOfRangeException)
-								{
-									_logger.LogInformation($"Skipped event for emaid with id: { storedEmail.Id }, externalId { storedEmail.ExternalId } for status change to { message.Status }");
-								}
-							}
-						}
-						else
-						{
-							_logger.LogWarning($"Stored Email with id: { message.EventPayload.Id } not found");
-						}
-					}
-				}
-
-				_channel.BasicAck(e.DeliveryTag, false);
+				message = JsonSerializer
+					.Deserialize<UpdateStoredEmailStatusMessage>(body.Span);
 			}
 			catch(Exception ex)
 			{
-				_logger.LogError(ex.Message);
-				throw;
+				_logger.LogError(ex, "Error occured while deserialization of UpdateStoredEmailStatusMessage: {ExceptionMessage}", ex.Message);
+				
+				_channel.BasicAck(e.DeliveryTag, false);
+				return;
 			}
-		}
 
-		private StoredEmailStates ConvertFromMailjetStatus(MailEventType mailEventType)
-		{
-			switch(mailEventType)
+			_logger.LogInformation(
+				"Recieved message to update status for stored email with id: {EmailId}" +
+				" to status: {NewStatus}, request recieved at: {RecievedDateTime}",
+				message.EventPayload.Id,
+				message.Status,
+				message.RecievedAt);
+
+			if(!message.EventPayload.Trackable)
 			{
-				case MailEventType.sent:
-					return StoredEmailStates.Delivered;
-				case MailEventType.open:
-					return StoredEmailStates.Opened;
-				case MailEventType.spam:
-					return StoredEmailStates.MarkedAsSpam;
-				case MailEventType.bounce:
-					return StoredEmailStates.SendingError;
-				case MailEventType.blocked:
-					return StoredEmailStates.Undelivered;
+				_channel.BasicAck(e.DeliveryTag, false);
+				return;
 			}
 
-			throw new ArgumentOutOfRangeException(nameof(mailEventType), $"Тип события { mailEventType } не поддерживается");
+			using var unitOfWork = _uowFactory.CreateWithoutRoot("Status update worker");
+
+			var storedEmail = _emailRepository.GetById(
+				unitOfWork,
+				message.EventPayload.Id);
+
+			if(storedEmail is null)
+			{
+				_logger.LogWarning(
+					"Stored Email with id: {EmailId} not found",
+					message.EventPayload.Id);
+
+				_channel.BasicAck(e.DeliveryTag, false);
+				return;
+			}
+
+			_logger.LogInformation("Found Email: {EmailId}," +
+				" externalId {ExternalEmailId}, status {OldStatus}",
+				storedEmail.Id,
+				storedEmail.ExternalId,
+				storedEmail.State);
+
+			if(storedEmail.StateChangeDate >= message.RecievedAt)
+			{
+				_channel.BasicAck(e.DeliveryTag, false);
+				return;
+			}
+
+			StoredEmailStates newStatus;
+
+			try
+			{
+				newStatus = message.Status.MapToStoredEmailStates();
+			}
+			catch(ArgumentOutOfRangeException)
+			{
+				_logger.LogInformation("Skipped event for emaid with id: {EmailId}," +
+					" externalId {ExternalEmailId} for status change to {NewStatus}",
+					storedEmail.Id,
+					storedEmail.ExternalId,
+					message.Status);
+
+				_channel.BasicAck(e.DeliveryTag, false);
+				return;
+			}
+
+			if(newStatus == StoredEmailStates.Undelivered
+				|| newStatus == StoredEmailStates.SendingError)
+			{
+				storedEmail.AddDescription(message.ErrorInfo);
+			}
+
+			storedEmail.State = newStatus;
+			storedEmail.StateChangeDate = message.RecievedAt;
+			storedEmail.ExternalId = message.MailjetMessageId;
+
+			try
+			{
+				unitOfWork.Save(storedEmail);
+				unitOfWork.Commit();
+
+				_logger.LogInformation("Email: {EmailId}," +
+					" externalId {ExternalEmailId}, status changed to {NewStatus}",
+					storedEmail.Id,
+					storedEmail.ExternalId,
+					storedEmail.State);
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError(
+					ex,
+					"Error occured while saving new Email Status: {ExceptionMessage}",
+					ex.Message);
+				return;
+			}
+
+			_channel.BasicAck(e.DeliveryTag, false);
+
+			await Task.CompletedTask;
 		}
 	}
 }
