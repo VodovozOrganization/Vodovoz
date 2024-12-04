807113ce535040939faa7be2f4eea944578ed237
N-Sem <nikiforov.semen@gmail.com>
2024-12-04 17:11:30 +0300

    Добавлен вью отчета

---
+++ Source/Applications/Desktop/Vodovoz/Views/Reports/OrderChangesReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrderChangesReportView : Gtk.Bin
+	{
+		public OrderChangesReportView()
+		{
+			this.Build();
+		}
+	}
+}
