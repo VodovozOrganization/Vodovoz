58acbc7cde34221a13f88518164ff32d716eafe6
N-Sem <nikiforov.semen@gmail.com>
2024-12-06 18:02:28 +0300

    Edo OrderDocuments

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/TrueMark/EdoDocumentType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Core.Domain.TrueMark
+{
+	public enum EdoDocumentType
+	{
+		[Display(Name = "УПД")]
+		UPD,
+
+		[Display(Name = "Счет")]
+		Bill,
+	}
+}
