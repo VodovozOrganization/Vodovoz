cb2ff345f133d2586c6d9633e2773cb9b1e6212a
Vadim Gordeev <enzogord@gmail.com>
2024-12-09 00:57:54 +0300

    Перенос StorageResource в Core

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/StoredResources/ImageType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.StoredResources
+{
+	public enum ImageType
+	{
+		[Display(Name = "Подпись")]
+		Signature,
+		[Display(Name = "Прочее")]
+		Other
+	}
+}
