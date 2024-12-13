f853d8ff994f4c38de2eb0710433b13ec7207da9
Sergey Efimov <efimov90@gmail.com>
2024-12-13 14:56:56 +0300

    Докблоки

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Documents/DefectSource.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Documents/DefectSource.cs
@@ -2,18 +2,35 @@
 
 namespace Vodovoz.Domain.Documents
 {
+	/// <summary>
+	/// Откуда брак
+	/// </summary>
 	public enum DefectSource
 	{
+		/// <summary>
+		/// Нет
+		/// </summary>
 		[Display(Name = "")]
 		None,
+		/// <summary>
+		/// Водитель
+		/// </summary>
 		[Display(Name = "Водитель")]
 		Driver,
+		/// <summary>
+		/// Клиент
+		/// </summary>
 		[Display(Name = "Клиент")]
 		Client,
+		/// <summary>
+		/// Производство
+		/// </summary>
 		[Display(Name = "Производство")]
 		Production,
+		/// <summary>
+		/// Склад
+		/// </summary>
 		[Display(Name = "Склад")]
 		Warehouse
 	}
 }
-
