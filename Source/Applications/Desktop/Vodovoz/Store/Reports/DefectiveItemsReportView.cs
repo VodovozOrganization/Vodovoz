1cd0a2ba9d13b51dacea31d6f841781688f1707b
Sergey Efimov <efimov90@gmail.com>
2024-12-13 17:55:42 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Store/Reports/DefectiveItemsReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Store.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DefectiveItemsReportView : Gtk.Bin
+	{
+		public DefectiveItemsReportView()
+		{
+			this.Build();
+		}
+	}
+}
