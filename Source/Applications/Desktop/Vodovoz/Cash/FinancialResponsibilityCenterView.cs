89a01b504586e0fa828c326a5213193f6de4cee0
Sergey Efimov <efimov90@gmail.com>
2024-12-18 15:54:15 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Cash/FinancialResponsibilityCenterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Cash
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FinancialResponsibilityCenterView : Gtk.Bin
+	{
+		public FinancialResponsibilityCenterView()
+		{
+			this.Build();
+		}
+	}
+}
