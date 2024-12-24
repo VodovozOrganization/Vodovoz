5f7e7605723ac8a07c7a89bf7843db9d3e7a9593
Andrey <as1854@yandex.ru>
2024-12-24 15:47:53 +0300

    перенес Entrance и Room types

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Clients/DeliveryPoints/RoomType.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Clients/DeliveryPoints/RoomType.cs
@@ -1,19 +1,40 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Vodovoz.Domain.Client
+namespace Vodovoz.Core.Domain.Clients.DeliveryPoints
 {
+	/// <summary>
+	/// Тип помещения
+	/// </summary>
 	public enum RoomType
 	{
+		/// <summary>
+		/// Квартира
+		/// </summary>
 		[Display(Name = "Квартира", ShortName = "кв.")]
 		Apartment,
+		/// <summary>
+		/// Офис
+		/// </summary>
 		[Display(Name = "Офис", ShortName = "оф.")]
 		Office,
+		/// <summary>
+		/// Склад
+		/// </summary>
 		[Display(Name = "Склад", ShortName = "склад")]
 		Store,
+		/// <summary>
+		/// Помещение
+		/// </summary>
 		[Display(Name = "Помещение", ShortName = "пом.")]
 		Room,
+		/// <summary>
+		/// Комната
+		/// </summary>
 		[Display(Name = "Комната", ShortName = "ком.")]
 		Chamber,
+		/// <summary>
+		/// Секция
+		/// </summary>
 		[Display(Name = "Секция", ShortName = "сек.")]
 		Section
 	}
