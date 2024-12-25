a49edf8137693367555ece0f0166727112c51c19
Vadim Gordeev <enzogord@gmail.com>
2024-12-25 20:21:15 +0300

    Добавлен сервис для прогрева соединения с бд при старте

---
+++ Source/Applications/Backend/Workers/Docker/Edo/Edo.Scheduler.Worker/HostWarmUpService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using QS.DomainModel.UoW;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Edo.Scheduler.Worker
+{
+	public sealed class HostWarmUpService : IHostedService
+	{
+		private readonly ILogger _logger;
+		private readonly IUnitOfWorkFactory _uowFactory;
+
+		public HostWarmUpService(
+			ILogger<HostWarmUpService> logger,
+			IUnitOfWorkFactory uowFactory,
+			IHostApplicationLifetime appLifetime)
+		{
+			_logger = logger;
+			_uowFactory = uowFactory ?? throw new ArgumentNullException(nameof(uowFactory));
+
+			appLifetime.ApplicationStarted.Register(OnStarted);
+		}
+
+		private void OnStarted()
+		{
+			_logger.LogInformation("Application starting");
+			WarmUp();
+			_logger.LogInformation("Application started");
+		}
+
+		public void WarmUp()
+		{
+			_uowFactory.CreateWithoutRoot().Dispose();
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
