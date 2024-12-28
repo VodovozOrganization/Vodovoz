f9eb75d6727dab9f220252f8c4742cadf73a18d2
N-Sem <nikiforov.semen@gmail.com>
2024-12-28 13:09:35 +0300

    Добавлен сервис работы с товарными ограничителями

---
+++ Source/Libraries/Core/FuelControl/FuelControl.Library/Services/IFuelControlFuelCardProductRestrictionService.cs
@@ -0,0 +1,14 @@
+﻿using FuelControl.Contracts.Dto;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FuelControl.Library.Services
+{
+	public interface IFuelControlFuelCardProductRestrictionService
+	{
+		Task<IEnumerable<FuelCardProductRestrictionDto>> GetProductRestrictionsByCardId(string cardId, string sessionId, string apiKey, CancellationToken cancellationToken);
+		Task<bool> RemoveProductRestictionById(string restrictionId, string sessionId, string apiKey, CancellationToken cancellationToken);
+		Task<IEnumerable<string>> SetProductRestriction(string cardId, string productGroupId, string sessionId, string apiKey, CancellationToken cancellationToken);
+	}
+}
