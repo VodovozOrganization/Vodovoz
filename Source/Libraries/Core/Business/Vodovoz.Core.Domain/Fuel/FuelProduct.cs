a345feba306d099865d64fd8723e488354dcba6f
N-Sem <nikiforov.semen@gmail.com>
2025-01-08 18:16:42 +0300

    Добавлен класс продукта топлива и мапинг

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Fuel/FuelProduct.cs
@@ -0,0 +1,53 @@
+﻿using QS.DomainModel.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Core.Domain.Fuel
+{
+	[Appellative(Gender = GrammaticalGender.Neuter,
+		NominativePlural = "топливо",
+		Nominative = "топливо"
+	)]
+	public class FuelProduct : PropertyChangedBase, IDomainObject
+	{
+		private int _id;
+		private int _fuelTypeId;
+		private string _productId;
+		private string _description;
+		private bool _isArchived;
+
+		[Display(Name = "Код")]
+		public virtual int Id
+		{
+			get => _id;
+			set => SetField(ref _id, value);
+		}
+
+		[Display(Name = "Код типа топлива")]
+		public virtual int FuelTypeId
+		{
+			get => _fuelTypeId;
+			set => SetField(ref _fuelTypeId, value);
+		}
+
+		[Display(Name = "Код топлива")]
+		public virtual string ProductId
+		{
+			get => _productId;
+			set => SetField(ref _productId, value);
+		}
+
+		[Display(Name = "Описание")]
+		public virtual string Description
+		{
+			get => _description;
+			set => SetField(ref _description, value);
+		}
+
+		[Display(Name = "В архиве")]
+		public virtual bool IsArchived
+		{
+			get => _isArchived;
+			set => SetField(ref _isArchived, value);
+		}
+	}
+}
---
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Fuel/FuelProductMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Core.Domain.Fuel;
+
+namespace Vodovoz.Data.NHibernate.HibernateMapping.Fuel
+{
+	public class FuelProductMap : ClassMap<FuelProduct>
+	{
+		public FuelProductMap()
+		{
+			Table("fuel_products");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.FuelTypeId).Column("fuel_type_id");
+			Map(x => x.ProductId).Column("product_id");
+			Map(x => x.Description).Column("description");
+			Map(x => x.IsArchived).Column("is_archived");
+		}
+	}
+}
