a64d269c0321f501e584449a16d292f0582979f7
Sergey Efimov <efimov90@gmail.com>
2025-01-09 08:48:43 +0300

    New DiscussionsWidget UI

---
+++ Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Discussions/DiscussionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using QS.ViewModels;
+
+namespace Vodovoz.Presentation.ViewModels.Discussions
+{
+	public class DiscussionViewModel : WidgetViewModelBase
+	{
+		public DiscussionViewModel()
+		{
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz.Presentation.Views/Vodovoz.Presentation.Views/Discussions/DiscussionView.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using QS.Views.GtkUI;
+using Vodovoz.Presentation.ViewModels.Discussions;
+
+namespace Vodovoz.Presentation.Views.Discussions
+{
+	[ToolboxItem(true)]
+	public partial class DiscussionView : WidgetViewBase<DiscussionViewModel>
+	{
+		public DiscussionView()
+		{
+			Build();
+		}
+	}
+}
