edb83ae7e0f4fa56cf7bcfb0c7d276d20a0b2a50
Andrey <as1854@yandex.ru>
2025-01-13 11:02:13 +0300

    добавил классы для проверки кодов в ЧЗ

---
+++ Source/Libraries/Core/Backend/TrueMark.Contracts/Requests/CodeCheckRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TrueMark.Contracts.Requests
+{
+	/// <summary>
+	/// Получение информации о коде
+	/// </summary>
+	public class CodeCheckRequest
+	{
+		/// <summary>
+		/// Заводской номер фискального накопителя
+		/// </summary>
+		public string FiscalDriveNumber { get; set; }
+		/// <summary>
+		/// Список кодов маркировки на проверку
+		/// </summary>
+		IEnumerable<string> Codes { get; set; }
+	}
+}
