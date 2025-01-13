4cc88935e0a462a4522400d4cbe390cfb63eacaf
Sergey Efimov <efimov90@gmail.com>
2025-01-13 06:34:57 +0300

    Temp2

--- Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Discussions/DiscussionViewModel.cs
+++ Source/Applications/Desktop/Vodovoz.Presentation.ViewModels/Discussions/DiscussionViewModel.cs
@@ -6,11 +6,14 @@ using QS.Services;
 using QS.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using Vodovoz.Application.FileStorage;
 using Vodovoz.Core.Domain.Common;
 using Vodovoz.Domain.Employees;
+using Vodovoz.EntityRepositories;
 using Vodovoz.Presentation.ViewModels.AttachedFiles;
 using Vodovoz.Services;
 using VodovozBusiness.Domain.Discussions;
@@ -19,14 +22,13 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 {
 	public abstract class DiscussionViewModel : WidgetViewModelBase
 	{
-
 	}
 
-	public class DiscussionViewModel<TContainer, TDiscussion, TDiscussionComment, TFileInformation>
+	public class DiscussionViewModel<TDiscussionContainer, TDiscussion, TDiscussionComment, TFileInformation>
 		: DiscussionViewModel
-		where TContainer : IDomainObject
+		where TDiscussionContainer : IDomainObject
 		where TDiscussionComment : class, IDiscussionComment<TFileInformation>, new()
-		where TDiscussion : class, IDiscussion<TContainer, TDiscussionComment, TFileInformation>
+		where TDiscussion : class, IDiscussion<TDiscussionContainer, TDiscussionComment, TFileInformation>
 		where TFileInformation : FileInformation
 	{
 		private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
@@ -36,6 +38,7 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly IUserService _userService;
+		private readonly IUserRepository _userRepository;
 		private readonly IEmployeeService _employeeService;
 		private readonly IInteractiveService _interactiveService;
 		private readonly IAttachedFileInformationsViewModelFactory _attachedFileInformationsViewModelFactory;
@@ -48,10 +51,11 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 
 		public DiscussionViewModel(
 			IUnitOfWork unitOfWork,
+			TDiscussion discussion,
 			IUserService userService,
+			IUserRepository userRepository,
 			IEmployeeService employeeService,
 			IInteractiveService interactiveService,
-			TDiscussion discussion,
 			ICurrentPermissionService currentPermissionService,
 			IAttachedFileInformationsViewModelFactory attachedFileInformationsViewModelFactory,
 			IEntityFileStorageService<TDiscussionComment> entityFileStorageService)
@@ -62,7 +66,7 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 			}
 
 			_containerEntiryPermissionResult =
-				currentPermissionService.ValidateEntityPermission(typeof(TContainer));
+				currentPermissionService.ValidateEntityPermission(typeof(TDiscussionContainer));
 			_discussionPermissionResult =
 				currentPermissionService.ValidateEntityPermission(typeof(TDiscussion));
 
@@ -70,6 +74,7 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 				?? throw new ArgumentNullException(nameof(unitOfWork));
 			_userService = userService
 				?? throw new ArgumentNullException(nameof(userService));
+			_userRepository = userRepository;
 			_employeeService = employeeService
 				?? throw new ArgumentNullException(nameof(employeeService));
 			_interactiveService = interactiveService
@@ -83,6 +88,7 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 			AddCommentCommand = new DelegateCommand(AddComment, () => CanAddComment);
 
 			DiscussionComment = new TDiscussionComment();
+			OpenFileCommand = new DelegateCommand<TFileInformation>(OpenFile);
 
 			AttachedFileInformationsViewModel = _attachedFileInformationsViewModelFactory.CreateAndInitialize<TDiscussionComment, TFileInformation>(
 				_unitOfWork,
@@ -96,7 +102,9 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 		}
 
 		public TDiscussion Discussion { get; }
+
 		public DelegateCommand AddCommentCommand { get; }
+		public DelegateCommand<TFileInformation> OpenFileCommand { get; }
 
 		[PropertyChangedAlso(nameof(CanAddComment))]
 		public virtual string NewCommentText
@@ -173,6 +181,70 @@ namespace Vodovoz.Presentation.ViewModels.Discussions
 				DiscussionComment.DeleteFileInformation);
 
 			AttachedFileInformationsViewModel.ReadOnly = !CanEdit;
+		}
+
+		private void OpenFile(TFileInformation fileInformation)
+		{
+			byte[] blob;
+
+			if(fileInformation.Id == 0)
+			{
+				blob = FilesToUploadOnSave.FirstOrDefault(actionCommentIdToFile => actionCommentIdToFile.Value.ContainsKey(fileInformation.FileName))
+					.Value[fileInformation.FileName];
+			}
+			else
+			{
+				var comment = Discussion.Comments.FirstOrDefault(cdc => cdc.Id == fileInformation.Id);
+
+				var fileResult = _entityFileStorageService.GetFileAsync(comment, fileInformation.FileName, _cancellationTokenSource.Token)
+					.GetAwaiter()
+					.GetResult();
+
+				if(fileResult.IsFailure)
+				{
+					return;
+				}
+
+				using(var ms = new MemoryStream())
+				{
+					fileResult.Value.CopyTo(ms);
+
+					blob = ms.ToArray();
+				}
+			}
+
+			var vodovozUserTempDirectory = _userRepository.GetTempDirForCurrentUser(_unitOfWork);
+
+			if(string.IsNullOrWhiteSpace(vodovozUserTempDirectory))
+			{
+				return;
+			}
+
+			var tempFilePath = Path.Combine(Path.GetTempPath(), vodovozUserTempDirectory, fileInformation.FileName);
+
+			if(!File.Exists(tempFilePath))
+			{
+				File.WriteAllBytes(tempFilePath, blob);
+			}
+
+			var process = new Process
+			{
+				EnableRaisingEvents = true
+			};
+
+			process.StartInfo.FileName = Path.Combine(vodovozUserTempDirectory, fileInformation.FileName);
+
+			process.Exited += OnProcessExited;
+			process.Start();
+		}
+
+		private void OnProcessExited(object sender, EventArgs e)
+		{
+			if(sender is Process process)
+			{
+				File.Delete(process.StartInfo.FileName);
+				process.Exited -= OnProcessExited;
+			}
 		}
 	}
 }
