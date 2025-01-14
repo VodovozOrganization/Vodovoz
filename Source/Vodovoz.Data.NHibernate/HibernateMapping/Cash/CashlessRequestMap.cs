4d8cfc92ff2e9adacf4cd490cd99007928dfed9d
Sergey Efimov <efimov90@gmail.com>
2025-01-14 18:50:24 +0300

    Split Mapping

---
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Cash/CashlessRequestMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Domain.Cash;
+
+namespace Vodovoz.Data.NHibernate.HibernateMapping.Cash
+{
+	public class CashlessRequestMap : SubclassMap<CashlessRequest>
+	{
+		public CashlessRequestMap()
+		{
+			DiscriminatorValue(PayoutRequestDocumentType.CashlessRequest.ToString());
+			Map(clr => clr.Sum).Column("sum");
+			References(clr => clr.Counterparty).Column("counterparty_id");
+			HasMany(clr => clr.AttachedFileInformations)
+				.Cascade.AllDeleteOrphan()
+				.Inverse()
+				.KeyColumn("cashless_request_id");
+		}
+	}
+}
