62fcb012b4892b7bf8d8a88109c26936d2b67659
Sergey Efimov <efimov90@gmail.com>
2025-01-15 05:54:51 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Organizations/OrganizationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Organizations
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrganizationView : Gtk.Bin
+	{
+		public OrganizationView()
+		{
+			this.Build();
+		}
+	}
+}
