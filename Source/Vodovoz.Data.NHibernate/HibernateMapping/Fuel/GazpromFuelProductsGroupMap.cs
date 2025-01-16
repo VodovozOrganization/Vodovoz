289993d783ee6cdb5cbfe06bc0e92e74a61378e3
N-Sem <nikiforov.semen@gmail.com>
2025-01-16 10:11:18 +0300

    Добавлена группа товаров и мапинг

---
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Fuel/GazpromFuelProductsGroupMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Core.Domain.Fuel;
+
+namespace Vodovoz.Data.NHibernate.HibernateMapping.Fuel
+{
+	public class GazpromFuelProductsGroupMap : ClassMap<GazpromFuelProductsGroup>
+	{
+		public GazpromFuelProductsGroupMap()
+		{
+			Table("fuel_products");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.FuelTypeId).Column("fuel_type_id");
+			Map(x => x.GazpromFuelProductGroupId).Column("gazprom_category_id");
+			Map(x => x.GazpromFuelProductGroupName).Column("gazprom_category_name");
+			Map(x => x.IsArchived).Column("is_archived");
+		}
+	}
+}
