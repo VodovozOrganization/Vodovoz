9685cb62c309806a5bd4cf59b0478d1c5898ce84
N-Sem <nikiforov.semen@gmail.com>
2025-01-20 15:00:54 +0300

    Добавлен класс нового отчета

---
+++ Source/Applications/Desktop/VodovozViewModels/Transport/Reports/IncorrectFuelReport.cs
@@ -0,0 +1,16 @@
+﻿namespace Vodovoz.ViewModels.Transport.Reports
+{
+	public class IncorrectFuelReport
+	{
+		public string CarRegNumber { get; set; }
+		public string CarOwnType { get; set; }
+		public string CarTypeOfUse { get; set; }
+		public string CarModel { get; set; }
+		public string DriverCategory { get; set; }
+		public string DriverName { get; set; }
+		public string FuelCardNumber { get; set; }
+		public string CarFuelType { get; set; }
+		public string TransactionFuelType { get; set; }
+		public string TransactionDateTime { get; set; }
+	}
+}
