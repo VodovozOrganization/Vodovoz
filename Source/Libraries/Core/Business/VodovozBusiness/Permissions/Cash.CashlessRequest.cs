185318b1432dd693e1b54a5b6f51751c3684f2b3
Sergey Efimov <efimov90@gmail.com>
2025-01-20 10:10:57 +0300

    Право

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Permissions/Cash.CashlessRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Vodovoz.Permissions
+{
+	public static partial class Cash
+	{
+		/// <summary>
+		/// Права заявок на выдачу денежных средств по безналу
+		/// </summary>
+		public static class CashlessRequest
+		{
+			/// <summary>
+			/// Достапно создание календаря платежей
+			/// </summary>
+			public static string CanCreateGiveOutSchedule => "can_create_give_out_schedule";
+		}
+	}
+}
