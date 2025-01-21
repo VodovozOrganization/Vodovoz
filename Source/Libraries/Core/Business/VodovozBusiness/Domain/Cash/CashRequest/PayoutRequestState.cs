fd0bc7ee05954f1b5add96cc0abb5c4154971f91
Sergey Efimov <efimov90@gmail.com>
2025-01-21 07:36:46 +0300

    В отдельный файл

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Cash/CashRequest/PayoutRequestState.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Domain.Cash
+{
+	/// <summary>
+	/// Состояние заявки на выдачу средств
+	/// </summary>
+	public enum PayoutRequestState
+	{
+		/// <summary>
+		/// Новая
+		/// </summary>
+		[Display(Name = "Новая")]
+		New,
+
+		/// <summary>
+		/// На уточнении
+		/// </summary>
+		[Display(Name = "На уточнении")]
+		OnClarification, // после отправки на пересогласование
+
+		/// <summary>
+		/// Подана
+		/// </summary>
+		[Display(Name = "Подана")]
+		Submited, // после подтверждения
+
+		/// <summary>
+		/// Согласована руководителем отдела
+		/// </summary>
+		[Display(Name = "Согласована руководителем отдела")]
+		AgreedBySubdivisionChief, // после согласования руководителем
+
+		/// <summary>
+		/// Согласована исполнительным директором
+		/// </summary>
+		[Display(Name = "Согласована исполнительным директором")]
+		Agreed, // после согласования исполнительным директором
+
+		/// <summary>
+		/// Передана на выдачу
+		/// </summary>
+		[Display(Name = "Передана на выдачу")]
+		GivenForTake,
+
+		/// <summary>
+		/// Частично закрыта
+		/// </summary>
+		[Display(Name = "Частично закрыта")]
+		PartiallyClosed, // содержит не выданные суммы
+
+		/// <summary>
+		/// Отменена
+		/// </summary>
+		[Display(Name = "Отменена")]
+		Canceled,
+
+		/// <summary>
+		/// Закрыта
+		/// </summary>
+		[Display(Name = "Закрыта")]
+		Closed // все суммы выданы
+	}
+}
