5776e3a744ca9c87698574f37e388d0bd387dedf
N-Sem <nikiforov.semen@gmail.com>
2025-01-27 08:57:17 +0300

    Класс строки отчета в отдельный файл

--- Source/Applications/Desktop/VodovozViewModels/Transport/Reports/IncorrectFuel/IncorrectFuelReport.cs
+++ Source/Applications/Desktop/VodovozViewModels/Transport/Reports/IncorrectFuel/IncorrectFuelReport.cs
@@ -13,7 +13,6 @@ using Vodovoz.Domain.Employees;
 using Vodovoz.Domain.Fuel;
 using Vodovoz.Domain.Logistic;
 using Vodovoz.Domain.Logistic.Cars;
-using Vodovoz.Extensions;
 using Vodovoz.Presentation.ViewModels.Reports;
 
 namespace Vodovoz.ViewModels.Transport.Reports.IncorrectFuel
@@ -145,29 +144,5 @@ namespace Vodovoz.ViewModels.Transport.Reports.IncorrectFuel
 
 			return report;
 		}
-	}
-
-	public class IncorrectFuelReportRow
-	{
-		public int RowNumber { get; set; }
-		public string CarRegNumber { get; set; }
-		public CarOwnType? CarOwnType { get; set; }
-		public CarTypeOfUse? CarTypeOfUse { get; set; }
-		public string CarModel { get; set; }
-		public EmployeeCategory? DriverCategory { get; set; }
-		public string DriverName { get; set; }
-		public string FuelCardNumber { get; set; }
-		public string CarFuelType { get; set; }
-		public int TransactionId { get; set; }
-		public string TransactionFuelType { get; set; }
-		public string TransactionFuelId { get; set; }
-		public decimal TransactionLitersAmount { get; set; }
-		public DateTime TransactionDateTime { get; set; }
-
-		public string CarOwnTypeString => CarOwnType?.GetEnumDisplayName();
-		public string CarTypeOfUseString => CarTypeOfUse?.GetEnumDisplayName();
-		public string DriverCategoryString => DriverCategory?.GetEnumDisplayName();
-		public string TransactionLitersAmountString => TransactionLitersAmount.ToString("F2");
-		public string TransactionDateTimeString => TransactionDateTime.ToString("dd.MM.yyyy\nHH:mm:ss");
 	}
 }
