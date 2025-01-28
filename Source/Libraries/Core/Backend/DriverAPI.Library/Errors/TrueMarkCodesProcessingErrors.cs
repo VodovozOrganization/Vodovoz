0bdaebea75ddc47cedd719282a96117ee078aaa0
N-Sem <nikiforov.semen@gmail.com>
2025-01-28 12:46:17 +0300

    Добавлены ошибки обработки кодов

---
+++ Source/Libraries/Core/Backend/DriverAPI.Library/Errors/TrueMarkCodesProcessingErrors.cs
@@ -0,0 +1,34 @@
+﻿using Vodovoz.Errors;
+
+namespace DriverAPI.Library.Errors
+{
+	/// <summary>
+	/// Ошибки обработки кодов ЧЗ в DriverApi
+	/// </summary>
+	public static class TrueMarkCodesProcessingErrors
+	{
+		/// <summary>
+		/// Количество переданных кодов ЧЗ для заказ не равно количеству ранее сохраненных кодов
+		/// </summary>
+		public static Error AddedAndSavedCodesCountNotEquals =>
+			new Error(typeof(TrueMarkCodesProcessingErrors),
+				nameof(AddedAndSavedCodesCountNotEquals),
+				"Количество переданных кодов ЧЗ для заказ не равно количеству ранее сохраненных кодов");
+
+		/// <summary>
+		/// Переданные коды ЧЗ для заказа отличаются от ранее сохраненных кодов
+		/// </summary>
+		public static Error AddedAndSavedCodesNotEquals =>
+			new Error(typeof(TrueMarkCodesProcessingErrors),
+				nameof(AddedAndSavedCodesNotEquals),
+				"Переданные коды ЧЗ для заказа отличаются от ранее сохраненных кодов");
+
+		/// <summary>
+		/// Не все коды ЧЗ для заказа были добавлены
+		/// </summary>
+		public static Error NotAllCodesAdded =>
+			new Error(typeof(TrueMarkCodesProcessingErrors),
+				nameof(NotAllCodesAdded),
+				"Не все коды ЧЗ для заказа были добавлены");
+	}
+}
