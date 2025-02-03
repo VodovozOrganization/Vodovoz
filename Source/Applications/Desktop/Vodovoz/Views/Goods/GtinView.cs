60bab42ddae12401eba400b41e9d56b7c5289c99
Art8m <9artbe@gmail.com>
2025-02-03 15:27:39 +0300

    Gtins

--- Source/Applications/Desktop/Vodovoz/Views/Goods/GtinView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/GtinView.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using QS.Views.Dialog;
+using QSWidgetLib;
+using Vodovoz.ViewModels.ViewModels.Goods;
+
 namespace Vodovoz.Views.Goods
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class GtinView : Gtk.Bin
+	public partial class GtinView : DialogViewBase<GtinViewModel>
 	{
-		public GtinView()
+		public GtinView(GtinViewModel viewModel) : base(viewModel)
 		{
-			this.Build();
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			validatedGtin.ValidationMode = ValidationType.numeric;
+			validatedGtin.Binding.AddBinding(ViewModel.Gtin, g => g.GtinNumber, w => w.Text).InitializeFromSource();
+
+			buttonSave.BindCommand(ViewModel.CloseCommand);
+			//buttonCancel.Clicked += (sender, args) => ViewModel.Close(true, CloseSource.Cancel);
 		}
 	}
 }
