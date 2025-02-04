0ebdde214eafaf27cc48d19b288f7ccad3d3a574
N-Sem <nikiforov.semen@gmail.com>
2025-02-04 16:38:11 +0300

    Добавлен новый проект, перенесен функционал

---
+++ Source/Libraries/Core/Backend/DriverApi.Notifications.Client/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using DriverApi.Notifications.Client.Clients;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+using Vodovoz.NotificationRecievers;
+using Vodovoz.Settings.Logistics;
+
+namespace DriverApi.Notifications.Client
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddDriverApiHelper(this IServiceCollection services)
+		{
+			services
+				.AddHttpClient(nameof(DriverApiNotificationsClient), (serviceProvider, client) =>
+				{
+					var databaseSettings = serviceProvider.GetRequiredService<IDriverApiSettings>();
+
+					client.BaseAddress = databaseSettings.ApiBase;
+					client.DefaultRequestHeaders.Accept.Clear();
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				});
+
+			services
+				.AddScoped<ISmsPaymentStatusNotificationReciever, DriverApiNotificationsClient>()
+				.AddScoped<IFastDeliveryOrderAddedNotificationReciever, DriverApiNotificationsClient>()
+				.AddScoped<IWaitingTimeChangedNotificationReciever, DriverApiNotificationsClient>()
+				.AddScoped<ICashRequestForDriverIsGivenForTakeNotificationReciever, DriverApiNotificationsClient>()
+				.AddScoped<IRouteListTransferhandByHandReciever, DriverApiNotificationsClient>();
+
+			return services;
+		}
+	}
+}
