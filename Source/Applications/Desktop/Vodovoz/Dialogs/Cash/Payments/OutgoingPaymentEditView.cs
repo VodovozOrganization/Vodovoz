5633de863a864c0c2f5dcbaeb5aee9a0ff2b0434
Sergey Efimov <efimov90@gmail.com>
2025-02-05 13:03:36 +0300

    UI
    Fix

---
+++ Source/Applications/Desktop/Vodovoz/Dialogs/Cash/Payments/OutgoingPaymentEditView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Dialogs.Cash.Payments
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OutgoingPaymentEditView : Gtk.Bin
+	{
+		public OutgoingPaymentEditView()
+		{
+			this.Build();
+		}
+	}
+}
