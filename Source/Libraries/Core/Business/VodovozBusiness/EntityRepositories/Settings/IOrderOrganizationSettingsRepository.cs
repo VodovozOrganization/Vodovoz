c8753465faf7a58368c83151d6bb3be620a7e295
Andrey <as1854@yandex.ru>
2025-02-06 11:41:50 +0300

    5114 добавил логику по добавлению нового источника в настройки

---
+++ Source/Libraries/Core/Business/VodovozBusiness/EntityRepositories/Settings/IOrderOrganizationSettingsRepository.cs
@@ -0,0 +1,10 @@
+﻿using QS.DomainModel.UoW;
+using VodovozBusiness.Domain.Orders;
+
+namespace Vodovoz.EntityRepositories.Settings
+{
+	public interface IOrderOrganizationSettingsRepository
+	{
+		OnlinePaymentTypeOrganizationSettings GetOnlinePaymentTypeOrganizationSettings(IUnitOfWork uow);
+	}
+}
---
+++ Source/Libraries/Infrastructure/Vodovoz.Infrastructure.Persistance/Settings/OrderOrganizationSettingsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using QS.DomainModel.UoW;
+using Vodovoz.EntityRepositories.Settings;
+using VodovozBusiness.Domain.Orders;
+
+namespace Vodovoz.Infrastructure.Persistance.Settings
+{
+	public class OrderOrganizationSettingsRepository : IOrderOrganizationSettingsRepository
+	{
+		public OnlinePaymentTypeOrganizationSettings GetOnlinePaymentTypeOrganizationSettings(IUnitOfWork uow)
+		{
+			return
+				(from settings in uow.Session.Query<OnlinePaymentTypeOrganizationSettings>()
+					select settings).SingleOrDefault();
+		}
+	}
+}
