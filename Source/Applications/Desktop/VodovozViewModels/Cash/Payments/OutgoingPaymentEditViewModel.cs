3b673cec8a34484dd6d565729cecc7199f6eda8b
Sergey Efimov <efimov90@gmail.com>
2025-02-06 09:34:48 +0300

    Фикс

--- Source/Applications/Desktop/VodovozViewModels/Cash/Payments/OutgoingPaymentEditViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Cash/Payments/OutgoingPaymentEditViewModel.cs
@@ -1,13 +1,18 @@
-﻿using QS.Commands;
+﻿using DocumentFormat.OpenXml.Bibliography;
+using QS.Commands;
+using QS.DomainModel.Entity;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
 using QS.Services;
 using QS.ViewModels;
 using QS.ViewModels.Control.EEVM;
+using Vodovoz.CachingRepositories.Common;
+using Vodovoz.Domain.Cash;
 using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Organizations;
 using Vodovoz.ViewModels.Extensions;
-using VodovozBusiness.Domain.Cash.Payments;
+using VodovozBusiness.Domain.Payments;
 
 namespace Vodovoz.ViewModels.Cash.Payments
 {
@@ -22,9 +27,26 @@ namespace Vodovoz.ViewModels.Cash.Payments
 			INavigationManager navigation)
 			: base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
 		{
+			PaymentDate = Entity.PaymentDate.ToString("d");
+
+			if(Entity.OrganizationId.HasValue)
+			{
+				OrganizationName = UoW.GetById<Organization>(Entity.OrganizationId.Value)?.FullName;
+			}
+
+			CashlessRequestId = Entity.CashlessRequestId.ToString();
+			PaymentNumber = Entity.PaymentNumber.ToString();
+			Sum = Entity.Sum.ToString("C");
+
 			SaveCommand = new DelegateCommand(SaveAndClose, () => PermissionResult.CanUpdate);
 			CancelCommand = new DelegateCommand(() => Close(HasChanges && PermissionResult.CanUpdate, CloseSource.Cancel));
 		}
+
+		public string PaymentDate { get; }
+		public string OrganizationName { get; }
+		public string CashlessRequestId { get; }
+		public string PaymentNumber { get; }
+		public string Sum { get; }
 
 		public Counterparty Counterparty
 		{
