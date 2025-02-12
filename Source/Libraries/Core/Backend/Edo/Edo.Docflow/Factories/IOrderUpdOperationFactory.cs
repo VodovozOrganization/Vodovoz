d36f1b5446eacb9c8e8e213920af8960a3a1eeb2
N-Sem <nikiforov.semen@gmail.com>
2025-02-12 08:44:52 +0300

    Перенос фабрики

---
+++ Source/Libraries/Core/Backend/Edo/Edo.Docflow/Factories/IOrderUpdOperationFactory.cs
@@ -0,0 +1,10 @@
+﻿using Vodovoz.Core.Domain.Edo;
+using Vodovoz.Core.Domain.Orders;
+
+namespace Edo.Docflow.Factories
+{
+	public interface IOrderUpdOperationFactory
+	{
+		OrderUpdOperation CreateOrUpdateOrderUpdOperation(OrderEntity order);
+	}
+}
