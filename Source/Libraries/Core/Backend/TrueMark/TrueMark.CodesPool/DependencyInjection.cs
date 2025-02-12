42bbf8ca5b0967a1d2037c33b9b9407cded5ef66
Vadim Gordeev <enzogord@gmail.com>
2025-02-12 22:37:23 +0300

    Выделенный проект пула кодов

---
+++ Source/Libraries/Core/Backend/TrueMark/TrueMark.CodesPool/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TrueMark.CodesPool
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddCodesPool(this IServiceCollection services)
+		{
+			services
+				.AddScoped<TrueMarkCodesPool>()
+				.AddScoped<TrueMarkCodesPoolManager>()
+				.AddScoped<TrueMarkCodesPoolFactory>()
+				;
+
+			return services;
+		}
+	}
+}
---
+++ Source/Libraries/Core/Backend/TrueMark/TrueMark.CodesPool/TrueMarkCodesPool.cs
@@ -0,0 +1,91 @@
+﻿using MySqlConnector;
+using NHibernate;
+using QS.DomainModel.UoW;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TrueMark.CodesPool
+{
+	/// <summary>
+	/// Предоставляет возможность добавлять и забирать коды из пула кодов 
+	/// </summary>
+	public class TrueMarkCodesPool
+	{
+		private const string _poolTableName = "truemark_codes_pool";
+
+		private readonly IUnitOfWork _uow;
+
+		public TrueMarkCodesPool(IUnitOfWork uow)
+		{
+			_uow = uow ?? throw new ArgumentNullException(nameof(uow));
+			_uow.OpenTransaction();
+		}
+
+		public virtual void PutCode(int codeId)
+		{
+			var query = GetPutCodeQuery(codeId);
+			try
+			{
+				query.ExecuteUpdate();
+			}
+			catch(MySqlException ex) when (ex.Number == (int)MySqlErrorCode.DuplicateKeyEntry)
+			{
+				return;
+			}
+		}
+
+		public virtual async Task PutCodeAsync(int codeId, CancellationToken cancellationToken)
+		{
+			var query = GetPutCodeQuery(codeId);
+			try
+			{
+				await query.ExecuteUpdateAsync(cancellationToken);
+			}
+			catch(MySqlException ex) when(ex.Number == (int)MySqlErrorCode.DuplicateKeyEntry)
+			{
+				return;
+			}
+		}
+
+		private IQuery GetPutCodeQuery(int codeId)
+		{
+			var sql = $@"INSERT INTO {_poolTableName} (code_id) VALUES(:code_id);";
+			var query = _uow.Session.CreateSQLQuery(sql)
+				.SetParameter("code_id", codeId);
+			return query;
+		}
+
+		public virtual int TakeCode(string gtin)
+		{
+			var query = GetTakeCodeQuery(gtin);
+			var codeId = query.UniqueResult<uint>();
+			return (int)codeId;
+		}
+
+		public virtual async Task<int> TakeCode(string gtin, CancellationToken cancellationToken)
+		{
+			var query = GetTakeCodeQuery(gtin);
+			var codeId = await query.UniqueResultAsync<uint>(cancellationToken);
+			return (int)codeId;
+		}
+
+		private IQuery GetTakeCodeQuery(string gtin)
+		{
+			var sql = $@"
+				SELECT pool.id
+				FROM {_poolTableName} pool
+					INNER JOIN true_mark_identification_code code ON code.id = pool.code_id 
+				WHERE pool.promoted 
+					AND code.gtin = :gtin
+				ORDER BY pool.adding_time DESC 
+				LIMIT 1
+				FOR UPDATE SKIP LOCKED
+				;";
+
+			var query = _uow.Session.CreateSQLQuery(sql)
+				.SetParameter("gtin", gtin);
+			return query;
+		}
+	}
+}
