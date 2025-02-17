d9db016913c37d7a147469cc6dfe4dda6ed390a0
Art8m <9artbe@gmail.com>
2025-02-17 17:02:38 +0300

    неустойка

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/NonReturnReason.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/NonReturnReason.cs
@@ -1,4 +1,4 @@
-﻿using QS.DomainModel.Entity;
+using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
 using QS.HistoryLog;
 
@@ -13,5 +13,10 @@ namespace Vodovoz.Domain.Orders
 	{
 		public virtual int Id { get; set; }
 		public virtual string Name { get; set; }
+		
+		/// <summary>
+		/// Необходимо начислять неустойку
+		/// </summary>
+		public virtual bool NeedForfeit { get; set; }
 	}
 }
--- Source/Vodovoz.Data.NHibernate/HibernateMapping/Order/NonReturnReasonMap.cs
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Order/NonReturnReasonMap.cs
@@ -10,6 +10,7 @@ namespace Vodovoz.Data.NHibernate.HibernateMapping.Order
 			Table("non_return_reasons");
 			Id(x => x.Id).Column("id").GeneratedBy.Native();
 			Map(x => x.Name).Column("name");
+			Map(x => x.NeedForfeit).Column("need_forfeit");
 		}
 	}
 }
