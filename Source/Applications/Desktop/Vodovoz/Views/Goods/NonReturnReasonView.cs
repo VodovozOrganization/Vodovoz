2f8a37b059f946b0f9bbd880dac814391ac48d92
Art8m <9artbe@gmail.com>
2025-02-17 06:10:31 -0800

    stetic

---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/NonReturnReasonView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class NonReturnReasonView : Gtk.Bin
+	{
+		public NonReturnReasonView()
+		{
+			this.Build();
+		}
+	}
+}
