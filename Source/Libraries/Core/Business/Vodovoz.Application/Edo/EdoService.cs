f18c39f3b0cdf3e58f4eb6bb65b381de51e153da
Sergey Efimov <efimov90@gmail.com>
2025-02-26 15:15:18 +0300

    Temp

---
+++ Source/Libraries/Core/Business/Vodovoz.Application/Edo/EdoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gamma.Utilities;
+using Vodovoz.Core.Domain.Clients;
+using Vodovoz.Core.Domain.Common;
+using Vodovoz.Core.Domain.Edo;
+using Vodovoz.Domain.Client;
+
+namespace Vodovoz.Application.Edo
+{
+	internal class EdoService
+	{
+
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Clients/Specifications/CounterpartySpecification.cs
@@ -0,0 +1,23 @@
+﻿using Core.Infrastructure.Specifications;
+using System;
+using System.Linq.Expressions;
+
+namespace Vodovoz.Core.Domain.Clients.Specifications
+{
+	public class CounterpartySpecification : ExpressionSpecification<CounterpartyEntity>
+	{
+		public CounterpartySpecification(Expression<Func<CounterpartyEntity, bool>> expression) : base(expression)
+		{
+		}
+
+		public static CounterpartySpecification ById(int id)
+		{
+			return new CounterpartySpecification(x => x.Id == id);
+		}
+
+		public static CounterpartySpecification NonEmptyInn()
+		{
+			return new CounterpartySpecification(x => !string.IsNullOrWhiteSpace(x.INN));
+		}
+	}
+}
