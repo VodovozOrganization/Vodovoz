35e0ddcdecc0483a2f7c60eccbe3a0e42e6b9007
Sergey Efimov <efimov90@gmail.com>
2025-02-26 11:24:12 +0300

    Dto изменение

---
+++ Source/Libraries/Core/Backend/WarehouseApi.Contracts/Dto/WarehouseApiTruemarkCodeLevel.cs
@@ -0,0 +1,21 @@
+﻿namespace WarehouseApi.Contracts.Dto
+{
+	/// <summary>
+	/// Уровень кода маркировки ЧЗ
+	/// </summary>
+	public enum WarehouseApiTruemarkCodeLevel
+    {
+        /// <summary>
+        /// Транспортный код
+        /// </summary>
+        transport,
+        /// <summary>
+        /// Групповой код
+        /// </summary>
+        group,
+        /// <summary>
+        /// Единичный код
+        /// </summary>
+        unit
+    }
+}
