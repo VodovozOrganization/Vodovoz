4b4e35f205ca5d50375d4124fe2c72731efe1682
Art8m <9artbe@gmail.com>
2025-02-28 03:04:04 -0800

    Ститик

---
+++ Source/Applications/Desktop/Vodovoz/Views/Documents/CodesScanView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Documents
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CodesScanView : Gtk.Bin
+	{
+		public CodesScanView()
+		{
+			this.Build();
+		}
+	}
+}
