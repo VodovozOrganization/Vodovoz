d728d8abc5510c990b676bf3bfb3953f6dbee121
Sergey Efimov <efimov90@gmail.com>
2025-02-28 11:25:17 +0300

    Semi-Fix

--- Source/Libraries/Core/Backend/WarehouseApi.Contracts/Dto/WarehouseApiTruemarkCodeLevel.cs
+++ Source/Libraries/Core/Backend/WarehouseApi.Contracts/Dto/WarehouseApiTruemarkCodeLevel.cs
@@ -1,21 +1,24 @@
-﻿namespace WarehouseApi.Contracts.Dto
+﻿using System.Text.Json.Serialization;
+
+namespace WarehouseApi.Contracts.Dto
 {
 	/// <summary>
 	/// Уровень кода маркировки ЧЗ
 	/// </summary>
+	[JsonConverter(typeof(JsonStringEnumConverter))]
 	public enum WarehouseApiTruemarkCodeLevel
-    {
-        /// <summary>
-        /// Транспортный код
-        /// </summary>
-        transport,
-        /// <summary>
-        /// Групповой код
-        /// </summary>
-        group,
-        /// <summary>
-        /// Единичный код
-        /// </summary>
-        unit
-    }
+	{
+		/// <summary>
+		/// Транспортный код
+		/// </summary>
+		transport,
+		/// <summary>
+		/// Групповой код
+		/// </summary>
+		group,
+		/// <summary>
+		/// Единичный код
+		/// </summary>
+		unit
+	}
 }
