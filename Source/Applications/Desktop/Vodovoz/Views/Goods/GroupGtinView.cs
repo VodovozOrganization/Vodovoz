89af82d59240f29b73f38540358930d5755e96b9
N-Sem <nikiforov.semen@gmail.com>
2025-03-03 16:32:01 +0300

    Диалог группового gtin

---
+++ Source/Applications/Desktop/Vodovoz/Views/Goods/GroupGtinView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Goods
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class GroupGtinView : Gtk.Bin
+	{
+		public GroupGtinView()
+		{
+			this.Build();
+		}
+	}
+}
