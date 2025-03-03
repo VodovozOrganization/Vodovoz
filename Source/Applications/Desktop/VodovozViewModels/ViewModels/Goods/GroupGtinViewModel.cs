2c90aeb9088314802e49fbe620d16b20840cb310
N-Sem <nikiforov.semen@gmail.com>
2025-03-03 17:19:31 +0300

    Добавлена вью-модель

---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Goods/GroupGtinViewModel.cs
@@ -0,0 +1,21 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Services;
+using QS.ViewModels;
+using VodovozBusiness.Domain.Goods;
+
+namespace Vodovoz.ViewModels.ViewModels.Goods
+{
+	public class GroupGtinViewModel : EntityTabViewModelBase<GroupGtin>
+	{
+		public GroupGtinViewModel(
+			IEntityUoWBuilder uowBuilder,
+			IUnitOfWorkFactory unitOfWorkFactory,
+			ICommonServices commonServices,
+			INavigationManager navigation)
+			: base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
+		{
+		}
+	}
+}
