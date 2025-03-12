2ef7c3d7d38752faadc66cbf45955224d30b3ec4
Sergey Efimov <efimov90@gmail.com>
2025-03-12 16:33:01 +0300

    Fix

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Validation/IValidationResultFactory.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Validation/IValidationResultFactory.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vodovoz.Core.Domain.Validation
 {
 	public interface IValidationResultFactory<TEntity>
 		where TEntity : class
 	{
+		ValidationResult CreateForDateNotInRange(string propertyName, DateTime minimalDate, DateTime maximalDate, DateTime? date);
 		ValidationResult CreateForNullProperty(string propertyName);
 	}
 }
--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Validation/ValidationResultFactory.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Validation/ValidationResultFactory.cs
@@ -18,21 +18,48 @@ namespace Vodovoz.Core.Domain.Validation
 
 		public ValidationResult CreateForNullProperty(string propertyName)
 		{
-			var propertyDisplayName = typeof(TEntity)
+			var propertyDisplayName = GetPropertyDisplayName(propertyName);
+
+			_logger.LogWarning($"Ошибка валидации: Не заполнено свойство {propertyDisplayName}");
+
+			return new ValidationResult($"Не заполнено свойство {propertyDisplayName}", new[] { propertyDisplayName });
+		}
+
+		public ValidationResult CreateForDateNotInRange(string propertyName, DateTime minimalDate, DateTime maximalDate, DateTime? date)
+		{
+			var propertyDisplayName = GetPropertyDisplayName(propertyName);
+
+			_logger.LogWarning($"Ошибка валидации: Указанная в свойстве {propertyDisplayName} дата {date:g} находится вне диапазона {minimalDate:g} - {maximalDate:g}");
+
+			return new ValidationResult($"Указанная в свойстве {propertyDisplayName} дата {date:g} находится вне диапазона {minimalDate:g} - {maximalDate:g}", new[] { propertyDisplayName });
+		}
+
+		private string GetPropertyDisplayName(string propertyName)
+		{
+			if(TryGetPropertyDisplayName(propertyName, out var propertyDisplayName))
+			{
+				return propertyDisplayName;
+			}
+
+			var exception = new ArgumentException("Некорректное значение названия свойства или не установлен аттрибут DisplayAttribute с параметром Name");
+
+			_logger.LogCritical(exception, "Ошибка настройки валидации");
+
+			throw exception;
+		}
+
+		private bool TryGetPropertyDisplayName(string propertyName, out string propertyDisplayName)
+		{
+			propertyDisplayName = typeof(TEntity)
 				.GetProperty(propertyName)?
 				.GetCustomAttribute<DisplayAttribute>()?.Name;
 
 			if(string.IsNullOrWhiteSpace(propertyDisplayName))
 			{
-				var exception = new ArgumentException("Некорректное значение названия свойства или не установлен аттрибут DisplayAttribute с параметром Name");
-
-				_logger.LogCritical(exception, "Ошибка настройки валидации");
-				throw exception;
+				return false;
 			}
 
-			_logger.LogWarning($"Ошибка валидации: Не заполнено свойство {propertyDisplayName}");
-
-			return new ValidationResult($"Не заполнено свойство {propertyDisplayName}", new[] { propertyDisplayName });
+			return true;
 		}
 	}
 }
