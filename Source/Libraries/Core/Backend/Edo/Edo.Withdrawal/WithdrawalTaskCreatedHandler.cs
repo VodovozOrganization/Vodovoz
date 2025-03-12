7041cf3c3d66098dd1afcea8573a484f8a609c31
N-Sem <nikiforov.semen@gmail.com>
2025-03-12 10:32:18 +0300

    Добавлен обработчик

---
+++ Source/Libraries/Core/Backend/Edo/Edo.Withdrawal/WithdrawalTaskCreatedHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using QS.DomainModel.UoW;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Vodovoz.Core.Domain.Edo;
+
+namespace Edo.Withdrawal
+{
+	public class WithdrawalTaskCreatedHandler : IDisposable
+	{
+		private readonly IUnitOfWork _uow;
+		private readonly ILogger<WithdrawalTaskCreatedHandler> _logger;
+		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+		public WithdrawalTaskCreatedHandler(
+			ILogger<WithdrawalTaskCreatedHandler> logger,
+			IUnitOfWorkFactory unitOfWorkFactory)
+		{
+			_logger =
+				logger ?? throw new ArgumentNullException(nameof(logger));
+			_uow =
+				(unitOfWorkFactory ?? throw new ArgumentNullException(nameof(unitOfWorkFactory)))
+				.CreateWithoutRoot(nameof(WithdrawalTaskCreatedHandler));
+		}
+
+		public async Task HandleWithdrawal(int withdrawalEdoTaskId, CancellationToken cancellationToken)
+		{
+			var withdrawalEdoTask = await _uow.Session.GetAsync<WithdrawalEdoTask>(withdrawalEdoTaskId, cancellationToken);
+		}
+
+		public void Dispose()
+		{
+			_uow?.Dispose();
+		}
+	}
+}
