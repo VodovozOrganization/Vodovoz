50d1e476ecb7553419e83b34eec6478144d16a58
N-Sem <nikiforov.semen@gmail.com>
2025-03-13 11:01:03 +0300

    Исправлены запросы

--- Source/Libraries/Infrastructure/Vodovoz.Infrastructure.Persistance/Orders/OrderRepository.cs
+++ Source/Libraries/Infrastructure/Vodovoz.Infrastructure.Persistance/Orders/OrderRepository.cs
@@ -1617,13 +1617,20 @@ namespace Vodovoz.Infrastructure.Persistance.Orders
 				mainQuery.Where(additionalParametersRestriction);
 			}
 
-			if(orderOnDayFilters.IsCodesScanInWarehouseRequired)
-			{
-				mainQuery
-					.Where(() => orderAlias.PaymentType == PaymentType.Cashless)
-					.Where(() =>
-						clientAlias.ConsentForEdoStatus == ConsentForEdoStatus.Agree
-						&& clientAlias.OrderStatusForSendingUpd == OrderStatusForSendingUpd.EnRoute);
+				if(orderOnDayFilters.FastDeliveryEnabled)
+				{
+					additionalParametersRestriction.Add(() => orderAlias.IsFastDelivery);
+				}
+
+				if(orderOnDayFilters.IsCodesScanInWarehouseRequired)
+				{
+					additionalParametersRestriction.Add(Restrictions.Conjunction()
+						.Add(() => orderAlias.PaymentType == PaymentType.Cashless)
+						.Add(() => clientAlias.ConsentForEdoStatus == ConsentForEdoStatus.Agree)
+						.Add(() => clientAlias.OrderStatusForSendingUpd == OrderStatusForSendingUpd.EnRoute));
+				}
+
+				mainQuery.Where(additionalParametersRestriction);
 			}
 
 			mainQuery.WhereRestrictionOn(() => orderAlias.OrderAddressType)
