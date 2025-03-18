7e24978175643b01f6466ec245e636305a4d78fb
Sergey Efimov <efimov90@gmail.com>
2025-03-18 12:16:47 +0300

    Temp

---
+++ Source/Libraries/Core/Backend/Pacs/Pacs.Server/Operators/WorkShiftEndArgs.cs
@@ -0,0 +1,11 @@
+﻿using Vodovoz.Core.Domain.Pacs;
+
+namespace Pacs.Server.Operators
+{
+	public class WorkShiftEndArgs
+	{
+		public WorkShiftChangedBy WorkShiftChangedBy { get; set; }
+		public int? AdminId { get; set; }
+		public string Reason { get; set; }
+	}
+}
