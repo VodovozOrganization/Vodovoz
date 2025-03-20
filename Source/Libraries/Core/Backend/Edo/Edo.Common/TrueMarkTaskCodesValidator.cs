26b39eff265defa5e111e424776404f83684bcaa
Art8m <9artbe@gmail.com>
2025-03-20 03:33:45 +0300

    Доработка валидации агрегатных кодов

--- Source/Libraries/Core/Backend/Edo/Edo.Common/TrueMarkTaskCodesValidator.cs
+++ Source/Libraries/Core/Backend/Edo/Edo.Common/TrueMarkTaskCodesValidator.cs
@@ -153,5 +153,35 @@ namespace Edo.Common
 		
 			return new TrueMarkTaskValidationResult(codeResults);
 		}
+
+		public async Task<TrueMarkTaskValidationResult> ValidateAsync(IEnumerable<string> codes, string organizationInn, CancellationToken cancellationToken)
+		{
+			var gtins = await _edoRepository.GetGtinsAsync(cancellationToken);
+			var gtinNumbers = gtins.Select(x => x.GtinNumber);
+
+			var edoOrganizations = await _edoRepository.GetEdoOrganizationsAsync(cancellationToken);
+			var ourOrganizationInns = edoOrganizations.Select(x => x.INN);
+
+			var productInstancesInfo = await _trueMarkApiClient.GetProductInstanceInfoAsync(codes, cancellationToken);
+
+			if(productInstancesInfo?.InstanceStatuses is null)
+			{
+				throw new Exception(productInstancesInfo?.ErrorMessage ?? "Не удалось получить информацию о коде в ЧЗ");
+			}
+
+			var codeResults = new List<TrueMarkCodeValidationResult>();
+
+			foreach(var productInstanceStatus in productInstancesInfo.InstanceStatuses)
+			{
+				var code = codes.Where(x => x == productInstanceStatus.IdentificationCode).First();
+
+				var codeResult = CreateCodeValidationResult(productInstanceStatus, gtinNumbers, ourOrganizationInns, organizationInn);
+				codeResult.CodeString = code;
+
+				codeResults.Add(codeResult);
+			}
+
+			return new TrueMarkTaskValidationResult(codeResults);
+		}
 	}
 }
