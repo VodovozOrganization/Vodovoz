925bd0b0d5a8f008b1634bc5871585a9c6e13059
N-Sem <nikiforov.semen@gmail.com>
2025-03-20 16:40:54 +0300

    Добавлен вью нового диалога

---
+++ Source/Applications/Desktop/Vodovoz/Views/TrueMark/CodesPoolView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.TrueMark
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CodesPoolView : Gtk.Bin
+	{
+		public CodesPoolView()
+		{
+			this.Build();
+		}
+	}
+}
