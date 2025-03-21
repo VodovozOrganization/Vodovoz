9e0e886bdd05b35f603f2e7ea913be746d1fb7ab
N-Sem <nikiforov.semen@gmail.com>
2025-03-21 17:04:16 +0300

    Окраска строк

--- Source/Applications/Desktop/Vodovoz/Views/TrueMark/CodesPoolView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/TrueMark/CodesPoolView.cs
@@ -1,11 +1,18 @@
 ﻿using Gamma.GtkWidgets;
+using Gdk;
+using Gtk;
 using QS.Views.GtkUI;
+using Vodovoz.Infrastructure;
 using Vodovoz.ViewModels.TrueMark.CodesPool;
 using static Vodovoz.ViewModels.TrueMark.CodesPool.CodesPoolViewModel;
+
 namespace Vodovoz.Views.TrueMark
 {
 	public partial class CodesPoolView : TabViewBase<CodesPoolViewModel>
 	{
+		private readonly Color _isNotEnoughCodesColor = GdkColors.DangerBase;
+		private readonly Color _baseColor = GdkColors.PrimaryBase;
+
 		public CodesPoolView(CodesPoolViewModel viewModel) : base(viewModel)
 		{
 			Build();
@@ -19,6 +26,10 @@ namespace Vodovoz.Views.TrueMark
 				.AddColumn("Количество в пуле").AddTextRenderer(node => node.CountInPool.ToString())
 				.AddColumn("Продано вчера").AddTextRenderer(node => node.SoldYesterday.ToString())
 				.AddColumn("Номенклатуры").AddTextRenderer(node => node.Nomenclatures)
+				.RowCells().AddSetter<CellRenderer>((cell, node) =>
+				{
+					cell.CellBackgroundGdk = node.IsNotEnoughCodes ? _isNotEnoughCodesColor : _baseColor;
+				})
 				.Finish();
 
 			ytreeviewData.Binding
