e7097ca24c8852356aaff3379ff957bebac6a133
N-Sem <nikiforov.semen@gmail.com>
2025-03-21 16:14:25 +0300

    Докблоки

--- Source/Libraries/Core/Business/VodovozBusiness/EntityRepositories/TrueMark/ITrueMarkRepository.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/EntityRepositories/TrueMark/ITrueMarkRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Vodovoz.Core.Domain.Edo;
-using Vodovoz.Domain.Goods;
 
 namespace Vodovoz.EntityRepositories.TrueMark
 {
@@ -16,7 +15,21 @@ namespace Vodovoz.EntityRepositories.TrueMark
 		ISet<string> GetAllowedCodeOwnersGtins();
 
 		IEnumerable<TrueMarkWaterIdentificationCode> GetTrueMarkCodeDuplicates(IUnitOfWork uow, string gtin, string serialNumber, string checkCode);
+
+		/// <summary>
+		/// Получить все значения Gtin в базе с названиями номенклатур
+		/// </summary>
+		/// <param name="uow">UnitOfWork</param>
+		/// <param name="cancellationToken">CancellationToken</param>
+		/// <returns>Словарь. Ключ - gtin, значение - список названий номенклатур с данным gtin</returns>
 		Task<IDictionary<string, List<string>>> GetGtinsNomenclatureData(IUnitOfWork uow, CancellationToken cancellationToken);
+
+		/// <summary>
+		/// Получить все значения Gtin товаров, проданных за предыдущий день, и их количество
+		/// </summary>
+		/// <param name="uow">UnitOfWork</param>
+		/// <param name="cancellationToken">CancellationToken</param>
+		/// <returns>Словарь. Ключ - gtin, значение - количество единиц товаров с данным gtin</returns>
 		Task<IDictionary<string, int>> GetSoldYesterdayGtinsCount(IUnitOfWork uow, CancellationToken cancellationToken);
 	}
 }
