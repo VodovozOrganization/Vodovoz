764176f52d73b82fd8bb3ed4bd618f6b3f0a339b
Vadim Gordeev <enzogord@gmail.com>
2025-03-24 10:52:34 +0300

    Добавление в Edo.ErrorDebug.Worker

---
+++ Source/Applications/Backend/Workers/Docker/Edo/Edo.Transfer.Dispatcher.ErrorDebugWorker/Consumers/TransferDocumentAcceptedErrorConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Edo.Contracts.Messages.Events;
+using Edo.Transfer.Dispatcher;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Edo.Receipt.Dispatcher.ErrorDebug.Consumers
+{
+	public class TransferDocumentAcceptedErrorConsumer : IConsumer<Batch<TransferDocumentAcceptedEvent>>
+	{
+		private readonly TransferEdoHandler _transferEdoHandler;
+
+		public TransferDocumentAcceptedErrorConsumer(
+			TransferEdoHandler transferEdoHandler)
+		{
+			_transferEdoHandler = transferEdoHandler ?? throw new ArgumentNullException(nameof(transferEdoHandler));
+		}
+
+		public async Task Consume(ConsumeContext<Batch<TransferDocumentAcceptedEvent>> context)
+		{
+			foreach(var batchItem in context.Message)
+			{
+				var msg = batchItem.Message;
+
+				await _transferEdoHandler.HandleTransferDocumentAcceptance(msg.DocumentId, context.CancellationToken);
+			}
+		}
+	}
+}
+
