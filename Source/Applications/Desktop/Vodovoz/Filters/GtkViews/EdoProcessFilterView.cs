d0dc0efe8148d0d1d8a783c302e2b27a0dab9048
Vadim Gordeev <Enzolineage@yandex.ru>
2025-04-01 12:19:53 +0300

    Добавлен фильтр для журнала ЭДО процессов

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/EdoProcessFilterView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using QS.Project.Filter;
+using QS.Views.GtkUI;
+
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoProcessFilterView : FilterViewBase<EdoProcessFilterViewModel>
+	{
+		public EdoProcessFilterView(EdoProcessFilterViewModel filterViewModel) : base(filterViewModel)
+		{
+			this.Build();
+			ConfigureDlg();
+		}
+
+		private void ConfigureDlg()
+		{
+			//entrySubdivision.ViewModel = ViewModel.SubdivisionViewModel;
+
+			//entryAuthor.ViewModel = ViewModel.AuthorViewModel;
+
+			//ydateperiodpickerFineDate.Binding.AddBinding(ViewModel, vm => vm.FineDateStart, w => w.StartDateOrNull).InitializeFromSource();
+			//ydateperiodpickerFineDate.Binding.AddBinding(ViewModel, vm => vm.FineDateEnd, w => w.EndDateOrNull).InitializeFromSource();
+			//ydateperiodpickerFineDate.Binding.AddBinding(ViewModel, vm => vm.CanEditFineDate, w => w.Sensitive).InitializeFromSource();
+
+			//ydateperiodpickerRouteList.Binding.AddBinding(ViewModel, vm => vm.RouteListDateStart, w => w.StartDateOrNull).InitializeFromSource();
+			//ydateperiodpickerRouteList.Binding.AddBinding(ViewModel, vm => vm.RouteListDateEnd, w => w.EndDateOrNull).InitializeFromSource();
+			//ydateperiodpickerRouteList.Binding.AddBinding(ViewModel, vm => vm.CanEditRouteListDate, w => w.Sensitive).InitializeFromSource();
+		}
+	}
+
+	public class EdoProcessFilterViewModel : FilterViewModelBase<EdoProcessFilterViewModel>
+	{
+
+	}
+}
