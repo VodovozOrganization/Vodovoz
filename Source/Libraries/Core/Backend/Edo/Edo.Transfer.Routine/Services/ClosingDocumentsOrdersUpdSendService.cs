9e6083aa5befcbc15ee4896c2cbba3aebff61914
N-Sem <nikiforov.semen@gmail.com>
2025-04-04 09:43:09 +0300

    Поправлен запрос

--- Source/Libraries/Core/Backend/Edo/Edo.Transfer.Routine/Services/ClosingDocumentsOrdersUpdSendService.cs
+++ Source/Libraries/Core/Backend/Edo/Edo.Transfer.Routine/Services/ClosingDocumentsOrdersUpdSendService.cs
@@ -8,10 +8,12 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Vodovoz.Core.Domain.Clients;
+using Vodovoz.Core.Domain.Documents;
 using Vodovoz.Core.Domain.Edo;
 using Vodovoz.Core.Domain.Orders;
 using Vodovoz.Domain.Client;
 using Vodovoz.Settings.Delivery;
+using Type = Vodovoz.Core.Domain.Documents.Type;
 
 namespace Edo.Transfer.Routine.Services
 {
@@ -38,24 +40,64 @@ namespace Edo.Transfer.Routine.Services
 		{
 			var orders = await GetCloseDocumentOrdersToSendEdoRequest(cancellationToken);
 
+			foreach(var order in orders)
+			{
+
+			}
+
 			await Task.CompletedTask;
 		}
 
 		private async Task<IEnumerable<OrderEntity>> GetCloseDocumentOrdersToSendEdoRequest(CancellationToken cancellationToken)
 		{
-			using(var uow = _unitOfWorkFactory.CreateWithoutRoot())
+			using(var uow = _unitOfWorkFactory.CreateWithoutRoot("Получение заказов с \"Закр док\" для отправки УПД"))
 			{
 				var orders =
 					from order in uow.Session.Query<OrderEntity>()
 					join client in uow.Session.Query<CounterpartyEntity>() on order.Client.Id equals client.Id
 					join er in uow.Session.Query<OrderEdoRequest>() on order.Id equals er.Order.Id into edoRequests
 					from edoRequest in edoRequests.DefaultIfEmpty()
+					join ec in uow.Session.Query<EdoContainerEntity>()
+						on new { OrderId = order.Id, DocType = Type.Upd } equals new { OrderId = ec.Order.Id, DocType = ec.Type } into edoContainers
+					from edoContainer in edoContainers.DefaultIfEmpty()
 					where
+					//Проверка, что Закр Док
 					order.PaymentType == PaymentType.Cashless
-					&& order.Client.IsNewEdoProcessing
+					&& client.IsNewEdoProcessing
+					&& client.ConsentForEdoStatus == ConsentForEdoStatus.Agree
+					&& edoContainer.Id == null
+					&& edoRequest.Id == null
 					select order;
 
 				return await orders.ToListAsync(cancellationToken);
+			}
+		}
+
+		private async Task CreateEdoRequests(IEnumerable<OrderEntity> order)
+		{
+			using(var uow = _unitOfWorkFactory.CreateWithoutRoot("Создание запросов на отправку УПД для заказов \"Закр док\""))
+			{
+				//var ordersHavingRequests =
+
+
+				//OrderEdoRequest edoRequest = _uow.Session.Query<OrderEdoRequest>()
+				//.Where(x => x.Order.Id == vodovozOrder.Id)
+				//.Take(1)
+				//.SingleOrDefault();
+
+				//var edoRequestCreated = false;
+				//if(!vodovozOrder.IsNeedIndividualSetOnLoad && edoRequest == null)
+				//{
+				//	edoRequest = CreateEdoRequests(vodovozOrder, routeListAddress);
+				//	edoRequestCreated = true;
+				//}
+
+				//_uow.Commit();
+
+				//if(edoRequestCreated)
+				//{
+				//	await _edoMessageService.PublishEdoRequestCreatedEvent(edoRequest.Id);
+				//}
 			}
 		}
 	}
