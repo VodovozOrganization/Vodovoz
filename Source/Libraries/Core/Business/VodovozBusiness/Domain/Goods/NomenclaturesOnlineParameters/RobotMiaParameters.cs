84d48cc00efb73e1c29f65f6e27f2db03882fb3b
Sergey Efimov <efimov90@gmail.com>
2025-04-09 10:10:15 +0300

    Temp

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Goods/NomenclaturesOnlineParameters/RobotMiaParameters.cs
@@ -0,0 +1,79 @@
+﻿using NHibernate.Util;
+using QS.DomainModel.Entity;
+using QS.Extensions.Observable.Collections.List;
+using System;
+using System.Linq;
+
+namespace VodovozBusiness.Domain.Goods.NomenclaturesOnlineParameters
+{
+	public class RobotMiaParameters : PropertyChangedBase, IDomainObject
+	{
+		private int _id;
+		private int? _nomenclatureId;
+		private IObservableList<SlangWord> _slangWords;
+
+		public int Id
+		{
+			get => _id;
+			set => SetField(ref _id, value);
+		}
+
+		public int? NomenclatureId
+		{
+			get => _nomenclatureId;
+			set => SetField(ref _nomenclatureId, value);
+		}
+
+		public IObservableList<SlangWord> SlangWords
+		{
+			get => _slangWords;
+			set => SetField(ref _slangWords, value);
+		}
+
+		public void AddSlangWord(string word)
+		{
+			if(NomenclatureId is null)
+			{
+				throw new InvalidOperationException("Нельзя добавить слово, не указана номенклатура");
+			}
+
+			if(!SlangWords.Any(sw => sw.Word == word))
+			{
+				SlangWords.Add(new SlangWord
+				{
+					Word = word
+				});
+			}
+		}
+
+		public void ChangeSlangWord(string wordOld, string wordNew)
+		{
+			if(NomenclatureId is null)
+			{
+				throw new InvalidOperationException("Нельзя изменить слово, не указана номенклатура");
+			}
+
+			var wordToChange = SlangWords.FirstOrDefault(sw => sw.Word == wordOld);
+
+			if(wordToChange is null)
+			{
+				wordToChange.Word = wordNew;
+			}
+		}
+
+		public void RemoveSlangWord(string word)
+		{
+			if(NomenclatureId is null)
+			{
+				throw new InvalidOperationException("Нельзя добавить слово, не указана номенклатура");
+			}
+
+			var toRemove = SlangWords.Where(sw => sw.Word == word);
+
+			foreach(var item in toRemove)
+			{
+				SlangWords.Remove(item);
+			}
+		}
+	}
+}
