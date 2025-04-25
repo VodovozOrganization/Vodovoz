6e5e6150b24c52dfe10be1a20da539bf1ade47c7
Art8m <9artbe@gmail.com>
2025-04-25 05:21:52 -0700

    stetic

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/EdoProblemFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoProblemFilterView : Gtk.Bin
+	{
+		public EdoProblemFilterView()
+		{
+			this.Build();
+		}
+	}
+}
