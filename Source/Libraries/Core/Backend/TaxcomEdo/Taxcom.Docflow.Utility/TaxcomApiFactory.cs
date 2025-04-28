7e7e7c09c9f5798d33606bef2a8b119eeed86430
Vadim Gordeev <enzogord@gmail.com>
2025-04-28 17:11:30 +0300

    Добавлен ручной запуск массовой переобработки событий документов из такском

---
+++ Source/Libraries/Core/Backend/TaxcomEdo/Taxcom.Docflow.Utility/TaxcomApiFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using TaxcomEdo.Client;
+using TaxcomEdo.Client.Configs;
+
+namespace Taxcom.Docflow.Utility
+{
+	public class TaxcomApiFactory : ITaxcomApiFactory
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+		public TaxcomApiFactory(
+			IHttpClientFactory httpClientFactory,
+			JsonSerializerOptions jsonSerializerOptions
+			)
+		{
+			_httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+			_jsonSerializerOptions = jsonSerializerOptions ?? throw new ArgumentNullException(nameof(jsonSerializerOptions));
+		}
+
+		public ITaxcomApiClient Create(TaxcomApiOptions taxcomApiOptions)
+		{
+			return new TaxcomApiClient(_httpClientFactory, taxcomApiOptions, _jsonSerializerOptions);
+		}
+	}
+}
