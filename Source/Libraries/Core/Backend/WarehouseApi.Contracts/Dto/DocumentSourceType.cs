191ae91541941ed55c1111892227bff410dc32ff
Sergey Efimov <efimov90@gmail.com>
2025-04-30 11:56:52 +0300

    Temp

---
+++ Source/Libraries/Core/Backend/WarehouseApi.Contracts/Dto/DocumentSourceType.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WarehouseApi.Contracts.Dto
+{
+	/// <summary>
+	/// Тип источника документа
+	/// </summary>
+	[JsonConverter(typeof(JsonStringEnumConverter))]
+	public enum DocumentSourceType
+	{
+		/// <summary>
+		/// Документ погрузки автомобиля
+		/// </summary>
+		CarLoadDocument,
+		/// <summary>
+		/// Накладная
+		/// </summary>
+		Invoice
+	}
+}
