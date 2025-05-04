ba5fcab122aea0d6d94751a86f0fb1333b200af5
N-Sem <nikiforov.semen@gmail.com>
2025-05-04 20:21:11 +0300

    Отправка документа из воркера

--- Source/Libraries/Core/Backend/Edo/Edo.Withdrawal/WithdrawalTaskCreatedHandler.cs
+++ Source/Libraries/Core/Backend/Edo/Edo.Withdrawal/WithdrawalTaskCreatedHandler.cs
@@ -3,12 +3,20 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using QS.DomainModel.UoW;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using TrueMark.Contracts;
+using TrueMark.Contracts.Documents;
 using TrueMarkApi.Client;
+using Vodovoz.Core.Domain.Clients;
 using Vodovoz.Core.Domain.Edo;
+using Vodovoz.Core.Domain.Orders;
+using Vodovoz.Core.Domain.TrueMark.TrueMarkProductCodes;
+using VodovozBusiness.Services.TrueMark;
 
 namespace Edo.Withdrawal
 {
@@ -18,6 +26,7 @@ namespace Edo.Withdrawal
 		private readonly IHttpClientFactory _httpClientFactory;
 		private readonly IUnitOfWorkFactory _uowFactory;
 		private readonly ITrueMarkApiClient _trueMarkApiClient;
+		private readonly ITrueMarkWaterCodeService _trueMarkWaterCodeService;
 		private readonly IOptions<TrueMarkOptions> _options;
 
 		public WithdrawalTaskCreatedHandler(
@@ -25,12 +34,14 @@ namespace Edo.Withdrawal
 			IHttpClientFactory httpClientFactory,
 			IUnitOfWorkFactory uowFactory,
 			ITrueMarkApiClient trueMarkApiClient,
+			ITrueMarkWaterCodeService trueMarkWaterCodeService,
 			IOptions<TrueMarkOptions> trueMarkOptions)
 		{
 			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
 			_httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
 			_uowFactory = uowFactory ?? throw new ArgumentNullException(nameof(uowFactory));
 			_trueMarkApiClient = trueMarkApiClient ?? throw new ArgumentNullException(nameof(trueMarkApiClient));
+			_trueMarkWaterCodeService = trueMarkWaterCodeService ?? throw new ArgumentNullException(nameof(trueMarkWaterCodeService));
 			_options = trueMarkOptions;
 		}
 
@@ -57,28 +68,71 @@ namespace Edo.Withdrawal
 					throw new InvalidOperationException($"Для задачи вывода из оборота с Id {withdrawalEdoTaskId} не найден заказ");
 				}
 
+				if(order.PaymentType != Vodovoz.Domain.Client.PaymentType.Cashless)
+				{
+					throw new InvalidOperationException($"Заказ {order.Id} не по безналу, вывод из оборота невозможен");
+				}
+
 				var client = order.Client;
 
-				if(client)
-				//Проверка, что заказ по безналу
-				//Проверка, что клиент согласен на ЭДО
-				//Проверка, что клиент не зарегистрирован в ЧЗ
-				//Проверка статусов кодов и владельцев
-
-				var codes = withdrawalEdoTask.Items.Select(x => x.ProductCode).ToList();
-
-				if(codes.Count == 0)
+				if(order.Client.ConsentForEdoStatus != ConsentForEdoStatus.Agree)
 				{
-					_logger.LogInformation("Задача {EdoTaskType} с Id {WithdrawalEdoTaskId} не содержит товаров",
+					throw new InvalidOperationException($"У клиента {client.Name} отсутствует согласие на ЭДО. Вывод из оборота невозможен");
+				}
+
+				if(order.Client.RegistrationInChestnyZnakStatus == RegistrationInChestnyZnakStatus.Registered)
+				{
+					throw new InvalidOperationException($"Клиент {client.Name} зарегистрирован в Честном Знаке, вывод из оборота невозможен");
+				}
+
+				var codesInOrder = withdrawalEdoTask.Items
+					.Select(x => x.ProductCode)
+					.Where(x => x.SourceCodeStatus == SourceProductCodeStatus.Accepted || x.SourceCodeStatus == SourceProductCodeStatus.Changed)
+					.Select(x => x.ResultCode.IdentificationCode)
+					.ToList();
+
+				if(codesInOrder.Count == 0)
+				{
+					_logger.LogInformation("Задача {EdoTaskType} с Id {WithdrawalEdoTaskId} не содержит кодов для вывода из оборота",
 						nameof(WithdrawalEdoTask),
 						withdrawalEdoTaskId);
 
-					return;
+					throw new InvalidOperationException(
+						$"Задача {nameof(WithdrawalEdoTask)} с Id {withdrawalEdoTaskId} не содержит кодов для вывода из оборота");
 				}
 
-				var firstOrganizationData = _options.Value.OrganizationCertificates.FirstOrDefault();
+				try
+				{
+					var productInstancesInfo = await _trueMarkApiClient.GetProductInstanceInfoAsync(codesInOrder, cancellationToken);
 
-				var crptToken = await _trueMarkApiClient.GetCrptTokenAsync(firstOrganizationData.CertificateThumbPrint, firstOrganizationData.Inn, cancellationToken);
+					if(productInstancesInfo?.InstanceStatuses is null)
+					{
+						throw new Exception(productInstancesInfo?.ErrorMessage ?? "Не удалось получить информацию о коде в ЧЗ");
+					}
+
+					var codesForDocument = productInstancesInfo.InstanceStatuses
+						.Where(x => x.Status == ProductInstanceStatusEnum.Introduced)
+						.Select(x => x.IdentificationCode)
+						.ToList();
+
+					var document = CreateIndividualAccountingWithdrawalDocument(order, codesInOrder);
+
+					var documentSendResponse =
+						_trueMarkApiClient.SendIndividualAccountingWithdrawalDocument(document, order.Contract.Organization.INN, cancellationToken);
+				}
+				catch(Exception ex)
+				{
+
+				}
+
+
+				withdrawalEdoTask.Problems.Add(new EdoTaskProblem
+				{
+					Type = EdoTaskProblemType.Exception,
+					ProblemDescription = "Вывод из оборота",
+					ProblemDate = DateTime.Now,
+					ProblemDocument = document
+				});
 
 				withdrawalEdoTask.Status = EdoTaskStatus.Completed;
 				withdrawalEdoTask.EndTime = DateTime.Now;
@@ -86,6 +140,26 @@ namespace Edo.Withdrawal
 				await uow.SaveAsync(withdrawalEdoTask, cancellationToken: cancellationToken);
 				uow.Commit();
 			}
+		}
+
+		private string CreateIndividualAccountingWithdrawalDocument(OrderEntity order, IEnumerable<string> identificationCodes)
+		{
+			var productDocument = new ProductDocumentIndividualAccountingDto
+			{
+				Inn = order.Contract.Organization.INN,
+				BuyerInn = order.Client.INN,
+				Action = "OWN_USE",
+				ActionDate = order.DeliveryDate.Value,
+				DocumentType = "OTHER",
+				DocumentNumber = order.Id.ToString(),
+				DocumentDate = order.DeliveryDate.Value,
+				PrimaryDocumentCustomName = "UTD",
+				Products = identificationCodes
+			};
+
+			var serializedProductDocument = JsonSerializer.Serialize(productDocument);
+
+			return serializedProductDocument;
 		}
 	}
 }
