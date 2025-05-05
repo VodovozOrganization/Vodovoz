4b7703f6e7ab6eb74c75fc711a4dda8705fc2529
Sergey Efimov <efimov90@gmail.com>
2025-05-05 10:42:18 +0300

    Перенос в версию 1

--- Source/Libraries/Core/Backend/WarehouseApi.Contracts/Responses/V1/AddOrderCodeResponse.cs
+++ Source/Libraries/Core/Backend/WarehouseApi.Contracts/Responses/V1/AddOrderCodeResponse.cs
@@ -1,6 +1,6 @@
 ﻿using WarehouseApi.Contracts.Dto;
 
-namespace WarehouseApi.Contracts.Responses
+namespace WarehouseApi.Contracts.Responses.V1
 {
 	/// <summary>
 	/// DTO ответа на запрос добавления кода маркировки ЧЗ в заказ
