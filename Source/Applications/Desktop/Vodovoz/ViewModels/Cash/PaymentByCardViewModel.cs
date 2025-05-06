69f1c1899b12bb3ab030bc9b8767644215899a85
Sergey Efimov <efimov90@gmail.com>
2025-05-06 16:45:19 +0300

    Накладная в оставшихся диалогах

--- Source/Applications/Desktop/Vodovoz/ViewModels/Cash/PaymentByCardViewModel.cs
+++ Source/Applications/Desktop/Vodovoz/ViewModels/Cash/PaymentByCardViewModel.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using QS.Commands;
+using QS.Dialog;
 using QS.DomainModel.UoW;
+using QS.Navigation;
 using QS.Project.Domain;
+using QS.Report;
 using QS.Services;
 using QS.Utilities.Extensions;
 using QS.ViewModels;
+using System;
+using System.Linq;
+using Vodovoz.Core.Domain.Orders;
 using Vodovoz.Domain.Client;
 using Vodovoz.Domain.Employees;
+using Vodovoz.Presentation.ViewModels.Documents;
 using Vodovoz.Settings.Delivery;
 using Vodovoz.Settings.Orders;
 using Vodovoz.Tools.CallTasks;
@@ -13,7 +20,7 @@ using Order = Vodovoz.Domain.Orders.Order;
 
 namespace Vodovoz.ViewModels.Cash
 {
-	public class PaymentByCardViewModel: EntityTabViewModelBase<Order> 
+	public class PaymentByCardViewModel : EntityTabViewModelBase<Order>
 	{
 		private readonly Employee _currentEmployee;
 		private readonly ICallTaskWorker _callTaskWorker;
@@ -32,7 +39,7 @@ namespace Vodovoz.ViewModels.Cash
 			{
 				throw new ArgumentNullException(nameof(orderPaymentSettings));
 			}
-			
+
 			if(orderSettings == null)
 			{
 				throw new ArgumentNullException(nameof(orderSettings));
@@ -50,16 +57,14 @@ namespace Vodovoz.ViewModels.Cash
 			Entity.PaymentType = PaymentType.Terminal;
 
 			Entity.PropertyChanged += Entity_PropertyChanged;
-			
+
 			ValidationContext.ServiceContainer.AddService(orderSettings);
 			ValidationContext.ServiceContainer.AddService(deliveryRulesSettings);
-		}
 
-		void Entity_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == nameof(Entity.PaymentType)){
-				OnPropertyChanged(nameof(PaymentType));
-			}
+			SaveCommand = new DelegateCommand(SaveHandler, () => CanSave);
+			SaveCommand.CanExecuteChangedWith(this, x => x.CanSave);
+
+			CloseCommand = new DelegateCommand(CloseHandler);
 		}
 
 		public PaymentType PaymentType
@@ -68,21 +73,82 @@ namespace Vodovoz.ViewModels.Cash
 			set => Entity.PaymentType = value;
 		}
 
+		public DelegateCommand SaveCommand { get; }
+		public DelegateCommand CloseCommand { get; }
+
+		public bool CanSave => Entity.OnlineOrder != null;
+
+		private void SaveHandler()
+		{
+			if(Entity.SelfDelivery)
+			{
+				if(!Save(false))
+				{
+					CommonServices.InteractiveService.ShowMessage(ImportanceLevel.Warning, "Не удалось сохранить документ");
+					return;
+				}
+
+				var document = Entity.OrderDocuments
+					.FirstOrDefault(x =>
+						x.Type == OrderDocumentType.Invoice
+						|| x.Type == OrderDocumentType.InvoiceBarter
+						|| x.Type == OrderDocumentType.InvoiceContractDoc)
+					as IPrintableRDLDocument;
+
+				var page = NavigationManager
+					.OpenViewModel<PrintableRdlDocumentViewModel<IPrintableRDLDocument>, IPrintableRDLDocument>(this, document, OpenPageOptions.AsSlave);
+
+				page.PageClosed += OnInvoiceDocumentPrintViewClosed;
+			}
+			else
+			{
+				SaveAndClose();
+			}
+		}
+
+		private void OnInvoiceDocumentPrintViewClosed(object sender, EventArgs eventArgs)
+		{
+			if(sender is IPage page)
+			{
+				page.PageClosed -= OnInvoiceDocumentPrintViewClosed;
+			}
+
+			Close(false, CloseSource.Save);
+		}
+
+		private void CloseHandler()
+		{
+			Close(true, CloseSource.Cancel);
+		}
+
+		private void Entity_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if(e.PropertyName == nameof(Entity.PaymentType))
+			{
+				OnPropertyChanged(nameof(PaymentType));
+			}
+
+			if(e.PropertyName == nameof(Entity.OnlineOrder))
+			{
+				OnPropertyChanged(nameof(CanSave));
+			}
+		}
+
 		protected override bool BeforeValidation()
-        {
+		{
 			Entity.ChangePaymentTypeToByCardTerminal(_callTaskWorker);
 
-	        if(!Entity.PayAfterShipment)
-	        {
-		        Entity.SelfDeliveryToLoading(_currentEmployee, CommonServices.CurrentPermissionService, _callTaskWorker);
-	        }
+			if(!Entity.PayAfterShipment)
+			{
+				Entity.SelfDeliveryToLoading(_currentEmployee, CommonServices.CurrentPermissionService, _callTaskWorker);
+			}
 
-	        if(Entity.SelfDelivery)
-	        {
-		        Entity.IsSelfDeliveryPaid = true;
-	        }
+			if(Entity.SelfDelivery)
+			{
+				Entity.IsSelfDeliveryPaid = true;
+			}
 
-	        return true;
-        }
-    }
+			return true;
+		}
+	}
 }
