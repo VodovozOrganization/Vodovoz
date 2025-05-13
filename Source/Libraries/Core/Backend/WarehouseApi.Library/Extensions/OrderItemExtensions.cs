332e0d85f963b080f129b82cec657b1192fa1e32
Sergey Efimov <efimov90@gmail.com>
2025-05-13 09:07:42 +0300

    Temp

--- Source/Libraries/Core/Backend/WarehouseApi.Library/Extensions/OrderItemExtensions.cs
+++ Source/Libraries/Core/Backend/WarehouseApi.Library/Extensions/OrderItemExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Vodovoz.Domain.Goods;
 using Vodovoz.Domain.Orders;
-using WarehouseApi.Contracts.Dto;
 using WarehouseApi.Contracts.Dto.V1;
 
 namespace WarehouseApi.Library.Extensions
