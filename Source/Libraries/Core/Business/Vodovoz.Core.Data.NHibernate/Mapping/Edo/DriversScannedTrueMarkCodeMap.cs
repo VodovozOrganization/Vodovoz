8e33b537ef4e7046627d8f9a3c05281e65dd7983
N-Sem <nikiforov.semen@gmail.com>
2025-05-16 15:56:59 +0300

    Новый класс для хранения отсканированных кодов

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Data.NHibernate/Mapping/Edo/DriversScannedTrueMarkCodeMap.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Core.Domain.Edo;
+
+namespace Vodovoz.Core.Data.NHibernate.Mapping.Edo
+{
+	public class DriversScannedTrueMarkCodeMap : ClassMap<DriversScannedTrueMarkCode>
+	{
+		public DriversScannedTrueMarkCodeMap()
+		{
+			Table("drivers_scanned_true_mark_codes");
+
+			HibernateMapping.DefaultAccess.CamelCaseField(Prefix.Underscore);
+
+			Id(x => x.Id)
+				.Column("id")
+				.GeneratedBy.Native();
+
+			Map(x => x.RawCode).Column("raw_code");
+			Map(x => x.OrderItemId).Column("order_item_id");
+			Map(x => x.IsDefective).Column("is_defective");
+			Map(x => x.IsProcessingCompleted).Column("is_processing_completed");
+		}
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Edo/DriversScannedTrueMarkCode.cs
@@ -0,0 +1,79 @@
+﻿using QS.DomainModel.Entity;
+using QS.HistoryLog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Core.Domain.Edo
+{
+	/// <summary>
+	/// Отсканированный водителем код ЧЗ
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Masculine,
+		Nominative = "отсканированный водителем код ЧЗ",
+		NominativePlural = "отсканированные водителями коды ЧЗ",
+		Prepositional = "отсканированном водителем коде ЧЗ",
+		PrepositionalPlural = "отсканированным водителями кодами ЧЗ",
+		Accusative = "отсканированный водителем код ЧЗ",
+		AccusativePlural = "отсканированные водителями коды ЧЗ",
+		Genitive = "отсканированного водителем кода ЧЗ",
+		GenitivePlural = "отсканированных водителями кодов ЧЗ")]
+	[HistoryTrace]
+	public class DriversScannedTrueMarkCode : PropertyChangedBase, IDomainObject
+	{
+		private int _id;
+		private string _rawCode;
+		private int _orderItemId;
+		private bool _isDefective;
+		private bool _isProcessingCompleted;
+
+		/// <summary>
+		/// Код
+		/// </summary>
+		[Display(Name = "Код")]
+		public virtual int Id
+		{
+			get => _id;
+			set => SetField(ref _id, value);
+		}
+
+		/// <summary>
+		/// Отсканированный код
+		/// </summary>
+		[Display(Name = "Отсканированный код")]
+		public virtual string RawCode
+		{
+			get => _rawCode;
+			set => SetField(ref _rawCode, value);
+		}
+
+		/// <summary>
+		/// Id строки МЛ
+		/// </summary>
+		[Display(Name = "Id строки заказа")]
+		public virtual int OrderItemId
+		{
+			get => _orderItemId;
+			set => SetField(ref _orderItemId, value);
+		}
+
+		/// <summary>
+		/// Дефект
+		/// </summary>
+		[Display(Name = "Дефект")]
+		public virtual bool IsDefective
+		{
+			get => _isDefective;
+			set => SetField(ref _isDefective, value);
+		}
+
+		/// <summary>
+		/// Обработка кода завершена
+		/// </summary>
+		[Display(Name = "Обработка кода завершена")]
+		public virtual bool IsProcessingCompleted
+		{
+			get => _isProcessingCompleted;
+			set => SetField(ref _isProcessingCompleted, value);
+		}
+	}
+}
