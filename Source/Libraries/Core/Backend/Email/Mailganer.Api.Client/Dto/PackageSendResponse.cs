f33bfa9672b2429ce48dbb304c4009cc791ec22f
Vadim Gordeev <enzogord@gmail.com>
2025-05-21 13:59:48 +0300

    Добавлен Mailganer клиент

---
+++ Source/Libraries/Core/Backend/Email/Mailganer.Api.Client/Dto/PackageSendResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mailganer.Api.Client.Dto
+{
+	public class PackageSendResponse
+	{
+		[JsonPropertyName("status")]
+		public string Status { get; set; }
+
+		[JsonPropertyName("message")]
+		public PackageResponseMessage Message { get; set; }
+	}
+}
