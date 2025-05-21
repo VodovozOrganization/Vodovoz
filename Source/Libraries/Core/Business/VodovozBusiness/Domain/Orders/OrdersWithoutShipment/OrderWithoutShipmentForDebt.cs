7ab75e21634c5c4763cade3d6d95aeef9b525eec
Vadim Gordeev <enzogord@gmail.com>
2025-05-21 14:07:49 +0300

    Изменение шаблонов писем под формат нового клиента

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/OrdersWithoutShipment/OrderWithoutShipmentForDebt.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/OrdersWithoutShipment/OrderWithoutShipmentForDebt.cs
@@ -131,18 +131,6 @@ namespace Vodovoz.Domain.Orders.OrdersWithoutShipment
 		{
 			var template = new EmailTemplate();
 
-			var imageId = "email_ad";
-			var image = new EmailAttachment();
-			image.MIMEType = "image/png";
-			image.FileName = "email_ad.png";
-			using(Stream stream = typeof(OrderWithoutShipmentForDebt).Assembly.GetManifestResourceStream("VodovozBusiness.Resources.email_ad.png")) {
-				byte[] buffer = new byte[stream.Length];
-				stream.Read(buffer, 0, buffer.Length);
-				image.Base64Content = Convert.ToBase64String(buffer);
-			}
-
-			template.Attachments.Add(imageId, image);
-
 			template.Title = "ООО \"Веселый водовоз\"";
 			template.Text =
 						"Здравствуйте,\n" +
@@ -179,7 +167,7 @@ namespace Vodovoz.Domain.Orders.OrdersWithoutShipment
 						"<p>Мы ВКонтакте: <a href=\"https://vk.com/vodovoz_spb\" target=\"_blank\">vk.com/vodovoz_spb</a></p>\n" +
 						"<p>Мы в Instagram: @vodovoz_lifestyle</p>\n" +
 						"<p>Наш официальный сайт: <a href=\"http://www.vodovoz-spb.ru/\" target=\"_blank\">www.vodovoz-spb.ru</a></p>\n" +
-						string.Format("<img src=\"cid:{0}\">", imageId);
+						"<img src=\"https://cloud1.vod.qsolution.ru/email-attachments/email_ad.png\">";
 
 			return template;
 		}
