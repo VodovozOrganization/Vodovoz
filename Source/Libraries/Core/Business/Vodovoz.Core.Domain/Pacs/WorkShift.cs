48c6b201c65e0b8db01b3a9ae2c83f91046ac79e
Sergey Efimov <efimov90@gmail.com>
2025-05-23 11:33:19 +0300

    Смена фикс

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Pacs/WorkShift.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Pacs/WorkShift.cs
@@ -18,18 +18,36 @@ namespace Vodovoz.Core.Domain.Pacs
 		private string _name;
 		private TimeSpan _duration;
 
+		/// <summary>
+		/// Для NHibernate, не использовать.
+		/// </summary>
+		protected WorkShift()
+		{
+		}
+
+		/// <summary>
+		/// Конструктор для создания смены.
+		/// </summary>
+		/// <param name="name">Название смены</param>
+		/// <param name="duration">Длительность смены</param>
+		protected WorkShift(string name, TimeSpan duration)
+		{
+			Name = name;
+			Duration = duration;
+		}
+
 		[Display(Name = "Код")]
 		public virtual int Id
 		{
 			get => _id;
-			set => SetField(ref _id, value);
+			protected set => SetField(ref _id, value);
 		}
 
 		[Display(Name = "Название")]
 		public virtual string Name
 		{
 			get => _name;
-			set => SetField(ref _name, value);
+			protected set => SetField(ref _name, value);
 		}
 
 
@@ -37,7 +55,29 @@ namespace Vodovoz.Core.Domain.Pacs
 		public virtual TimeSpan Duration
 		{
 			get => _duration;
-			set => SetField(ref _duration, value);
+			protected set => SetField(ref _duration, value);
+		}
+
+		/// <summary>
+		/// Создает смену с заданным названием и длительностью.
+		/// </summary>
+		/// <param name="name">Название</param>
+		/// <param name="duration">Длительность</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException"></exception>
+		public static WorkShift Create(string name, TimeSpan duration)
+		{
+			if(duration <= TimeSpan.Zero)
+			{
+				throw new ArgumentException("Длительность должна быть больше нуля", nameof(Duration));
+			}
+
+			if(string.IsNullOrWhiteSpace(name))
+			{
+				throw new ArgumentException("Название должно быть заполнено", nameof(name));
+			}
+
+			return new WorkShift(name, duration);
 		}
 
 		public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
