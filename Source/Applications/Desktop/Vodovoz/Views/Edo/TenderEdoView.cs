2ab1ac7a1cef4941df5eeaf4f0227432c45073e2
Art8m <9artbe@gmail.com>
2025-05-27 05:44:26 -0700

    Stetic

---
+++ Source/Applications/Desktop/Vodovoz/Views/Edo/TenderEdoView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Edo
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class TenderEdoView : Gtk.Bin
+	{
+		public TenderEdoView()
+		{
+			this.Build();
+		}
+	}
+}
