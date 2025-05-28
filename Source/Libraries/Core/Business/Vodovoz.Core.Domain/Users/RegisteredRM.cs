ae10ca16d287f497c8011afeaecb36445baf0904
Sergey Efimov <efimov90@gmail.com>
2025-05-28 14:02:56 +0300

    RegisteredRM

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/RegisteredRM.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/RegisteredRM.cs
@@ -1,114 +1,113 @@
 ﻿using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
-using System;
+using QS.Extensions.Observable.Collections.List;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Bindings.Collections.Generic;
-using Vodovoz.Domain.Employees;
 
-namespace Vodovoz.Domain.Security
+namespace Vodovoz.Core.Domain.Users
 {
-    [Appellative(Gender = GrammaticalGender.Masculine,
-        NominativePlural = "зарегистрированные RM",
-        Nominative = "зарегистрированный RM")]
-    [EntityPermission]
-    public class RegisteredRM : PropertyChangedBase, IDomainObject, IValidatableObject
-    {
-        /// <summary>
-        /// Идентификатор
-        /// </summary>
-        [Display(Name = "Идентификатор")]
-        public virtual int Id { get; set; }
-        
-        private string username;
-        /// <summary>
-        /// Имя пользователя в системе
-        /// </summary>
-        [Display(Name = "Имя пользователя в системе")]
-        public virtual string Username
-        {
-            get => username;
-            set => SetField(ref username, value);
-        }
+	/// <summary>
+	/// Зарегистрированный пользователь операционной системы, который может подключаться к программе
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Masculine,
+		Accusative = "зарегистрированный RM",
+		AccusativePlural = "зарегистрированные RM",
+		Genitive = "зарегистрированного RM",
+		GenitivePlural = "зарегистрированных RM",
+		Nominative = "зарегистрированный RM",
+		NominativePlural = "зарегистрированные RM",
+		Prepositional = "зарегистрированном RM",
+		PrepositionalPlural = "зарегистрированных RM")]
+	[EntityPermission]
+	public class RegisteredRM : PropertyChangedBase, IDomainObject, IValidatableObject
+	{
+		private string _username;
+		private string _domain;
+		private string _sid;
+		private IObservableList<User> _users = new ObservableList<User>();
+		private bool _isActive;
+		private int _id;
 
-        private string domain;
-        /// <summary>
-        /// Имя домена
-        /// </summary>
-        [Display(Name = "Имя домена")]
-        public virtual string Domain
-        {
-            get => domain;
-            set => SetField(ref domain, value);
-        }
+		/// <summary>
+		/// Идентификатор
+		/// </summary>
+		[Display(Name = "Идентификатор")]
+		public virtual int Id
+		{
+			get => _id;
+			set => SetField(ref _id, value);
+		}
 
-        private string sID;
-        /// <summary>
-        /// SID пользователя
-        /// </summary>
-        [Display(Name = "SID пользователя")]
-        public virtual string SID
-        {
-            get => sID;
-            set => SetField(ref sID, value);
-        }
+		/// <summary>
+		/// Имя пользователя в системе
+		/// </summary>
+		[Display(Name = "Имя пользователя в системе")]
+		public virtual string Username
+		{
+			get => _username;
+			set => SetField(ref _username, value);
+		}
 
-        private IList<User> users = new List<User>();
-        /// <summary>
-        /// Пользователи программы
-        /// </summary>
-        [Display (Name = "Пользователи программы доступные для подключения")]
-        public virtual IList<User> Users
-        {
-            get => users;
-            set => SetField(ref users, value);
-        }
+		/// <summary>
+		/// Имя домена
+		/// </summary>
+		[Display(Name = "Имя домена")]
+		public virtual string Domain
+		{
+			get => _domain;
+			set => SetField(ref _domain, value);
+		}
 
-        GenericObservableList<User> observableUsers;
-        //FIXME Кослыль пока не разберемся как научить hibernate работать с обновляемыми списками.
-        public virtual GenericObservableList<User> ObservableUsers
-        {
-            get
-            {
-                if (observableUsers == null)
-                {
-                    observableUsers = new GenericObservableList<User>(users);
-                    observableUsers.ListContentChanged += ObservableUsers_ListContentChanged;
-                }
+		/// <summary>
+		/// SID пользователя
+		/// </summary>
+		[Display(Name = "SID пользователя")]
+		public virtual string SID
+		{
+			get => _sid;
+			set => SetField(ref _sid, value);
+		}
 
-                return observableUsers;
-            }
-        }
+		/// <summary>
+		/// Пользователи программы
+		/// </summary>
+		[Display(Name = "Пользователи программы доступные для подключения")]
+		public virtual IObservableList<User> Users
+		{
+			get => _users;
+			set => SetField(ref _users, value);
+		}
 
-        private void ObservableUsers_ListContentChanged(object sender, EventArgs e)
-        {
-            OnPropertyChanged(nameof(ObservableUsers));
-        }
+		/// <summary>
+		/// Запись активна
+		/// </summary>
+		[Display(Name = "Запись активна")]
+		public virtual bool IsActive
+		{
+			get => _isActive;
+			set => SetField(ref _isActive, value);
+		}
 
-        private bool isActive;
-        /// <summary>
-        /// Запись активна
-        /// </summary>
-        [Display(Name = "Запись активна")]
-        public virtual bool IsActive
-        {
-            get => isActive;
-            set => SetField(ref isActive, value);
-        }
+		public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (string.IsNullOrWhiteSpace(Username))
+			{
+				yield return new ValidationResult("Имя пользователя не может быть пустым",
+					new[] { nameof(Username) });
+			}
 
-        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (string.IsNullOrWhiteSpace(Username))
-                yield return new ValidationResult("Имя пользователя не может быть пустым",
-                    new[] { nameof(Username) });
+			if (string.IsNullOrWhiteSpace(Domain))
+			{
+				yield return new ValidationResult("Домен не может быть пустым",
+					new[] { nameof(Domain) });
+			}
 
-            if (string.IsNullOrWhiteSpace(Domain))
-                yield return new ValidationResult("Домен не может быть пустым",
-                    new[] { nameof(Domain) });
-
-            if (string.IsNullOrWhiteSpace(SID))
-                yield return new ValidationResult("SID пользователя не может быть пустым",
-                    new[] { nameof(SID) });
-        }
-    }
+			if (string.IsNullOrWhiteSpace(SID))
+			{
+				yield return new ValidationResult("SID пользователя не может быть пустым",
+					new[] { nameof(SID) });
+			}
+		}
+	}
 }
