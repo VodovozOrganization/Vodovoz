464de7d7a43057dd5a51643e8c60fbfd553105fd
Sergey Efimov <efimov90@gmail.com>
2025-05-28 14:09:53 +0300

    UserWarehousePermission

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Warehouses/UserWarehousePermission.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Warehouses/UserWarehousePermission.cs
@@ -3,20 +3,39 @@ using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
 using QS.HistoryLog;
 using Vodovoz.Core.Domain.Users;
+using Vodovoz.Domain.Permissions.Warehouses;
 
-namespace Vodovoz.Domain.Permissions.Warehouses
+namespace Vodovoz.Core.Domain.Warehouses
 {
+	/// <summary>
+	/// Право на склад для пользователя.
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Feminine,
+		Accusative = "право на склад пользователя",
+		AccusativePlural = "права на склад пользователей",
+		Genitive = "права на склад пользователя",
+		GenitivePlural = "прав складов пользователей",
+		Nominative = "Право на склад пользователя",
+		NominativePlural = "Права на склад пользователей",
+		Prepositional = "праве на склад пользователя",
+		PrepositionalPlural = "правах на склады пользователей")]
 	[EntityPermission]
 	[HistoryTrace]
 	public class UserWarehousePermission : WarehousePermissionBase
 	{
-		public override PermissionType PermissionType => PermissionType.User;
-
 		private User _user;
+
+		/// <summary>
+		/// Пользователь, которому принадлежат права на склад
+		/// </summary>
 		[Display(Name = "Пользователь")]
-		public virtual User User {
+		public virtual User User
+		{
 			get => _user;
 			set => SetField(ref _user, value);
 		}
+
+		public override PermissionType PermissionType => PermissionType.User;
 	}
 }
