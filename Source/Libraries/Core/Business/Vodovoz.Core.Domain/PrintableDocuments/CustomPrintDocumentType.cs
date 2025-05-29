c023317a3297ada4d301ce4483d8b1cb9c0aa90d
Sergey Efimov <efimov90@gmail.com>
2025-05-29 15:50:48 +0300

    CustomPrintDocumentType

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/PrintableDocuments/CustomPrintDocumentType.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/PrintableDocuments/CustomPrintDocumentType.cs
@@ -1,13 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Vodovoz.PrintableDocuments
+namespace Vodovoz.Core.Domain.PrintableDocuments
 {
+	/// <summary>
+	/// Дополнительные типы документов для печати
+	/// </summary>
 	public enum CustomPrintDocumentType
 	{
+		/// <summary>
+		/// Талон погрузки (вода)
+		/// </summary>
 		[Display(Name = "Талон погрузки (вода)")]
 		WaterCarLoadDocument,
+		/// <summary>
+		/// Талон погрузки (оборудование)
+		/// </summary>
 		[Display(Name = "Талон погрузки (оборуд.)")]
 		EquipmentCarLoadDocument,
+		/// <summary>
+		/// Талон погрузки (контроль)
+		/// </summary>
 		[Display(Name = "Талон погрузки (контроль)")]
 		ControlCarLoadDocument
 	}
