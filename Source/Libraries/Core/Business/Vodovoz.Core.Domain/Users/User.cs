5cf4afbcc91afc4d11ecac702a603130357a461c
Sergey Efimov <efimov90@gmail.com>
2025-05-29 09:42:38 +0300

    User

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/User.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/User.cs
@@ -1,26 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using QS.Extensions.Observable.Collections.List;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Bindings.Collections.Generic;
+using System.Text.Json;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json;
+using Vodovoz.Core.Domain.Warehouses;
 using Vodovoz.Domain.Permissions;
-using Vodovoz.Domain.Permissions.Warehouses;
-using Vodovoz.Domain.Security;
 
-namespace Vodovoz.Domain.Employees
+namespace Vodovoz.Core.Domain.Users
 {
-	public class User: QS.Project.Domain.UserBase, IValidatableObject
+	/// <summary>
+	/// Пользователь системы.
+	/// </summary>
+	public class User : QS.Project.Domain.UserBase, IValidatableObject
 	{
 		private bool _needPasswordChange;
 		private UserRole _currentUserRole;
-		private IList<RegisteredRM> _registeredRMs = new List<RegisteredRM>();
-		private IList<UserRole> _userRoles = new List<UserRole>();
-		private GenericObservableList<UserRole> _observableUserRoles;
+		private IObservableList<RegisteredRM> _registeredRMs = new ObservableList<RegisteredRM>();
+		private IObservableList<UserRole> _userRoles = new ObservableList<UserRole>();
+		private string _warehouseAccess;
 
-		public virtual string WarehouseAccess { get; set; }
+		/// <summary>
+		/// Права доступа к складам пользователя в виде JSON-строки.
+		/// </summary>
+		public virtual string WarehouseAccess { get => _warehouseAccess; set => _warehouseAccess = value; }
 
+		/// <summary>
+		/// Требуется ли смена пароля при следующем входе в систему
+		/// </summary>
 		[Display(Name = "Требуется смена пароля")]
-		public virtual bool NeedPasswordChange {
+		public virtual bool NeedPasswordChange
+		{
 			get => _needPasswordChange;
 			set => SetField(ref _needPasswordChange, value);
 		}
@@ -29,29 +38,41 @@ namespace Vodovoz.Domain.Employees
 		/// Пользователи операционной системы доступные для подключения по этой учетной записи пользователя
 		/// </summary>
 		[Display(Name = "Пользователи операционной системы доступные для подключения по этой учетной записи пользователя")]
-		public virtual IList<RegisteredRM> RegisteredRMs
+		public virtual IObservableList<RegisteredRM> RegisteredRMs
 		{
 			get => _registeredRMs;
 			set => SetField(ref _registeredRMs, value);
 		}
-		
+
+		/// <summary>
+		/// Текущая роль пользователя в системе
+		/// </summary>
 		[Display(Name = "Текущая роль пользователя")]
 		public virtual UserRole CurrentUserRole
 		{
 			get => _currentUserRole;
 			set => SetField(ref _currentUserRole, value);
 		}
-		
+
+		/// <summary>
+		/// Роли пользователя в системе
+		/// </summary>
 		[Display(Name = "Роли пользователя")]
-		public virtual IList<UserRole> UserRoles
+		public virtual IObservableList<UserRole> UserRoles
 		{
 			get => _userRoles;
 			set => SetField(ref _userRoles, value);
 		}
-		
-		//FIXME Кослыль пока не разберемся как научить hibernate работать с обновляемыми списками.
-		public virtual GenericObservableList<UserRole> ObservableUserRoles =>
-			_observableUserRoles ?? (_observableUserRoles = new GenericObservableList<UserRole>(UserRoles));
+
+		/// <summary>
+		/// Получает права доступа к складам пользователя в виде словаря,
+		/// где ключ - тип прав доступа, значение - коллекция идентификаторов складов
+		/// </summary>
+		/// <returns></returns>
+		public virtual IDictionary<WarehousePermissionsType, IEnumerable<int>> GetWarehousePermissions()
+		{
+			return JsonSerializer.Deserialize<Dictionary<WarehousePermissionsType, IEnumerable<int>>>(WarehouseAccess);
+		}
 
 		public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
 		{
@@ -61,11 +82,5 @@ namespace Vodovoz.Domain.Employees
 				yield return new ValidationResult("Логин может состоять только из букв английского алфавита, нижнего подчеркивания, дефиса, точки и запятой", new[] { nameof(Login) });
 			}
 		}
-
-		public virtual IDictionary<WarehousePermissionsType, IList<int>> GetWarehousePermissions()
-		{
-			return JsonConvert.DeserializeObject<Dictionary<WarehousePermissionsType, IList<int>>>(WarehouseAccess);
-		}
 	}
 }
-
