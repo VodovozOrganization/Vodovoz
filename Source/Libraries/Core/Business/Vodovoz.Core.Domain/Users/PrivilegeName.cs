083103a0feda12267400216d29487f883802c078
Sergey Efimov <efimov90@gmail.com>
2025-05-29 10:09:10 +0300

    PrivelegeName

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/PrivilegeName.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/PrivilegeName.cs
@@ -1,23 +1,67 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using QS.DomainModel.Entity;
+using Vodovoz.Domain.Permissions;
 
-namespace Vodovoz.Domain.Permissions
+namespace Vodovoz.Core.Domain.Users
 {
-	[Appellative(Gender = GrammaticalGender.Feminine,
+	/// <summary>
+	/// Имя привилегии, которое используется для определения прав доступа к базам данных.
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Feminine,
+		Accusative = "имя привелегии",
+		AccusativePlural = "имена привелегий",
+		Genitive = "имени привелегии",
+		GenitivePlural = "имен привелегий",
+		Nominative = "имя привелегии",
 		NominativePlural = "имена привелегий",
-		Nominative = "имя привелегии")]
+		Prepositional = "имени привелегии",
+		PrepositionalPlural = "именах привелегий")]
 	public class PrivilegeName : IDomainObject
 	{
-		public virtual int Id { get; set; }
-		
-		[Display(Name = "Имя")]
-		public virtual string Name { get; set; }
-		
-		[Display(Name = "Тип")]
-		public virtual PrivilegeType PrivilegeType { get; set; }
+		private int _id;
+		private string _name;
+		private PrivilegeType _privilegeType;
+		private IList<AvailableDatabase> _unavailableDatabases = new List<AvailableDatabase>();
 
+		/// <summary>
+		/// Идентификатор
+		/// </summary>
+		public virtual int Id
+		{
+			get => _id;
+			set => _id = value;
+		}
+
+		/// <summary>
+		/// Имя
+		/// </summary>
+		[Display(Name = "Имя")]
+		public virtual string Name
+		{
+			get => _name;
+			set => _name = value;
+		}
+
+		/// <summary>
+		/// Тип
+		/// </summary>
+		[Display(Name = "Тип")]
+		public virtual PrivilegeType PrivilegeType
+		{
+			get => _privilegeType;
+			set => _privilegeType = value;
+		}
+
+		/// <summary>
+		/// Список недоступных баз данных для данной привилегии
+		/// </summary>
 		[Display(Name = "Недоступные БД")]
-		public virtual IList<AvailableDatabase> UnavailableDatabases { get; set; } = new List<AvailableDatabase>();
+		public virtual IList<AvailableDatabase> UnavailableDatabases
+		{
+			get => _unavailableDatabases;
+			set => _unavailableDatabases = value;
+		}
 	}
 }
