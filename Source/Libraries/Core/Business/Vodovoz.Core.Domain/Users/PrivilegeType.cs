3a931a45b758c34c18144d17e3b65b9ec28f31af
Sergey Efimov <efimov90@gmail.com>
2025-05-29 10:05:05 +0300

    PrivelegeType

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/PrivilegeType.cs
@@ -0,0 +1,25 @@
+﻿namespace Vodovoz.Core.Domain.Users
+{
+	/// <summary>
+	/// Типы привилегий для работы с БД.
+	/// </summary>
+	public enum PrivilegeType
+	{
+		/// <summary>
+		/// Глобальная привилегия, которая применяется ко всем базам данных и таблицам.
+		/// </summary>
+		GlobalPrivilege,
+		/// <summary>
+		/// Привилегия, которая применяется к конкретной базе данных.
+		/// </summary>
+		DatabasePrivilege,
+		/// <summary>
+		/// Привилегия, которая применяется к конкретной таблице в базе данных.
+		/// </summary>
+		TablePrivilege,
+		/// <summary>
+		/// Специальная привилегия, которая может включать в себя дополнительные права или ограничения.
+		/// </summary>
+		SpecialPrivilege
+	}
+}
