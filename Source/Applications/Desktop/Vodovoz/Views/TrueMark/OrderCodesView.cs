6f5177f85a86d0c923a814fd6202c8192977a14d
Vadim Gordeev <enzogord@gmail.com>
2025-05-30 14:10:58 +0300

    Добавлен UI диалога просмотра кодов по заказу

---
+++ Source/Applications/Desktop/Vodovoz/Views/TrueMark/OrderCodesView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.TrueMark
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrderCodesView : Gtk.Bin
+	{
+		public OrderCodesView()
+		{
+			this.Build();
+		}
+	}
+}
