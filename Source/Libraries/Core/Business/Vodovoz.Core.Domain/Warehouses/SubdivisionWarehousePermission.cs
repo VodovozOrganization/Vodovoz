a1db0794d4218d328910455270f7578b8490f276
Sergey Efimov <efimov90@gmail.com>
2025-06-02 09:05:16 +0300

    SubdivisionWarehousePermission

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Warehouses/SubdivisionWarehousePermission.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Warehouses/SubdivisionWarehousePermission.cs
@@ -1,21 +1,40 @@
-using System.ComponentModel.DataAnnotations;
-using QS.DomainModel.Entity;
+﻿using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
 using QS.HistoryLog;
+using System.ComponentModel.DataAnnotations;
+using Vodovoz.Core.Domain.Organizations;
 
-namespace Vodovoz.Domain.Permissions.Warehouses
+namespace Vodovoz.Core.Domain.Warehouses
 {
+	/// <summary>
+	/// Право на склад для подразделения.
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Feminine,
+		Accusative = "право на склад подразделения",
+		AccusativePlural = "права на склад подразделений",
+		Nominative = "право на склад подразделения",
+		NominativePlural = "права на склад подразделения",
+		Genitive = "права на склад подразделения",
+		GenitivePlural = "прав складов подразделений",
+		Prepositional = "праве на склад подразделения",
+		PrepositionalPlural = "правах на склады подразделений")]
 	[EntityPermission]
 	[HistoryTrace]
-	public class SubdivisionWarehousePermission: WarehousePermissionBase
+	public class SubdivisionWarehousePermission : WarehousePermissionBase
 	{
-		public override PermissionType PermissionType => PermissionType.Subdivision;
+		private SubdivisionEntity _subdivision;
 
-		private Subdivision _subdivision;
+		/// <summary>
+		/// Подразделение, которому принадлежат права на склад
+		/// </summary>
 		[Display(Name = "Подразделение")]
-		public virtual Subdivision Subdivision {
+		public virtual SubdivisionEntity Subdivision
+		{
 			get => _subdivision;
 			set => SetField(ref _subdivision, value);
 		}
+
+		public override PermissionType PermissionType => PermissionType.Subdivision;
 	}
 }
