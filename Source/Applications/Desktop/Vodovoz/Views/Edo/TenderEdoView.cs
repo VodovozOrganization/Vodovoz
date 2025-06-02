adbebdfee87eaae45d1c799dfa6eea152aec8ec4
Art8m <9artbe@gmail.com>
2025-06-02 18:48:39 +0300

    Выгрузка кодов по тендеру

--- Source/Applications/Desktop/Vodovoz/Views/Edo/TenderEdoView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Edo/TenderEdoView.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Gamma.ColumnConfig;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Edo;
+
 namespace Vodovoz.Views.Edo
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class TenderEdoView : Gtk.Bin
+	public partial class TenderEdoView : TabViewBase<TenderEdoViewModel>
 	{
-		public TenderEdoView()
+		public TenderEdoView(TenderEdoViewModel viewModel)
+			: base(viewModel)
 		{
-			this.Build();
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			ylabelGeneratedInfo.Binding
+				.AddBinding(ViewModel, vm => vm.Info, w => w.Text)
+				.InitializeFromSource();
+
+			ytreeViewCodes.ColumnsConfig = FluentColumnsConfig<string>.Create()
+				.AddColumn("Код")
+				.AddTextRenderer(c => c)
+				.AddColumn("")
+				.Finish();
+
+			ytreeViewCodes.ItemsDataSource = ViewModel.Codes;
+
+			ybuttonExportCodes.BindCommand(ViewModel.ExportCodesCommand);
 		}
 	}
 }
