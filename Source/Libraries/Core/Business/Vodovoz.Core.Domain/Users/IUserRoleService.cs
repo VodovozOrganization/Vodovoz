258f848a414264c0a106dac65adf7eab64f26ca1
Sergey Efimov <efimov90@gmail.com>
2025-06-02 09:25:20 +0300

    UserRoleService

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Users/IUserRoleService.cs
@@ -0,0 +1,10 @@
+﻿using QS.DomainModel.UoW;
+
+namespace Vodovoz.Core.Domain.Users
+{
+	public interface IUserRoleService
+	{
+		void GrantDatabasePrivileges(IUnitOfWork uow, UserRole userRole);
+		string SearchingPatternFromUserGrants(string login);
+	}
+}
