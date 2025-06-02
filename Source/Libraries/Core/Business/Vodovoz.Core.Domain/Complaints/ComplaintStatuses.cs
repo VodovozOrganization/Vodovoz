6bc51f09452416fe0e56d0d5c89bed95e8072306
Sergey Efimov <efimov90@gmail.com>
2025-06-02 07:50:24 +0300

    ComplaintStatuses

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Complaints/ComplaintStatuses.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Complaints/ComplaintStatuses.cs
@@ -1,17 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Vodovoz.Domain.Complaints
+namespace Vodovoz.Core.Domain.Complaints
 {
+	/// <summary>
+	/// Статусы рекламаций
+	/// </summary>
 	public enum ComplaintStatuses
 	{
+		/// <summary>
+		/// Не взята в работу
+		/// </summary>
 		[Display(Name = "Не взята в работу")]
 		NotTakenInProcess,
+		/// <summary>
+		/// В работе
+		/// </summary>
 		[Display(Name = "В работе")]
 		InProcess,
+		/// <summary>
+		/// На проверке
+		/// </summary>
 		[Display(Name = "На проверке")]
 		Checking,
+		/// <summary>
+		/// Ожидает реакции
+		/// </summary>
 		[Display(Name = "Ожидает реакции")]
 		WaitingForReaction,
+		/// <summary>
+		/// Закрыта
+		/// </summary>
 		[Display(Name = "Закрыт")]
 		Closed
 	}
