b1a92353b6e025843420c8b56ccf8ab04f9447d9
Sergey Efimov <efimov90@gmail.com>
2025-06-04 13:35:13 +0300

    UI

---
+++ Source/Applications/Desktop/Vodovoz/Goods/Recomendations/RecomendationView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Goods.Recomendations
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class RecomendationView : Gtk.Bin
+	{
+		public RecomendationView()
+		{
+			this.Build();
+		}
+	}
+}
