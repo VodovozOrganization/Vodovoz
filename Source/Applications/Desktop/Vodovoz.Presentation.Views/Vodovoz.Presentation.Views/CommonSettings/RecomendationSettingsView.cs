7d30f8b9c1538d2c058b68408ed8ca88265ba490
Sergey Efimov <efimov90@gmail.com>
2025-06-05 09:14:46 +0300

    RecomendationsSettings

--- Source/Applications/Desktop/Vodovoz.Presentation.Views/Vodovoz.Presentation.Views/CommonSettings/RecomendationSettingsView.cs
+++ Source/Applications/Desktop/Vodovoz.Presentation.Views/Vodovoz.Presentation.Views/CommonSettings/RecomendationSettingsView.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using QS.Views.GtkUI;
+using System;
+using System.ComponentModel;
+using Vodovoz.Presentation.ViewModels.CommonSettings;
+
 namespace Vodovoz.Presentation.Views.CommonSettings
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class RecomendationSettingsView : Gtk.Bin
+	[ToolboxItem(true)]
+	public partial class RecomendationSettingsView : WidgetViewBase<RecomendationSettingsViewModel>
 	{
 		public RecomendationSettingsView()
 		{
-			this.Build();
+			Build();
+		}
+
+		protected override void ConfigureWidget()
+		{
+			base.ConfigureWidget();
+
+			ysbOperator.Binding.CleanSources();
+			ysbOperator.Binding
+				.AddBinding(ViewModel, vm => vm.OperatorCount, w => w.ValueAsInt)
+				.InitializeFromSource();
+
+			ysbRobot.Binding.CleanSources();
+			ysbRobot.Binding
+				.AddBinding(ViewModel, vm => vm.RobotCount, w => w.ValueAsInt)
+				.InitializeFromSource();
+
+			ysbIpz.Binding.CleanSources();
+			ysbIpz.Binding
+				.AddBinding(ViewModel, vm => vm.IpzCount, w => w.ValueAsInt)
+				.InitializeFromSource();
+
+			buttonSave.Clicked += OnSaveClicked;
+		}
+
+		private void OnSaveClicked(object sender, EventArgs e)
+		{
+			ViewModel?.SaveCommand.Execute(null);
 		}
 	}
 }
