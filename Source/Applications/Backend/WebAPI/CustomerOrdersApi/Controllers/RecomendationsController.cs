3bb31e400eda898ccd09ddb6ead57a5c55319b55
Sergey Efimov <efimov90@gmail.com>
2025-06-11 15:38:46 +0300

    CustomersOrdersApi

---
+++ Source/Applications/Backend/WebAPI/CustomerOrdersApi/Controllers/RecomendationsController.cs
@@ -0,0 +1,69 @@
+﻿using CustomerOrdersApi.Library.Dto.Orders;
+using CustomerOrdersApi.Library.Services;
+using Gamma.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Vodovoz.Results;
+using System;
+using System.Collections.Generic;
+
+namespace CustomerOrdersApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RecomendationsController : SignatureControllerBase
+	{
+		private readonly ICustomerOrdersService _customerOrdersService;
+
+		public RecomendationsController(
+			ILogger<RecomendationsController> logger,
+			ICustomerOrdersService customerOrdersService)
+			: base(logger)
+		{
+			_customerOrdersService = customerOrdersService
+				?? throw new ArgumentNullException(nameof(customerOrdersService));
+		}
+
+		[HttpGet]
+		public IActionResult GetRecommendations(GetRecomendationsDto getRecomendationsDto)
+		{
+			var sourceName = getRecomendationsDto.Source.GetEnumTitle();
+
+			try
+			{
+				Logger.LogInformation(
+					"Поступил запрос от {Source} на создание заявки на звонок c подписью {Signature}, проверяем...",
+					sourceName,
+					getRecomendationsDto.Signature);
+
+				if(!_customerOrdersService.ValidateRequestRecomendationsSignature(getRecomendationsDto, out var generatedSignature))
+				{
+					return InvalidSignature(getRecomendationsDto.Signature, generatedSignature);
+				}
+
+				Logger.LogInformation("Подпись валидна, получаем рекомендации");
+
+				return _customerOrdersService
+					.GetRecomendations(getRecomendationsDto)
+					.Match<IEnumerable<RecomendationItemDto>, Exception, IActionResult>(
+						recomendations => Ok(recomendations),
+						error =>
+						{
+							Logger.LogError(error, "Ошибка при запросе рекомендаций: {Message}",
+								error.Message);
+
+							return Problem(error.Message);
+						});
+			}
+			catch(Exception e)
+			{
+				Logger.LogError(e,
+					"Ошибка при сохранении заявки на звонок контакта {Phone} от {ErpCounterpartyId}",
+					getRecomendationsDto.ErpCounterpartyId,
+					sourceName);
+
+				return Problem();
+			}
+		}
+	}
+}
