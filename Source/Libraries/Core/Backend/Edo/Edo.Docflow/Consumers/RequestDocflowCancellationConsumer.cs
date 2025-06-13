ee34455316749ebcf9ce15ede14c0254707d117e
Vadim Gordeev <enzogord@gmail.com>
2025-06-13 21:05:41 +0300

    Сервис документооборота

---
+++ Source/Libraries/Core/Backend/Edo/Edo.Docflow/Consumers/RequestDocflowCancellationConsumer.cs
@@ -0,0 +1,32 @@
+﻿using Edo.Contracts.Messages.Events;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Edo.Docflow.Consumers
+{
+	public class RequestDocflowCancellationConsumer : IConsumer<RequestDocflowCancellationEvent>
+	{
+		private readonly ILogger<RequestDocflowCancellationConsumer> _logger;
+		private readonly DocflowHandler _docflowHandler;
+
+		public RequestDocflowCancellationConsumer(
+			ILogger<RequestDocflowCancellationConsumer> logger,
+			DocflowHandler docflowHandler
+			)
+		{
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			_docflowHandler = docflowHandler ?? throw new ArgumentNullException(nameof(docflowHandler));
+		}
+
+		public async Task Consume(ConsumeContext<RequestDocflowCancellationEvent> context)
+		{
+			await _docflowHandler.HandleTransferDocumentCancellation(
+				context.Message.TaskId, 
+				context.Message.Reason,
+				context.CancellationToken
+				);
+		}
+	}
+}
