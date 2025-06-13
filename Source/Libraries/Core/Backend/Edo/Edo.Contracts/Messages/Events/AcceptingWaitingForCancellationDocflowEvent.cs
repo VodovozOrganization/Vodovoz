79b73c7ec7fc9f47144a4b681f2fbe424cae5257
Vadim Gordeev <enzogord@gmail.com>
2025-06-13 21:03:16 +0300

    Новые ивенты

---
+++ Source/Libraries/Core/Backend/Edo/Edo.Contracts/Messages/Events/AcceptingWaitingForCancellationDocflowEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Edo.Contracts.Messages.Events
+{
+	/// <summary>
+	/// Событие для подписания входящего документооборота
+	/// </summary>
+	public class AcceptingWaitingForCancellationDocflowEvent
+	{
+		public const string Event = "accepting-waiting-for-cancellation-docflow-event";
+
+		/// <summary>
+		/// Код кабинета в ЭДО
+		/// </summary>
+		public string EdoAccount { get; set; }
+
+		/// <summary>
+		/// Id документооборота
+		/// </summary>
+		public string DocFlowId { get; set; }
+	}
+}
---
+++ Source/Libraries/Core/Backend/Edo/Edo.Contracts/Messages/Events/RequestDocflowCancellationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Edo.Contracts.Messages.Events
+{
+	public class RequestDocflowCancellationEvent
+	{
+		public int TaskId { get; set; }
+		public string Reason { get; set; }
+	}
+}
