8efe772b8127c4c47cbd8d13e4ec75f86fd00919
Sergey Efimov <efimov90@gmail.com>
2025-06-16 11:02:07 +0300

    Фикс

--- Source/Applications/Backend/WebAPI/RobotMia/Api/Controllers/V1/RecommendedProductsController.cs
+++ Source/Applications/Backend/WebAPI/RobotMia/Api/Controllers/V1/RecommendedProductsController.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using QS.DomainModel.UoW;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
