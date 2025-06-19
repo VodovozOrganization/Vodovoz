68505431b86e2a93800dfa679045869f58c990dc
Andrey <as1854@yandex.ru>
2025-06-19 09:46:37 +0300

    5309 вынес в отдельный виджет контейнер по аккаунты с матрицей

---
+++ Source/Applications/Desktop/Vodovoz/Views/Client/CounterpartyEdoAccountsByOrganizationView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Vodovoz.Views.Client
+{
+	[ToolboxItem(true)]
+	public partial class CounterpartyEdoAccountsByOrganizationView : Gtk.Bin
+	{
+		public CounterpartyEdoAccountsByOrganizationView()
+		{
+			Build();
+		}
+	}
+}
