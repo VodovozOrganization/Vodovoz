5dc4eb3b806a87fa91ee0983ab87377874512605
N-Sem <nikiforov.semen@gmail.com>
2025-06-27 01:13:13 +0300

    Класс промежуточных кодов и мапинг

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Data.NHibernate/Mapping/TrueMark/TrueMarkCodeStagingMap.cs
@@ -0,0 +1,31 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Core.Domain.Edo;
+
+namespace Vodovoz.Core.Data.NHibernate.Mapping.TrueMark
+{
+	public class TrueMarkCodeStagingMap : ClassMap<TrueMarkCodeStaging>
+	{
+		public TrueMarkCodeStagingMap()
+		{
+			Table("true_mark_code_staging");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.ParentCodeId).Column("parent_code_id");
+			Map(x => x.RawCode).Column("raw_code");
+			Map(x => x.GTIN).Column("gtin");
+			Map(x => x.SerialNumber).Column("serial_number");
+			Map(x => x.CheckCode).Column("check_code");
+			Map(x => x.CodeType).Column("code_type");
+			Map(x => x.RelatedDocumentType).Column("related_document_type");
+			Map(x => x.RelatedDocumentId).Column("related_document_id");
+
+			HasMany(x => x.InnerCodes)
+				.KeyColumn("parent_code_id")
+				.Not.LazyLoad()
+				.Fetch.Subselect()
+				.Inverse()
+				.Cascade.All();
+		}
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Edo/TrueMarkCodeStaging.cs
@@ -0,0 +1,206 @@
+﻿using QS.DomainModel.Entity;
+using QS.Extensions.Observable.Collections.List;
+using QS.HistoryLog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vodovoz.Core.Domain.Edo
+{
+	/// <summary>
+	/// Код честного знака для промежуточного хранения
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Masculine,
+		Nominative = "код ЧЗ для промежуточного хранения",
+		NominativePlural = "коды ЧЗ для промежуточного хранения",
+		Prepositional = "коде ЧЗ для промежуточного хранения",
+		PrepositionalPlural = "кодах ЧЗ для промежуточного хранения",
+		Accusative = "код ЧЗ для промежуточного хранения",
+		AccusativePlural = "коды ЧЗ для промежуточного хранения",
+		Genitive = "коде ЧЗ для промежуточного хранения",
+		GenitivePlural = "коды ЧЗ для промежуточного хранения")]
+	[HistoryTrace]
+	public class TrueMarkCodeStaging : PropertyChangedBase, IDomainObject
+	{
+		private int _id;
+		private string _rawCode;
+		private string _gtin;
+		private string _serialNumber;
+		private string _checkCode;
+		private TrueMarkCodeStagingType _codeType;
+		private TrueMarkCodeStagingRelatedDocumentType _relatedDocumentType;
+		private int _relatedDocumentId;
+
+		/// <summary>
+		/// Идентификатор
+		/// </summary>
+		public virtual int Id
+		{
+			get => _id;
+			set
+			{
+				if(SetField(ref _id, value))
+				{
+					UpdateChildCodes();
+				}
+			}
+		}
+
+		public virtual int? ParentCodeId { get; set; }
+
+		/// <summary>
+		/// Необработанный код
+		/// </summary>
+		[Display(Name = "Необработанный код")]
+		public virtual string RawCode
+		{
+			get => _rawCode;
+			set => SetField(ref _rawCode, value);
+		}
+
+		/// <summary>
+		/// Код GTIN
+		/// </summary>
+		[Display(Name = "Код GTIN")]
+		public virtual string GTIN
+		{
+			get => _gtin;
+			set => SetField(ref _gtin, value);
+		}
+
+		/// <summary>
+		/// Серийный номер экземпляра
+		/// </summary>
+		[Display(Name = "Серийный номер экземпляра")]
+		public virtual string SerialNumber
+		{
+			get => _serialNumber;
+			set => SetField(ref _serialNumber, value);
+		}
+
+		/// <summary>
+		/// Код проверки
+		/// </summary>
+		[Display(Name = "Код проверки")]
+		public virtual string CheckCode
+		{
+			get => _checkCode;
+			set => SetField(ref _checkCode, value);
+		}
+
+		/// <summary>
+		/// Тип кода
+		/// </summary>
+		[Display(Name = "Тип кода")]
+		public virtual TrueMarkCodeStagingType CodeType
+		{
+			get => _codeType;
+			set => SetField(ref _codeType, value);
+		}
+
+		/// <summary>
+		/// Тип связанного документа
+		/// </summary>
+		[Display(Name = "Тип связанного документа")]
+		public virtual TrueMarkCodeStagingRelatedDocumentType RelatedDocumentType
+		{
+			get => _relatedDocumentType;
+			set => SetField(ref _relatedDocumentType, value);
+		}
+
+		/// <summary>
+		/// Id связанного документа
+		/// </summary>
+		[Display(Name = "Id связанного документа")]
+		public virtual int RelatedDocumentId
+		{
+			get => _relatedDocumentId;
+			set => SetField(ref _relatedDocumentId, value);
+		}
+
+		public virtual string IdentificationCode =>
+			CodeType == TrueMarkCodeStagingType.Transport
+			? RawCode
+			: $"01{GTIN}21{SerialNumber}";
+
+		public virtual IObservableList<TrueMarkCodeStaging> InnerCodes { get; set; }
+			= new ObservableList<TrueMarkCodeStaging>();
+
+		public virtual void AddInnerCode(TrueMarkCodeStaging innerCode)
+		{
+			innerCode.ParentCodeId = Id;
+			InnerCodes.Add(innerCode);
+		}
+
+		public virtual void UpdateChildCodes()
+		{
+			foreach(var innerCode in InnerCodes)
+			{
+				innerCode.ParentCodeId = Id;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Тип кода ЧЗ для промежуточного хранения
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Masculine,
+		Nominative = "тип кода ЧЗ для промежуточного хранения",
+		NominativePlural = "типы кодов ЧЗ для промежуточного хранения",
+		Prepositional = "типе кода ЧЗ для промежуточного хранения",
+		PrepositionalPlural = "типах кодов ЧЗ для промежуточного хранения",
+		Accusative = "тип кода ЧЗ для промежуточного хранения",
+		AccusativePlural = "типы кодов ЧЗ для промежуточного хранения",
+		Genitive = "типа кода ЧЗ для промежуточного хранения",
+		GenitivePlural = "типы кодов ЧЗ для промежуточного хранения")]
+	public enum TrueMarkCodeStagingType
+	{
+		/// <summary>
+		/// Код экземпляра
+		/// </summary>
+		[Display(Name = "Код экземпляра")]
+		Identification,
+		/// <summary>
+		/// Групповой код
+		/// </summary>
+		[Display(Name = "Групповой код")]
+		Group,
+		/// <summary>
+		/// Транспортный код
+		/// </summary>
+		[Display(Name = "Транспортный код")]
+		Transport
+	}
+
+	/// <summary>
+	/// Тип связанного документа кода ЧЗ для промежуточного хранения
+	/// </summary>
+	[Appellative(
+		Gender = GrammaticalGender.Masculine,
+		Nominative = "тип связанного документа кода ЧЗ для промежуточного хранения",
+		NominativePlural = "типы связанных документов кодов ЧЗ для промежуточного хранения",
+		Prepositional = "типе связанного документа кода ЧЗ для промежуточного хранения",
+		PrepositionalPlural = "типах связанных документов кодов ЧЗ для промежуточного хранения",
+		Accusative = "тип связанного документа кода ЧЗ для промежуточного хранения",
+		AccusativePlural = "типы связанных документов кодов ЧЗ для промежуточного хранения",
+		Genitive = "типе связанного документа кода ЧЗ для промежуточного хранения",
+		GenitivePlural = "типах связанных документов кодов ЧЗ для промежуточного хранения")]
+	public enum TrueMarkCodeStagingRelatedDocumentType
+	{
+		/// <summary>
+		/// Строка талона погрузки
+		/// </summary>
+		[Display(Name = "Строка талона погрузки")]
+		CarLoadDocumentItem,
+		/// <summary>
+		/// Строка маршрутного листа
+		/// </summary>
+		[Display(Name = "Строка маршрутного листа")]
+		RouteListItem,
+		/// <summary>
+		/// Строка документа самовывоза
+		/// </summary>
+		[Display(Name = "Строка документа самовывоза")]
+		SelfDeliveryDocumentItem
+	}
+}
