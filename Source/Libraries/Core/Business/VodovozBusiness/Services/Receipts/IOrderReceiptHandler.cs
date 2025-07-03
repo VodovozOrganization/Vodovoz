a63aea619634563b0ddebdd75059b8127fe99aae
Andrey <as1854@yandex.ru>
2025-07-03 13:28:47 +0300

    еще одна порция док блоков

--- Source/Libraries/Core/Business/VodovozBusiness/Services/Receipts/IOrderReceiptHandler.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Services/Receipts/IOrderReceiptHandler.cs
@@ -3,9 +3,22 @@ using Vodovoz.Domain.Orders;
 
 namespace VodovozBusiness.Services.Receipts
 {
+	/// <summary>
+	/// Контракт сервиса по работе с чеками в ДВ
+	/// </summary>
 	public interface IOrderReceiptHandler
 	{
+		/// <summary>
+		/// Обновление чеков
+		/// </summary>
+		/// <param name="uow">UnitOfWork</param>
+		/// <param name="order">Заказ</param>
 		void RenewOrderCashReceipts(IUnitOfWork uow, Order order);
+		/// <summary>
+		/// Проверка необходимости отправки чека
+		/// </summary>
+		/// <param name="orderId">Id заказа</param>
+		/// <returns><c>true</c> - нужен чек, <c>false</c> - чек не нужен</returns>
 		bool HasNeededReceipt(int orderId);
 	}
 }
