dcf78b17566036d137b8e6d632f155353bce838d
N-Sem <nikiforov.semen@gmail.com>
2025-07-14 11:51:08 +0300

    Правка отображения поля установленных фильтров

--- Source/Applications/Desktop/VodovozViewModels/ReportsParameters/Orders/FirstSecondClientReportViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/ReportsParameters/Orders/FirstSecondClientReportViewModel.cs
@@ -1,19 +1,24 @@
 ﻿using QS.Commands;
+using QS.Dialog;
 using QS.DomainModel.UoW;
 using QS.Project.Journal.EntitySelector;
 using QS.Report;
 using QS.Report.ViewModels;
+using QS.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Vodovoz.Domain.Employees;
 using Vodovoz.Domain.Orders;
 using Vodovoz.EntityRepositories.DiscountReasons;
+using Vodovoz.Extensions;
 using Vodovoz.TempAdapters;
 
 namespace Vodovoz.ViewModels.ReportsParameters.Orders
 {
 	public class FirstSecondClientReportViewModel : ReportParametersUowViewModelBase
 	{
+		private readonly ICommonServices _commonServices;
 		private readonly IDiscountReasonRepository _discountReasonRepository;
 
 		private DateTime? _startDate;
@@ -30,11 +35,13 @@ namespace Vodovoz.ViewModels.ReportsParameters.Orders
 			RdlViewerViewModel rdlViewerViewModel,
 			IReportInfoFactory reportInfoFactory,
 			IUnitOfWorkFactory uowFactory,
+			ICommonServices commonServices,
 			IEmployeeJournalFactory employeeJournalFactory,
 			IDiscountReasonRepository discountReasonRepository
 		) : base(rdlViewerViewModel, uowFactory, reportInfoFactory)
 		{
 			var employeesFactory = employeeJournalFactory ?? throw new ArgumentNullException(nameof(employeeJournalFactory));
+			_commonServices = commonServices ?? throw new ArgumentNullException(nameof(commonServices));
 			_discountReasonRepository = discountReasonRepository ?? throw new ArgumentNullException(nameof(discountReasonRepository));
 
 			Title = "Отчёт по первичным/вторичным заказам";
@@ -47,7 +54,7 @@ namespace Vodovoz.ViewModels.ReportsParameters.Orders
 
 			DiscountReasons = _discountReasonRepository.GetDiscountReasons(UoW);
 
-			GenerateReportCommand = new DelegateCommand(LoadReport);
+			GenerateReportCommand = new DelegateCommand(GenerateReport);
 
 			_firstOrderStatuses = SelectedByDefaultOrderStatuses;
 			_secondOrderStatuses = SelectedByDefaultOrderStatuses;
@@ -114,24 +121,62 @@ namespace Vodovoz.ViewModels.ReportsParameters.Orders
 		private IList<OrderStatus> SelectedByDefaultOrderStatuses =>
 			new List<OrderStatus> { OrderStatus.Shipped, OrderStatus.UnloadingOnStock, OrderStatus.Closed };
 
+		private void GenerateReport()
+		{
+			if(StartDate is null || EndDate is null)
+			{
+				_commonServices.InteractiveService.ShowMessage(ImportanceLevel.Warning, "Необходимо указать период");
+			}
+
+			LoadReport();
+		}
+
 		protected override Dictionary<string, object> Parameters
 		{
 			get
 			{
 				var parameters = new Dictionary<string, object>
-					{
-						{ "start_date", StartDate },
-						{ "end_date", EndDate },
-						{ "discount_id", DiscountReason?.Id ?? 0 },
-						{ "show_only_client_with_one_order", ShowOnlyClientsWithOneOrder },
-						{ "author_employer_id", Author?.Id ?? 0 },
-						{ "has_promo_set", HasPromoset },
-						{ "first_order_statuses", FirstOrderStatuses },
-						{ "second_order_statuses", SecondOrderStatuses }
+				{
+					{ "start_date", StartDate.Value.Date },
+					{ "end_date", EndDate.Value.Date },
+					{ "discount_id", DiscountReason?.Id ?? 0 },
+					{ "has_promo_set", HasPromoset },
+					{ "show_only_client_with_one_order", ShowOnlyClientsWithOneOrder },
+					{ "author_employer_id", Author?.Id ?? 0 },
+					{ "first_order_statuses", FirstOrderStatuses },
+					{ "second_order_statuses", SecondOrderStatuses },
+					{ "selected_filters_info", SelectedFiltersInfo }
 					};
 
 				return parameters;
 			}
 		}
+
+		private string SelectedFiltersInfo =>
+			$"Период даты доставки заказа: {StartDate.Value.Date : dd.MM.yyyy} - {EndDate.Value.Date: dd.MM.yyyy}\n" +
+			$"Основание скидки первого заказа: {DiscountReasonInfo}\n" +
+			$"Только с промонаборами: {HasPromosetInfo}\n" +
+			$"Только клиенты с одним заказом: {ShowOnlyClientsWithOneOrderInfo}\n" +
+			$"Автор первого заказа: {AuthorInfo}\n" +
+			$"Выбранные статусы для первого заказа: {string.Join(", ", FirstOrderStatuses.Select(x => x.GetEnumDisplayName()))}\n" +
+			$"Выбранные статусы для второго заказа: {string.Join(", ", SecondOrderStatuses.Select(x => x.GetEnumDisplayName()))}";
+
+		private string DiscountReasonInfo =>
+			DiscountReason is null
+			? "Не выбрано"
+			: DiscountReason.Name;
+
+		private string HasPromosetInfo =>
+			HasPromoset
+			? "Да"
+			: "Нет";
+
+		private string ShowOnlyClientsWithOneOrderInfo =>
+			ShowOnlyClientsWithOneOrder
+			? "Да"
+			: "Нет";
+
+		private string AuthorInfo =>
+			Author is null ? "Не выбрано" : $"{Author.LastName} {Author.Name} {Author.Patronymic}";
 	}
 }
