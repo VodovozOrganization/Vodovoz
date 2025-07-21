b2ef9789a7d2c0de355f57e34d3da5e7e9947989
Art8m <9artbe@gmail.com>
2025-07-22 00:06:45 +0300

    фиксы десктопа

--- Source/Applications/Desktop/VodovozViewModels/ViewModels/Warehouses/IncomingInvoiceViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Warehouses/IncomingInvoiceViewModel.cs
@@ -37,6 +37,7 @@ using Vodovoz.ViewModels.Journals.JournalViewModels.Goods;
 using Vodovoz.ViewModels.Journals.JournalViewModels.Nomenclatures;
 using Vodovoz.ViewModels.Journals.JournalViewModels.Store;
 using Vodovoz.ViewModels.ViewModels.Goods;
+using IWarehousePermissionService = Vodovoz.Infrastructure.Services.IWarehousePermissionService;
 using Order = Vodovoz.Domain.Orders.Order;
 
 namespace Vodovoz.ViewModels.Warehouses
