27fdf84bdf2f24ab97c657f5a7bf6d55ee522ff5
Semen Svobodin <work.semen.sd@gmail.com>
2025-07-22 19:21:23 +0700

    Сделал криво-косо TreeItems

--- Source/Applications/Desktop/Vodovoz/ViewModels/Logistic/SelfDeliveringOrderEditViewModel.cs
+++ Source/Applications/Desktop/Vodovoz/ViewModels/Logistic/SelfDeliveringOrderEditViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using Autofac;
+using Gamma.GtkWidgets;
+using Gtk;
 using QS.Commands;
+using QS.Dialog;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
 using QS.Project.Journal.EntitySelector;
+using QS.Project.Services;
 using QS.Services;
 using QS.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using Vodovoz.Controllers;
 using Vodovoz.Domain.Orders;
 using Vodovoz.Domain.Sale;
 using Vodovoz.TempAdapters;
@@ -18,38 +24,59 @@ namespace Vodovoz.ViewModels.Logistic
 {
 	public class SelfDeliveringOrderEditViewModel : EntityTabViewModelBase<Order>
 	{
+		private readonly IOrderDiscountsController _discountsController;
+		private readonly IInteractiveService _interactiveService;
+		private readonly ICurrentPermissionService _currentPermissionService;
+
+		private bool _canChangeDiscountValue;
+
+		public yTreeView TreeItems { get; set; }
+		public DelegateCommand SaveCommand { get; }
+		public DelegateCommand CloseCommand { get; }
+		public DelegateCommand PaymentTypeCommand { get; }
+		public IEntityAutocompleteSelectorFactory CounterpartyAutocompleteSelectorFactory { get; }
+		public IOrderDiscountsController DiscountsController => _discountsController;
+		public bool CanChangeDiscountValue => _canChangeDiscountValue;
 		public SelfDeliveringOrderEditViewModel(
 
 			ILifetimeScope lifetimeScope,
-			IEntityUoWBuilder uowBuilder, 
-			IUnitOfWorkFactory unitOfWorkFactory, 
+			IEntityUoWBuilder uowBuilder,
+			IUnitOfWorkFactory unitOfWorkFactory,
 			ICommonServices commonServices,
 			ICounterpartyJournalFactory counterpartyJournalFactory,
+			IOrderDiscountsController discountsController,
+			IInteractiveService interactiveService,
+			ICurrentPermissionService currentPermissionService,
 			INavigationManager navigation = null) : base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
 		{
 			CounterpartyAutocompleteSelectorFactory =
 				(counterpartyJournalFactory ?? throw new ArgumentNullException(nameof(counterpartyJournalFactory)))
 				.CreateCounterpartyAutocompleteSelectorFactory(lifetimeScope);
+			_discountsController = discountsController ?? throw new ArgumentNullException(nameof(discountsController));
+			_interactiveService = interactiveService ?? throw new ArgumentNullException(nameof(interactiveService));
+			_currentPermissionService = currentPermissionService ?? throw new ArgumentNullException(nameof(currentPermissionService));
+
 			TabName = "Редактирование самовывоза";
-			var t = Entity.GetNomenclaturesWithFixPrices;
-			var e = Entity;
-			
+
 			SaveCommand = new DelegateCommand(
 				() => Save(),
 				() => BeforeSave());
 			CloseCommand = new DelegateCommand(() => Console.WriteLine());
 			PaymentTypeCommand = new DelegateCommand(() => Console.WriteLine());
 
+			SetPermissions();
 		}
-		public DelegateCommand SaveCommand { get; }
-		public DelegateCommand CloseCommand { get; }
-		public DelegateCommand PaymentTypeCommand { get; }
-		public IEntityAutocompleteSelectorFactory CounterpartyAutocompleteSelectorFactory { get; }
 
-		protected override bool BeforeSave()
+		public void OnSpinPriceEdited(object o, EditedArgs args)
 		{
+			decimal.TryParse(args.NewText, NumberStyles.Any, CultureInfo.InvariantCulture, out var newPrice);
+			var node = TreeItems.YTreeModel.NodeAtPath(new TreePath(args.Path));
+			if(!(node is OrderItem orderItem))
+			{
+				return;
+			}
 
-			return base.BeforeSave();
+			orderItem.SetPrice(newPrice);
 		}
 		public IEnumerable<GeoGroup> GetSelfDeliveryGeoGroups()
 		{
@@ -58,6 +85,48 @@ namespace Vodovoz.ViewModels.Logistic
 			var geoGroups = UoW.GetAll<GeoGroup>().Where(geo => !geo.IsArchived || geo.Id == currentGeoGroupId).ToList();
 
 			return geoGroups;
+		}
+
+		public void OnDiscountReasonComboEdited(object o, EditedArgs args)
+		{
+			var index = int.Parse(args.Path);
+			var node = TreeItems.YTreeModel.NodeAtPath(new TreePath(args.Path));
+			if(!(node is OrderItem orderItem))
+			{
+				return;
+			}
+
+			var previousDiscountReason = orderItem.DiscountReason;
+
+			//Дополнительно проверяем основание скидки на null, т.к при двойном щелчке
+			//комбо-бокс не откроется, но событие сработает и прилетит null
+			if(orderItem.DiscountReason != null)
+			{
+				if(!_discountsController.SetDiscountFromDiscountReasonForOrderItem(
+					orderItem.DiscountReason, orderItem, _canChangeDiscountValue, out string message))
+				{
+					orderItem.DiscountReason = previousDiscountReason;
+				}
+
+				if(message != null)
+				{
+					_interactiveService.ShowMessage(ImportanceLevel.Warning,
+						$"На позицию:\n№{index + 1} {message}нельзя применить скидку," +
+						" т.к. она из промонабора или на нее есть фикса.\nОбратитесь к руководителю");
+				}
+			}
+
+			Entity?.RecalculateItemsPrice();
+		}
+		protected override bool BeforeSave()
+		{
+
+			return base.BeforeSave();
+		}
+		private void SetPermissions()
+		{
+			_canChangeDiscountValue = _currentPermissionService.ValidatePresetPermission("can_set_direct_discount_value");
+
 		}
 	}
 }
--- Source/Applications/Desktop/Vodovoz/Views/Logistic/SelfDeliveringOrderEditView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/SelfDeliveringOrderEditView.cs
@@ -1,9 +1,18 @@
-﻿using FluentNHibernate.Data;
+﻿using DocumentFormat.OpenXml.Presentation;
+using FluentNHibernate.Data;
+using Gamma.GtkWidgets.Cells;
+using Gtk;
+using QS.Utilities;
 using QS.Views.GtkUI;
 using QSWidgetLib;
 using ReactiveUI;
 using System.Collections.Generic;
+using System.Linq;
+using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Goods;
+using Vodovoz.Domain.Orders;
 using Vodovoz.Domain.Sale;
+using Vodovoz.EntityRepositories.Orders;
 using Vodovoz.Infrastructure.Converters;
 using Vodovoz.ViewModels.Logistic;
 namespace Vodovoz.Views.Logistic
@@ -19,15 +28,20 @@ namespace Vodovoz.Views.Logistic
 
 		private void Configure()
 		{
+			ViewModel.TreeItems = treeItems;
+
 			ybuttonSave.BindCommand(ViewModel.SaveCommand);
 			ybuttonCancel.BindCommand(ViewModel.CloseCommand);
 			
 			entityVMEntryClient1.SetEntityAutocompleteSelectorFactory(ViewModel.CounterpartyAutocompleteSelectorFactory);
-			entityVMEntryClient1.Binding.AddBinding(ViewModel.Entity, s => s.Client, w => w.Subject).InitializeFromSource();
+			entityVMEntryClient1.Binding
+				.AddBinding(ViewModel.Entity, s => s.Client, w => w.Subject)
+				.InitializeFromSource();
 			//Проверку сделать?
 			//entityVMEntryClient1.CanEditReference = !ViewModel.UserHasOnlyAccessToWarehouseAndComplaints;
 
-			ycheckbuttonPaymentAfterShipment.Binding.AddBinding(ViewModel.Entity, e => e.PayAfterShipment, w => w.Active)
+			ycheckbuttonPaymentAfterShipment.Binding
+				.AddBinding(ViewModel.Entity, e => e.PayAfterShipment, w => w.Active)
 				.InitializeFromSource();
 			//Не Работает
 			//specialListCmbSelfDeliveryGeoGroup.Binding.AddBinding(ViewModel.Entity, e => e.SelfDeliveryGeoGroup, w => w.SelectedItem)
@@ -39,17 +53,95 @@ namespace Vodovoz.Views.Logistic
 				.AddBinding(e => e.SelfDelivery, w => w.Visible)
 				.InitializeFromSource();
 
-			yentryPaymentType.Binding.AddBinding(ViewModel.Entity, e => e.PaymentType, w => w.Text)
-				.InitializeFromSource();
+			//yentryPaymentType.Binding
+			//	.AddBinding(ViewModel.Entity, e => e.PaymentType, w => w.Text)
+			//	.InitializeFromSource();
 
 			buttonSelectPaymentType.BindCommand(ViewModel.PaymentTypeCommand);
 
-			// Не Работает
+			treeItems.CreateFluentColumnsConfig<OrderItem>()
+				.AddColumn("№")
+					.HeaderAlignment(0.5f)
+					.AddNumericRenderer(node => ViewModel.Entity.OrderItems.IndexOf(node) + 1)
+				.AddColumn("Номенклатура")
+					.SetTag(nameof(Nomenclature))
+					.HeaderAlignment(0.5f)
+					.AddTextRenderer(node => node.NomenclatureString)
+				.AddColumn("Цена")
+					.HeaderAlignment(0.5f)
+					.AddNumericRenderer(node => node.Price).Digits(2).WidthChars(10)
+					.Adjustment(new Adjustment(0, 0, 1000000, 1, 100, 0)).Editing(true)
+					.AddSetter((c, node) => c.Editable = node.CanEditPrice)
+					.EditedEvent(ViewModel.OnSpinPriceEdited)
+					.AddTextRenderer(node => CurrencyWorks.CurrencyShortName, false)
+				.AddColumn("Альтерн.\nцена")
+					.AddToggleRenderer(x => x.IsAlternativePrice).Editing(false)
+				.AddColumn("Сумма")
+					.HeaderAlignment(0.5f)
+					.AddTextRenderer(node => CurrencyWorks.GetShortCurrencyString(node.ActualSum))
+					.AddSetter((c, n) =>
+					{
+						if(ViewModel.Entity.OrderStatus == OrderStatus.DeliveryCanceled || ViewModel.Entity.OrderStatus == OrderStatus.NotDelivered)
+							c.Text = CurrencyWorks.GetShortCurrencyString(n.OriginalSum);
+					}
+					)
+				.AddColumn("Скидка")
+					.HeaderAlignment(0.5f)
+					.AddNumericRenderer(node => node.ManualChangingDiscount)
+					.AddSetter((c, n) => c.Editable = ViewModel.CanChangeDiscountValue)
+					.AddSetter(
+						(c, n) => c.Adjustment = n.IsDiscountInMoney
+									? new Adjustment(0, 0, (double)(n.Price * n.CurrentCount), 1, 100, 1)
+									: new Adjustment(0, 0, 100, 1, 100, 1)
+					)
+					.AddSetter((c, n) =>
+					{
+						if(ViewModel.Entity.OrderStatus == OrderStatus.DeliveryCanceled || ViewModel.Entity.OrderStatus == OrderStatus.NotDelivered)
+						{
+							c.Text = n.ManualChangingOriginalDiscount.ToString();
+						}
+					})
+					.Digits(2)
+					.WidthChars(10)
+					.AddTextRenderer(n => n.IsDiscountInMoney ? CurrencyWorks.CurrencyShortName : "%", false)
+				.AddColumn("Скидка \nв рублях?")
+					.AddToggleRenderer(x => x.IsDiscountInMoney)
+					.AddSetter((c, n) => c.Activatable = ViewModel.CanChangeDiscountValue)
+				.AddColumn("Основание скидки")
+					.HeaderAlignment(0.5f)
+					.AddComboRenderer(x => x.DiscountReason)
+					.SetDisplayFunc(x => x.Name)
+					.DynamicFillListFunc(item =>
+					{
+						var orderReasons = ViewModel.Entity.OrderItems
+							.Select(oi => oi.DiscountReason)
+							.Where(dr => dr != null)
+							.Distinct()
+							.ToList();
+
+						return orderReasons;
+					})
+					.EditedEvent(ViewModel.OnDiscountReasonComboEdited)
+					.AddSetter((cell, node) => cell.Editable = node.DiscountByStock == 0)
+					//.AddSetter(
+					//	(c, n) =>
+					//		c.BackgroundGdk = n.Discount > 0 && n.DiscountReason == null && n.PromoSet == null ? colorLightRed : colorPrimaryBase
+					//)
+					.AddSetter((c, n) =>
+					{
+						if(n.PromoSet != null && n.DiscountReason == null && n.Discount > 0)
+						{
+							c.Text = n.PromoSet.DiscountReasonInfo;
+						}
+						else if(ViewModel.Entity.OrderStatus == OrderStatus.DeliveryCanceled || ViewModel.Entity.OrderStatus == OrderStatus.NotDelivered)
+						{
+							c.Text = n.OriginalDiscountReason?.Name ?? n.DiscountReason?.Name;
+						}
+					})
+				.RowCells()
+					.XAlign(0.5f)
+				.Finish();
 			treeItems.ItemsDataSource = ViewModel.Entity.ObservableOrderItems;
-            treeItems.Binding.AddBinding(ViewModel.Entity, 
-                e => e.OrderItems,
-                w => w.ItemsDataSource)
-                .InitializeFromSource();
 
             // Переделать под yEntryPaymentNumber
             /*yEntryPaymentNumber.ValidationMode = (QS.Widgets.ValidationType)ValidationType.numeric;
