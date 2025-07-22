c6146b15eb79be2843873724f8cfb8ee05c71dfb
Semen Svobodin <work.semen.sd@gmail.com>
2025-07-22 17:30:11 +0700

    minor

--- Source/Applications/Desktop/Vodovoz/ViewModels/Logistic/SelfDeliveringOrderEditViewModel.cs
+++ Source/Applications/Desktop/Vodovoz/ViewModels/Logistic/SelfDeliveringOrderEditViewModel.cs
@@ -1,33 +1,63 @@
-﻿using QS.Commands;
+﻿using Autofac;
+using QS.Commands;
 using QS.DomainModel.UoW;
 using QS.Navigation;
 using QS.Project.Domain;
+using QS.Project.Journal.EntitySelector;
 using QS.Services;
 using QS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using Vodovoz.Domain.Orders;
+using Vodovoz.Domain.Sale;
+using Vodovoz.TempAdapters;
 
 namespace Vodovoz.ViewModels.Logistic
 {
 	public class SelfDeliveringOrderEditViewModel : EntityTabViewModelBase<Order>
 	{
 		public SelfDeliveringOrderEditViewModel(
+
+			ILifetimeScope lifetimeScope,
 			IEntityUoWBuilder uowBuilder, 
 			IUnitOfWorkFactory unitOfWorkFactory, 
 			ICommonServices commonServices,
+			ICounterpartyJournalFactory counterpartyJournalFactory,
 			INavigationManager navigation = null) : base(uowBuilder, unitOfWorkFactory, commonServices, navigation)
 		{
+			CounterpartyAutocompleteSelectorFactory =
+				(counterpartyJournalFactory ?? throw new ArgumentNullException(nameof(counterpartyJournalFactory)))
+				.CreateCounterpartyAutocompleteSelectorFactory(lifetimeScope);
 			TabName = "Редактирование самовывоза";
 			var t = Entity.GetNomenclaturesWithFixPrices;
 			var e = Entity;
-		
+			
+			SaveCommand = new DelegateCommand(
+				() => Save(),
+				() => BeforeSave());
+			CloseCommand = new DelegateCommand(() => Console.WriteLine());
+			PaymentTypeCommand = new DelegateCommand(() => Console.WriteLine());
+
 		}
 		public DelegateCommand SaveCommand { get; }
 		public DelegateCommand CloseCommand { get; }
+		public DelegateCommand PaymentTypeCommand { get; }
+		public IEntityAutocompleteSelectorFactory CounterpartyAutocompleteSelectorFactory { get; }
 
 		protected override bool BeforeSave()
 		{
 
 			return base.BeforeSave();
+		}
+		public IEnumerable<GeoGroup> GetSelfDeliveryGeoGroups()
+		{
+			var currentGeoGroupId = Entity?.SelfDeliveryGeoGroup?.Id;
+
+			var geoGroups = UoW.GetAll<GeoGroup>().Where(geo => !geo.IsArchived || geo.Id == currentGeoGroupId).ToList();
+
+			return geoGroups;
 		}
 	}
 }
--- Source/Applications/Desktop/Vodovoz/Views/Logistic/SelfDeliveringOrderEditView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/SelfDeliveringOrderEditView.cs
@@ -1,4 +1,10 @@
-﻿using QS.Views.GtkUI;
+﻿using FluentNHibernate.Data;
+using QS.Views.GtkUI;
+using QSWidgetLib;
+using ReactiveUI;
+using System.Collections.Generic;
+using Vodovoz.Domain.Sale;
+using Vodovoz.Infrastructure.Converters;
 using Vodovoz.ViewModels.Logistic;
 namespace Vodovoz.Views.Logistic
 {
@@ -13,6 +19,46 @@ namespace Vodovoz.Views.Logistic
 
 		private void Configure()
 		{
-		}
+			ybuttonSave.BindCommand(ViewModel.SaveCommand);
+			ybuttonCancel.BindCommand(ViewModel.CloseCommand);
+			
+			entityVMEntryClient1.SetEntityAutocompleteSelectorFactory(ViewModel.CounterpartyAutocompleteSelectorFactory);
+			entityVMEntryClient1.Binding.AddBinding(ViewModel.Entity, s => s.Client, w => w.Subject).InitializeFromSource();
+			//Проверку сделать?
+			//entityVMEntryClient1.CanEditReference = !ViewModel.UserHasOnlyAccessToWarehouseAndComplaints;
+
+			ycheckbuttonPaymentAfterShipment.Binding.AddBinding(ViewModel.Entity, e => e.PayAfterShipment, w => w.Active)
+				.InitializeFromSource();
+			//Не Работает
+			//specialListCmbSelfDeliveryGeoGroup.Binding.AddBinding(ViewModel.Entity, e => e.SelfDeliveryGeoGroup, w => w.SelectedItem)
+			//	.InitializeFromSource();
+
+			specialListCmbSelfDeliveryGeoGroup.ItemsList = ViewModel.GetSelfDeliveryGeoGroups();
+			specialListCmbSelfDeliveryGeoGroup.Binding
+				.AddBinding(ViewModel.Entity, e => e.SelfDeliveryGeoGroup, w => w.SelectedItem)
+				.AddBinding(e => e.SelfDelivery, w => w.Visible)
+				.InitializeFromSource();
+
+			yentryPaymentType.Binding.AddBinding(ViewModel.Entity, e => e.PaymentType, w => w.Text)
+				.InitializeFromSource();
+
+			buttonSelectPaymentType.BindCommand(ViewModel.PaymentTypeCommand);
+
+			// Не Работает
+			treeItems.ItemsDataSource = ViewModel.Entity.ObservableOrderItems;
+            treeItems.Binding.AddBinding(ViewModel.Entity, 
+                e => e.OrderItems,
+                w => w.ItemsDataSource)
+                .InitializeFromSource();
+
+            // Переделать под yEntryPaymentNumber
+            /*yEntryPaymentNumber.ValidationMode = (QS.Widgets.ValidationType)ValidationType.numeric;
+			yEntryPaymentNumber.Binding.AddBinding(ViewModel.Entity,
+				e => e.OnlinePaymentNumber,
+				w => w.Text,
+				new NullableIntToStringConverter()).InitializeFromSource();*/
+            //ylabelPaymentNumber.Binding.AddBinding(ViewModel, vm => vm.Entity.OnlinePaymentNumber, w => w.Text)
+            //	.InitializeFromSource();
+        }
 	}
 }
