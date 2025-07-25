0fd33e43661e95ac7c9df451ea6560716e1a1ba7
Andrey <as1854@yandex.ru>
2025-07-25 08:51:18 +0300

    док блоки

--- Source/Libraries/Core/Backend/TrueMark/TrueMark.Codes.Pool/DependencyInjection.cs
+++ Source/Libraries/Core/Backend/TrueMark/TrueMark.Codes.Pool/DependencyInjection.cs
@@ -6,6 +6,11 @@ namespace TrueMark.Codes.Pool
 {
 	public static class DependencyInjection
 	{
+		/// <summary>
+		/// Расширение для добавления зависимостей работы с пулом кодов
+		/// </summary>
+		/// <param name="services">Контейнер зависимостей</param>
+		/// <returns></returns>
 		public static IServiceCollection AddCodesPool(this IServiceCollection services)
 		{
 			services.TryAddScoped<ITrueMarkCodesPool, TrueMarkCodesPool>();
