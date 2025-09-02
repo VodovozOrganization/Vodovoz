e5e3df03fa906218eb8e348b33d1a750ea2c273a
Vadim Gordeev <enzogord@gmail.com>
2025-09-02 14:39:33 +0300

    PacsPermissions

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Permissions/PacsPermissions.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Permissions/PacsPermissions.cs
@@ -1,6 +1,6 @@
 ﻿namespace Vodovoz.Core.Domain.Permissions
 {
-	public static class Pacs
+	public static class PacsPermissions
 	{
 		public static string IsAdministrator => "pacs.is_admin";
 		public static string CanEnablePacs => "pacs.can_enable";
