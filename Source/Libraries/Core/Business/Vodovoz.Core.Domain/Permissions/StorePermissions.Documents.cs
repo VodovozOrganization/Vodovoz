0efd0fff589ecac8268c1f1a6d8d418bc5a83c26
Vadim Gordeev <enzogord@gmail.com>
2025-09-02 14:47:14 +0300

    Documents.MovementDocument to StorePermissions.Documents

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Permissions/StorePermissions.Documents.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Permissions/StorePermissions.Documents.cs
@@ -23,6 +23,12 @@ namespace Vodovoz.Core.Domain.Permissions
 				Name = "Разрешение отгрузки самовывоза",
 				Description = "Пользователь может переводить заказ с самовывозом в статус на погрузку")]
 			public static string CanLoadSelfDeliveryDocument => "allow_load_selfdelivery";
+
+			/// <summary>
+			/// Может редактировать данные перевозчика в документах перемещения ТМЦ
+			/// </summary>
+			public static string CanEditStoreMovementDocumentTransporterData => "can_edit_store_movement_document_transporter_data";
+
 		}
 	}
 }
