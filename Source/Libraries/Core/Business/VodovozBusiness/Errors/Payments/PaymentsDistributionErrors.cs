3e335d405625fc5f213357e8ee25ef81743a7113
Vadim Gordeev <enzogord@gmail.com>
2025-09-02 15:35:53 +0300

    PaymentsDistributionErrors

--- Source/Libraries/Core/Business/VodovozBusiness/Errors/Payments/PaymentsDistributionErrors.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Errors/Payments/PaymentsDistributionErrors.cs
@@ -2,23 +2,23 @@
 
 namespace Vodovoz.Errors.Payments
 {
-	public static class PaymentsDistribution
+	public static class PaymentsDistributionErrors
 	{
 		public static Error AutomaticDistribution(string message) =>
 			new Error(
-				typeof(PaymentsDistribution),
+				typeof(PaymentsDistributionErrors),
 				nameof(AutomaticDistribution),
 				$"Не удалось завершить автоматическое распределение: {message}");
 
 		public static Error NoOrdersToDistribute(int counterpartyId) =>
 			new Error(
-				typeof(PaymentsDistribution),
+				typeof(PaymentsDistributionErrors),
 				nameof(NoOrdersToDistribute),
 				$"Нет заказов для распределения у контрагента #{counterpartyId}");
 
 		public static Error NoPaymentsWithPositiveBalance(int counterpartyId) =>
 			new Error(
-				typeof(PaymentsDistribution),
+				typeof(PaymentsDistributionErrors),
 				nameof(NoPaymentsWithPositiveBalance),
 				$"Нет платежей с положительным балансом у контрагента #{counterpartyId}");
 	}
