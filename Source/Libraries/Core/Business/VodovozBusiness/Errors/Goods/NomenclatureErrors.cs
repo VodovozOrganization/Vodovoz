8298ec51985d1790a489bdf09d150e6348400f0d
Vadim Gordeev <enzogord@gmail.com>
2025-09-03 08:07:19 +0300

    NomenclatureErrors

--- Source/Libraries/Core/Business/VodovozBusiness/Errors/Goods/NomenclatureErrors.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Errors/Goods/NomenclatureErrors.cs
@@ -2,30 +2,30 @@
 
 namespace Vodovoz.Errors.Goods
 {
-	public static class Nomenclature
+	public static class NomenclatureErrors
 	{
 		public static Error NotFound(int nomenclatureId) => new Error(
-			typeof(Nomenclature),
+			typeof(NomenclatureErrors),
 			nameof(NotFound),
 			$"Номенклатура #{nomenclatureId} не найдена");
 
 		public static Error HasResiduesInWarhouses = new Error(
-			typeof(Nomenclature),
+			typeof(NomenclatureErrors),
 			nameof(HasResiduesInWarhouses),
 			"Есть остатки на складах");
 
 		public static Error HasResiduesOnEmployees = new Error(
-			typeof(Nomenclature),
+			typeof(NomenclatureErrors),
 			nameof(HasResiduesOnEmployees),
 			"Есть остатки у сотрудников");
 
 		public static Error HasResiduesOnCars = new Error(
-			typeof(Nomenclature),
+			typeof(NomenclatureErrors),
 			nameof(HasResiduesOnCars),
 			"Есть остатки на автомобилях");
 
 		public static Error HasNotAcceptedTransfers = new Error(
-			typeof(Nomenclature),
+			typeof(NomenclatureErrors),
 			nameof(HasNotAcceptedTransfers),
 			"Есть непринятые документы перемещения");
 	}
