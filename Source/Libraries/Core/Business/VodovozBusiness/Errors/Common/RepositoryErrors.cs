086739f6b70b1234ca596508fbd874aac97a106e
Vadim Gordeev <enzogord@gmail.com>
2025-09-03 08:10:24 +0300

    RepositoryErrors

--- Source/Libraries/Core/Business/VodovozBusiness/Errors/Common/RepositoryErrors.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Errors/Common/RepositoryErrors.cs
@@ -3,14 +3,14 @@ using Vodovoz.Core.Domain.Results;
 
 namespace Vodovoz.Errors.Common
 {
-	public static class Repository
+	public static class RepositoryErrors
 	{
 		/// <summary>
 		/// Ошибка получения данных
 		/// </summary>
 		[Display(Name = "Ошибка получения данных")]		
 		public static Error DataRetrievalError => new Error(
-			typeof(Repository),
+			typeof(RepositoryErrors),
 			nameof(DataRetrievalError),
 			"Ошибка получения данных");
 	}
