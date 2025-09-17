4c9c2fecd0e3419c3f9bb28f47546ea7bde9734b
N-Sem <nikiforov.semen@gmail.com>
2025-09-17 10:34:28 +0300

    Только уникальные авто

--- Source/Libraries/Infrastructure/Vodovoz.Infrastructure.Persistance/Logistic/CarRepository.cs
+++ Source/Libraries/Infrastructure/Vodovoz.Infrastructure.Persistance/Logistic/CarRepository.cs
@@ -303,7 +303,7 @@ namespace Vodovoz.Infrastructure.Persistance.Logistic
 					carsQuery.Where(Restrictions.Not(Restrictions.In(Projections.Property(() => carModelAlias.Id), excludedCarModelIds)));
 				}
 
-				return carsQuery.List<Car>();
+				return carsQuery.List<Car>().Distinct().ToList();
 			},
 			cancellationToken);
 		}
