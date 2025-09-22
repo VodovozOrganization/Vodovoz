23111ea36ce3957f2e27f987787bcc66476a1fa0
Andrey <as1854@yandex.ru>
2025-09-22 14:44:53 +0300

    не переобрабатываем ошибку дубля онлайна

--- Source/Applications/Backend/Workers/Docker/CustomerOnlineOrdersRegistrar/Consumers/OnlineOrderRegisteredConsumer.cs
+++ Source/Applications/Backend/Workers/Docker/CustomerOnlineOrdersRegistrar/Consumers/OnlineOrderRegisteredConsumer.cs
@@ -4,7 +4,9 @@ using CustomerOnlineOrdersRegistrar.Factories;
 using CustomerOrdersApi.Library.Dto.Orders;
 using MassTransit;
 using Microsoft.Extensions.Logging;
+using MySqlConnector;
 using QS.DomainModel.UoW;
+using QS.Utilities.Debug;
 using Vodovoz.EntityRepositories.Orders;
 using Vodovoz.Settings.Delivery;
 using Vodovoz.Settings.OnlineOrders;
@@ -43,7 +45,12 @@ namespace CustomerOnlineOrdersRegistrar.Consumers
 		public Task Consume(ConsumeContext<OnlineOrderInfoDto> context)
 		{
 			var message = context.Message;
-			Logger.LogInformation("Пришел онлайн заказ {ExternalOrderId}, регистрируем...", message.ExternalOrderId);
+			Logger.LogInformation("Пришел онлайн заказ {ExternalOrderId} от пользователя {ExternalCounterpartyId} клиента {ClientId} " +
+				"с контактным номером {ContactPhone}, регистрируем...",
+				message.ExternalOrderId,
+				message.ExternalCounterpartyId,
+				message.CounterpartyErpId,
+				message.ContactPhone);
 			
 			try
 			{
@@ -52,6 +59,12 @@ namespace CustomerOnlineOrdersRegistrar.Consumers
 			}
 			catch(Exception e)
 			{
+				if(e.FindExceptionTypeInInner<MySqlException>() is { ErrorCode: MySqlErrorCode.DuplicateKeyEntry })
+				{
+					Logger.LogInformation("Пришел дубль уже зарегистрированного заказа {ExternalOrderId}, пропускаем", message.ExternalOrderId);
+					return Task.CompletedTask;
+				}
+				
 				Logger.LogError(e, "Ошибка при регистрации онлайн заказа {ExternalOrderId}", message.ExternalOrderId);
 				message.FaultedMessage = true;
 				_bus.Publish<OnlineOrderInfoDto>(message);
