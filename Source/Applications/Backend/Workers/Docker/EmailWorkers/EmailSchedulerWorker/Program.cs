627da9ef6369c068bfc955f625352d235e7bbd06
Semen Svobodin <work.semen.sd@gmail.com>
2025-09-26 16:45:53 +0700

    Создал шаблон воркера

---
+++ Source/Applications/Backend/Workers/Docker/EmailWorkers/EmailSchedulerWorker/Program.cs
@@ -0,0 +1,17 @@
+namespace EmailSchedulerWorker
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			IHost host = Host.CreateDefaultBuilder(args)
+				.ConfigureServices(services =>
+				{
+					services.AddHostedService<Worker>();
+				})
+				.Build();
+
+			host.Run();
+		}
+	}
+}
