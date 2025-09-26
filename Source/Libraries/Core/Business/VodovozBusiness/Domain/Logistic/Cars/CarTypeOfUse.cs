a3af2e2eaa2d40a62a235aefa84d7f0681c3f887
N-Sem <nikiforov.semen@gmail.com>
2025-09-26 09:04:28 +0300

    Новый тип авто

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Logistic/Cars/CarTypeOfUse.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Logistic/Cars/CarTypeOfUse.cs
@@ -15,22 +15,28 @@ namespace Vodovoz.Domain.Logistic.Cars
 	public enum CarTypeOfUse
 	{
 		/// <summary>
-		/// Фургон (Ларгус)
+		/// Легковой
 		/// </summary>
-		[Display(Name = "Фургон (Ларгус)")]
+		[Display(Name = "Легковая (Ларгус)")]
 		Largus,
 
 		/// <summary>
-		/// Фура
+		/// Фургон
 		/// </summary>
-		[Display(Name = "Фура")]
-		Truck,
+		[Display(Name = "Фургон (Transit Mini)")]
+		Minivan,
 
 		/// <summary>
 		/// Грузовой
 		/// </summary>
 		[Display(Name = "Грузовой")]
 		GAZelle,
+
+		/// <summary>
+		/// Фура
+		/// </summary>
+		[Display(Name = "Фура")]
+		Truck,
 
 		/// <summary>
 		/// Погрузчик
