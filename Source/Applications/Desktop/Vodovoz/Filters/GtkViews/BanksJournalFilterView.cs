94d5219b916804e5659be79b650c683148dbef6d
Andrey <as1854@yandex.ru>
2025-10-02 16:00:53 +0300

    добавил вьюху фильтра журнала банков

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/BanksJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class BanksJournalFilterView : Gtk.Bin
+	{
+		public BanksJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
