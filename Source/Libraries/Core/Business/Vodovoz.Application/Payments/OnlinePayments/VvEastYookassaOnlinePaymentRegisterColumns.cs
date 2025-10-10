84eb9186df7cee9e27f50c814a40efb9a7885723
Andrey <as1854@yandex.ru>
2025-10-10 12:57:35 +0300

    обновление функционала загрузки онлайн платежей
    - разбил логику, чтобы можно было легче грузить разные форматы по Юкассе;
    - добавил новый формат по ВВ Восток;

---
+++ Source/Libraries/Core/Business/Vodovoz.Application/Payments/OnlinePayments/VvEastYookassaOnlinePaymentRegisterColumns.cs
@@ -0,0 +1,15 @@
+﻿using VodovozBusiness.Domain.Payments;
+
+namespace Vodovoz.Application.Payments.OnlinePayments
+{
+	/// <inheritdoc/>
+	public class VvEastYookassaOnlinePaymentRegisterColumns : IOnlinePaymentRegisterColumns
+	{
+		public int PaymentSumColumn => 1;
+		public int DateAndTimeColumn => 6;
+		public int PaymentNumberColumn => 8;
+		public int? EmailColumn => null;
+		
+		public static IOnlinePaymentRegisterColumns Create() => new VvEastYookassaOnlinePaymentRegisterColumns();
+	}
+}
