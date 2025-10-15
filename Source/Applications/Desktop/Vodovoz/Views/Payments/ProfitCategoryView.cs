16de540d69df4ba2883d0a090a184a19962989e0
Andrey <as1854@yandex.ru>
2025-10-15 09:06:56 +0300

    добавил вьюху для карточки категории дохода

---
+++ Source/Applications/Desktop/Vodovoz/Views/Payments/ProfitCategoryView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Vodovoz.Views.Payments
+{
+	[ToolboxItem(true)]
+	public partial class ProfitCategoryView : Gtk.Bin
+	{
+		public ProfitCategoryView()
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+
+		}
+	}
+}
