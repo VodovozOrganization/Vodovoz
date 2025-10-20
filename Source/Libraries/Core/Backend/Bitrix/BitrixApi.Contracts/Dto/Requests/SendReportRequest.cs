501cb97bcdfc58646f8ad543898f5870c55865d5
N-Sem <nikiforov.semen@gmail.com>
2025-10-20 15:03:52 +0300

    Атрибуты для dto запроса

--- Source/Libraries/Core/Backend/Bitrix/BitrixApi.Contracts/Dto/Requests/SendReportRequest.cs
+++ Source/Libraries/Core/Backend/Bitrix/BitrixApi.Contracts/Dto/Requests/SendReportRequest.cs
@@ -17,17 +17,20 @@ namespace BitrixApi.Contracts.Dto.Requests
 		/// Организация Id
 		/// </summary>
 		[JsonPropertyName("organization")]
+		[JsonRequired]
 		public int OrganizationId { get; set; }
 		/// <summary>
 		/// Адрес электронной почты получателя
 		/// </summary>
 		[JsonPropertyName("emailAdress")]
+		[JsonRequired]
 		public string EmailAdress { get; set; }
 		/// <summary>
 		/// Тип отчета
 		/// </summary>
 		[JsonPropertyName("reportType")]
 		[JsonConverter(typeof(JsonStringEnumConverter))]
+		[JsonRequired]
 		public ReportTypeDto ReportType { get; set; }
 	}
 }
