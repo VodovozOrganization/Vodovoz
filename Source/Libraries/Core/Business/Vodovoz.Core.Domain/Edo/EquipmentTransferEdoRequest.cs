2b6dedb4213a0ac77a1d48b6e2a3ab5471432999
Semen Svobodin <work.semen.sd@gmail.com>
2025-11-19 17:05:59 +0700

    Поправил EquipmentTransferEdoRequest

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Edo/EquipmentTransferEdoRequest.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Edo/EquipmentTransferEdoRequest.cs
@@ -1,13 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using QS.DomainModel.Entity;
+using System.ComponentModel.DataAnnotations;
+using Vodovoz.Core.Domain.Orders;
 
 namespace Vodovoz.Core.Domain.Edo
 {
 	/// <summary>
 	/// Заявка на отправку акта приёма-передачи оборудования по ЭДО
 	/// </summary>
-	public class EquipmentTransferEdoRequest : OrderEdoRequest
+	[Appellative(Gender = GrammaticalGender.Feminine,
+		Nominative = "заявка на отправку акта приёма-передачи оборудования по ЭДО",
+		NominativePlural = "заявки на отправку акта приёма-передачи оборудования по ЭДО"
+	)]
+	public class EquipmentTransferEdoRequest : CustomerEdoRequest
 	{
 		private int _equipmentTransferId;
+		private OrderEntity _order;
 
 		/// <summary>
 		/// Код акта приёма-передачи оборудования
@@ -17,6 +24,16 @@ namespace Vodovoz.Core.Domain.Edo
 		{
 			get => _equipmentTransferId;
 			set => SetField(ref _equipmentTransferId, value);
+		}
+
+		/// <summary>
+		/// Код заказа c актом приёма-передачи оборудования
+		/// </summary>
+		[Display(Name = "Код заказа")]
+		public virtual OrderEntity Order
+		{
+			get => _order;
+			set => SetField(ref _order, value);
 		}
 
 		public override EdoDocumentType DocumentType => EdoDocumentType.EquipmentTransfer;
