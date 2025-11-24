63bb02466d1208eae440034895b210caade253d0
Art8m <9artbe@gmail.com>
2025-11-24 02:45:33 -0800

    stetic1

---
+++ Source/Applications/Desktop/Vodovoz/Views/Reports/CallCenterMotivationReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CallCenterMotivationReportView : Gtk.Bin
+	{
+		public CallCenterMotivationReportView()
+		{
+			this.Build();
+		}
+	}
+}
