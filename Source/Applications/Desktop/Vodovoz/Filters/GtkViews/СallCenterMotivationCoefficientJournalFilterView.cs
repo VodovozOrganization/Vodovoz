4d523edd43e24e0f8f5abc008385cc85653ae9fe
Art8m <9artbe@gmail.com>
2025-11-24 02:45:33 -0800

    stetic1

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/СallCenterMotivationCoefficientJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class СallCenterMotivationCoefficientJournalFilterView : Gtk.Bin
+	{
+		public СallCenterMotivationCoefficientJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
