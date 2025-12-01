bca9116c2f814faa2446052f8480eb222cfe2f37
Bidenko David <thefaded69@yadnex.ru>
2025-12-01 07:22:11 +0300

    View

---
+++ Source/Applications/Desktop/Vodovoz/Views/Cash/VatRateView.cs
@@ -0,0 +1,54 @@
+﻿using QS.Views.GtkUI;
+using System;
+using System.ComponentModel;
+using Vodovoz.Core.Domain.Cash;
+using Vodovoz.ViewModels.ViewModels.Cash;
+
+namespace Vodovoz.Views.Cash
+{
+	[ToolboxItem(true)]
+	public partial class VatRateView : EntityTabViewBase<VatRateViewModel, VatRate>
+	{
+		public VatRateView(VatRateViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			yentryVatRate.Binding
+				.AddBinding(ViewModel.Entity, e => e.VatRateValue, w => w.Text)
+				.AddBinding(ViewModel, vm => vm.CanEdit, w => w.Sensitive)
+				.InitializeFromSource();
+
+			ycheckbuttonIsArchieve.Binding
+				.AddFuncBinding(ViewModel, vm => !vm.IsNew, w => w.Visible)
+				.AddBinding(ViewModel.Entity, e => e.IsArchive, w => w.Active)
+				.AddBinding(ViewModel, vm => vm.CanEdit, w => w.Sensitive)
+				.InitializeFromSource();
+
+			buttonSave.Binding
+				.AddBinding(ViewModel, vm => vm.CanEdit, w => w.Sensitive)
+				.InitializeFromSource();
+			buttonSave.BindCommand(ViewModel.SaveCommand);
+
+			buttonCancel.Clicked += (sender, e) => ViewModel.Close(ViewModel.AskSaveOnClose, QS.Navigation.CloseSource.Cancel);
+			buttonCancel.BindCommand(ViewModel.CancelCommand);
+
+			buttonCopy.Binding
+				.AddFuncBinding(ViewModel, vm => !vm.IsNew, w => w.Visible)
+				.InitializeFromSource();
+
+			buttonCopy.Clicked += (sender, e) => OnBtnCopyEntityIdClicked(sender, e);
+		}
+
+		protected void OnBtnCopyEntityIdClicked(object sender, EventArgs e)
+		{
+			if(ViewModel.Entity.Id > 0)
+			{
+				GetClipboard(Gdk.Selection.Clipboard).Text = ViewModel.Entity.Id.ToString();
+			}
+		}
+	}
+}
