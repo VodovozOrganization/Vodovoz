03e0a65b8327c9839add0743502ff29a8dd6af80
David Bidenko <thefaded69@yandex.ru>
2025-12-02 07:01:34 +0300

    Добавил отображение для энама

--- Source/Libraries/Core/Business/Vodovoz.Core.Domain/Cash/Vat1cType.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Cash/Vat1cType.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Vodovoz.Core.Domain.Attributes;
 
 namespace Vodovoz.Core.Domain.Cash
@@ -9,24 +10,28 @@ namespace Vodovoz.Core.Domain.Cash
 		/// БезНДС
 		/// </summary>
 		[Value1cType("БезНДС")]
+		[Display(Name = "Без НДС")]
 		No,
 		
 		/// <summary>
 		/// Общая
 		/// </summary>
 		[Value1cType("Общая")]
+		[Display(Name = "Общая")]
 		Common,
 		
 		/// <summary>
 		/// Пониженная
 		/// </summary>
 		[Value1cType("Пониженная")]
+		[Display(Name = "Пониженная")]
 		Reduced,
 		
 		/// <summary>
 		/// ИП
 		/// </summary>
 		[Value1cType("ИП")]
+		[Display(Name = "ИП")]
 		IndividualEntrepreneur
 		
 		
