a9377b1ba53f69ba834301ce6e97c9fc83d307af
N-Sem <nikiforov.semen@gmail.com>
2025-12-24 22:45:12 +0300

    Добавлена вью для нового диалога установки уровней зп ставок

---
+++ Source/Applications/Desktop/Vodovoz/Views/WageCalculation/WageDistrictLevelRatesAssigningView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.WageCalculation
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class WageDistrictLevelRatesAssigningView : Gtk.Bin
+	{
+		public WageDistrictLevelRatesAssigningView()
+		{
+			this.Build();
+		}
+	}
+}
