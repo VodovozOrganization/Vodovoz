e30187ad1989bb9cd071fa4dc8fa452738e59b80
David Bidenko <thefaded69@yandex.ru>
2026-01-16 06:45:20 +0300

    Доработки после теста

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/SpecialBill.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/SpecialBill.cs
@@ -41,7 +41,7 @@ namespace Vodovoz.Domain.Orders.Documents
 
 		public virtual string SpecialContractNumber => Order.Client.IsForRetail ? Order.Client.GetSpecialContractString() : string.Empty;
 
-		public virtual string Title => $"Особый счет № { Order.Id } от { Order.BillDate.ToShortDateString() } { SpecialContractNumber }";
+		public virtual string Title => $"{ Name } от { Order.BillDate.ToShortDateString() } { SpecialContractNumber }";
 
 		public override string Name => Order?.DeliveryDate >= new DateTime(2026, 1, 1) 
 			?  $"Особый счет №{DocumentOrganizationCounter?.DocumentNumber ?? "-"}"
