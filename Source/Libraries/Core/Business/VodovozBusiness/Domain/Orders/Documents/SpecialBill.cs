206627cf0e1bba10b85b79362aef6ee98837669c
David Bidenko <thefaded69@yandex.ru>
2026-01-16 14:49:03 +0300

    Изменил отображение

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/SpecialBill.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Documents/SpecialBill.cs
@@ -44,7 +44,7 @@ namespace Vodovoz.Domain.Orders.Documents
 		public virtual string Title => $"{ Name } от { Order.BillDate.ToShortDateString() } { SpecialContractNumber }";
 
 		public override string Name => Order?.DeliveryDate >= new DateTime(2026, 1, 1) 
-			?  $"Особый счет №{DocumentOrganizationCounter?.DocumentNumber ?? "-"}"
+			?  $"Особый счет №{DocumentOrganizationCounter?.DocumentNumber ?? Order?.Id.ToString()}"
 			:  $"Особый счет №{Order?.Id}";
 
 		public override DateTime? DocumentDate => Order?.BillDate;
