54f3f829c4537684b4c00064f659114655680771
Andrey <as1854@yandex.ru>
2026-01-25 17:24:14 +0300

    добавил проверку суммы онлайн заказа

---
+++ Source/Libraries/Core/Business/Vodovoz.Application/Orders/CheckOnlineOrderSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Vodovoz.Core.Domain.Orders.OnlineOrders;
+
+namespace Vodovoz.Application.Orders
+{
+	public class CheckOnlineOrderSum : ICheckOnlineOrderSum
+	{
+		protected CheckOnlineOrderSum(int nomenclatureId, decimal count, decimal price, decimal discount)
+		{
+			NomenclatureId = nomenclatureId;
+			Count = count;
+			Price = price;
+			DiscountMoney = discount;
+		}
+		
+		public int NomenclatureId { get; set; }
+		public decimal Price { get; set; }
+		public decimal Count { get; set; }
+		public decimal DiscountMoney { get; set; }
+
+		public decimal Sum => Math.Round(Count * Price - DiscountMoney, 2);
+		public decimal CalculateDiscountMoney(decimal percentDiscount) =>
+			Price * Count * percentDiscount / 100;
+		
+		public static CheckOnlineOrderSum Create(int nomenclatureId, decimal count, decimal price, decimal discount) =>
+			new CheckOnlineOrderSum(nomenclatureId, count, price, discount);
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Orders/OnlineOrders/ICheckOnlineOrderSum.cs
@@ -0,0 +1,11 @@
+﻿namespace Vodovoz.Core.Domain.Orders.OnlineOrders
+{
+	public interface ICheckOnlineOrderSum
+	{
+		int NomenclatureId { get; }
+		decimal Price { get; }
+		decimal Count { get; }
+		decimal DiscountMoney { get; }
+		decimal Sum { get; }
+	}
+}
