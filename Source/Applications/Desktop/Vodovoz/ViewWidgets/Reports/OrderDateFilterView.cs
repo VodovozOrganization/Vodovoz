1cb284cded257d774b89c8dd88d0b787ed16fe48
Vadim Gordeev <enzogord@gmail.com>
2026-01-30 16:34:27 +0300

    UI виджета выбора даты

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Reports/OrderDateFilterView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using QS.ViewModels;
+using QS.Views.GtkUI;
+
+namespace Vodovoz.ViewWidgets.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrderDateFilterView : WidgetViewBase<OrderDateFilterViewModel>
+	{
+		public OrderDateFilterView()
+		{
+			this.Build();
+		}
+	}
+
+	public partial class OrderDateFilterViewModel : WidgetViewModelBase
+	{
+
+	}
+}
