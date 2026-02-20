5144037a76b9c87952f7e9014aaf8f646ecf3a1a
N-Sem <nikiforov.semen@gmail.com>
2026-02-20 17:05:26 +0300

    Созданы новые ивенты

---
+++ Source/Libraries/Core/Backend/Edo/Edo.Contracts/Messages/Events/OrderDocflowCompletedEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Edo.Contracts.Messages.Events
+{
+	/// <summary>
+	/// Событие о том, что документооборот по заказу успешно завершён
+	/// </summary>
+	public class OrderDocflowCompletedEvent
+	{
+		/// <summary>
+		/// Идентификатор документа ЭДО
+		/// </summary>
+		public int DocumentId { get; set; }
+	}
+}
