2efff0680740e74805620ef2a61ea27f466ad95d
N-Sem <nikiforov.semen@gmail.com>
2026-02-20 16:28:29 +0300

    Добавлен новый проект воркера и библиотеки

---
+++ Source/Applications/Backend/Workers/Docker/Edo/Edo.Withdrawal.Routine.Worker/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edo.Withdrawal.Routine.Worker
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureServices((hostContext, services) =>
+				{
+				});
+	}
+}
