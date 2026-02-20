b90a8d32d32b0d7f945ce19488c9a3f9fc069e6d
N-Sem <nikiforov.semen@gmail.com>
2026-02-20 17:00:10 +0300

    Добавлен новый тип заявки ЭДО и мапинг

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Data.NHibernate/Mapping/Edo/WithdrawalEdoRequestMap.cs
@@ -0,0 +1,13 @@
+﻿using FluentNHibernate.Mapping;
+using Vodovoz.Core.Domain.Edo;
+
+namespace Vodovoz.Core.Data.NHibernate.Mapping.Edo
+{
+	public class WithdrawalEdoRequestMap : SubclassMap<WithdrawalEdoRequest>
+	{
+		public WithdrawalEdoRequestMap()
+		{
+			DiscriminatorValue(nameof(EdoRequestType.Withdrawal));
+		}
+	}
+}
---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Edo/WithdrawalEdoRequest.cs
@@ -0,0 +1,19 @@
+﻿using QS.DomainModel.Entity;
+
+namespace Vodovoz.Core.Domain.Edo
+{
+	/// <summary>
+	/// Заявка на вывод кодов маркировки из оборота
+	/// </summary>
+	[Appellative(Gender = GrammaticalGender.Feminine,
+		Nominative = "заявка на вывод кодов из оборота",
+		NominativePlural = "заявки на вывод кодов из оборота"
+	)]
+	public class WithdrawalEdoRequest : FormalEdoRequest
+	{
+		/// <summary>
+		/// Тип заявки - вывод из оборота
+		/// </summary>
+		public override EdoRequestType DocumentRequestType => EdoRequestType.Withdrawal;
+	}
+}
