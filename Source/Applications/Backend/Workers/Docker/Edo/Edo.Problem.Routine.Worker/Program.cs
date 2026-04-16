a44ad4edc7b92fe654f70180b55730b2e342afe3
N-Sem <nikiforov.semen@gmail.com>
2026-04-16 17:59:30 +0300

    Добавлены проекты и воркер

---
+++ Source/Applications/Backend/Workers/Docker/Edo/Edo.Problem.Routine.Worker/Program.cs
@@ -0,0 +1,54 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using QS.Project.Core;
+using System;
+using System.Text;
+using Vodovoz.Core.Data.NHibernate;
+using Vodovoz.Zabbix.Sender;
+
+namespace Edo.Problem.Routine.Worker
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureLogging((ctx, builder) =>
+				{
+					builder.AddNLog();
+					builder.AddConfiguration(ctx.Configuration.GetSection("NLog"));
+				})
+				.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+				.ConfigureServices((hostContext, services) =>
+				{
+					services
+						.AddMappingAssemblies(
+							typeof(QS.Project.HibernateMapping.UserBaseMap).Assembly,
+							typeof(QS.Banks.Domain.Bank).Assembly,
+							typeof(QS.HistoryLog.HistoryMain).Assembly,
+							typeof(QS.Project.Domain.TypeOfEntity).Assembly,
+							typeof(Vodovoz.Core.Data.NHibernate.AssemblyFinder).Assembly,
+							typeof(QS.BusinessCommon.HMap.MeasurementUnitsMap).Assembly
+						)
+						.AddDatabaseConnection()
+						.AddNHibernateConventions()
+						.AddTrackedUoW()
+						.AddEdoProblemRoutine()
+						;
+
+					services.AddHostedService<OrderSelfDeliveryPaidProblemWorker>();
+
+					services
+						.ConfigureZabbixSenderFromDataBase(nameof(OrderSelfDeliveryPaidProblemWorker));
+				});
+	}
+}
---
+++ Source/Libraries/Core/Backend/Edo/Edo.Problem.Routine/Services/OrderSelfDeliveryPaidProblemService.cs
@@ -0,0 +1,35 @@
+﻿using Edo.Problem.Routine.Options;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Edo.Problem.Routine.Services
+{
+	/// <summary>
+	/// Сервис обработки проблем с оплатой при самовывозе в ЭДО
+	/// </summary>
+	public class OrderSelfDeliveryPaidProblemService
+	{
+		private readonly ILogger<OrderSelfDeliveryPaidProblemService> _logger;
+		private readonly IOptionsMonitor<OrderSelfDeliveryPaidProblemWorkerOptions> _options;
+
+		public OrderSelfDeliveryPaidProblemService(
+			ILogger<OrderSelfDeliveryPaidProblemService> logger,
+			IOptionsMonitor<OrderSelfDeliveryPaidProblemWorkerOptions> options)
+		{
+			_logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+			_options = options;
+		}
+
+		/// <summary>
+		/// Обработчик задач с проблемой оплаты при самовывозе в ЭДО
+		/// </summary>
+		/// <param name="cancellationToken">Токен отмены</param>
+		/// <returns></returns>
+		public async Task ProcessProblemTasks(CancellationToken cancellationToken)
+		{
+			var timeout = _options.CurrentValue.ProblemTimeout;
+		}
+	}
+}
