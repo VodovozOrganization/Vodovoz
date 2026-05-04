21a11344fe1e5ff296252509ea7f21b9610fe383
N-Sem <nikiforov.semen@gmail.com>
2026-05-04 12:59:56 +0300

    Сохранение настроек претензионных писем

--- Source/Libraries/Core/Business/Vodovoz.Settings.Database/Counterparty/DebtorsSettings.cs
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/Counterparty/DebtorsSettings.cs
@@ -32,6 +32,25 @@ namespace Vodovoz.Settings.Database.Counterparty
 
 		public int LettersOfClaimMaxCountPerDay => _settingsController.GetIntValue("DebtorsSettings.LettersOfClaimMaxCountPerDay"); 
 
-		public int LettersOfClaimResendIntervalDays => _settingsController.GetIntValue("DebtorsSettings.LettersOfClaimResendIntervalDays"); 
+		public int LettersOfClaimResendIntervalDays => _settingsController.GetIntValue("DebtorsSettings.LettersOfClaimResendIntervalDays");
+
+		public string ClaimDocumentCreatedBy => _settingsController.GetValue<string>("ClaimDocument.CreatedBy");
+
+		public string ClaimDocumentCreatorPhone => _settingsController.GetValue<string>("ClaimDocument.CreatorPhone");
+
+		public void SetLettersOfClaimTimeoutDays(int value)
+		{
+			_settingsController.CreateOrUpdateSetting("DebtorsSettings.LettersOfClaimTimeoutDays}", value.ToString());
+		}
+
+		public void SetClaimDocumentCreatedBy(string value)
+		{
+			_settingsController.CreateOrUpdateSetting("ClaimDocument.CreatedBy}", value);
+		}
+
+		public void SetClaimDocumentCreatorPhone(string value)
+		{
+			_settingsController.CreateOrUpdateSetting("ClaimDocument.CreatorPhone}", value);
+		}
 	}
 }
--- Source/Libraries/Core/Business/Vodovoz.Settings/Counterparty/IDebtorsSettings.cs
+++ Source/Libraries/Core/Business/Vodovoz.Settings/Counterparty/IDebtorsSettings.cs
@@ -49,5 +49,33 @@ namespace Vodovoz.Settings.Counterparty
 		/// Интервал повторной отправки письма о претензии, если долг не был погашен после предыдущей отправки, в днях
 		/// </summary>
 		int LettersOfClaimResendIntervalDays { get; }
+
+		/// <summary>
+		/// Исполнитель, указанный в документе претензия по долгу
+		/// </summary>
+		string ClaimDocumentCreatedBy { get; }
+
+		/// <summary>
+		/// Номер телефона исполнителя, указанный в документе претензия по долгу
+		/// </summary>
+		string ClaimDocumentCreatorPhone { get; }
+
+		/// <summary>
+		/// Установка значения количества дней сверх ПДЗ до отправки претензионного письма
+		/// </summary>
+		/// <param name="value">Количество дней сверх ПДЗ</param>
+		void SetLettersOfClaimTimeoutDays(int value);
+
+		/// <summary>
+		/// Установить исполнителя указанного в документе претензия по долгу
+		/// </summary>
+		/// <param name="value">Исполнитель</param>
+		void SetClaimDocumentCreatedBy(string value);
+
+		/// <summary>
+		/// Установить номер телефона исполнителя, указанного в документе претензия по долгу
+		/// </summary>
+		/// <param name="value">Номер телефона исполнителя</param>
+		void SetClaimDocumentCreatorPhone(string value);
 	}
 }
