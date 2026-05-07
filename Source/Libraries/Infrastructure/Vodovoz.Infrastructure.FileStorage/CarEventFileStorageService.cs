e5558b7165a39b5a3e25de490a793d258368fda6
ArthurProgger <sanyamalkov228@gmail.com>
2026-05-07 13:16:58 +0500

    Revert "Добавлена заглушка для сохранения сканов событий ТС, так как в dev-среде S3 отсутсвует (можно было реализовать NullCarEventFileStorageService для более чистой архитектуры, но не стал лезть в DI-регистрации)."
    This reverts commit e1f8703580b3500f32da1dbc8d6cf6180dd32a06.

--- Source/Libraries/Infrastructure/Vodovoz.Infrastructure.FileStorage/CarEventFileStorageService.cs
+++ Source/Libraries/Infrastructure/Vodovoz.Infrastructure.FileStorage/CarEventFileStorageService.cs
@@ -1,8 +1,4 @@
-﻿using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using Vodovoz.Core.Application.FileStorage;
-using Vodovoz.Core.Domain.Results;
 
 namespace Vodovoz.Infrastructure.FileStorage
 {
@@ -11,12 +7,6 @@ namespace Vodovoz.Infrastructure.FileStorage
 		public CarEventFileStorageService(IS3FileStorageService s3FileStorageService)
 			: base(s3FileStorageService)
 		{
-		}
-
-		// Должен быть убран при деплое в продакшен, так как S3 недоступен в dev-среде.
-		public new Task<Result> CreateFileAsync(string fileName, Stream inputStream, CancellationToken cancellationToken)
-		{
-			return Task.FromResult(Result.Failure(Core.Application.Errors.S3.ServiceUnavailable));
 		}
 
 		protected override string BucketName => "car-event-attachments";
