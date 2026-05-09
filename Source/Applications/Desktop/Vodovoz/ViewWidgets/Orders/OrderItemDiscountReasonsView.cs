d8992315d63b4039ec4b44c24ba40080d3714450
N-Sem <nikiforov.semen@gmail.com>
2026-05-09 14:11:46 +0300

    Добавлен новый виджет выбора основания скидки

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Orders/OrderItemDiscountReasonsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OrderItemDiscountReasonsView : Gtk.Bin
+	{
+		public OrderItemDiscountReasonsView()
+		{
+			this.Build();
+		}
+	}
+}
