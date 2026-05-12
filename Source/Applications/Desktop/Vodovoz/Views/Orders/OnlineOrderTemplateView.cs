9178b6ccb4ea70a8a8efbee1063c3077b0b5e7da
Andrey <as1854@yandex.ru>
2026-05-12 16:13:43 +0300

    начальный вид карточки шаблона

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/OnlineOrderTemplateView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OnlineOrderTemplateView : Gtk.Bin
+	{
+		public OnlineOrderTemplateView()
+		{
+			this.Build();
+		}
+	}
+}
