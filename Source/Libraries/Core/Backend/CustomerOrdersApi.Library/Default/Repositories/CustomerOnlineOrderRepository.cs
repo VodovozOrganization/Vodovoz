2c5998eaf8a6207fb642eabbd041a38c05ed5a82
N-Sem <nikiforov.semen@gmail.com>
2026-05-16 19:43:05 +0300

    Запрос проверки наличия онлайн заказов у клиента

--- Source/Libraries/Core/Backend/CustomerOrdersApi.Library/Default/Repositories/CustomerOnlineOrderRepository.cs
+++ Source/Libraries/Core/Backend/CustomerOrdersApi.Library/Default/Repositories/CustomerOnlineOrderRepository.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using CustomerOrders.Contracts;
 using CustomerOrders.Contracts.Default.Orders;
+using NHibernate.Linq;
 using QS.DomainModel.UoW;
+using Vodovoz.Core.Domain.Clients;
+using Vodovoz.Core.Domain.Orders;
 using Vodovoz.Domain.Logistic;
 using Vodovoz.Domain.Orders;
 
@@ -60,6 +65,34 @@ namespace CustomerOrdersApi.Library.Default.Repositories
 				};
 
 			return onlineOrders;
+		}
+
+		public async Task<bool> IsClientHasNotCancelledOnlineOrdersFromSource(
+			IUnitOfWork uow,
+			Guid externalCounterpartyId,
+			int? counterpartyErpId,
+			Source source,
+			CancellationToken cancellationToken = default)
+		{
+			var orderNotDeliveredStatuses = new[] { OrderStatus.Canceled, OrderStatus.DeliveryCanceled, OrderStatus.NotDelivered };
+
+			var query =
+				from onlineOrder in uow.Session.Query<OnlineOrder>()
+				join o in uow.Session.Query<Order>() on onlineOrder.Id equals o.OnlineOrder.Id into orders
+				from order in orders.DefaultIfEmpty()
+
+				where
+				onlineOrder.OnlineOrderStatus != OnlineOrderStatus.Canceled
+				&& (order.Id == null || !orderNotDeliveredStatuses.Contains(order.OrderStatus))
+				&& onlineOrder.Source == source
+				&& (onlineOrder.ExternalCounterpartyId == externalCounterpartyId
+					|| (counterpartyErpId != null && order.Client.Id == counterpartyErpId.Value))
+
+				select onlineOrder;
+
+			var firstNotCancelledOnlineOrder = await query.FirstOrDefaultAsync(cancellationToken);
+
+			return firstNotCancelledOnlineOrder != null;
 		}
 	}
 }
--- Source/Libraries/Core/Backend/CustomerOrdersApi.Library/Default/Repositories/ICustomerOnlineOrderRepository.cs
+++ Source/Libraries/Core/Backend/CustomerOrdersApi.Library/Default/Repositories/ICustomerOnlineOrderRepository.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using CustomerOrders.Contracts.Default.Orders;
 using QS.DomainModel.UoW;
+using Vodovoz.Core.Domain.Clients;
 
 namespace CustomerOrdersApi.Library.Default.Repositories
 {
 	public interface ICustomerOnlineOrderRepository
 	{
 		IEnumerable<OrderDto> GetCounterpartyOnlineOrdersWithoutOrder(IUnitOfWork uow, int counterpartyId, DateTime ratingAvailableFrom);
+
+		/// <summary>
+		/// Проверят, что у клиента есть не отмененный онлайн-заказ, созданный из указанного источника
+		/// </summary>
+		/// <param name="uow">UnitOfWork</param>
+		/// <param name="externalCounterpartyId">Внешний Id пользователя</param>
+		/// <param name="counterpartyErpId">Id пользователя в ДВ</param>
+		/// <param name="source">Источник онлайн заказа</param>
+		/// <param name="cancellationToken">Токен отмены</param>
+		/// <returns></returns>
+		Task<bool> IsClientHasNotCancelledOnlineOrdersFromSource(IUnitOfWork uow, Guid externalCounterpartyId, int? counterpartyErpId, Source source, CancellationToken cancellationToken = default);
 	}
 }
