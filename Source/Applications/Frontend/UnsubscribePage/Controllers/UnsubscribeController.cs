7919e47b037af159f28ba5c6d1744c35f2821bbb
Art8m <9artbe@gmail.com>
2026-05-19 18:03:20 +0300

    Страница ошибки: фикс ошибки при пост запросе, рефакторинг

--- Source/Applications/Frontend/UnsubscribePage/Controllers/UnsubscribeController.cs
+++ Source/Applications/Frontend/UnsubscribePage/Controllers/UnsubscribeController.cs
@@ -1,40 +1,84 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QS.DomainModel.UoW;
 using System;
 using System.Diagnostics;
 using System.Linq;
 using UnsubscribePage.Models;
+using Vodovoz.Domain.Client;
 using Vodovoz.EntityRepositories;
 using Vodovoz.Settings.Common;
 
 namespace UnsubscribePage.Controllers
 {
+	/// <summary>
+	/// Контроллер страницы отписки от email-рассылки.
+	/// </summary>
 	public class UnsubscribeController : Controller
 	{
 		private readonly IUnsubscribeViewModelFactory _unsubscribeViewModelFactory;
 		private readonly IEmailRepository _emailRepository;
 		private readonly IEmailSettings _emailSettings;
+		private readonly IUnitOfWorkFactory _uowFactory;
 
-		public UnsubscribeController(IUnsubscribeViewModelFactory unsubscribeViewModelFactory, IEmailRepository emailRepository, IEmailSettings emailSettings)
+		/// <summary>
+		/// Создаёт экземпляр контроллера.
+		/// </summary>
+		/// <param name="unsubscribeViewModelFactory">Фабрика ViewModel.</param>
+		/// <param name="emailRepository">Репозиторий email-данных.</param>
+		/// <param name="emailSettings">Настройки email.</param>
+		/// <param name="uowFactory">Фабрика UnitOfWork.</param>
+		public UnsubscribeController(
+			IUnsubscribeViewModelFactory unsubscribeViewModelFactory,
+			IEmailRepository emailRepository,
+			IEmailSettings emailSettings,
+			IUnitOfWorkFactory uowFactory)
 		{
 			_unsubscribeViewModelFactory = unsubscribeViewModelFactory ?? throw new ArgumentNullException(nameof(unsubscribeViewModelFactory));
 			_emailRepository = emailRepository ?? throw new ArgumentNullException(nameof(emailRepository));
 			_emailSettings = emailSettings ?? throw new ArgumentNullException(nameof(emailSettings));
+			_uowFactory = uowFactory ?? throw new ArgumentNullException(nameof(uowFactory));
 		}
 
+		/// <summary>
+		/// Открывает страницу отписки и сразу сохраняет факт отписки.
+		/// </summary>
+		/// <param name="emailGuid">Guid email-ссылки.</param>
+		/// <returns>Страница отписки.</returns>
 		[HttpGet]
 		[Route("/{emailGuid:guid}")]
 		public IActionResult Index(Guid emailGuid)
 		{
 			var viewModel = _unsubscribeViewModelFactory.CreateNewUnsubscribeViewModel(emailGuid, _emailRepository, _emailSettings);
 
-			if(viewModel.CounterpartyId != 0)
+			if(viewModel.GuidCounterpartyEmail?.CounterpartyId != null)
 			{
-				viewModel.SaveUnsubscribe();
+				var unsubscribingEvent = new UnsubscribingBulkEmailEvent
+				{
+					CounterpartyEmailType = viewModel.GuidCounterpartyEmail.CounterpartyEmailType,
+
+					Counterparty = new Counterparty
+					{
+						Id = viewModel.GuidCounterpartyEmail.CounterpartyId
+					}
+				};
+
+				using(var unitOfWork = _uowFactory.CreateWithoutRoot("Отписка от массовой рассылки"))
+				{
+					unitOfWork.Save(unsubscribingEvent);
+					unitOfWork.Commit();
+				}
+
+				viewModel.EmailEventId = unsubscribingEvent.Id;
 			}
 
 			return View(viewModel);
 		}
 
+		/// <summary>
+		/// Сохраняет причину отписки.
+		/// </summary>
+		/// <param name="viewModel">Данные формы.</param>
+		/// <returns>Redirect на финальную страницу.</returns>
 		[HttpPost]
 		[Route("/{emailGuid:guid}")]
 		public IActionResult Index(UnsubscribeViewModel viewModel)
@@ -45,21 +89,51 @@ namespace UnsubscribePage.Controllers
 			}
 
 			var selectedReason = viewModel.ReasonsList?.Single(x => x.Id == viewModel.SelectedReasonId);
-			
-			viewModel.SaveUnsubscribe(selectedReason);
+
+			using var unitOfWork = _uowFactory.CreateWithoutRoot("Сохранение причины отписки");
+
+			var unsubscribingEvent = unitOfWork.GetById<UnsubscribingBulkEmailEvent>(viewModel.EmailEventId);
+
+			if(unsubscribingEvent == null)
+			{
+				return NotFound();
+			}
+
+			unsubscribingEvent.Reason = selectedReason;
+
+			unsubscribingEvent.ReasonDetail = selectedReason?.Id == viewModel.OtherReasonId
+				? viewModel.OtherReason
+				: null;
+
+			unitOfWork.Save(unsubscribingEvent);
+			unitOfWork.Commit();
 
 			return RedirectToAction(nameof(Finish));
 		}
 
+		/// <summary>
+		/// Финальная страница после отправки причины отписки.
+		/// </summary>
+		/// <returns>Финальная страница.</returns>
 		public IActionResult Finish()
 		{
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		/// <summary>
+		/// Страница отображения ошибки.
+		/// </summary>
+		/// <returns>Страница ошибки.</returns>
+		[ResponseCache(
+			Duration = 0,
+			Location = ResponseCacheLocation.None,
+			NoStore = true)]
 		public IActionResult Error()
 		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			return View(new ErrorViewModel
+			{
+				RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+			});
 		}
 	}
 }
--- Source/Applications/Frontend/UnsubscribePage/Controllers/UnsubscribeViewModelFactory.cs
+++ Source/Applications/Frontend/UnsubscribePage/Controllers/UnsubscribeViewModelFactory.cs
@@ -1,5 +1,6 @@
 ﻿using QS.DomainModel.UoW;
 using System;
+using System.Text.Json;
 using UnsubscribePage.Models;
 using Vodovoz.EntityRepositories;
 using Vodovoz.Settings.Common;
@@ -15,7 +16,22 @@ namespace UnsubscribePage.Controllers
 			_uowFactory = uowFactory ?? throw new ArgumentNullException(nameof(uowFactory));
 		}
 
-		public UnsubscribeViewModel CreateNewUnsubscribeViewModel(Guid guid, IEmailRepository emailRepository, IEmailSettings emailSettings) =>
-			new UnsubscribeViewModel(_uowFactory, guid, emailRepository, emailSettings);
+		public UnsubscribeViewModel CreateNewUnsubscribeViewModel(
+			Guid guid,
+			IEmailRepository emailRepository,
+			IEmailSettings emailSettings)
+		{
+			using var unitOfWork = _uowFactory.CreateWithoutRoot("Инициализация страницы отписки");
+
+			var guidCounterpartyEmail = emailRepository.GetCounterpartyEmailByGuidForUnsubscribing(unitOfWork, guid);
+			var reasons = emailRepository.GetUnsubscribingReasons(unitOfWork, emailSettings, isForUnsubscribePage: true);
+
+			return new UnsubscribeViewModel
+			{
+				OtherReasonId = emailSettings.BulkEmailEventOtherReasonId,
+				GuidCounterpartyEmailSerialized = JsonSerializer.Serialize(guidCounterpartyEmail),
+				ReasonsListSerialized = JsonSerializer.Serialize(reasons)
+			};
+		}
 	}
 }
