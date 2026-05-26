1c928af0d904e1d3d3b2c1001ec59d789e3a2371
Andrey <as1854@yandex.ru>
2026-05-26 17:45:08 +0300

    добавлен ui фильтра журнала шаблонов

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/OnlineOrderTemplatesJournalFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OnlineOrderTemplatesJournalFilterView : Gtk.Bin
+	{
+		public OnlineOrderTemplatesJournalFilterView()
+		{
+			this.Build();
+		}
+	}
+}
