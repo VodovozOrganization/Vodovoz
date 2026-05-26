bacb1c215be78ab107a06ab5b17480ef9dcc9512
Александр Садовский <brosammi@gmail.com>
2026-05-26 17:22:38 +0300

    new RefundEntity RefundJournalNode RefundJournalViewModel

---
+++ Source/Applications/Desktop/VodovozViewModels/Journals/JournalNodes/Refunds/RefundJournalNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using QS.Project.Journal;
+using Vodovoz.Core.Domain.Payments;
+using Vodovoz.Core.Domain.Refunds;
+using Vodovoz.Domain.Orders;
+using Vodovoz.Domain.Payments;
+
+
+namespace Vodovoz.ViewModels.Journals.JournalNodes.Refunds
+{
+	public class RefundJournalNode : JournalEntityNodeBase<RefundEntity>
+	{
+		public override string Title => $"{Order}";
+		public DateTime Date {  get; set; }
+		public string OrderOnlineId { get; set; }
+		public string Order { get; set; }
+	}
+}
---
+++ Source/Applications/Desktop/VodovozViewModels/Journals/JournalViewModels/Refund/RefundJournalViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using NHibernate;
+using NHibernate.Transform;
+using QS.Dialog;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Journal;
+using QS.Project.Services;
+using QS.Services;
+using Vodovoz.Core.Domain.Refunds;
+using Vodovoz.Filters.ViewModels;
+using Vodovoz.ViewModels.Journals.JournalNodes.Refunds;
+using Vodovoz.ViewModels.ViewModels.Refunds;
+
+namespace Vodovoz.ViewModels.Journals.JournalViewModels.Refund
+{
+	public class RefundJournalViewModel : EntityJournalViewModelBase<RefundEntity, RefundViewModel, RefundJournalNode>
+	{
+		public RefundJournalViewModel(
+			IUnitOfWorkFactory unitOfWorkFactory, 
+			IInteractiveService interactiveService, 
+			INavigationManager navigationManager, 
+			IDeleteEntityService deleteEntityService = null, 
+			ICurrentPermissionService currentPermissionService = null) : base(unitOfWorkFactory, interactiveService, navigationManager, deleteEntityService, currentPermissionService)
+		{
+
+		}
+
+		protected override IQueryOver<RefundEntity> ItemsQuery(IUnitOfWork uow)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
