d5ab6d3efb1731da6583522675c0e41e5205f559
ArthurProgger <sanyamalkov228@gmail.com>
2026-06-09 16:37:09 +0500

    Доработка отправки УПД по закрывающимся документам.

--- Source/Libraries/Core/Backend/Edo/Edo.Transfer.Routine/Services/ClosingDocumentsOrdersUpdSendService.cs
+++ Source/Libraries/Core/Backend/Edo/Edo.Transfer.Routine/Services/ClosingDocumentsOrdersUpdSendService.cs
@@ -2,23 +2,17 @@ using Edo.Transfer.Routine.Options;
 using Edo.Transport;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using NHibernate.Linq;
 using QS.DomainModel.UoW;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Vodovoz.Core.Domain.Clients;
-using Vodovoz.Core.Domain.Documents;
+using Vodovoz.Core.Data.Repositories;
 using Vodovoz.Core.Domain.Edo;
 using Vodovoz.Core.Domain.Orders;
-using Vodovoz.Core.Domain.Organizations;
-using Vodovoz.Domain.Client;
 using Vodovoz.Domain.Orders;
 using Vodovoz.Settings.Delivery;
-using Vodovoz.Settings.Organizations;
-using DocumentContainerType = Vodovoz.Core.Domain.Documents.DocumentContainerType;
 
 namespace Edo.Transfer.Routine.Services
 {
@@ -26,9 +20,9 @@ namespace Edo.Transfer.Routine.Services
 	{
 		private readonly ILogger<ClosingDocumentsOrdersUpdSendService> _logger;
 		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
-		private readonly IOrganizationSettings _organizationSettings;
 		private readonly IDeliveryScheduleSettings _deliveryScheduleSettings;
 		private readonly IOptionsMonitor<ClosingDocumentsOrdersUpdSendSettings> _optionsMonitor;
+		private readonly IOrderRepository _orderRepository;
 		private readonly MessageService _edoMessageService;
 
 		private readonly IEnumerable<OrderStatus> _orderStatusesToSendUpd =
@@ -37,16 +31,16 @@ namespace Edo.Transfer.Routine.Services
 		public ClosingDocumentsOrdersUpdSendService(
 			ILogger<ClosingDocumentsOrdersUpdSendService> logger,
 			IUnitOfWorkFactory unitOfWorkFactory,
-			IOrganizationSettings organizationSettings,
 			IDeliveryScheduleSettings deliveryScheduleSettings,
 			IOptionsMonitor<ClosingDocumentsOrdersUpdSendSettings> optionsMonitor,
+			IOrderRepository orderRepository,
 			MessageService edoMessageService)
 		{
 			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
 			_unitOfWorkFactory = unitOfWorkFactory ?? throw new ArgumentNullException(nameof(unitOfWorkFactory));
-			_organizationSettings = organizationSettings ?? throw new ArgumentNullException(nameof(organizationSettings));
 			_deliveryScheduleSettings = deliveryScheduleSettings ?? throw new ArgumentNullException(nameof(deliveryScheduleSettings));
 			_optionsMonitor = optionsMonitor;
+			_orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
 			_edoMessageService = edoMessageService ?? throw new ArgumentNullException(nameof(edoMessageService));
 		}
 
@@ -54,10 +48,11 @@ namespace Edo.Transfer.Routine.Services
 		{
 			using(var uow = _unitOfWorkFactory.CreateWithoutRoot(nameof(ClosingDocumentsOrdersUpdSendService)))
 			{
+				var unprocessedEdoRequests = await GetUnprocessedClosingDocumentUpdEdoRequests(uow, cancellationToken);
 				var orders = await GetCloseDocumentOrdersToSendEdoRequest(uow, cancellationToken);
 				var edoRequests = await CreateEdoRequests(uow, orders, cancellationToken);
 
-				await PublishEdoRequestCreatedEvents(edoRequests);
+				await PublishEdoRequestCreatedEvents(unprocessedEdoRequests.Concat(edoRequests));
 			}
 		}
 
@@ -65,43 +60,34 @@ namespace Edo.Transfer.Routine.Services
 		{
 			_logger.LogInformation("Получаем заказы для отправки ЭДО УПД");
 
-			var orders =
-				await (from order in uow.Session.Query<OrderEntity>()
-					join client in uow.Session.Query<CounterpartyEntity>() on order.Client.Id equals client.Id
-					join contract in uow.Session.Query<CounterpartyContractEntity>()
-						on order.Contract.Id equals contract.Id
-					join organization in uow.Session.Query<OrganizationEntity>()
-						on contract.Organization.Id equals organization.Id
-					join er in uow.Session.Query<FormalEdoRequest>() on order.Id equals er.Order.Id into edoRequests
-					from edoRequest in edoRequests.DefaultIfEmpty()
-					join ec in uow.Session.Query<EdoContainerEntity>()
-					   on new { OrderId = order.Id, DocType = DocumentContainerType.Upd } equals new { OrderId = ec.Order.Id, DocType = ec.Type } into edoContainers
-					from edoContainer in edoContainers.DefaultIfEmpty()
-					join defaultEdoAccount in uow.Session.Query<CounterpartyEdoAccountEntity>()
-						on new { a = client.Id, b = (int?)contract.Organization.Id, c = true }
-						equals new { a = defaultEdoAccount.Counterparty.Id, b = defaultEdoAccount.OrganizationId, c = defaultEdoAccount.IsDefault }
-						into edoAccountsByOrder
-					from edoAccountByOrder in edoAccountsByOrder.DefaultIfEmpty()
-					join defaultVodovozEdoAccount in uow.Session.Query<CounterpartyEdoAccountEntity>()
-						on new { a = client.Id, b = (int?)_organizationSettings.VodovozOrganizationId, c = true }
-						equals new { a = defaultVodovozEdoAccount.Counterparty.Id, b = defaultVodovozEdoAccount.OrganizationId, c = defaultVodovozEdoAccount.IsDefault }
-					where
-						order.PaymentType == PaymentType.Cashless
-						&& order.DeliveryDate >= DateTime.Today.AddDays(-_optionsMonitor.CurrentValue.MaxDaysFromDeliveryDate)
-						&& order.DeliverySchedule.Id == _deliveryScheduleSettings.ClosingDocumentDeliveryScheduleId
-						&& _orderStatusesToSendUpd.Contains(order.OrderStatus)
-						&& client.IsNewEdoProcessing
-						&& (edoAccountByOrder.ConsentForEdoStatus == ConsentForEdoStatus.Agree
-							|| defaultVodovozEdoAccount.ConsentForEdoStatus == ConsentForEdoStatus.Agree)
-						&& !client.IsNotSendDocumentsByEdo
-						&& edoContainer.Id == null
-						&& edoRequest.Id == null
-					select order)
-				.ToListAsync(cancellationToken);
+			var orders = await _orderRepository.GetOrdersForClosingDocumentUpdEdoAsync(
+				uow,
+				_optionsMonitor.CurrentValue.MaxDaysFromDeliveryDate,
+				_deliveryScheduleSettings.ClosingDocumentDeliveryScheduleId,
+				_orderStatusesToSendUpd,
+				cancellationToken);
 
 			_logger.LogInformation("Найдено {OrdersCount} заказов для отправки ЭДО УПД", orders.Count());
 
 			return orders;
+		}
+
+		private async Task<IEnumerable<PrimaryEdoRequest>> GetUnprocessedClosingDocumentUpdEdoRequests(IUnitOfWork uow, CancellationToken cancellationToken)
+		{
+			_logger.LogInformation("Получаем необработанные заявки на отправку ЭДО УПД по заказам Закр.Док");
+
+			var edoRequests = await _orderRepository.GetUnprocessedClosingDocumentUpdEdoRequestsAsync(
+				uow,
+				_optionsMonitor.CurrentValue.MaxDaysFromDeliveryDate,
+				_deliveryScheduleSettings.ClosingDocumentDeliveryScheduleId,
+				_orderStatusesToSendUpd,
+				cancellationToken);
+
+			var filteredRequests = FilterRequestsWithoutAccountableInTrueMarkItems(edoRequests).ToList();
+
+			_logger.LogInformation("Найдено {RequestsCount} необработанных заявок на отправку ЭДО УПД", filteredRequests.Count);
+
+			return filteredRequests;
 		}
 
 		private async Task<IEnumerable<PrimaryEdoRequest>> CreateEdoRequests(IUnitOfWork uow, IEnumerable<OrderEntity> orders, CancellationToken cancellationToken)
@@ -141,6 +127,29 @@ namespace Edo.Transfer.Routine.Services
 			_logger.LogInformation("Создано {RequestsCount} заявок", edoRequests.Count);
 
 			return edoRequests;
+		}
+
+		private IEnumerable<PrimaryEdoRequest> FilterRequestsWithoutAccountableInTrueMarkItems(IEnumerable<PrimaryEdoRequest> edoRequests)
+		{
+			foreach(var edoRequest in edoRequests)
+			{
+				if(edoRequest?.Order is null)
+				{
+					continue;
+				}
+
+				if(edoRequest.Order.OrderItems.Any(x => x.Nomenclature.IsAccountableInTrueMark))
+				{
+					_logger.LogError(
+						"В заказе \"Закр док\" имеются маркированные товары. Отправка документов по ЭДО недоступна. RequestId: {RequestId}. OrderId: {OrderId}",
+						edoRequest.Id,
+						edoRequest.Order.Id);
+
+					continue;
+				}
+
+				yield return edoRequest;
+			}
 		}
 
 		private PrimaryEdoRequest CreateEdoRequests(OrderEntity order)
