d89b39b1df82d36e2cac7d7c1b326942014b525c
Vadim Gordeev <enzogord@gmail.com>
2026-06-25 13:42:32 +0300

    ui3

---
+++ Source/Applications/Desktop/Vodovoz/Views/Edo/EdoInOrderActionsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Edo
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoInOrderActionsView : Gtk.Bin
+	{
+		public EdoInOrderActionsView()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/Views/Edo/EdoInOrderView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Edo
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoInOrderView : Gtk.Bin
+	{
+		public EdoInOrderView()
+		{
+			this.Build();
+		}
+	}
+}
