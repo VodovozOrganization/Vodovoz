73ad3fd500a3df85999ddddf0398441236939167
N-Sem <nikiforov.semen@gmail.com>
2026-06-30 11:03:51 +0300

    Директория задана в параметрах бд

---
+++ Source/Libraries/Core/Business/Vodovoz.Settings.Database/Resources/FinancialResourcesSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Vodovoz.Settings.Database.Resources
+{
+	public class FinancialResourcesSettings
+	{
+		private readonly ISettingsController _settingsController;
+
+		public FinancialResourcesSettings(ISettingsController settingsController)
+		{
+			_settingsController = settingsController ?? throw new ArgumentNullException(nameof(settingsController));
+		}
+
+		public string BankStatementsDirectory => _settingsController.GetStringValue("bank_statements_directory");
+	}
+}
