b30e0a394a0b5d22b76c860c24c150a641215d0b
Andrey <as1854@yandex.ru>
2026-07-10 14:26:04 +0300

    док блоки

--- Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Delivery/IOrderFreeDeliveryPrice.cs
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Orders/Delivery/IOrderFreeDeliveryPrice.cs
@@ -3,11 +3,26 @@ using Vodovoz.Domain.Orders;
 
 namespace VodovozBusiness.Domain.Orders.Delivery
 {
+	/// <summary>
+	/// Данные для проверки у заказа бесплатной доставки
+	/// </summary>
 	public interface IOrderFreeDeliveryPrice : IFreeDeliveryPrice
 	{
+		/// <summary>
+		/// Тип заказа <see cref="Vodovoz.Domain.Orders.OrderAddressType"/>
+		/// </summary>
 		OrderAddressType OrderAddressType { get; }
+		/// <summary>
+		/// Бутылей на возврат
+		/// </summary>
 		int? BottlesReturn { get; }
+		/// <summary>
+		/// Список оборудования
+		/// </summary>
 		IEnumerable<OrderEquipment> ObservableOrderEquipments { get; }
+		/// <summary>
+		/// Список залогов
+		/// </summary>
 		IEnumerable<OrderDepositItem> ObservableOrderDepositItems { get; }
 	}
 }
