0373ac1a79ffa5d62e878889592d1af9307cf8d7
Vadim Gordeev <enzogord@gmail.com>
2026-07-23 07:12:49 +0300

    UI5

---
+++ Source/Applications/Desktop/Vodovoz/Views/Edo/EdoInOrderDocflowsStageView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Edo
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoInOrderDocflowsStageView : Gtk.Bin
+	{
+		public EdoInOrderDocflowsStageView()
+		{
+			this.Build();
+		}
+	}
+}
