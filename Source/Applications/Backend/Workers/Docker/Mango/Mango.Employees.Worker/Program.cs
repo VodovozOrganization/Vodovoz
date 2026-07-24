18b1735768453756601e52f61ce097387a5da5ef
N-Sem <nikiforov.semen@gmail.com>
2026-07-24 13:15:32 +0300

    Добавлены новый воркер и сервис

---
+++ Source/Applications/Backend/Workers/Docker/Mango/Mango.Employees.Worker/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Mango.Employees.Worker
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureServices((hostContext, services) =>
+				{
+				});
+	}
+}
