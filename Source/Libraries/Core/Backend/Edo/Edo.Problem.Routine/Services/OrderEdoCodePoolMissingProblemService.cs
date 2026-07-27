07abefdb3273f5affdc707dd4aec6cc1d5526cc7
Semen Svobodin <work.semen.sd@gmail.com>
2026-07-27 20:02:42 +0700

    Сервис

--- Source/Libraries/Core/Backend/Edo/Edo.Problem.Routine/Services/OrderEdoCodePoolMissingProblemService.cs
+++ Source/Libraries/Core/Backend/Edo/Edo.Problem.Routine/Services/OrderEdoCodePoolMissingProblemService.cs
@@ -1,51 +1,39 @@
-﻿using System;
+﻿using Edo.Contracts.Messages.Events;
+using Edo.Problems.Validation;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Edo.Contracts.Messages.Events;
-using Edo.Problem.Routine.Options;
-using Edo.Problems.Validation;
-using MassTransit;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using QS.DomainModel.UoW;
-using Vodovoz.Core.Data.Repositories;
 using Vodovoz.Core.Domain.Edo;
 
 namespace Edo.Problem.Routine.Services
 {
-	public class OrderEdoCodePoolMissingProblemService
+	public class OrderEdoCodePoolMissingProblemService : IOrderEdoCodePoolMissingProblemService
 	{
 		private const string _problemSourceName = "EdoCodePoolMissingCodeException";
 
 		private readonly ILogger<OrderEdoCodePoolMissingProblemService> _logger;
-		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
 		private readonly IServiceProvider _serviceProvider;
 		private readonly IBus _messageBus;
 		private readonly IEdoTaskValidator _edoCodePoolValidator;
 
 		public OrderEdoCodePoolMissingProblemService(
 			ILogger<OrderEdoCodePoolMissingProblemService> logger,
-			IUnitOfWorkFactory unitOfWorkFactory,
 			IEnumerable<IEdoTaskValidator> validators,
 			IServiceProvider serviceProvider,
 			IBus messageBus)
 		{
 			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-			_unitOfWorkFactory = unitOfWorkFactory ?? throw new ArgumentNullException(nameof(unitOfWorkFactory));
 			_edoCodePoolValidator = (validators ?? throw new ArgumentNullException(nameof(validators)))
 				.FirstOrDefault(v => v.Name == _problemSourceName);
 			_serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
 			_messageBus = messageBus ?? throw new ArgumentNullException(nameof(messageBus));
 		}
 
-		public async Task TryResumeTask(OrderEdoTask edoTask, CancellationToken cancellationToken)
-		{
-			await TryResumeTaskAsync(edoTask, cancellationToken);
-		}
-
-		private async Task TryResumeTaskAsync(OrderEdoTask edoTask, CancellationToken cancellationToken)
+		public async Task TryResumeTaskAsync(OrderEdoTask edoTask, CancellationToken cancellationToken)
 		{
 			if(_edoCodePoolValidator != null)
 			{
@@ -57,7 +45,7 @@ namespace Edo.Problem.Routine.Services
 						"Задача ЭДО {EdoTaskId}: пул кодов не прошел проверку по заказу №{OrderId}",
 						edoTask.Id,
 						edoTask.FormalEdoRequest.Order.Id);
-					
+
 					throw new ArgumentException(
 						$"Задача ЭДО {edoTask.Id}: пул кодов не прошел проверку по заказу №{edoTask.FormalEdoRequest.Order.Id}");
 				}
@@ -154,6 +142,11 @@ namespace Edo.Problem.Routine.Services
 				nameof(ReceiptTaskCreatedEvent));
 
 			await _messageBus.Publish(new ReceiptTaskCreatedEvent { ReceiptEdoTaskId = edoTask.Id }, cancellationToken);
+		}
+
+		public Task ProcessProblemTasks(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
