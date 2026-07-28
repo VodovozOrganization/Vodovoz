af659eb0a6dfbe6a0e93f18414788be1bac7c354
Semen Svobodin <work.semen.sd@gmail.com>
2026-07-28 19:02:32 +0700

    Переносы

---
+++ Source/Libraries/Core/Business/Vodovoz.Core.Domain/Edo/EdoProblemGtinItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vodovoz.Core.Domain.Goods;
+
+namespace Vodovoz.Core.Domain.Edo
+{
+	/// <summary>
+	/// Пользовательский элемент проблемы, связанный с GTIN.
+	/// </summary>
+	public class EdoProblemGtinItem : EdoProblemCustomItem
+	{
+		private GtinEntity _gtin;
+
+		public override EdoProblemCustomItemType Type => EdoProblemCustomItemType.Gtin;
+
+		/// <summary>
+		/// Сущность GTIN, связанная с проблемой.
+		/// </summary>
+		[Display(Name = "Gtin")]
+		public virtual GtinEntity Gtin
+		{
+			get => _gtin;
+			set => SetField(ref _gtin, value);
+		}
+	}
+}
