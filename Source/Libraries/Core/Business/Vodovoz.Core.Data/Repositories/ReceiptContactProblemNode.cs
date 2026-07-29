1c8ff84eea1cd1c251371bfb70d782d62f06962a
ArthurProgger <sanyamalkov228@gmail.com>
2026-07-29 12:40:16 +0500

    Добавлены док-блоки.

--- Source/Libraries/Core/Business/Vodovoz.Core.Data/Repositories/ReceiptContactProblemNode.cs
+++ Source/Libraries/Core/Business/Vodovoz.Core.Data/Repositories/ReceiptContactProblemNode.cs
@@ -7,10 +7,29 @@ namespace Vodovoz.Core.Data.Repositories
 	/// </summary>
 	public class ReceiptContactProblemNode
 	{
+		/// <summary>
+		/// Задача ЭДО на отправку чека.
+		/// </summary>
 		public ReceiptEdoTask ReceiptTask { get; set; }
+
+		/// <summary>
+		/// Активная проблема контакта.
+		/// </summary>
 		public EdoTaskProblem Problem { get; set; }
+
+		/// <summary>
+		/// Состояние повторной обработки проблемы.
+		/// </summary>
 		public EdoTaskProblemRoutineState RoutineState { get; set; }
+
+		/// <summary>
+		/// Идентификатор заказа.
+		/// </summary>
 		public int OrderId { get; set; }
+
+		/// <summary>
+		/// Есть ли у задачи коды, уже сохраненные в пул.
+		/// </summary>
 		public bool HasCodesSavedToPool { get; set; }
 	}
 }
