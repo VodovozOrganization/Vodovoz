09a21864d64e1686fe94eb3fbeb25e671f734878
Semen Svobodin <work.semen.sd@gmail.com>
2026-08-05 19:54:09 +0700

    Исправления после мержа

--- Source/Applications/Backend/Workers/Docker/Edo/Edo.Problem.Routine.Worker/CodePoolMissingProblemWorker.cs
+++ Source/Applications/Backend/Workers/Docker/Edo/Edo.Problem.Routine.Worker/CodePoolMissingProblemWorker.cs
@@ -16,15 +16,19 @@ namespace Edo.Problem.Routine.Worker
 		private readonly ILogger<CodePoolMissingProblemWorker> _logger;
 		private readonly IOptions<CodePoolMissingProblemWorkerOptions> _options;
 		private readonly IServiceScopeFactory _serviceScopeFactory;
+		private readonly IZabbixSender _zabbixSender;
 
 		public CodePoolMissingProblemWorker(
 			ILogger<CodePoolMissingProblemWorker> logger,
 			IOptions<CodePoolMissingProblemWorkerOptions> options,
-			IServiceScopeFactory serviceScopeFactory)
+			IServiceScopeFactory serviceScopeFactory,
+			IZabbixSender zabbixSender
+		)
 		{
 			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
 			_options = options ?? throw new ArgumentNullException(nameof(options));
 			_serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
+			_zabbixSender = zabbixSender ?? throw new ArgumentNullException(nameof(zabbixSender));
 		}
 
 		protected override TimeSpan Interval => _options.Value.WorkerInterval;
@@ -32,7 +36,6 @@ namespace Edo.Problem.Routine.Worker
 		protected override async Task DoWork(CancellationToken stoppingToken)
 		{
 			using var scope = _serviceScopeFactory.CreateScope();
-			var zabbixSender = scope.ServiceProvider.GetRequiredService<IZabbixSender>();
 			var orderEdoCodePoolMissingProblemService = scope.ServiceProvider.GetRequiredService<ICodePoolMissingProblemService>();
 
 			_logger.LogInformation("Запуск обработки задач ЭДО с ошибкой нехватки кодов в пуле");
@@ -43,13 +46,14 @@ namespace Edo.Problem.Routine.Worker
 
 				_logger.LogInformation("Обработка задач ЭДО с ошибкой нехватки кодов в пуле успешно завершена");
 
-				await zabbixSender.SendIsHealthyAsync(stoppingToken);
+				await _zabbixSender.SendIsHealthyAsync(nameof(CodePoolMissingProblemWorker), stoppingToken);
 			}
 			catch(Exception ex)
 			{
 				_logger.LogError(ex, "Ошибка при обработке задач ЭДО с ошибкой нехватки кодов в пуле");
 
-				await zabbixSender.SendProblemMessageAsync(
+				await _zabbixSender.SendProblemMessageAsync(
+					nameof(CodePoolMissingProblemWorker),
 					ZabixSenderMessageType.Problem,
 					$"Ошибка при обработке задач ЭДО с ошибкой нехватки кодов в пуле: {ex.Message}",
 					stoppingToken);
