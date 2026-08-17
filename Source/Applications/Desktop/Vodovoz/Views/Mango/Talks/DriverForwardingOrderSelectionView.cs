3f6a950230b5ad71634522c5c05375da4c22d744
N-Sem <nikiforov.semen@gmail.com>
2026-08-17 16:57:16 +0300

    Добавлен новое окно для выбора заказа клиента

---
+++ Source/Applications/Desktop/Vodovoz/Views/Mango/Talks/DriverForwardingOrderSelectionView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Mango.Talks
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DriverForwardingOrderSelectionView : Gtk.Bin
+	{
+		public DriverForwardingOrderSelectionView()
+		{
+			this.Build();
+		}
+	}
+}
