fe3295d6492453562efed79e2133d849a0853414
N-Sem <nikiforov.semen@gmail.com>
2026-08-17 16:26:25 +0300

    Добавлен вью кнопки

---
+++ Source/Applications/Desktop/Vodovoz/ViewWidgets/Mango/MangoCallButtonView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.ViewWidgets.Mango
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class MangoCallButtonView : Gtk.Bin
+	{
+		public MangoCallButtonView()
+		{
+			this.Build();
+		}
+	}
+}
