fb42abcc8e0981ebd4d9e177e9317a258f3c8d42
N-Sem <nikiforov.semen@gmail.com>
2026-09-09 18:30:02 +0300

    Добавлен вью нового виджета

---
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/AdditionalFuelTypeManagementView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Logistic
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AdditionalFuelTypeManagementView : Gtk.Bin
+	{
+		public AdditionalFuelTypeManagementView()
+		{
+			this.Build();
+		}
+	}
+}
