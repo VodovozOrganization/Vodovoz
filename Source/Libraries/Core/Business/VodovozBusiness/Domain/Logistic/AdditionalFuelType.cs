c3d80921796f06be695e969c65bc141497f114bb
N-Sem <nikiforov.semen@gmail.com>
2026-09-09 16:48:52 +0300

    Новая сущность и мапинги

---
+++ Source/Libraries/Core/Business/VodovozBusiness/Domain/Logistic/AdditionalFuelType.cs
@@ -0,0 +1,52 @@
+﻿using QS.DomainModel.Entity;
+using QS.DomainModel.Entity.EntityPermissions;
+using QS.HistoryLog;
+using System.ComponentModel.DataAnnotations;
+using Vodovoz.Domain.Logistic;
+using Vodovoz.Domain.Logistic.Cars;
+
+namespace VodovozBusiness.Domain.Logistic
+{
+	[Appellative(Gender = GrammaticalGender.Masculine,
+		NominativePlural = "дополнительные виды топлива",
+		Nominative = "дополнительный вид топлива",
+		GenitivePlural = "дополнительных видов топлива")]
+	[EntityPermission]
+	[HistoryTrace]
+	public class AdditionalFuelType : PropertyChangedBase, IDomainObject
+	{
+		private int _id;
+		private Car _car;
+		private FuelType _fuelType;
+
+		/// <summary>
+		/// Код
+		/// </summary>
+		[Display(Name = "Код")]
+		public virtual int Id
+		{
+			get => _id;
+			set => SetField(ref _id, value);
+		}
+
+		/// <summary>
+		/// Автомобиль
+		/// </summary>
+		[Display(Name = "Автомобиль")]
+		public virtual Car Car
+		{
+			get => _car;
+			set => SetField(ref _car, value);
+		}
+
+		/// <summary>
+		/// Вид топлива
+		/// </summary>
+		[Display(Name = "Вид топлива")]
+		public virtual FuelType FuelType
+		{
+			get => _fuelType;
+			set => SetField(ref _fuelType, value);
+		}
+	}
+}
---
+++ Source/Vodovoz.Data.NHibernate/HibernateMapping/Logistic/AdditionalFuelTypeMap.cs
@@ -0,0 +1,25 @@
+﻿using FluentNHibernate.Mapping;
+using VodovozBusiness.Domain.Logistic;
+
+namespace Vodovoz.Data.NHibernate.HibernateMapping.Logistic
+{
+	public class AdditionalFuelTypeMap : ClassMap<AdditionalFuelType>
+	{
+		public AdditionalFuelTypeMap()
+		{
+			Table("additional_fuel_types");
+
+			HibernateMapping.DefaultAccess.CamelCaseField(Prefix.Underscore);
+
+			Id(x => x.Id)
+				.Column("id")
+				.GeneratedBy.Native();
+
+			References(x => x.Car)
+				.Column("car_id");
+
+			References(x => x.FuelType)
+				.Column("fuel_type_id");
+		}
+	}
+}
