33626bda6cef377d80fba084cd05ee8fb09ec064
N-Sem <nikiforov.semen@gmail.com>
2026-09-09 23:53:05 +0300

    Логика вью-модели и привязки вью

--- Source/Applications/Desktop/Vodovoz/Views/Logistic/AdditionalFuelTypeManagementView.cs
+++ Source/Applications/Desktop/Vodovoz/Views/Logistic/AdditionalFuelTypeManagementView.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Gamma.ColumnConfig;
+using QS.Views.GtkUI;
+using Vodovoz.Domain.Logistic;
+using Vodovoz.ViewModels.Widgets.Cars;
 namespace Vodovoz.Views.Logistic
 {
 	[System.ComponentModel.ToolboxItem(true)]
-	public partial class AdditionalFuelTypeManagementView : Gtk.Bin
+	public partial class AdditionalFuelTypeManagementView : WidgetViewBase<AdditionalFuelTypeManagementViewModel>
 	{
 		public AdditionalFuelTypeManagementView()
 		{
-			this.Build();
+			Build();
+		}
+
+		protected override void ConfigureWidget()
+		{
+			base.ConfigureWidget();
+
+			yvboxMain.Sensitive = ViewModel.CanEditAdditionalFuelTypes;
+
+			entityentryFuelType.ViewModel = ViewModel.FuelTypeViewModel;
+
+			ytreeviewFuelTypes.ColumnsConfig = FluentColumnsConfig<FuelType>.Create()
+				.AddColumn("Вид топлива")
+					.AddTextRenderer(x => x.Name).XAlign(0.5f)
+				.AddColumn("")
+				.Finish();
+
+			ytreeviewFuelTypes.Binding
+				.AddBinding(ViewModel, vm => vm.SelectedExistingFuelType, w => w.SelectedRow)
+				.InitializeFromSource();
+
+			ytreeviewFuelTypes.ItemsDataSource = ViewModel.Entity.AdditionalFuelTypes;
+
+			ybuttonAdd.BindCommand(ViewModel.AddFuelTypeCommand);
+			ybuttonRemove.BindCommand(ViewModel.RemoveFuelTypeCommand);
 		}
 	}
 }
--- Source/Applications/Desktop/VodovozViewModels/Widgets/Cars/AdditionalFuelTypeManagementViewModel.cs
+++ Source/Applications/Desktop/VodovozViewModels/Widgets/Cars/AdditionalFuelTypeManagementViewModel.cs
@@ -1,9 +1,13 @@
-﻿using QS.Services;
+﻿using QS.Commands;
+using QS.Dialog;
+using QS.DomainModel.Entity;
+using QS.Services;
 using QS.ViewModels;
 using QS.ViewModels.Control.EEVM;
 using QS.ViewModels.Dialog;
 using System;
 using Vodovoz.Core.Application.Logistics.Fuel;
+using Vodovoz.Core.Domain.Permissions;
 using Vodovoz.Domain.Logistic;
 using Vodovoz.Domain.Logistic.Cars;
 using Vodovoz.ViewModels.Dialogs.Fuel;
@@ -13,6 +17,11 @@ namespace Vodovoz.ViewModels.Widgets.Cars
 	public class AdditionalFuelTypeManagementViewModel : EntityWidgetViewModelBase<Car>
 	{
 		private readonly AdditionalFuelTypeManagementService _additionalFuelTypeManagementService;
+		private readonly IInteractiveService _interactiveService;
+		private readonly ICurrentPermissionService _currentPermissionService;
+
+		private FuelType _selectedNewFuelType;
+		private FuelType _selectedExistingFuelType;
 
 		public AdditionalFuelTypeManagementViewModel(
 			Car entity,
@@ -35,17 +44,90 @@ namespace Vodovoz.ViewModels.Widgets.Cars
 			_additionalFuelTypeManagementService = additionalFuelTypeManagementService
 				?? throw new ArgumentNullException(nameof(additionalFuelTypeManagementService));
 
+			_interactiveService = commonServices.InteractiveService;
+			_currentPermissionService = commonServices.CurrentPermissionService;
+
 			FuelTypeViewModel = fuelTypeEEVMBuilder
 				.SetUnitOfWork(UoW)
 				.SetViewModel(parentDialog)
-				.ForProperty(Entity, x => x.FuelType)
+				.ForProperty(this, x => x.SelectedNewFuelType)
 				.UseViewModelJournalAndAutocompleter<FuelTypeJournalViewModel>()
 				.UseViewModelDialog<FuelTypeViewModel>()
 				.Finish(); ;
 
 			_additionalFuelTypeManagementService.Initialize(Entity);
+
+			AddFuelTypeCommand = new DelegateCommand(AddFuelType, () => CanAddFuelType);
+			AddFuelTypeCommand.CanExecuteChangedWith(this, x => x.CanAddFuelType);
+
+			RemoveFuelTypeCommand = new DelegateCommand(RemoveFuelType, () => CanRemoveFuelType);
+			RemoveFuelTypeCommand.CanExecuteChangedWith(this, x => x.CanRemoveFuelType);
 		}
 
 		public IEntityEntryViewModel FuelTypeViewModel { get; }
+
+		public DelegateCommand AddFuelTypeCommand { get; }
+		public DelegateCommand RemoveFuelTypeCommand { get; }
+
+		public bool CanEditAdditionalFuelTypes =>
+			((Entity.Id == 0 && _currentPermissionService.ValidateEntityPermission(typeof(Car)).CanCreate)
+				|| _currentPermissionService.ValidateEntityPermission(typeof(Car)).CanUpdate)
+			&& _currentPermissionService.ValidatePresetPermission(LogisticPermissions.Car.CanEditCarCard);
+
+		[PropertyChangedAlso(nameof(CanAddFuelType))]
+		public FuelType SelectedNewFuelType
+		{
+			get => _selectedNewFuelType;
+			set => SetField(ref _selectedNewFuelType, value);
+		}
+
+		[PropertyChangedAlso(nameof(CanRemoveFuelType))]
+		public FuelType SelectedExistingFuelType
+		{
+			get => _selectedExistingFuelType;
+			set => SetField(ref _selectedExistingFuelType, value);
+		}
+
+		public bool CanAddFuelType =>
+			SelectedNewFuelType != null
+			&& _additionalFuelTypeManagementService.CanAddFuelType(SelectedNewFuelType);
+
+		public bool CanRemoveFuelType =>
+			SelectedExistingFuelType != null
+			&& _additionalFuelTypeManagementService.CanRemoveFuelType(SelectedExistingFuelType);
+
+		public void AddFuelType()
+		{
+			if(SelectedNewFuelType is null)
+			{
+				_interactiveService.ShowMessage(ImportanceLevel.Error, "Пожалуйста, выберите тип топлива для добавления");
+				return;
+			}
+
+			var addFuelTypeResult =
+				_additionalFuelTypeManagementService.AddFuelType(SelectedNewFuelType);
+
+			if(addFuelTypeResult.IsFailure)
+			{
+				_interactiveService.ShowMessage(ImportanceLevel.Error, addFuelTypeResult.GetErrorsString());
+			}
+		}
+
+		public void RemoveFuelType()
+		{
+			if(SelectedExistingFuelType is null)
+			{
+				_interactiveService.ShowMessage(ImportanceLevel.Error, "Пожалуйста, выберите тип топлива для удаления");
+				return;
+			}
+
+			var removeFuelTypeResult =
+				_additionalFuelTypeManagementService.RemoveFuelType(SelectedExistingFuelType);
+
+			if(removeFuelTypeResult.IsFailure)
+			{
+				_interactiveService.ShowMessage(ImportanceLevel.Error, removeFuelTypeResult.GetErrorsString());
+			}
+		}
 	}
 }
