7b1fe59bdc8532dc29da38cdec6b48a8cd95e64e
N-Sem <nikiforov.semen@gmail.com>
2026-09-14 16:07:18 +0300

    Добавлен фильтр нового журнал

---
+++ Source/Applications/Desktop/Vodovoz/Filters/GtkViews/EdoDeviationFilterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Filters.GtkViews
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoDeviationFilterView : Gtk.Bin
+	{
+		public EdoDeviationFilterView()
+		{
+			this.Build();
+		}
+	}
+}
