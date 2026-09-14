89f63bd869b5ed5ab794650ac7d405e056a4aaf7
Vadim Gordeev <enzogord@gmail.com>
2026-09-14 17:29:52 +0300

    ui3

---
+++ Source/Applications/Desktop/Vodovoz/Views/Edo/EdoDiscrepancyDialogView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Edo
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoDiscrepancyDialogView : Gtk.Bin
+	{
+		public EdoDiscrepancyDialogView()
+		{
+			this.Build();
+		}
+	}
+}
