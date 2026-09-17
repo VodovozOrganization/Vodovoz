037b2c8955ff2efacf405e99a5e634bcd06bedf2
Andrey <as1854@yandex.ru>
2026-09-17 09:02:29 +0300

    перенес вьюху и вью модель шаблона

---
+++ Source/Applications/Desktop/Vodovoz/Views/Orders/OnlineOrderTemplateView.cs
@@ -0,0 +1,21 @@
+﻿using QS.Views;
+using QS.Views.GtkUI;
+using Vodovoz.ViewModels.ViewModels.Orders;
+
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class OnlineOrderTemplateView : TabViewBase<OnlineOrderTemplateViewModel>
+	{
+		public OnlineOrderTemplateView(OnlineOrderTemplateViewModel viewModel) : base(viewModel)
+		{
+			Build();
+			Configure();
+		}
+
+		private void Configure()
+		{
+			
+		}
+	}
+}
