9ccf4c098f3d420e587c989782504f9679316326
Andrey <as1854@yandex.ru>
2026-09-25 11:26:04 +0300

    док блоки ноды

--- Source/Applications/Desktop/VodovozViewModels/Dialogs/Nodes/NomenclatureOnlinePricesNode.cs
+++ Source/Applications/Desktop/VodovozViewModels/Dialogs/Nodes/NomenclatureOnlinePricesNode.cs
@@ -2,8 +2,14 @@
 
 namespace Vodovoz.ViewModels.Dialogs.Nodes
 {
+	/// <summary>
+	/// Нода онлайн цен ИПЗ
+	/// </summary>
 	public class NomenclatureOnlinePricesNode
 	{
+		/// <summary>
+		/// Минимальное количество, при котором будет действовать цена
+		/// </summary>
 		public decimal MinCount
 		{
 			get
@@ -18,13 +24,40 @@ namespace Vodovoz.ViewModels.Dialogs.Nodes
 					: default(decimal);
 			}
 		}
+		
+		/// <summary>
+		/// Цена для МП
+		/// </summary>
 		public NomenclatureOnlinePrice MobileAppNomenclatureOnlinePrice { get; set; }
+		
+		/// <summary>
+		/// Цена для сайта ВВ
+		/// </summary>
 		public NomenclatureOnlinePrice VodovozWebSiteNomenclatureOnlinePrice { get; set; }
+		
+		/// <summary>
+		/// Цена для сайта Кулер сэйл
+		/// </summary>
 		public NomenclatureOnlinePrice KulerSaleWebSiteNomenclatureOnlinePrice { get; set; }
+		
+		/// <summary>
+		/// Цена для ИИ бота
+		/// </summary>
 		public NomenclatureOnlinePrice AiBotNomenclatureOnlinePrice { get; set; }
+		
+		/// <summary>
+		/// Прайсовая цена
+		/// </summary>
 		public decimal? NomenclaturePrice => MobileAppNomenclatureOnlinePrice?.NomenclaturePrice.Price;
+		
+		/// <summary>
+		/// Альтернативная цена(для Кулер сэйл)
+		/// </summary>
 		public decimal? KulerSalePrice => KulerSaleWebSiteNomenclatureOnlinePrice?.NomenclaturePrice.Price;
 
+		/// <summary>
+		/// Цена без скидки для МП
+		/// </summary>
 		public string MobileAppPriceWithoutDiscountString
 		{
 			get => MobileAppNomenclatureOnlinePrice?.PriceWithoutDiscount.ToString();
@@ -45,6 +78,9 @@ namespace Vodovoz.ViewModels.Dialogs.Nodes
 			}
 		} 
 
+		/// <summary>
+		/// Цена без скидки для сайта ВВ
+		/// </summary>
 		public string VodovozWebSitePriceWithoutDiscountString
 		{
 			get => VodovozWebSiteNomenclatureOnlinePrice?.PriceWithoutDiscount.ToString();
@@ -65,6 +101,9 @@ namespace Vodovoz.ViewModels.Dialogs.Nodes
 			}
 		}
 
+		/// <summary>
+		/// Цена без скидки для сайта Кулер сэйл
+		/// </summary>
 		public string KulerSaleWebSitePriceWithoutDiscountString
 		{
 			get => KulerSaleWebSiteNomenclatureOnlinePrice?.PriceWithoutDiscount.ToString();
@@ -85,6 +124,9 @@ namespace Vodovoz.ViewModels.Dialogs.Nodes
 			}
 		}
 		
+		/// <summary>
+		/// Цена без скидки для ИИ бота
+		/// </summary>
 		public string AiBotPriceWithoutDiscountString
 		{
 			get => AiBotNomenclatureOnlinePrice?.PriceWithoutDiscount.ToString();
@@ -105,9 +147,24 @@ namespace Vodovoz.ViewModels.Dialogs.Nodes
 			}
 		}
 		
+		/// <summary>
+		/// Возможность изменять цену без скидки для МП
+		/// </summary>
 		public bool CanChangeMobileAppPriceWithoutDiscount => NomenclaturePrice.HasValue;
+		
+		/// <summary>
+		/// Возможность изменять цену без скидки для сайта ВВ
+		/// </summary>
 		public bool CanChangeVodovozWebSitePriceWithoutDiscount => NomenclaturePrice.HasValue;
+		
+		/// <summary>
+		/// Возможность изменять цену без скидки для сайта Кулер сэйл
+		/// </summary>
 		public bool CanChangeKulerSaleWebSitePriceWithoutDiscount => KulerSalePrice.HasValue;
+		
+		/// <summary>
+		/// Возможность изменять цену без скидки для ИИ бота
+		/// </summary>
 		public bool CanChangeAiBotPriceWithoutDiscount => NomenclaturePrice.HasValue;
 	}
 }
